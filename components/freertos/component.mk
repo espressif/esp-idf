3b1c3dab4b7b40fe805b39bd572da38540cf8441
Angus Gratton <angus@espressif.com>
2016-09-01 09:19:06 +1000

    Name component makefiles component.mk instead of Makefile
    Fixes problems with Eclipse trying to build in directories it shouldn't.
    
    This is a breaking change for existing repositories, they need to rename
    any component Makefiles to component.mk and rename their references to
    $(IDF_PATH)/make/component.mk to $(IDF_PATH)/make/component_common.mk

--- components/freertos/component.mk
+++ components/freertos/component.mk
@@ -6,4 +6,4 @@ COMPONENT_ADD_LDFLAGS = -l$(COMPONENT_NAME) -Wl,--undefined=uxTopUsedPriority
 COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENT_PRIV_INCLUDEDIRS := include/freertos
 
-include $(IDF_PATH)/make/component.mk
+include $(IDF_PATH)/make/component_common.mk
