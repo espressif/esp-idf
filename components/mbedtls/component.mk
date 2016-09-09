7c58c1e06b9704d5c0dfea6248e2f1f1e2d10129
Angus Gratton <angus@espressif.com>
2016-09-09 10:14:38 +1000

    Build system: Allow components to add to the global CFLAGS via Makefile.projbuild
    Used by mbedTLS to set MBEDTLS_CONFIG_FILE in all components.
    
    This change sets CFLAGS/etc at the project level and then exports those
    variables for components, rather than setting them independently each time
    a component Makefile is invoked.

--- components/mbedtls/component.mk
+++ components/mbedtls/component.mk
@@ -6,6 +6,4 @@ COMPONENT_ADD_INCLUDEDIRS := port/include include
 
 COMPONENT_SRCDIRS := library port
 
-EXTRA_CFLAGS += -DMBEDTLS_CONFIG_FILE='"mbedtls/esp_config.h"'
-
 include $(IDF_PATH)/make/component_common.mk
--- components/tcpip_adapter/component.mk
+++ components/tcpip_adapter/component.mk
@@ -2,6 +2,6 @@
 # Component Makefile
 #
 
-EXTRA_CFLAGS := -DLWIP_ESP8266
+CFLAGS += -DLWIP_ESP8266
 
 include $(IDF_PATH)/make/component_common.mk
