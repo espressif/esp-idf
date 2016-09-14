f01cabf71d5f2a26c4bdd8a944e79b181f1761f5
Angus Gratton <angus@espressif.com>
2016-09-14 17:52:24 +1000

    mbedtls hwcrypto sha512: Fix redirection of function names

--- components/mbedtls/port/include/sha512_alt.h
+++ components/mbedtls/port/include/sha512_alt.h
@@ -17,12 +17,12 @@ extern "C" {
 typedef esp_sha_context  mbedtls_sha512_context;
 
 #define mbedtls_sha512_init     esp_sha512_init
-#define mbedtls_sha512_process  esp_sha512_process
 #define mbedtls_sha512_clone    esp_sha512_clone
 #define mbedtls_sha512_starts   esp_sha512_start
 #define mbedtls_sha512_update   esp_sha512_update
 #define mbedtls_sha512_finish   esp_sha512_finish
-#define mbedtls_sha512_free(...)
+#define mbedtls_sha512_free     esp_sha512_free
+#define mbedtls_sha512_process(...)
 
 #endif
 
