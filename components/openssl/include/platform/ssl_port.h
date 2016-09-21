b89168d0f1be251fce1b37bfe0684ae097da30f7
dongheng <dongheng@espressif.com>
2016-09-21 17:51:12 +0800

    components/openssl: add ssl_port.c & .h file

---
+++ components/openssl/include/platform/ssl_port.h
@@ -0,0 +1,29 @@
+/* Copyright 2015-2016 Espressif Systems (Wuxi) PTE LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _SSL_PORT_H_
+#define _SSL_PORT_H_
+
+#include "esp_types.h"
+
+void* ssl_zalloc(size_t size);
+void *ssl_malloc(size_t size);
+void ssl_free(void *p);
+void* ssl_memcpy(void *to, const void *from, size_t size);
+
+void ssl_speed_up_enter(void);
+void ssl_speed_up_exit(void);
+
+#endif
---
+++ components/openssl/platform/ssl_port.c
@@ -0,0 +1,56 @@
+/* Copyright 2015-2016 Espressif Systems (Wuxi) PTE LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string.h>
+#include "ssl_port.h"
+#include "malloc.h"
+
+/*********************************************************************************************/
+/********************************* SSL general interface *************************************/
+
+void* ssl_zalloc(size_t size)
+{
+    void *p = malloc(size);
+
+    if (p)
+        memset(p, 0, size);
+
+    return p;
+}
+
+void *ssl_malloc(size_t size)
+{
+    return ssl_zalloc(size);
+}
+
+void ssl_free(void *p)
+{
+    free(p);
+}
+
+void* ssl_memcpy(void *to, const void *from, size_t size)
+{
+    return memcpy(to, from, size);
+}
+
+void ssl_speed_up_enter(void)
+{
+
+}
+
+void ssl_speed_up_exit(void)
+{
+
+}
