5c5f7eb7feb16ba2632ad4701d7ef283a3371fec
dongheng <dongheng@espressif.com>
2016-09-23 14:53:19 +0800

    components/openssl: add openssl stack object function

---
+++ components/openssl/include/internal/ssl_stack.h
@@ -0,0 +1,33 @@
+#ifndef _SSL_STACK_H_
+#define _SSL_STACK_H_
+
+#include "ssl_types.h"
+
+/**
+ * @brief create a openssl stack object
+ *
+ * @param c - stack function
+ *
+ * @return openssl stack object point
+ */
+OPENSSL_STACK* OPENSSL_sk_new(OPENSSL_sk_compfunc c);
+
+/**
+ * @brief create a NULL function openssl stack object
+ *
+ * @param none
+ *
+ * @return openssl stack object point
+ */
+OPENSSL_STACK *OPENSSL_sk_new_null(void);
+
+/**
+ * @brief free openssl stack object
+ *
+ * @param openssl stack object point
+ *
+ * @return none
+ */
+void OPENSSL_sk_free(OPENSSL_STACK *stack);
+
+#endif
