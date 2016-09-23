5e6b2e9c4557a8d9a4e57866a5d0c152aa6a8229
Ivan Grokhotkov <ivan@espressif.com>
2016-09-23 14:46:39 +0800

    clean up warnings
    For third party components (lwip and expat), compilation flags are adjusted to silence existing warnings (i have manually checked that all warnings are benign).
    In components/esp32, replaced use of WIFI_DEBUG with ESP_LOG functions.
    Only remaining warning is in FreeRTOS queue.c, and it may be a useful one.

--- components/lwip/component.mk
+++ components/lwip/component.mk
@@ -6,6 +6,6 @@ COMPONENT_ADD_INCLUDEDIRS := include/lwip include/lwip/port include/lwip/posix
 
 COMPONENT_SRCDIRS := api apps/sntp apps core/ipv4 core/ipv6 core netif port/freertos port/netif port
 
-CFLAGS += -Wno-error=address -Waddress
+CFLAGS += -Wno-address -Wno-unused-variable -Wno-unused-but-set-variable
 
 include $(IDF_PATH)/make/component_common.mk
