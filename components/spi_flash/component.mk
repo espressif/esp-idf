1413ec3ff0ba9409bbb9f839be644be25543f20f
Angus Gratton <angus@espressif.com>
2016-10-21 17:08:05 +1100

    Remove SPIUnlock from linker script symbols
    Add a comment about why it was removed and where it went.

--- components/spi_flash/component.mk
+++ components/spi_flash/component.mk
@@ -1,3 +1,8 @@
 COMPONENT_ADD_INCLUDEDIRS := include
 
+ifdef IS_BOOTLOADER_BUILD
+# Bootloader needs updated SPIUnlock from this file
+COMPONENT_OBJS := spi_flash_rom_patch.o
+endif
+
 include $(IDF_PATH)/make/component_common.mk
