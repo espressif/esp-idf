858fe9815b37068bef9abd8b99e534876d7e3caa
Dong Heng <dongheng@espressif.com>
2016-11-15 11:12:58 +0800

    feature/openssl: add openssl client demo

--- examples/09_openssl_client/main/component.mk
+++ examples/09_openssl_client/main/component.mk
@@ -1,7 +1,7 @@
 #
 # Main Makefile. This is basically the same as a component makefile.
 #
-# This Makefile should, at the very least, just include $(SDK_PATH)/make/component_common.mk. By default, 
+# This Makefile should, at the very least, just include $(IDF_PATH)/make/component_common.mk. By default, 
 # this will take the sources in the src/ directory, compile them and link them into 
 # lib(subdirectory_name).a in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
