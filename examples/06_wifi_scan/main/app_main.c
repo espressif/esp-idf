062477c14d49e8dd2dca9332f906c959f4e1824f
Jack Tan <jiankemeng@gmail.com>
2016-11-15 13:14:10 +0800

    examples/wifi-scan: change the license header

--- examples/06_wifi_scan/main/app_main.c
+++ examples/06_wifi_scan/main/app_main.c
@@ -1,7 +1,12 @@
 /*
- *  MaiKe Labs (2016 - 2026)
+ * MaiKe Labs (2016 - 2026)
  *
- *  Written by Jack Tan <jiankemeng@gmail.com>
+ * Written by Jack Tan <jiankemeng@gmail.com>
+ *
+ * This example code is in the Public Domain (or CC0 licensed, at your option.)
+ * Unless required by applicable law or agreed to in writing, this
+ * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
  *
 */
 #include <stdio.h>
