9ae07082ff97ed712a4ee99c38c3a8d874454c17
Angus Gratton <angus@espressif.com>
2016-11-16 14:20:47 +1100

    examples: Update component.mk for new conventions

--- examples/01_hello_world/main/component.mk
+++ examples/01_hello_world/main/component.mk
@@ -1,5 +1,5 @@
 #
-# Main Makefile. This is basically the same as a component makefile.
+# "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
