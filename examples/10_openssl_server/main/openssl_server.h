2b196b4f3e2a7f23fd2a15a690207cea8794ad36
Dong Heng <dongheng@espressif.com>
2016-11-18 10:07:34 +0800

    components/openssl_demo: remove the apache license header and add cc license head

--- examples/10_openssl_server/main/openssl_server.h
+++ examples/10_openssl_server/main/openssl_server.h
@@ -1,3 +1,12 @@
+/* OpenSSL server Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
 #ifndef _OPENSSL_DEMO_H_
 #define _OPENSSL_DEMO_H_
 
