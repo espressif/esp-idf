1cdd082a9e14916d751a175c632587f84ede2f53
Xia Xiaotian <xiaxiaotian@espressif.com>
2016-11-24 20:41:44 +0800

    wpa2 enterprise: update wifi lib in master branch

--- components/esp32/include/esp_wpa2.h
+++ components/esp32/include/esp_wpa2.h
@@ -1,3 +1,17 @@
+// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef ESP_WPA2_H
 #define ESP_WPA2_H
 
@@ -63,7 +77,6 @@ void esp_wifi_sta_clear_enterprise_username(void);
   *         ESP_ERR_WIFI_ARG: fail(len <= 0)
   *         ESP_ERR_WIFI_NO_MEM: fail(internal memory malloc fail)
   */
-
 esp_err_t esp_wifi_sta_set_enterprise_password(unsigned char *password, int len);
 
 /**
