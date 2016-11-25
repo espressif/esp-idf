a9d5e2674831e174ee160e8eabd369ccc06c8397
Angus Gratton <angus@espressif.com>
2016-11-25 14:13:05 +1100

    Secure boot: Correctly re-sign if signing key changes, better error if missing

--- components/bootloader_support/component.mk
+++ components/bootloader_support/component.mk
@@ -17,14 +17,6 @@ ifdef CONFIG_SECURE_BOOT_ENABLED
 # this path is created relative to the component build directory
 SECURE_BOOT_VERIFICATION_KEY := $(abspath signature_verification_key.bin)
 
-$(SECURE_BOOT_SIGNING_KEY):
-	@echo "Need to generate secure boot signing key."
-	@echo "One way is to run this command:"
-	@echo "$(ESPSECUREPY) generate_signing_key $@"
-	@echo "Keep key file safe after generating."
-	@echo "(See secure boot documentation for risks & alternatives.)"
-	@exit 1
-
 $(SECURE_BOOT_VERIFICATION_KEY): $(SECURE_BOOT_SIGNING_KEY)
 	$(ESPSECUREPY) extract_public_key --keyfile $< $@
 
