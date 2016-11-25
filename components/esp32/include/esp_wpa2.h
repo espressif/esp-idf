6e3b7f4ea217eb74e9aa137535f92e7aeaab2502
Xia Xiaotian <xiaxiaotian@espressif.com>
2016-11-25 18:08:21 +0800

    wpa2 enterprise: change wpa2 enterprise APIs to esp_wifi_sta_wpa2_ent_XXX

--- components/esp32/include/esp_wpa2.h
+++ components/esp32/include/esp_wpa2.h
@@ -30,7 +30,7 @@ extern "C" {
   * @return ESP_ERR_WIFI_OK: succeed.
   *         ESP_ERR_WIFI_NO_MEM: fail(internal memory malloc fail)
   */
-esp_err_t esp_wifi_sta_wpa2_enterprise_enable(void);
+esp_err_t esp_wifi_sta_wpa2_ent_enable(void);
 
 /**
   * @brief  Disable wpa2 enterprise authentication.
@@ -40,7 +40,7 @@ esp_err_t esp_wifi_sta_wpa2_enterprise_enable(void);
   *
   * @return ESP_ERR_WIFI_OK: succeed.
   */
-esp_err_t esp_wifi_sta_wpa2_enterprise_disable(void);
+esp_err_t esp_wifi_sta_wpa2_ent_disable(void);
 
 /**
   * @brief  Set username for PEAP/TTLS method.
@@ -54,12 +54,12 @@ esp_err_t esp_wifi_sta_wpa2_enterprise_disable(void);
   *         ESP_ERR_WIFI_ARG: fail(len <= 0 or len >= 128)
   *         ESP_ERR_WIFI_NO_MEM: fail(internal memory malloc fail)
   */
-esp_err_t esp_wifi_sta_set_enterprise_username(unsigned char *username, int len);
+esp_err_t esp_wifi_sta_wpa2_ent_set_username(unsigned char *username, int len);
 
 /**
   * @brief  Clear username for PEAP/TTLS method.
   */
-void esp_wifi_sta_clear_enterprise_username(void);
+void esp_wifi_sta_wpa2_ent_clear_username(void);
 
 /**
   * @brief  Set password for PEAP/TTLS method..
@@ -73,12 +73,12 @@ void esp_wifi_sta_clear_enterprise_username(void);
   *         ESP_ERR_WIFI_ARG: fail(len <= 0)
   *         ESP_ERR_WIFI_NO_MEM: fail(internal memory malloc fail)
   */
-esp_err_t esp_wifi_sta_set_enterprise_password(unsigned char *password, int len);
+esp_err_t esp_wifi_sta_wpa2_ent_set_password(unsigned char *password, int len);
 
 /**
   * @brief  Clear password for PEAP/TTLS method..
   */
-void esp_wifi_sta_clear_enterprise_password(void);
+void esp_wifi_sta_wpa2_ent_clear_password(void);
 
 /**
   * @brief  Set new password for MSCHAPv2 method..
@@ -94,12 +94,12 @@ void esp_wifi_sta_clear_enterprise_password(void);
   *         ESP_ERR_WIFI_NO_MEM: fail(internal memory malloc fail)
   */
 
-esp_err_t esp_wifi_sta_set_enterprise_new_password(unsigned char *password, int len);
+esp_err_t esp_wifi_sta_wpa2_ent_set_new_password(unsigned char *password, int len);
 
 /**
   * @brief  Clear new password for MSCHAPv2 method..
   */
-void esp_wifi_sta_clear_enterprise_new_password(void);
+void esp_wifi_sta_wpa2_ent_clear_new_password(void);
 
 /**
   * @brief  Set CA certificate for PEAP/TTLS method.
@@ -112,12 +112,12 @@ void esp_wifi_sta_clear_enterprise_new_password(void);
   *
   * @return ESP_ERR_WIFI_OK: succeed
   */
-esp_err_t esp_wifi_sta_set_enterprise_ca_cert(unsigned char *ca_cert, int len);
+esp_err_t esp_wifi_sta_wpa2_ent_set_ca_cert(unsigned char *ca_cert, int len);
 
 /**
   * @brief  Clear CA certificate for PEAP/TTLS method.
   */
-void esp_wifi_sta_clear_enterprise_ca_cert(void);
+void esp_wifi_sta_wpa2_ent_clear_ca_cert(void);
 
 /**
   * @brief  Set client certificate and key.
@@ -134,12 +134,12 @@ void esp_wifi_sta_clear_enterprise_ca_cert(void);
   *
   * @return ESP_ERR_WIFI_OK: succeed
   */
-esp_err_t esp_wifi_sta_set_enterprise_cert_key(unsigned char *client_cert, int client_cert_len, unsigned char *private_key, int private_key_len, unsigned char *private_key_passwd, int private_key_passwd_len);
+esp_err_t esp_wifi_sta_wpa2_ent_set_cert_key(unsigned char *client_cert, int client_cert_len, unsigned char *private_key, int private_key_len, unsigned char *private_key_passwd, int private_key_passwd_len);
 
 /**
   * @brief  Clear client certificate and key.
   */
-void esp_wifi_sta_clear_enterprise_cert_key(void);
+void esp_wifi_sta_wpa2_ent_clear_cert_key(void);
 
 /**
   * @brief  Set wpa2 enterprise certs time check(disable or not).
@@ -149,7 +149,7 @@ void esp_wifi_sta_clear_enterprise_cert_key(void);
   *
   * @return  ESP_OK: succeed
   */
-esp_err_t esp_wifi_sta_set_enterprise_disable_time_check(bool disable);
+esp_err_t esp_wifi_sta_wpa2_ent_set_disable_time_check(bool disable);
 
 /**
   * @brief  Get wpa2 enterprise certs time check(disable or not).
@@ -158,7 +158,7 @@ esp_err_t esp_wifi_sta_set_enterprise_disable_time_check(bool disable);
   *
   * @return  ESP_OK: succeed
   */
-esp_err_t esp_wifi_sta_get_enterprise_disable_time_check(bool *disable);
+esp_err_t esp_wifi_sta_wpa2_ent_get_disable_time_check(bool *disable);
 
 #ifdef __cplusplus
 }
