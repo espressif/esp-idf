94d2f776432da7da68e4b6ecaed58b8ac4b2eb1f
Angus Gratton <angus@espressif.com>
2016-12-06 10:52:40 -0800

    build system: Remove FLAGS_XXX variable option, replace with per-target overrides
    Use for targeted disabling of warnings in LWIP.

--- components/lwip/component.mk
+++ components/lwip/component.mk
@@ -6,5 +6,7 @@ COMPONENT_ADD_INCLUDEDIRS := include/lwip include/lwip/port include/lwip/posix
 
 COMPONENT_SRCDIRS := api apps/sntp apps core/ipv4 core/ipv6 core netif port/freertos port/netif port
 
-CFLAGS += -Wno-address -Wno-unused-variable -Wno-unused-but-set-variable
+CFLAGS += -Wno-address  # lots of LWIP source files evaluate macros that check address of stack variables
 
+api/tcpip.o apps/dhcpserver.o: CFLAGS += -Wno-unused-variable
+apps/dhcpserver.o core/pbuf.o core/tcp_in.o: CFLAGS += -Wno-unused-but-set-variable
