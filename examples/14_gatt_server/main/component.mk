c277735e6ec8445facac3227d539a2248b33ee0f
Tian Hao <tianhao@espressif.com>
2016-12-07 20:04:44 +0800

    component/bt : cleanup demo warnings

--- examples/14_gatt_server/main/component.mk
+++ examples/14_gatt_server/main/component.mk
@@ -6,5 +6,3 @@
 # lib(subdirectory_name).a in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
 #
-
-include $(IDF_PATH)/make/component_common.mk
