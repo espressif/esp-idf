cbff82cc44c4052715ae3187455fb90a5c760ba1
Deomid Ryabkov <rojer@cesanta.com>
2016-12-14 19:30:40 +0000

    Allow gw to be null
    Allow interfaces to be configured without a default gateway, for
    local-only communication.
    
    In case of the AP interface, if gw is not set, do not offer it.

--- components/tcpip_adapter/tcpip_adapter_lwip.c
+++ components/tcpip_adapter/tcpip_adapter_lwip.c
@@ -213,7 +213,7 @@ esp_err_t tcpip_adapter_set_ip_info(tcpip_adapter_if_t tcpip_if, tcpip_adapter_i
     tcpip_adapter_dhcp_status_t status;
 
     if (tcpip_if >= TCPIP_ADAPTER_IF_MAX || ip_info == NULL ||
-            ip4_addr_isany_val(ip_info->ip) || ip4_addr_isany_val(ip_info->netmask) || ip4_addr_isany_val(ip_info->gw)) {
+            ip4_addr_isany_val(ip_info->ip) || ip4_addr_isany_val(ip_info->netmask)) {
         return ESP_ERR_TCPIP_ADAPTER_INVALID_PARAMS;
     }
 
