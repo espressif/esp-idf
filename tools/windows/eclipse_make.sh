d28ee9a25ed504ebf4cdbfc13b6aed00ea766cc9
Angus Gratton <angus@espressif.com>
2017-01-04 12:36:59 +1100

    build system: Account for Windows behaviour of make wildcard for some dirs
    See github #166

--- tools/windows/eclipse_make.sh
+++ tools/windows/eclipse_make.sh
@@ -4,6 +4,7 @@
 # Eclipse's output parser expects to see output of the form C:/dir/dir/file but our Make
 # process uses MinGW paths of the form /c/dir/dir/file. So parse these out...
 #
-# (regexp deliberate only matches after a space character to try and avoid false-positives.)
+# A little hacky as it looks for any single character of form /X/something.
+#
 echo "Running make in $(pwd)"
-make $@ V=1 | sed -E "s@ /([a-z])/(.+)/@ \1:/\2/@g" | sed -E "s@-I/([a-z])/(.+)/@-I\1:/\2/@g" | sed -E "s@-L/([a-z])/(.+)/@-L\1:/\2/@g"
+make $@ V=1 | sed -E "s@/([a-z])/([^/+])@\1:/\2@g"
