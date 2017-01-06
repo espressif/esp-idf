4491dd0e2a2024e5c3fcc3e877aa459079179d87
Wu Jian Gang <wujiangang@espressif.com>
2017-01-06 18:42:46 +0800

    example: fix CI error of coap server demo

--- examples/24_coap_server/main/coap_server.c
+++ examples/24_coap_server/main/coap_server.c
@@ -6,9 +6,9 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include "coap_server.h"
 
 #include <string.h>
+#include <sys/socket.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -19,12 +19,20 @@
 #include "esp_event_loop.h"
 
 #include "nvs_flash.h"
-#include <sys/socket.h>
 
-#include "coap_config.h"
-#include "resource.h"
 #include "coap.h"
 
+/* The examples use simple WiFi configuration that you can set via
+   'make menuconfig'.
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define EXAMPLE_WIFI_SSID            CONFIG_WIFI_SSID
+#define EXAMPLE_WIFI_PASS            CONFIG_WIFI_PASSWORD
+
+#define COAP_DEFAULT_TIME_SEC 5
+#define COAP_DEFAULT_TIME_USEC 0
 
 static EventGroupHandle_t wifi_event_group;
 
@@ -33,32 +41,32 @@ static EventGroupHandle_t wifi_event_group;
    to the AP with an IP? */
 const static int CONNECTED_BIT = BIT0;
 
-const static char *TAG = "CoAP_demo";
+const static char *TAG = "CoAP_server";
 
 static coap_async_state_t *async = NULL;
 
 static void
 send_async_response(coap_context_t *ctx, const coap_endpoint_t *local_if)
 {
-	coap_pdu_t *response;
-	unsigned char buf[3];
-	const char* response_data     = "Hello World!";
-	size_t size = sizeof(coap_hdr_t) + 20;
-	response = coap_pdu_init(async->flags & COAP_MESSAGE_CON, COAP_RESPONSE_CODE(205), 0, size);
-	response->hdr->id = coap_new_message_id(ctx);
-	if (async->tokenlen)
-		coap_add_token(response, async->tokenlen, async->token);
-	coap_add_option(response, COAP_OPTION_CONTENT_TYPE, coap_encode_var_bytes(buf, COAP_MEDIATYPE_TEXT_PLAIN), buf);
-	coap_add_data  (response, strlen(response_data), (unsigned char *)response_data);
+    coap_pdu_t *response;
+    unsigned char buf[3];
+    const char* response_data     = "Hello World!";
+    size_t size = sizeof(coap_hdr_t) + 20;
+    response = coap_pdu_init(async->flags & COAP_MESSAGE_CON, COAP_RESPONSE_CODE(205), 0, size);
+    response->hdr->id = coap_new_message_id(ctx);
+    if (async->tokenlen)
+        coap_add_token(response, async->tokenlen, async->token);
+    coap_add_option(response, COAP_OPTION_CONTENT_TYPE, coap_encode_var_bytes(buf, COAP_MEDIATYPE_TEXT_PLAIN), buf);
+    coap_add_data  (response, strlen(response_data), (unsigned char *)response_data);
 
-	if (coap_send(ctx, local_if, &async->peer, response) == COAP_INVALID_TID) {
+    if (coap_send(ctx, local_if, &async->peer, response) == COAP_INVALID_TID) {
 
-	}
-	coap_delete_pdu(response);
-	coap_async_state_t *tmp;
-	coap_remove_async(ctx, async->id, &tmp);
-	coap_free_async(async);
-	async = NULL;
+    }
+    coap_delete_pdu(response);
+    coap_async_state_t *tmp;
+    coap_remove_async(ctx, async->id, &tmp);
+    coap_free_async(async);
+    async = NULL;
 }
 
 /*
@@ -69,76 +77,70 @@ async_handler(coap_context_t *ctx, struct coap_resource_t *resource,
               const coap_endpoint_t *local_interface, coap_address_t *peer,
               coap_pdu_t *request, str *token, coap_pdu_t *response)
 {
-	async = coap_register_async(ctx, peer, request, COAP_ASYNC_SEPARATE | COAP_ASYNC_CONFIRM, (void*)"no data");
+    async = coap_register_async(ctx, peer, request, COAP_ASYNC_SEPARATE | COAP_ASYNC_CONFIRM, (void*)"no data");
 }
 
 static void coap_demo_thread(void *p)
 {
-	coap_context_t*  ctx = NULL;
-	coap_address_t   serv_addr;
-	coap_resource_t* resource = NULL;
-	fd_set           readfds;
-	struct timeval tv;
-	int flags = 0;
-	/* Prepare the CoAP server socket */
-	coap_address_init(&serv_addr);
-	serv_addr.addr.sin.sin_family      = AF_INET;
-	serv_addr.addr.sin.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.addr.sin.sin_port        = htons(COAP_DEFAULT_PORT);
-	ctx                                = coap_new_context(&serv_addr);
-	if (ctx) {
-		flags = fcntl(ctx->sockfd, F_GETFL, 0);
-		fcntl(ctx->sockfd, F_SETFL, flags|O_NONBLOCK);
+    coap_context_t*  ctx = NULL;
+    coap_address_t   serv_addr;
+    coap_resource_t* resource = NULL;
+    fd_set           readfds;
+    struct timeval tv;
+    int flags = 0;
 
-		tv.tv_usec = COAP_DEFAULT_TIME_USEC;
-		tv.tv_sec = COAP_DEFAULT_TIME_SEC;
-		/* Initialize the resource */
-		resource = coap_resource_init((unsigned char *)"Espressif", 9, 0);
-		if (resource){
-			coap_register_handler(resource, COAP_REQUEST_GET, async_handler);
-			coap_add_resource(ctx, resource);
-			/*For incoming connections*/
-			for (;;) {
-				FD_ZERO(&readfds);
-				FD_CLR( ctx->sockfd, &readfds);
-				FD_SET( ctx->sockfd, &readfds);
+    while (1) {
+        /* Wait for the callback to set the CONNECTED_BIT in the
+           event group.
+        */
+        xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+                            false, true, portMAX_DELAY);
+        ESP_LOGI(TAG, "Connected to AP");
 
-				int result = select( FD_SETSIZE, &readfds, 0, 0, &tv );
-				if (result > 0){
-					if (FD_ISSET( ctx->sockfd, &readfds ))
-						coap_read(ctx);
-				} else if (result < 0){
-					break;
-				} else {
-					printf("select timeout\n");
-				}
+        /* Prepare the CoAP server socket */
+        coap_address_init(&serv_addr);
+        serv_addr.addr.sin.sin_family      = AF_INET;
+        serv_addr.addr.sin.sin_addr.s_addr = INADDR_ANY;
+        serv_addr.addr.sin.sin_port        = htons(COAP_DEFAULT_PORT);
+        ctx                                = coap_new_context(&serv_addr);
+        if (ctx) {
+            flags = fcntl(ctx->sockfd, F_GETFL, 0);
+            fcntl(ctx->sockfd, F_SETFL, flags|O_NONBLOCK);
 
-				if (async)
-					send_async_response(ctx, ctx->endpoint);
-			}
-		}
+            tv.tv_usec = COAP_DEFAULT_TIME_USEC;
+            tv.tv_sec = COAP_DEFAULT_TIME_SEC;
+            /* Initialize the resource */
+            resource = coap_resource_init((unsigned char *)"Espressif", 9, 0);
+            if (resource){
+                coap_register_handler(resource, COAP_REQUEST_GET, async_handler);
+                coap_add_resource(ctx, resource);
+                /*For incoming connections*/
+                for (;;) {
+                    FD_ZERO(&readfds);
+                    FD_CLR( ctx->sockfd, &readfds);
+                    FD_SET( ctx->sockfd, &readfds);
 
-		coap_free_context(ctx);
-	}
+                    int result = select( FD_SETSIZE, &readfds, 0, 0, &tv );
+                    if (result > 0){
+                        if (FD_ISSET( ctx->sockfd, &readfds ))
+                            coap_read(ctx);
+                    } else if (result < 0){
+                        break;
+                    } else {
+                        ESP_LOGE(TAG, "select timeout");
+                    }
 
-	vTaskDelete(NULL);
-}
+                    if (async) {
+                        send_async_response(ctx, ctx->endpoint);
+                    }
+                }
+            }
 
-static void coap_server_init(void)
-{
-    int ret = pdPASS;
-    xTaskHandle coap_handle = NULL;
-
-    ret = xTaskCreate(coap_demo_thread,
-                      COAP_DEMO_THREAD_NAME,
-                      COAP_DEMO_THREAD_STACK_WORDS,
-                      NULL,
-                      COAP_DEMO_THREAD_PRORIOTY,
-                      &coap_handle);
-
-    if (ret != pdPASS)  {
-        ESP_LOGI(TAG, "create thread %s failed", COAP_DEMO_THREAD_NAME);
+            coap_free_context(ctx);
+        }
     }
+
+    vTaskDelete(NULL);
 }
 
 static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
@@ -149,7 +151,6 @@ static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
         break;
     case SYSTEM_EVENT_STA_GOT_IP:
         xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-        coap_server_init();
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
         /* This is a workaround as ESP32 WiFi libs don't currently
@@ -179,7 +180,6 @@ static void wifi_conn_init(void)
     };
     ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
     ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-
     ESP_ERROR_CHECK( esp_wifi_start() );
 }
 
@@ -187,4 +187,6 @@ void app_main(void)
 {
     nvs_flash_init();
     wifi_conn_init();
+
+    xTaskCreate(coap_demo_thread, "coap", 2048, NULL, 5, NULL);
 }
