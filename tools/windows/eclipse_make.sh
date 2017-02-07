8dede8f8a46f42661665e9b924b7f455d06ef00a
Angus Gratton <angus@espressif.com>
2017-02-07 14:21:58 +1100

    Eclipse: Process Windows paths correctly using cygpath
    Includes splitting the Windows Eclipse setup doc into a separate page,
    as it has so many additional steps.
    
    Addresses github #17 and #166
    https://github.com/espressif/esp-idf/issues/17
    https://github.com/espressif/esp-idf/issues/166

--- tools/windows/eclipse_make.sh
+++ tools/windows/eclipse_make.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# A wrapper for make on Windows with Eclipse
-#
-# Eclipse's output parser expects to see output of the form C:/dir/dir/file but our Make
-# process uses MinGW paths of the form /c/dir/dir/file. So parse these out...
-#
-# A little hacky as it looks for any single character of form /X/something.
-#
-echo "Running make in $(pwd)"
-make $@ V=1 | sed -E "s@/([a-z])/([^/+])@\1:/\2@g"
+echo "eclipse_make.sh has been replaced with eclipse_make.py. Check the Windows Eclipse docs for the new command."
+echo "This shell script will continue to work until the next major release."
+python ${IDF_PATH}/tools/windows/eclipse_make.py $@
