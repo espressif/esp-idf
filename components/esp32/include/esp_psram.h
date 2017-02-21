6262253742aed25b496cbe6f6a99ccfa868b0d6c
Jeroen Domburg <jeroen@espressif.com>
2017-02-21 17:18:47 +0800

    Some fixes to MR comments

--- components/esp32/include/esp_psram.h
+++ components/esp32/include/esp_psram.h
@@ -19,9 +19,9 @@
 #include "esp_err.h"
 
 typedef enum {
-    PSRAM_CACHE_F80M_S40M = 0, //DO NOT USE FOR NOW
-    PSRAM_CACHE_F40M_S40M,     //SUPPORTED
-    PSRAM_CACHE_F80M_S80M,     //DO NOT USE FOR NOW
+    PSRAM_CACHE_F80M_S40M = 0, ///< DO NOT USE FOR NOW
+    PSRAM_CACHE_F40M_S40M,     ///< SUPPORTED
+    PSRAM_CACHE_F80M_S80M,     ///< DO NOT USE FOR NOW
     PSRAM_CACHE_MAX,
 } psram_cache_mode_t;
 
