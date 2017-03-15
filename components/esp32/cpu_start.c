378216e594e497dca89974e3e759972f023eb4b6
Jeroen Domburg <jeroen@espressif.com>
2017-03-15 18:17:35 +0800

    Change default mode to low/high for dualcore

--- components/esp32/cpu_start.c
+++ components/esp32/cpu_start.c
@@ -92,7 +92,7 @@ static const char* TAG = "cpu_start";
 #if CONFIG_FREERTOS_UNICORE
 #define PSRAM_MODE PSRAM_VADDR_MODE_NORMAL
 #else
-#define PSRAM_MODE PSRAM_VADDR_MODE_EVENODD
+#define PSRAM_MODE PSRAM_VADDR_MODE_LOWHIGH
 #endif
 
 /*
