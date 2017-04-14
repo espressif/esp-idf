079b0128deecde89e38b9989d90194308db94d8d
Anton Maklakov <anton@espressif.com>
2017-04-14 12:00:59 +0800

    build: Fix comments and avoid build warning

--- examples/bluetooth/gatt_client/main/component.mk
+++ examples/bluetooth/gatt_client/main/component.mk
@@ -1,10 +1,4 @@
 #
-# Main Makefile. This is basically the same as a component makefile.
+# "main" pseudo-component makefile.
 #
-# This Makefile should, at the very least, just include $(SDK_PATH)/make/component_common.mk. By default, 
-# this will take the sources in the src/ directory, compile them and link them into 
-# lib(subdirectory_name).a in the build directory. This behaviour is entirely configurable,
-# please read the ESP-IDF documents if you need to do this.
-#
-
-#include $(IDF_PATH)/make/component_common.mk
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
