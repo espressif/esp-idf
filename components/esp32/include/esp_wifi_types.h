100d2dd938d1188ea23baddf77737c66072da5c5
XiaXiaotian <xiaxiaotian@espressif.com>
2017-04-26 11:45:30 +0800

    optimize scan before station connecting to AP
        1. Store the information of AP(ssid, password, bssid, channel, etc)
        into nvs when station connects to AP successfully. If station
        connects to the same AP next time, it will scan the stored channel of the AP
        first.
    
        2. Add a parameter of channel for scanning before connecting to AP.
        If it is set to 1~13, station will scan starting from the channel.
        If the channel of AP is unknown, set it to 0.

--- components/esp32/include/esp_wifi_types.h
+++ components/esp32/include/esp_wifi_types.h
@@ -164,6 +164,7 @@ typedef struct {
     uint8_t password[64];  /**< password of target AP*/
     bool bssid_set;        /**< whether set MAC address of target AP or not. Generally, station_config.bssid_set needs to be 0; and it needs to be 1 only when users need to check the MAC address of the AP.*/
     uint8_t bssid[6];     /**< MAC address of target AP*/
+    uint8_t channel;       /**< channel of target AP. Set to 1~13 to scan starting from the specified channel before connecting to AP. If the channel of AP is unknown, set it to 0.*/
 } wifi_sta_config_t;
 
 typedef union {
