b6063d160cdcec8b47f64a649b43544b5f72da67
Angus Gratton <angus@espressif.com>
2017-05-17 12:06:31 +1000

    nghttp module: Update to v1.22, use submodule instead of in-tree source

--- components/nghttp/port/include/nghttp2/nghttp2ver.h
+++ components/nghttp/port/include/nghttp2/nghttp2ver.h
@@ -1,7 +1,7 @@
 /*
  * nghttp2 - HTTP/2 C Library
  *
- * Copyright (c) 2014 Tatsuhiro Tsujikawa
+ * Copyright (c) 2012, 2013 Tatsuhiro Tsujikawa
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -22,24 +22,21 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#ifndef NGHTTP2_MEM_H
-#define NGHTTP2_MEM_H
+#ifndef NGHTTP2VER_H
+#define NGHTTP2VER_H
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif /* HAVE_CONFIG_H */
+/**
+ * @macro
+ * Version number of the nghttp2 library release
+ */
+#define NGHTTP2_VERSION "v1.22.0"
 
-#include <nghttp2/nghttp2.h>
+/**
+ * @macro
+ * Numerical representation of the version number of the nghttp2 library
+ * release. This is a 24 bit number with 8 bits for major number, 8 bits
+ * for minor and 8 bits for patch. Version 1.2.3 becomes 0x010203.
+ */
+#define NGHTTP2_VERSION_NUM 0x012200
 
-/* The default, system standard memory allocator */
-nghttp2_mem *nghttp2_mem_default(void);
-
-/* Convenient wrapper functions to call allocator function in
-   |mem|. */
-void *nghttp2_mem_malloc(nghttp2_mem *mem, size_t size);
-void nghttp2_mem_free(nghttp2_mem *mem, void *ptr);
-void nghttp2_mem_free2(nghttp2_free free_func, void *ptr, void *mem_user_data);
-void *nghttp2_mem_calloc(nghttp2_mem *mem, size_t nmemb, size_t size);
-void *nghttp2_mem_realloc(nghttp2_mem *mem, void *ptr, size_t size);
-
-#endif /* NGHTTP2_MEM_H */
+#endif /* NGHTTP2VER_H */
