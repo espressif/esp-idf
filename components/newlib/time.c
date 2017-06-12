8ccb2a499064bcd1c651265c7d9d41a60db115c6
Ivan Grokhotkov <ivan@espressif.com>
2017-06-12 19:51:17 +0800

    esp32: make time monotonic across resets
    Small changes to clock calibration value will cause increasing errors
    the longer the device runs. Consider the case of deep sleep, assuming
    that RTC counter is used for timekeeping:
    - before sleep:
       time_before = rtc_counter * calibration_val
    - after sleep:
       time_after = (rtc_counter + sleep_count) * (calibration_val + epsilon)
    where 'epsilon' is a small estimation error of 'calibration_val'.
    The apparent sleep duration thus will be:
    time_after - time_before = sleep_count * (calibration_val + epsilon)
                               + rtc_counter * epsilon
    
    Second term on the right hand side is the error in time difference
    estimation, it is proportional to the total system runtime (rtc_counter).
    
    To avoid this issue, this change makes RTC_SLOW_CLK calibration value
    persistent across restarts. This allows the calibration value update to
    be preformed, while keeping time after update same as before the update.

--- components/newlib/time.c
+++ components/newlib/time.c
@@ -130,6 +130,32 @@ static uint64_t get_boot_time()
 }
 #endif //defined(WITH_RTC) || defined(WITH_FRC1)
 
+
+void esp_clk_slowclk_cal_set(uint32_t new_cal)
+{
+#if defined(WITH_RTC)
+    /* To force monotonic time values even when clock calibration value changes,
+     * we adjust boot time, given current time and the new calibration value:
+     *      T = boot_time_old + cur_cal * ticks / 2^19
+     *      T = boot_time_adj + new_cal * ticks / 2^19
+     * which results in:
+     *      boot_time_adj = boot_time_old + ticks * (cur_cal - new_cal) / 2^19
+     */
+    const int64_t ticks = (int64_t) rtc_time_get();
+    const uint32_t cur_cal = REG_READ(RTC_SLOW_CLK_CAL_REG);
+    int32_t cal_diff = (int32_t) (cur_cal - new_cal);
+    int64_t boot_time_diff = ticks * cal_diff / (1LL << RTC_CLK_CAL_FRACT);
+    uint64_t boot_time_adj = get_boot_time() + boot_time_diff;
+    set_boot_time(boot_time_adj);
+#endif // WITH_RTC
+    REG_WRITE(RTC_SLOW_CLK_CAL_REG, new_cal);
+}
+
+uint32_t esp_clk_slowclk_cal_get()
+{
+    return REG_READ(RTC_SLOW_CLK_CAL_REG);
+}
+
 void esp_setup_time_syscalls()
 {
 #if defined( WITH_FRC1 )
