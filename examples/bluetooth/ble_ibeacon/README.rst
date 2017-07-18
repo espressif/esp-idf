eff95f3799a992d9a91eef0b2fd4079bcdcc6a4f
island <island@espressif.com>
2017-07-18 20:42:22 +0800

    component/bt: add ibeacon demo

---
+++ examples/bluetooth/ble_ibeacon/README.rst
@@ -0,0 +1,5 @@
+ESP-IDF GATT CLIENT demo
+========================
+
+This is the demo for user to use ESP_APIs to create a GATT Client.
+
---
+++ examples/bluetooth/ble_ibeacon/main/ibeacon_demo.c
@@ -0,0 +1,254 @@
+// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+
+/****************************************************************************
+*
+* This file is for gatt client. It can scan ble device, connect one device,
+*
+****************************************************************************/
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "controller.h"
+
+#include "bt.h"
+#include "bt_trace.h"
+#include "bt_types.h"
+#include "btm_api.h"
+#include "bta_api.h"
+#include "bta_gatt_api.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gattc_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_bt_main.h"
+
+#define IBEACON_TAG "IBEACON_DEMO"
+
+/* Because current ESP IDF version doesn't support scan and adv simultaneously,
+ * so Ibeacon sender and receiver should not run simultaneously */
+#define IBEACON_SENDER      0
+#define IBEACON_RECEIVER    1
+//#define IBEACON_MODE IBEACON_SENDER //IBEACON_RECEIVER
+#define IBEACON_MODE IBEACON_RECEIVER
+
+///Declare static functions
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+
+#if (IBEACON_MODE == IBEACON_RECEIVER)
+static esp_ble_scan_params_t ble_scan_params = {
+    .scan_type              = BLE_SCAN_TYPE_ACTIVE,
+    .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
+    .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
+    .scan_interval          = 0x50,
+    .scan_window            = 0x30
+};
+
+#elif (IBEACON_MODE == IBEACON_SENDER)
+static esp_ble_adv_params_t ble_adv_params = {
+    .adv_int_min        = 0x20,
+    .adv_int_max        = 0x40,
+    .adv_type           = ADV_TYPE_NONCONN_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+#endif
+
+
+typedef struct {
+    uint8_t flags[3];
+    uint8_t length;
+    uint8_t type;
+    uint16_t company_id;
+    uint16_t beacon_type;
+}__attribute__((packed)) esp_ble_ibeacon_head_t;
+
+typedef struct {
+    uint8_t proximity_uuid[16];
+    uint16_t major;
+    uint16_t minor;
+    uint8_t tx_power;
+}__attribute__((packed)) esp_ble_ibeacon_vendor_t;
+
+
+typedef struct {
+    esp_ble_ibeacon_head_t ibeacon_head;
+    esp_ble_ibeacon_vendor_t ibeacon_vendor;
+}__attribute__((packed)) esp_ble_ibeacon_t;
+
+const uint8_t uuid_zeros[ESP_UUID_LEN_128] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+/* For Ibeacon packet format, please refer to Apple "Proximity Beacon Specification" doc */
+const esp_ble_ibeacon_head_t ibeacon_common_head = {
+    .flags = {0x02, 0x01, 0x06},
+    .length = 0x1A,
+    .type = 0xFF,
+    .company_id = 0x004C,
+    .beacon_type = 0x1502
+};
+
+bool esp_ble_is_ibeacon_packet (uint8_t *adv_data, uint8_t adv_data_len){
+    bool result = FALSE;
+    
+    if ((adv_data != NULL) && (adv_data_len == 0x1E)){
+        if (!memcmp(adv_data, (uint8_t*)&ibeacon_common_head, sizeof(ibeacon_common_head))){
+            result = TRUE;
+        }
+    }
+
+    return result;
+}
+
+esp_err_t esp_ble_config_ibeacon_data (esp_ble_ibeacon_vendor_t *vendor_config, esp_ble_ibeacon_t *ibeacon_adv_data){
+    if ((vendor_config == NULL) || (ibeacon_adv_data == NULL) || (!memcmp(vendor_config->proximity_uuid, uuid_zeros, sizeof(uuid_zeros)))){
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    memcpy(&(ibeacon_adv_data->ibeacon_head), (uint8_t*)&ibeacon_common_head, sizeof(ibeacon_common_head));
+    memcpy(&(ibeacon_adv_data->ibeacon_vendor), vendor_config, sizeof(esp_ble_ibeacon_vendor_t));
+
+    return ESP_OK;
+}
+
+
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
+{
+    switch (event) {
+    case ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT:{
+#if (IBEACON_MODE == IBEACON_SENDER)
+		esp_ble_gap_start_advertising(&ble_adv_params);
+#endif
+		break;
+	}
+    case ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT: {
+#if (IBEACON_MODE == IBEACON_RECEIVER)
+        //the unit of the duration is second, 0 means scan permanently
+        uint32_t duration = 0;
+        esp_ble_gap_start_scanning(duration);
+#endif
+        break;
+    }
+    case ESP_GAP_BLE_SCAN_START_COMPLETE_EVT:
+        //scan start complete event to indicate scan start successfully or failed
+        if (param->scan_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(IBEACON_TAG, "Scan start failed");
+        }
+        break;
+	case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+        //adv start complete event to indicate adv start successfully or failed
+		if (param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+			ESP_LOGE(IBEACON_TAG, "Adv start failed");
+	    }
+		break;
+    case ESP_GAP_BLE_SCAN_RESULT_EVT: {
+        esp_ble_gap_cb_param_t *scan_result = (esp_ble_gap_cb_param_t *)param;
+        switch (scan_result->scan_rst.search_evt) {
+        case ESP_GAP_SEARCH_INQ_RES_EVT:
+            /* Search for BLE Ibeacon Packet */
+            if (esp_ble_is_ibeacon_packet(scan_result->scan_rst.ble_adv, scan_result->scan_rst.adv_data_len)){
+                esp_ble_ibeacon_t *ibeacon_data = (esp_ble_ibeacon_t*)(scan_result->scan_rst.ble_adv);
+                ESP_LOGI(IBEACON_TAG, "----------Ibeacon Found----------");
+                esp_log_buffer_hex("IBEACON_DEMO: Device address:", scan_result->scan_rst.bda, BD_ADDR_LEN );
+                esp_log_buffer_hex("IBEACON_DEMO: Proximity UUID:", ibeacon_data->ibeacon_vendor.proximity_uuid, ESP_UUID_LEN_128);
+
+                ESP_LOGI(IBEACON_TAG, "Major: 0x%04x", ibeacon_data->ibeacon_vendor.major);
+                ESP_LOGI(IBEACON_TAG, "Minor: 0x%04x", ibeacon_data->ibeacon_vendor.minor);
+                ESP_LOGI(IBEACON_TAG, "Tx Power:0x%02x", ibeacon_data->ibeacon_vendor.tx_power);
+            }
+            break;
+        default:
+            break;
+        }
+        break;
+    }
+
+    case ESP_GAP_BLE_SCAN_STOP_COMPLETE_EVT:
+        if (param->scan_stop_cmpl.status != ESP_BT_STATUS_SUCCESS){
+            ESP_LOGE(IBEACON_TAG, "Scan stop failed");
+        }
+        else {
+            ESP_LOGI(IBEACON_TAG, "Stop scan successfully");
+        }
+        break;
+
+    case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
+        if (param->adv_stop_cmpl.status != ESP_BT_STATUS_SUCCESS){
+            ESP_LOGE(IBEACON_TAG, "Adv stop failed");
+        }
+        else {
+            ESP_LOGI(IBEACON_TAG, "Stop adv successfully");
+        }
+        break;
+
+    default:
+        break;
+    }
+}
+
+
+void ble_client_appRegister(void)
+{
+    esp_err_t status;
+
+    ESP_LOGI(IBEACON_TAG, "register callback");
+
+    //register the scan callback function to the gap module
+    if ((status = esp_ble_gap_register_callback(esp_gap_cb)) != ESP_OK) {
+        ESP_LOGE(IBEACON_TAG, "gap register error, error code = %x", status);
+        return;
+    }
+
+}
+
+void gattc_client_test(void)
+{
+    esp_bluedroid_init();
+    esp_bluedroid_enable();
+    ble_client_appRegister();
+}
+
+void app_main()
+{
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    esp_bt_controller_init(&bt_cfg);
+    esp_bt_controller_enable(ESP_BT_MODE_BTDM);
+
+    gattc_client_test();
+
+    /* set scan parameters */
+#if (IBEACON_MODE == IBEACON_RECEIVER)
+    esp_ble_gap_set_scan_params(&ble_scan_params);
+
+#elif (IBEACON_MODE == IBEACON_SENDER)
+	esp_ble_ibeacon_t ibeacon_adv_data;
+	esp_ble_ibeacon_vendor_t vendor_config = {
+		.proximity_uuid = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0XFF},
+		.major = 0x0001,
+		.minor = 0x0010,
+		.tx_power = 0xC5
+	};
+    esp_err_t status = esp_ble_config_ibeacon_data (&vendor_config, &ibeacon_adv_data);
+    if (status == ESP_OK){
+		esp_ble_gap_config_adv_data_raw((uint8_t*)&ibeacon_adv_data, sizeof(ibeacon_adv_data));
+	}
+	else {
+		ESP_LOGE(IBEACON_TAG, "Config Ibeacon data failed, status =0x%x\n", status);
+	}
+#endif
+}
+
