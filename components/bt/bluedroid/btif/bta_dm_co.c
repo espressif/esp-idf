c5ea00c727a9f53864ca3321cd643275b9ecd174
Yulong <huangyulong@espressif.com>
2017-08-18 03:48:57 -0400

    component/bt: Change the debug info.

--- components/bt/bluedroid/btif/bta_dm_co.c
+++ components/bt/bluedroid/btif/bta_dm_co.c
@@ -403,7 +403,6 @@ void bta_dm_co_ble_load_local_keys(tBTA_DM_BLE_LOCAL_KEY_MASK *p_key_mask, BT_OC
 #endif  ///defined(BTIF_INCLUDED) && BTIF_INCLUDED == TRUE
 #if (SMP_INCLUDED == TRUE)
     btc_dm_get_ble_local_keys( p_key_mask, er, p_id_keys);
-    BTIF_TRACE_DEBUG("bta_dm_co_ble_load_local_keys: func not ported\n");
 #endif  ///SMP_INCLUDED == TRUE
 }
 
