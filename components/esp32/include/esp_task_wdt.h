616baa239db0c1bf17b74a9ae53b40aedbead6da
Darian Leung <darian@espressif.com>
2017-08-30 21:11:10 +0800

    esp32: New Task Watchdog API
    Legacy API of task watchdog used the same function esp_task_wdt_feed() to add
    and feed a task. This caused issues of implicitly adding a task to the wdt list
    if the function was used in shared code.
    
    The new API introduces init, adding, feeding, deleting, deinit functions. Tasks
    must now be explicitly added to the task watchdog using their handles. Deletion
    must also be explicit using task handles. This resolves the issue of implicit
    task additions to the task watchdog due to shared code calling
    esp_task_wdt_feed().
    
    Task watchdog is now fully configurable at runtime by calling the init and
    deinit functions.
    
    Also added functions to get the handles of idle tasks of the other core. This
    helps when adding idle tasks to the watchdog at run time.
    
    Configuring the task watchdog using menu config is still available, however
    menu config will only result in calling the init and add functions for idle
    tasks shortly after the scheduler starts.
    
    Menu config also allows for using legacy behavior, however the legacy behavior
    willcall the new API functions but with slight variations to make them legacy
    compatible.
    
    Documentation and example have also been updated
    
    gcov_rtio.c headers updated to prevent error of freertos header files being
    included in the wrong order.
    
    Resolves issue TW#13265

--- components/esp32/include/esp_task_wdt.h
+++ components/esp32/include/esp_task_wdt.h
@@ -15,60 +15,136 @@
 #ifndef __ESP_TASK_WDT_H
 #define __ESP_TASK_WDT_H
 
+#include "freertos/task.h"
+#include "esp_err.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
-/** \defgroup Watchdog_APIs Watchdog APIs
-  * @brief Watchdog APIs
-  */
-
-/** @addtogroup Watchdog_APIs
-  * @{
-  */
-
 /*
-This routine enables a more general-purpose task watchdog: tasks can individually
-feed the watchdog and the watchdog will bark if one or more tasks haven't fed the
-watchdog within the specified time. Optionally, the idle tasks can also configured
-to feed the watchdog in a similar fashion, to detect CPU starvation.
+This routine enables a more general-purpose task watchdog which uses the TIMERG0
+WDT: All tasks subscribed to the task wdt must feed the wdt at least once
+before the wdt times out or else the wdt will bark. The Idle tasks can also be
+subscribed to the wdt. This allows for the detection of CPU starvation.
 
-This uses the TIMERG0 WDT.
+To enable/disable legacy behavior, toggle the CONFIG_TASK_WDT_LEGACY_BEHAVIOR
+option in menuconfig. Under legacy behavior, a task will subscribe to the
+wdt on its first call of esp_task_wdt_feed(). esp_task_wdt_add() SHOULD NOT BE
+CALLED in an application if legacy behavior is enabled. esp_task_wdt_delete()
+will only allow tasks to unsubscribe themselves from the task wdt under legacy
+behavior. Enabling legacy behavior will also force the task wdt to be enabled on
+startup.
 */
 
-
 /**
-  * @brief  Initialize the task watchdog. This is called in the init code, if the
-  *         task watchdog is enabled in menuconfig.
+  * @brief  Initialize the task watchdog timer
   *
-  */
-void esp_task_wdt_init();
-
-/**
-  * @brief  Feed the watchdog. After the first feeding session, the watchdog will expect the calling
-  *         task to keep feeding the watchdog until task_wdt_delete() is called.
+  * This function will initialize the task watchdog timer. If the wdt
+  * has already been initialized, calling this function again will change the
+  * timeout and panic configuration, then reset the timer count. Once the watchdog
+  * timer has been initialized, tasks can subscribe to the watchdog timer.
   *
-  */
-
-void esp_task_wdt_feed();
-
-
-/**
-  * @brief  Delete the watchdog for the current task.
+  * @param[in]  timeout     Timeout(ms) period of watchdog timer
+  * @param[in]  panic       Flag that if set, will cause the wdt to trigger the
+  *                         panic handler when it times out
+  * @return
+  *     - ESP_OK:           Initialization was successful
+  *     - ESP_ERR_NO_MEM:   Initialization was unsuccessful due to lack of
+  *                         memory
   *
+  * @note   esp_task_wdt_init() must only be called after the scheduler
+  *         started
   */
-void esp_task_wdt_delete();
+esp_err_t esp_task_wdt_init(uint32_t timeout, bool panic);
+
 
 /**
-  * @}
-  */
+ * @brief   Deinitialize the task watchdog timer
+ *
+ * This function will deinitialize the task watchdog timer. This function should
+ * be called when all tasks have unsubscribed form the watchdog timer.
+ *
+ * @return
+ *      - ESP_OK:                 Watchdog timer successfully deinitialized
+ *      - ESP_ERR_INVALID_STATE:  Error due to wdt not being initialized, or
+ *                                if tasks are still subscribed to the wdt
+ */
+esp_err_t esp_task_wdt_deinit();
 
+
+/**
+  * @brief  Subscribes a tasks to the task watchdog timer
+  *
+  * This function subscribes a task to the task watchdog timer. Note that once
+  * subscribed, a task must periodically feed the watchdog timer. If the task
+  * is an IDLE task, this function will enable feeding from the idle hook
+  * automatically.
+  *
+  * @param[in]  handle            Handle of the task to be added. Input NULL
+  *                               to add the current running task
+  *
+  * @return
+  *     - ESP_OK:                 Successfully subscribed the task to the wdt
+  *     - ESP_ERR_INVALID_ARG:    Failed as task is already subscribed
+  *     - ESP_ERR_INVALID_STATE:  Failed as wdt has not been initialized
+  *     - ESP_ERR_NO_MEM:         Failed due to lack of memory
+  *
+  * @note   This function should not be called from application code if
+  *         CONFIG_TASK_WDT_LEGACY_BEHAVIOR has been enabled. Legacy behavior
+  *         uses esp_task_wdt_feed() to subscribe tasks to the wdt.
+  */
+esp_err_t esp_task_wdt_add(TaskHandle_t handle);
+
+/**
+  * @brief  Feed the current running task
+  *
+  * This function will feed the current running task if it has subscribed to the
+  * task watchdog timer. All non-idle tasks subscribed to the wdt must call this
+  * function at least once per watchdog timeout period. Idle tasks will feed
+  * the task watchdog automatically from their idle hooks.
+  *
+  * @return
+  *     - ESP_OK:                 Successfully fed the current running task
+  *     - ESP_ERR_INVALID_STATE:  Failed to feed current running task as the
+  *                               wdt has not been initialized, or the
+  *                               current running task has not subscribed
+  *
+  * @note   If CONFIG_TASK_WDT_LEGACY_BEHAVIOR is enabled, tasks will subscribe
+  *         to the watchdog on their first call of esp_task_wdt_feed().
+  */
+esp_err_t esp_task_wdt_feed();
+
+/**
+  * @brief  Unsubscribes a task from the task watchdog timer
+  *
+  * This function unsubscribes a task from the task watchdog. After unsubscribing,
+  * the task should no longer feed the wdt. If the unsubscribing task is an idle
+  * task, this function will disable feeding from the idle hook automatically.
+  *
+  * @param[in]  handle              Handle of the task to be deleted. Input NULL
+  *                                 if deleting the current running task.
+  *
+  * @return
+  *     - ESP_OK:                 Successfully unsubscribed task form the wdt
+  *     - ESP_ERR_INVALID_ARG:    Failed to unsubscribe task as it was not
+  *                               subscribed to begin with
+  *     - ESP_ERR_INVALID_STATE:  Failed to unsubscribe task as wdt has not
+  *                               been initialized yet
+  *
+  * @note   Legacy behavior only allows tasks to unsubscribe from the wdt on their
+  *         own behalf. Therefore, if CONFIG_TASK_WDT_LEGACY_BEHAVIOR is
+  *         enabled, this esp_task_wdt_delete() will accept no parameters and
+  *         unsubscribe the calling task from the wdt.
+  */
+#ifdef CONFIG_TASK_WDT_LEGACY_BEHAVIOR
+esp_err_t esp_task_wdt_delete();
+#else
+esp_err_t esp_task_wdt_delete(TaskHandle_t handle);
+#endif      //CONFIG_TASK_WDT_LEGACY_BEHAVIOR
 
 #ifdef __cplusplus
 }
 #endif
 
-
-
-#endif
+#endif      //__ESP_TASK_WDT_H
--- docs/api-reference/system/wdts.rst
+++ docs/api-reference/system/wdts.rst
@@ -28,22 +28,39 @@ Task watchdog
 ^^^^^^^^^^^^^
 
 Any tasks can elect to be watched by the task watchdog. If such a task does not feed the watchdog within the time
-specified by the task watchdog timeout (which is configurable using ``make menuconfig``), the watchdog will
-print out a warning with information about which processes are running on the ESP32 CPUs and which processes
-failed to feed the watchdog.
+specified by the task watchdog timeout, the watchdog will print out a warning with information about which processes 
+are running on the ESP32 CPUs and which processes failed to feed the watchdog.
 
-By default, the task watchdog watches the idle tasks. The usual cause of idle tasks not feeding the watchdog 
+Ideally, the task watchdog should watch the idle tasks. The usual cause of the idle tasks not feeding the watchdog 
 is a higher-priority process looping without yielding to the lower-priority processes, and can be an indicator
 of badly-written code that spinloops on a peripheral or a task that is stuck in an infinite loop.
 
-Other task can elect to be watched by the task watchdog by calling ``esp_task_wdt_feed()``. Calling this routine
-for the first time will register the task to the task watchdog; calling it subsequent times will feed
-the watchdog. If a task does not want to be watched anymore (e.g. because it is finished and will call 
-``vTaskDelete()`` on itself), it needs to call ``esp_task_wdt_delete()``.
+Other task can elect to be watched by the task watchdog. If the watchdog watches multiple tasks, all the tasks
+must feed before the watchdog times out. If one or more tasks fails to feed, the watchdog timeout will trigger.
+The watchdog timer can be initialized using :cpp:func:`esp_task_wdt_init` which will configure the hardware timer and
+establish the hardware timer interrupts. In order for a task to be watched by the task watchdog, a task must subscribe to
+the task watchdog using :cpp:func:`esp_task_wdt_add`. Once added, each subscribed task must call :cpp:func:`esp_task_wdt_feed` 
+periodically to prevent the watchdog from timing out. If all tasks being watched have fed, the watchdog timer counter immediately resets
+and starts recounting to the timeout value from zero. To unsubscribe a task from the task watchdog, call :cpp:func:`esp_task_wdt_delete()`.
+Once all tasks have been unsubscribed form the task watchdog, :cpp:func:`esp_task_wdt_deinit()` can be called to deinitialize
+the task watchdog. Deinitialization will stop the hardware timer, deregister the hardware timer interrupts, and remove the idle hooks
+if idle tasks were being watched.
 
-The task watchdog is built around the hardware watchdog in timer group 0. If this watchdog for some reason
+By setting the task watchdog options in ``make menuconfig``, the task watchdog can be automatically initialized
+during startup by enabling the :ref:`CONFIG_TASK_WDT`. Moreover the two Idle tasks can also be subscibed to the 
+task watchdog on startup as well by enabling :ref:`CONFIG_TASK_WDT_CHECK_IDLE_TASK_CPU0` and 
+:ref:`CONFIG_TASK_WDT_CHECK_IDLE_TASK_CPU1`.
+
+The task watchdog is built around the hardware watchdog in timer group 0. If the watchdog for some reason
 cannot execute the interrupt handler that prints the task data (e.g. because IRAM is overwritten by garbage
 or interrupts are disabled entirely) it will hard-reset the SOC.
+
+Note: ``make menuconfig`` provides a :ref:`CONFIG_TASK_WDT_LEGACY_BEHAVIOR` option which will change the behavior of the 
+task watchdog to make it compatible with the legacy code. Note that the legacy behavior causes tasks to subscribe to the
+task watchdog on their first call to :cpp:func:`esp_task_wdt_feed`. Moreover, legacy behavior only allows
+tasks to unsubscribe on their own behalf when calling :cpp:func:`esp_task_wdt_delete()`. It is strongly recommended that 
+non-legacy behavior is used as additions/deletions/feeds are explicit.
+
 
 JTAG and watchdogs
 ^^^^^^^^^^^^^^^^^^
@@ -53,20 +70,27 @@ CPU. This makes it very hard to debug code; that is why the OpenOCD config will 
 This does mean that you will not get any warnings or panics from either the task or interrupt watchdog when the ESP32
 is connected to OpenOCD via JTAG.
 
-API Reference
--------------
+Interrupt Watchdog API Reference
+--------------------------------
 
-Header Files
-^^^^^^^^^^^^
+Header File
+^^^^^^^^^^^
 
   * :component_file:`esp32/include/esp_int_wdt.h`
-  * :component_file:`esp32/include/esp_task_wdt.h`
 
 
 Functions
 ---------
-
+ 
 .. doxygenfunction:: esp_int_wdt_init
-.. doxygenfunction:: esp_task_wdt_init
-.. doxygenfunction:: esp_task_wdt_feed
-.. doxygenfunction:: esp_task_wdt_delete
+
+Task Watchdog API Reference
+----------------------------
+
+NOTE: Ensure that the :ref:`CONFIG_TASK_WDT_LEGACY_BEHAVIOR` configuraiton is disabled using ``make menuconfig``
+to prevent legacy behavior of the task watchdog.
+
+A full example using the Task Watchdog is available in esp-idf: :example:`system/task_watchdog`
+
+
+.. include:: /_build/inc/esp_task_wdt.inc
