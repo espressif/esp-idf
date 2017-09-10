4e0c3a0415aeb32c90d948126216255cca674899
Alexey Gerenkov <alexey@espressif.com>
2017-09-10 19:17:46 +0300

    esp32: Fixes crash during core dump.
    Removes piece of debugging code introduced by 8d43859b.

--- components/freertos/include/freertos/FreeRTOSConfig.h
+++ components/freertos/include/freertos/FreeRTOSConfig.h
@@ -269,9 +269,7 @@ extern void vPortCleanUpTCB ( void *pxTCB );
 #define configXT_BOARD                      1   /* Board mode */
 #define configXT_SIMULATOR					0
 
-#if CONFIG_ESP32_ENABLE_COREDUMP
 #define configENABLE_TASK_SNAPSHOT			1
-#endif
 
 #if CONFIG_SYSVIEW_ENABLE
 #ifndef __ASSEMBLER__
