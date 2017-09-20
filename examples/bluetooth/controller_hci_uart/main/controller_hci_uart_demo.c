0d0c9e7172c792de1881f6c6dcd09592453d3216
Tian Hao <tianhao@espressif.com>
2017-09-20 18:13:51 +0800

    fix example of controller_hci_uart peripheral clk init

--- examples/bluetooth/controller_hci_uart/main/controller_hci_uart_demo.c
+++ examples/bluetooth/controller_hci_uart/main/controller_hci_uart_demo.c
@@ -23,7 +23,14 @@ static const char *tag = "CONTROLLER_UART_HCI";
 
 static void uart_gpio_reset(void)
 {
-#if CONFIG_BT_HCI_UART_NO
+#if CONFIG_BT_HCI_UART_NO == 1
+    periph_module_enable(PERIPH_UART1_MODULE);
+#elif CONFIG_BT_HCI_UART_NO == 2
+    periph_module_enable(PERIPH_UART2_MODULE);
+#endif
+    periph_module_enable(PERIPH_UHCI0_MODULE);
+
+#ifdef CONFIG_BT_HCI_UART_NO
     ESP_LOGI(tag, "HCI UART%d Pin select: TX 5, RX, 18, CTS 23, RTS 19", CONFIG_BT_HCI_UART_NO);
 
     uart_set_pin(CONFIG_BT_HCI_UART_NO, 5, 18, 19, 23);
