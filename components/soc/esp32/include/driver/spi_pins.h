5cf7d3768d7f8756d231add0b0560aaaf9aa7127
michael <xiaoxufeng@espressif.com>
2017-09-28 20:19:18 +0800

    feat(spi_master): fine tune the timing of SPI

---
+++ components/soc/esp32/include/driver/spi_pins.h
@@ -0,0 +1,28 @@
+// Copyright 2015-2018 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef _DRIVER_SPI_PINS_H_
+#define _DRIVER_SPI_PINS_H_
+
+#define HSPI_NATIVE_PIN_NUM_MISO 12
+#define HSPI_NATIVE_PIN_NUM_MOSI 13
+#define HSPI_NATIVE_PIN_NUM_CLK  14
+#define HSPI_NATIVE_PIN_NUM_CS   15
+
+#define VSPI_NATIVE_PIN_NUM_MISO 19
+#define VSPI_NATIVE_PIN_NUM_MOSI 23
+#define VSPI_NATIVE_PIN_NUM_CLK  18
+#define VSPI_NATIVE_PIN_NUM_CS   5
+
+#endif
