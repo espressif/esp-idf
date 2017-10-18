d55ddfe56d9622b855a5d374193acd1e60a3b3f0
Ivan Grokhotkov <ivan@espressif.com>
2017-10-18 20:01:29 +0800

    update libstdc++.a built with pSRAM workaround

--- components/esp32/component.mk
+++ components/esp32/component.mk
@@ -40,7 +40,7 @@ COMPONENT_ADD_LDFLAGS += $(COMPONENT_PATH)/libhal.a \
 
 #The cache workaround also needs a c++ standard library recompiled with the workaround.
 ifdef CONFIG_SPIRAM_CACHE_WORKAROUND
-COMPONENT_ADD_LDFLAGS += $(COMPONENT_PATH)/libstdcc++-cache-workaround.a
+COMPONENT_ADD_LDFLAGS += $(COMPONENT_PATH)/libstdc++-psram-workaround.a
 endif
 
 ALL_LIB_FILES := $(patsubst %,$(COMPONENT_PATH)/lib/lib%.a,$(LIBS))
