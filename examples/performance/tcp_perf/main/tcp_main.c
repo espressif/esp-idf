0dea8bca46a72b77a57c42089026df05cd4ff3e9
petermccloud <peter@mccloudaero.com>
2017-11-15 19:44:25 -0800

    fixed broken tcp_perf example by adding nvs_flash

--- examples/performance/tcp_perf/main/tcp_main.c
+++ examples/performance/tcp_perf/main/tcp_main.c
@@ -33,6 +33,7 @@ step3:
 #include "freertos/event_groups.h"
 #include "esp_log.h"
 #include "esp_err.h"
+#include "nvs_flash.h"
 
 #include "tcp_perf.h"
 
@@ -123,6 +124,7 @@ static void tcp_conn(void *pvParameters)
 
 void app_main(void)
 {
+    nvs_flash_init();
 #if EXAMPLE_ESP_WIFI_MODE_AP
     ESP_LOGI(TAG, "EXAMPLE_ESP_WIFI_MODE_AP");
     wifi_init_softap();
