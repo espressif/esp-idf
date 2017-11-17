306d59d32c0a768b7d8089068ab5972324ddebb6
Kedar Sovani <kedars@gmail.com>
2017-11-17 15:49:12 +0530

    Make read/write function pointers
    as per Ivan's feedback

--- components/esp-tls/esp-tls.h
+++ components/esp-tls/esp-tls.h
@@ -6,29 +6,23 @@
 #include <openssl/ssl.h>
 
 struct esp_tls {
-    SSL_CTX *ctx;
-    SSL     *ssl;
-    int      sockfd;
+    SSL_CTX  *ctx;
+    SSL      *ssl;
+    int       sockfd;
+    ssize_t (*read)(struct esp_tls *tls, char *data, size_t datalen);
+    ssize_t (*write)(struct esp_tls *tls, const char *data, size_t datalen);
 };
 
 struct esp_tls *esp_tls_conn_new(const char *hostname, int hostlen, int port, bool is_tls);
 
 static inline ssize_t esp_tls_conn_write(struct esp_tls *tls, const char *data, size_t datalen)
 {
-    if (tls->ssl) {
-        return SSL_write(tls->ssl, data, datalen);
-    } else {
-        return send(tls->sockfd, data, datalen, 0);
-    }
+    return tls->write(tls, data, datalen);
 }
 
 static inline ssize_t esp_tls_conn_read(struct esp_tls *tls, char *data, size_t datalen)
 {
-    if (tls->ssl) {
-        return SSL_read(tls->ssl, data, datalen);
-    } else {
-        return recv(tls->sockfd, data, datalen, 0);
-    }
+    return tls->read(tls, data, datalen);
 }
 
 void esp_tls_conn_delete(struct esp_tls *tls);
