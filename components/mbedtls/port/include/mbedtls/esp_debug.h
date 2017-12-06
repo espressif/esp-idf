8a47679d046aa1f256d87060653568e9e4d595a7
Paul Reimer <paul@p-rimes.net>
2017-12-06 09:09:40 -0800

    Add #include guards and __cplusplus guards to esp_debug.h
    Merges https://github.com/espressif/esp-idf/pull/1358

--- components/mbedtls/port/include/mbedtls/esp_debug.h
+++ components/mbedtls/port/include/mbedtls/esp_debug.h
@@ -11,6 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#ifndef _ESP_DEBUG_H_
+#define _ESP_DEBUG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include "sdkconfig.h"
 #ifdef CONFIG_MBEDTLS_DEBUG
@@ -43,3 +49,9 @@ void mbedtls_esp_disable_debug_log(mbedtls_ssl_config *conf);
 
 
 #endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ESP_DEBUG_H__ */
