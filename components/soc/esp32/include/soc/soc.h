fdf50b7ff8c3791ae5a473ac983f0e359e261ce8
Darian Leung <darian@espressif.com>
2017-12-18 20:32:29 +0800

    CAN Driver
    The following commit contains the first version of the ESP32 CAN Driver.
    
    closes #544

--- components/soc/esp32/include/soc/soc.h
+++ components/soc/esp32/include/soc/soc.h
@@ -120,6 +120,7 @@
 #define DR_REG_I2C1_EXT_BASE                    0x3ff67000
 #define DR_REG_SDMMC_BASE                       0x3ff68000
 #define DR_REG_EMAC_BASE                        0x3ff69000
+#define DR_REG_CAN_BASE                         0x3ff6B000
 #define DR_REG_PWM1_BASE                        0x3ff6C000
 #define DR_REG_I2S1_BASE                        0x3ff6D000
 #define DR_REG_UART2_BASE                       0x3ff6E000
