6220723e7b039affc3f261396ea2f922d3501a6b
Tian Hao <tianhao@espressif.com>
2017-12-22 15:13:45 +0800

    component/bt : change header file name bt.h to esp_bt.h in new bluetooth example

--- examples/bluetooth/bt_discovery/main/bt_discovery.c
+++ examples/bluetooth/bt_discovery/main/bt_discovery.c
@@ -28,7 +28,7 @@
 #include "nvs_flash.h"
 #include "esp_system.h"
 #include "esp_log.h"
-#include "bt.h"
+#include "esp_bt.h"
 #include "esp_bt_main.h"
 #include "esp_bt_device.h"
 #include "esp_gap_bt_api.h"
