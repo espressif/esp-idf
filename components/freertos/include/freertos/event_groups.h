29cf610197e0bdb8225602f01f9f338c88672823
JoveToo <johan@verrept.eu>
2017-12-24 17:44:57 +0100

    fix typo

--- components/freertos/include/freertos/event_groups.h
+++ components/freertos/include/freertos/event_groups.h
@@ -133,7 +133,7 @@ typedef TickType_t EventBits_t;
  *
  * Internally, within the FreeRTOS implementation, event groups use a [small]
  * block of memory, in which the event group's structure is stored.  If an event
- * groups is created using xEventGropuCreate() then the required memory is
+ * groups is created using xEventGroupCreate() then the required memory is
  * automatically dynamically allocated inside the xEventGroupCreate() function.
  * (see http://www.freertos.org/a00111.html).  If an event group is created
  * using xEventGropuCreateStatic() then the application writer must instead
