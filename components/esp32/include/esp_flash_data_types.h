cf7a4cc650e20cf5e725d970b5a9fc8c59722ce3
Roland Dobai <dobai.roland@gmail.com>
2018-01-31 14:45:12 +0100

    Protect partition table by MD5 checksum

--- components/esp32/include/esp_flash_data_types.h
+++ components/esp32/include/esp_flash_data_types.h
@@ -23,6 +23,7 @@ extern "C"
 
 #define ESP_PARTITION_TABLE_ADDR 0x8000
 #define ESP_PARTITION_MAGIC 0x50AA
+#define ESP_PARTITION_MAGIC_MD5 0xEBEB
 
 /* OTA selection structure (two copies in the OTA data partition.)
    Size of 32 bytes is friendly to flash encryption */
