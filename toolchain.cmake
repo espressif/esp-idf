e6e1b94b136380fe9ad1301b740c8c048fe1de62
Angus Gratton <angus@espressif.com>
2018-02-01 17:45:41 +0800

    cmake: Generate PHY init data partition as part of the build

--- toolchain.cmake
+++ toolchain.cmake
@@ -3,5 +3,6 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_C_COMPILER xtensa-esp32-elf-gcc)
 set(CMAKE_CXX_COMPILER xtensa-esp32-elf-g++)
 set(CMAKE_ASM_COMPILER xtensa-esp32-elf-gcc)
+set(CMAKE_OBJCOPY xtensa-esp32-elf-objcopy)
 
 set(CMAKE_EXE_LINKER_FLAGS "-nostdlib" CACHE STRING "Linker Base Flags")
