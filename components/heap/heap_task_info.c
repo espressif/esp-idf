dea771e43f5528284ec2ef21d25583f5ae3bffbb
Stephen Casner <casner@acm.org>
2018-02-09 15:08:00 -0800

    Change heap_caps_get_per_task_info() to take struct of params
    In addition to moving the large number of function parameters into a
    struct as the single parameter, the following changes were made:
    
    - Clear out the totals for any prepopulated tasks so the app code
      doesn't have to do it.
    
    - Rather than partitioning the per-task totals into a predetermined
      set of heap capabilities, take a list of <caps,mask> pairs to
      compare the caps to the heap capabilities as masked.  This lets the
      caller configure the desired partitioning, or none.
    
    - Allow the totals array pointer or the blocks array pointer to be
      NULL to indicate not to collect that part of the information.
    
    - In addition to returning the total space allocated by each task,
      return the number of blocks allocated by each task.
    
    - Don't need to return the heap capabilities as part of the details
      for each block since the heap region (and therefore its
      capabilities) can be determined from the block address.
    
    - Renamed heap_task_info.h to esp_heap_task_info.h to fit the naming
      convention, and renamed the structs for totals and block details to
      better fit the revised function name.
    
    - Provide full Doxygen commenting for the function and parameter
      structs.

--- components/heap/heap_task_info.c
+++ components/heap/heap_task_info.c
@@ -3,95 +3,113 @@
 #include <multi_heap.h>
 #include "multi_heap_internal.h"
 #include "heap_private.h"
-#include "heap_task_info.h"
+#include "esp_heap_task_info.h"
 
 #ifdef CONFIG_HEAP_TASK_TRACKING
 
 /*
- * For each task that has allocated memory from the heap, return totals for
- * allocations of each type of heap region (8-bit DRAM, 8-bit D/IRAM, 32-bit
- * IRAM, SPIRAM) into provided array of heap_dump_totals_t structs 'totals'.
+ * Return per-task heap allocation totals and lists of blocks.
  *
- * If one or more task handles is provided in array 'tasks', for each block
- * allocated by one of the tasks a heap_dump_block_t struct is written into
- * provided array 'buffer' to give the task, address, size and region type for
- * the block.
+ * For each task that has allocated memory from the heap, return totals for
+ * allocations within regions matching one or more sets of capabilities.
+ *
+ * Optionally also return an array of structs providing details about each
+ * block allocated by one or more requested tasks, or by all tasks.
+ *
+ * Returns the number of block detail structs returned.
  */
-size_t heap_caps_get_per_task_info(heap_dump_totals_t* totals, size_t* ntotal, size_t max,
-                                   TaskHandle_t* tasks, size_t ntasks,
-                                   heap_dump_block_t* buffer, size_t size)
+size_t heap_caps_get_per_task_info(heap_task_info_params_t *params)
 {
     heap_t *reg;
-    size_t count = *ntotal;
-    size_t remaining = size;
-    size_t i;
+    heap_task_block_t *blocks = params->blocks;
+    size_t count = *params->num_totals;
+    size_t remaining = params->max_blocks;
+
+    // Clear out totals for any prepopulated tasks.
+    if (params->totals) {
+        for (size_t i = 0; i < count; ++i) {
+            for (size_t type = 0; type < NUM_HEAP_TASK_CAPS; ++type) {
+                params->totals[i].size[type] = 0;
+                params->totals[i].count[type] = 0;
+            }
+        }
+    }
 
     SLIST_FOREACH(reg, &registered_heaps, next) {
         multi_heap_handle_t heap = reg->heap;
         if (heap == NULL) {
             continue;
         }
+
+        // Find if the capabilities of this heap region match on of the desired
+        // sets of capabilities.
         uint32_t caps = get_all_caps(reg);
-        enum region_types type = DRAM;
-        if (caps & MALLOC_CAP_EXEC) {
-            type = D_IRAM;
+        uint32_t type;
+        for (type = 0; type < NUM_HEAP_TASK_CAPS; ++type) {
+            if ((caps & params->mask[type]) == params->caps[type]) {
+                break;
+            }
         }
-        if (!(caps & MALLOC_CAP_8BIT)) {
-            type = IRAM;
+        if (type == NUM_HEAP_TASK_CAPS) {
+            continue;
         }
-        if (caps & MALLOC_CAP_SPIRAM) {
-            type = SPIRAM;
-        }
+
         multi_heap_block_handle_t b = multi_heap_get_first_block(heap);
         multi_heap_internal_lock(heap);
-        while (b) {
+        for ( ; b ; b = multi_heap_get_next_block(heap, b)) {
             if (multi_heap_is_free(b)) {
-                b = multi_heap_get_next_block(heap, b);
                 continue;
             }
-            void* p = multi_heap_get_block_address(b);  // Safe, only arithmetic
+            void *p = multi_heap_get_block_address(b);  // Safe, only arithmetic
             size_t bsize = multi_heap_get_allocated_size(heap, p); // Validates
             TaskHandle_t btask = (TaskHandle_t)multi_heap_get_block_owner(b);
-            size_t index;
-            for (index = 0; index < count; ++index) {
-                if (totals[index].task == btask) {
-                    break;
-                }
-            }
-            if (index < count) {
-                totals[index].size[type] += bsize;
-            }
-            else {
-                if (count < max) {
-                    totals[count].task = btask;
-                    totals[count].size[type] = bsize;
-                    ++count;
-                }
-            }
-            if (tasks) {
-                for (i = 0; i < ntasks; ++i) {
-                    if (btask == tasks[i]) {
+
+            // Accumulate per-task allocation totals.
+            if (params->totals) {
+                size_t i;
+                for (i = 0; i < count; ++i) {
+                    if (params->totals[i].task == btask) {
                         break;
                     }
                 }
-                if (i == ntasks) {
-                    b = multi_heap_get_next_block(heap, b);
-                    continue;
+                if (i < count) {
+                    params->totals[i].size[type] += bsize;
+                    params->totals[i].count[type] += 1;
+                }
+                else {
+                    if (count < params->max_totals) {
+                        params->totals[count].task = btask;
+                        params->totals[count].size[type] = bsize;
+                        params->totals[i].count[type] = 1;
+                        ++count;
+                    }
                 }
             }
-            if (remaining > 0) {
-                buffer->task = btask;
-                buffer->address = p;
-                buffer->size = bsize;
-                buffer->type = type;
-                ++buffer;
+
+            // Return details about allocated blocks for selected tasks.
+            if (blocks && remaining > 0) {
+                if (params->tasks) {
+                    size_t i;
+                    for (i = 0; i < params->num_tasks; ++i) {
+                        if (btask == params->tasks[i]) {
+                            break;
+                        }
+                    }
+                    if (i == params->num_tasks) {
+                        continue;
+                    }
+                }
+                blocks->task = btask;
+                blocks->address = p;
+                blocks->size = bsize;
+                ++blocks;
                 --remaining;
             }
-            b = multi_heap_get_next_block(heap, b);
         }
         multi_heap_internal_unlock(heap);
     }
-    *ntotal = count;
-    return size - remaining;
+    *params->num_totals = count;
+    return params->max_blocks - remaining;
 }
-#endif
+
+#endif // CONFIG_HEAP_TASK_TRACKING
