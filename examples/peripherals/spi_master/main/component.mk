50f5ae36f1b4e180e05bf2b33d3b1407c7f013e0
Jeroen Domburg <jeroen@espressif.com>
2018-03-09 12:40:12 +0800

    Examples/spi_master: spiffier graphics

--- examples/peripherals/spi_master/main/component.mk
+++ examples/peripherals/spi_master/main/component.mk
@@ -3,3 +3,6 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
+
+#Compile image file into the resulting firmware binary
+COMPONENT_EMBED_FILES := image.jpg
