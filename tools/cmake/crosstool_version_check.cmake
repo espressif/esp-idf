c4a244c57dbad43085afb3cb15f1d295080abdf3
Angus Gratton <angus@espressif.com>
2018-03-20 12:27:00 +1100

    cmake: Style cleanup, add cmake linter to gitlab CI

--- tools/cmake/crosstool_version_check.cmake
+++ tools/cmake/crosstool_version_check.cmake
@@ -18,10 +18,10 @@ function(crosstool_version_check expected_ctng_version)
 
     string(REGEX MATCH "crosstool-ng-[0-9a-g\\.-]+" ctng_version "${toolchain_stderr}")
     string(REPLACE "crosstool-ng-" "" ctng_version "${ctng_version}")
-	# We use FIND to match version instead of STREQUAL because some toolchains are built
-	# with longer git hash strings than others. This will match any version which starts with
-	# the expected version string.
-	string(FIND "${ctng_version}" "${expected_ctng_version}" found_expected_version)
+    # We use FIND to match version instead of STREQUAL because some toolchains are built
+    # with longer git hash strings than others. This will match any version which starts with
+    # the expected version string.
+    string(FIND "${ctng_version}" "${expected_ctng_version}" found_expected_version)
     if(NOT ctng_version)
         message(WARNING "Xtensa toolchain ${CMAKE_C_COMPILER} does not appear to be built with crosstool-ng. "
             "${ctng_version_warning}")
