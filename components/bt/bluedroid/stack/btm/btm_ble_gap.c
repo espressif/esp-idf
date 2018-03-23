88e5397c44099fde6eea512c165ec03072fdc72b
Hopkins Kong <hopkinskong@gmail.com>
2018-03-23 23:27:09 +0800

    Fix compilation errors and warnings

--- components/bt/bluedroid/stack/btm/btm_ble_gap.c
+++ components/bt/bluedroid/stack/btm/btm_ble_gap.c
@@ -2985,12 +2985,12 @@ void btm_ble_process_adv_pkt (UINT8 *p_data)
 {
     BD_ADDR             bda;
     BD_ADDR             temp_bda;
-    UINT8               temp_addr_type = 0;
     UINT8               evt_type = 0, *p = p_data;
     UINT8               addr_type = 0;
     UINT8               num_reports;
     UINT8               data_len;
 #if (defined BLE_PRIVACY_SPT && BLE_PRIVACY_SPT == TRUE)
+    UINT8               temp_addr_type = 0;
     BOOLEAN             match = FALSE;
 #endif
 
@@ -3007,11 +3007,11 @@ void btm_ble_process_adv_pkt (UINT8 *p_data)
         STREAM_TO_UINT8    (evt_type, p);
         STREAM_TO_UINT8    (addr_type, p);
         STREAM_TO_BDADDR   (bda, p);
-        temp_addr_type = addr_type;
         memcpy(temp_bda, bda, BD_ADDR_LEN);
         //BTM_TRACE_ERROR("btm_ble_process_adv_pkt:bda= %0x:%0x:%0x:%0x:%0x:%0x\n",
         //                              bda[0],bda[1],bda[2],bda[3],bda[4],bda[5]);
 #if (defined BLE_PRIVACY_SPT && BLE_PRIVACY_SPT == TRUE)
+        temp_addr_type = addr_type;
         /* map address to security record */
         match = btm_identity_addr_to_random_pseudo(bda, &addr_type, FALSE);
 
@@ -3022,7 +3022,8 @@ void btm_ble_process_adv_pkt (UINT8 *p_data)
             btm_ble_resolve_random_addr(bda, btm_ble_resolve_random_addr_on_adv, p_data);
         } else
 #endif
-        btm_ble_process_adv_pkt_cont(bda, addr_type, evt_type, p);
+			btm_ble_process_adv_pkt_cont(bda, addr_type, evt_type, p);
+		#if (defined BLE_PRIVACY_SPT && BLE_PRIVACY_SPT == TRUE)
         //save current adv addr information if p_dev_rec!= NULL
         tBTM_SEC_DEV_REC *p_dev_rec = btm_find_dev (bda);
         if(p_dev_rec) {
@@ -3030,6 +3031,7 @@ void btm_ble_process_adv_pkt (UINT8 *p_data)
             memcpy(p_dev_rec->ble.current_addr, temp_bda, BD_ADDR_LEN);
             p_dev_rec->ble.current_addr_valid = true;
         }
+		#endif
         STREAM_TO_UINT8(data_len, p);
 
         /* Advance to the next event data_len + rssi byte */
