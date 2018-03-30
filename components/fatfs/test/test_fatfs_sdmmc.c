e20e64ace9948189103cd36565bfe79901444c21
Ivan Grokhotkov <ivan@espressif.com>
2018-03-30 18:40:06 +0800

    fatfs/test: use 16k cluster size to speed up formatting

--- components/fatfs/test/test_fatfs_sdmmc.c
+++ components/fatfs/test/test_fatfs_sdmmc.c
@@ -39,7 +39,8 @@ static void test_setup(void)
     sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
     esp_vfs_fat_sdmmc_mount_config_t mount_config = {
         .format_if_mount_failed = true,
-        .max_files = 5
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
     };
     TEST_ESP_OK(esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, NULL));
 }
