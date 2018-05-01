e9f6949d7e6bc84e492df921c7dca5f01aee6b00
Jouni Malinen <j@w1.fi>
2018-05-01 17:53:07 +0300

    EAP-TLS peer: MSK/EMSK derivation with TLS v1.3
    Use new MSK/EMSK derivation mechanism if TLS v1.3 or newer is used per
    draft-mattsson-eap-tls13-02.txt.
    
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- components/wpa_supplicant/src/eap_peer/eap_tls.c
+++ components/wpa_supplicant/src/eap_peer/eap_tls.c
@@ -112,6 +112,8 @@ static struct wpabuf * eap_tls_failure(struct eap_sm *sm,
 static void eap_tls_success(struct eap_sm *sm, struct eap_tls_data *data,
 			    struct eap_method_ret *ret)
 {
+	const char *label;
+
 	wpa_printf(MSG_DEBUG, "EAP-TLS: Done");
 
 	if (data->ssl.tls_out) {
@@ -120,18 +122,21 @@ static void eap_tls_success(struct eap_sm *sm, struct eap_tls_data *data,
 	}
 
 	if (data->ssl.tls_v13) {
+		label = "client EAP encryption KM";
+
 		/* A possible NewSessionTicket may be received before
 		 * EAP-Success, so need to allow it to be received. */
 		ret->methodState = METHOD_MAY_CONT;
 		ret->decision = DECISION_COND_SUCC;
 	} else {
+		label = "client EAP encryption";
+
 		ret->methodState = METHOD_DONE;
 		ret->decision = DECISION_UNCOND_SUCC;
 	}
 
 	os_free(data->key_data);
-	data->key_data = eap_peer_tls_derive_key(sm, &data->ssl,
-						 "client EAP encryption",
+	data->key_data = eap_peer_tls_derive_key(sm, &data->ssl, label,
 						 EAP_TLS_KEY_LEN +
 						 EAP_EMSK_LEN);
 	if (data->key_data) {
