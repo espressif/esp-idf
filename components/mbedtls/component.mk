c94a5ecfdd4f90a9f2be760545d671a80ad85c6d
Angus Gratton <angus@espressif.com>
2018-05-14 16:07:18 +0800

    mbedtls: Add mbedtls submodule check to build system

--- components/mbedtls/component.mk
+++ components/mbedtls/component.mk
@@ -7,3 +7,6 @@ COMPONENT_ADD_INCLUDEDIRS := port/include mbedtls/include
 COMPONENT_SRCDIRS := mbedtls/library port
 
 COMPONENT_OBJEXCLUDE := mbedtls/library/net_sockets.o
+
+COMPONENT_SUBMODULES += mbedtls
+
