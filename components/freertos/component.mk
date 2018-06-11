4e43c920d882eae61879417efaa1e22acba20260
Mahavir Jain <mahavir@espressif.com>
2018-06-11 14:06:32 +0530

    freertos: make task{ENTER/EXIT} macros deprecated for external use
    Signed-off-by: Mahavir Jain <mahavir@espressif.com>

--- components/freertos/component.mk
+++ components/freertos/component.mk
@@ -5,3 +5,5 @@
 COMPONENT_ADD_LDFLAGS += -Wl,--undefined=uxTopUsedPriority
 COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENT_PRIV_INCLUDEDIRS := include/freertos
+
+tasks.o event_groups.o timers.o queue.o ringbuf.o: CFLAGS += -D_ESP_FREERTOS_INTERNAL
