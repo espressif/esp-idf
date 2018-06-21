70e68c99d3b031dcd81c1f968387735332c9a453
Renz Bagaporo <renz@espressif.com>
2018-06-21 16:51:44 +0800

    Modifications for fs profiling tool

--- components/spi_flash/sim/SpiFlash.h
+++ components/spi_flash/sim/SpiFlash.h
@@ -15,6 +15,8 @@
 #ifndef _SpiFlash_H_
 #define _SpiFlash_H_
 
+#include <stdbool.h>
+
 #include "esp_err.h"
 #include "rom/spi_flash.h"
 
@@ -29,32 +31,49 @@ public:
     SpiFlash();
     ~SpiFlash();
 
-    void init(size_t chip_size, size_t block_size, size_t sector_size, size_t page_size, const char* partitions_bin);
+    void init(uint32_t chip_size, uint32_t block_size, uint32_t sector_size, uint32_t page_size, const char* partitions_bin);
+
+    uint32_t get_chip_size();
+    uint32_t get_block_size();
+    uint32_t get_sector_size();
+    uint32_t get_page_size();
 
     esp_rom_spiflash_result_t erase_block(uint32_t block);
     esp_rom_spiflash_result_t erase_sector(uint32_t sector);
     esp_rom_spiflash_result_t erase_page(uint32_t page);
 
-    esp_rom_spiflash_result_t write(size_t dest_addr, const void *src, size_t size);
-    esp_rom_spiflash_result_t read(size_t src_addr, void *dest, size_t size);
+    esp_rom_spiflash_result_t write(uint32_t dest_addr, const void *src, uint32_t size);
+    esp_rom_spiflash_result_t read(uint32_t src_addr, void *dest, uint32_t size);
 
-    size_t get_chip_size();
-    size_t get_sector_size();
+    uint32_t get_erase_cycles(uint32_t sector);
+    uint32_t get_total_erase_cycles();
+    
+    void set_erase_cycles_limit(uint32_t limit);
+    void set_total_erase_cycles_limit(uint32_t limit);
+
+    void reset_erase_cycles();
+    void reset_total_erase_cycles();
 
     uint8_t* get_memory_ptr(uint32_t src_address);
 
 private:
-    bool inited = false;
+    uint32_t chip_size;
+    uint32_t block_size;
+    uint32_t sector_size;
+    uint32_t page_size;
 
-    size_t chip_size;
-    size_t block_size;
-    size_t sector_size;
-    size_t page_size;
+    uint32_t blocks;
+    uint32_t sectors;
+    uint32_t pages;
 
     uint8_t* memory;
 
-    void* partitions;
-    uint8_t partitions_num;
+    bool* erase_states;
+
+    uint32_t* erase_cycles;
+    uint32_t erase_cycles_limit;
+    uint32_t total_erase_cycles;
+    uint32_t total_erase_cycles_limit;
 
     void deinit();
 };
