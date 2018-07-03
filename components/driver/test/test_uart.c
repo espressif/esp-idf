61bc7337910617e5e9a357054c7b51e5df213b33
Alex Lisitsyn <aleks@espressif.com>
2018-07-03 16:17:03 +0200

    driver: rs485 test support
    Adds test functionality with TEST_CASE_MULTIPLE_DEVICES() macro for RS485 test support.

--- components/driver/test/test_uart.c
+++ components/driver/test/test_uart.c
@@ -324,5 +324,5 @@ static void rs485_master()
  * correctness of RS485 interface channel communication. It requires
  * RS485 bus driver hardware to be connected to boards.
 */
-TEST_CASE_MULTIPLE_DEVICES("RS485 half duplex uart multiple devices test.", "[driver][ignore]", rs485_master, rs485_slave);
+TEST_CASE_MULTIPLE_DEVICES("RS485 half duplex uart multiple devices test.", "[driver_RS485][test_env=UT_T2_RS485][timeout=200]", rs485_master, rs485_slave);
 
