b379530a961a3d91cf2834e4ff0e0f49e05e1aab
Ivan Grokhotkov <ivan@espressif.com>
2018-07-09 15:25:59 +0800

    freertos: add configTASKLIST_INCLUDE_COREID

--- components/freertos/tasks.c
+++ components/freertos/tasks.c
@@ -3784,7 +3784,10 @@ BaseType_t xTaskGetAffinity( TaskHandle_t xTask )
 				pxTaskStatusArray[ uxTask ].xTaskNumber = pxNextTCB->uxTCBNumber;
 				pxTaskStatusArray[ uxTask ].eCurrentState = eState;
 				pxTaskStatusArray[ uxTask ].uxCurrentPriority = pxNextTCB->uxPriority;
+
+				#if ( configTASKLIST_INCLUDE_COREID == 1 )
 				pxTaskStatusArray[ uxTask ].xCoreID = pxNextTCB->xCoreID;
+				#endif /* configTASKLIST_INCLUDE_COREID */
 
 				#if ( INCLUDE_vTaskSuspend == 1 )
 				{
@@ -4450,7 +4453,7 @@ For ESP32 FreeRTOS, vTaskExitCritical implements both portEXIT_CRITICAL and port
 				pcWriteBuffer = prvWriteNameToBuffer( pcWriteBuffer, pxTaskStatusArray[ x ].pcTaskName );
 
 				/* Write the rest of the string. */
-#ifdef CONFIG_FREERTOS_VTASKLIST_INCLUDE_COREID
+#if configTASKLIST_INCLUDE_COREID
 				sprintf( pcWriteBuffer, "\t%c\t%u\t%u\t%u\t%hd\r\n", cStatus, ( unsigned int ) pxTaskStatusArray[ x ].uxCurrentPriority, ( unsigned int ) pxTaskStatusArray[ x ].usStackHighWaterMark, ( unsigned int ) pxTaskStatusArray[ x ].xTaskNumber, ( int ) pxTaskStatusArray[ x ].xCoreID );
 #else
 				sprintf( pcWriteBuffer, "\t%c\t%u\t%u\t%u\r\n", cStatus, ( unsigned int ) pxTaskStatusArray[ x ].uxCurrentPriority, ( unsigned int ) pxTaskStatusArray[ x ].usStackHighWaterMark, ( unsigned int ) pxTaskStatusArray[ x ].xTaskNumber );
