275edd8684fc49dd3e473d127d2b679b2c461acc
Amey Inamdar <amey@espressif.com>
2018-07-30 21:33:10 +0530

    Protocomm : Added component core for protocol communication
    * This manages secure sessions and provides framework for multiple transports.
    * The application can use protocomm layer directly to have application specific extensions for provisioning (or non-provisioning) use cases.
    * Following features are available for provisioning :
    
      * Security - Security0 (no security), Security1 (curve25519 key exchange + AES-CTR encryption)
      * Proof-of-possession support for Security1
    
    * Protocomm requires specific protocol buffer modules for compilation which can be generated from the `.proto` files in the `proto` directory using make.
    
    Co-Authored-By: Amey Inamdar <amey@espressif.com>
    Co-Authored-By: Anurag Kar <anurag.kar@espressif.com>

---
+++ components/protocomm/include/security/protocomm_security.h
@@ -0,0 +1,93 @@
+// Copyright 2018 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <esp_err.h>
+
+/**
+ * @brief   Proof Of Possession for authenticating a secure session
+ */
+typedef struct protocomm_security_pop {
+    /**
+     * Pointer to buffer containing the proof of possession data
+     */
+    const uint8_t *data;
+
+    /**
+     * Length (in bytes) of the proof of possession data
+     */
+    uint16_t len;
+} protocomm_security_pop_t;
+
+/**
+ * @brief   Protocomm security object structure.
+ *
+ * The member functions are used for implementing secure
+ * protocomm sessions.
+ *
+ * @note    This structure should not have any dynamic
+ *          members to allow re-entrancy
+ */
+typedef struct protocomm_security {
+    /**
+     * Unique version number of security implmentation
+     */
+    int ver;
+
+    /**
+     * Function for initialising/allocating security
+     * infrastructure
+     */
+    esp_err_t (*init)();
+
+    /**
+     * Function for deallocating security infrastructure
+     */
+    esp_err_t (*cleanup)();
+
+    /**
+     * Starts new secure transport session with specified ID
+     */
+    esp_err_t (*new_transport_session)(uint32_t session_id);
+
+    /**
+     * Closes a secure transport session with specified ID
+     */
+    esp_err_t (*close_transport_session)(uint32_t session_id);
+
+    /**
+     * Handler function for authenticating connection
+     * request and establishing secure session
+     */
+    esp_err_t (*security_req_handler)(const protocomm_security_pop_t *pop,
+                                      uint32_t session_id,
+                                      const uint8_t *inbuf, ssize_t inlen,
+                                      uint8_t **outbuf, ssize_t *outlen,
+                                      void *priv_data);
+
+    /**
+     * Function which implements the encryption algorithm
+     */
+    esp_err_t (*encrypt)(uint32_t session_id,
+                         const uint8_t *inbuf, ssize_t inlen,
+                         uint8_t *outbuf, ssize_t *outlen);
+
+    /**
+     * Function which implements the decryption algorithm
+     */
+    esp_err_t (*decrypt)(uint32_t session_id,
+                         const uint8_t *inbuf, ssize_t inlen,
+                         uint8_t *outbuf, ssize_t *outlen);
+} protocomm_security_t;
---
+++ components/protocomm/src/security/security0.c
@@ -0,0 +1,111 @@
+// Copyright 2018 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <esp_err.h>
+#include <esp_log.h>
+
+#include <protocomm_security.h>
+#include <protocomm_security0.h>
+
+#include "session.pb-c.h"
+#include "sec0.pb-c.h"
+#include "constants.pb-c.h"
+
+static const char* TAG = "security0";
+
+static esp_err_t sec0_session_setup(uint32_t session_id,
+                                    SessionData *req, SessionData *resp,
+                                    const protocomm_security_pop_t *pop)
+{
+    Sec0Payload *out = (Sec0Payload *) malloc(sizeof(Sec0Payload));
+    S0SessionResp *s0resp = (S0SessionResp *) malloc(sizeof(S0SessionResp));
+    if (!out || !s0resp) {
+        ESP_LOGE(TAG, "Error allocating response");
+        return ESP_ERR_NO_MEM;
+    }
+    sec0_payload__init(out);
+    s0_session_resp__init(s0resp);
+    s0resp->status = STATUS__Success;
+
+    out->msg = SEC0_MSG_TYPE__S0_Session_Response;
+    out->payload_case = SEC0_PAYLOAD__PAYLOAD_SR;
+    out->sr = s0resp;
+
+    resp->proto_case = SESSION_DATA__PROTO_SEC0;
+    resp->sec0 = out;
+
+    return ESP_OK;
+}
+
+static void sec0_session_setup_cleanup(uint32_t session_id, SessionData *resp)
+{
+    if (!resp) {
+        return;
+    }
+
+    free(resp->sec0->sr);
+    free(resp->sec0);
+    return;
+}
+
+static esp_err_t sec0_req_handler(const protocomm_security_pop_t *pop, uint32_t session_id,
+                                  const uint8_t *inbuf, ssize_t inlen,
+                                  uint8_t **outbuf, ssize_t *outlen,
+                                  void *priv_data)
+{
+    SessionData *req;
+    SessionData resp;
+    esp_err_t ret;
+
+    req = session_data__unpack(NULL, inlen, inbuf);
+    if (!req) {
+        ESP_LOGE(TAG, "Unable to unpack setup_req");
+        return ESP_ERR_INVALID_ARG;
+    }
+    if (req->sec_ver != protocomm_security0.ver) {
+        ESP_LOGE(TAG, "Security version mismatch. Closing connection");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    session_data__init(&resp);
+    ret = sec0_session_setup(session_id, req, &resp, pop);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Session setup error %d", ret);
+        return ESP_FAIL;
+    }
+
+    session_data__free_unpacked(req, NULL);
+
+    resp.sec_ver = req->sec_ver;
+
+    *outlen = session_data__get_packed_size(&resp);
+    *outbuf = (uint8_t *) malloc(*outlen);
+    if (!*outbuf) {
+        ESP_LOGE(TAG, "System out of memory");
+        return ESP_ERR_NO_MEM;
+    }
+    session_data__pack(&resp, *outbuf);
+
+    sec0_session_setup_cleanup(session_id, &resp);
+    return ESP_OK;
+}
+
+const protocomm_security_t protocomm_security0 = {
+    .ver = 0,
+    .security_req_handler = sec0_req_handler,
+};
