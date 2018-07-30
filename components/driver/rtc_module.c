d78452ed29ccfa8a6ae141f882349882ef1903bc
Mahavir Jain <mahavir@espressif.com>
2018-07-30 11:17:11 +0530

    wpa_supplicant: removed unrequired header dir include from component.mk
    - Also fixed license header in rtc driver
    
    Signed-off-by: Mahavir Jain <mahavir@espressif.com>

--- components/driver/rtc_module.c
+++ components/driver/rtc_module.c
@@ -1,6 +1,9 @@
+// Copyright 2016-2018 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
