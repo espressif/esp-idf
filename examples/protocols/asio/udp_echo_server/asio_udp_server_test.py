6abcfe7dccd26264dfd5316ed333509824be01ca
Angus Gratton <angus@espressif.com>
2018-08-03 16:59:23 +1000

    asio example tests: Increase timeout for DHCP lease to 30s
    Covers time to connect to WiFi and negotiate lease,
    may be more than 5-10s on some busy APs

--- examples/protocols/asio/udp_echo_server/asio_udp_server_test.py
+++ examples/protocols/asio/udp_echo_server/asio_udp_server_test.py
@@ -38,7 +38,7 @@ def test_examples_protocol_asio_udp_server(env, extra_data):
     # 1. start test
     dut1.start_app()
     # 2. get the server IP address
-    data = dut1.expect(re.compile(r" sta ip: ([^,]+),"))
+    data = dut1.expect(re.compile(r" sta ip: ([^,]+),"), timeout=30)
     # 3. create tcp client and connect to server
     cli = socket(AF_INET, SOCK_DGRAM)
     cli.connect((data[0], 80))
