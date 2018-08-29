93f92ba546e394174133f548f4c0060638a75c0b
MartinValik <martin.valik@mail.com>
2018-08-29 10:22:54 +0200

    lwip: DHCP restore last IP
    Closes https://github.com/espressif/esp-idf/issues/799

---
+++ components/lwip/port/esp32/include/netif/dhcp_state.h
@@ -0,0 +1,33 @@
+// Copyright 2018 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef _DHCP_STATE_H_
+#define _DHCP_STATE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool dhcp_ip_addr_restore(void *netif);
+
+void dhcp_ip_addr_store(void *netif);
+
+void dhcp_ip_addr_erase(void *netif);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*  _DHCP_STATE_H_ */
