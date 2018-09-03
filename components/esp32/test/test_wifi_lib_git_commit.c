e0700ffc3fa93682619cec2213e30f0c23e59cd2
Nicola Lunghi <25422924+nicola-lunghi@users.noreply.github.com>
2018-09-03 15:30:07 +0100

    esp32: test_wifi_lib_git_commit_id: remove unused esp_log

--- components/esp32/test/test_wifi_lib_git_commit.c
+++ components/esp32/test/test_wifi_lib_git_commit.c
@@ -2,10 +2,7 @@
  Tests for the Wi-Fi
 */
 #include "unity.h"
-#include "esp_log.h"
 #include "esp_wifi_internal.h"
-
-static const char* TAG = "test_wifi_lib_git_commit_id";
 
 TEST_CASE("wifi lib git commit id","[wifi]")
 {    
