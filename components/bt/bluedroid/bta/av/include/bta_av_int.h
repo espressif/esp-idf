54cc99ca05c3e4546e648892c2b5fdadca756baf
wangmengyang <wangmengyang@espressif.com>
2018-09-07 12:03:33 +0800

    component/bt: stop to initiate AVDTP connection on failure of A2DP service discovery
    In opening state of AVDTP connection, if SDP client fails to get valid A2DP service record\
    from peer SDP server, then do not initiate AVDTP connection request

--- components/bt/bluedroid/bta/av/include/bta_av_int.h
+++ components/bt/bluedroid/bta/av/include/bta_av_int.h
@@ -661,6 +661,7 @@ extern void bta_av_switch_role (tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data);
 extern void bta_av_role_res (tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data);
 extern void bta_av_delay_co (tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data);
 extern void bta_av_open_at_inc (tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data);
+extern void bta_av_open_fail_sdp (tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data);
 
 /* ssm action functions - vdp specific */
 extern void bta_av_do_disc_vdp (tBTA_AV_SCB *p_scb, tBTA_AV_DATA *p_data);
