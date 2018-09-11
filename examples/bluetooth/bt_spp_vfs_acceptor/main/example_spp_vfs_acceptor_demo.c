ce126a7262ba9fddd15d04d68fc85ea4ad9123c8
wangmengyang <wangmengyang@espressif.com>
2018-09-11 11:20:30 +0800

    component/bt: release some controller memory according to the configuration of bluetooth examples

--- examples/bluetooth/bt_spp_vfs_acceptor/main/example_spp_vfs_acceptor_demo.c
+++ examples/bluetooth/bt_spp_vfs_acceptor/main/example_spp_vfs_acceptor_demo.c
@@ -164,6 +164,8 @@ void app_main()
     }
     ESP_ERROR_CHECK( ret );
 
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
+
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     if (esp_bt_controller_init(&bt_cfg) != ESP_OK) {
         ESP_LOGE(SPP_TAG, "%s initialize controller failed", __func__);
