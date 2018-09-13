946f55db859f80ad71adf403bebc7f990209b685
Renz Christian Bagaporo <renz@espressif.com>
2018-09-13 14:54:59 +0800

    cmake: add ulp components build support

---
+++ components/ulp/cmake/toolchain-ulp.cmake
@@ -0,0 +1,15 @@
+# CMake toolchain file for ULP
+
+set(CMAKE_SYSTEM_NAME Generic)
+
+# Compiler is only used for preprocessing
+set(CMAKE_C_COMPILER "xtensa-esp32-elf-gcc")
+
+set(CMAKE_ASM_COMPILER "esp32ulp-elf-as")
+set(CMAKE_LINKER "esp32ulp-elf-ld")
+
+set(CMAKE_ASM${ASM_DIALECT}_COMPILE_OBJECT "${CMAKE_ASM${ASM_DIALECT}_COMPILER} \
+                                    <DEFINES> <INCLUDES> -o <OBJECT> -c <SOURCE>")
+set(CMAKE_EXE_LINKER_FLAGS "-A elf32-esp32ulp -nostdlib" CACHE STRING "ULP Linker Base Flags")
+set(CMAKE_ASM_LINK_EXECUTABLE "${CMAKE_LINKER} <FLAGS> <CMAKE_ASM_LINK_FLAGS> \
+                                <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
