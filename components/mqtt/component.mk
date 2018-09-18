d6a5cd6e82d590daaeac5e83fc27c6a778e508e1
Anton Maklakov <anton@espressif.com>
2018-09-18 14:16:19 +0800

    mqtt: silence a format warning

--- components/mqtt/component.mk
+++ components/mqtt/component.mk
@@ -2,3 +2,7 @@ COMPONENT_SUBMODULES += esp-mqtt
 COMPONENT_ADD_INCLUDEDIRS := esp-mqtt/include
 COMPONENT_SRCDIRS :=  esp-mqtt esp-mqtt/lib
 COMPONENT_PRIV_INCLUDEDIRS := esp-mqtt/lib/include
+
+ifeq ($(GCC_NOT_5_2_0), 1)
+esp-mqtt/lib/transport_ws.o: CFLAGS += -Wno-format-overflow
+endif
