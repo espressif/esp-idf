d50d1ca9255a96383136dd9d71259d295b27c2ae
Nikita Pushchin <vbnekit@p5y.su>
2018-09-20 16:54:49 +0300

    cmake -DCMAKE_${X}_COMPILER problem fixed
    - bootloader external project: CMAKE_[C+CXX+ASM]_COMPILER parameters forwarding enabled;
    - esp32 toolchain: "isset" check before "set" enabled.
    
    If we run cmake with the "-DCMAKE_ASM_COMPILER:STRING=/path/to/xtensa-esp32-elf-gcc -DCMAKE_CXX_COMPILER:STRING=/path/to/xtensa-esp32-elf-g++ -DCMAKE_C_COMPILER:STRING=/path/to/xtensa-esp32-elf-gcc" instead of changing PATH, then we'll get the following errors:
    
    > The ASM compiler identification is unknown
    > Found assembler: xtensa-esp32-elf-gcc
    > The C compiler identification is unknown
    > The CXX compiler identification is unknown
    > CMake Error at /home/nikita/work/esp/esp-idf/tools/cmake/project.cmake:99 (_project):
    >   The CMAKE_ASM_COMPILER:
    >     xtensa-esp32-elf-gcc
    >   is not a full path and was not found in the PATH.
    >   Tell CMake where to find the compiler by setting either the environment
    >   variable "ASM" or the CMake cache entry CMAKE_ASM_COMPILER to the full path
    >   to the compiler, or to the compiler name if it is in the PATH.
    > Call Stack (most recent call first):
    >   CMakeLists.txt:6 (project)
    > Warning: Did not find file Compiler/-ASM
    >
    > CMake Error at /home/nikita/work/esp/esp-idf/tools/cmake/project.cmake:99 (_project):
    >   The CMAKE_C_COMPILER:
    > ... blah blah blah ...
    
    It happens because:
     - tools/cmake/toolchain-esp32.cmake override user definitions;
     - bootloader external project executor doesn't forward theese options.
    
    This commit fixes it.

--- components/bootloader/project_include.cmake
+++ components/bootloader/project_include.cmake
@@ -17,7 +17,7 @@ externalproject_add(bootloader
     # TODO: support overriding the bootloader in COMPONENT_PATHS
     SOURCE_DIR "${IDF_PATH}/components/bootloader/subproject"
     BINARY_DIR "${bootloader_build_dir}"
-    CMAKE_ARGS -DSDKCONFIG=${SDKCONFIG} -DIDF_PATH=${IDF_PATH}
+    CMAKE_ARGS -DSDKCONFIG=${SDKCONFIG} -DIDF_PATH=${IDF_PATH} -DCMAKE_ASM_COMPILER=${CMAKE_ASM_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
     INSTALL_COMMAND ""
     BUILD_ALWAYS 1  # no easy way around this...
     BUILD_BYPRODUCTS ${bootloader_binary_files}
