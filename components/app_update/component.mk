d1be73159a8acb3fb3d7e014532e072f2a8be76e
Konstantin Kondrashov <konstantin@espressif.com>
2018-10-05 20:29:07 +0800

    esp32: Add firmware version to app
    Added a new structure esp_app_desc_t. It has info about firmware:
    version, secure_version, project_name, time/date build and IDF version.
    Added the ability to add a custom structure with a description of the firmware.
    
    The esp_app_desc_t is located in fixed place in start of ROM secotor. It is located after structures esp_image_header_t and esp_image_segment_header_t.
    
    app_version is filed from PROJECT_VER variable (if set in custom make file) or PROJECT_PATH/version.txt or git repo (git describe).
    
    Add API to get app_desc from partition.

--- components/app_update/component.mk
+++ components/app_update/component.mk
@@ -3,3 +3,47 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
+# esp_app_desc structure is added as an undefined symbol because otherwise the 
+# linker will ignore this structure as it has no other files depending on it.
+COMPONENT_ADD_LDFLAGS += -u esp_app_desc
+
+ifndef IS_BOOTLOADER_BUILD
+GET_PROJECT_VER ?=
+ifeq ("${PROJECT_VER}", "")
+ifeq ("$(wildcard ${PROJECT_PATH}/version.txt)","")
+GET_PROJECT_VER := $(shell cd ${PROJECT_PATH} && git describe --always --tags --dirty || echo "Not found git repo")
+ifeq ("${GET_PROJECT_VER}", "Not found git repo")
+$(info Project do not have git repo, it needs to get PROJECT_VER from `git describe` command.)
+GET_PROJECT_VER := ""
+endif
+else
+# read from version.txt
+GET_PROJECT_VER := $(shell cat ${PROJECT_PATH}/version.txt)
+endif
+endif
+# If ``PROJECT_VER`` variable set in project Makefile file, its value will be used.
+# Else, if the ``$PROJECT_PATH/version.txt`` exists, its contents will be used as ``PROJECT_VER``.
+# Else, if the project is located inside a Git repository, the output of git describe will be used.
+# Otherwise, ``PROJECT_VER`` will be empty.
+
+ifeq ("${PROJECT_VER}", "")
+PROJECT_VER:= $(GET_PROJECT_VER)
+else
+PROJECT_VER:= $(PROJECT_VER)
+endif
+$(info App "$(PROJECT_NAME)" version: $(PROJECT_VER))
+
+NEW_DEFINES:= $(PROJECT_VER) $(PROJECT_NAME) $(IDF_VER)
+ifeq ("$(wildcard ${TMP_DEFINES})","")
+OLD_DEFINES:=
+else
+OLD_DEFINES:= $(shell cat $(TMP_DEFINES))
+endif
+
+# If NEW_DEFINES (PROJECT_VER, PROJECT_NAME) were changed then rebuild only esp_app_desc.
+ifneq ("${NEW_DEFINES}", "${OLD_DEFINES}")
+$(shell echo $(NEW_DEFINES) > $(TMP_DEFINES); rm -f esp_app_desc.o;)
+endif
+
+esp_app_desc.o: CPPFLAGS += -D PROJECT_VER=\"$(PROJECT_VER)\" -D PROJECT_NAME=\"$(PROJECT_NAME)\"
+endif
