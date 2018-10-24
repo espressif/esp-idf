e998e3eaf7c6d3cd963dcd44b7462bacf7a2508a
zhangyanjiao <zhangyanjiao@espressif.com>
2018-10-24 10:31:58 +0800

    wifi: add long range code

--- examples/wifi/espnow/main/espnow_example_main.c
+++ examples/wifi/espnow/main/espnow_example_main.c
@@ -67,6 +67,10 @@ static void example_wifi_init(void)
      * been already on the same channel.
      */
     ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, 0) );
+
+#if CONFIG_ENABLE_LONG_RANGE
+    ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
+#endif
 }
 
 /* ESPNOW sending or receiving callback function is called in WiFi task.
