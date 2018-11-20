9436764a8a952df4db853149dda9501d773fa277
Ivan Grokhotkov <ivan@espressif.com>
2018-11-21 00:43:52 +0800

    examples/protocols/coap: use common network component

--- examples/protocols/coap_server/main/coap_server_example_main.c
+++ examples/protocols/coap_server/main/coap_server_example_main.c
@@ -16,32 +16,17 @@
 
 #include "esp_log.h"
 #include "esp_wifi.h"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 
 #include "nvs_flash.h"
 
+#include "protocol_examples_common.h"
+
 #include "coap.h"
-
-/* The examples use simple WiFi configuration that you can set via
-   'make menuconfig'.
-
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-#define EXAMPLE_WIFI_SSID            CONFIG_WIFI_SSID
-#define EXAMPLE_WIFI_PASS            CONFIG_WIFI_PASSWORD
 
 /* Set this to 9 to get verbose logging from within libcoap */
 #define COAP_LOGGING_LEVEL 0
 
-static EventGroupHandle_t wifi_event_group;
-
-/* The event group allows multiple bits for each event,
-   but we only care about one event - are we connected
-   to the AP with an IP? */
-const static int CONNECTED_BIT = BIT0;
-
-const static char *TAG = "CoAP_server";
 static char espressif_data[100];
 static int espressif_data_len = 0;
 
@@ -122,13 +107,6 @@ static void coap_example_thread(void *p)
         coap_endpoint_t *ep_tcp = NULL;
         unsigned wait_ms;
 
-        /* Wait for the callback to set the CONNECTED_BIT in the
-           event group.
-        */
-        xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
-                            false, true, portMAX_DELAY);
-        ESP_LOGI(TAG, "Connected to AP");
-
         /* Prepare the CoAP server socket */
         coap_address_init(&serv_addr);
         serv_addr.addr.sin.sin_family      = AF_INET;
@@ -181,50 +159,17 @@ clean_up:
     vTaskDelete(NULL);
 }
 
-static esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
-{
-    switch(event->event_id) {
-    case SYSTEM_EVENT_STA_START:
-        esp_wifi_connect();
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-        /* This is a workaround as ESP32 WiFi libs don't currently
-           auto-reassociate. */
-        esp_wifi_connect();
-        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
-        break;
-    default:
-        break;
-    }
-    return ESP_OK;
-}
-
-static void wifi_conn_init(void)
-{
-    tcpip_adapter_init();
-    wifi_event_group = xEventGroupCreate();
-    ESP_ERROR_CHECK( esp_event_loop_init(wifi_event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_WIFI_SSID,
-            .password = EXAMPLE_WIFI_PASS,
-        },
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-}
-
 void app_main(void)
 {
     ESP_ERROR_CHECK( nvs_flash_init() );
-    wifi_conn_init();
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
 
     xTaskCreate(coap_example_thread, "coap", 1024 * 5, NULL, 5, NULL);
 }
