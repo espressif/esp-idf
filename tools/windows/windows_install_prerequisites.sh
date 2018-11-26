espressif/esp-idf

commit 7c8d5d2782b2966ef93b5d0078a5006c4a46cecd

docs, scripts: Correct the priority of the PATH setting

tools/windows/windows_install_prerequisites.sh
@@ -54,7 +54,7 @@ rm ~/${TOOLCHAIN_ZIP}
 cat > /etc/profile.d/esp32_toolchain.sh << EOF
 # This file was created by ESP-IDF windows_install_prerequisites.sh
 # and will be overwritten if that script is run again.
-export PATH="\$PATH:/opt/xtensa-esp32-elf/bin"
+export PATH="\/opt/xtensa-esp32-elf/bin:$PATH"
 EOF
 
 # clean up pacman package cache to save some disk space

add_path.sh
@@ -13,7 +13,7 @@ else
 	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/espcoredump"
 	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/components/partition_table/"
 	IDF_ADD_PATHS_EXTRAS="${IDF_ADD_PATHS_EXTRAS}:${IDF_PATH}/tools/"
-	export PATH="${PATH}:${IDF_ADD_PATHS_EXTRAS}"
+	export PATH="${IDF_ADD_PATHS_EXTRAS}:${PATH}"
 	echo "Added to PATH: ${IDF_ADD_PATHS_EXTRAS}"
 fi
 

commit b7f56e86ba706ad1ec437b169c0dca8f538a555e

windows_install_prerequisites: Fix generation of PATH command
Accidental regression in 7c8d5d2782b2966ef93b5d0078a5006c4a46cecd - escape
character didn't move with PATH.

Reported on forum https://esp32.com/viewtopic.php?f=13&t=8201

tools/windows/windows_install_prerequisites.sh
@@ -54,7 +54,7 @@ rm ~/${TOOLCHAIN_ZIP}
 cat > /etc/profile.d/esp32_toolchain.sh << EOF
 # This file was created by ESP-IDF windows_install_prerequisites.sh
 # and will be overwritten if that script is run again.
-export PATH="\/opt/xtensa-esp32-elf/bin:$PATH"
+export PATH="/opt/xtensa-esp32-elf/bin:\$PATH"
 EOF
 
 # clean up pacman package cache to save some disk space