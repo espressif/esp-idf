7570a25f7606af79c80c715d41571994747cf16b
Angus Gratton <angus@espressif.com>
2018-12-06 15:07:24 +1100

    ldgen make: Use "sed -E" for extended POSIX regular expression syntax w/ BSD sed

--- make/ldgen.mk
+++ make/ldgen.mk
@@ -8,7 +8,8 @@ LDGEN_FRAGMENT_FILES = $(COMPONENT_LDFRAGMENTS)
 define ldgen_process_template
 $(BUILD_DIR_BASE)/ldgen.section_infos: $(LDGEN_SECTIONS_INFO_FILES) $(IDF_PATH)/make/ldgen.mk
 	printf "$(foreach section_info,$(LDGEN_SECTIONS_INFO_FILES),$(section_info)\n)" > $(BUILD_DIR_BASE)/ldgen.section_infos
-	sed -i 's|^[[:blank:]]*||g' $(BUILD_DIR_BASE)/ldgen.section_infos
+	sed -E -i.bak 's|^[[:blank:]]*||g' $(BUILD_DIR_BASE)/ldgen.section_infos
+	rm $(BUILD_DIR_BASE)/ldgen.section_infos.bak
 ifeq ($(OS), Windows_NT)
 	mv $(BUILD_DIR_BASE)/ldgen.section_infos $(BUILD_DIR_BASE)/ldgen.section_infos.temp
 	cygpath -w -f $(BUILD_DIR_BASE)/ldgen.section_infos.temp > $(BUILD_DIR_BASE)/ldgen.section_infos
