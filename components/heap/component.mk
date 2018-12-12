d47f480728b9408d349d7c4cf7d8bea91dfbfa37
Alexey Gerenkov <alexey@espressif.com>
2018-12-12 20:29:47 +0300

    heap: Separate standalone and common part of tracing module

--- components/heap/component.mk
+++ components/heap/component.mk
@@ -2,7 +2,7 @@
 # Component Makefile
 #
 
-COMPONENT_OBJS := heap_caps_init.o heap_caps.o multi_heap.o heap_trace.o
+COMPONENT_OBJS := heap_caps_init.o heap_caps.o multi_heap.o
 
 ifndef CONFIG_HEAP_POISONING_DISABLED
 COMPONENT_OBJS += multi_heap_poisoning.o
@@ -10,6 +10,12 @@ COMPONENT_OBJS += multi_heap_poisoning.o
 ifdef CONFIG_HEAP_TASK_TRACKING
 COMPONENT_OBJS += heap_task_info.o
 endif
+endif
+
+ifdef CONFIG_HEAP_TRACING_STANDALONE
+
+COMPONENT_OBJS += heap_trace_standalone.o
+
 endif
 
 ifdef CONFIG_HEAP_TRACING
