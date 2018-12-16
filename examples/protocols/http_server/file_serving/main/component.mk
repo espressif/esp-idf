d82efb99068d511c9ac6757de65a6fbbd05ccc49
Anurag Kar <anurag.kar@espressif.com>
2018-12-16 23:40:24 +0530

    HTTP Server : File server example added
    This example demonstrates the capability of wildcard URI matching
    allowing for a full fledged file server to be created using esp_http_server.

---
+++ examples/protocols/http_server/file_serving/main/component.mk
@@ -0,0 +1,7 @@
+#
+# "main" pseudo-component makefile.
+#
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+COMPONENT_EMBED_FILES := favicon.ico
+COMPONENT_EMBED_FILES += upload_script.html
