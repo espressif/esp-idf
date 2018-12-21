e91dd4a458915bf165260a155935a85f7a73e3a6
Ivan Grokhotkov <ivan@espressif.com>
2018-12-21 12:49:33 +0800

    freertos: check that mutex is released by owner task
    Mutex type semaphores should be acquired and released by the same task.
    Add a check to xQueueGenericSend for this condition.

--- components/freertos/queue.c
+++ components/freertos/queue.c
@@ -724,6 +724,12 @@ Queue_t * const pxQueue = ( Queue_t * ) xQueue;
 		configASSERT( !( ( xTaskGetSchedulerState() == taskSCHEDULER_SUSPENDED ) && ( xTicksToWait != 0 ) ) );
 	}
 	#endif
+	#if ( configUSE_MUTEXES == 1 && configCHECK_MUTEX_GIVEN_BY_OWNER == 1)
+	{
+		configASSERT(pxQueue->uxQueueType != queueQUEUE_IS_MUTEX || pxQueue->pxMutexHolder == NULL || xTaskGetCurrentTaskHandle() == pxQueue->pxMutexHolder);
+	}
+	#endif
+
 
 
 	/* This function relaxes the coding standard somewhat to allow return
