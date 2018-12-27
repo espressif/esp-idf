76c20ab9b309766d27371ddba30b58a95c356980
Liu Zhi Fu <liuzhifu@espressif.com>
2018-12-27 21:55:34 +0800

    esp32: make WiFi IRAM optimization configurable
    With this feature, the WiFi IRAM optimization can be disabled/enabled via menuconfig.

--- components/esp32/component.mk
+++ components/esp32/component.mk
@@ -18,6 +18,13 @@ endif
 #specifies its own scripts.
 LINKER_SCRIPTS += esp32.common.ld esp32.rom.ld esp32.peripherals.ld
 
+# Add a different linker search path depending on WiFi optimisations
+ifdef CONFIG_ESP32_WIFI_IRAM_OPT
+COMPONENT_ADD_LDFLAGS += -L $(COMPONENT_PATH)/ld/wifi_iram_opt
+else
+COMPONENT_ADD_LDFLAGS += -L $(COMPONENT_PATH)/ld/wifi_iram_noopt
+endif
+
 #Force pure functions from libgcc.a to be linked from ROM
 LINKER_SCRIPTS += esp32.rom.libgcc.ld
 
