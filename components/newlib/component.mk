20d3458c783a04573c537636f5a6a48538698d67
Ivan Grokhotkov <ivan@espressif.com>
2019-03-08 15:07:50 +0800

    newlib: when compiling with GCC8, use newlib headers and libraries from toolchain

--- components/newlib/component.mk
+++ components/newlib/component.mk
@@ -1,8 +1,26 @@
 
+ifeq ($(GCC_NOT_5_2_0), 1)
+
+ifdef CONFIG_NEWLIB_NANO_FORMAT
+LIBC := c_nano
+else  # CONFIG_NEWLIB_NANO_FORMAT
+LIBC := c
+endif  # CONFIG_NEWLIB_NANO_FORMAT
+
+COMPONENT_ADD_LDFLAGS := -l$(LIBC) -lm -lnewlib
+COMPONENT_ADD_INCLUDEDIRS := platform_include
+
+ifdef CONFIG_SPIRAM_CACHE_WORKAROUND
+COMPONENT_ADD_LDFRAGMENTS := esp32-spiram-rom-functions-c.lf
+endif
+
+else # GCC_NOT_5_2_0
+# Remove this section when GCC 5.2.0 is no longer supported
+
 ifdef CONFIG_SPIRAM_CACHE_WORKAROUND
 LIBC_PATH := $(COMPONENT_PATH)/lib/libc-psram-workaround.a
 LIBM_PATH := $(COMPONENT_PATH)/lib/libm-psram-workaround.a
-COMPONENT_ADD_LDFRAGMENTS := esp32-spiram-rom-functions.lf
+COMPONENT_ADD_LDFRAGMENTS := esp32-spiram-rom-functions-psram-workaround.lf
 else
 
 ifdef CONFIG_NEWLIB_NANO_FORMAT
@@ -20,5 +38,6 @@ COMPONENT_ADD_LDFLAGS := $(LIBC_PATH) $(LIBM_PATH) -lnewlib
 COMPONENT_ADD_LINKER_DEPS := $(LIBC_PATH) $(LIBM_PATH)
 
 COMPONENT_ADD_INCLUDEDIRS := platform_include include
+endif  # GCC_NOT_5_2_0
 
 syscalls.o: CFLAGS += -fno-builtin
