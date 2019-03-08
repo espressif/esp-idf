bf2ddd6edb0c9b2ed32bbb56cdc3460c6b6a3f75
Ivan Grokhotkov <ivan@espressif.com>
2019-03-08 15:07:00 +0800

    pthread: add dummy implementation of pthread_setcancelstate
    Used by new versions of newlib in stdio functions.

--- components/newlib/pthread.c
+++ components/newlib/pthread.c
@@ -8,3 +8,8 @@ int pthread_condattr_setclock(pthread_condattr_t *attr, clockid_t clock_id)
     ESP_LOGW(TAG, "%s: not yet supported!", __FUNCTION__);   
     return 0;
 }
+
+int pthread_setcancelstate(int state, int *oldstate)
+{
+    return 0;
+}
