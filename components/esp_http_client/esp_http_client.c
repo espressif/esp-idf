ff2624f09c576ddc1e8b75f34f73ed3f957cd825
Jitin George <jitin@espressif.com>
2019-03-14 11:20:02 +0530

    esp_http_client: Fix header sent event bug

--- components/esp_http_client/esp_http_client.c
+++ components/esp_http_client/esp_http_client.c
@@ -1095,6 +1095,7 @@ static esp_err_t esp_http_client_request_send(esp_http_client_handle_t client, i
 
     client->data_written_index = 0;
     client->data_write_left = client->post_len;
+    http_dispatch_event(client, HTTP_EVENT_HEADER_SENT, NULL, 0);
     client->state = HTTP_STATE_REQ_COMPLETE_HEADER;
     return ESP_OK;
 }
