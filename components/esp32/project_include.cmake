ffec9d494722a11378ed3c05a6286b627672e4f5
Renz Christian Bagaporo <renz@espressif.com>
2019-05-10 10:53:08 +0800

    components: update with build system changes

--- components/esp32/project_include.cmake
+++ components/esp32/project_include.cmake
@@ -3,6 +3,19 @@ if(CONFIG_SPIRAM_CACHE_WORKAROUND)
     # are not part of the ESP-IDF build system (for cases where a generic
     # non-IDF CMakeLists.txt file is imported into a component) don't depend
     # on the esp32 component so don't get the extra flag. This handles that case.
-    add_compile_options(-mfix-esp32-psram-cache-issue)
+    idf_build_set_property(COMPILE_OPTIONS "-mfix-esp32-psram-cache-issue" APPEND)
 endif()
 
+# Check toolchain is configured properly in cmake
+if(NOT ( ${CMAKE_SYSTEM_NAME} STREQUAL "Generic" AND ${CMAKE_C_COMPILER} MATCHES xtensa))
+    message(FATAL_ERROR "Internal error, toolchain has not been set correctly by project "
+        "(or an invalid CMakeCache.txt file has been generated somehow)")
+endif()
+
+#
+# Warn if the toolchain version doesn't match
+#
+# TODO: make these platform-specific for diff toolchains
+get_expected_ctng_version(expected_toolchain expected_gcc)
+gcc_version_check("${expected_gcc}")
+crosstool_version_check("${expected_toolchain}")
--- components/esptool_py/project_include.cmake
+++ components/esptool_py/project_include.cmake
@@ -1,13 +1,10 @@
-if(NOT IDF_BUILD_ARTIFACTS)
-    return()
-endif()
-
 # Set some global esptool.py variables
 #
 # Many of these are read when generating flash_app_args & flash_project_args
-set(ESPTOOLPY ${PYTHON} "${CMAKE_CURRENT_LIST_DIR}/esptool/esptool.py" --chip esp32)
-set(ESPSECUREPY ${PYTHON} "${CMAKE_CURRENT_LIST_DIR}/esptool/espsecure.py")
-set(ESPEFUSEPY ${PYTHON} "${CMAKE_CURRENT_LIST_DIR}/esptool/espefuse.py")
+idf_build_get_property(python PYTHON)
+set(ESPTOOLPY ${python} "${CMAKE_CURRENT_LIST_DIR}/esptool/esptool.py" --chip esp32)
+set(ESPSECUREPY ${python} "${CMAKE_CURRENT_LIST_DIR}/esptool/espsecure.py")
+set(ESPEFUSEPY ${python} "${CMAKE_CURRENT_LIST_DIR}/esptool/espefuse.py")
 
 set(ESPFLASHMODE ${CONFIG_ESPTOOLPY_FLASHMODE})
 set(ESPFLASHFREQ ${CONFIG_ESPTOOLPY_FLASHFREQ})
@@ -62,44 +59,51 @@ if(CONFIG_ESPTOOLPY_FLASHSIZE_DETECT)
     set(ESPFLASHSIZE detect)
 endif()
 
-get_filename_component(IDF_PROJECT_NAME ${IDF_PROJECT_EXECUTABLE} NAME_WE)
+idf_build_get_property(build_dir BUILD_DIR)
+
+idf_build_get_property(elf_name EXECUTABLE_NAME GENERATOR_EXPRESSION)
+idf_build_get_property(elf EXECUTABLE GENERATOR_EXPRESSION)
+
 if(CONFIG_SECURE_BOOT_BUILD_SIGNED_BINARIES AND NOT BOOTLOADER_BUILD)
-    set(unsigned_project_binary "${IDF_PROJECT_NAME}-unsigned.bin")
+    set(unsigned_project_binary "${elf_name}-unsigned.bin")
 else()
-    set(unsigned_project_binary "${IDF_PROJECT_NAME}.bin")
+    set(unsigned_project_binary "${elf_name}.bin")
 endif()
+
+set(PROJECT_BIN "${elf_name}.bin")
 
 #
 # Add 'app.bin' target - generates with elf2image
 #
-add_custom_command(OUTPUT "${IDF_BUILD_ARTIFACTS_DIR}/${unsigned_project_binary}"
+add_custom_command(OUTPUT "${build_dir}/.app_hash"
     COMMAND ${ESPTOOLPY} elf2image ${ESPTOOLPY_ELF2IMAGE_FLASH_OPTIONS} ${ESPTOOLPY_ELF2IMAGE_OPTIONS}
-        -o "${IDF_BUILD_ARTIFACTS_DIR}/${unsigned_project_binary}" "${IDF_PROJECT_EXECUTABLE}"
-    DEPENDS ${IDF_PROJECT_EXECUTABLE}
+        -o "${build_dir}/${unsigned_project_binary}" "${elf}"
+    COMMAND ${CMAKE_COMMAND} -E md5sum "${build_dir}/${unsigned_project_binary}" > "${build_dir}/.app_hash"
+    DEPENDS ${elf}
     VERBATIM
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    WORKING_DIRECTORY ${build_dir}
     )
-
-get_filename_component(IDF_PROJECT_BIN ${IDF_PROJECT_EXECUTABLE} NAME_WE)
-set(IDF_PROJECT_BIN ${IDF_PROJECT_BIN}.bin)
+add_custom_target(gen_project_binary DEPENDS "${build_dir}/.app_hash")
 
 if(NOT BOOTLOADER_BUILD AND
     CONFIG_SECURE_BOOT_BUILD_SIGNED_BINARIES)
 
     # for locally signed secure boot image, add a signing step to get from unsigned app to signed app
-    add_custom_target(gen_unsigned_project_binary ALL DEPENDS "${IDF_BUILD_ARTIFACTS_DIR}/${unsigned_project_binary}")
-    add_custom_command(OUTPUT "${IDF_BUILD_ARTIFACTS_DIR}/${IDF_PROJECT_BIN}"
+    add_custom_command(OUTPUT "${build_dir}/.signed_app_hash"
         COMMAND ${ESPSECUREPY} sign_data --keyfile ${secure_boot_signing_key}
-            -o "${IDF_BUILD_ARTIFACTS_DIR}/${IDF_PROJECT_BIN}" "${IDF_BUILD_ARTIFACTS_DIR}/${unsigned_project_binary}"
-        DEPENDS gen_unsigned_project_binary
+            -o "${build_dir}/${PROJECT_BIN}" "${build_dir}/${unsigned_project_binary}"
+        COMMAND ${CMAKE_COMMAND} -E md5sum "${build_dir}/${PROJECT_BIN}" > "${build_dir}/.signed_app_hash"
+        DEPENDS "${build_dir}/.app_hash"
         VERBATIM
         )
+    add_custom_target(gen_signed_project_binary DEPENDS "${build_dir}/.signed_app_hash")
+    add_dependencies(gen_project_binary gen_signed_project_binary)
 endif()
 
 if(NOT BOOTLOADER_BUILD)
-    add_custom_target(app ALL DEPENDS "${IDF_BUILD_ARTIFACTS_DIR}/${IDF_PROJECT_BIN}")
+    add_custom_target(app ALL DEPENDS gen_project_binary)
 else()
-    add_custom_target(bootloader ALL DEPENDS "${IDF_BUILD_ARTIFACTS_DIR}/${IDF_PROJECT_BIN}")
+    add_custom_target(bootloader ALL DEPENDS gen_project_binary)
 endif()
 
 if(NOT BOOTLOADER_BUILD AND
@@ -109,7 +113,7 @@ if(NOT BOOTLOADER_BUILD AND
         COMMAND ${CMAKE_COMMAND} -E echo
             "App built but not signed. Sign app before flashing"
         COMMAND ${CMAKE_COMMAND} -E echo
-            "\t${ESPSECUREPY} sign_data --keyfile KEYFILE ${IDF_BUILD_ARTIFACTS_DIR}/${IDF_PROJECT_BIN}"
+            "\t${ESPSECUREPY} sign_data --keyfile KEYFILE ${build_dir}/${elf_bin}"
         VERBATIM)
 endif()
 
@@ -117,12 +121,13 @@ endif()
 # Add 'flash' target - not all build systems can run this directly
 #
 function(esptool_py_custom_target target_name flasher_filename dependencies)
+    idf_build_get_property(idf_path IDF_PATH)
     add_custom_target(${target_name} DEPENDS ${dependencies}
         COMMAND ${CMAKE_COMMAND}
-        -D IDF_PATH="${IDF_PATH}"
+        -D IDF_PATH="${idf_path}"
         -D ESPTOOLPY="${ESPTOOLPY}"
         -D ESPTOOL_ARGS="write_flash;@flash_${flasher_filename}_args"
-        -D ESPTOOL_WORKING_DIR="${IDF_BUILD_ARTIFACTS_DIR}"
+        -D ESPTOOL_WORKING_DIR="${build_dir}"
         -P run_esptool.cmake
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
         USES_TERMINAL
@@ -142,8 +147,9 @@ function(esptool_py_flash_project_args entry offset image)
     set(options FLASH_IN_PROJECT)  # flash the image when flashing the project
     set(single_value FLASH_FILE_TEMPLATE) # template file to use to be able to
                                         # flash the image individually using esptool
-    cmake_parse_arguments(flash_entry "${options}" "${single_value}" "" "${ARGN}")
+    cmake_parse_arguments(_ "${options}" "${single_value}" "" "${ARGN}")
 
+    idf_build_get_property(build_dir BUILD_DIR)
     get_property(flash_project_entries TARGET flash_project_args_target PROPERTY FLASH_PROJECT_ENTRIES)
 
     if(${entry} IN_LIST flash_project_entries)
@@ -156,14 +162,16 @@ function(esptool_py_flash_project_args entry offset image)
     file(RELATIVE_PATH image ${CMAKE_BINARY_DIR} ${image})
 
     # Generate the standalone flash file to flash the image individually using esptool
-    set(entry_flash_args ${IDF_BUILD_ARTIFACTS_DIR}/flash_${entry}_args)
-    if(NOT flash_entry_FLASH_FILE_TEMPLATE)
+    set(entry_flash_args ${build_dir}/flash_${entry}_args)
+    if(NOT __FLASH_FILE_TEMPLATE)
         file(GENERATE OUTPUT ${entry_flash_args} CONTENT "${offset} ${image}")
     else()
-        configure_file(${flash_entry_FLASH_FILE_TEMPLATE} ${entry_flash_args})
+        get_filename_component(template "${__FLASH_FILE_TEMPLATE}" ABSOLUTE)
+        file(GENERATE OUTPUT ${entry_flash_args} INPUT ${template})
     endif()
 
-    set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${entry_flash_args})
+    set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${entry_flash_args})
 
     # Generate standalone entries in the flasher args json file
     get_property(flash_project_args_entry_json TARGET
@@ -174,7 +182,7 @@ function(esptool_py_flash_project_args entry offset image)
                 PROPERTY FLASH_PROJECT_ARGS_ENTRY_JSON "${flash_project_args_entry_json}")
 
     # Generate entries in the flasher args json file
-    if(flash_entry_FLASH_IN_PROJECT)
+    if(__FLASH_IN_PROJECT)
         get_property(flash_project_args TARGET flash_project_args_target PROPERTY FLASH_PROJECT_ARGS)
         list(APPEND flash_project_args "${offset} ${image}")
         set_property(TARGET flash_project_args_target PROPERTY FLASH_PROJECT_ARGS "${flash_project_args}")
