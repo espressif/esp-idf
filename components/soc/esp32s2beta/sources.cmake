28286183d16a9087649eb9c7cae741e07e12ab14
houwenxiang <houwenxiang@espressif.com>
2019-06-05 11:32:20 +0800

    feature(I2C): Add i2c hal support.

--- components/soc/esp32s2beta/sources.cmake
+++ components/soc/esp32s2beta/sources.cmake
@@ -12,7 +12,8 @@ set(SOC_SRCS    "cpu_util.c"
                 "soc_memory_layout.c"
                 "spi_periph.c"
                 "ledc_periph.c"
-                "i2s_periph.c")
+                "i2s_periph.c"
+                "i2c_periph.c")
 
 if(NOT CMAKE_BUILD_EARLY_EXPANSION)
     set_source_files_properties("esp32s2beta/rtc_clk.c" PROPERTIES
