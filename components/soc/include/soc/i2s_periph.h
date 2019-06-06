81bbf91a1856e2181afb14723bab3295cf067935
kooho <2229179028@qq.com>
2019-06-06 17:00:03 +0800

    Update I2S driver for esp32s2beta.

--- components/soc/include/soc/i2s_periph.h
+++ components/soc/include/soc/i2s_periph.h
@@ -13,5 +13,36 @@
 // limitations under the License.
 
 #pragma once
+#include "soc/soc.h"
+#include "soc/periph_defs.h"
 #include "soc/i2s_struct.h"
 #include "soc/i2s_reg.h"
+#include "soc/i2s_caps.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ Stores a bunch of per-I2S-peripheral data.
+*/
+typedef struct {
+    const uint8_t o_bck_in_sig;
+    const uint8_t o_ws_in_sig;
+    const uint8_t o_bck_out_sig;
+    const uint8_t o_ws_out_sig;
+    const uint8_t o_data_out_sig;
+    const uint8_t i_bck_in_sig;
+    const uint8_t i_ws_in_sig;
+    const uint8_t i_bck_out_sig;
+    const uint8_t i_ws_out_sig;
+    const uint8_t i_data_in_sig;
+    const uint8_t irq;
+    const periph_module_t module;
+} i2s_signal_conn_t;
+
+extern const i2s_signal_conn_t i2s_periph_signal[SOC_I2S_PERIPH_NUM];
+
+#ifdef __cplusplus
+}
+#endif
