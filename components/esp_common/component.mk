3be6fd6818dd3c66735425813ee1ccea7490eb0a
Mahavir Jain <mahavir@espressif.com>
2019-06-06 18:51:52 +0530

    esp_common: fix stack protection crash due to incorrect CFLAG

--- components/esp_common/component.mk
+++ components/esp_common/component.mk
@@ -6,4 +6,4 @@ COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENT_SRCDIRS := src
 
 # disable stack protection in files which are involved in initialization of that feature
-stack_check.o: CFLAGS := $(filter-out -fstack-protector%, $(CFLAGS))
+src/stack_check.o: CFLAGS := $(filter-out -fstack-protector%, $(CFLAGS))
