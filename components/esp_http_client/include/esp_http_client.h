cbfd56245ff1a35a3ec5881041cbb64dc726fc12
Jitin George <jitin@espressif.com>
2019-06-12 14:12:59 +0530

    esp_http_client: Add support to disable validation of certificate's CN field

--- components/esp_http_client/include/esp_http_client.h
+++ components/esp_http_client/include/esp_http_client.h
@@ -120,6 +120,7 @@ typedef struct {
     void                        *user_data;               /*!< HTTP user_data context */
     bool                        is_async;                 /*!< Set asynchronous mode, only supported with HTTPS for now */
     bool                        use_global_ca_store;      /*!< Use a global ca_store for all the connections in which this bool is set. */
+    bool                        skip_cert_common_name_check;    /*!< Skip any validation of server certificate CN field */
 } esp_http_client_config_t;
 
 /**
