f2f22e393c4c52e5b3e815b82cc8d67551b724ce
Li Shuai <lishuai@espressif.com>
2019-06-27 21:45:24 +0800

    util-test: Copy the test in the esp32 directory to the esp32s2beta directory

---
+++ components/esp32s2beta/test/component.mk
@@ -0,0 +1,15 @@
+#
+#Component Makefile
+#
+
+COMPONENT_EXTRA_CLEAN := test_tjpgd_logo.h
+
+COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
+
+COMPONENT_SRCDIRS := .
+
+test_tjpgd.o: test_tjpgd_logo.h
+
+test_tjpgd_logo.h: $(COMPONENT_PATH)/logo.jpg
+	$(summary) XXD logo.jpg
+	cd $(COMPONENT_PATH); xxd -i logo.jpg $(COMPONENT_BUILD_DIR)/test_tjpgd_logo.h
