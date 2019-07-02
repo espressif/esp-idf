14240d69eac5ea2464426b2131e9b0a35f074fdb
Prasad Alatkar <prasad.alatkar@espressif.com>
2019-07-02 14:20:10 +0800

    NimBLE-provisioning: Add NimBLE support to unified provisioning framework
    - Adds NimBLE stack support to existing BLE variant of unified provisioning.
    - Uses scan response to send device name, 128 bit custom UUIDs based on service
      UUID.
    - Minimal changes to `app_prov.c`, component.mk, CMakeLists.txt and
      `protocomm_ble.h` to accomodate NimBLE stack for provisioning.

--- components/wifi_provisioning/component.mk
+++ components/wifi_provisioning/component.mk
@@ -2,6 +2,9 @@ COMPONENT_SRCDIRS := src proto-c
 COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENT_PRIV_INCLUDEDIRS := src proto-c ../protocomm/proto-c/
 
-ifneq ($(filter y, $(CONFIG_BT_ENABLED) $(CONFIG_BT_BLUEDROID_ENABLED)),y y)
-	COMPONENT_OBJEXCLUDE := src/scheme_ble.o
+ifndef CONFIG_BT_BLUEDROID_ENABLED
+    ifndef CONFIG_BT_NIMBLE_ENABLED
+        COMPONENT_OBJEXCLUDE := src/scheme_ble.o
+    endif
 endif
+
