b99468273675615ef83e8f95a245bc5723c28b2a
xiehang <xiehang@espressif.com>
2019-07-08 11:26:18 +0800

    WiFi: Modify the wifi_country struct in VNC to keep it consistent with IDF
    1. Modfy wifi_country struct
    2. Add md5 check esp_wifi_types.h
    3. Add md5 check esp_wifi.h

--- components/esp32/test/test_header_files_md5.c
+++ components/esp32/test/test_header_files_md5.c
@@ -11,7 +11,7 @@ TEST_CASE("wifi os adapter MD5","[wifi]")
 {    
     const char *test_wifi_os_funcs_md5 = WIFI_OS_ADAPTER_MD5;
 
-    ESP_LOGI(TAG, "test wifi os adapter MD5...");
+    ESP_LOGI(TAG, "test eps_wifi_os_adapter.h MD5...");
     TEST_ESP_OK(esp_wifi_internal_osi_funcs_md5_check(test_wifi_os_funcs_md5));
 
     ESP_LOGI(TAG, "test passed...");
@@ -21,8 +21,28 @@ TEST_CASE("wifi crypto types MD5","[wifi]")
 {    
     const char *test_wifi_crypto_funcs_md5 = WIFI_CRYPTO_MD5;
 
-    ESP_LOGI(TAG, "test wifi crypto adapter MD5...");
+    ESP_LOGI(TAG, "test esp_wifi_crypto_adapter.h MD5...");
     TEST_ESP_OK(esp_wifi_internal_crypto_funcs_md5_check(test_wifi_crypto_funcs_md5));
+
+    ESP_LOGI(TAG, "test passed...");
+}
+
+TEST_CASE("wifi type MD5","[wifi]")
+{    
+    const char *test_wifi_type_md5 = WIFI_TYPE_MD5;
+
+    ESP_LOGI(TAG, "test esp_wifi_types.h MD5...");
+    TEST_ESP_OK(esp_wifi_internal_wifi_type_md5_check(test_wifi_type_md5));
+
+    ESP_LOGI(TAG, "test passed...");
+}
+
+TEST_CASE("esp wifi MD5","[wifi]")
+{    
+    const char *test_esp_wifi_md5 = WIFI_ESP_WIFI_MD5;
+
+    ESP_LOGI(TAG, "test esp_wifi.h MD5...");
+    TEST_ESP_OK(esp_wifi_internal_esp_wifi_md5_check(test_esp_wifi_md5));
 
     ESP_LOGI(TAG, "test passed...");
 }
