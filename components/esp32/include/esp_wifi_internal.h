2dc98f3be8b21de310327e925590735aa5895ea3
xiehang <xiehang@espressif.com>
2019-07-08 11:26:18 +0800

    WiFi: Modify the wifi_country struct in VNC to keep it consistent with IDF.
    1. Modfy wifi_country struct
    2. Add md5 check esp_wifi_types.h
    3. Add md5 check esp_wifi.h

--- components/esp32/include/esp_wifi_internal.h
+++ components/esp32/include/esp_wifi_internal.h
@@ -204,6 +204,28 @@ esp_err_t esp_wifi_internal_osi_funcs_md5_check(const char *md5);
 esp_err_t esp_wifi_internal_crypto_funcs_md5_check(const char *md5);
 
 /**
+  * @brief     Check the MD5 values of the esp_wifi_types.h in IDF and WiFi library
+  *
+  * @attention 1. It is used for internal CI version check
+  *
+  * @return
+  *     - ESP_OK : succeed
+  *     - ESP_WIFI_INVALID_ARG : MD5 check fail
+  */
+esp_err_t esp_wifi_internal_wifi_type_md5_check(const char *md5);
+
+/**
+  * @brief     Check the MD5 values of the esp_wifi.h in IDF and WiFi library
+  *
+  * @attention 1. It is used for internal CI version check
+  *
+  * @return
+  *     - ESP_OK : succeed
+  *     - ESP_WIFI_INVALID_ARG : MD5 check fail
+  */
+esp_err_t esp_wifi_internal_esp_wifi_md5_check(const char *md5);
+
+/**
   * @brief     Check the git commit id of WiFi library
   *
   * @attention 1. It is used for internal CI WiFi library check
