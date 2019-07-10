ccc95191ea8fbe5ef6d37598dc45ffd2a935d413
Saket Dandawate <newton3143@gmail.com>
2019-07-10 07:37:37 -0700

    ulp: Add aditional uint32_t object to `ulp_insn_t`
    Used to get the encoded instruction from bit-field structs.
    
    Merges https://github.com/espressif/esp-idf/pull/3759

--- components/ulp/include/esp32/ulp.h
+++ components/ulp/include/esp32/ulp.h
@@ -293,6 +293,8 @@ typedef union {
         uint32_t opcode: 4;         /*!< Opcode (OPCODE_MACRO) */
     } macro;                        /*!< Format of tokens used by MACROs */
 
+    uint32_t instruction;           /*!< Encoded instruction for ULP coprocessor */
+
 } ulp_insn_t;
 
 _Static_assert(sizeof(ulp_insn_t) == 4, "ULP coprocessor instruction size should be 4 bytes");
