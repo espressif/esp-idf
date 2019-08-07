5203a1543996e5ad9aacd33672dda9103c0c8aad
Mahavir Jain <mahavir@espressif.com>
2019-08-07 11:49:55 +0530

    esp32_gdbstub: fix build error with esp32-2019r1 toolchain
    Fixes https://github.com/espressif/esp-idf/issues/3866

--- components/esp32/panic.c
+++ components/esp32/panic.c
@@ -429,6 +429,7 @@ static inline void disableAllWdts(void)
     TIMERG1.wdt_wprotect = 0;
 }
 
+#if CONFIG_ESP32_PANIC_PRINT_REBOOT || CONFIG_ESP32_PANIC_SILENT_REBOOT
 static void esp_panic_dig_reset(void) __attribute__((noreturn));
 
 static void esp_panic_dig_reset(void)
@@ -444,6 +445,7 @@ static void esp_panic_dig_reset(void)
         ;
     }
 }
+#endif
 
 static void putEntry(uint32_t pc, uint32_t sp)
 {
