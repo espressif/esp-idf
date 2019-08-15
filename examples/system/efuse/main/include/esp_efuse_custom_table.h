d7fa288a6c957c0e53f9ae5adeec62d0589bbd86
KonstantinKondrashov <konstantin@espressif.com>
2019-08-15 18:41:28 +0800

    examples: Add an example efuse API usage
    Added: read, write operations for the common and custom efuse tables.
    Writing is possible only for CONFIG_EFUSE_VIRTUAL.
    
    Closes: IDF-773

---
+++ examples/system/efuse/main/include/esp_efuse_custom_table.h
@@ -0,0 +1,36 @@
+// Copyright 2017-2018 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at",
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+// md5_digest_table 584317af6a850ee16ef6206c139d6cf5
+// This file was generated from the file esp_efuse_custom_table.csv. DO NOT CHANGE THIS FILE MANUALLY.
+// If you want to change some fields, you need to change esp_efuse_custom_table.csv file
+// then run `efuse_common_table` or `efuse_custom_table` command it will generate this file.
+// To show efuse_table run the command 'show_efuse_table'.
+
+
+extern const esp_efuse_desc_t* ESP_EFUSE_MODULE_VERSION[];
+extern const esp_efuse_desc_t* ESP_EFUSE_DEVICE_ROLE[];
+extern const esp_efuse_desc_t* ESP_EFUSE_SETTING_1[];
+extern const esp_efuse_desc_t* ESP_EFUSE_SETTING_2[];
+extern const esp_efuse_desc_t* ESP_EFUSE_CUSTOM_SECURE_VERSION[];
+
+#ifdef __cplusplus
+}
+#endif
+
