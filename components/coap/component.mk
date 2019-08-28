430b73776034e62dcdd3541bec6ee0dd47e67d22
Jon Shallow <supjps-libcoap@jpshallow.com>
2019-08-28 10:03:17 +0100

    Fix libcoap building with various MbedTLS compile time options
    components/coap/port/coap_mbedtls.c:
    
    Add in additional compile time check wrappers for different MbedTLS
    configurations.
    
    components/coap/CMakeLists.txt:
    components/coap/component.mk:
    components/coap/port/coap_notls.c:
    components/coap/port/include/coap_config_posix.h:
    
    Add in the ability to compile and run if MbedTLS does not have any TLS
    mode enabled.
    
    examples/protocols/coap_client/main/coap_client_example_main.c:
    
    Inform user that MbedTLS Client Mode is required for DTLS if not enabled,
    and coaps:// has been requested.
    [Lower libcoap library will still error and report this condition]
    
    examples/protocols/coap_server/main/coap_server_example_main.c:
    
    Inform user that MbedTLS Server Mode is required for DTLS if not enabled.
    [Lower libcoap library will still error and report this condition]
    
    Closes https://github.com/espressif/esp-idf/issues/3961
    Closes https://github.com/espressif/esp-idf/issues/3971
    Closes https://github.com/espressif/esp-idf/pull/3977

--- components/coap/component.mk
+++ components/coap/component.mk
@@ -4,7 +4,7 @@
 
 COMPONENT_ADD_INCLUDEDIRS := port/include port/include/coap libcoap/include libcoap/include/coap2
 
-COMPONENT_OBJS = libcoap/src/address.o libcoap/src/async.o libcoap/src/block.o libcoap/src/coap_event.o libcoap/src/coap_hashkey.o libcoap/src/coap_session.o libcoap/src/coap_time.o port/coap_debug.o libcoap/src/encode.o libcoap/src/mem.o libcoap/src/net.o libcoap/src/option.o libcoap/src/pdu.o libcoap/src/resource.o libcoap/src/str.o libcoap/src/subscribe.o libcoap/src/uri.o port/coap_mbedtls.o libcoap/src/coap_io.o
+COMPONENT_OBJS = libcoap/src/address.o libcoap/src/async.o libcoap/src/block.o libcoap/src/coap_event.o libcoap/src/coap_hashkey.o libcoap/src/coap_session.o libcoap/src/coap_time.o port/coap_debug.o libcoap/src/encode.o libcoap/src/mem.o libcoap/src/net.o libcoap/src/option.o libcoap/src/pdu.o libcoap/src/resource.o libcoap/src/str.o libcoap/src/subscribe.o libcoap/src/uri.o port/coap_mbedtls.o libcoap/src/coap_io.o port/coap_notls.o
 
 COMPONENT_SRCDIRS := libcoap/src libcoap port
 
