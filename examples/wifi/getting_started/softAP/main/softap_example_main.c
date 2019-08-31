76c3f568983c6bcde8f006fd4c5e3f32a9784181
David Cermak <cermak@espressif.com>
2019-08-31 16:21:31 +0200

    examples: wifi getting started examples to use esp_netif_init() and     create default netif instances for station and AP

--- examples/wifi/getting_started/softAP/main/softap_example_main.c
+++ examples/wifi/getting_started/softAP/main/softap_example_main.c
@@ -45,8 +45,9 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
 
 void wifi_init_softap(void)
 {
-    tcpip_adapter_init();
+    esp_netif_init();
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
--- examples/wifi/getting_started/station/main/station_example_main.c
+++ examples/wifi/getting_started/station/main/station_example_main.c
@@ -64,12 +64,14 @@ void wifi_init_sta(void)
 {
     s_wifi_event_group = xEventGroupCreate();
 
-    tcpip_adapter_init();
+    esp_netif_init();
 
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
 
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
