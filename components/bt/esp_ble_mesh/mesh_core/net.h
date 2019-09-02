a73cc70af92ee060aebee69b7626b94e5d4954c2
lly <lly@espressif.com>
2019-09-02 15:17:20 +0800

    ble_mesh: lpn remove msg from cache on rejection

--- components/bt/esp_ble_mesh/mesh_core/net.h
+++ components/bt/esp_ble_mesh/mesh_core/net.h
@@ -289,6 +289,7 @@ struct bt_mesh_net_rx {
            net_if: 2,      /* Network interface */
            local_match: 1, /* Matched a local element */
            friend_match: 1; /* Matched an LPN we're friends for */
+    u16_t  msg_cache_idx;  /* Index of entry in message cache */
     s8_t   rssi;
 };
 
