31dbf57c6aa28a388b7b365be0d48ad31bac9175
lly <lly@espressif.com>
2019-09-02 13:08:05 +0800

    ble_mesh: fix failed to set device role caused mem leak

--- components/bt/esp_ble_mesh/btc/btc_ble_mesh_lighting_model.c
+++ components/bt/esp_ble_mesh/btc/btc_ble_mesh_lighting_model.c
@@ -302,7 +302,7 @@ void btc_ble_mesh_lighting_client_call_handler(btc_msg_t *msg)
         role_param.role = params->msg_role;
         if (bt_mesh_set_client_model_role(&role_param)) {
             LOG_ERROR("%s, Failed to set model role", __func__);
-            return;
+            break;
         }
         common.opcode = params->opcode;
         common.model = (struct bt_mesh_model *)params->model;
@@ -328,7 +328,7 @@ void btc_ble_mesh_lighting_client_call_handler(btc_msg_t *msg)
         role_param.role = params->msg_role;
         if (bt_mesh_set_client_model_role(&role_param)) {
             LOG_ERROR("%s, Failed to set model role", __func__);
-            return;
+            break;
         }
         common.opcode = params->opcode;
         common.model = (struct bt_mesh_model *)params->model;
