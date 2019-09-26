7a450222f5152576cc6210fbf988a1ab2d849396
lly <lly@espressif.com>
2019-09-26 19:38:25 +0800

    ble_mesh: fix client model invalid address access

--- components/bt/esp_ble_mesh/mesh_core/mesh_kernel.c
+++ components/bt/esp_ble_mesh/mesh_core/mesh_kernel.c
@@ -103,7 +103,7 @@ void k_delayed_work_init(struct k_delayed_work *work, k_work_handler_t handler)
 
     osi_mutex_lock(&bm_alarm_lock, OSI_MUTEX_MAX_TIMEOUT);
     if (!hash_map_has_key(bm_alarm_hash_map, (void *)work)) {
-        alarm = osi_alarm_new("bt_mesh", (osi_alarm_callback_t)handler, (void *)work, 0);
+        alarm = osi_alarm_new("bt_mesh", (osi_alarm_callback_t)handler, (void *)&work->work, 0);
         if (alarm == NULL) {
             BT_ERR("%s, Unable to create alarm", __func__);
             return;
