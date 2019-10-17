2acdc1e2c1fa97595d74340dd8ce9992aa78e6bf
David Cermak <cermak@espressif.com>
2019-10-17 11:36:34 +0200

    esp_netif: example init code fixed to assert only variables, not function calls so it won't be skipped if compiled without asserts

--- examples/protocols/esp_local_ctrl/main/app_main.c
+++ examples/protocols/esp_local_ctrl/main/app_main.c
@@ -68,7 +68,8 @@ void wifi_init_sta(void)
     esp_netif_init();
 
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    assert(esp_netif_create_default_wifi_sta());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
--- examples/wifi/smart_config/main/smartconfig_main.c
+++ examples/wifi/smart_config/main/smartconfig_main.c
@@ -81,7 +81,8 @@ static void initialise_wifi(void)
     esp_netif_init();
     s_wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    assert(esp_netif_create_default_wifi_sta());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
