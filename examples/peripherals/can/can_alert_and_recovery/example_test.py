2e3491e5ccd4422633aac16234b204933ff7d3b6
Darian Leung <darian@espressif.com>
2019-10-25 14:10:06 +0800

    CAN: Reenable example tests
    This commit updates the CAN example tests and updates the job
    and environment labels in the target-test.yml

--- examples/peripherals/can/can_alert_and_recovery/example_test.py
+++ examples/peripherals/can/can_alert_and_recovery/example_test.py
@@ -20,7 +20,7 @@ STR_EXPECT = ("CAN Alert and Recovery: Driver installed", "CAN Alert and Recover
 EXPECT_TIMEOUT = 20
 
 
-@IDF.idf_example_test(env_tag='Example_CAN1', ignore=True)
+@IDF.idf_example_test(env_tag='Example_CAN1')
 def test_can_alert_and_recovery_example(env, extra_data):
     dut = env.get_dut('dut1', 'examples/peripherals/can/can_alert_and_recovery', dut_class=ESP32DUT)
     dut.start_app()
