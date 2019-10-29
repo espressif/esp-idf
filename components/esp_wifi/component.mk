9afba2abf53f4dbd761a601ccf9c0266fdfad67a
Xia Xiaotian <xiaxiaotian@espressif.com>
2019-10-29 15:16:32 +0800

    esp_wifi: merge esp32s2beta WiFi library to master branch

--- components/esp_wifi/component.mk
+++ components/esp_wifi/component.mk
@@ -7,14 +7,14 @@ COMPONENT_SRCDIRS := src
 
 ifndef CONFIG_ESP32_NO_BLOBS
     LIBS := core rtc net80211 pp smartconfig coexist espnow phy mesh
-    COMPONENT_ADD_LDFLAGS += -L$(COMPONENT_PATH)/lib_$(IDF_TARGET) \
+    COMPONENT_ADD_LDFLAGS += -L$(COMPONENT_PATH)/lib/$(IDF_TARGET) \
                           $(addprefix -l,$(LIBS))
 
     COMPONENT_ADD_LDFRAGMENTS += linker.lf
 
-    COMPONENT_SUBMODULES += lib_$(IDF_TARGET)
+    COMPONENT_SUBMODULES += lib
 
-    ALL_LIB_FILES := $(patsubst %,$(COMPONENT_PATH)/lib_$(IDF_TARGET)/lib%.a,$(LIBS))
+    ALL_LIB_FILES := $(patsubst %,$(COMPONENT_PATH)/lib/$(IDF_TARGET)/lib%.a,$(LIBS))
     COMPONENT_ADD_LINKER_DEPS += $(ALL_LIB_FILES)
 endif
 
