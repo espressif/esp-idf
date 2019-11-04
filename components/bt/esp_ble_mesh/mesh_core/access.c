4aa7f31f02fbfc8f7bea674e93df199d5b1d231b
lly <lly@espressif.com>
2019-11-04 17:54:50 +0800

    ble_mesh: Model message macros
    Creates macros for determining model message lengths based on opcode,
    payload length and MIC size. Also adds macro wrapping
    NET_BUF_SIMPLE_DEFINE to serve the most common use case.

--- components/bt/esp_ble_mesh/mesh_core/access.c
+++ components/bt/esp_ble_mesh/mesh_core/access.c
@@ -761,21 +761,21 @@ void bt_mesh_model_msg_init(struct net_buf_simple *msg, u32_t opcode)
 {
     net_buf_simple_init(msg, 0);
 
-    if (opcode < 0x100) {
-        /* 1-byte OpCode */
+    switch (BLE_MESH_MODEL_OP_LEN(opcode)) {
+    case 1:
         net_buf_simple_add_u8(msg, opcode);
-        return;
-    }
-
-    if (opcode < 0x10000) {
-        /* 2-byte OpCode */
+        break;
+    case 2:
         net_buf_simple_add_be16(msg, opcode);
-        return;
+        break;
+    case 3:
+        net_buf_simple_add_u8(msg, ((opcode >> 16) & 0xff));
+        net_buf_simple_add_le16(msg, opcode & 0xffff);
+        break;
+    default:
+        BT_WARN("Unknown opcode format");
+        break;
     }
-
-    /* 3-byte OpCode */
-    net_buf_simple_add_u8(msg, ((opcode >> 16) & 0xff));
-    net_buf_simple_add_le16(msg, opcode & 0xffff);
 }
 
 static bool ready_to_send(u8_t role, u16_t dst)
