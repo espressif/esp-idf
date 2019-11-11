0f3f7d984a98ba940395b76df84c7dfb61fa37c2
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2019-11-11 19:12:24 +0530

    Fixed warnings for components driver, esp32 and mbedtls

--- components/driver/rtc_module.c
+++ components/driver/rtc_module.c
@@ -995,7 +995,7 @@ esp_err_t touch_pad_filter_start(uint32_t filter_period_ms)
     }
     if (s_touch_pad_filter->timer == NULL) {
         s_touch_pad_filter->timer = xTimerCreate("filter_tmr", filter_period_ms / portTICK_PERIOD_MS, pdFALSE,
-        NULL, touch_pad_filter_cb);
+        NULL, (void(*)(TimerHandle_t))touch_pad_filter_cb);
         if (s_touch_pad_filter->timer == NULL) {
             ret = ESP_ERR_NO_MEM;
         }
