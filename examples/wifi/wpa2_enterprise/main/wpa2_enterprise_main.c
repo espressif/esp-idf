cde8df7887e8606e4327871cf99d8aaa5145c631
Hrudaynath Dhabe <hrudaynath.dhabe@espressif.com>
2019-12-02 13:54:51 +0800

    Minor bugfix in wpa2_enterprise_example

--- examples/wifi/wpa2_enterprise/main/wpa2_enterprise_main.c
+++ examples/wifi/wpa2_enterprise/main/wpa2_enterprise_main.c
@@ -107,7 +107,7 @@ static void initialise_wifi(void)
     esp_netif_init();
     wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    sta_netif = esp_netif_create_default_wifi_sta();
     assert(sta_netif);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
