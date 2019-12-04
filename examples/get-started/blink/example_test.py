74d768fe6ddbdcb794b200980aec72f5f6a4f3e2
David Cermak <cermak@espressif.com>
2019-12-04 14:21:29 +0100

    ci: updated mqtt weekend test for qemu support
    Added default sdkconfig for qemu build for the mqtt publish example,
    Added environment configuration for running the same test on target
    or in qemu
    Updated missing example tests per latest ttfw refactoring

--- examples/get-started/blink/example_test.py
+++ examples/get-started/blink/example_test.py
@@ -7,17 +7,8 @@ import re
 import os
 import hashlib
 
-try:
-    import IDF
-except ImportError:
-    # This environment variable is expected on the host machine
-    test_fw_path = os.getenv("TEST_FW_PATH")
-    if test_fw_path and test_fw_path not in sys.path:
-        sys.path.insert(0, test_fw_path)
-
-    import IDF
-from IDF.IDFDUT import ESP32DUT, ESP32QEMUDUT
-import Utility
+from tiny_test_fw import Utility
+import ttfw_idf
 
 
 def verify_elf_sha256_embedding(dut):
@@ -37,9 +28,9 @@ def verify_elf_sha256_embedding(dut):
         raise ValueError('ELF file SHA256 mismatch')
 
 
-@IDF.idf_example_test(env_tag="Example_QEMU")
+@ttfw_idf.idf_example_test(env_tag="Example_WIFI")
 def test_examples_blink(env, extra_data):
-    dut = env.get_dut("blink", "examples/get-started/blink", dut_class=ESP32QEMUDUT)
+    dut = env.get_dut("blink", "examples/get-started/blink", dut_class=ttfw_idf.ESP32DUT)
     binary_file = os.path.join(dut.app.binary_path, "blink.bin")
     bin_size = os.path.getsize(binary_file)
     ttfw_idf.log_performance("blink_bin_size", "{}KB".format(bin_size // 1024))
