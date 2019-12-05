8188138c35a1642182c52c9f913ef6fbb87240b7
ronghulin <ronghulin@espressif.com>
2019-12-05 11:12:26 +0800

    esp32: fix a WiFi RX bug
    Support WiFi/BT MAC register writting when the WiFi/BT common clock is disabled.

--- components/esp32/include/esp_phy_init.h
+++ components/esp32/include/esp_phy_init.h
@@ -203,6 +203,18 @@ esp_err_t esp_phy_rf_deinit(phy_rf_module_t module);
 void esp_phy_load_cal_and_init(phy_rf_module_t module);
 
 /**
+* @brief Enable WiFi/BT common clock
+*
+*/
+void esp_phy_common_clock_enable(void);
+
+/**
+* @brief Disable WiFi/BT common clock
+*
+*/
+void esp_phy_common_clock_disable(void);
+
+/**
  * @brief Module requires to enter modem sleep
  */
 esp_err_t esp_modem_sleep_enter(modem_sleep_module_t module);
