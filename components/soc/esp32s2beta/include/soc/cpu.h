888316fc64b7ca662475d8a7d9d1f67f16877e0c
morris <maoshengrong@espressif.com>
2019-12-12 19:03:25 +0800

    bootloader_support: refactor to better support multi target

--- components/soc/esp32s2beta/include/soc/cpu.h
+++ components/soc/esp32s2beta/include/soc/cpu.h
@@ -51,13 +51,6 @@ static inline void cpu_write_itlb(unsigned vpn, unsigned attr)
     asm volatile ("witlb  %1, %0; isync\n" :: "r" (vpn), "r" (attr));
 }
 
-static inline void cpu_init_memctl(void)
-{
-#if XCHAL_ERRATUM_572
-#error "Shouldn't have this errata or need this call on esp32s2beta"
-#endif
-}
-
 /**
  * @brief Configure memory region protection
  *
