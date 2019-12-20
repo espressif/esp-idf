3b9fe36494cf35160b1ea4269f6979d61b8b608c
lly <lly@espressif.com>
2019-12-20 14:08:33 +0800

    ble_mesh: Fix memory leak when node is reset
    When node is being reset, the init functions of each sig-defined
    models will be invoked again, this will cause memory leak because
    some model internal data will be allocated again.
    Hence before trying to allocate memory for them, we add some check
    to make sure no memory has been allocated previously.
    And for client model, when the init functions are invoked again,
    we will clear the list items.

--- components/bt/esp_ble_mesh/mesh_models/client/sensor_client.c
+++ components/bt/esp_ble_mesh/mesh_models/client/sensor_client.c
@@ -604,19 +604,22 @@ int bt_mesh_sensor_cli_init(struct bt_mesh_model *model, bool primary)
         return -EINVAL;
     }
 
-    /* TODO: call osi_free() when deinit function is invoked*/
-    internal = osi_calloc(sizeof(sensor_internal_data_t));
-    if (!internal) {
-        BT_ERR("%s, Failed to allocate memory", __func__);
-        return -ENOMEM;
+    if (!client->internal_data) {
+        internal = osi_calloc(sizeof(sensor_internal_data_t));
+        if (!internal) {
+            BT_ERR("%s, Failed to allocate memory", __func__);
+            return -ENOMEM;
+        }
+
+        sys_slist_init(&internal->queue);
+
+        client->model = model;
+        client->op_pair_size = ARRAY_SIZE(sensor_op_pair);
+        client->op_pair = sensor_op_pair;
+        client->internal_data = internal;
+    } else {
+        bt_mesh_client_clear_list(client->internal_data);
     }
-
-    sys_slist_init(&internal->queue);
-
-    client->model = model;
-    client->op_pair_size = ARRAY_SIZE(sensor_op_pair);
-    client->op_pair = sensor_op_pair;
-    client->internal_data = internal;
 
     bt_mesh_sensor_client_mutex_new();
 
