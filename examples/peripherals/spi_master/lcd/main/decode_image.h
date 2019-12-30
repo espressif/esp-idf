0f1041cc044e1daa2e4896c8887681b8657bb853
Michael (XIAO Xufeng) <xiaoxufeng@espressif.com>
2019-12-30 13:31:12 +0800

    example: move spi master example to a subfolder

--- examples/peripherals/spi_master/lcd/main/decode_image.h
+++ examples/peripherals/spi_master/lcd/main/decode_image.h
@@ -1,3 +1,11 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
 #pragma once
 #include <stdint.h>
 #include "esp_err.h"
