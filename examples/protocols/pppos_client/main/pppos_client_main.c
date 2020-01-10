espressif/esp-idf

commit 25913af2cc12cfa4667e23d931f45915a22532fe

pppos_client: udated example code to use esp-netif in PPP configuration

examples/protocols/pppos_client/main/pppos_client_main.c
@@ -10,8 +10,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "esp_netif.h"
+#include "esp_netif_ppp.h"
 #include "mqtt_client.h"
 #include "esp_modem.h"
+#include "esp_modem_netif.h"
 #include "esp_log.h"
 #include "sim800.h"
 #include "bg96.h"
@@ -112,21 +114,6 @@ static void modem_event_handler(void *event_handler_arg, esp_event_base_t event_
     case MODEM_EVENT_PPP_START:
         ESP_LOGI(TAG, "Modem PPP Started");
         break;
-    case MODEM_EVENT_PPP_CONNECT:
-        ESP_LOGI(TAG, "Modem Connect to PPP Server");
-        ppp_client_ip_info_t *ipinfo = (ppp_client_ip_info_t *)(event_data);
-        ESP_LOGI(TAG, "~~~~~~~~~~~~~~");
-        ESP_LOGI(TAG, "IP          : " IPSTR, IP2STR(&ipinfo->ip));
-        ESP_LOGI(TAG, "Netmask     : " IPSTR, IP2STR(&ipinfo->netmask));
-        ESP_LOGI(TAG, "Gateway     : " IPSTR, IP2STR(&ipinfo->gw));
-        ESP_LOGI(TAG, "Name Server1: " IPSTR, IP2STR(&ipinfo->ns1));
-        ESP_LOGI(TAG, "Name Server2: " IPSTR, IP2STR(&ipinfo->ns2));
-        ESP_LOGI(TAG, "~~~~~~~~~~~~~~");
-        xEventGroupSetBits(event_group, CONNECT_BIT);
-        break;
-    case MODEM_EVENT_PPP_DISCONNECT:
-        ESP_LOGI(TAG, "Modem Disconnect from PPP Server");
-        break;
     case MODEM_EVENT_PPP_STOP:
         ESP_LOGI(TAG, "Modem PPP Stopped");
         xEventGroupSetBits(event_group, STOP_BIT);
@@ -179,15 +166,72 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
     return ESP_OK;
 }
 
+static void on_ppp_changed(void *arg, esp_event_base_t event_base,
+                        int32_t event_id, void *event_data)
+{
+    ESP_LOGI(TAG, "PPP state changed event %d", event_id);
+    if (event_id == NETIF_PPP_ERRORUSER) {
+        /* User interrupted event from esp-netif */
+        esp_netif_t *netif = event_data;
+        ESP_LOGI(TAG, "User interrupted event from netif:%p", netif);
+    }
+}
+
+
+static void on_ip_event(void *arg, esp_event_base_t event_base,
+                      int32_t event_id, void *event_data)
+{
+    ESP_LOGI(TAG, "IP event! %d", event_id);
+    if (event_id == IP_EVENT_PPP_GOT_IP) {
+        esp_netif_dns_info_t dns_info;
+
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        esp_netif_t *netif = event->esp_netif;
+
+        ESP_LOGI(TAG, "Modem Connect to PPP Server");
+        ESP_LOGI(TAG, "~~~~~~~~~~~~~~");
+        ESP_LOGI(TAG, "IP          : " IPSTR, IP2STR(&event->ip_info.ip));
+        ESP_LOGI(TAG, "Netmask     : " IPSTR, IP2STR(&event->ip_info.netmask));
+        ESP_LOGI(TAG, "Gateway     : " IPSTR, IP2STR(&event->ip_info.ip));
+        esp_netif_get_dns_info(netif, 0, &dns_info);
+        ESP_LOGI(TAG, "Name Server1: " IPSTR, IP2STR(&dns_info.ip.u_addr.ip4));
+        esp_netif_get_dns_info(netif, 1, &dns_info);
+        ESP_LOGI(TAG, "Name Server2: " IPSTR, IP2STR(&dns_info.ip.u_addr.ip4));
+        ESP_LOGI(TAG, "~~~~~~~~~~~~~~");
+        xEventGroupSetBits(event_group, CONNECT_BIT);
+
+        ESP_LOGI(TAG, "GOT ip event!!!");
+    } else if (event_id == IP_EVENT_PPP_LOST_IP) {
+        ESP_LOGI(TAG, "Modem Disconnect from PPP Server");
+    }
+}
+
 void app_main(void)
 {
+#if CONFIG_LWIP_PPP_PAP_SUPPORT
+    esp_netif_auth_type_t auth_type = NETIF_PPP_AUTHTYPE_PAP;
+#elif CONFIG_LWIP_PPP_CHAP_SUPPORT
+    esp_netif_auth_type_t auth_type = NETIF_PPP_AUTHTYPE_CHAP;
+#else
+#error "Unsupported AUTH Negotiation"
+#endif
     esp_netif_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, ESP_EVENT_ANY_ID, &on_ip_event, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(NETIF_PPP_STATUS, ESP_EVENT_ANY_ID, &on_ppp_changed, NULL));
+
     event_group = xEventGroupCreate();
+
+    // Init netif object
+    esp_netif_config_t cfg = ESP_NETIF_DEFAULT_PPP();
+    esp_netif_t *esp_netif = esp_netif_new(&cfg);
+    assert(esp_netif);
+
     /* create dte object */
     esp_modem_dte_config_t config = ESP_MODEM_DTE_DEFAULT_CONFIG();
     modem_dte_t *dte = esp_modem_dte_init(&config);
     /* Register event handler */
-    ESP_ERROR_CHECK(esp_modem_add_event_handler(dte, modem_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_modem_add_event_handler(dte, modem_event_handler, ESP_EVENT_ANY_ID, NULL));
     /* create dce object */
 #if CONFIG_EXAMPLE_MODEM_DEVICE_SIM800
     modem_dce_t *dce = sim800_init(dte);
@@ -211,10 +255,15 @@ void app_main(void)
     uint32_t voltage = 0, bcs = 0, bcl = 0;
     ESP_ERROR_CHECK(dce->get_battery_status(dce, &bcs, &bcl, &voltage));
     ESP_LOGI(TAG, "Battery voltage: %d mV", voltage);
-    /* Setup PPP environment */
-    esp_modem_setup_ppp(dte);
+    /* setup PPPoS network parameters */
+    esp_netif_ppp_set_auth(esp_netif, auth_type, CONFIG_EXAMPLE_MODEM_PPP_AUTH_USERNAME, CONFIG_EXAMPLE_MODEM_PPP_AUTH_PASSWORD);
+    void *modem_netif_adapter = esp_modem_netif_setup(dte);
+    esp_modem_netif_set_default_handlers(modem_netif_adapter, esp_netif);
+    /* attach the modem to the network interface */
+    esp_netif_attach(esp_netif, modem_netif_adapter);
     /* Wait for IP address */
     xEventGroupWaitBits(event_group, CONNECT_BIT, pdTRUE, pdTRUE, portMAX_DELAY);
+    ESP_ERROR_CHECK(dce->power_down(dce));
     /* Config MQTT */
     esp_mqtt_client_config_t mqtt_config = {
         .uri = BROKER_URL,
@@ -225,7 +274,10 @@ void app_main(void)
     xEventGroupWaitBits(event_group, GOT_DATA_BIT, pdTRUE, pdTRUE, portMAX_DELAY);
     esp_mqtt_client_destroy(mqtt_client);
     /* Exit PPP mode */
-    ESP_ERROR_CHECK(esp_modem_exit_ppp(dte));
+    ESP_ERROR_CHECK(esp_modem_stop_ppp(dte));
+    /* Destroy the netif adapter withe events, which internally frees also the esp-netif instance */
+    esp_modem_netif_clear_default_handlers(modem_netif_adapter);
+    esp_modem_netif_teardown(modem_netif_adapter);
     xEventGroupWaitBits(event_group, STOP_BIT, pdTRUE, pdTRUE, portMAX_DELAY);
 #if CONFIG_EXAMPLE_SEND_MSG
     const char *message = "Welcome to ESP32!";

components/esp_netif/include/esp_netif_ppp.h
@@ -1,0 +1,109 @@
+// Copyright 2019 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef _ESP_NETIF_PPP_H_
+#define _ESP_NETIF_PPP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @brief PPP event base */
+ESP_EVENT_DECLARE_BASE(NETIF_PPP_STATUS);
+
+/** @brief Configuration structure for PPP network interface
+ *
+ */
+typedef struct esp_netif_ppp_config {
+    bool ppp_phase_event_enabled;  /**< Enables events coming from PPP PHASE change */
+    bool ppp_error_event_enabled;  /**< Enables events from main PPP state machine producing errors */
+} esp_netif_ppp_config_t;
+
+/** @brief event id offset for PHASE related events
+ *
+ * All PPP related events are produced from esp-netif under `NETIF_PPP_STATUS`, this offset defines
+ * helps distinguish between error and phase events
+ */
+#define NETIF_PP_PHASE_OFFSET (0x100)
+
+/** @brief event ids for different PPP related events
+ *
+ */
+typedef enum {
+    NETIF_PPP_ERRORNONE        = 0,  /* No error. */
+    NETIF_PPP_ERRORPARAM       = 1,  /* Invalid parameter. */
+    NETIF_PPP_ERROROPEN        = 2,  /* Unable to open PPP session. */
+    NETIF_PPP_ERRORDEVICE      = 3,  /* Invalid I/O device for PPP. */
+    NETIF_PPP_ERRORALLOC       = 4,  /* Unable to allocate resources. */
+    NETIF_PPP_ERRORUSER        = 5,  /* User interrupt. */
+    NETIF_PPP_ERRORCONNECT     = 6,  /* Connection lost. */
+    NETIF_PPP_ERRORAUTHFAIL    = 7,  /* Failed authentication challenge. */
+    NETIF_PPP_ERRORPROTOCOL    = 8,  /* Failed to meet protocol. */
+    NETIF_PPP_ERRORPEERDEAD    = 9,  /* Connection timeout */
+    NETIF_PPP_ERRORIDLETIMEOUT = 10, /* Idle Timeout */
+    NETIF_PPP_ERRORCONNECTTIME = 11, /* Max connect time reached */
+    NETIF_PPP_ERRORLOOPBACK    = 12, /* Loopback detected */
+    NETIF_PPP_PHASE_DEAD         = NETIF_PP_PHASE_OFFSET +  0,
+    NETIF_PPP_PHASE_MASTER       = NETIF_PP_PHASE_OFFSET +  1,
+    NETIF_PPP_PHASE_HOLDOFF      = NETIF_PP_PHASE_OFFSET +  2,
+    NETIF_PPP_PHASE_INITIALIZE   = NETIF_PP_PHASE_OFFSET +  3,
+    NETIF_PPP_PHASE_SERIALCONN   = NETIF_PP_PHASE_OFFSET +  4,
+    NETIF_PPP_PHASE_DORMANT      = NETIF_PP_PHASE_OFFSET +  5,
+    NETIF_PPP_PHASE_ESTABLISH    = NETIF_PP_PHASE_OFFSET +  6,
+    NETIF_PPP_PHASE_AUTHENTICATE = NETIF_PP_PHASE_OFFSET +  7,
+    NETIF_PPP_PHASE_CALLBACK     = NETIF_PP_PHASE_OFFSET +  8,
+    NETIF_PPP_PHASE_NETWORK      = NETIF_PP_PHASE_OFFSET +  9,
+    NETIF_PPP_PHASE_RUNNING      = NETIF_PP_PHASE_OFFSET +  10,
+    NETIF_PPP_PHASE_TERMINATE    = NETIF_PP_PHASE_OFFSET +  11,
+    NETIF_PPP_PHASE_DISCONNECT   = NETIF_PP_PHASE_OFFSET +  12,
+} esp_netif_ppp_status_event_t;
+
+/** @brief definitions of different authorisation types
+ *
+ */
+typedef enum {
+    NETIF_PPP_AUTHTYPE_PAP =       0x01,
+    NETIF_PPP_AUTHTYPE_CHAP =      0x02,
+    NETIF_PPP_AUTHTYPE_MSCHAP =    0x04,
+    NETIF_PPP_AUTHTYPE_MSCHAP_V2 = 0x08,
+    NETIF_PPP_AUTHTYPE_EAP =       0x10,
+} esp_netif_auth_type_t;
+
+/** @brief Sets the auth parameters for the supplied esp-netif.
+ *
+ * @param[in]  esp_netif Handle to esp-netif instance
+ * @param[in]  authtype Authorisation type
+ * @param[in]  user User name
+ * @param[in]  passwd Password
+ *
+ * @return     ESP_OK on success, ESP_ERR_ESP_NETIF_INVALID_PARAMS if netif null or not PPP
+ */
+esp_err_t esp_netif_ppp_set_auth(esp_netif_t *netif, esp_netif_auth_type_t authtype, const char *user, const char *passwd);
+
+/** @brief Sets common parameters for the supplied esp-netif.
+ *
+ * @param[in]  esp_netif Handle to esp-netif instance
+ * @param[in]  config Pointer to PPP netif configuration structure
+ *
+ * @return     ESP_OK on success, ESP_ERR_ESP_NETIF_INVALID_PARAMS if netif null or not PPP
+ */
+esp_err_t esp_netif_ppp_set_params(esp_netif_t *netif, const esp_netif_ppp_config_t *config);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //_ESP_NETIF_PPP_H_

components/esp_netif/lwip/esp_netif_lwip_ppp.c
@@ -1,0 +1,327 @@
+// Copyright 2019 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "lwip/dns.h"
+#include "esp_netif.h"
+#include "netif/ppp/pppapi.h"
+#include "netif/ppp/pppos.h"
+#include "esp_log.h"
+#include "esp_netif_net_stack.h"
+#include "esp_event.h"
+#include "esp_netif_ppp.h"
+#include "esp_netif_lwip_internal.h"
+
+ESP_EVENT_DEFINE_BASE(NETIF_PPP_STATUS);
+
+static const char *TAG = "esp-netif_lwip-ppp";
+
+#if PPPOS_SUPPORT
+
+/**
+ * @brief internal lwip_ppp context struct, used to hold PPP netif related parameters
+ */
+struct lwip_ppp_ctx {
+    bool ppp_phase_event_enabled;
+    bool ppp_error_event_enabled;
+    ppp_pcb *ppp;
+};
+
+/**
+ * @brief lwip callback from PPP client used here to produce PPP error related events,
+ * as well as some IP events
+ */
+static void on_ppp_status_changed(ppp_pcb *pcb, int err_code, void *ctx)
+{
+    struct netif *pppif = ppp_netif(pcb);
+    const ip_addr_t *dest_ip = NULL;
+    ip_event_ap_staipassigned_t evt = { 0 };
+    esp_err_t err;
+    esp_netif_t *netif = ctx;
+    struct lwip_ppp_ctx *obj =  netif->lwip_ppp_ctx;
+    esp_netif_ip_info_t ipinfo = { {0}, {0}, {0} };
+    esp_ip4_addr_t ns1;
+    esp_ip4_addr_t ns2;
+    switch (err_code) {
+        case PPPERR_NONE: /* Connected */
+            ESP_LOGI(TAG, "Connected");
+
+            ipinfo.ip.addr = pppif->ip_addr.u_addr.ip4.addr;
+            ipinfo.gw.addr = pppif->gw.u_addr.ip4.addr;
+            ipinfo.netmask.addr = pppif->netmask.u_addr.ip4.addr;
+            dest_ip = dns_getserver(0);
+            if(dest_ip != NULL){
+                ns1.addr = (*dest_ip).u_addr.ip4.addr;
+            }
+            dest_ip = dns_getserver(1);
+            if(dest_ip != NULL){
+                ns2.addr = (*dest_ip).u_addr.ip4.addr;
+            }
+            ESP_LOGI(TAG, "Name Server1: " IPSTR, IP2STR(&ns1));
+            ESP_LOGI(TAG, "Name Server2: " IPSTR, IP2STR(&ns2));
+
+            evt.ip.addr =  ipinfo.ip.addr;
+
+            err = esp_event_post(IP_EVENT, netif->get_ip_event, &evt, sizeof(evt), 0);
+            if (ESP_OK != err) {
+                ESP_LOGE(TAG, "esp_event_send_internal failed with code %d", err);
+            }
+            return;
+
+        case PPPERR_PARAM:
+            ESP_LOGE(TAG, "Invalid parameter");
+            break;
+        case PPPERR_OPEN:
+            ESP_LOGE(TAG, "Unable to open PPP session");
+            break;
+        case PPPERR_DEVICE:
+            ESP_LOGE(TAG, "Invalid I/O device for PPP");
+            break;
+        case PPPERR_ALLOC:
+            ESP_LOGE(TAG, "Unable to allocate resources");
+            break;
+        case PPPERR_USER: /* User interrupt */
+            ESP_LOGI(TAG, "User interrupt");
+            break;
+        case PPPERR_CONNECT: /* Connection lost */
+            ESP_LOGI(TAG, "Connection lost");
+            err = esp_event_post(IP_EVENT, netif->lost_ip_event, &evt, sizeof(evt), 0);
+
+            if (ESP_OK != err) {
+                ESP_LOGE(TAG, "esp_event_send_internal failed with code %d", err);
+            }
+            return;
+
+        case PPPERR_AUTHFAIL:
+            ESP_LOGE(TAG, "Failed authentication challenge");
+            break;
+        case PPPERR_PROTOCOL:
+            ESP_LOGE(TAG, "Failed to meet protocol");
+            break;
+        case PPPERR_PEERDEAD:
+            ESP_LOGE(TAG, "Connection timeout");
+            break;
+        case PPPERR_IDLETIMEOUT:
+            ESP_LOGE(TAG, "Idle Timeout");
+            break;
+        case PPPERR_CONNECTTIME:
+            ESP_LOGE(TAG, "Max connect time reached");
+            break;
+        case PPPERR_LOOPBACK:
+            ESP_LOGE(TAG, "Loopback detected");
+            break;
+        default:
+            ESP_LOGE(TAG, "Unknown error code %d", err_code);
+            break;
+    }
+    if (obj->ppp_error_event_enabled) {
+        err = esp_event_post(NETIF_PPP_STATUS, err_code, netif, sizeof(netif), 0);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "esp_event_post failed with code %d", err);
+        }
+
+    }
+}
+
+#if PPP_NOTIFY_PHASE
+/**
+ * @brief Notify phase callback which is called on each PPP internal state change
+ *
+ * @param pcb PPP control block
+ * @param phase Phase ID
+ * @param ctx Context of callback
+ */
+static void on_ppp_notify_phase(ppp_pcb *pcb, u8_t phase, void *ctx)
+{
+    switch (phase) {
+        case PPP_PHASE_DEAD:
+            ESP_LOGD(TAG, "Phase Dead");
+            break;
+        case PPP_PHASE_INITIALIZE:
+            ESP_LOGD(TAG, "Phase Start");
+            break;
+        case PPP_PHASE_ESTABLISH:
+            ESP_LOGD(TAG, "Phase Establish");
+            break;
+        case PPP_PHASE_AUTHENTICATE:
+            ESP_LOGD(TAG, "Phase Authenticate");
+            break;
+        case PPP_PHASE_NETWORK:
+            ESP_LOGD(TAG, "Phase Network");
+            break;
+        case PPP_PHASE_RUNNING:
+            ESP_LOGD(TAG, "Phase Running");
+            break;
+        case PPP_PHASE_TERMINATE:
+            ESP_LOGD(TAG, "Phase Terminate");
+            break;
+        case PPP_PHASE_DISCONNECT:
+            ESP_LOGD(TAG, "Phase Disconnect");
+            break;
+        default:
+            ESP_LOGW(TAG, "Phase Unknown: %d", phase);
+            break;
+    }
+    esp_netif_t *netif = ctx;
+    struct lwip_ppp_ctx *obj =  netif->lwip_ppp_ctx;
+    if (obj && obj->ppp_phase_event_enabled) {
+        esp_err_t err = esp_event_post(NETIF_PPP_STATUS, NETIF_PP_PHASE_OFFSET + phase, netif, sizeof(netif), 0);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "esp_event_post failed with code %d", err);
+        }
+    }
+}
+#endif // PPP_NOTIFY_PHASE
+
+/**
+ * @brief PPP low level output callback used to transmit data using standard esp-netif interafce
+ *
+ * @param pcb PPP control block
+ * @param data Buffer to write to serial port
+ * @param len Length of the data buffer
+ * @param ctx Context of callback
+ *
+ * @return uint32_t Length of data successfully sent
+ */
+static uint32_t pppos_low_level_output(ppp_pcb *pcb, uint8_t *data, uint32_t len, void *netif)
+{
+    esp_err_t ret = esp_netif_transmit(netif, data, len);
+    if (ret == ESP_OK) {
+        return len;
+    }
+    return 0;
+}
+
+esp_err_t esp_netif_ppp_set_auth(esp_netif_t *netif, esp_netif_auth_type_t authtype, const char *user, const char *passwd)
+{
+    if (netif == NULL || !netif->is_ppp_netif) {
+        return ESP_ERR_ESP_NETIF_INVALID_PARAMS;
+    }
+    struct lwip_ppp_ctx *obj =  netif->lwip_ppp_ctx;
+#if PAP_SUPPORT
+    pppapi_set_auth(obj->ppp, authtype, user, passwd);
+#elif CHAP_SUPPORT
+    pppapi_set_auth(obj->ppp, authtype, user, passwd);
+#else
+#error "Unsupported AUTH Negotiation"
+#endif
+    return ESP_OK;
+}
+
+void esp_netif_ppp_set_default_netif(lwip_ppp_ctx_t* ppp_ctx)
+{
+    pppapi_set_default(ppp_ctx->ppp);
+}
+
+lwip_ppp_ctx_t* esp_netif_new_ppp(esp_netif_t *esp_netif, const esp_netif_netstack_config_t *esp_netif_stack_config)
+{
+    struct netif * netif_impl = esp_netif->lwip_netif;
+    struct lwip_ppp_ctx * ppp_obj = calloc(1, sizeof(struct lwip_ppp_ctx));
+    if (ppp_obj == NULL) {
+        ESP_LOGE(TAG, "%s: cannot allocate lwip_ppp_ctx", __func__);
+        return NULL;
+    }
+
+    ppp_obj->ppp = pppapi_pppos_create(netif_impl, pppos_low_level_output, on_ppp_status_changed, esp_netif);
+    ESP_LOGD(TAG, "%s: PPP connection created: %p", __func__, ppp_obj->ppp);
+    if (!ppp_obj->ppp) {
+        ESP_LOGE(TAG, "%s: lwIP PPP connection cannot be created", __func__);
+    }
+#if PPP_NOTIFY_PHASE
+    ppp_set_notify_phase_callback(ppp_obj->ppp, on_ppp_notify_phase);
+#endif
+    ppp_set_usepeerdns(ppp_obj->ppp, 1);
+
+    return ppp_obj;
+}
+
+esp_err_t esp_netif_start_ppp(lwip_ppp_ctx_t *ppp_ctx)
+{
+    ESP_LOGD(TAG, "%s: Starting PPP connection: %p", __func__, ppp_ctx->ppp);
+    esp_err_t err = pppapi_connect(ppp_ctx->ppp, 0);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "%s: PPP connection cannot be started", __func__);
+        return ESP_FAIL;
+    }
+    return ESP_OK;
+}
+
+void esp_netif_lwip_ppp_input(void *ppp_ctx, void *buffer, size_t len, void *eb)
+{
+    struct lwip_ppp_ctx * obj = ppp_ctx;
+    err_t ret = pppos_input_tcpip(obj->ppp, buffer, len);
+    if (ret != ERR_OK) {
+        ESP_LOGE(TAG, "pppos_input_tcpip failed with %d", ret);
+    }
+}
+
+esp_err_t esp_netif_stop_ppp(lwip_ppp_ctx_t *ppp_ctx)
+{
+    ESP_LOGD(TAG, "%s: Stopped PPP connection: %p", __func__, ppp_ctx->ppp);
+    err_t ret = pppapi_close(ppp_ctx->ppp, 0);
+    if (ret != ERR_OK) {
+        ESP_LOGE(TAG, "pppapi_close failed with %d", ret);
+        return ESP_FAIL;
+    }
+    return ESP_OK;
+}
+
+void esp_netif_destroy_ppp(lwip_ppp_ctx_t *ppp_ctx)
+{
+    pppapi_free(ppp_ctx->ppp);
+    free(ppp_ctx);
+}
+
+esp_err_t esp_netif_ppp_set_params(esp_netif_t *netif, const esp_netif_ppp_config_t *config)
+{
+    struct lwip_ppp_ctx *obj =  netif->lwip_ppp_ctx;
+    obj->ppp_phase_event_enabled = config->ppp_phase_event_enabled;
+    obj->ppp_error_event_enabled = config->ppp_error_event_enabled;
+    return ESP_OK;
+}
+#else  /* PPPOS_SUPPORT */
+
+/**
+ * @brief If PPP not enabled in menuconfig, log the error and return appropriate code indicating failure
+*/
+#define LOG_PPP_DISABLED_AND_DO(action) \
+    {   \
+    ESP_LOGE(TAG, "%s not supported, please enable PPP in lwIP component configuration", __func__); \
+    action; \
+    }
+
+esp_err_t esp_netif_ppp_set_auth(esp_netif_t *netif, esp_netif_auth_type_t authtype, const char *user, const char *passwd)
+    LOG_PPP_DISABLED_AND_DO(return ESP_ERR_NOT_SUPPORTED)
+
+void esp_netif_ppp_set_default_netif(lwip_ppp_ctx_t* ppp_ctx)
+    LOG_PPP_DISABLED_AND_DO()
+
+lwip_ppp_ctx_t* esp_netif_new_ppp(esp_netif_t *esp_netif, const esp_netif_netstack_config_t *esp_netif_stack_config)
+    LOG_PPP_DISABLED_AND_DO(return NULL)
+
+esp_err_t esp_netif_start_ppp(lwip_ppp_ctx_t *ppp_ctx)
+    LOG_PPP_DISABLED_AND_DO(return ESP_ERR_NOT_SUPPORTED)
+
+void esp_netif_lwip_ppp_input(void *ppp_ctx, void *buffer, size_t len, void *eb)
+    LOG_PPP_DISABLED_AND_DO()
+
+esp_err_t esp_netif_stop_ppp(lwip_ppp_ctx_t *ppp_ctx)
+    LOG_PPP_DISABLED_AND_DO(return ESP_ERR_NOT_SUPPORTED)
+
+void esp_netif_destroy_ppp(lwip_ppp_ctx_t *ppp_ctx)
+    LOG_PPP_DISABLED_AND_DO()
+
+esp_err_t esp_netif_ppp_set_params(esp_netif_t *netif, const esp_netif_ppp_config_t *config)
+    LOG_PPP_DISABLED_AND_DO(return ESP_ERR_NOT_SUPPORTED)
+
+#endif /* PPPOS_SUPPORT */

components/esp_netif/lwip/esp_netif_lwip_ppp.h
@@ -1,0 +1,76 @@
+// Copyright 2019 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef _ESP_NETIF_LWIP_PPP_H_
+#define _ESP_NETIF_LWIP_PPP_H_
+
+/**
+ * @brief  Creates new PPP related structure
+ *
+ * @param[in]     esp_netif pointer esp-netif instance
+ * @param[in]     stack_config TCP/IP stack configuration structure
+ *
+ * @return
+ *         - pointer to ppp-netif object on success
+ *         - NULL otherwise
+ */
+lwip_ppp_ctx_t* esp_netif_new_ppp(esp_netif_t *esp_netif, const esp_netif_netstack_config_t *esp_netif_stack_config);
+
+/**
+ * @brief  Creates new PPP related structure
+ *
+ * @param[in]    ppp pointer to internal ppp context instance
+ *
+ * @return
+ *         - ESP_OK on success
+ */
+esp_err_t esp_netif_start_ppp(lwip_ppp_ctx_t *ppp);
+
+/**
+ * @brief  Data path API to input incoming packets to PPP
+ *
+ * @param[in]    ppp pointer to internal ppp context instance
+ * @param[in]    buffer pointer to the incoming data
+ * @param[in]    len length of the data
+ * @param[in]    eb external buffer ptr not used here (to be inline with input function prototypes)
+ *
+ * @return
+ *         - ESP_OK on success
+ */
+void esp_netif_lwip_ppp_input(void *ppp, void *buffer, size_t len, void *eb);
+
+/**
+ * @brief   Destroys the ppp netif object
+ *
+ * @param[in]    ppp pointer to internal ppp context instance
+ */
+void esp_netif_destroy_ppp(lwip_ppp_ctx_t *ppp);
+
+/**
+ * @brief  Stops the PPP interface
+ *
+ * @param[in]    ppp pointer to internal ppp context instance
+ *
+ * @return
+ *         - ESP_OK on success
+ */
+esp_err_t esp_netif_stop_ppp(lwip_ppp_ctx_t *ppp);
+
+/**
+ * @brief  Sets default netif for routing priority config
+ *
+ */
+void esp_netif_ppp_set_default_netif(lwip_ppp_ctx_t* ppp_ctx);
+
+#endif // _ESP_NETIF_LWIP_PPP_H_

components/lwip/port/esp32/include/netif/ethernetif.h
@@ -24,7 +24,7 @@ extern "C" {
 
 err_t ethernetif_init(struct netif *netif);
 
-void ethernetif_input(void *h, void *buffer, size_t len, void *eb);
+void ethernetif_input(void *netif, void *buffer, size_t len, void *eb);
 
 void netif_reg_addr_change_cb(void* cb);
 

examples/protocols/pppos_client/components/modem/CMakeLists.txt
@@ -1,5 +1,6 @@
 set(srcs "src/esp_modem.c"
         "src/esp_modem_dce_service"
+        "src/esp_modem_netif.c"
         "src/sim800.c"
         "src/bg96.c")
 

examples/protocols/pppos_client/components/modem/include/esp_modem.h
@@ -21,7 +21,6 @@ extern "C" {
 #include "esp_modem_dte.h"
 #include "esp_event.h"
 #include "driver/uart.h"
-#include "lwip/ip_addr.h"
 
 /**
  * @brief Declare Event Base for ESP Modem
@@ -35,8 +34,6 @@ ESP_EVENT_DECLARE_BASE(ESP_MODEM_EVENT);
  */
 typedef enum {
     MODEM_EVENT_PPP_START,      /*!< ESP Modem Start PPP Session */
-    MODEM_EVENT_PPP_CONNECT,    /*!< ESP Modem Connect to PPP Server */
-    MODEM_EVENT_PPP_DISCONNECT, /*!< ESP Modem Disconnect from PPP Server */
     MODEM_EVENT_PPP_STOP,       /*!< ESP Modem Stop PPP Session*/
     MODEM_EVENT_UNKNOWN         /*!< ESP Modem Unknown Response */
 } esp_modem_event_t;
@@ -53,6 +50,12 @@ typedef struct {
     modem_flow_ctrl_t flow_control; /*!< Flow control type */
     uint32_t baud_rate;             /*!< Communication baud rate */
 } esp_modem_dte_config_t;
+
+/**
+ * @brief Type used for reception callback
+ *
+ */
+typedef esp_err_t (*esp_modem_on_receive)(void *buffer, size_t len, void *context);
 
 /**
  * @brief ESP Modem DTE Default Configuration
@@ -88,7 +91,7 @@ modem_dte_t *esp_modem_dte_init(const esp_modem_dte_config_t *config);
  *      - ESP_ERR_NO_MEM on allocating memory for the handler failed
  *      - ESP_ERR_INVALID_ARG on invalid combination of event base and event id
  */
-esp_err_t esp_modem_add_event_handler(modem_dte_t *dte, esp_event_handler_t handler, void *handler_args);
+esp_err_t esp_modem_add_event_handler(modem_dte_t *dte, esp_event_handler_t handler, int32_t event_id, void *handler_args);
 
 /**
  * @brief Unregister event handler for ESP Modem event loop
@@ -102,18 +105,6 @@ esp_err_t esp_modem_add_event_handler(modem_dte_t *dte, esp_event_handler_t hand
 esp_err_t esp_modem_remove_event_handler(modem_dte_t *dte, esp_event_handler_t handler);
 
 /**
- * @brief PPPoS Client IP Information
- *
- */
-typedef struct {
-    ip4_addr_t ip;      /*!< IP Address */
-    ip4_addr_t netmask; /*!< Net Mask */
-    ip4_addr_t gw;      /*!< Gateway */
-    ip4_addr_t ns1;     /*!< Name Server1 */
-    ip4_addr_t ns2;     /*!< Name Server2 */
-} ppp_client_ip_info_t;
-
-/**
  * @brief Setup PPP Session
  *
  * @param dte Modem DTE object
@@ -121,7 +112,7 @@ typedef struct {
  *      - ESP_OK on success
  *      - ESP_FAIL on error
  */
-esp_err_t esp_modem_setup_ppp(modem_dte_t *dte);
+esp_err_t esp_modem_start_ppp(modem_dte_t *dte);
 
 /**
  * @brief Exit PPP Session
@@ -131,7 +122,18 @@ esp_err_t esp_modem_setup_ppp(modem_dte_t *dte);
  *      - ESP_OK on success
  *      - ESP_FAIL on error
  */
-esp_err_t esp_modem_exit_ppp(modem_dte_t *dte);
+esp_err_t esp_modem_stop_ppp(modem_dte_t *dte);
+
+/**
+ * @brief Setup on reception callback
+ *
+ * @param dte ESP Modem DTE object
+ * @param receive_cb Function pointer to the reception callback
+ * @param receive_cb_ctx Contextual pointer to be passed to the reception callback
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t esp_modem_set_rx_cb(modem_dte_t *dte, esp_modem_on_receive receive_cb, void *receive_cb_ctx);
 
 #ifdef __cplusplus
 }

commit 5853fe65f49287d4d897a180b68c75241ed47ce2

pppos_client: Fix power_down dce immediately after got IP
This was accidently commited bug when convert to use esp-netif, fix it.

Fixes: 25913af2cc12 ("pppos_client: udated example code to use esp-netif in PPP configuration")
Signed-off-by: Axel Lin <axel.lin@gmail.com>

examples/protocols/pppos_client/main/pppos_client_main.c
@@ -263,7 +263,6 @@ void app_main(void)
     esp_netif_attach(esp_netif, modem_netif_adapter);
     /* Wait for IP address */
     xEventGroupWaitBits(event_group, CONNECT_BIT, pdTRUE, pdTRUE, portMAX_DELAY);
-    ESP_ERROR_CHECK(dce->power_down(dce));
     /* Config MQTT */
     esp_mqtt_client_config_t mqtt_config = {
         .uri = BROKER_URL,