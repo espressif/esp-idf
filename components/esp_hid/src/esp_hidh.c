606d7e48d22471e10683224b617d8c4020f29241
Murray Fordyce <undeadherbs@gmail.com>
2020-01-26 10:28:45 -0600

    Fixed left right key confusion in comments.
    "Left" and "Right" were swapped in some comments.

--- components/esp_hid/src/esp_hidh.c
+++ components/esp_hid/src/esp_hidh.c
@@ -1,16 +1,8 @@
-// Copyright 2017-2019 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2017-2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include "sys/queue.h"
 #include "esp_hidh_private.h"
@@ -361,7 +353,7 @@ esp_hidh_dev_report_t *esp_hidh_dev_get_input_report_by_id_and_proto(esp_hidh_de
 {
     esp_hidh_dev_report_t *r = dev->reports;
     while (r) {
-        if (r->report_id == report_id && (r->report_type & 1) && r->protocol_mode == protocol_mode) {
+        if (r->report_id == report_id && (r->report_type & ESP_HID_REPORT_TYPE_INPUT) && r->protocol_mode == protocol_mode) {
             return r;
         }
         r = r->next;
