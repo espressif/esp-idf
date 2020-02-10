7ccf6ebadae432e24d933a5d4090b6f2373cf2d0
Ivan Grokhotkov <ivan@espressif.com>
2020-02-10 19:14:41 +0100

    idf_tools.py: fix install-python-env failure with virtualenv 20.0.0
    --no-site-packages is the default behavior since virtualenv 1.7
    (released in Nov 2011). This option was removed in virtualenv 20.0.

--- tools/idf_tools.py
+++ tools/idf_tools.py
@@ -1180,7 +1180,7 @@ def action_install_python_env(args):
             subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', 'virtualenv'],
                                   stdout=sys.stdout, stderr=sys.stderr)
 
-        subprocess.check_call([sys.executable, '-m', 'virtualenv', '--no-site-packages', idf_python_env_path],
+        subprocess.check_call([sys.executable, '-m', 'virtualenv', idf_python_env_path],
                               stdout=sys.stdout, stderr=sys.stderr)
     run_args = [virtualenv_python, '-m', 'pip', 'install', '--no-warn-script-location']
     requirements_txt = os.path.join(global_idf_path, 'requirements.txt')
