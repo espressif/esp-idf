9c8289b0d911a2c7c0c2d180fae5808c3592a769
Felipe Neves <felipe.neves@espressif.com>
2020-02-10 12:03:24 -0300

    shared_stack: using watchpoint 1 to monitor the shared_stack instead of watchpoint 0

--- components/freertos/xtensa/include/freertos/portmacro.h
+++ components/freertos/xtensa/include/freertos/portmacro.h
@@ -386,6 +386,7 @@ extern void esp_vApplicationTickHook( void );
 
 void _xt_coproc_release(volatile void * coproc_sa_base);
 void vApplicationSleep( TickType_t xExpectedIdleTime );
+void vPortSetStackWatchpoint( void* pxStackStart );
 
 #define portSUPPRESS_TICKS_AND_SLEEP( idleTime ) vApplicationSleep( idleTime )
 
