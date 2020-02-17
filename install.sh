14d3dcaf8cc29924e80625353d6833d7eecac798
Michal Szutenberg <michal@szutenberg.pl>
2020-02-17 21:05:09 +0100

    install.sh: use python3 (IDFGH-2718)

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ set -u
 export IDF_PATH=$(cd $(dirname $0); pwd)
 
 echo "Installing ESP-IDF tools"
-${IDF_PATH}/tools/idf_tools.py install
+python3 ${IDF_PATH}/tools/idf_tools.py install
 
 echo "Installing Python environment and packages"
-${IDF_PATH}/tools/idf_tools.py install-python-env
+python3 ${IDF_PATH}/tools/idf_tools.py install-python-env
 
 basedir="$(dirname $0)"
 echo "All done! You can now run:"
