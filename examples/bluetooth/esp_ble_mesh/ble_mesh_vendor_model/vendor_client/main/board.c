da113cdfa80ef178cc51f0d2b4d508067970959b
lly <lly@espressif.com>
2020-03-02 18:17:44 +0800

    ble_mesh: Add ESP BLE Mesh vendor models example

---
+++ examples/bluetooth/esp_ble_mesh/ble_mesh_vendor_model/vendor_client/main/board.c
@@ -0,0 +1,38 @@
+/* board.c - Board-specific hooks */
+
+/*
+ * Copyright (c) 2017 Intel Corporation
+ * Additional Copyright (c) 2018 Espressif Systems (Shanghai) PTE LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "iot_button.h"
+
+#define TAG "BOARD"
+
+#define BUTTON_IO_NUM           0
+#define BUTTON_ACTIVE_LEVEL     0
+
+extern void example_ble_mesh_send_vendor_message(bool resend);
+
+static void button_tap_cb(void* arg)
+{
+    example_ble_mesh_send_vendor_message(false);
+}
+
+static void board_button_init(void)
+{
+    button_handle_t btn_handle = iot_button_create(BUTTON_IO_NUM, BUTTON_ACTIVE_LEVEL);
+    if (btn_handle) {
+        iot_button_set_evt_cb(btn_handle, BUTTON_CB_RELEASE, button_tap_cb, "RELEASE");
+    }
+}
+
+void board_init(void)
+{
+    board_button_init();
+}
---
+++ examples/bluetooth/esp_ble_mesh/ble_mesh_vendor_model/vendor_server/README.md
@@ -0,0 +1,4 @@
+ESP BLE Mesh Vendor Server Example
+==================================
+
+For description of this example please refer to [ESP BLE Mesh Vendor Client Example](../vendor_client/README.md)
