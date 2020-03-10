414b8437557836f47f281f990b9771710701d6f9
xiehang <xiehang@espressif.com>
2020-03-10 20:01:39 +0800

    esp_wifi: Update WiFi lib
    1,Fix sniffer bug caused by mode switch
    2,Reduce WiFi bin size
    3,Add TX packets size check
    4,Fix scan get rssi error
    5,Add wifi stop check at WiFi deinit entry
    6,Return fail when setting AP's channel is out of range
    7,Fix the bug for setting channel when WiFi in NULL mode

--- components/esp32/include/esp_wifi_os_adapter.h
+++ components/esp32/include/esp_wifi_os_adapter.h
@@ -105,6 +105,7 @@ typedef struct {
     int32_t (* _get_time)(void *t);
     unsigned long (* _random)(void);
     void (* _log_write)(uint32_t level, const char* tag, const char* format, ...);
+    void (* _log_writev)(uint32_t level, const char* tag, const char* format, va_list args);
     uint32_t (* _log_timestamp)(void);
     void * (* _malloc_internal)(size_t size);
     void * (* _realloc_internal)(void *ptr, size_t size);
