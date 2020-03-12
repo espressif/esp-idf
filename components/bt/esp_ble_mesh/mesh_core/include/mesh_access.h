eee8bd4612f2733924cf5f086ecc304d9c3e3e34
lly <lly@espressif.com>
2020-03-12 17:48:09 +0800

    ble_mesh: Move dev_role to the end of model_pub

--- components/bt/esp_ble_mesh/mesh_core/include/mesh_access.h
+++ components/bt/esp_ble_mesh/mesh_core/include/mesh_access.h
@@ -369,11 +369,11 @@ struct bt_mesh_model_pub {
      */
     int (*update)(struct bt_mesh_model *mod);
 
-    /* Change by Espressif, role of the device going to publish messages */
-    u8_t dev_role;
-
     /** Publish Period Timer. Only for stack-internal use. */
     struct k_delayed_work timer;
+
+    /* Change by Espressif, role of the device going to publish messages */
+    u8_t dev_role;
 };
 
 /** @def BLE_MESH_MODEL_PUB_DEFINE
