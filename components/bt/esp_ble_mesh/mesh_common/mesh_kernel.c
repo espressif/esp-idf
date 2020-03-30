2f40c4449fdd5c11000a0a48fbf7f1f2f43b38ad
lly <lly@espressif.com>
2020-03-30 19:33:25 +0800

    ble_mesh: Split mesh os related into a separate file

---
+++ components/bt/esp_ble_mesh/mesh_common/mesh_kernel.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2016 Wind River Systems, Inc.
+ * Additional Copyright (c) 2020 Espressif Systems (Shanghai) PTE LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mesh_kernel.h"
+
+void k_sleep(s32_t duration)
+{
+    vTaskDelay(duration / portTICK_PERIOD_MS);
+}
