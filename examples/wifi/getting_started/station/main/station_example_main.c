f0744184f52cff58f4acf9c6798623cfa471cc55
ronghulin <ronghulin@espressif.com>
2020-04-07 21:53:50 +0800

    fix: add event unregister comment

--- examples/wifi/getting_started/station/main/station_example_main.c
+++ examples/wifi/getting_started/station/main/station_example_main.c
@@ -120,6 +120,7 @@ void wifi_init_sta(void)
         ESP_LOGE(TAG, "UNEXPECTED EVENT");
     }
 
+    /* The event will not be processed after unregister */
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
     vEventGroupDelete(s_wifi_event_group);
