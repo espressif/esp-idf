897e69cc9d94fa1070db0eba613ac502f0e1f990
Ivan Grokhotkov <ivan@espressif.com>
2020-04-17 18:54:12 +0200

    soc, esp32s2: add usb_wrap peripheral

--- components/soc/soc/esp32s2/include/soc/usb_pins.h
+++ components/soc/soc/esp32s2/include/soc/usb_pins.h
@@ -14,9 +14,14 @@
 
 #pragma once
 
+/* GPIOs used to connect an external USB PHY */
 #define USBPHY_VP_NUM 33
 #define USBPHY_VM_NUM 34
 #define USBPHY_RCV_NUM 35
 #define USBPHY_OEN_NUM 36
 #define USBPHY_VPO_NUM 37
 #define USBPHY_VMO_NUM 38
+
+/* GPIOs corresponding to the pads of the internal USB PHY */
+#define USBPHY_DP_NUM 20
+#define USBPHY_DM_NUM 19
