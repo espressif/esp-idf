19c73192ba757baed908c5a98dee7a1f68c5619e
Ivan Grokhotkov <ivan@espressif.com>
2020-04-30 16:52:17 +0200

    ble_mesh_coex_test: fix deprecated Kconfig option name

--- examples/bluetooth/esp_ble_mesh/ble_mesh_coex_test/main/main.c
+++ examples/bluetooth/esp_ble_mesh/ble_mesh_coex_test/main/main.c
@@ -49,7 +49,7 @@ static void initialize_console(void)
      * correct while APB frequency is changing in light sleep mode.
      */
     const uart_config_t uart_config = {
-        .baud_rate = CONFIG_CONSOLE_UART_BAUDRATE,
+        .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
