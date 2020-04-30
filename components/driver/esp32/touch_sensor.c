34d41645d3c5b1bec5f942453a94c23f9fc9c873
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2020-04-30 16:00:27 +0530

    touch_sensor.c: Fix datatype of argument for timer callback function

--- components/driver/esp32/touch_sensor.c
+++ components/driver/esp32/touch_sensor.c
@@ -432,7 +432,7 @@ esp_err_t touch_pad_filter_start(uint32_t filter_period_ms)
     }
     if (s_touch_pad_filter->timer == NULL) {
         s_touch_pad_filter->timer = xTimerCreate("filter_tmr", filter_period_ms / portTICK_PERIOD_MS, pdFALSE,
-                                    NULL, touch_pad_filter_cb);
+                                    NULL, (TimerCallbackFunction_t) touch_pad_filter_cb);
         if (s_touch_pad_filter->timer == NULL) {
             free(s_touch_pad_filter);
             s_touch_pad_filter = NULL;
