68fd967934ca6ddb2f4e5a93d3bf8da856934373
lly <lly@espressif.com>
2020-05-13 16:12:18 +0800

    ble_mesh: Move the example init to the common_components

--- examples/bluetooth/esp_ble_mesh/common_components/example_init/ble_mesh_demo_init.h
+++ examples/bluetooth/esp_ble_mesh/common_components/example_init/ble_mesh_demo_init.h
@@ -9,10 +9,12 @@
 #ifndef _BLE_MESH_DEMO_INIT_H_
 #define _BLE_MESH_DEMO_INIT_H_
 
-#define TAG "ble_mesh_provisioner"
+#include "esp_err.h"
+
+#define TAG "EXAMPLE"
 
 void ble_mesh_get_dev_uuid(uint8_t *dev_uuid);
 
 esp_err_t bluetooth_init(void);
 
-#endif
+#endif /* _BLE_MESH_DEMO_INIT_H_ */
