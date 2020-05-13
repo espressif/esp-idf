bc42073a8bd1bb14c4d3eb0c2ce4354f7e42066a
Andrei Gramakov <andrei.gramakov@espressif.com>
2020-05-13 14:00:38 +0200

    fixes for powershell on unix

--- install.ps1
+++ install.ps1
@@ -1,18 +1,13 @@
-if ($env:MSYSTEM -ne $null) {
-    Write-Output "This .ps1 file is for Windows Powershell only. When using MSYS, run:`n. ./export.sh."
-    exit 1
-}
-
-
+#!/usr/bin/env pwsh
 $IDF_PATH = $PSScriptRoot
 
 
 Write-Output "Installing ESP-IDF tools"
-Start-Process -Wait -NoNewWindow -FilePath "python.exe" -Args "$IDF_PATH/tools/idf_tools.py install"
+Start-Process -Wait -NoNewWindow -FilePath "python" -Args "$IDF_PATH/tools/idf_tools.py install"
 if ($LASTEXITCODE -ne 0)  { exit $LASTEXITCODE } # if error
 
 Write-Output "Setting up Python environment"
-Start-Process -Wait  -NoNewWindow -FilePath "python.exe" -Args "$IDF_PATH/tools/idf_tools.py install-python-env"
+Start-Process -Wait  -NoNewWindow -FilePath "python" -Args "$IDF_PATH/tools/idf_tools.py install-python-env"
 if ($LASTEXITCODE -ne 0)  { exit $LASTEXITCODE} # if error
 
 
