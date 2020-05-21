ffc6072f80fb69e9100530ce48fd696ebb821571
zhiweijian <zhiweijian@espressif.com>
2020-05-21 11:33:51 +0800

    component/bt: add clear white list API

--- components/bt/host/bluedroid/bta/include/bta/bta_api.h
+++ components/bt/host/bluedroid/bta/include/bta/bta_api.h
@@ -1539,6 +1539,8 @@ void BTA_DmBleSetChannels(const uint8_t *channels, tBTA_CMPL_CB  *set_channels_c
 
 extern void BTA_DmUpdateWhiteList(BOOLEAN add_remove,  BD_ADDR remote_addr, tBLE_ADDR_TYPE addr_type, tBTA_ADD_WHITELIST_CBACK *add_wl_cb);
 
+extern void BTA_DmClearWhiteList(void);
+
 extern void BTA_DmBleReadAdvTxPower(tBTA_CMPL_CB *cmpl_cb);
 #endif  ///BLE_INCLUDED == TRUE
 
