8cbc93101e46fb8cc872b65d48b49137b55ab8e4
lly <lly@espressif.com>
2020-05-26 18:17:41 +0800

    ble_mesh: stack: Add role check before enabling device

--- components/bt/esp_ble_mesh/mesh_core/mesh.h
+++ components/bt/esp_ble_mesh/mesh_core/mesh.h
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2017 Intel Corporation
+ * Additional Copyright (c) 2020 Espressif Systems (Shanghai) PTE LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -15,13 +16,13 @@
 extern "C" {
 #endif
 
-#define BLE_MESH_KEY_PRIMARY 0x0000
-#define BLE_MESH_KEY_ANY     0xffff
+#define BLE_MESH_KEY_PRIMARY            0x0000
+#define BLE_MESH_KEY_ANY                0xffff
 
-#define BLE_MESH_ADDR_IS_UNICAST(addr) ((addr) && (addr) < 0x8000)
-#define BLE_MESH_ADDR_IS_GROUP(addr) ((addr) >= 0xc000 && (addr) <= 0xff00)
-#define BLE_MESH_ADDR_IS_VIRTUAL(addr) ((addr) >= 0x8000 && (addr) < 0xc000)
-#define BLE_MESH_ADDR_IS_RFU(addr) ((addr) >= 0xff00 && (addr) <= 0xfffb)
+#define BLE_MESH_ADDR_IS_UNICAST(addr)  ((addr) && (addr) < 0x8000)
+#define BLE_MESH_ADDR_IS_GROUP(addr)    ((addr) >= 0xc000 && (addr) <= 0xff00)
+#define BLE_MESH_ADDR_IS_VIRTUAL(addr)  ((addr) >= 0x8000 && (addr) < 0xc000)
+#define BLE_MESH_ADDR_IS_RFU(addr)      ((addr) >= 0xff00 && (addr) <= 0xfffb)
 
 struct bt_mesh_net;
 
