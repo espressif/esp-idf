5e867121716cf336c743e8adcd49deb268a39ceb
Robin Krens <robin@robinkrens.nl>
2020-06-01 12:37:02 +0200

    spacing styling correction

--- components/esp_hid/src/ble_hidd.c
+++ components/esp_hid/src/ble_hidd.c
@@ -337,7 +337,7 @@ static esp_err_t create_hid_db(esp_ble_hidd_dev_t *dev, int device_index)
                 add_db_record(_last_db, index++, (uint8_t *)&s_character_declaration_uuid, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&s_char_prop_read_write_write_nr);
                 report->index = index;
                 add_db_record(_last_db, index++, (uint8_t *)&s_hid_report_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, report->value_len, 0, NULL);
-	    } else {
+            } else {
                 //Feature Report
                 add_db_record(_last_db, index++, (uint8_t *)&s_character_declaration_uuid, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&s_char_prop_read_write);
                 report->index = index;
