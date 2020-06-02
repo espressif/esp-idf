19f69333f7f270533a1db6a44e890f37dfc3f8b9
lly <lly@espressif.com>
2020-06-02 18:22:18 +0800

    ble_mesh: Remove relay option dependency
    When the relay functionality of a node is not enabled, this
    issue will cause the node failing to send messages to fixed
    group.

--- components/bt/esp_ble_mesh/mesh_core/net.c
+++ components/bt/esp_ble_mesh/mesh_core/net.c
@@ -1473,9 +1473,8 @@ void bt_mesh_net_recv(struct net_buf_simple *data, s8_t rssi,
     /* Relay if this was a group/virtual address, or if the destination
      * was neither a local element nor an LPN we're Friends for.
      */
-    if (IS_ENABLED(CONFIG_BLE_MESH_RELAY) &&
-            (!BLE_MESH_ADDR_IS_UNICAST(rx.ctx.recv_dst) ||
-            (!rx.local_match && !rx.friend_match))) {
+    if (!BLE_MESH_ADDR_IS_UNICAST(rx.ctx.recv_dst) ||
+            (!rx.local_match && !rx.friend_match)) {
         net_buf_simple_restore(&buf, &state);
         bt_mesh_net_relay(&buf, &rx);
     }
