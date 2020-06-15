62237cf28cdd5e91a4b1e244c59930512b8fc121
Meowthink <meowthink@googlemail.com>
2020-06-15 09:59:20 +0800

    Add Microchip KSZ8041 PHY support.
    Merges https://github.com/espressif/esp-idf/pull/5452

--- components/esp_eth/include/esp_eth_phy.h
+++ components/esp_eth/include/esp_eth_phy.h
@@ -251,6 +251,17 @@ esp_eth_phy_t *esp_eth_phy_new_dp83848(const eth_phy_config_t *config);
 *      - NULL: create PHY instance failed because some error occurred
 */
 esp_eth_phy_t *esp_eth_phy_new_dm9051(const eth_phy_config_t *config);
+
+/**
+* @brief Create a PHY instance of KSZ8041
+*
+* @param[in] config: configuration of PHY
+*
+* @return
+*      - instance: create PHY instance successfully
+*      - NULL: create PHY instance failed because some error occurred
+*/
+esp_eth_phy_t *esp_eth_phy_new_ksz8041(const eth_phy_config_t *config);
 #endif
 #ifdef __cplusplus
 }
