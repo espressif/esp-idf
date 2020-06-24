6f6b4c3983298f55b7b86f8224fe6705cc7c8057
Angus Gratton <angus@espressif.com>
2020-06-24 15:52:50 +1000

    cmake partition_table: Check binaries fit in partition spaces at build time
    - Bootloader is checked not to overlap partition table
    - Apps are checked not to overlap any app partition regions
    
    Supported for CMake build system only.
    
    Closes https://github.com/espressif/esp-idf/pull/612
    Closes https://github.com/espressif/esp-idf/issues/5043
    Probable fix for https://github.com/espressif/esp-idf/issues/5456

--- components/partition_table/project_include.cmake
+++ components/partition_table/project_include.cmake
@@ -1,6 +1,11 @@
-if(NOT BOOTLOADER_BUILD)
-    set(PARTITION_TABLE_OFFSET ${CONFIG_PARTITION_TABLE_OFFSET})
+set(PARTITION_TABLE_OFFSET ${CONFIG_PARTITION_TABLE_OFFSET})
 
+set(PARTITION_TABLE_CHECK_SIZES_TOOL_PATH "${CMAKE_CURRENT_LIST_DIR}/check_sizes.py")
+
+idf_build_get_property(build_dir BUILD_DIR)
+idf_build_set_property(PARTITION_TABLE_BIN_PATH "${build_dir}/partition_table/partition-table.bin")
+
+if(NOT BOOTLOADER_BUILD)
     # Set PARTITION_CSV_PATH to the configured partition CSV file
     # absolute path
     if(CONFIG_PARTITION_TABLE_CUSTOM)
@@ -48,4 +53,58 @@ function(partition_table_get_partition_info result get_part_info_args part_info)
         message(WARNING "parttool.py execution failed (${result}), problem with partition CSV file (see above)")
     endif()
     set(${result} ${info} PARENT_SCOPE)
+endfunction()
+
+# Add a custom target (see add_custom_target) that checks a given binary built by the build system
+# doesn't overflow any partitions with the given partition type and (optional) sub-type.
+#
+# Adding the target doesn't mean it gets called during the build, use add_dependencies to make another
+# target depend on this one.
+#
+# Arguments:
+# - target name - (first argument) name of the target to create
+# - DEPENDS - dependencies the target should have (i.e. whatever target generates the binary).
+# - BINARY_PATH - path to the target binary file to check
+# - PARTITION_TYPE - partition type to check against
+# - PARTITION_SUBTYPE - (optional) partition subtype to check against
+
+function(partition_table_add_check_size_target target_name)
+    # result binary_path partition_type partition_subtype
+    set(args BINARY_PATH PARTITION_TYPE PARTITION_SUBTYPE)
+    set(multi_args DEPENDS)
+    cmake_parse_arguments(CMD "" "${args}" "${multi_args}" ${ARGN})
+
+    idf_build_get_property(python PYTHON)
+    idf_build_get_property(table_bin PARTITION_TABLE_BIN_PATH)
+    if(CMD_PARTITION_SUBTYPE)
+        set(subtype_arg --subtype ${CMD_PARTITION_SUBTYPE})
+    else()
+        set(subtype_arg)
+    endif()
+    set(command ${python} ${PARTITION_TABLE_CHECK_SIZES_TOOL_PATH}
+        --offset ${PARTITION_TABLE_OFFSET}
+        partition --type ${CMD_PARTITION_TYPE} ${subtype_arg}
+        ${table_bin} ${CMD_BINARY_PATH})
+
+    add_custom_target(${target_name} COMMAND ${command} DEPENDS ${CMD_DEPENDS} partition_table_bin)
+endfunction()
+
+# Add a custom target (see add_custom_target) that checks a bootloader binary
+# built by the build system will not overlap the partition table.
+#
+# Adding the target doesn't mean it gets called during the build, use add_dependencies to make another
+# target depend on this one.
+#
+# Arguments:
+# - target name - (first argument) name of the target to create
+# - DEPENDS - dependencies the new target should have (i.e. whatever target generates the bootloader binary)
+# - BOOTLOADER_BINARY_PATH - path to bootloader binary file
+function(partition_table_add_check_bootloader_size_target target_name)
+    cmake_parse_arguments(CMD "" "BOOTLOADER_BINARY_PATH" "DEPENDS" ${ARGN})
+    idf_build_get_property(python PYTHON)
+
+    set(command ${python} ${PARTITION_TABLE_CHECK_SIZES_TOOL_PATH}
+        --offset ${PARTITION_TABLE_OFFSET}
+        bootloader ${BOOTLOADER_OFFSET} ${CMD_BOOTLOADER_BINARY_PATH})
+    add_custom_target(${target_name} COMMAND ${command} DEPENDS ${CMD_DEPENDS})
 endfunction()
