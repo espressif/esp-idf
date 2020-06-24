99d572700d5d8f9405bbdd9ab75f3dde67eb2fae
Chinmay Chhajed <chinmay.chhajed@espressif.com>
2020-06-24 15:23:56 +0530

    Bluedroid: Authentication fixes in Legacy and Secure Connection.
    Prevent a remote device from doing a Bluetooth Impersonation Attack
    (BIAS) by:
    
    - Preventing remote device to downgrade secure connection
    feature mask. Secure connection feature mask should remain same or
    increase to enabled in link key generation and authentication.
    
    - Doing a mutual authentication during Legacy Authentication.

--- components/bt/bluedroid/bta/dm/include/bta_dm_int.h
+++ components/bt/bluedroid/bta/dm/include/bta_dm_int.h
@@ -384,6 +384,7 @@ typedef struct {
     UINT8           new_role;
     BD_ADDR         bd_addr;
     UINT8           hci_status;
+    BOOLEAN         sc_downgrade;
 #if BLE_INCLUDED == TRUE
     UINT16          handle;
     tBT_TRANSPORT   transport;
@@ -425,6 +426,7 @@ typedef struct {
     BD_NAME             bd_name;
     UINT8               features[BTA_FEATURE_BYTES_PER_PAGE * (BTA_EXT_FEATURES_PAGE_MAX + 1)];
     UINT8               pin_length;
+    UINT8               sc_support;
 } tBTA_DM_API_ADD_DEVICE;
 
 /* data type for BTA_DM_API_REMOVE_ACL_EVT */
