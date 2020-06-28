f11e347e2e0636d412fde012e6256ff974ee9b52
lly <lly@espressif.com>
2020-06-28 15:28:22 +0800

    ble_mesh: stack: Update client model msg send

--- components/bt/esp_ble_mesh/mesh_models/client/generic_client.c
+++ components/bt/esp_ble_mesh/mesh_models/client/generic_client.c
@@ -725,9 +725,7 @@ static int gen_get_state(bt_mesh_client_common_param_t *common, void *value)
         }
     }
 
-    err = bt_mesh_client_send_msg(common->model, common->opcode, &common->ctx, &msg,
-                                  timeout_handler, common->msg_timeout, true,
-                                  common->cb, common->cb_data);
+    err = bt_mesh_client_send_msg(common, &msg, true, timeout_handler);
     if (err) {
         BT_ERR("Failed to send Generic Get message (err %d)", err);
     }
@@ -904,9 +902,7 @@ static int gen_set_state(bt_mesh_client_common_param_t *common,
         goto end;
     }
 
-    err = bt_mesh_client_send_msg(common->model, common->opcode, &common->ctx, msg,
-                                  timeout_handler, common->msg_timeout, need_ack,
-                                  common->cb, common->cb_data);
+    err = bt_mesh_client_send_msg(common, msg, need_ack, timeout_handler);
     if (err) {
         BT_ERR("Failed to send Generic Set message (err %d)", err);
     }
