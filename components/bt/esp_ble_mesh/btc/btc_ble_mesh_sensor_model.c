7f6735fd606e236a317f803ed6eb8f6fbc6347a0
lly <lly@espressif.com>
2020-06-29 20:06:32 +0800

    ble_mesh: stack: Copy recv_rssi for other received messages
    Follow the commit: 2b80cb688359c2f8136307edbc75f3d1952848f7,
    copy the recv_rssi for other mesh messages.

--- components/bt/esp_ble_mesh/btc/btc_ble_mesh_sensor_model.c
+++ components/bt/esp_ble_mesh/btc/btc_ble_mesh_sensor_model.c
@@ -501,6 +501,7 @@ void bt_mesh_sensor_client_cb_evt_to_btc(u32_t opcode, u8_t evt_type,
     params.ctx.recv_ttl = ctx->recv_ttl;
     params.ctx.recv_op = ctx->recv_op;
     params.ctx.recv_dst = ctx->recv_dst;
+    params.ctx.recv_rssi = ctx->recv_rssi;
 
     cb_params.error_code = 0;
     cb_params.params = &params;
@@ -874,6 +875,7 @@ void bt_mesh_sensor_server_cb_evt_to_btc(u8_t evt_type,
     cb_params.ctx.recv_ttl = ctx->recv_ttl;
     cb_params.ctx.recv_op = ctx->recv_op;
     cb_params.ctx.recv_dst = ctx->recv_dst;
+    cb_params.ctx.recv_rssi = ctx->recv_rssi;
 
     if (val && len) {
         length = (len <= sizeof(cb_params.value)) ? len : sizeof(cb_params.value);
