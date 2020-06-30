6ca4ea1a1f1d35720b453d62d573a46917e3170f
lly <lly@espressif.com>
2020-06-30 14:45:51 +0800

    ble_mesh: stack: Update some fast prov functions

--- components/bt/esp_ble_mesh/mesh_core/fast_prov.h
+++ components/bt/esp_ble_mesh/mesh_core/fast_prov.h
@@ -29,9 +29,9 @@ struct bt_mesh_app_key *bt_mesh_fast_prov_app_key_find(u16_t app_idx);
 
 u8_t bt_mesh_set_fast_prov_net_idx(u16_t net_idx);
 
-u8_t bt_mesh_add_fast_prov_net_key(const u8_t net_key[16]);
+u8_t bt_mesh_fast_prov_net_key_add(const u8_t net_key[16]);
 
-const u8_t *bt_mesh_get_fast_prov_net_key(u16_t net_idx);
+const u8_t *bt_mesh_fast_prov_net_key_get(u16_t net_idx);
 
 const u8_t *bt_mesh_get_fast_prov_app_key(u16_t net_idx, u16_t app_idx);
 
