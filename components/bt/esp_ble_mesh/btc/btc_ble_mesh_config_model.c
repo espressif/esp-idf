c399b9520b76941f1d834ae80465e1b8459fda0b
lly <lly@espressif.com>
2020-07-01 20:02:26 +0800

    ble_mesh: stack: Update send_ttl in btc when recv a msg
    Update send_ttl mainly for server models. When a server model
    receives a message, and the status is required to be replied
    by the application, we need to set send_ttl to the msg context.
    If send_ttl is not updated in btc, and the applcation does not
    set the TTL either, then the status will be replied with TTL=0,
    which may cause the client side (e.g. the phone App) failed to
    receive the status.
    
    Closes https://github.com/espressif/esp-idf/issues/5300

--- components/bt/esp_ble_mesh/btc/btc_ble_mesh_config_model.c
+++ components/bt/esp_ble_mesh/btc/btc_ble_mesh_config_model.c
@@ -352,6 +352,7 @@ void bt_mesh_config_client_cb_evt_to_btc(u32_t opcode, u8_t evt_type,
     params.ctx.recv_op = ctx->recv_op;
     params.ctx.recv_dst = ctx->recv_dst;
     params.ctx.recv_rssi = ctx->recv_rssi;
+    params.ctx.send_ttl = ctx->send_ttl;
 
     cb_params.error_code = 0;
     cb_params.params = &params;
@@ -753,6 +754,7 @@ void bt_mesh_config_server_cb_evt_to_btc(u8_t evt_type,
     cb_params.ctx.recv_op = ctx->recv_op;
     cb_params.ctx.recv_dst = ctx->recv_dst;
     cb_params.ctx.recv_rssi = ctx->recv_rssi;
+    cb_params.ctx.send_ttl = ctx->send_ttl;
 
     if (val && len) {
         length = (len <= sizeof(cb_params.value)) ? len : sizeof(cb_params.value);
