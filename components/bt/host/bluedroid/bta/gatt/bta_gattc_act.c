0880c7bcf93a705afe8e156db62e4c5e81d6f2df
zwj <zhiweijian@espressif.com>
2020-08-04 20:19:25 +0800

    fix ble read multi char err when the number of handles is more than 10

--- components/bt/host/bluedroid/bta/gatt/bta_gattc_act.c
+++ components/bt/host/bluedroid/bta/gatt/bta_gattc_act.c
@@ -1156,12 +1156,16 @@ void bta_gattc_read_multi(tBTA_GATTC_CLCB *p_clcb, tBTA_GATTC_DATA *p_data)
         memset(&read_param, 0, sizeof(tGATT_READ_PARAM));
 
         if (status == BTA_GATT_OK) {
-            read_param.read_multiple.num_handles = p_data->api_read_multi.num_attr;
-            read_param.read_multiple.auth_req = p_data->api_read_multi.auth_req;
-            memcpy(&read_param.read_multiple.handles, p_data->api_read_multi.handles,
-                                        sizeof(UINT16) * p_data->api_read_multi.num_attr);
+            if(p_data->api_read_multi.num_attr < GATT_MAX_READ_MULTI_HANDLES) {
+                read_param.read_multiple.num_handles = p_data->api_read_multi.num_attr;
+                read_param.read_multiple.auth_req = p_data->api_read_multi.auth_req;
+                memcpy(&read_param.read_multiple.handles, p_data->api_read_multi.handles,
+                                            sizeof(UINT16) * p_data->api_read_multi.num_attr);
 
-            status = GATTC_Read(p_clcb->bta_conn_id, GATT_READ_MULTIPLE, &read_param);
+                status = GATTC_Read(p_clcb->bta_conn_id, GATT_READ_MULTIPLE, &read_param);
+            } else {
+                status = GATT_ILLEGAL_PARAMETER;
+            }
         }
 
         /* read fail */
