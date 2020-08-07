1be101f342daea9ad1a4b4c9c2e00a6fd8b9a06b
wangcheng <wangcheng@espressif.com>
2020-08-07 15:02:53 +0800

    component/bt: Add a macro to control the compilation of blufi.

--- components/bt/host/bluedroid/btc/profile/esp/blufi/blufi_protocol.c
+++ components/bt/host/bluedroid/btc/profile/esp/blufi/blufi_protocol.c
@@ -34,8 +34,7 @@
 #include "blufi_int.h"
 
 //#include "esp_wifi.h"
-
-#if (GATTS_INCLUDED == TRUE)
+#if (BLUFI_INCLUDED == TRUE)
 // extern tBLUFI_ENV blufi_env;
 
 void btc_blufi_protocol_handler(uint8_t type, uint8_t *data, int len)
@@ -269,4 +268,4 @@ void btc_blufi_protocol_handler(uint8_t type, uint8_t *data, int len)
     }
 }
 
-#endif	///(GATTS_INCLUDED == TRUE)
+#endif  ///BLUFI_INCLUDED == TRUE
