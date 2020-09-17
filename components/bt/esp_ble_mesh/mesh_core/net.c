0c136620d96782984adbbe6b2eded9e58c034f6f
lly <lly@espressif.com>
2020-09-17 11:22:49 +0800

    ble_mesh: stack: Persistent storage misc fixes
    * Fix the issue that deinit node with "erase_flash"
      set to true, but info is not erased from nvs
    * Reuse bt_mesh_cfg_reset() when deinit node
    * Optimize Provisioner related erase operations
    * No store pending timeout will be used when Node
      is not provisioned OR Provisioner is disabled
      and erase operation is performed
    * Change the default timeout for settings operation
      to 0, and rpl store rate to 0

--- components/bt/esp_ble_mesh/mesh_core/net.c
+++ components/bt/esp_ble_mesh/mesh_core/net.c
@@ -1557,7 +1557,7 @@ void bt_mesh_net_init(void)
     k_work_init(&bt_mesh.local_work, bt_mesh_net_local);
 }
 
-void bt_mesh_net_deinit(bool erase)
+void bt_mesh_net_deinit(void)
 {
     k_delayed_work_free(&bt_mesh.ivu_timer);
 
@@ -1579,11 +1579,4 @@ void bt_mesh_net_deinit(bool erase)
 
     bt_mesh.iv_index = 0U;
     bt_mesh.seq = 0U;
-
-    memset(bt_mesh.flags, 0, sizeof(bt_mesh.flags));
-
-    if (erase && IS_ENABLED(CONFIG_BLE_MESH_SETTINGS)) {
-        bt_mesh_clear_seq();
-        bt_mesh_clear_iv();
-    }
 }
