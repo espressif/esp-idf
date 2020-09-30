85d63af3019c8c5fb4812b41844a375334ce8714
xutao <xutao@espressif.com>
2020-09-30 11:34:00 +0800

    tcp_transport: fix an issue when use

--- components/tcp_transport/transport_ssl.c
+++ components/tcp_transport/transport_ssl.c
@@ -82,12 +82,18 @@ static int ssl_poll_read(esp_transport_handle_t t, int timeout_ms)
 {
     transport_ssl_t *ssl = esp_transport_get_context_data(t);
     int ret = -1;
+    int remain = 0;
     fd_set readset;
     fd_set errset;
     FD_ZERO(&readset);
     FD_ZERO(&errset);
     FD_SET(ssl->tls->sockfd, &readset);
     FD_SET(ssl->tls->sockfd, &errset);
+
+    if ((remain = esp_tls_get_bytes_avail(ssl->tls)) > 0) {
+        ESP_LOGD(TAG, "remain data in cache, need to read again");
+        return remain;
+    }
     struct timeval timeout;
     esp_transport_utils_ms_to_timeval(timeout_ms, &timeout);
 
@@ -146,10 +152,8 @@ static int ssl_read(esp_transport_handle_t t, char *buffer, int len, int timeout
     int poll, ret;
     transport_ssl_t *ssl = esp_transport_get_context_data(t);
 
-    if (esp_tls_get_bytes_avail(ssl->tls) <= 0) {
-        if ((poll = esp_transport_poll_read(t, timeout_ms)) <= 0) {
-            return poll;
-        }
+    if ((poll = esp_transport_poll_read(t, timeout_ms)) <= 0) {
+        return poll;
     }
     ret = esp_tls_conn_read(ssl->tls, (unsigned char *)buffer, len);
     if (ret < 0) {
