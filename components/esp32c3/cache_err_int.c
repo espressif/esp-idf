8330b2541a0019989992b5e7d379da11d9feca1c
morris <maoshengrong@espressif.com>
2020-12-01 20:03:10 +0800

    esp32c3: added target component

---
+++ components/esp32c3/cache_err_int.c
@@ -0,0 +1,34 @@
+// Copyright 2015-2020 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+ The cache has an interrupt that can be raised as soon as an access to a cached
+ region (flash) is done without the cache being enabled. We use that here
+ to panic the CPU, which from a debugging perspective is better than grabbing bad
+ data from the bus.
+*/
+
+#include <stdint.h>
+#include "sdkconfig.h"
+#include "esp_attr.h"
+
+// TODO ESP32-C3 IDF-2450
+void esp_cache_err_int_init(void)
+{
+}
+
+int IRAM_ATTR esp_cache_err_get_cpuid(void)
+{
+    return 0;
+}
