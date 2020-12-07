9aa1aabf9a7ae414b8d46077a3844dcf8055204b
Coleman Dill <powerofpickle@protonmail.com>
2020-12-07 00:19:55 -0600

    Fix invalid clock source in i2c_self_test example

--- examples/peripherals/i2c/i2c_self_test/main/i2c_example_main.c
+++ examples/peripherals/i2c/i2c_self_test/main/i2c_example_main.c
@@ -151,6 +151,7 @@ static esp_err_t i2c_master_init(void)
     conf.scl_io_num = I2C_MASTER_SCL_IO;
     conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
     conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    conf.clk_flags = 0;
     i2c_param_config(i2c_master_port, &conf);
     return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
 }
