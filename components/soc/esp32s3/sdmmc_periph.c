17c65dad272885c5cc03db56525dcd0bcc82b609
Ivan Grokhotkov <ivan@espressif.com>
2021-01-04 20:34:31 +0100

    soc: add esp32s3 sdmmc support
    * sync the latest struct header file from ESP32
    * add soc_caps.h macros to distinguish between IO MUX and GPIO Matrix
      support in SDMMC on different chips.
    * store GPIO matrix signal numbers in sdmmc_slot_info_t

--- components/soc/esp32s3/sdmmc_periph.c
+++ components/soc/esp32s3/sdmmc_periph.c
@@ -14,37 +14,44 @@
 
 #include "soc/sdmmc_periph.h"
 
-const sdmmc_slot_info_t sdmmc_slot_info[2]  = {
+const sdmmc_slot_info_t sdmmc_slot_info[SOC_SDMMC_NUM_SLOTS] = {
     {
-        .clk_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_CLK,
-        .cmd_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_CMD,
-        .d0_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_D0,
-        .d1_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_D1,
-        .d2_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_D2,
-        .d3_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_D3,
-        .d4_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_D4,
-        .d5_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_D5,
-        .d6_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_D6,
-        .d7_gpio = SDMMC_SLOT0_IOMUX_PIN_NUM_D7,
+        .width = 8,
         .card_detect = SDHOST_CARD_DETECT_N_1_IDX,
         .write_protect = SDHOST_CARD_WRITE_PRT_1_IDX,
         .card_int = SDHOST_CARD_INT_N_1_IDX,
-        .width = 8
     },
     {
-        .clk_gpio = SDMMC_SLOT1_IOMUX_PIN_NUM_CLK,
-        .cmd_gpio = SDMMC_SLOT1_IOMUX_PIN_NUM_CMD,
-        .d0_gpio = SDMMC_SLOT1_IOMUX_PIN_NUM_D0,
-        .d1_gpio = SDMMC_SLOT1_IOMUX_PIN_NUM_D1,
-        .d2_gpio = SDMMC_SLOT1_IOMUX_PIN_NUM_D2,
-        .d3_gpio = SDMMC_SLOT1_IOMUX_PIN_NUM_D3,
-        .d4_gpio = -1,  //slot1 has no D4-7
-        .d5_gpio = -1,
-        .d6_gpio = -1,
-        .d7_gpio = -1,
+        .width = 8,
         .card_detect = SDHOST_CARD_DETECT_N_2_IDX,
         .write_protect = SDHOST_CARD_WRITE_PRT_2_IDX,
         .card_int = SDHOST_CARD_INT_N_2_IDX,
-        .width = 4
+    }
+};
+
+const sdmmc_slot_io_info_t sdmmc_slot_gpio_sig[SOC_SDMMC_NUM_SLOTS] = {
+    {
+        .clk = SDHOST_CCLK_OUT_1_IDX,
+        .cmd = SDHOST_CCMD_OUT_1_IDX,
+        .d0 = SDHOST_CDATA_OUT_10_IDX,
+        .d1 = SDHOST_CDATA_OUT_11_IDX,
+        .d2 = SDHOST_CDATA_OUT_12_IDX,
+        .d3 = SDHOST_CDATA_OUT_13_IDX,
+        .d4 = SDHOST_CDATA_OUT_14_IDX,
+        .d5 = SDHOST_CDATA_OUT_15_IDX,
+        .d6 = SDHOST_CDATA_OUT_16_IDX,
+        .d7 = SDHOST_CDATA_OUT_17_IDX,
+    },
+    {
+        .clk = SDHOST_CCLK_OUT_2_IDX,
+        .cmd = SDHOST_CCMD_OUT_2_IDX,
+        .d0 = SDHOST_CDATA_OUT_20_IDX,
+        .d1 = SDHOST_CDATA_OUT_21_IDX,
+        .d2 = SDHOST_CDATA_OUT_22_IDX,
+        .d3 = SDHOST_CDATA_OUT_23_IDX,
+        .d4 = SDHOST_CDATA_OUT_24_IDX,
+        .d5 = SDHOST_CDATA_OUT_25_IDX,
+        .d6 = SDHOST_CDATA_OUT_26_IDX,
+        .d7 = SDHOST_CDATA_OUT_27_IDX,
     }
 };
--- components/soc/include/soc/sdmmc_periph.h
+++ components/soc/include/soc/sdmmc_periph.h
@@ -15,6 +15,7 @@
 #pragma once
 #include <stdint.h>
 //include soc related (generated) definitions
+#include "soc/soc_caps.h"
 #include "soc/soc_pins.h"
 #include "soc/sdmmc_reg.h"
 #include "soc/sdmmc_struct.h"
@@ -24,25 +25,50 @@
 extern "C" {
 #endif
 
+/**
+ * Common SDMMC slot info, doesn't depend on SOC_SDMMC_USE_{IOMUX,GPIO_MATRIX}
+ */
 typedef struct {
-    uint8_t clk_gpio;
-    uint8_t cmd_gpio;
-    uint8_t d0_gpio;
-    uint8_t d1_gpio;
-    uint8_t d2_gpio;
-    uint8_t d3_gpio;
-    uint8_t d4_gpio;
-    uint8_t d5_gpio;
-    uint8_t d6_gpio;
-    uint8_t d7_gpio;
-    uint8_t card_detect;
-    uint8_t write_protect;
-    uint8_t card_int;
-    uint8_t width;
+    uint8_t width;          /*!< Maximum supported slot width (1, 4, 8) */
+    uint8_t card_detect;    /*!< Card detect signal in GPIO Matrix */
+    uint8_t write_protect;  /*!< Write protect signal in GPIO Matrix */
+    uint8_t card_int;       /*!< Card interrupt signal in GPIO Matrix */
 } sdmmc_slot_info_t;
 
-/** pin and signal information of each slot */
-extern const sdmmc_slot_info_t sdmmc_slot_info[];
+/** Width and GPIO matrix signal numbers for auxillary SD host signals, one structure per slot */
+extern const sdmmc_slot_info_t sdmmc_slot_info[SOC_SDMMC_NUM_SLOTS];
+
+/**
+ * This structure lists pin numbers (if SOC_SDMMC_USE_IOMUX is set)
+ * or GPIO Matrix signal numbers (if SOC_SDMMC_USE_GPIO_MATRIX is set)
+ * for the SD bus signals. Field names match SD bus signal names.
+ */
+typedef struct {
+    uint8_t clk;
+    uint8_t cmd;
+    uint8_t d0;
+    uint8_t d1;
+    uint8_t d2;
+    uint8_t d3;
+    uint8_t d4;
+    uint8_t d5;
+    uint8_t d6;
+    uint8_t d7;
+} sdmmc_slot_io_info_t;
+
+/* Note: it is in theory possible to have both IOMUX and GPIO Matrix supported
+ * in the same SoC. However this is not used on any SoC at this point, and would
+ * complicate the driver. Hence only one of these options is supported at a time.
+ */
+#if SOC_SDMMC_USE_IOMUX
+/** GPIO pin numbers of SD bus signals, one structure per slot */
+extern const sdmmc_slot_io_info_t sdmmc_slot_gpio_num[SOC_SDMMC_NUM_SLOTS];
+
+#elif SOC_SDMMC_USE_GPIO_MATRIX
+/** GPIO matrix signal numbers of SD bus signals, one structure per slot */
+extern const sdmmc_slot_io_info_t sdmmc_slot_gpio_sig[SOC_SDMMC_NUM_SLOTS];
+
+#endif // SOC_SDMMC_USE_{IOMUX,GPIO_MATRIX}
 
 #ifdef __cplusplus
 }
