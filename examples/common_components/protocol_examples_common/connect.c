c260c223e979c2604806cb585064e4fcfe80fdba
xiehang <xiehang@espressif.com>
2021-01-11 14:35:29 +0800

    example: We should not check the return value of esp_wifi_connect() in any case

--- examples/common_components/protocol_examples_common/connect.c
+++ examples/common_components/protocol_examples_common/connect.c
@@ -271,7 +271,7 @@ static esp_netif_t *wifi_start(void)
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
-    ESP_ERROR_CHECK(esp_wifi_connect());
+    esp_wifi_connect();
     return netif;
 }
 
