7e17f64a2d1029d7815231503b6f1558a1b477b2
yuanjm <yuanjianmin@espressif.com>
2021-01-15 19:33:52 +0800

    Make OTA example support keepalive

--- examples/system/ota/native_ota_example/main/native_ota_example.c
+++ examples/system/ota/native_ota_example/main/native_ota_example.c
@@ -97,6 +97,7 @@ static void ota_example_task(void *pvParameter)
         .url = CONFIG_EXAMPLE_FIRMWARE_UPG_URL,
         .cert_pem = (char *)server_cert_pem_start,
         .timeout_ms = CONFIG_EXAMPLE_OTA_RECV_TIMEOUT,
+        .keep_alive_enable = true,
     };
 
 #ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL_FROM_STDIN
