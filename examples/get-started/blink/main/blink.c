d8a3f59d29ed1ff8441b65cf255f08a3db365102
Ivan Grokhotkov <ivan@espressif.com>
2021-01-20 01:15:51 +0100

    examples/blink: convert tabs to spaces

--- examples/get-started/blink/main/blink.c
+++ examples/get-started/blink/main/blink.c
@@ -30,11 +30,11 @@ void app_main(void)
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     while(1) {
         /* Blink off (output low) */
-	printf("Turning off the LED\n");
+        printf("Turning off the LED\n");
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         /* Blink on (output high) */
-	printf("Turning on the LED\n");
+        printf("Turning on the LED\n");
         gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
