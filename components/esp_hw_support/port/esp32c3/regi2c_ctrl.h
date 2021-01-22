f5f046126468ff27c8ec2c4e2e06bdf74c5b1e5c
Armando <douyiwen@espressif.com>
2021-01-22 19:50:31 +0800

    tmpsensor: add temp_sensor.h for c3

--- components/esp_hw_support/port/esp32c3/regi2c_ctrl.h
+++ components/esp_hw_support/port/esp32c3/regi2c_ctrl.h
@@ -45,12 +45,12 @@ extern "C" {
 #define ANA_CONFIG_M    (0x3FF)
 
 #define ANA_I2C_SAR_FORCE_PD BIT(18)
-#define ANA_I2C_BBPLL_M     (BIT(17)) /* Clear to enable BBPLL */
-#define ANA_I2C_APLL_M      (BIT(14)) /* Clear to enable APLL */
+#define ANA_I2C_BBPLL_M      BIT(17) /* Clear to enable BBPLL */
+#define ANA_I2C_APLL_M       BIT(14) /* Clear to enable APLL */
 
 
 #define ANA_CONFIG2_REG  0x6000E048
-#define ANA_CONFIG2_M   (BIT(18))
+#define ANA_CONFIG2_M    BIT(18)
 
 #define ANA_I2C_SAR_FORCE_PU BIT(16)
 
