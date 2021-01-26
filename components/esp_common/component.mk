883aba20be4efdbcef3ed0abe77f2872d2cf9a46
Renz Bagaporo <renz.bagaporo@espressif.com>
2021-01-26 11:31:41 +0800

    esp_common: move stack check

--- components/esp_common/component.mk
+++ components/esp_common/component.mk
@@ -4,10 +4,6 @@
 
 COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENT_SRCDIRS := src
-
 ifndef CONFIG_IDF_ENV_FPGA
 COMPONENT_OBJEXCLUDE += src/fpga_overrides.o
 endif
-
-# disable stack protection in files which are involved in initialization of that feature
-src/stack_check.o: CFLAGS := $(filter-out -fstack-protector%, $(CFLAGS))
