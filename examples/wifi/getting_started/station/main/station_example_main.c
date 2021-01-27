1ef10dc284e12a588f38d1cf4f41a4cf150c6a73
xiehang <xiehang@espressif.com>
2021-01-27 10:54:06 +0800

    esp_wifi: Modify ESP_IF_WIFI_STA to WIFI_IF_STA

--- examples/wifi/getting_started/station/main/station_example_main.c
+++ examples/wifi/getting_started/station/main/station_example_main.c
@@ -104,7 +104,7 @@ void wifi_init_sta(void)
         },
     };
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
     ESP_ERROR_CHECK(esp_wifi_start() );
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
