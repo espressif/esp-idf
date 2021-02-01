179e23b38ae35c8e75854cab1750dc22a6145de9
Martin Gaňo <martin.gano@espressif.com>
2021-02-01 11:40:03 +0100

    Fixed automatic connection in idf_monitor
    Closes https://github.com/espressif/esp-idf/issues/6415

--- tools/idf_py_actions/errors.py
+++ tools/idf_py_actions/errors.py
@@ -2,9 +2,14 @@ class FatalError(RuntimeError):
     """
     Wrapper class for runtime errors that aren't caused by bugs in idf.py or the build process.
     """
+
     def __init__(self, message, ctx=None):
         super(RuntimeError, self).__init__(message)
         # if context is defined, check for the cleanup tasks
         if ctx is not None and 'cleanup' in ctx.meta:
             # cleans up the environment before failure
             ctx.meta['cleanup']()
+
+
+class NoSerialPortFoundError(FatalError):
+    pass
