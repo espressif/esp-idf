1d8ca35eec9d5fc2706979883fa1d7a77aeab174
Jakob Hasse <jakob.hasse@espressif.com>
2021-02-07 16:51:52 +0800

    [C++]: wrapper functions around unwind code
    * Replaced all C++ exception related
      functions with wrappers if -fno-exception
      is used. This prevents linking of the
      corresponding code in libgcc. The code
      size will decrease by around 7-9 KB when
      building with -fno-exception.
    * added no except test app
    
    Closes https://github.com/espressif/esp-idf/pull/5380
    Closes https://github.com/espressif/esp-idf/issues/5363
    Closes https://github.com/espressif/esp-idf/issues/5224
    Closes IDFGH-3153
    Closes IDF-2577

--- components/cxx/component.mk
+++ components/cxx/component.mk
@@ -6,7 +6,12 @@ ifndef CONFIG_COMPILER_CXX_EXCEPTIONS
 # If exceptions are disabled, ensure our fatal exception
 # hooks are preferentially linked over libstdc++ which
 # has full exception support
+WRAP_FUNCTIONS = _Unwind_SetEnableExceptionFdeSorting __register_frame_info_bases __register_frame_info __register_frame __register_frame_info_table_bases __register_frame_info_table __register_frame_table __deregister_frame_info_bases __deregister_frame_info _Unwind_Find_FDE _Unwind_GetGR _Unwind_GetCFA _Unwind_GetIP _Unwind_GetIPInfo _Unwind_GetRegionStart _Unwind_GetDataRelBase _Unwind_GetTextRelBase _Unwind_SetIP _Unwind_SetGR _Unwind_GetLanguageSpecificData _Unwind_FindEnclosingFunction _Unwind_Resume _Unwind_RaiseException _Unwind_DeleteException _Unwind_ForcedUnwind _Unwind_Resume_or_Rethrow _Unwind_Backtrace __cxa_call_unexpected __gxx_personality_v0
+WRAP_ARGUMENT := -Wl,--wrap=
+
+COMPONENT_ADD_LDFLAGS = -l$(COMPONENT_NAME) $(addprefix $(WRAP_ARGUMENT),$(WRAP_FUNCTIONS))
 COMPONENT_ADD_LDFLAGS += -u __cxx_fatal_exception
-endif
+
+endif # CONFIG_COMPILER_CXX_EXCEPTIONS
 
 COMPONENT_ADD_INCLUDEDIRS =
