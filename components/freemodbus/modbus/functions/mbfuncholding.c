9457555173612749ea08e138855b8aebdca3c6b0
aleks <aleks@espressif.com>
2021-02-08 09:25:25 +0100

    freemodbus: potential loss of significant bits and auto merge issues

--- components/freemodbus/modbus/functions/mbfuncholding.c
+++ components/freemodbus/modbus/functions/mbfuncholding.c
@@ -184,7 +184,7 @@ eMBFuncReadHoldingRegister( UCHAR * pucFrame, USHORT * usLen )
         usRegAddress++;
 
         usRegCount = ( USHORT )( pucFrame[MB_PDU_FUNC_READ_REGCNT_OFF] << 8 );
-        usRegCount = ( USHORT )( pucFrame[MB_PDU_FUNC_READ_REGCNT_OFF + 1] );
+        usRegCount |= ( USHORT )( pucFrame[MB_PDU_FUNC_READ_REGCNT_OFF + 1] );
 
         /* Check if the number of registers to read is valid. If not
          * return Modbus illegal data value exception.
