774f0101961567b99f9a999b767020f8e4fc7c6d
Omar Chebib <omar.chebib@espressif.com>
2021-02-10 16:18:04 +0800

    freertos: Fix delay between interrupt request and trigger on RISC-V
    NOP instructions have been added in order to prevent the code
    from executing code it shouldn't execute. This is due to a delay
    between the moment an interrupt is requested and the moment it
    is fired. It only happens on RISC-V SoC.

--- components/freertos/port/riscv/port.c
+++ components/freertos/port/riscv/port.c
@@ -178,6 +178,24 @@ void prvTaskExitError(void)
 void vPortClearInterruptMask(int mask)
 {
     REG_WRITE(INTERRUPT_CORE0_CPU_INT_THRESH_REG, mask);
+    /**
+     * The delay between the moment we unmask the interrupt threshold register
+     * and the moment the potential requested interrupt is triggered is not
+     * null: up to three machine cycles/instructions can be executed.
+     *
+     * When compilation size optimization is enabled, this function and its
+     * callers returning void will have NO epilogue, thus the instruction
+     * following these calls will be executed.
+     *
+     * If the requested interrupt is a context switch to a higher priority
+     * task then the one currently running, we MUST NOT execute any instruction
+     * before the interrupt effectively happens.
+     * In order to prevent this, force this routine to have a 3-instruction
+     * delay before exiting.
+     */
+    asm volatile ( "nop" );
+    asm volatile ( "nop" );
+    asm volatile ( "nop" );
 }
 
 /* Set interrupt mask and return current interrupt enable register */
@@ -188,6 +206,17 @@ int vPortSetInterruptMask(void)
     ret = REG_READ(INTERRUPT_CORE0_CPU_INT_THRESH_REG);
     REG_WRITE(INTERRUPT_CORE0_CPU_INT_THRESH_REG, RVHAL_EXCM_LEVEL);
     RV_SET_CSR(mstatus, old_mstatus & MSTATUS_MIE);
+    /**
+     * In theory, this function should not return immediately as there is a
+     * delay between the moment we mask the interrupt threshold register and
+     * the moment a potential lower-priority interrupt is triggered (as said
+     * above), it should have a delay of 2 machine cycles/instructions.
+     *
+     * However, in practice, this function has an epilogue of one instruction,
+     * thus the instruction masking the interrupt threshold register is
+     * followed by two instructions: `ret` and `csrrs` (RV_SET_CSR).
+     * That's why we don't need any additional nop instructions here.
+     */
     return ret;
 }
 
