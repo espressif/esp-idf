7fc2f918419703e334e58370e7c14ccd0f93a47b
Angus Gratton <angus@espressif.com>
2021-02-12 11:13:47 +1100

    freertos: Increase minimum task stack size when stack smashing checker is enabled
    Fixes issue with DPORT init task, this task uses minimum stack size and may not be
    enough if stack smashing detection is set to Overall mode.
    
    Also reworks the way we calculate minimum stack to allow for adding multiple
    contributing factors.
    
    Closes https://github.com/espressif/esp-idf/issues/6403

--- components/esp32/dport_access.c
+++ components/esp32/dport_access.c
@@ -165,6 +165,9 @@ static void dport_access_init_core(void *arg)
     dport_access_end[core_id] = 0;
     dport_core_state[core_id] = DPORT_CORE_STATE_RUNNING;
 
+    /* If this fails then the minimum stack size for this config is too close to running out */
+    assert(uxTaskGetStackHighWaterMark(NULL) > 128);
+
     vTaskDelete(NULL);
 }
 #endif
