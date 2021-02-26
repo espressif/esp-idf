716edebbbdc22a01bede532e7df140eda904233c
baohongde <baohongde@espressif.com>
2021-02-26 14:44:57 +0800

    components/bt: Fix assert without sw coexist enabled

--- components/bt/bt.c
+++ components/bt/bt.c
@@ -1107,7 +1107,7 @@ static uint8_t coex_schm_curr_period_get_wrapper(void)
 #if CONFIG_SW_COEXIST_ENABLE
     return coex_schm_curr_period_get();
 #else
-    return 0;
+    return 1;
 #endif
 }
 
@@ -1125,13 +1125,7 @@ static int coex_wifi_channel_get_wrapper(uint8_t *primary, uint8_t *secondary)
 #if CONFIG_SW_COEXIST_ENABLE
     return coex_wifi_channel_get(primary, secondary);
 #else
-    if (primary == NULL || secondary == NULL) {
-        return -1;
-    }
-
-    *primary = 0;
-    *secondary = 0;
-    return 0;
+    return -1;
 #endif
 }
 
@@ -1140,7 +1134,7 @@ static int coex_register_wifi_channel_change_callback_wrapper(void *cb)
 #if CONFIG_SW_COEXIST_ENABLE
     return coex_register_wifi_channel_change_callback(cb);
 #else
-    return 0;
+    return -1;
 #endif
 }
 
