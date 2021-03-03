espressif/esp-idf

commit fed98cd6adbe6576beda5076d0840eac0b4944e6

ci: ttfw: Encode serial port data to whatever the console encoding is
This is a bit of a hack, but gives us a way to always log it

tools/ci/python_packages/tiny_test_fw/DUT.py
@@ -40,6 +40,7 @@ If they using different port then need to implement their DUTPort class as well.
 from __future__ import print_function
 import time
 import re
+import sys
 import threading
 import copy
 import functools
@@ -78,11 +79,12 @@ def _expect_lock(func):
 def _decode_data(data):
     """ for python3, if the data is bytes, then decode it to string """
     if isinstance(data, bytes):
-        # convert bytes to string
+        # convert bytes to string. This is a bit of a hack, we know that we want to log this
+        # later so encode to the stdout encoding with backslash escapes for anything non-encodable
         try:
-            data = data.decode("utf-8", "ignore")
-        except UnicodeDecodeError:
-            data = data.decode("iso8859-1", )
+            return data.decode(sys.stdout.encoding, "backslashreplace")
+        except UnicodeDecodeError:  # Python <3.5 doesn't support backslashreplace
+            return data.decode(sys.stdout.encoding, "replace")
     return data
 
 

commit 2c4f90dab29c06eb03016c9ada4ec24288e76967

ci: Fix missing sys.stdout.encoding in python2 runners
Regression in fed98cd6adbe6576beda5076d0840eac0b4944e6

tools/ci/python_packages/tiny_test_fw/DUT.py
@@ -81,10 +81,13 @@ def _decode_data(data):
     if isinstance(data, bytes):
         # convert bytes to string. This is a bit of a hack, we know that we want to log this
         # later so encode to the stdout encoding with backslash escapes for anything non-encodable
+        encoding = sys.stdout.encoding
+        if encoding is None:
+            encoding = 'ascii'
         try:
-            return data.decode(sys.stdout.encoding, "backslashreplace")
-        except UnicodeDecodeError:  # Python <3.5 doesn't support backslashreplace
-            return data.decode(sys.stdout.encoding, "replace")
+            return data.decode(encoding, "backslashreplace")
+        except (UnicodeDecodeError, TypeError):  # Python <3.5 doesn't support backslashreplace
+            return data.decode(encoding, "replace")
     return data
 
 