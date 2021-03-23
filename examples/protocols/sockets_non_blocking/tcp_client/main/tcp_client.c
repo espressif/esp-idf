e8bbe2f94fc680c443e40682b3faa6973e46d8b7
suren.gabrielyan <suren.gabrielyan@espressif.com>
2021-03-23 20:00:17 +0400

    examples: Added non blocking sockets example tests
    Closes https://github.com/espressif/esp-idf/issues/6675

---
+++ examples/protocols/sockets_non_blocking/tcp_client/main/tcp_client.c
@@ -0,0 +1,154 @@
+/* BSD Socket API Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "addr_from_stdin.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+
+
+#if defined(CONFIG_EXAMPLE_IPV4)
+#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
+#elif defined(CONFIG_EXAMPLE_IPV6)
+#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV6_ADDR
+#else
+#define HOST_IP_ADDR ""
+#endif
+
+#define PORT CONFIG_EXAMPLE_PORT
+#define TIMEOUT     10
+
+static const char *TAG = "example";
+static const char *payload = "Message from ESP32 ";
+
+static void tcp_client_task(void *pvParameters)
+{
+    char rx_buffer[128];
+    char host_ip[] = HOST_IP_ADDR;
+    int addr_family = 0;
+    int ip_protocol = 0;
+    int flags;
+    fd_set wrfds;
+
+    FD_ZERO(&wrfds);
+    while (1) {
+#if defined(CONFIG_EXAMPLE_IPV4)
+        struct sockaddr_in dest_addr;
+        dest_addr.sin_addr.s_addr = inet_addr(host_ip);
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(PORT);
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+#elif defined(CONFIG_EXAMPLE_IPV6)
+        struct sockaddr_in6 dest_addr = { 0 };
+        inet6_aton(host_ip, &dest_addr.sin6_addr);
+        dest_addr.sin6_family = AF_INET6;
+        dest_addr.sin6_port = htons(PORT);
+        dest_addr.sin6_scope_id = esp_netif_get_netif_impl_index(EXAMPLE_INTERFACE);
+        addr_family = AF_INET6;
+        ip_protocol = IPPROTO_IPV6;
+#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
+        struct sockaddr_storage dest_addr = { 0 };
+        ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_STREAM, &ip_protocol, &addr_family, &dest_addr));
+#endif
+
+        // Creating a client socket
+		int sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
+
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Socket created, connecting to %s:%d", host_ip, PORT);
+
+        // Getting file descriptor flags
+        flags = fcntl(sock, F_GETFL);
+
+        // Marking the socket as non-blocking
+        if (fcntl(sock, F_SETFL, flags | O_NONBLOCK) == -1) {
+            ESP_LOGE(TAG, "Unable to set socket non blocking: errno %d", errno);
+        }
+        if (connect(sock, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr_in6)) == -1) {
+            if (errno == EINPROGRESS) {
+                ESP_LOGI(TAG, "connect EINPROGRESS OK");
+                FD_SET(sock, &wrfds);
+            } else {
+                ESP_LOGE(TAG, "Socket unable to connect: error %s", strerror(errno));
+            }
+        } else {
+            ESP_LOGI(TAG, "%s successfully connected on port %d", host_ip, PORT);
+        }
+
+        // Setting socket back to blocking mode for read/write operations
+        flags = fcntl(sock, F_GETFL);
+
+        if (fcntl(sock, F_SETFL, flags ^ O_NONBLOCK) == -1) {
+            ESP_LOGE(TAG, "Unable to set socket non blocking: errno %d", errno);
+        }
+
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        while (1) {
+            int err = send(sock, payload, strlen(payload), 0);
+
+			if(err == -1) {
+				ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+            	vTaskDelay(1000 / portTICK_PERIOD_MS);
+				continue;
+			}
+
+            int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+
+            if (len == -1 ) {
+                ESP_LOGE(TAG, "recv failed, err msg : %s", strerror(errno));
+                break;
+            }
+            // Data received
+            else {
+                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+                ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
+                ESP_LOGI(TAG, "%s", rx_buffer);
+				break;
+            }
+
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    xTaskCreate(tcp_client_task, "tcp_client", 4096, NULL, 5, NULL);
+}
