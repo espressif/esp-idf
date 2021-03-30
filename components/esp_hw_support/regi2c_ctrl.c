dd8843fec34bddfb64aaf2c6d823556603bdf4a7
Omar Chebib <omar.chebib@espressif.com>
2021-03-30 14:50:27 +0800

    regi2c: add a spinlock for accessing (reg)I2C devices
    When not compiling bootloader, a spinlock will be used for reading or writing
    I2C internal devices/registers.
    When compiling for bootloader, no need to use any lock.

---
+++ components/esp_hw_support/regi2c_ctrl.c
@@ -0,0 +1,50 @@
+// Copyright 2020 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "regi2c_ctrl.h"
+#include <stdint.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+
+static portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
+uint8_t IRAM_ATTR regi2c_ctrl_read_reg(uint8_t block, uint8_t host_id, uint8_t reg_add)
+{
+    portENTER_CRITICAL_ISR(&mux);
+    uint8_t value = i2c_read_reg_raw(block, host_id, reg_add);
+    portEXIT_CRITICAL_ISR(&mux);
+    return value;
+}
+
+uint8_t IRAM_ATTR regi2c_ctrl_read_reg_mask(uint8_t block, uint8_t host_id, uint8_t reg_add, uint8_t msb, uint8_t lsb)
+{
+    portENTER_CRITICAL_ISR(&mux);
+    uint8_t value = i2c_read_reg_mask_raw(block, host_id, reg_add, msb, lsb);
+    portEXIT_CRITICAL_ISR(&mux);
+    return value;
+}
+
+void IRAM_ATTR regi2c_ctrl_write_reg(uint8_t block, uint8_t host_id, uint8_t reg_add, uint8_t data)
+{
+    portENTER_CRITICAL_ISR(&mux);
+    i2c_write_reg_raw(block, host_id, reg_add, data);
+    portEXIT_CRITICAL_ISR(&mux);
+}
+
+void IRAM_ATTR regi2c_ctrl_write_reg_mask(uint8_t block, uint8_t host_id, uint8_t reg_add, uint8_t msb, uint8_t lsb, uint8_t data)
+{
+    portENTER_CRITICAL_ISR(&mux);
+    i2c_write_reg_mask_raw(block, host_id, reg_add, msb, lsb, data);
+    portEXIT_CRITICAL_ISR(&mux);
+}
