632796887e8d3948d8c6c15dc9451174ddfcc320
Angus Gratton <angus@espressif.com>
2021-03-31 17:35:00 +1100

    cmake: Fix passing PYTHON path via CMake variable if using IDF as library
    Closes https://github.com/espressif/esp-idf/issues/6285

--- tools/cmake/build.cmake
+++ tools/cmake/build.cmake
@@ -130,9 +130,11 @@ function(__build_init idf_path)
     # Create the build target, to which the ESP-IDF build properties, dependencies are attached to
     add_library(__idf_build_target STATIC IMPORTED)
 
-    set_default(python "python")
+    # Set the Python path (which may be passed in via -DPYTHON=) and store in a build property
+    set_default(PYTHON "python")
+    file(TO_CMAKE_PATH ${PYTHON} PYTHON)
+    idf_build_set_property(PYTHON ${PYTHON})
 
-    idf_build_set_property(PYTHON ${python})
     idf_build_set_property(IDF_PATH ${idf_path})
 
     idf_build_set_property(__PREFIX idf)
