d73a09cd8b8117c2906bf6025aef89d6229c510b
Li Shuai <lishuai@espressif.com>
2021-04-13 16:50:22 +0800

    light sleep: add wifi mac sleep support for esp32s3

---
+++ components/esp_rom/include/esp32s3/rom/apb_backup_dma.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ets_apb_backup_init_lock_func(void(* _apb_backup_lock)(void), void(* _apb_backup_unlock)(void));
+
+#ifdef __cplusplus
+}
+#endif
