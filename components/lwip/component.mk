8767aa7a4f92b490807cd72dfe0103bada858716
liuhan <liuhan@espressif.com>
2021-04-15 20:48:52 +0800

    lwip: Add CONFIG_LWIP_ICMP and CONFIG_LWIP_ICMP6 to sperate the code

--- components/lwip/component.mk
+++ components/lwip/component.mk
@@ -36,6 +36,10 @@ else
     COMPONENT_OBJEXCLUDE += port/esp32/no_vfs_syscalls.o
 endif
 
+ifndef CONFIG_LWIP_ICMP
+    COMPONENT_OBJEXCLUDE += apps/ping/esp_ping.o apps/ping/ping.o apps/ping/ping_sock.o
+endif
+
 ifdef CONFIG_LWIP_PPP_SUPPORT
     COMPONENT_SRCDIRS += lwip/src/netif/ppp lwip/src/netif/ppp/polarssl
 endif
