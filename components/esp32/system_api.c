cb702f3d37dac28f62eb949600b3ebb8bd1a1ec8
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-04-20 16:21:39 +0200

    Support for OEM ESP32-solo1 variant
    used in xiaomi and yeelight devices

--- components/esp32/system_api.c
+++ components/esp32/system_api.c
@@ -115,6 +115,10 @@ esp_err_t esp_efuse_mac_get_default(uint8_t* mac)
                 return ESP_OK;
             }
         } else {
+            if (esp_efuse_get_pkg_ver() ==  3) {
+                ESP_LOGI(TAG, "Found OEM-type ESP32, ignore EFUSE CRC error ...");
+                return ESP_OK; // override for Xiaomi SOC's and maybe others too
+            }
             ESP_LOGE(TAG, "Base MAC address from BLK0 of EFUSE CRC error, efuse_crc = 0x%02x; calc_crc = 0x%02x", efuse_crc, calc_crc);
             abort();
         }
