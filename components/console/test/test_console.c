28a94328d4f03a057411d69bcae82572385b8dd2
Jan Brudný <jan.brudny@espressif.com>
2021-05-17 03:41:32 +0200

    console: update copyright notice

--- components/console/test/test_console.c
+++ components/console/test/test_console.c
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <stdio.h>
 #include <string.h>
 #include "sdkconfig.h"
