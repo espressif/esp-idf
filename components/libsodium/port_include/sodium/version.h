e20826cc7fc81214280c4fc92e17445897bc276b
Jan Brudný <jan.brudny@espressif.com>
2021-05-17 02:58:32 +0200

    libsodium: update copyright notice

--- components/libsodium/port_include/sodium/version.h
+++ components/libsodium/port_include/sodium/version.h
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #ifndef sodium_version_H
 #define sodium_version_H
--- components/libsodium/test/test_sodium.c
+++ components/libsodium/test/test_sodium.c
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include "unity.h"
 #include "sodium/crypto_hash_sha256.h"
 #include "sodium/crypto_hash_sha512.h"
