83f2d802ce4d613a60fa223066eebd949c7d0acb
morris <maoshengrong@espressif.com>
2021-05-18 10:50:01 +0800

    esp_eth: fix default PHY reset gpio
    Closes https://github.com/espressif/esp-idf/issues/7034

--- components/esp_eth/component.mk
+++ components/esp_eth/component.mk
@@ -5,7 +5,7 @@ COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENT_SRCDIRS := src
 
 ifndef CONFIG_ETH_USE_ESP32_EMAC
-    COMPONENT_OBJEXCLUDE += src/esp_eth_mac_esp32.o
+    COMPONENT_OBJEXCLUDE += src/esp_eth_mac_esp.o
 endif
 
 ifndef CONFIG_ETH_SPI_ETHERNET_DM9051
