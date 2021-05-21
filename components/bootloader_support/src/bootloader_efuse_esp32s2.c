3cf4fbc1505055f439ebb26aa3556a148e2006fd
KonstantinKondrashov <konstantin@espressif.com>
2021-05-21 22:24:31 +0800

    efuse(esp32s2): Added flash_ver, psram_ver, pkg_ver efuses

--- components/bootloader_support/src/bootloader_efuse_esp32s2.c
+++ components/bootloader_support/src/bootloader_efuse_esp32s2.c
@@ -19,5 +19,5 @@ uint8_t bootloader_common_get_chip_revision(void)
 uint32_t bootloader_common_get_chip_ver_pkg(void)
 {
     // should return the same value as esp_efuse_get_pkg_ver()
-    return REG_GET_FIELD(EFUSE_RD_MAC_SPI_SYS_3_REG, EFUSE_PKG_VERSION);
+    return REG_GET_FIELD(EFUSE_RD_MAC_SPI_SYS_4_REG, EFUSE_PKG_VERSION);
 }
