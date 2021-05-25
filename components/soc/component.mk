d7687ce6fae79ef2721098edca71bd5c900420e6
Omar Chebib <omar.chebib@espressif.com>
2021-05-25 17:16:43 +0800

    regi2c: add a spinlock for accessing (reg)I2C devices
    When not compiling bootloader, a spinlock will be used for reading or writing
    I2C internal devices/registers.
    When compiling for bootloader, no need to use any lock.

--- components/soc/component.mk
+++ components/soc/component.mk
@@ -1,10 +1,10 @@
 SOC_NAME := $(IDF_TARGET)
 
 COMPONENT_SRCDIRS := src src/hal
-COMPONENT_ADD_INCLUDEDIRS := 
+COMPONENT_ADD_INCLUDEDIRS :=
 
-# Since there can be chip-specific HAL headers which can include the common 
-# HAL header via include_next, process the build scripts here first so that 
+# Since there can be chip-specific HAL headers which can include the common
+# HAL header via include_next, process the build scripts here first so that
 # include directories appear first in the compile command.
 -include $(COMPONENT_PATH)/src/$(SOC_NAME)/component.mk
 COMPONENT_ADD_INCLUDEDIRS += include
@@ -12,3 +12,7 @@ COMPONENT_ADD_INCLUDEDIRS += include
 -include $(COMPONENT_PATH)/soc/component.mk
 
 COMPONENT_ADD_LDFRAGMENTS += linker.lf
+
+ifdef IS_BOOTLOADER_BUILD
+	COMPONENT_OBJEXCLUDE += src/regi2c_ctrl.o
+endif
--- components/soc/src/esp32s2/regi2c_ctrl.h
+++ components/soc/src/esp32s2/regi2c_ctrl.h
@@ -45,20 +45,45 @@ void i2c_rtc_write_reg(uint8_t block, uint8_t host_id, uint8_t reg_add, uint8_t 
 void i2c_rtc_write_reg_mask(uint8_t block, uint8_t host_id, uint8_t reg_add, uint8_t msb, uint8_t lsb, uint8_t data);
 void i2c_rtc_init(void);
 
+#ifdef BOOTLOADER_BUILD
+
+/**
+ * If compiling for the bootloader, internal functions can be called directly,
+ * without the need of a lock.
+ */
+#define regi2c_ctrl_read_reg         i2c_rtc_read_reg
+#define regi2c_ctrl_read_reg_mask    i2c_rtc_read_reg_mask
+#define regi2c_ctrl_write_reg        i2c_rtc_write_reg
+#define regi2c_ctrl_write_reg_mask   i2c_rtc_write_reg_mask
+
+#else
+
+#define i2c_read_reg_raw        i2c_rtc_read_reg
+#define i2c_read_reg_mask_raw   i2c_rtc_read_reg_mask
+#define i2c_write_reg_raw       i2c_rtc_write_reg
+#define i2c_write_reg_mask_raw  i2c_rtc_write_reg_mask
+
+uint8_t regi2c_ctrl_read_reg(uint8_t block, uint8_t host_id, uint8_t reg_add);
+uint8_t regi2c_ctrl_read_reg_mask(uint8_t block, uint8_t host_id, uint8_t reg_add, uint8_t msb, uint8_t lsb);
+void regi2c_ctrl_write_reg(uint8_t block, uint8_t host_id, uint8_t reg_add, uint8_t data);
+void regi2c_ctrl_write_reg_mask(uint8_t block, uint8_t host_id, uint8_t reg_add, uint8_t msb, uint8_t lsb, uint8_t data);
+
+#endif // BOOTLOADER_BUILD
+
 /* Convenience macros for the above functions, these use register definitions
  * from regi2c_apll.h/regi2c_bbpll.h header files.
  */
 #define REGI2C_WRITE_MASK(block, reg_add, indata) \
-      i2c_rtc_write_reg_mask(block, block##_HOSTID,  reg_add,  reg_add##_MSB,  reg_add##_LSB,  indata)
+      regi2c_ctrl_write_reg_mask(block, block##_HOSTID,  reg_add,  reg_add##_MSB,  reg_add##_LSB,  indata)
 
 #define REGI2C_READ_MASK(block, reg_add) \
-      i2c_rtc_read_reg_mask(block, block##_HOSTID,  reg_add,  reg_add##_MSB,  reg_add##_LSB)
+      regi2c_ctrl_read_reg_mask(block, block##_HOSTID,  reg_add,  reg_add##_MSB,  reg_add##_LSB)
 
 #define REGI2C_WRITE(block, reg_add, indata) \
-      i2c_rtc_write_reg(block, block##_HOSTID,  reg_add, indata)
+      regi2c_ctrl_write_reg(block, block##_HOSTID,  reg_add, indata)
 
 #define REGI2C_READ(block, reg_add) \
-      i2c_rtc_read_reg(block, block##_HOSTID,  reg_add)
+      regi2c_ctrl_read_reg(block, block##_HOSTID,  reg_add)
 
 #ifdef __cplusplus
 }
