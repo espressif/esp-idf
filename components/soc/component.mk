8d1f243bff5d99223deb1a32d8e7867b6106ddd7
Omar Chebib <omar.chebib@espressif.com>
2021-05-25 17:16:43 +0800

    regi2c: add a spinlock for accessing (reg)I2C devices
    When not compiling bootloader, a spinlock will be used for reading or writing
    I2C internal devices/registers.
    When compiling for bootloader, no need to use any lock.

--- components/soc/component.mk
+++ components/soc/component.mk
@@ -2,7 +2,11 @@ SOC_NAME := $(IDF_TARGET)
 
 COMPONENT_SRCDIRS := $(SOC_NAME) src src/hal
 
-COMPONENT_ADD_INCLUDEDIRS := $(SOC_NAME)/include include
+COMPONENT_ADD_INCLUDEDIRS := $(SOC_NAME) $(SOC_NAME)/include include
 
 -include $(COMPONENT_PATH)/$(SOC_NAME)/component.mk
 COMPONENT_ADD_LDFRAGMENTS += linker.lf
+
+ifdef IS_BOOTLOADER_BUILD
+	COMPONENT_OBJEXCLUDE += src/regi2c_ctrl.o
+endif
