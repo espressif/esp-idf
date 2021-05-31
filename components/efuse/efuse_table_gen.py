1bac0d1e4205f143eea541347fc7080d5ed9bfba
Jan Brudný <jan.brudny@espressif.com>
2021-05-31 06:43:23 +0200

    efuse: update copyright notice

--- components/efuse/efuse_table_gen.py
+++ components/efuse/efuse_table_gen.py
@@ -4,19 +4,9 @@
 #
 # Converts efuse table to header file efuse_table.h.
 #
-# Copyright 2017-2018 Espressif Systems (Shanghai) PTE LTD
+# SPDX-FileCopyrightText: 2017-2021 Espressif Systems (Shanghai) CO LTD
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http:#www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 from __future__ import division, print_function
 
 import argparse
