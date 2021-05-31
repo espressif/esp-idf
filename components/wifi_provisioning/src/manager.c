f06d1cdc75318c622b109ebd2d273550dc468de7
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2021-05-31 14:56:57 +0530

    wifi_provisioning: Add check for WIFI_REASON_MIC_FAILURE error code in wifi_prov_mgr_event_handler_internal

--- components/wifi_provisioning/src/manager.c
+++ components/wifi_provisioning/src/manager.c
@@ -871,6 +871,7 @@ static void wifi_prov_mgr_event_handler_internal(
         case WIFI_REASON_AUTH_FAIL:
         case WIFI_REASON_ASSOC_EXPIRE:
         case WIFI_REASON_HANDSHAKE_TIMEOUT:
+        case WIFI_REASON_MIC_FAILURE:
             ESP_LOGE(TAG, "STA Auth Error");
             prov_ctx->wifi_disconnect_reason = WIFI_PROV_STA_AUTH_ERROR;
             break;
