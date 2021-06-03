325deed20805140486a4d09389a585447f744256
Jan Brudný <jan.brudny@espressif.com>
2021-06-03 13:40:09 +0200

    esp32: update copyright notice

--- components/esp32/test/test_4mpsram.c
+++ components/esp32/test/test_4mpsram.c
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include "esp_heap_caps.h"
 #include "unity.h"
 #include "esp_log.h"
