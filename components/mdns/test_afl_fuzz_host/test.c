espressif/esp-idf

commit 7d762451731cb305c3b090509827740f0195a496

mdns: Minor correction of the test code

components/mdns/test_afl_fuzz_host/test.c
@@ -204,9 +204,7 @@ int main(int argc, char** argv)
         //
         // Note: parameter1 is a file (mangled packet) which caused the crash
         file = fopen(argv[1], "r");
-        if (file) {
-            len = fread(buf, 1, 1460, file);
-        }
+        assert(file >= 0 );
         fclose(file);
     }
 

commit dae803335e6bc6d9751a360cd3f675ce4027853b

mdns/fuzzer: Fix non-instrumentation test to reproduce fuzzer issues
Regression from 7d76245173 skipped reading the packet causing issues when locally reproducing crashed found by the fuzzer

components/mdns/test_afl_fuzz_host/test.c
@@ -205,6 +205,7 @@ int main(int argc, char** argv)
         // Note: parameter1 is a file (mangled packet) which caused the crash
         file = fopen(argv[1], "r");
         assert(file >= 0 );
+        len = fread(buf, 1, 1460, file);
         fclose(file);
     }
 