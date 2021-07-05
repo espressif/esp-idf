eebf301acc5e32b10aa0c1bf74f84a90d1a96849
Jakob Hasse <jakob.hasse@espressif.com>
2021-07-05 19:10:59 +0800

    [cxx]: fixed I2C master timeout

--- examples/cxx/experimental/experimental_cpp_component/include/i2c_cxx.hpp
+++ examples/cxx/experimental/experimental_cpp_component/include/i2c_cxx.hpp
@@ -475,7 +475,7 @@ TReturn I2CTransfer<TReturn>::do_transfer(i2c_port_t i2c_num, uint8_t i2c_addr)
 
     CHECK_THROW_SPECIFIC(i2c_master_stop(cmd_link.handle), I2CException);
 
-    CHECK_THROW_SPECIFIC(i2c_master_cmd_begin(i2c_num, cmd_link.handle, 1000 / portTICK_RATE_MS), I2CTransferException);
+    CHECK_THROW_SPECIFIC(i2c_master_cmd_begin(i2c_num, cmd_link.handle, driver_timeout / portTICK_RATE_MS), I2CTransferException);
 
     return process_result();
 }
