e1af4a398bc594f30ebfb06e6ba90b1493ec2c94
Yuan Hong Hui <yuanhonghui@espressif.com>
2021-07-06 14:06:43 +0800

    update ble mesh console files

--- examples/bluetooth/esp_ble_mesh/ble_mesh_console/main/register_bluetooth.c
+++ examples/bluetooth/esp_ble_mesh/ble_mesh_console/main/register_bluetooth.c
@@ -11,9 +11,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include "string.h"
 
-#include "esp_bt_device.h"
 #include "esp_console.h"
+
+#ifdef CONFIG_BT_BLUEDROID_ENABLED
+#include "esp_bt_device.h"
+#endif
+
+#ifdef CONFIG_BT_NIMBLE_ENABLED
+#include "host/ble_hs.h"
+#endif
 
 #define MAC2STR(a) (a)[0], (a)[1], (a)[2], (a)[3], (a)[4], (a)[5]
 #define MACSTR "%02x:%02x:%02x:%02x:%02x:%02x"
@@ -25,9 +33,45 @@ void register_bluetooth(void)
     register_ble_address();
 }
 
+int get_bd_addr(uint8_t addr[6])
+{
+    if (addr) {
+#ifdef CONFIG_BT_BLUEDROID_ENABLED
+        memcpy(addr, esp_bt_dev_get_address(), 6);
+        return 0;
+#endif
+
+#ifdef CONFIG_BT_NIMBLE_ENABLED
+        uint8_t own_addr_type = 0;
+        uint8_t mac[6] = {0};
+        int rc = 0;
+
+        rc = ble_hs_id_infer_auto(0, &own_addr_type);
+        if (rc != 0) {
+            printf("error determining address type; rc=%d\n", rc);
+            return rc;
+        }
+
+        ble_hs_id_copy_addr(own_addr_type, mac, NULL);
+
+        uint8_t *src = addr;
+        uint8_t *dst = mac + 5;
+        for (uint8_t length = 6; length > 0; length--) {
+            *src++ = *dst--;
+        }
+        return 0;
+#endif
+    }
+
+    return -1;
+}
+
 int bt_mac(int argc, char** argv)
 {
-    const uint8_t *mac = esp_bt_dev_get_address();
+    uint8_t mac[6] = {0};
+
+    get_bd_addr(mac);
+
     printf("+BTMAC:"MACSTR"\n", MAC2STR(mac));
     return 0;
 }
