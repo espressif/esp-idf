espressif/esp-idf

commit 27101f94546bf054ae022fcb9c3acb673de0e270

wpa_supplicant: Add initial roaming support
This commit adds different features from 802.11k and 802.11v
specifications to make the device ready for network assisted
roaming. It also adds initial framework for device to detect
whether it needs to move to a better AP.

Followings are added as part of this.

1. Support for sending neighbor report request and provide
   the report back to the APP.
2. Support for beacon measurement report.
3. Support for link measurement report.
4. Support for sending bss transition management query frame
   (triggered by the APP).
5. Support for bss transition management request and move
   to the candidate based on that.
6. Sending the bss transition management response.

components/wpa_supplicant/src/esp_supplicant/esp_scan.c
@@ -1,0 +1,271 @@
+/**
+ * Copyright 2020 Espressif Systems (Shanghai) PTE LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "utils/includes.h"
+
+#include "utils/common.h"
+#include "common/ieee802_11_defs.h"
+#include "common/wpa_supplicant_i.h"
+#include "utils/wpa_debug.h"
+#include "esp_wifi_driver.h"
+#include "esp_wifi_types.h"
+#include "drivers/driver.h"
+#include "common/scan.h"
+#include "common/bss.h"
+#include "common/rrm.h"
+#include "common/ieee802_11_common.h"
+#include "esp_supplicant/esp_common_i.h"
+#include "common/wnm_sta.h"
+
+extern struct wpa_supplicant g_wpa_supp;
+
+static void esp_scan_done_event_handler(void* arg, esp_event_base_t event_base,
+					int event_id, void* event_data)
+{
+	struct wpa_supplicant *wpa_s = &g_wpa_supp;
+	if (!wpa_s->scanning) {
+		/* update last scan time */
+		wpa_s->scan_start_tsf = esp_wifi_get_tsf_time(ESP_IF_WIFI_STA);
+		wpa_printf(MSG_DEBUG, "scan not triggered by supplicant, ignore");
+		return;
+	}
+	wpa_s->type &= ~(1 << WLAN_FC_STYPE_BEACON) & ~(1 << WLAN_FC_STYPE_PROBE_RESP);
+	esp_wifi_register_mgmt_frame_internal(wpa_s->type, wpa_s->subtype);
+	esp_supplicant_post_evt(SIG_SUPPLICANT_SCAN_DONE, 0);
+
+}
+
+static void esp_supp_handle_wnm_scan_done(struct wpa_supplicant *wpa_s)
+{
+	struct wpa_bss *bss = wpa_bss_get_next_bss(wpa_s, wpa_s->current_bss);
+
+	if (wpa_s->wnm_neighbor_report_elements) {
+		wnm_scan_process(wpa_s, 1);
+	} else if (wpa_s->wnm_dissoc_timer) {
+		if (wpa_s->num_bss == 1) {
+			wpa_printf(MSG_INFO, "not able to find another candidate, do nothing");
+			return;
+		}
+		/* this is a already matched bss */
+		if (bss) {
+			wnm_bss_tm_connect(wpa_s, bss, NULL, 1);
+		}
+	}
+}
+
+static void esp_supp_scan_done_cleanup(struct wpa_supplicant *wpa_s)
+{
+	uint16_t number = 1;
+	wifi_ap_record_t ap_records;
+
+	wpa_s->scanning = 0;
+	wpa_s->scan_reason = 0;
+	/* clean scan list from net80211 */
+	esp_wifi_scan_get_ap_records(&number, &ap_records);
+}
+
+void esp_supplicant_handle_scan_done_evt(void)
+{
+	struct wpa_supplicant *wpa_s = &g_wpa_supp;
+
+	wpa_printf(MSG_INFO, "scan done received");
+	/* Check which module started this, call the respective function */
+	if (wpa_s->scan_reason == REASON_RRM_BEACON_REPORT) {
+		wpas_beacon_rep_scan_process(wpa_s, wpa_s->scan_start_tsf);
+	} else if (wpa_s->scan_reason == REASON_WNM_BSS_TRANS_REQ) {
+		esp_supp_handle_wnm_scan_done(wpa_s);
+	}
+	esp_supp_scan_done_cleanup(wpa_s);
+	wpa_bss_update_end(wpa_s);
+#ifndef SCAN_CACHE_SUPPORTED
+	wpa_bss_flush(wpa_s);
+#endif
+}
+
+void esp_scan_init(struct wpa_supplicant *wpa_s)
+{
+	wpa_s->scanning = 0;
+	wpa_bss_init(wpa_s);
+	wpa_s->last_scan_res = NULL;
+	esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_SCAN_DONE,
+			&esp_scan_done_event_handler, NULL);
+}
+
+void esp_scan_deinit(struct wpa_supplicant *wpa_s)
+{
+	wpa_bss_deinit(wpa_s);
+}
+
+int esp_handle_beacon_probe(u8 type, u8 *frame, size_t len, u8 *sender,
+			    u32 rssi, u8 channel, u64 current_tsf)
+{
+	struct wpa_supplicant *wpa_s = &g_wpa_supp;
+	struct os_reltime now;
+	struct wpa_scan_res *res;
+	u8 *ptr;
+
+	if (len < 12) {
+		wpa_printf(MSG_ERROR, "beacon/probe is having short len=%d\n", len);
+		return -1;
+	}
+
+	res = os_zalloc(sizeof(struct wpa_scan_res) + len - 12);
+	if (!res) {
+		wpa_printf(MSG_ERROR, "failed to allocate memory");
+		return -1;
+	}
+
+	ptr = (u8 *)res;
+	os_get_time(&now);
+	os_memcpy(res->bssid, sender, ETH_ALEN);
+	res->tsf = WPA_GET_LE64(frame);
+	frame += 8;
+	len -= 8;
+
+	if ((wpa_s->scan_start_tsf == 0) &&
+	    wpa_s->current_bss &&
+	    (os_memcmp(wpa_s->current_bss, sender, ETH_ALEN) == 0)) {
+		wpa_s->scan_start_tsf = res->tsf;
+		os_memcpy(wpa_s->tsf_bssid, sender, ETH_ALEN);
+	}
+	res->beacon_int = WPA_GET_LE16(frame);
+
+	frame += 2;
+	len -= 2;
+	res->caps = WPA_GET_LE16(frame);
+	frame += 2;
+	len -= 2;
+
+	res->chan = channel;
+	res->noise = 0;
+	res->level = rssi;
+	os_memcpy(res->tsf_bssid, wpa_s->tsf_bssid, ETH_ALEN);
+	res->parent_tsf = current_tsf - wpa_s->scan_start_tsf;
+	if (type == WLAN_FC_STYPE_PROBE_RESP)
+		res->ie_len = len;
+	else if (type == WLAN_FC_STYPE_BEACON)
+		res->beacon_ie_len = len;
+
+	ptr += sizeof(struct wpa_scan_res);
+
+	/* update rest of the frame */
+	os_memcpy(ptr, frame, len);
+	wpa_bss_update_scan_res(wpa_s, res, &now);
+	os_free(res);
+
+	return 0;
+}
+
+static int esp_issue_scan(struct wpa_supplicant *wpa_s,
+			  struct wpa_driver_scan_params *scan_params)
+{
+	wifi_scan_config_t *params = NULL;
+	int ret = 0;
+	u64 scan_start_tsf = esp_wifi_get_tsf_time(ESP_IF_WIFI_STA);
+
+	/* TODO: Directly try to connect if scan results are recent */
+	if ((scan_start_tsf -  wpa_s->scan_start_tsf) > 100000) {
+		wpa_printf(MSG_DEBUG, "flushing old scan cache %llu",
+				(scan_start_tsf -  wpa_s->scan_start_tsf));
+		wpa_bss_flush(wpa_s);
+	}
+
+	esp_wifi_get_macaddr_internal(ESP_IF_WIFI_STA, wpa_s->tsf_bssid);
+
+	if (scan_params) {
+		params = os_zalloc(sizeof(wifi_scan_config_t));
+		if (!params) {
+			wpa_printf(MSG_ERROR, "failed to allocate memory");
+			return -1;
+		}
+		if (scan_params->num_ssids) {
+			params->ssid = os_zalloc(scan_params->ssids[0].ssid_len + 1);
+			if (!params->ssid) {
+				wpa_printf(MSG_ERROR, "failed to allocate memory");
+				return -1;
+			}
+			os_memcpy(params->ssid, scan_params->ssids[0].ssid, scan_params->ssids[0].ssid_len);
+			params->scan_type = WIFI_SCAN_TYPE_ACTIVE;
+		} else
+			params->scan_type = WIFI_SCAN_TYPE_PASSIVE;
+
+		if (scan_params->bssid) {
+			params->bssid = os_zalloc(ETH_ALEN);
+			if (!params->bssid) {
+				wpa_printf(MSG_ERROR, "failed to allocate memory");
+				return -1;
+			}
+			os_memcpy(params->bssid, scan_params->bssid, ETH_ALEN);
+		}
+		if (scan_params->channel) {
+			params->channel = scan_params->channel;
+		}
+
+		if (scan_params->duration) {
+			params->scan_time.passive = scan_params->duration;
+			params->scan_time.active.min = scan_params->duration;
+			params->scan_time.active.max = scan_params->duration;
+		}
+	}
+
+	wpa_s->scan_start_tsf = scan_start_tsf;
+	/* Register frames to come to supplicant when we park on channel */
+	wpa_s->type |= (1 << WLAN_FC_STYPE_BEACON) | (1 << WLAN_FC_STYPE_PROBE_RESP);
+	esp_wifi_register_mgmt_frame_internal(wpa_s->type, wpa_s->subtype);
+
+	/* issue scan */
+	if (esp_wifi_scan_start(params, false) < 0) {
+		ret = -1;
+		goto cleanup;
+	}
+	wpa_s->scanning = 1;
+	wpa_bss_update_start(wpa_s);
+	wpa_printf(MSG_INFO, "scan issued at time=%llu", wpa_s->scan_start_tsf);
+
+cleanup:
+	if (params->ssid)
+		os_free(params->ssid);
+	if (params->bssid)
+		os_free(params->bssid);
+	os_free(params);
+
+	return ret;
+}
+
+/**
+ * wpa_supplicant_trigger_scan - Request driver to start a scan
+ * @wpa_s: Pointer to wpa_supplicant data
+ * @params: Scan parameters
+ * Returns: 0 on success, -1 on failure
+ */
+int wpa_supplicant_trigger_scan(struct wpa_supplicant *wpa_s,
+				struct wpa_driver_scan_params *params)
+{
+	return esp_issue_scan(wpa_s, params);
+}
+
+void wpa_scan_results_free(struct wpa_scan_results *res)
+{
+	size_t i;
+
+	if (res == NULL)
+		return;
+
+	for (i = 0; i < res->num; i++)
+		os_free(res->res[i]);
+	os_free(res->res);
+	os_free(res);
+}

components/esp_event/event_send.c
@@ -52,6 +52,9 @@ static system_event_id_t esp_event_legacy_wifi_event_id(int32_t event_id)
     case WIFI_EVENT_STA_AUTHMODE_CHANGE:
         return SYSTEM_EVENT_STA_AUTHMODE_CHANGE;
 
+    case WIFI_EVENT_STA_BSS_RSSI_LOW:
+	return SYSTEM_EVENT_STA_BSS_RSSI_LOW;
+
     case WIFI_EVENT_STA_WPS_ER_SUCCESS:
         return SYSTEM_EVENT_STA_WPS_ER_SUCCESS;
 

components/esp_event/include/esp_event_legacy.h
@@ -36,6 +36,7 @@ typedef enum {
     SYSTEM_EVENT_STA_AUTHMODE_CHANGE,      /*!< the auth mode of AP connected by ESP32 station changed */
     SYSTEM_EVENT_STA_GOT_IP,               /*!< ESP32 station got IP from connected AP */
     SYSTEM_EVENT_STA_LOST_IP,              /*!< ESP32 station lost IP and the IP is reset to 0 */
+    SYSTEM_EVENT_STA_BSS_RSSI_LOW,         /*!< ESP32 station connected BSS rssi goes below threshold */
     SYSTEM_EVENT_STA_WPS_ER_SUCCESS,       /*!< ESP32 station wps succeeds in enrollee mode */
     SYSTEM_EVENT_STA_WPS_ER_FAILED,        /*!< ESP32 station wps fails in enrollee mode */
     SYSTEM_EVENT_STA_WPS_ER_TIMEOUT,       /*!< ESP32 station wps timeout in enrollee mode */

components/esp_wifi/include/esp_wifi.h
@@ -229,7 +229,7 @@ extern uint64_t g_wifi_feature_caps;
   * @attention 2. Always use WIFI_INIT_CONFIG_DEFAULT macro to init the config to default values, this can
   *               guarantee all the fields got correct value when more fields are added into wifi_init_config_t
   *               in future release. If you want to set your owner initial values, overwrite the default values
-  *               which are set by WIFI_INIT_CONFIG_DEFAULT, please be notified that the field 'magic' of 
+  *               which are set by WIFI_INIT_CONFIG_DEFAULT, please be notified that the field 'magic' of
   *               wifi_init_config_t should always be WIFI_INIT_CONFIG_MAGIC!
   *
   * @param  config pointer to WiFi init configuration structure; can point to a temporary variable.
@@ -247,7 +247,7 @@ esp_err_t esp_wifi_init(const wifi_init_config_t *config);
   *
   * @attention 1. This API should be called if you want to remove WiFi driver from the system
   *
-  * @return 
+  * @return
   *    - ESP_OK: succeed
   *    - ESP_ERR_WIFI_NOT_INIT: WiFi is not initialized by esp_wifi_init
   */
@@ -332,11 +332,11 @@ esp_err_t esp_wifi_restore(void);
   * @attention 3. The scanning triggered by esp_wifi_start_scan() will not be effective until connection between ESP32 and the AP is established.
   *               If ESP32 is scanning and connecting at the same time, ESP32 will abort scanning and return a warning message and error
   *               number ESP_ERR_WIFI_STATE.
-  *               If you want to do reconnection after ESP32 received disconnect event, remember to add the maximum retry time, otherwise the called	
+  *               If you want to do reconnection after ESP32 received disconnect event, remember to add the maximum retry time, otherwise the called
   *               scan will not work. This is especially true when the AP doesn't exist, and you still try reconnection after ESP32 received disconnect
   *               event with the reason code WIFI_REASON_NO_AP_FOUND.
-  *   
-  * @return 
+  *
+  * @return
   *    - ESP_OK: succeed
   *    - ESP_ERR_WIFI_NOT_INIT: WiFi is not initialized by esp_wifi_init
   *    - ESP_ERR_WIFI_NOT_STARTED: WiFi is not started by esp_wifi_start
@@ -431,7 +431,7 @@ esp_err_t esp_wifi_scan_get_ap_num(uint16_t *number);
 /**
   * @brief     Get AP list found in last scan
   *
-  * @param[inout]  number As input param, it stores max AP number ap_records can hold. 
+  * @param[inout]  number As input param, it stores max AP number ap_records can hold.
   *                As output param, it receives the actual AP number this API returns.
   * @param         ap_records  wifi_ap_record_t array to hold the found APs
   *
@@ -456,7 +456,7 @@ esp_err_t esp_wifi_scan_get_ap_records(uint16_t *number, wifi_ap_record_t *ap_re
   * @return
   *    - ESP_OK: succeed
   *    - ESP_ERR_WIFI_CONN: The station interface don't initialized
-  *    - ESP_ERR_WIFI_NOT_CONNECT: The station is in disconnect status 
+  *    - ESP_ERR_WIFI_NOT_CONNECT: The station is in disconnect status
   */
 esp_err_t esp_wifi_sta_get_ap_info(wifi_ap_record_t *ap_info);
 
@@ -554,7 +554,7 @@ esp_err_t esp_wifi_get_bandwidth(wifi_interface_t ifx, wifi_bandwidth_t *bw);
   * @attention 1. This API should be called after esp_wifi_start()
   * @attention 2. When ESP32 is in STA mode, this API should not be called when STA is scanning or connecting to an external AP
   * @attention 3. When ESP32 is in softAP mode, this API should not be called when softAP has connected to external STAs
-  * @attention 4. When ESP32 is in STA+softAP mode, this API should not be called when in the scenarios described above  
+  * @attention 4. When ESP32 is in STA+softAP mode, this API should not be called when in the scenarios described above
   *
   * @param     primary  for HT20, primary is the channel number, for HT40, primary is the primary channel
   * @param     second   for HT20, second is ignored, for HT40, second is the second channel
@@ -596,7 +596,7 @@ esp_err_t esp_wifi_get_channel(uint8_t *primary, wifi_second_chan_t *second);
   * @attention 4. When the country info is changed because of configuration or because the station connects to a different
   *               external AP, the country IE in probe response/beacon of the soft-AP is changed also.
   * @attention 5. The country configuration is stored into flash.
-  * @attention 6. This API doesn't validate the per-country rules, it's up to the user to fill in all fields according to 
+  * @attention 6. This API doesn't validate the per-country rules, it's up to the user to fill in all fields according to
   *               local regulations.
   * @attention 7. When this API is called, the PHY init data will switch to the PHY init data type corresponding to the
   *               country info.
@@ -660,7 +660,7 @@ esp_err_t esp_wifi_set_mac(wifi_interface_t ifx, const uint8_t mac[6]);
 esp_err_t esp_wifi_get_mac(wifi_interface_t ifx, uint8_t mac[6]);
 
 /**
-  * @brief The RX callback function in the promiscuous mode. 
+  * @brief The RX callback function in the promiscuous mode.
   *        Each time a packet is received, the callback function will be called.
   *
   * @param buf  Data received. Type of data in buffer (wifi_promiscuous_pkt_t or wifi_pkt_rx_ctrl_t) indicated by 'type' parameter.
@@ -972,14 +972,14 @@ esp_err_t esp_wifi_get_event_mask(uint32_t *mask);
   *
   * @attention Currently only support for sending beacon/probe request/probe response/action and non-QoS
   *            data frame
-  * 
+  *
   * @param     ifx interface if the Wi-Fi mode is Station, the ifx should be WIFI_IF_STA. If the Wi-Fi
-  *            mode is SoftAP, the ifx should be WIFI_IF_AP. If the Wi-Fi mode is Station+SoftAP, the 
+  *            mode is SoftAP, the ifx should be WIFI_IF_AP. If the Wi-Fi mode is Station+SoftAP, the
   *            ifx should be WIFI_IF_STA or WIFI_IF_AP. If the ifx is wrong, the API returns ESP_ERR_WIFI_IF.
   * @param     buffer raw ieee80211 buffer
   * @param     len the length of raw buffer, the len must be <= 1500 Bytes and >= 24 Bytes
-  * @param     en_sys_seq indicate whether use the internal sequence number. If en_sys_seq is false, the 
-  *            sequence in raw buffer is unchanged, otherwise it will be overwritten by WiFi driver with 
+  * @param     en_sys_seq indicate whether use the internal sequence number. If en_sys_seq is false, the
+  *            sequence in raw buffer is unchanged, otherwise it will be overwritten by WiFi driver with
   *            the system sequence number.
   *            Generally, if esp_wifi_80211_tx is called before the Wi-Fi connection has been set up, both
   *            en_sys_seq==true and en_sys_seq==false are fine. However, if the API is called after the Wi-Fi
@@ -995,12 +995,12 @@ esp_err_t esp_wifi_get_event_mask(uint32_t *mask);
 esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
 
 /**
-  * @brief The RX callback function of Channel State Information(CSI)  data. 
+  * @brief The RX callback function of Channel State Information(CSI)  data.
   *
   *        Each time a CSI data is received, the callback function will be called.
   *
-  * @param ctx context argument, passed to esp_wifi_set_csi_rx_cb() when registering callback function. 
-  * @param data CSI data received. The memory that it points to will be deallocated after callback function returns. 
+  * @param ctx context argument, passed to esp_wifi_set_csi_rx_cb() when registering callback function.
+  * @param data CSI data received. The memory that it points to will be deallocated after callback function returns.
   *
   */
 typedef void (* wifi_csi_cb_t)(void *ctx, wifi_csi_info_t *data);
@@ -1025,7 +1025,7 @@ esp_err_t esp_wifi_set_csi_rx_cb(wifi_csi_cb_t cb, void *ctx);
   * @brief Set CSI data configuration
   *
   * @param config configuration
-  * 
+  *
   * return
   *    - ESP_OK: succeed
   *    - ESP_ERR_WIFI_NOT_INIT: WiFi is not initialized by esp_wifi_init
@@ -1152,6 +1152,20 @@ esp_err_t esp_wifi_get_inactive_time(wifi_interface_t ifx, uint16_t *sec);
   *    - others: failed
   */
 esp_err_t esp_wifi_statis_dump(uint32_t modules);
+
+/**
+  * @brief      Set RSSI threshold below which APP will get an event
+  *
+  * @attention  This API needs to be called every time after WIFI_EVENT_STA_BSS_RSSI_LOW event is received.
+  *
+  * @param      rssi threshold value in dbm between -100 to 0
+  *
+  * @return
+  *    - ESP_OK: succeed
+  *    - ESP_ERR_WIFI_NOT_INIT: WiFi is not initialized by esp_wifi_init
+  *    - ESP_ERR_WIFI_ARG: invalid argument
+  */
+esp_err_t esp_wifi_set_rssi_threshold(int32_t rssi);
 
 #ifdef __cplusplus
 }

components/esp_wifi/include/esp_wifi_types.h
@@ -95,6 +95,7 @@ typedef enum {
     WIFI_REASON_HANDSHAKE_TIMEOUT        = 204,
     WIFI_REASON_CONNECTION_FAIL          = 205,
     WIFI_REASON_AP_TSF_RESET             = 206,
+    WIFI_REASON_ROAMING                  = 207,
 } wifi_err_reason_t;
 
 typedef enum {
@@ -235,6 +236,9 @@ typedef struct {
     wifi_sort_method_t sort_method;    /**< sort the connect AP in the list by rssi or security mode */
     wifi_scan_threshold_t  threshold;     /**< When sort_method is set, only APs which have an auth mode that is more secure than the selected auth mode and a signal stronger than the minimum RSSI will be used. */
     wifi_pmf_config_t pmf_cfg;    /**< Configuration for Protected Management Frame. Will be advertized in RSN Capabilities in RSN IE. */
+    uint32_t rm_enabled:1;        /**< Whether Radio Measurements are enabled for the connection */
+    uint32_t btm_enabled:1;       /**< Whether BSS Transition Management is enabled for the connection */
+    uint32_t reserved:30;         /**< Reserved for future feature set */
 } wifi_sta_config_t;
 
 /** @brief Configuration data for ESP32 AP or STA.
@@ -541,6 +545,9 @@ typedef enum {
 
     WIFI_EVENT_FTM_REPORT,               /**< Receive report of FTM procedure */
 
+    /* Add next events after this only */
+    WIFI_EVENT_STA_BSS_RSSI_LOW,         /**< AP's RSSI crossed configured threshold */
+
     WIFI_EVENT_MAX,                      /**< Invalid WiFi event ID */
 } wifi_event_t;
 
@@ -621,6 +628,11 @@ typedef struct {
     int rssi;                 /**< Received probe request signal strength */
     uint8_t mac[6];           /**< MAC address of the station which send probe request */
 } wifi_event_ap_probe_req_rx_t;
+
+/** Argument structure for WIFI_EVENT_STA_BSS_RSSI_LOW event */
+typedef struct {
+    int32_t rssi;                 /**< RSSI value of bss */
+} wifi_event_bss_rssi_low_t;
 
 #define WIFI_STATIS_BUFFER    (1<<0)
 #define WIFI_STATIS_RXTX      (1<<1)

components/wpa_supplicant/CMakeLists.txt
@@ -6,6 +6,7 @@ set(srcs "port/os_xtensa.c"
     "src/common/sae.c"
     "src/common/wpa_common.c"
     "src/common/dpp.c"
+    "src/utils/bitfield.c"
     "src/crypto/aes-ctr.c"
     "src/crypto/aes-siv.c"
     "src/crypto/sha256-kdf.c"
@@ -106,7 +107,21 @@ else()
     )
 endif()
 
-idf_component_register(SRCS "${srcs}" "${tls_src}"
+if(CONFIG_WPA_11KV_SUPPORT)
+    set(roaming_src
+    "src/common/rrm.c"
+    "src/common/wnm_sta.c"
+    "src/common/bss.c"
+    "src/common/scan.c"
+    "src/common/ieee802_11_common.c"
+    "src/esp_supplicant/esp_common.c"
+    "src/esp_supplicant/esp_scan.c"
+    )
+else()
+    set(roaming_src "")
+endif()
+
+idf_component_register(SRCS "${srcs}" "${tls_src}" "${roaming_src}"
                     INCLUDE_DIRS include port/include include/esp_supplicant
                     PRIV_INCLUDE_DIRS src
                     PRIV_REQUIRES mbedtls esp_timer)
@@ -132,6 +147,7 @@ target_compile_definitions(${COMPONENT_LIB} PRIVATE
     CONFIG_WPA3_SAE
     CONFIG_SHA256
     CONFIG_DPP
+    CONFIG_WNM
     )
 
 set_property(TARGET ${COMPONENT_LIB} APPEND PROPERTY LINK_INTERFACE_MULTIPLICITY 3)

components/wpa_supplicant/Kconfig
@@ -42,4 +42,28 @@ menu "Supplicant"
             button bit without setting virtual/phycial display/button bit which
             will cause M2 validation fail, bypassing WPS-Config method validation.
 
+    menuconfig WPA_11KV_SUPPORT
+        bool "Enable 802.11k, 802.11v APIs handling in supplicant"
+        default n
+        help
+            Select this option to enable 802.11k 802.11v APIs(RRM and BTM support).
+            Only APIs which are helpful for network assisted roaming
+            are supported for now.
+            Enable this option with BTM and RRM enabled in sta config
+            to make device ready for network assisted roaming.
+            BTM: BSS transition management enables an AP to request a station to transition
+            to a specific AP, or to indicate to a station a set of preferred APs.
+            RRM: Radio measurements enable STAs to understand the radio environment,
+            it enables STAs to observe and gather data on radio link performance
+            and on the radio environment. Current implementation adds beacon report,
+            link measurement, neighbor report.
+
+    if WPA_11KV_SUPPORT
+        config WPA_SCAN_CACHE
+            bool "Keep scan results in cache"
+            default n
+            help
+                Keep scan results in cache, if not enabled, those
+                will be flushed immediately.
+    endif
 endmenu

components/wpa_supplicant/component.mk
@@ -2,10 +2,10 @@
 
 COMPONENT_PRIV_INCLUDEDIRS := src
 COMPONENT_SRCDIRS := port src/ap src/common src/crypto src/eap_peer src/rsn_supp src/tls src/utils src/esp_supplicant src/wps
-COMPONENT_ADD_INCLUDEDIRS := include port/include include/esp_supplicant
+COMPONENT_ADD_INCLUDEDIRS := include port/include include/esp_supplicant src/utils
 
 ifeq ($(CONFIG_WPA_MBEDTLS_CRYPTO), y)
-    COMPONENT_OBJEXCLUDE := src/tls/asn1.o \
+    COMPONENT_OBJEXCLUDE += src/tls/asn1.o \
     src/tls/bignum.o \
     src/tls/pkcs1.o \
     src/tls/pkcs5.o \
@@ -25,5 +25,14 @@ ifeq ($(CONFIG_WPA_MBEDTLS_CRYPTO), y)
 else
     COMPONENT_OBJEXCLUDE := src/crypto/tls_mbedtls.o
 endif
+ifneq ($(CONFIG_WPA_11KV_SUPPORT), y)
+    COMPONENT_OBJEXCLUDE += src/common/rrm.o \
+    src/common/wnm_sta.o \
+    src/common/bss.o \
+    src/common/scan.o \
+    src/common/ieee802_11_common.o \
+    src/esp_supplicant/esp_common.o \
+    src/esp_supplicant/esp_scan.o
+endif
 
-CFLAGS += -DCONFIG_DPP -DCONFIG_WPA3_SAE -DCONFIG_IEEE80211W -DESP_SUPPLICANT -DIEEE8021X_EAPOL -DEAP_PEER_METHOD -DEAP_TLS -DEAP_TTLS -DEAP_PEAP -DEAP_MSCHAPv2 -DUSE_WPA2_TASK -DCONFIG_WPS2 -DCONFIG_WPS_PIN -DUSE_WPS_TASK -DESPRESSIF_USE -DESP32_WORKAROUND -DCONFIG_ECC -D__ets__ -Wno-strict-aliasing
+CFLAGS += -DCONFIG_DPP -DCONFIG_WPA3_SAE -DCONFIG_IEEE80211W -DESP_SUPPLICANT -DIEEE8021X_EAPOL -DEAP_PEER_METHOD -DEAP_TLS -DEAP_TTLS -DEAP_PEAP -DEAP_MSCHAPv2 -DUSE_WPA2_TASK -DCONFIG_WPS2 -DCONFIG_WPS_PIN -DUSE_WPS_TASK -DESPRESSIF_USE -DESP32_WORKAROUND -DCONFIG_ECC -DCONFIG_WNM -D__ets__ -Wno-strict-aliasing

commit 204a32bece9d6c18a279adf141e028cb5e4e2498

wpa_supplicant: Fix memory leak in esp_issue_scan error paths
Fix memory leak when allocate memory for params->ssid / params->bssid fails.

Fixes: 27101f94546b ("wpa_supplicant: Add initial roaming support")
Signed-off-by: Axel Lin <axel.lin@gmail.com>

components/wpa_supplicant/src/esp_supplicant/esp_scan.c
@@ -195,7 +195,8 @@ static int esp_issue_scan(struct wpa_supplicant *wpa_s,
 			params->ssid = os_zalloc(scan_params->ssids[0].ssid_len + 1);
 			if (!params->ssid) {
 				wpa_printf(MSG_ERROR, "failed to allocate memory");
-				return -1;
+				ret = -1;
+				goto cleanup;
 			}
 			os_memcpy(params->ssid, scan_params->ssids[0].ssid, scan_params->ssids[0].ssid_len);
 			params->scan_type = WIFI_SCAN_TYPE_ACTIVE;
@@ -206,7 +207,8 @@ static int esp_issue_scan(struct wpa_supplicant *wpa_s,
 			params->bssid = os_zalloc(ETH_ALEN);
 			if (!params->bssid) {
 				wpa_printf(MSG_ERROR, "failed to allocate memory");
-				return -1;
+				ret = -1;
+				goto cleanup;
 			}
 			os_memcpy(params->bssid, scan_params->bssid, ETH_ALEN);
 		}