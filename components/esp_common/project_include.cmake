a7d5798b855128f54160916ead15b2062f821e17
David Cermak <cermak@espressif.com>
2021-07-08 18:11:33 +0200

    esp_common: Add support for linux target providing header only library

--- components/esp_common/project_include.cmake
+++ components/esp_common/project_include.cmake
@@ -1,6 +1,8 @@
 #
 # Warn if the toolchain version doesn't match
 #
-get_expected_ctng_version(expected_toolchain expected_gcc)
-gcc_version_check("${expected_gcc}")
-crosstool_version_check("${expected_toolchain}")
+if(NOT ${target} STREQUAL "linux")
+    get_expected_ctng_version(expected_toolchain expected_gcc)
+    gcc_version_check("${expected_gcc}")
+    crosstool_version_check("${expected_toolchain}")
+endif()
