d0d7fbf6b1d67086666e75a34fd6d060828a75d5
yuanjm <yuanjianmin@espressif.com>
2021-07-23 17:55:01 +0800

    eth2ap: Fix eth2ap example crash issue
    Closes https://github.com/espressif/esp-idf/issues/7260

--- examples/ethernet/eth2ap/main/ethernet_example_main.c
+++ examples/ethernet/eth2ap/main/ethernet_example_main.c
@@ -248,7 +248,6 @@ void app_main(void)
     ESP_ERROR_CHECK(ret);
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(initialize_flow_control());
-
-    initialize_ethernet();
     initialize_wifi();
+    initialize_ethernet();
 }
