75fd6d31059730245eb715c0fb597ace314b61e3
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2021-08-06 22:52:04 +0200

    twai: TWAI_ALERT_RX_DATA indicates that frames were received

--- components/driver/twai.c
+++ components/driver/twai.c
@@ -130,6 +130,7 @@ static inline void twai_handle_rx_buffer_frames(BaseType_t *task_woken, int *ale
             //Valid frame copied from RX buffer
             if (xQueueSendFromISR(p_twai_obj->rx_queue, &frame, task_woken) == pdTRUE) {
                 p_twai_obj->rx_msg_count++;
+                twai_alert_handler(TWAI_ALERT_RX_DATA, alert_req);
             } else {    //Failed to send to queue
                 p_twai_obj->rx_missed_count++;
                 twai_alert_handler(TWAI_ALERT_RX_QUEUE_FULL, alert_req);
