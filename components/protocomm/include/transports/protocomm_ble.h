09800028e1b224f4f64180b91a38648660c7ed7c
isha pardikar <isha.pardikar@espressif.com>
2021-09-08 13:11:33 +0530

    Wifi_prov_mgr:Fix for ios device needs to do forget device

--- components/protocomm/include/transports/protocomm_ble.h
+++ components/protocomm/include/transports/protocomm_ble.h
@@ -1,16 +1,8 @@
-// Copyright 2018 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2018-2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #pragma once
 
@@ -86,6 +78,10 @@ typedef struct protocomm_ble_config {
      * Pointer to the Name-UUID lookup table
      */
     protocomm_ble_name_uuid_t *nu_lookup;
+
+    /* BLE bonding */
+    unsigned ble_bonding:1;
+
 } protocomm_ble_config_t;
 
 /**
