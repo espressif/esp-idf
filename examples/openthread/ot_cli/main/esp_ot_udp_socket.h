c09633d41ba7d5c2984afdce69a7c48ab99afd0f
Shu Chen <chenshu@espressif.com>
2021-09-09 21:08:57 +0800

    openthread: enrich documentation for openthread examples

--- examples/openthread/ot_cli/main/esp_ot_udp_socket.h
+++ examples/openthread/ot_cli/main/esp_ot_udp_socket.h
@@ -1,7 +1,10 @@
-/*
- * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
+/* OpenThread Command Line Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
 */
 
 #pragma once
@@ -17,7 +20,7 @@ extern "C" {
 void esp_ot_process_udp_server(void *aContext, uint8_t aArgsLength, char *aArgs[]);
 
 /**
- * @brief Yser command "udpsockserver" process.
+ * @brief User command "udpsockclient" process.
  *
  */
 void esp_ot_process_udp_client(void *aContext, uint8_t aArgsLength, char *aArgs[]);
