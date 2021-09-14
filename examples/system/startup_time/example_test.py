8a2c9cf47fdf45970861d16f076a50cae6d4eee6
Aleksei Apaseev <aleksei.apaseev@espressif.com>
2021-09-14 10:22:49 +0800

    feat: Added tracking startup_time example startup times

--- examples/system/startup_time/example_test.py
+++ examples/system/startup_time/example_test.py
@@ -3,27 +3,19 @@ from __future__ import print_function
 import re
 
 import ttfw_idf
+from tiny_test_fw import TinyFW
 
 
 @ttfw_idf.idf_example_test(env_tag='Example_GENERIC', target=['esp32', 'esp32s2', 'esp32c3'])
-def test_startup_time_example(env, extra_data):
-    app_name = 'startup_time'
-    dut = env.get_dut(app_name, 'examples/system/startup_time')
+def test_startup_time_example(env, _):
+    key = 'startup_time'
+    dut = env.get_dut(key, 'examples/system/startup_time')
     dut.start_app()
 
     res = dut.expect(re.compile(r'\((\d+)\) [^:]+: App started!'))
     time = int(res[0])
 
-    # Allow ci-dashboard to track startup times
-    print('------ startup time info ------\n'
-          '[app_name] {}\n'
-          '[startup_time] {}\n'
-          '[config] {}\n'
-          '[target] {}\n'
-          '------ startup time end ------'.format(app_name,
-                                                  time,
-                                                  dut.app.config_name,
-                                                  dut.TARGET))
+    TinyFW.JunitReport.update_performance([(key, time)])
 
 
 if __name__ == '__main__':
