44da7d27ef61033cc7c31c8a8e0fde79fd5ac000
Li Shuai <lishuai@espressif.com>
2021-09-27 21:31:37 +0800

    heap: add a new heap caps attribute for RTC fast memory

--- components/heap/include/esp_heap_caps.h
+++ components/heap/include/esp_heap_caps.h
@@ -41,6 +41,7 @@ extern "C" {
 #define MALLOC_CAP_DEFAULT          (1<<12) ///< Memory can be returned in a non-capability-specific memory allocation (e.g. malloc(), calloc()) call
 #define MALLOC_CAP_IRAM_8BIT        (1<<13) ///< Memory must be in IRAM and allow unaligned access
 #define MALLOC_CAP_RETENTION        (1<<14)
+#define MALLOC_CAP_RTCRAM           (1<<15) ///< Memory must be in RTC fast memory
 
 #define MALLOC_CAP_INVALID          (1<<31) ///< Memory can't be used / list end marker
 
