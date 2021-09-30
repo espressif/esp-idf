03b7ffdf4af61101fa88c4b06f466a2451446fcf
Li Jingyi <lijingyi@espressif.com>
2021-09-30 15:23:32 +0800

    feat(mbedtls): modify __wrap_mbedtls_ssl_setup to decrease SSL peak heap cost

--- components/mbedtls/port/dynamic/esp_mbedtls_dynamic_impl.h
+++ components/mbedtls/port/dynamic/esp_mbedtls_dynamic_impl.h
@@ -1,20 +1,14 @@
-// Copyright 2020 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+/*
+ * SPDX-FileCopyrightText: 2020-2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #ifndef _DYNAMIC_IMPL_H_
 #define _DYNAMIC_IMPL_H_
 
 #include <stddef.h>
+#include <string.h>
 #include "mbedtls/ssl.h"
 #include "mbedtls/ssl_internal.h"
 #include "mbedtls/platform.h"
