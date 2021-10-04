dffb92e45f69d590e16848c600e40efa40dc7eb1
Martin Vychodil <martin.vychodil@espressif.com>
2021-10-04 22:01:50 +0200

    System/Security: wrong check of the Memprot feature in esp_restart()/panic_restart()
    esp_restart()/panic_restart() never resets the Digital system (so far required only by the Memprot feature) as there's a typo in the corresponding #define:
    it checks CONFIG_ESP_SYSTEM_CONFIG_MEMPROT_FEATURE instead of CONFIG_ESP_SYSTEM_MEMPROT_FEATURE.
    Issue fixed.
    
    IDF-4094

--- components/esp_system/port/panic_handler.c
+++ components/esp_system/port/panic_handler.c
@@ -1,16 +1,9 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+/*
+ * SPDX-FileCopyrightText: 2015-2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 #include <stdlib.h>
 
 #include "esp_spi_flash.h"
@@ -199,7 +192,8 @@ static void panic_handler(void *frame, bool pseudo_excause)
  * This function must always be in IRAM as it is required to
  * re-enable the flash cache.
  */
-static void IRAM_ATTR panic_enable_cache(void) {
+static void IRAM_ATTR panic_enable_cache(void)
+{
     int core_id = cpu_hal_get_core_id();
 
     if (!spi_flash_cache_enabled()) {
@@ -234,7 +228,7 @@ void __attribute__((noreturn)) panic_restart(void)
         digital_reset_needed = true;
     }
 #endif
-#if CONFIG_ESP_SYSTEM_CONFIG_MEMPROT_FEATURE
+#if CONFIG_ESP_SYSTEM_MEMPROT_FEATURE
     if (esp_memprot_is_intr_ena_any() || esp_memprot_is_locked_any()) {
         digital_reset_needed = true;
     }
