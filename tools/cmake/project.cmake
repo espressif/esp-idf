09e50b27edbd87b855e1e6a7021c83c268160613
Ivan Grokhotkov <ivan@espressif.com>
2021-10-06 09:43:05 +0200

    cmake: handling of space-separated EXTRA_COMPONENT_DIRS
    COMPONENT_DIRS and EXTRA_COMPONENT_DIRS should be defined as CMake
    lists, using 'set' or 'list' commands. Some applications written
    for earlier versions of ESP-IDF used to define these variables as
    space separated strings.
    
    For example, the following is correct:
    
      set(EXTRA_COMPONENT_DIRS path/to/components path/to/more/components)
    
    The following is not correct:
    
      set(EXTRA_COMPONENT_DIRS "${EXTRA_COMPONENT_DIRS} component1")
      set(EXTRA_COMPONENT_DIRS "${EXTRA_COMPONENT_DIRS} component2")
    
    The string "component1 component2" may indicate a single directory
    name with a space, or two directory names separated by space.
    
    However due to the fact that such way of defining EXTRA_COMPONENT_DIRS
    was supported in IDF 4.3 and earlier, we need to provide backward
    compatibility for it.
    
    This commit introduces a new script, split_paths_by_spaces.py, which
    is invoked if EXTRA_COMPONENT_DIRS or COMPONENT_DIRS variable contains
    spaces. The script tries to determine if each space should be
    interpreted as a separator or as part of the directory name.
    
    When this cannot be done unambiguously, the script reports an error.
    
    In all cases when space separators are detected, the script reports
    a warning, and prints instructions for fixing the CMakeLists.txt.
    
    Breaking change in this commit: specifying non-existent directories
    in COMPONENT_DIRS or EXTRA_COMPONENT_DIRS is no longer allowed.

--- tools/cmake/project.cmake
+++ tools/cmake/project.cmake
@@ -63,6 +63,38 @@ function(__project_get_revision var)
     set(${var} "${PROJECT_VER}" PARENT_SCOPE)
 endfunction()
 
+
+# paths_with_spaces_to_list
+#
+# Replacement for spaces2list in cases where it was previously used on
+# directory lists.
+#
+# If the variable doesn't contain spaces, (e.g. is already a CMake list)
+# then the variable is unchanged. Otherwise an external Python script is called
+# to try to split the paths, and the variable is updated with the result.
+#
+# This feature is added only for compatibility. Please do not introduce new
+# space separated path lists.
+#
+function(paths_with_spaces_to_list variable_name)
+    if("${${variable_name}}" MATCHES "[ \t]")
+        idf_build_get_property(python PYTHON)
+        idf_build_get_property(idf_path IDF_PATH)
+        execute_process(
+            COMMAND ${python}
+                "${idf_path}/tools/split_paths_by_spaces.py"
+                "--var-name=${variable_name}"
+                "${${variable_name}}"
+            WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+            OUTPUT_VARIABLE result
+            RESULT_VARIABLE ret)
+        if(NOT ret EQUAL 0)
+            message(FATAL_ERROR "Failed to parse ${variable_name}, see diagnostics above")
+        endif()
+        set("${variable_name}" "${result}" PARENT_SCOPE)
+    endif()
+endfunction()
+
 #
 # Output the built components to the user. Generates files for invoking idf_monitor.py
 # that doubles as an overview of some of the more important build properties.
@@ -182,9 +214,13 @@ function(__project_init components_var test_components_var)
     # extra directories, etc. passed from the root CMakeLists.txt.
     if(COMPONENT_DIRS)
         # User wants to fully override where components are pulled from.
-        spaces2list(COMPONENT_DIRS)
+        paths_with_spaces_to_list(COMPONENT_DIRS)
         idf_build_set_property(__COMPONENT_TARGETS "")
         foreach(component_dir ${COMPONENT_DIRS})
+            get_filename_component(component_abs_path ${component_dir} ABSOLUTE)
+            if(NOT EXISTS ${component_abs_path})
+                message(FATAL_ERROR "Directory specified in COMPONENT_DIRS doesn't exist: ${component_abs_path}")
+            endif()
             __project_component_dir(${component_dir})
         endforeach()
     else()
@@ -192,8 +228,12 @@ function(__project_init components_var test_components_var)
             __project_component_dir("${CMAKE_CURRENT_LIST_DIR}/main")
         endif()
 
-        spaces2list(EXTRA_COMPONENT_DIRS)
+        paths_with_spaces_to_list(EXTRA_COMPONENT_DIRS)
         foreach(component_dir ${EXTRA_COMPONENT_DIRS})
+            get_filename_component(component_abs_path ${component_dir} ABSOLUTE)
+            if(NOT EXISTS ${component_abs_path})
+                message(FATAL_ERROR "Directory specified in EXTRA_COMPONENT_DIRS doesn't exist: ${component_abs_path}")
+            endif()
             __project_component_dir("${component_dir}")
         endforeach()
 
