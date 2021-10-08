f4705f8eb44fa2e07e09618178b832ac3b5d024b
laokaiyao <laokaiyao@espressif.com>
2021-10-08 11:45:57 +0800

    touch sensor: update copyright notice

--- examples/peripherals/touch_sensor/touch_element/touch_elements_combination/main/touch_elements_example_main.c
+++ examples/peripherals/touch_sensor/touch_element/touch_elements_combination/main/touch_elements_example_main.c
@@ -1,16 +1,8 @@
-/* Touch Sensor - Example
-
-   For other examples please check:
-   https://github.com/espressif/esp-idf/tree/master/examples
-
-   See README.md file to get detailed usage of this example.
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
