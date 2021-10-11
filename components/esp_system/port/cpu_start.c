76e606ab3244a20d0c9bdf8a0207fd3b9ca17a44
Mahavir Jain <mahavir@espressif.com>
2021-10-11 20:47:27 +0530

    cpu_start: let individual core clear its interrupt matrix
    There was race condition where interrupt entries set by APP cpu core
    could have been cleared during PRO cpu startup.
    
    This was causing "cache access error" not being detected for ESP32 APP
    CPU core.
    
    This fix allows to NOT modify or clear any entries set by other core
    (APP or PRO) and thus avoiding any race conditions during startup code.

--- components/esp_system/port/cpu_start.c
+++ components/esp_system/port/cpu_start.c
@@ -131,6 +131,15 @@ static volatile bool s_resume_cores;
 // If CONFIG_SPIRAM_IGNORE_NOTFOUND is set and external RAM is not found or errors out on testing, this is set to false.
 bool g_spiram_ok = true;
 
+static void intr_matrix_clear(void)
+{
+    uint32_t core_id = cpu_hal_get_core_id();
+
+    for (int i = 0; i < ETS_MAX_INTR_SOURCE; i++) {
+        intr_matrix_set(core_id, i, ETS_INVALID_INUM);
+    }
+}
+
 #if !CONFIG_ESP_SYSTEM_SINGLE_CORE_MODE
 void startup_resume_other_cores(void)
 {
@@ -163,6 +172,9 @@ void IRAM_ATTR call_start_cpu1(void)
 
     s_cpu_up[1] = true;
     ESP_EARLY_LOGI(TAG, "App cpu up.");
+
+    // Clear interrupt matrix for APP CPU core
+    intr_matrix_clear();
 
     //Take care putting stuff here: if asked, FreeRTOS will happily tell you the scheduler
     //has started, but it isn't active *on this CPU* yet.
@@ -233,16 +245,6 @@ static void start_other_core(void)
     }
 }
 #endif // !CONFIG_ESP_SYSTEM_SINGLE_CORE_MODE
-
-static void intr_matrix_clear(void)
-{
-    for (int i = 0; i < ETS_MAX_INTR_SOURCE; i++) {
-        intr_matrix_set(0, i, ETS_INVALID_INUM);
-#if !CONFIG_ESP_SYSTEM_SINGLE_CORE_MODE
-        intr_matrix_set(1, i, ETS_INVALID_INUM);
-#endif
-    }
-}
 
 /*
  * We arrive here after the bootloader finished loading the program from flash. The hardware is mostly uninitialized,
@@ -447,6 +449,7 @@ void IRAM_ATTR call_start_cpu0(void)
     // and default RTC-backed system time provider.
     g_startup_time = esp_rtc_get_time_us();
 
+    // Clear interrupt matrix for PRO CPU core
     intr_matrix_clear();
 
 #ifdef CONFIG_ESP_CONSOLE_UART
