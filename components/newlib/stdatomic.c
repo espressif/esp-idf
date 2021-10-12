d5e4fc8356bdf4d343cf490dba222914b1fd62da
Scott Mabin <scott@mabez.dev>
2021-10-12 16:28:49 +0100

    remove `__clang` ifdef around atomic emulation
    * CI errors led me to believe these were needed, but as it turns out the
    load/store intrinsics are required even when idf is built by gcc when
    linking to a clang based project.
    * remove ... postfix inside `SYNC_LOCK_TEST_AND_SET` expansion

--- components/newlib/stdatomic.c
+++ components/newlib/stdatomic.c
@@ -200,7 +200,7 @@ CLANG_DECLARE_ALIAS( __sync_bool_compare_and_swap_ ## n )
 }                                                                                \
 CLANG_DECLARE_ALIAS( __sync_val_compare_and_swap_ ## n )
 
-#define SYNC_LOCK_TEST_AND_SET(n, type) type  CLANG_ATOMIC_SUFFIX(__sync_lock_test_and_set_ ## n)  (type *ptr, type val, ...) \
+#define SYNC_LOCK_TEST_AND_SET(n, type) type  CLANG_ATOMIC_SUFFIX(__sync_lock_test_and_set_ ## n)  (type *ptr, type val) \
 {                                                                                \
     unsigned state = _ATOMIC_ENTER_CRITICAL();                                   \
     type ret = *ptr;                                                             \
@@ -210,7 +210,7 @@ CLANG_DECLARE_ALIAS( __sync_val_compare_and_swap_ ## n )
 }
 CLANG_DECLARE_ALIAS( __sync_lock_test_and_set_ ## n )
 
-#define SYNC_LOCK_RELEASE(n, type) void  CLANG_ATOMIC_SUFFIX(__sync_lock_release_ ## n)  (type *ptr, ...) \
+#define SYNC_LOCK_RELEASE(n, type) void  CLANG_ATOMIC_SUFFIX(__sync_lock_release_ ## n)  (type *ptr) \
 {                                                                                \
     unsigned state = _ATOMIC_ENTER_CRITICAL();                                   \
     *ptr = 0;                                                                    \
@@ -277,16 +277,6 @@ SYNC_VAL_CMP_EXCHANGE(1, uint8_t)
 SYNC_VAL_CMP_EXCHANGE(2, uint16_t)
 SYNC_VAL_CMP_EXCHANGE(4, uint32_t)
 
-#ifdef __clang__
-
-// LLVM has not implemented native atomic load/stores for riscv targets without the Atomic extension
-// therfore we provide libcalls here when building with the clang toolchain. LLVM thread: https://reviews.llvm.org/D47553.
-ATOMIC_LOAD(1, uint8_t)
-ATOMIC_LOAD(2, uint16_t)
-ATOMIC_LOAD(4, uint32_t)
-ATOMIC_STORE(1, uint8_t)
-ATOMIC_STORE(2, uint16_t)
-ATOMIC_STORE(4, uint32_t)
 
 SYNC_LOCK_TEST_AND_SET(1, uint8_t)
 SYNC_LOCK_TEST_AND_SET(2, uint16_t)
@@ -296,15 +286,18 @@ SYNC_LOCK_RELEASE(1, uint8_t)
 SYNC_LOCK_RELEASE(2, uint16_t)
 SYNC_LOCK_RELEASE(4, uint32_t)
 
-#endif
+// LLVM has not implemented native atomic load/stores for riscv targets without the Atomic extension. LLVM thread: https://reviews.llvm.org/D47553.
+// Even though GCC does transform them, these libcalls need to be available for the case where a LLVM based project links against IDF.
+ATOMIC_LOAD(1, uint8_t)
+ATOMIC_LOAD(2, uint16_t)
+ATOMIC_LOAD(4, uint32_t)
+ATOMIC_STORE(1, uint8_t)
+ATOMIC_STORE(2, uint16_t)
+ATOMIC_STORE(4, uint32_t)
 
 #endif // !HAS_ATOMICS_32
 
 #if !HAS_ATOMICS_64
-
-ATOMIC_LOAD(8, uint64_t)
-
-ATOMIC_STORE(8, uint64_t)
 
 ATOMIC_EXCHANGE(8, uint64_t)
 
@@ -334,11 +327,12 @@ SYNC_BOOL_CMP_EXCHANGE(8, uint64_t)
 
 SYNC_VAL_CMP_EXCHANGE(8, uint64_t)
 
-#ifdef __clang__
-
 SYNC_LOCK_TEST_AND_SET(8, uint64_t)
 SYNC_LOCK_RELEASE(8, uint64_t)
 
-#endif
+// LLVM has not implemented native atomic load/stores for riscv targets without the Atomic extension. LLVM thread: https://reviews.llvm.org/D47553.
+// Even though GCC does transform them, these libcalls need to be available for the case where a LLVM based project links against IDF.
+ATOMIC_LOAD(8, uint64_t)
+ATOMIC_STORE(8, uint64_t)
 
 #endif // !HAS_ATOMICS_64
