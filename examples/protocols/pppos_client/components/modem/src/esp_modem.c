espressif/esp-idf

commit 336de29413503797688935f65d30806cc535cf28

Examples/pppos_client: Fix manual parsing to accept unexpected lines
Regression from a90817cda019910b95a2f10ba31c260c7be26446 that caused
parsing exit in case of an unexpected result found (line handler
reported ESP_FAIL). Prevously if any error found in the line handler we
just posted an unexpected event, but continued with parsing. Fixed by
allowing for further parsing.

Related: https://github.com/espressif/esp-idf/issues/7176

examples/protocols/pppos_client/components/modem/src/esp_modem.c
@@ -1,16 +1,8 @@
-// Copyright 2015-2018 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2015-2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <stdlib.h>
 #include <string.h>
 #include <sys/param.h>
@@ -83,6 +75,13 @@ static inline bool is_only_cr_lf(const char *str, uint32_t len)
     return true;
 }
 
+static inline void report_unknown_line(esp_modem_dte_t *esp_dte, char *line)
+{
+    /* Send ESP_MODEM_EVENT_UNKNOWN signal to event loop */
+    esp_event_post_to(esp_dte->event_loop_hdl, ESP_MODEM_EVENT, ESP_MODEM_EVENT_UNKNOWN,
+                      (void *)line, strlen(line) + 1, pdMS_TO_TICKS(100));
+}
+
 esp_err_t esp_modem_set_rx_cb(modem_dte_t *dte, esp_modem_on_receive receive_cb, void *receive_cb_ctx)
 {
     esp_modem_dte_t *esp_dte = __containerof(dte, esp_modem_dte_t, parent);
@@ -118,18 +117,19 @@ static esp_err_t esp_dte_handle_line(esp_modem_dte_t *esp_dte, char * line, size
             if (dce->handle_line == NULL) {
                 /* Received an asynchronous line, but no handler waiting this this */
                 ESP_LOGD(MODEM_TAG, "No handler for line: %s", p);
-                err = ESP_OK; /* Not an error, just propagate the line to user handler */
-                goto post_event_unknown;
+                report_unknown_line(esp_dte, line);
+                return ESP_OK; /* Not an error, just propagate the line to user handler */
             }
-            MODEM_CHECK(dce->handle_line(dce, p) == ESP_OK, "handle line failed", post_event_unknown);
+            if (dce->handle_line(dce, p) != ESP_OK) {
+                ESP_LOGE(MODEM_TAG, "handle line failed");
+                report_unknown_line(esp_dte, line);
+            }
         }
         p = strtok_r(NULL, "\n", &str_ptr);
     }
     return ESP_OK;
 post_event_unknown:
-    /* Send ESP_MODEM_EVENT_UNKNOWN signal to event loop */
-    esp_event_post_to(esp_dte->event_loop_hdl, ESP_MODEM_EVENT, ESP_MODEM_EVENT_UNKNOWN,
-                      (void *)line, strlen(line) + 1, pdMS_TO_TICKS(100));
+    report_unknown_line(esp_dte, line);
 err:
     return err;
 }

commit 0a15cc5e5a91b2e3cb07a072de93ca75657faa0d

esp_modem: Fix report unknown line
In esp_dte_handle_line(), it tokenize the data to call handlers separately
for each *line*. So it needs to post the tokenized data when report unknown
line instead of the first token (which could be a '\r' in my test).

Fixes: 336de2941350 ("Examples/pppos_client: Fix manual parsing to accept unexpected lines")
Signed-off-by: Axel Lin <axel.lin@gmail.com>

examples/protocols/pppos_client/components/modem/src/esp_modem.c
@@ -75,11 +75,11 @@ static inline bool is_only_cr_lf(const char *str, uint32_t len)
     return true;
 }
 
-static inline void report_unknown_line(esp_modem_dte_t *esp_dte, char *line)
+static inline void report_unknown_line(esp_modem_dte_t *esp_dte, char *line, int line_len)
 {
     /* Send ESP_MODEM_EVENT_UNKNOWN signal to event loop */
     esp_event_post_to(esp_dte->event_loop_hdl, ESP_MODEM_EVENT, ESP_MODEM_EVENT_UNKNOWN,
-                      (void *)line, strlen(line) + 1, pdMS_TO_TICKS(100));
+                      (void *)line, line_len + 1, pdMS_TO_TICKS(100));
 }
 
 esp_err_t esp_modem_set_rx_cb(modem_dte_t *dte, esp_modem_on_receive receive_cb, void *receive_cb_ctx)
@@ -112,24 +112,25 @@ static esp_err_t esp_dte_handle_line(esp_modem_dte_t *esp_dte, char * line, size
     char *str_ptr = NULL;
     char *p = strtok_r(line, "\n", &str_ptr);
     while (p) {
-        if (len > 2 && !is_only_cr_lf(p, strlen(p))) {
+        int plen = strlen(p);
+        if (plen > 2 && !is_only_cr_lf(p, plen)) {
             ESP_LOGD(MODEM_TAG, "Handling line: >>%s\n<<", p);
             if (dce->handle_line == NULL) {
                 /* Received an asynchronous line, but no handler waiting this this */
                 ESP_LOGD(MODEM_TAG, "No handler for line: %s", p);
-                report_unknown_line(esp_dte, line);
+                report_unknown_line(esp_dte, p, plen);
                 return ESP_OK; /* Not an error, just propagate the line to user handler */
             }
             if (dce->handle_line(dce, p) != ESP_OK) {
                 ESP_LOGE(MODEM_TAG, "handle line failed");
-                report_unknown_line(esp_dte, line);
+                report_unknown_line(esp_dte, p, plen);
             }
         }
         p = strtok_r(NULL, "\n", &str_ptr);
     }
     return ESP_OK;
 post_event_unknown:
-    report_unknown_line(esp_dte, line);
+    report_unknown_line(esp_dte, line, strlen(line));
 err:
     return err;
 }