01b96aad3f8771af9dfc48b25a0f81e452cb072e
songruojing <songruojing@espressif.com>
2021-10-27 11:56:56 +0800

    esp8684: support GPIO peripheral, IO_MUX, GPIO matrix.
    Generic GPIO example and GPIO unit test on esp8684 are both supported as well.

--- components/soc/esp32s3/include/soc/io_mux_reg.h
+++ components/soc/esp32s3/include/soc/io_mux_reg.h
@@ -1,16 +1,8 @@
-// Copyright 2017-2020 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2017-2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #pragma once
 
 #include "soc.h"
@@ -138,7 +130,6 @@
 #define IO_MUX_GPIO47_REG       PERIPHS_IO_MUX_SPICLK_P_U
 #define IO_MUX_GPIO48_REG       PERIPHS_IO_MUX_SPICLK_N_U
 
-#define FUNC_GPIO_GPIO                              1
 #define PIN_FUNC_GPIO                               1
 
 #define GPIO_PAD_PULLDOWN(num) do{PIN_PULLDWN_DIS(IOMUX_REG_GPIO##num);PIN_PULLUP_EN(IOMUX_REG_GPIO##num);}while(0)
