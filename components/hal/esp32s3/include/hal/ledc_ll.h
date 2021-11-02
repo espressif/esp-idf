1e8a0909f6924db1e468a7470b535617a4eec587
Omar Chebib <omar.chebib@espressif.com>
2021-11-02 14:40:58 +0800

    LEDC: divisor calculation will now be rounded up when necessary
    Closes https://github.com/espressif/esp-idf/issues/7722

--- components/hal/esp32s3/include/hal/ledc_ll.h
+++ components/hal/esp32s3/include/hal/ledc_ll.h
@@ -1,16 +1,8 @@
-// Copyright 2020 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2020-2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 // The LL layer for LEDC register operations.
 // Note that most of the register operations in this layer are non-atomic operations.
@@ -29,9 +21,9 @@ extern "C" {
 #define LEDC_LL_FRACTIONAL_BITS    (8)
 #define LEDC_LL_FRACTIONAL_MAX     ((1 << LEDC_LL_FRACTIONAL_BITS) - 1)
 #define LEDC_LL_GLOBAL_CLOCKS { \
-                                LEDC_USE_APB_CLK, \
-                                LEDC_USE_XTAL_CLK, \
-                                LEDC_USE_RTC8M_CLK, \
+                                LEDC_SLOW_CLK_APB, \
+                                LEDC_SLOW_CLK_XTAL, \
+                                LEDC_SLOW_CLK_RTC8M, \
                               }
 #define LEDC_LL_TIMER_SPECIFIC_CLOCKS \
                             {\
