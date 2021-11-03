1ac98324b37f9315231cf784471ba3763c7c2fd4
Emil Muratov <gpm@hotplug.ru>
2021-11-03 13:55:11 +0300

    sntp example - updated ntp over dhcp code/comments

--- examples/protocols/sntp/main/sntp_example_main.c
+++ examples/protocols/sntp/main/sntp_example_main.c
@@ -23,6 +23,10 @@
 
 static const char *TAG = "example";
 
+#ifndef INET6_ADDRSTRLEN
+#define INET6_ADDRSTRLEN 48
+#endif
+
 /* Variable holding number of times ESP32 restarted since first boot.
  * It is placed into RTC memory using RTC_DATA_ATTR and
  * maintains its value when ESP32 wakes from deep sleep.
@@ -124,10 +128,15 @@ static void obtain_time(void)
 
     /**
      * NTP server address could be aquired via DHCP,
-     * see LWIP_DHCP_GET_NTP_SRV menuconfig option
+     * see following menuconfig options:
+     * 'LWIP_DHCP_GET_NTP_SRV' - enable STNP over DHCP
+     * 'LWIP_SNTP_DEBUG' - enable debugging messages
+     *
+     * NOTE: This call should be made BEFORE esp aquires IP address from DHCP,
+     * otherwise NTP option would be rejected by default.
      */
 #ifdef LWIP_DHCP_GET_NTP_SRV
-    sntp_servermode_dhcp(1);
+    sntp_servermode_dhcp(1);      // accept NTP offers from DHCP server, if any
 #endif
 
     /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
@@ -142,7 +151,7 @@ static void obtain_time(void)
     time_t now = 0;
     struct tm timeinfo = { 0 };
     int retry = 0;
-    const int retry_count = 10;
+    const int retry_count = 15;
     while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
         ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
@@ -157,10 +166,43 @@ static void initialize_sntp(void)
 {
     ESP_LOGI(TAG, "Initializing SNTP");
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
+
+/*
+ * If 'NTP over DHCP' is enabled, we set dynamic pool address
+ * as a 'secondary' server. It will act as a fallback server in case that address
+ * provided via NTP over DHCP is not accessible
+ */
+#if defined LWIP_DHCP_GET_NTP_SRV && (SNTP_MAX_SERVERS > 1)
+    sntp_setservername(1, "pool.ntp.org");
+
+#if LWIP_IPV6                   // statically assigned IPv6 address is also possible
+    ip_addr_t ip6;
+    if (ipaddr_aton("2a01:3f7::1", &ip6))       // ipv6 ntp source "ntp.netnod.se"
+        sntp_setserver(2, &ip6);
+#endif  /* LWIP_IPV6 */
+
+#else   /* LWIP_DHCP_GET_NTP_SRV && (SNTP_MAX_SERVERS > 1) */
+    // otherwise use DNS address from a pool
     sntp_setservername(0, "pool.ntp.org");
+#endif
+
     sntp_set_time_sync_notification_cb(time_sync_notification_cb);
 #ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
     sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
 #endif
     sntp_init();
+
+    ESP_LOGI(TAG, "List of configured NTP servers:");
+
+    for (uint8_t i = 0; i < SNTP_MAX_SERVERS; ++i){
+        if (sntp_getservername(i)){
+            ESP_LOGI(TAG, "server %d: %s", i, sntp_getservername(i));
+        } else {
+            // we have either IPv4 or IPv6 address, let's print it
+            char buff[INET6_ADDRSTRLEN];
+            ip_addr_t const *ip = sntp_getserver(i);
+            if (ipaddr_ntoa_r(ip, buff, INET6_ADDRSTRLEN) != NULL)
+                ESP_LOGI(TAG, "server %d: %s", i, buff);
+        }
+    }
 }
