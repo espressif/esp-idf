384ea8123b188c87482fa39ff073449b77cf4651
Aditya Patwardhan <aditya.patwardhan@espressif.com>
2021-11-12 15:48:53 +0530

    https_request_example: Use local python server to test client session tickets Update example test to test the client session tickets Check for minimum heap info in the example test.

--- examples/protocols/https_request/main/component.mk
+++ examples/protocols/https_request/main/component.mk
@@ -2,4 +2,4 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-COMPONENT_EMBED_TXTFILES := server_root_cert.pem
+COMPONENT_EMBED_TXTFILES := server_root_cert.pem local_server_cert.pem
