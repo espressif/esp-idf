6ed81a632c6b735f9a226f1cd88791a3d6b2cd1d
Sergei Silnov <sergei.silnov@espressif.com>
2021-11-24 12:32:22 +0100

    ci: Remove component manager test from build system tests
    Component manager itself has a set of integration tests
    that run against different ESP-IDF versions

--- tools/ci/test_build_system_cmake.sh
+++ tools/ci/test_build_system_cmake.sh
@@ -701,19 +701,6 @@ endmenu\n" >> ${IDF_PATH}/Kconfig
     git checkout CMakeLists.txt
     rm -f log.txt
 
-    print_status "Compiles with dependencies delivered by component manager"
-    clean_build_dir
-    # Make sure that component manager is not installed
-    pip uninstall -y idf_component_manager
-    printf "\n#include \"test_component.h\"\n" >> main/main.c
-    printf "dependencies:\n  test_component:\n    path: test_component\n    git: ${COMPONENT_MANAGER_TEST_REPO}\n" >> idf_project.yml
-    ! idf.py build || failure "Build should fail if dependencies are not installed"
-    pip install ${COMPONENT_MANAGER_REPO} || failure "Failed to install the component manager"
-    idf.py reconfigure build || failure "Build didn't succeed with required components installed by package manager"
-    pip uninstall -y idf_component_manager
-    rm idf_project.yml
-    git checkout main/main.c
-
     print_status "Build fails if partitions don't fit in flash"
     sed -i.bak "s/CONFIG_ESPTOOLPY_FLASHSIZE.\+//" sdkconfig  # remove all flashsize config
     echo "CONFIG_ESPTOOLPY_FLASHSIZE_1MB=y" >> sdkconfig     # introduce undersize flash
