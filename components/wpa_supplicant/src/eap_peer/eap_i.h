86e33207b161d62391931cb7587dc8ff3783ac95
Hrudaynath Dhabe <hrudaynath.dhabe@espressif.com>
2021-12-02 19:43:04 +0530

    esp_wifi: Fix the negotiation of method with eap server.

--- components/wpa_supplicant/src/eap_peer/eap_i.h
+++ components/wpa_supplicant/src/eap_peer/eap_i.h
@@ -331,6 +331,7 @@ int eap_get_config_fragment_size(struct eap_sm *sm);
 struct eap_peer_config * eap_get_config(struct eap_sm *sm);
 void eap_set_config_blob(struct eap_sm *sm, struct wpa_config_blob *blob);
 const struct wpa_config_blob * eap_get_config_blob(struct eap_sm *sm, const char *name);
+int eap_allowed_method(struct eap_sm *sm, int vendor, u32 method);
 bool wifi_sta_get_enterprise_disable_time_check(void);
 
 struct wpabuf * eap_sm_build_identity_resp(struct eap_sm *sm, u8 id, int encrypted);
