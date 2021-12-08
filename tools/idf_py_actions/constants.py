c6a6eaeb6076854a5d4d8fe67b09378e2716beae
simon.chupin <simon.chupin@espressif.com>
2021-12-08 18:29:14 +0100

    idf.py: Add automated hints on how to resolve errors

--- tools/idf_py_actions/constants.py
+++ tools/idf_py_actions/constants.py
@@ -4,17 +4,23 @@ import collections
 import multiprocessing
 import os
 import platform
+from typing import Dict, Union
 
-GENERATORS = collections.OrderedDict([
+GENERATORS: Dict[str, Union[str, Dict, list]] = collections.OrderedDict([
     # - command: build command line
     # - version: version command line
     # - dry_run: command to run in dry run mode
     # - verbose_flag: verbose flag
+    # - force_progression: one liner status of the progress
+    # - envvar: environment variables
     ('Ninja', {
         'command': ['ninja'],
         'version': ['ninja', '--version'],
         'dry_run': ['ninja', '-n'],
-        'verbose_flag': '-v'
+        'verbose_flag': '-v',
+        # as opposed to printing the status updates each in a in new line
+        'force_progression': True,
+        'envvar': {}
     }),
 ])
 
@@ -23,7 +29,10 @@ if os.name != 'nt':
     GENERATORS['Unix Makefiles'] = {'command': [MAKE_CMD, '-j', str(multiprocessing.cpu_count() + 2)],
                                     'version': [MAKE_CMD, '--version'],
                                     'dry_run': [MAKE_CMD, '-n'],
-                                    'verbose_flag': 'VERBOSE=1'}
+                                    'verbose_flag': 'VERBOSE=1',
+                                    'force_progression': False,
+                                    # CLICOLOR_FORCE if set forcing make to print ANSI escape sequence
+                                    'envvar': {'CLICOLOR_FORCE': '1'}}
 
 URL_TO_DOC = 'https://docs.espressif.com/projects/esp-idf'
 
