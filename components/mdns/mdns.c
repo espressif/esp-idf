c8a4a3f2c9d7e1eb227a560250b436891d11551f
caffreyfans <caffreyfans@163.com>
2021-12-17 16:05:38 +0800

    Fix mDNS memory leak

--- components/mdns/mdns.c
+++ components/mdns/mdns.c
@@ -2315,6 +2315,7 @@ static mdns_service_t * _mdns_create_service(const char * service, const char * 
     if (hostname) {
         s->hostname = strndup(hostname, MDNS_NAME_BUF_LEN - 1);
         if (!s->hostname) {
+            free((char *)s->instance);
             free(s);
             return NULL;
         }
@@ -2324,12 +2325,14 @@ static mdns_service_t * _mdns_create_service(const char * service, const char * 
 
     s->service = strndup(service, MDNS_NAME_BUF_LEN - 1);
     if (!s->service) {
+        free((char *)s->instance);
         free(s);
         return NULL;
     }
 
     s->proto = strndup(proto, MDNS_NAME_BUF_LEN - 1);
     if (!s->proto) {
+        free((char *)s->instance);
         free((char *)s->service);
         free(s);
         return NULL;
