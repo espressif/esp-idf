d77963fbcc8117f97f9318d1abd4a1bbe5d3a4ad
morris <maoshengrong@espressif.com>
2021-12-24 11:10:38 +0800

    fast_gpio: support esp32c2/esp8684

--- components/soc/esp8684/include/soc/soc_caps.h
+++ components/soc/esp8684/include/soc/soc_caps.h
@@ -12,6 +12,7 @@
 
 #define SOC_CPU_CORES_NUM               1
 #define SOC_ADC_SUPPORTED               1
+#define SOC_DEDICATED_GPIO_SUPPORTED    1
 #define SOC_GDMA_SUPPORTED              1
 #define SOC_BT_SUPPORTED                0 // Enable during bringup, IDF-4357
 #define SOC_WIFI_SUPPORTED              0 // Enable during bringup, IDF-3905
@@ -106,6 +107,11 @@
 
 // Support to configure sleep status
 #define SOC_GPIO_SUPPORT_SLP_SWITCH  (1)
+
+/*-------------------------- Dedicated GPIO CAPS -----------------------------*/
+#define SOC_DEDIC_GPIO_OUT_CHANNELS_NUM (8) /*!< 8 outward channels on each CPU core */
+#define SOC_DEDIC_GPIO_IN_CHANNELS_NUM  (8) /*!< 8 inward channels on each CPU core */
+#define SOC_DEDIC_PERIPH_ALWAYS_ENABLE  (1) /*!< The dedicated GPIO (a.k.a. fast GPIO) is featured by some customized CPU instructions, which is always enabled */
 
 /*-------------------------- I2C CAPS ----------------------------------------*/
 // TODO IDF-3918
