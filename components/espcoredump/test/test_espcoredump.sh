4f779bb7c6e8a4200f62af009a951637a6547ab0
Fu Hanxi <fuhanxi@espressif.com>
2022-01-04 09:35:08 +0800

    ci: use `python -m coverage` instead of `coverage` executable

--- components/espcoredump/test/test_espcoredump.sh
+++ components/espcoredump/test/test_espcoredump.sh
@@ -11,26 +11,21 @@ else
   elf_dir=$1
 fi
 
-if ! command -v coverage &> /dev/null; then
-  echo "coverage could not be found, please install it ('pip install coverage')"
-  exit 1
-fi
-
 SUPPORTED_TARGETS=("esp32" "esp32s2" "esp32c3" "esp32s3" )
 res=0
-coverage erase
+python -m coverage erase
 for chip in "${SUPPORTED_TARGETS[@]}"; do
   {
     echo "run b64 decoding tests on $chip"
-    coverage run -a --source=corefile ../espcoredump.py --chip="$chip" --gdb-timeout-sec 5 info_corefile -m -t b64 -c "${chip}/coredump.b64" -s "${chip}/core.elf" "${elf_dir}/${chip}.elf" &>"${chip}/output" &&
+    python -m coverage run -a --source=corefile ../espcoredump.py --chip="$chip" --gdb-timeout-sec 5 info_corefile -m -t b64 -c "${chip}/coredump.b64" -s "${chip}/core.elf" "${elf_dir}/${chip}.elf" &>"${chip}/output" &&
       diff "${chip}/expected_output" "${chip}/output" &&
-      coverage run -a --source=corefile ../espcoredump.py --chip="$chip" --gdb-timeout-sec 5 info_corefile -m -t elf -c "${chip}/core.elf" "${elf_dir}/${chip}.elf" &>"${chip}/output2" &&
+      python -m coverage run -a --source=corefile ../espcoredump.py --chip="$chip" --gdb-timeout-sec 5 info_corefile -m -t elf -c "${chip}/core.elf" "${elf_dir}/${chip}.elf" &>"${chip}/output2" &&
       diff "${chip}/expected_output" "${chip}/output2"
   } || {
     echo 'The test for espcoredump has failed!'
     res=1
   }
 done
-coverage run -a --source=corefile ./test_espcoredump.py
-coverage report ../corefile/*.py ../espcoredump.py
+python -m coverage run -a --source=corefile ./test_espcoredump.py
+python -m coverage report ../corefile/*.py ../espcoredump.py
 exit $res
