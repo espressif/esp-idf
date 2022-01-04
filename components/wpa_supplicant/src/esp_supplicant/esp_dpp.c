b1f7ad6983aa875da1243eb55075f88e4b8e1615
Kapil Gupta <kapil.gupta@espressif.com>
2022-01-04 11:34:06 +0530

    esp_example: Enable MbedTLS for DPP enrollee by default

--- components/wpa_supplicant/src/esp_supplicant/esp_dpp.c
+++ components/wpa_supplicant/src/esp_supplicant/esp_dpp.c
@@ -1,16 +1,8 @@
-// Copyright 2020 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2020-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include "esp_dpp_i.h"
 #include "esp_dpp.h"
@@ -20,6 +12,7 @@
 #include "esp_wifi.h"
 #include "common/ieee802_11_defs.h"
 
+#ifdef CONFIG_DPP
 static void *s_dpp_task_hdl = NULL;
 static void *s_dpp_evt_queue = NULL;
 static void *s_dpp_api_lock = NULL;
@@ -676,3 +669,4 @@ void esp_supp_dpp_deinit(void)
     dpp_global_deinit(s_dpp_ctx.dpp_global);
     esp_dpp_post_evt(SIG_DPP_DEL_TASK, 0);
 }
+#endif
