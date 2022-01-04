507be01df68ac5703f42a42a0f5f1064ab4bff52
Jiacheng Guo <guojiacheng@espressif.com>
2022-01-04 19:06:38 +0800

    lwip: add ip6 input hook
    The MR adds Kconfig options and a default weak implementation for the
    lwIP ip6 input hook.

--- components/lwip/port/esp32/hooks/lwip_default_hooks.c
+++ components/lwip/port/esp32/hooks/lwip_default_hooks.c
@@ -1,16 +1,8 @@
-// Copyright 2020 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2020-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include "lwip_default_hooks.h"
 
@@ -44,6 +36,16 @@ const ip6_addr_t *__weak lwip_hook_nd6_get_gw(struct netif *netif, const ip6_add
 {
     LWIP_UNUSED_ARG(netif);
     LWIP_UNUSED_ARG(dest);
+
+    return 0;
+}
+#endif
+
+#ifdef CONFIG_LWIP_HOOK_IP6_INPUT_DEFAULT
+int __weak lwip_hook_ip6_input(struct pbuf *p, struct netif *inp)
+{
+    LWIP_UNUSED_ARG(p);
+    LWIP_UNUSED_ARG(inp);
 
     return 0;
 }
