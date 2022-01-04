7554581cbe411eda4bed6b1e912219b35c2c955e
Fu Hanxi <fuhanxi@espressif.com>
2022-01-04 09:35:08 +0800

    ci: use `python -m coverage` instead of `coverage` executable

--- tools/esp_app_trace/test/logtrace/test.sh
+++ tools/esp_app_trace/test/logtrace/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-{ coverage debug sys \
-    && coverage erase &> output \
-    && coverage run -a $IDF_PATH/tools/esp_app_trace/logtrace_proc.py adc_log.trc test.elf &>> output \
+{ python -m coverage debug sys \
+    && python -m coverage erase &> output \
+    && python -m coverage run -a $IDF_PATH/tools/esp_app_trace/logtrace_proc.py adc_log.trc test.elf &>> output \
     && diff output expected_output \
-    && coverage report \
+    && python -m coverage report \
 ; } || { echo 'The test for logtrace_proc has failed. Please examine the artifacts.' ; exit 1; }
