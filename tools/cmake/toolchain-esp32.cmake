c21a5d14129ea7fe33556887f3866ea7427e5dcb
morris <maoshengrong@espressif.com>
2022-01-20 15:45:13 +0800

    cmake: assign toolchain prefix manually

--- tools/cmake/toolchain-esp32.cmake
+++ tools/cmake/toolchain-esp32.cmake
@@ -5,6 +5,7 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_C_COMPILER xtensa-esp32-elf-gcc)
 set(CMAKE_CXX_COMPILER xtensa-esp32-elf-g++)
 set(CMAKE_ASM_COMPILER xtensa-esp32-elf-gcc)
+set(_CMAKE_TOOLCHAIN_PREFIX xtensa-esp32-elf-)
 
 
 remove_duplicated_flags("-mlongcalls -Wno-frame-address ${CMAKE_C_FLAGS}" UNIQ_CMAKE_C_FLAGS)
