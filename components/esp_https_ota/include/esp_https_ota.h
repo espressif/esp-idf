3eb8f3b54e0a0effe1648abb3d182d38e3abaefc
Mahavir Jain <mahavir@espressif.com>
2022-01-28 16:37:42 +0530

    esp_https_ota: add some return values to API documentation

--- components/esp_https_ota/include/esp_https_ota.h
+++ components/esp_https_ota/include/esp_https_ota.h
@@ -1,21 +1,14 @@
-// Copyright 2017-2018 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2017-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #pragma once
 
 #include <esp_http_client.h>
 #include <bootloader_common.h>
+#include <sdkconfig.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -108,6 +101,7 @@ esp_err_t esp_https_ota_begin(esp_https_ota_config_t *ota_config, esp_https_ota_
  *    - ESP_OK: OTA update was successful
  *    - ESP_FAIL: OTA update failed
  *    - ESP_ERR_INVALID_ARG: Invalid argument
+ *    - ESP_ERR_INVALID_VERSION: Invalid chip revision in image header
  *    - ESP_ERR_OTA_VALIDATE_FAILED: Invalid app image
  *    - ESP_ERR_NO_MEM: Cannot allocate memory for OTA operation.
  *    - ESP_ERR_FLASH_OP_TIMEOUT or ESP_ERR_FLASH_OP_FAIL: Flash write failed.
@@ -181,6 +175,7 @@ esp_err_t esp_https_ota_abort(esp_https_ota_handle_t https_ota_handle);
  *
  * @return
  *    - ESP_ERR_INVALID_ARG: Invalid arguments
+ *    - ESP_ERR_INVALID_STATE: Invalid state to call this API. esp_https_ota_begin() not called yet.
  *    - ESP_FAIL: Failed to read image descriptor
  *    - ESP_OK: Successfully read image descriptor
  */
