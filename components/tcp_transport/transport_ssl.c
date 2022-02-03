5355ba06f14d2d4f9282d61f39d8e7ac38c3de73
Mahavir Jain <mahavir@espressif.com>
2022-02-03 18:15:11 +0530

    transport_ssl: use return value from `close` for non-TLS case

--- components/tcp_transport/transport_ssl.c
+++ components/tcp_transport/transport_ssl.c
@@ -1,16 +1,8 @@
-// Copyright 2015-2018 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2015-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include <string.h>
 #include <stdlib.h>
@@ -287,9 +279,8 @@ static int base_close(esp_transport_handle_t t)
         ssl->ssl_initialized = false;
         ssl->sockfd = INVALID_SOCKET;
     } else if (ssl && ssl->sockfd >= 0) {
-        close(ssl->sockfd);
+        ret = close(ssl->sockfd);
         ssl->sockfd = INVALID_SOCKET;
-        ret = 0;
     }
     return ret;
 }
