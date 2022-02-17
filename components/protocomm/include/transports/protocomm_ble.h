6a92fb2d76cd7bc1bc12233baee0ef69c0b08b33
Isha Pardikar <isha.pardikar@espressif.com>
2022-02-17 18:42:25 +0530

    Merge branch 'bugfix/wifi_prov_mgr_conn_issue' into 'release/v4.3'
    NimBLE: Fixed device disconnection issue of wifi prov mgr
    
    Closes IDF-4655
    
    See merge request espressif/esp-idf!17621

--- components/protocomm/include/transports/protocomm_ble.h
+++ components/protocomm/include/transports/protocomm_ble.h
@@ -84,6 +84,11 @@ typedef struct protocomm_ble_config {
      */
     unsigned ble_bonding:1;
 
+    /**
+     * BLE security flag
+     */
+    unsigned ble_sm_sc:1;
+
 } protocomm_ble_config_t;
 
 /**
