f8f93d936e0494c02e9416d3033b433e2849b1c2
Marius Vikhammer <marius.vikhammer@espressif.com>
2022-02-28 18:12:28 +0800

    ulp-riscv: add support for using ADC as well as an example show-casing it.

---
+++ components/ulp/ulp_riscv/include/ulp_riscv/ulp_riscv_adc_ulp_core.h
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "hal/adc_ll.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Start an ADC conversion and get the converted value.
+ *
+ * @note Will block until the conversion is completed
+ *
+ * @note ADC should be initilized for ULP by main CPU by calling ulp_riscv_adc_init()
+ *       before calling this.
+ *
+ * @param      adc_n   ADC unit.
+ * @param      channel ADC channel number.
+ *
+ * @return             Converted value, -1 if conversion failed
+ */
+int32_t ulp_riscv_adc_read_channel(adc_unit_t adc_n, int channel);
+
+#ifdef __cplusplus
+}
+#endif
