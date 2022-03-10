fe1fb25078765eb7dc9a3365deec54a49c4b9d65
xiewenxiang <xiewenxiang@espressif.com>
2022-03-10 10:40:12 +0800

    doc(bt): Add description for de-initialize bt controller

--- components/bt/include/esp32c3/include/esp_bt.h
+++ components/bt/include/esp32c3/include/esp_bt.h
@@ -337,6 +337,8 @@ esp_err_t esp_bt_controller_init(esp_bt_controller_config_t *cfg);
 
 /**
  * @brief  De-initialize BT controller to free resource and delete task.
+ *         You should stop advertising and scanning, as well as
+ *         disconnect all existing connections before de-initializing BT controller.
  *
  * This function should be called only once, after any other BT functions are called.
  * This function is not whole completed, esp_bt_controller_init cannot called after this function.
