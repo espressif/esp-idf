12717cbc0040485e063134ddc4e4f69b957810b3
Ivan Grokhotkov <ivan@espressif.com>
2022-03-14 18:45:40 +0100

    hal: fix issues reported by PVS-Studio
    https://www.viva64.com/en/b/0790/#ID369075A8F4
    https://www.viva64.com/en/b/0790/#IDF03E449184
    Reported in https://github.com/espressif/esp-idf/issues/6440

--- components/hal/esp32/touch_sensor_hal.c
+++ components/hal/esp32/touch_sensor_hal.c
@@ -1,16 +1,8 @@
-// Copyright 2015-2019 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2015-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 // The HAL layer for Touch sensor (common part)
 
@@ -46,6 +38,7 @@ void touch_hal_get_wakeup_status(touch_pad_t *pad_num)
     touch_ll_read_trigger_status_mask(&touch_mask);
     if (touch_mask == 0) {
         *pad_num = -1;
+    } else {
+        *pad_num = (touch_pad_t)(__builtin_ffs(touch_mask) - 1);
     }
-    *pad_num = (touch_pad_t)(__builtin_ffs(touch_mask) - 1);
 }
