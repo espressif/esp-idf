espressif/esp-idf

commit 49a48644e42458366b2dd7b7d153acc943d50e0f

spi: allow using esp_flash and spi_master driver on the same bus

components/driver/include/driver/spi_common.h
@@ -102,7 +102,7 @@ typedef struct {
  *
  * @warning For now, only supports HSPI and VSPI.
  *
- * @param host SPI peripheral that controls this bus
+ * @param host_id SPI peripheral that controls this bus
  * @param bus_config Pointer to a spi_bus_config_t struct specifying how the host should be initialized
  * @param dma_chan Either channel 1 or 2, or 0 in the case when no DMA is required. Selecting a DMA channel
  *                 for a SPI bus allows transfers on the bus to have sizes only limited by the amount of
@@ -123,20 +123,20 @@ typedef struct {
  *         - ESP_ERR_NO_MEM        if out of memory
  *         - ESP_OK                on success
  */
-esp_err_t spi_bus_initialize(spi_host_device_t host, const spi_bus_config_t *bus_config, int dma_chan);
+esp_err_t spi_bus_initialize(spi_host_device_t host_id, const spi_bus_config_t *bus_config, int dma_chan);
 
 /**
  * @brief Free a SPI bus
  *
  * @warning In order for this to succeed, all devices have to be removed first.
  *
- * @param host SPI peripheral to free
+ * @param host_id SPI peripheral to free
  * @return
  *         - ESP_ERR_INVALID_ARG   if parameter is invalid
  *         - ESP_ERR_INVALID_STATE if not all devices on the bus are freed
  *         - ESP_OK                on success
  */
-esp_err_t spi_bus_free(spi_host_device_t host);
+esp_err_t spi_bus_free(spi_host_device_t host_id);
 
 #ifdef __cplusplus
 }

components/driver/spi_common.c
@@ -31,6 +31,7 @@
 #include "stdatomic.h"
 #include "hal/spi_hal.h"
 
+
 static const char *SPI_TAG = "spi";
 
 #define SPI_CHECK(a, str, ret_val) do { \
@@ -53,6 +54,23 @@ typedef struct spi_device_t spi_device_t;
 
 #define DMA_CHANNEL_ENABLED(dma_chan)    (BIT(dma_chan-1))
 
+
+typedef struct {
+    int host_id;
+    spi_destroy_func_t destroy_func;
+    void* destroy_arg;
+    spi_bus_attr_t bus_attr;
+} spicommon_bus_context_t;
+
+#define MAIN_BUS_DEFAULT() { \
+        .host_id = 0, \
+        .bus_attr = { \
+            .dma_chan = 0, \
+            .max_transfer_sz = SOC_SPI_MAXIMUM_BUFFER_SIZE, \
+            .dma_desc_num= 0, \
+        }, \
+    }
+
 //Periph 1 is 'claimed' by SPI flash code.
 static atomic_bool spi_periph_claimed[SOC_SPI_PERIPH_NUM] = { ATOMIC_VAR_INIT(true), ATOMIC_VAR_INIT(false), ATOMIC_VAR_INIT(false),
 #if SOC_SPI_PERIPH_NUM >= 4
@@ -62,6 +80,9 @@ static atomic_bool spi_periph_claimed[SOC_SPI_PERIPH_NUM] = { ATOMIC_VAR_INIT(tr
 static const char* spi_claiming_func[3] = {NULL, NULL, NULL};
 static uint8_t spi_dma_chan_enabled = 0;
 static portMUX_TYPE spi_dma_spinlock = portMUX_INITIALIZER_UNLOCKED;
+
+static spicommon_bus_context_t s_mainbus = MAIN_BUS_DEFAULT();
+static spicommon_bus_context_t* bus_ctx[SOC_SPI_PERIPH_NUM] = {&s_mainbus};
 
 
 //Returns true if this peripheral is successfully claimed, false if otherwise.
@@ -415,6 +436,173 @@ bool spicommon_bus_using_iomux(spi_host_device_t host)
     CHECK_IOMUX_PIN(host, spihd);
     return true;
 }
+
+
+void spi_bus_main_set_lock(spi_bus_lock_handle_t lock)
+{
+    bus_ctx[0]->bus_attr.lock = lock;
+}
+
+spi_bus_lock_handle_t spi_bus_lock_get_by_id(spi_host_device_t host_id)
+{
+    return bus_ctx[host_id]->bus_attr.lock;
+}
+
+static inline bool is_valid_host(spi_host_device_t host)
+{
+    return host >= SPI1_HOST && host <= SPI3_HOST;
+}
+
+esp_err_t spi_bus_initialize(spi_host_device_t host_id, const spi_bus_config_t *bus_config, int dma_chan)
+{
+    esp_err_t err = ESP_OK;
+    spicommon_bus_context_t *ctx = NULL;
+    spi_bus_attr_t *bus_attr = NULL;
+    SPI_CHECK(is_valid_host(host_id), "invalid host_id", ESP_ERR_INVALID_ARG);
+    SPI_CHECK(bus_ctx[host_id] == NULL, "SPI bus already initialized.", ESP_ERR_INVALID_STATE);
+#ifdef CONFIG_IDF_TARGET_ESP32
+    SPI_CHECK( dma_chan >= 0 && dma_chan <= 2, "invalid dma channel", ESP_ERR_INVALID_ARG );
+#elif CONFIG_IDF_TARGET_ESP32S2
+    SPI_CHECK( dma_chan == 0 || dma_chan == host_id, "invalid dma channel", ESP_ERR_INVALID_ARG );
+#endif
+    SPI_CHECK((bus_config->intr_flags & (ESP_INTR_FLAG_HIGH|ESP_INTR_FLAG_EDGE|ESP_INTR_FLAG_INTRDISABLED))==0, "intr flag not allowed", ESP_ERR_INVALID_ARG);
+#ifndef CONFIG_SPI_MASTER_ISR_IN_IRAM
+    SPI_CHECK((bus_config->intr_flags & ESP_INTR_FLAG_IRAM)==0, "ESP_INTR_FLAG_IRAM should be disabled when CONFIG_SPI_MASTER_ISR_IN_IRAM is not set.", ESP_ERR_INVALID_ARG);
+#endif
+
+    bool spi_chan_claimed = spicommon_periph_claim(host_id, "spi master");
+    SPI_CHECK(spi_chan_claimed, "host_id already in use", ESP_ERR_INVALID_STATE);
+
+    if (dma_chan != 0) {
+        bool dma_chan_claimed=spicommon_dma_chan_claim(dma_chan);
+        if (!dma_chan_claimed) {
+            spicommon_periph_free(host_id);
+            SPI_CHECK(false, "dma channel already in use", ESP_ERR_INVALID_STATE);
+        }
+    }
+
+    //clean and initialize the context
+    ctx = (spicommon_bus_context_t*)malloc(sizeof(spicommon_bus_context_t));
+    if (!ctx) {
+        err = ESP_ERR_NO_MEM;
+        goto cleanup;
+    }
+    *ctx = (spicommon_bus_context_t) {
+        .host_id = host_id,
+        .bus_attr = {
+            .bus_cfg = *bus_config,
+            .dma_chan = dma_chan,
+        },
+    };
+
+    bus_attr = &ctx->bus_attr;
+    if (dma_chan == 0) {
+        bus_attr->max_transfer_sz = SOC_SPI_MAXIMUM_BUFFER_SIZE;
+        bus_attr->dma_desc_num = 0;
+    } else {
+        //See how many dma descriptors we need and allocate them
+        int dma_desc_ct = lldesc_get_required_num(bus_config->max_transfer_sz);
+        if (dma_desc_ct == 0) dma_desc_ct = 1; //default to 4k when max is not given
+
+        bus_attr->max_transfer_sz = dma_desc_ct * LLDESC_MAX_NUM_PER_DESC;
+        bus_attr->dmadesc_tx = heap_caps_malloc(sizeof(lldesc_t) * dma_desc_ct, MALLOC_CAP_DMA);
+        bus_attr->dmadesc_rx = heap_caps_malloc(sizeof(lldesc_t) * dma_desc_ct, MALLOC_CAP_DMA);
+        if (bus_attr->dmadesc_tx == NULL || bus_attr->dmadesc_rx == NULL) {
+            err = ESP_ERR_NO_MEM;
+            goto cleanup;
+        }
+        bus_attr->dma_desc_num = dma_desc_ct;
+    }
+
+    spi_bus_lock_config_t lock_config = {
+        .host_id = host_id,
+        .cs_num = SOC_SPI_PERIPH_CS_NUM(host_id),
+    };
+    err = spi_bus_init_lock(&bus_attr->lock, &lock_config);
+    if (err != ESP_OK) {
+        goto cleanup;
+    }
+
+#ifdef CONFIG_PM_ENABLE
+    err = esp_pm_lock_create(ESP_PM_APB_FREQ_MAX, 0, "spi_master",
+            &bus_attr->pm_lock);
+    if (err != ESP_OK) {
+        goto cleanup;
+    }
+#endif //CONFIG_PM_ENABLE
+
+    err = spicommon_bus_initialize_io(host_id, bus_config, dma_chan, SPICOMMON_BUSFLAG_MASTER | bus_config->flags, &bus_attr->flags);
+    if (err != ESP_OK) {
+        goto cleanup;
+    }
+
+    bus_ctx[host_id] = ctx;
+    return ESP_OK;
+
+cleanup:
+    if (bus_attr) {
+#ifdef CONFIG_PM_ENABLE
+        esp_pm_lock_delete(bus_attr->pm_lock);
+#endif
+        if (bus_attr->lock) {
+            spi_bus_deinit_lock(bus_attr->lock);
+        }
+        free(bus_attr->dmadesc_tx);
+        free(bus_attr->dmadesc_rx);
+    }
+    free(ctx);
+    if (dma_chan) {
+        spicommon_dma_chan_free(dma_chan);
+    }
+    spicommon_periph_free(host_id);
+    return err;
+}
+
+const spi_bus_attr_t* spi_bus_get_attr(spi_host_device_t host_id)
+{
+    if (bus_ctx[host_id] == NULL) return NULL;
+
+    return &bus_ctx[host_id]->bus_attr;
+}
+
+esp_err_t spi_bus_free(spi_host_device_t host_id)
+{
+    esp_err_t err = ESP_OK;
+    spicommon_bus_context_t* ctx = bus_ctx[host_id];
+    spi_bus_attr_t* bus_attr = &ctx->bus_attr;
+
+    if (ctx->destroy_func) {
+        err = ctx->destroy_func(ctx->destroy_arg);
+    }
+
+    spicommon_bus_free_io_cfg(&bus_attr->bus_cfg);
+
+#ifdef CONFIG_PM_ENABLE
+    esp_pm_lock_delete(bus_attr->pm_lock);
+#endif
+    spi_bus_deinit_lock(bus_attr->lock);
+
+    free(bus_attr->dmadesc_rx);
+    free(bus_attr->dmadesc_tx);
+
+    if (bus_attr->dma_chan > 0) {
+        spicommon_dma_chan_free (bus_attr->dma_chan);
+    }
+    spicommon_periph_free(host_id);
+
+    free(ctx);
+    bus_ctx[host_id] = NULL;
+    return err;
+}
+
+esp_err_t spi_bus_register_destroy_func(spi_host_device_t host_id,
+                                        spi_destroy_func_t f, void *arg)
+{
+    bus_ctx[host_id]->destroy_func = f;
+    bus_ctx[host_id]->destroy_arg = arg;
+    return ESP_OK;
+}
+
 
 /*
 Code for workaround for DMA issue in ESP32 v0/v1 silicon

components/driver/CMakeLists.txt
@@ -18,6 +18,7 @@ set(srcs
     "spi_common.c"
     "spi_master.c"
     "spi_slave.c"
+    "spi_bus_lock.c"
     "timer.c"
     "touch_sensor_common.c"
     "uart.c")
@@ -45,7 +46,7 @@ idf_component_register(SRCS "${srcs}"
                     INCLUDE_DIRS ${includes}
                     PRIV_INCLUDE_DIRS "include/driver"
                     PRIV_REQUIRES efuse esp_timer
-                    REQUIRES esp_ringbuf soc) #cannot totally hide soc headers, since there are a lot arguments in the driver are chip-dependent
+                    REQUIRES esp_ringbuf freertos soc) #cannot totally hide soc headers, since there are a lot arguments in the driver are chip-dependent
 
 # uses C11 atomic feature
 set_source_files_properties(spi_master.c PROPERTIES COMPILE_FLAGS -std=gnu11)

components/driver/include/driver/spi_master.h
@@ -171,7 +171,7 @@ typedef struct spi_device_t* spi_device_handle_t;  ///< Handle for a device on a
  * @note While in general, speeds up to 80MHz on the dedicated SPI pins and 40MHz on GPIO-matrix-routed pins are
  *       supported, full-duplex transfers routed over the GPIO matrix only support speeds up to 26MHz.
  *
- * @param host SPI peripheral to allocate device on
+ * @param host_id SPI peripheral to allocate device on
  * @param dev_config SPI interface protocol config for the device
  * @param handle Pointer to variable to hold the device handle
  * @return
@@ -180,7 +180,7 @@ typedef struct spi_device_t* spi_device_handle_t;  ///< Handle for a device on a
  *         - ESP_ERR_NO_MEM        if out of memory
  *         - ESP_OK                on success
  */
-esp_err_t spi_bus_add_device(spi_host_device_t host, const spi_device_interface_config_t *dev_config, spi_device_handle_t *handle);
+esp_err_t spi_bus_add_device(spi_host_device_t host_id, const spi_device_interface_config_t *dev_config, spi_device_handle_t *handle);
 
 
 /**

components/driver/spi_slave.c
@@ -41,8 +41,6 @@ static const char *SPI_TAG = "spi_slave";
         return (ret_val); \
     }
 
-#define VALID_HOST(x) (x > SPI1_HOST && x <= SPI3_HOST)
-
 #ifdef CONFIG_SPI_SLAVE_ISR_IN_IRAM
 #define SPI_SLAVE_ISR_ATTR IRAM_ATTR
 #else
@@ -75,6 +73,16 @@ static spi_slave_t *spihost[SOC_SPI_PERIPH_NUM];
 
 static void IRAM_ATTR spi_intr(void *arg);
 
+static inline bool is_valid_host(spi_host_device_t host)
+{
+#if CONFIG_IDF_TARGET_ESP32
+    return host >= SPI1_HOST && host <= SPI3_HOST;
+#elif CONFIG_IDF_TARGET_ESP32S2
+// SPI_HOST (SPI1_HOST) is not supported by the SPI Slave driver on ESP32-S2
+    return host >= SPI2_HOST && host <= SPI3_HOST;
+#endif
+}
+
 static inline bool bus_is_iomux(spi_slave_t *host)
 {
     return host->flags&SPICOMMON_BUSFLAG_IOMUX_PINS;
@@ -102,7 +110,7 @@ esp_err_t spi_slave_initialize(spi_host_device_t host, const spi_bus_config_t *b
     esp_err_t ret = ESP_OK;
     esp_err_t err;
     //We only support HSPI/VSPI, period.
-    SPI_CHECK(VALID_HOST(host), "invalid host", ESP_ERR_INVALID_ARG);
+    SPI_CHECK(is_valid_host(host), "invalid host", ESP_ERR_INVALID_ARG);
 #if defined(CONFIG_IDF_TARGET_ESP32)
     SPI_CHECK( dma_chan >= 0 && dma_chan <= 2, "invalid dma channel", ESP_ERR_INVALID_ARG );
 #elif defined(CONFIG_IDF_TARGET_ESP32S2)
@@ -224,7 +232,7 @@ cleanup:
 
 esp_err_t spi_slave_free(spi_host_device_t host)
 {
-    SPI_CHECK(VALID_HOST(host), "invalid host", ESP_ERR_INVALID_ARG);
+    SPI_CHECK(is_valid_host(host), "invalid host", ESP_ERR_INVALID_ARG);
     SPI_CHECK(spihost[host], "host not slave", ESP_ERR_INVALID_ARG);
     if (spihost[host]->trans_queue) vQueueDelete(spihost[host]->trans_queue);
     if (spihost[host]->ret_queue) vQueueDelete(spihost[host]->ret_queue);
@@ -248,7 +256,7 @@ esp_err_t spi_slave_free(spi_host_device_t host)
 esp_err_t SPI_SLAVE_ATTR spi_slave_queue_trans(spi_host_device_t host, const spi_slave_transaction_t *trans_desc, TickType_t ticks_to_wait)
 {
     BaseType_t r;
-    SPI_CHECK(VALID_HOST(host), "invalid host", ESP_ERR_INVALID_ARG);
+    SPI_CHECK(is_valid_host(host), "invalid host", ESP_ERR_INVALID_ARG);
     SPI_CHECK(spihost[host], "host not slave", ESP_ERR_INVALID_ARG);
     SPI_CHECK(spihost[host]->dma_chan == 0 || trans_desc->tx_buffer==NULL || esp_ptr_dma_capable(trans_desc->tx_buffer),
 			"txdata not in DMA-capable memory", ESP_ERR_INVALID_ARG);
@@ -268,7 +276,7 @@ esp_err_t SPI_SLAVE_ATTR spi_slave_queue_trans(spi_host_device_t host, const spi
 esp_err_t SPI_SLAVE_ATTR spi_slave_get_trans_result(spi_host_device_t host, spi_slave_transaction_t **trans_desc, TickType_t ticks_to_wait)
 {
     BaseType_t r;
-    SPI_CHECK(VALID_HOST(host), "invalid host", ESP_ERR_INVALID_ARG);
+    SPI_CHECK(is_valid_host(host), "invalid host", ESP_ERR_INVALID_ARG);
     SPI_CHECK(spihost[host], "host not slave", ESP_ERR_INVALID_ARG);
     r = xQueueReceive(spihost[host]->ret_queue, (void *)trans_desc, ticks_to_wait);
     if (!r) return ESP_ERR_TIMEOUT;

commit bee9b6b9eb0150cba1190721b95f1ba966a41491

spi: fixed crash when calling spi_bus_free when not initialized
introduced in 49a48644e42458366b2dd7b7d153acc943d50e0f

Closes: https://github.com/espressif/esp-idf/issues/8642

components/driver/include/driver/spi_common.h
@@ -159,7 +159,7 @@ esp_err_t spi_bus_initialize(spi_host_device_t host_id, const spi_bus_config_t *
  * @param host_id SPI peripheral to free
  * @return
  *         - ESP_ERR_INVALID_ARG   if parameter is invalid
- *         - ESP_ERR_INVALID_STATE if not all devices on the bus are freed
+ *         - ESP_ERR_INVALID_STATE if bus hasn't been initialized before, or not all devices on the bus are freed
  *         - ESP_OK                on success
  */
 esp_err_t spi_bus_free(spi_host_device_t host_id);

components/driver/spi_common.c
@@ -862,6 +862,10 @@ const spi_bus_attr_t* spi_bus_get_attr(spi_host_device_t host_id)
 
 esp_err_t spi_bus_free(spi_host_device_t host_id)
 {
+    if (bus_ctx[host_id] == NULL) {
+        return ESP_ERR_INVALID_STATE;
+    }
+
     esp_err_t err = ESP_OK;
     spicommon_bus_context_t* ctx = bus_ctx[host_id];
     spi_bus_attr_t* bus_attr = &ctx->bus_attr;