58cca07104625df4f8ef1d89807c9481d5ebe52c
Matus Fabo <matus.fabo@espressif.com>
2022-04-08 15:06:30 +0200

    add: Kconfig assert or errorcode option add: private include header add: macro encapsulation for assertion or error check add: ESP_FAIL return code documentation in public headers change: replaced all assertions by NVS_ASSERT_OR_RETURN macro change: few internal function return values from void to esp_err_t change: ESP_ERR_NVS_VALUE_TOO_LONG macro comment

--- components/nvs_flash/test_nvs_host/test_compressed_enum_table.cpp
+++ components/nvs_flash/test_nvs_host/test_compressed_enum_table.cpp
@@ -1,16 +1,8 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2015-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include "catch.hpp"
 #include "compressed_enum_table.hpp"
 #include <cstring>
@@ -27,26 +19,28 @@ TEST_CASE("test if CompressedEnumTable works as expected", "[enumtable]")
     };
     CompressedEnumTable<TEnum1, 2, 252> table;
     memset(table.data(), 0xff, table.byteSize());
+    TEnum1 tmp;
     for (size_t i = 0; i < table.count(); ++i) {
-        CHECK(table.get(i) == TEnum1::THREE);
+        CHECK(table.get(i, &tmp) == ESP_OK);
+        CHECK(tmp == TEnum1::THREE);
     }
 
-    table.set(0, TEnum1::ONE);
-    table.set(1, TEnum1::TWO);
-    table.set(2, TEnum1::ZERO);
-    table.set(3, TEnum1::ONE);
-    table.set(4, TEnum1::TWO);
-    table.set(5, TEnum1::ZERO);
-    table.set(6, TEnum1::ONE);
-    table.set(7, TEnum1::TWO);
-    table.set(8, TEnum1::ZERO);
-    table.set(9, TEnum1::ZERO);
-    table.set(10, TEnum1::ONE);
-    table.set(11, TEnum1::TWO);
+    CHECK(table.set(0, TEnum1::ONE) == ESP_OK);
+    CHECK(table.set(1, TEnum1::TWO) == ESP_OK);
+    CHECK(table.set(2, TEnum1::ZERO) == ESP_OK);
+    CHECK(table.set(3, TEnum1::ONE) == ESP_OK);
+    CHECK(table.set(4, TEnum1::TWO) == ESP_OK);
+    CHECK(table.set(5, TEnum1::ZERO) == ESP_OK);
+    CHECK(table.set(6, TEnum1::ONE) == ESP_OK);
+    CHECK(table.set(7, TEnum1::TWO) == ESP_OK);
+    CHECK(table.set(8, TEnum1::ZERO) == ESP_OK);
+    CHECK(table.set(9, TEnum1::ZERO) == ESP_OK);
+    CHECK(table.set(10, TEnum1::ONE) == ESP_OK);
+    CHECK(table.set(11, TEnum1::TWO) == ESP_OK);
 //  table.set(12, ...
-    table.set(13, TEnum1::ZERO);
-    table.set(14, TEnum1::ONE);
-    table.set(15, TEnum1::TWO);
+    CHECK(table.set(13, TEnum1::ZERO) == ESP_OK);
+    CHECK(table.set(14, TEnum1::ONE) == ESP_OK);
+    CHECK(table.set(15, TEnum1::TWO) == ESP_OK);
 
     // b10010011100100001001001001001001
     // h   9   3   9   0   9   2   4   9
