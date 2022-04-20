f9da23edb914f16f2293d521dbb239cd1cc10bff
Laukik Hase <laukik.hase@espressif.com>
2022-04-20 14:57:47 +0530

    http_client: Added explicit conversion for esp_tls_error_handle_t - in protocols/esp_http_client example
    Closes https://github.com/espressif/esp-idf/issues/8795

--- examples/protocols/esp_http_client/main/esp_http_client_example.c
+++ examples/protocols/esp_http_client/main/esp_http_client_example.c
@@ -100,7 +100,7 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
         case HTTP_EVENT_DISCONNECTED:
             ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
             int mbedtls_err = 0;
-            esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+            esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)evt->data, &mbedtls_err, NULL);
             if (err != 0) {
                 ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
                 ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
