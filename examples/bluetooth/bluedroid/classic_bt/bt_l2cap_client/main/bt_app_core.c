7624d5dd93596d36fedfdc64b94c98f4f9cefefe
xiongweichao <xiongweichao@espressif.com>
2022-04-21 19:28:51 +0800

    add l2cap client demo

---
+++ examples/bluetooth/bluedroid/classic_bt/bt_l2cap_client/main/bt_app_core.c
@@ -0,0 +1,144 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include "freertos/xtensa_api.h"
+#include "freertos/FreeRTOSConfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "bt_app_core.h"
+#include "freertos/ringbuf.h"
+
+/*******************************
+ * STATIC FUNCTION DECLARATIONS
+ ******************************/
+
+/* handler for application task */
+static void bt_app_task_handler(void *arg);
+/* message sender */
+static bool bt_app_send_msg(bt_app_msg_t *msg);
+/* handle dispatched messages */
+static void bt_app_work_dispatched(bt_app_msg_t *msg);
+
+/*******************************
+ * STATIC VARIABLE DEFINITIONS
+ ******************************/
+
+static QueueHandle_t s_bt_app_task_queue = NULL;  /* handle of work queue */
+static TaskHandle_t s_bt_app_task_handle = NULL;  /* handle of application task  */
+
+/*******************************
+ * STATIC FUNCTION DEFINITIONS
+ ******************************/
+
+static bool bt_app_send_msg(bt_app_msg_t *msg)
+{
+    if (msg == NULL) {
+        return false;
+    }
+
+    /* send the message to work queue */
+    if (xQueueSend(s_bt_app_task_queue, msg, 10 / portTICK_PERIOD_MS) != pdTRUE) {
+        ESP_LOGE(BT_APP_CORE_TAG, "%s xQueue send failed", __func__);
+        return false;
+    }
+    return true;
+}
+
+static void bt_app_work_dispatched(bt_app_msg_t *msg)
+{
+    if (msg->cb) {
+        msg->cb(msg->event, msg->param);
+    }
+}
+
+static void bt_app_task_handler(void *arg)
+{
+    bt_app_msg_t msg;
+
+    for (;;) {
+        /* receive message from work queue and handle it */
+        if (pdTRUE == xQueueReceive(s_bt_app_task_queue, &msg, (TickType_t)portMAX_DELAY)) {
+            ESP_LOGD(BT_APP_CORE_TAG, "%s, signal: 0x%x, event: 0x%x", __func__, msg.sig, msg.event);
+
+            switch (msg.sig) {
+            case BT_APP_SIG_WORK_DISPATCH:
+                bt_app_work_dispatched(&msg);
+                break;
+            default:
+                ESP_LOGW(BT_APP_CORE_TAG, "%s, unhandled signal: %d", __func__, msg.sig);
+                break;
+            } /* switch (msg.sig) */
+
+            if (msg.param) {
+                free(msg.param);
+            }
+        }
+    }
+}
+
+/********************************
+ * EXTERNAL FUNCTION DEFINITIONS
+ *******************************/
+
+bool bt_app_work_dispatch(bt_app_cb_t p_cback, uint16_t event, void *p_params, int param_len, bt_app_copy_cb_t p_copy_cback)
+{
+    ESP_LOGD(BT_APP_CORE_TAG, "%s event: 0x%x, param len: %d", __func__, event, param_len);
+
+    bt_app_msg_t msg;
+    memset(&msg, 0, sizeof(bt_app_msg_t));
+
+    msg.sig = BT_APP_SIG_WORK_DISPATCH;
+    msg.event = event;
+    msg.cb = p_cback;
+
+    if (param_len == 0) {
+        return bt_app_send_msg(&msg);
+    } else if (p_params && param_len > 0) {
+        if ((msg.param = malloc(param_len)) != NULL) {
+            memcpy(msg.param, p_params, param_len);
+            /* check if caller has provided a copy callback to do the deep copy */
+            if (p_copy_cback) {
+                p_copy_cback(msg.param, p_params, param_len);
+            }
+            return bt_app_send_msg(&msg);
+        }
+    }
+
+    return false;
+}
+
+void bt_app_task_start_up(void)
+{
+    s_bt_app_task_queue = xQueueCreate(10, sizeof(bt_app_msg_t));
+    xTaskCreate(bt_app_task_handler, "BtAppTask", 3072, NULL, configMAX_PRIORITIES - 3, &s_bt_app_task_handle);
+}
+
+void bt_app_task_shut_down(void)
+{
+    if (s_bt_app_task_handle) {
+        vTaskDelete(s_bt_app_task_handle);
+        s_bt_app_task_handle = NULL;
+    }
+    if (s_bt_app_task_queue) {
+        vQueueDelete(s_bt_app_task_queue);
+        s_bt_app_task_queue = NULL;
+    }
+}
+
+void l2cap_wr_task_start_up(l2cap_wr_task_cb_t p_cback, int fd)
+{
+    xTaskCreate(p_cback, "write_read", 2048, (void *)fd, 5, NULL);
+}
+
+void l2cap_wr_task_shut_down(void)
+{
+    vTaskDelete(NULL);
+}
