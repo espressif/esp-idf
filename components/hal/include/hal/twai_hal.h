09dc46552b16f3549e47ea897401050de407534a
Darian Leung <darian@espressif.com>
2022-04-22 15:23:56 +0800

    twai: Fix twai_ll_parse_frame_buffer() typo
    Closes https://github.com/espressif/esp-idf/issues/8743

--- components/hal/include/hal/twai_hal.h
+++ components/hal/include/hal/twai_hal.h
@@ -1,16 +1,8 @@
-// Copyright 2015-2019 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2015-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 /*******************************************************************************
  * NOTICE
@@ -253,7 +245,7 @@ static inline void twai_hal_format_frame(const twai_message_t *message, twai_hal
 static inline void twai_hal_parse_frame(twai_hal_frame_t *frame, twai_message_t *message)
 {
     //Direct call to ll function
-    twai_ll_prase_frame_buffer(frame, &message->identifier, &message->data_length_code,
+    twai_ll_parse_frame_buffer(frame, &message->identifier, &message->data_length_code,
                               message->data, &message->flags);
 }
 
