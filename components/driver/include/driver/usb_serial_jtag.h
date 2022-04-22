9d93aef2b7d49527f2b263df84d5ecbc16822b9f
david zuhn <zoo@blueknobby.com>
2022-04-22 00:23:53 -0500

    usb_serial_jtag: make CONFIG_DEFAULT initializer match order of structure definition

--- components/driver/include/driver/usb_serial_jtag.h
+++ components/driver/include/driver/usb_serial_jtag.h
@@ -33,8 +33,8 @@ typedef struct {
 } usb_serial_jtag_driver_config_t;
 
 #define USB_SERIAL_JTAG_DRIVER_CONFIG_DEFAULT() (usb_serial_jtag_driver_config_t) {\
-    .rx_buffer_size = 256,\
     .tx_buffer_size = 256,\
+    .rx_buffer_size = 256,\
 }
 
 /**
