5aa01cb80ebcc1befbfb0caea7d7c4555e05603d
Marius Vikhammer <marius.vikhammer@espressif.com>
2022-04-28 15:59:03 +0800

    docs: update linkcheck configs
    linkcheck CI job can now pass, no longer necessary to ignore the results from it.

--- docs/conf_common.py
+++ docs/conf_common.py
@@ -196,6 +196,21 @@ google_analytics_id = os.environ.get('CI_GOOGLE_ANALYTICS_ID', None)
 
 project_homepage = 'https://github.com/espressif/esp-idf'
 
+linkcheck_anchors = False
+
+linkcheck_exclude_documents = ['index',  # several false positives due to the way we link to different sections
+                               'api-reference/protocols/esp_local_ctrl',  # Fails due to `https://<mdns-hostname>.local`
+                               'api-reference/provisioning/wifi_provisioning',  # Fails due to `https://<mdns-hostname>.local`
+                               ]
+
+
+linkcheck_ignore = ['https://webhome.phy.duke.edu/~rgb/General/dieharder.php',  # Certificate error
+                    'https://dl.espressif.com/dl/esp32s3_rom.elf',  # Not published
+                    'https://docs.espressif.com/projects/esptool/en/latest/esp32c2/espefuse/index.html',  # Not published
+                    'https://www.espressif.com/sites/default/files/documentation/esp32-c2_datasheet_en.pdf',  # Not published
+                    ]
+
+
 # Custom added feature to allow redirecting old URLs
 with open('../page_redirects.txt') as f:
     lines = [re.sub(' +', ' ', line.strip()) for line in f.readlines() if line.strip() != '' and not line.startswith('#')]
