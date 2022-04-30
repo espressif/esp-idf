780f7f2669d9c8ebd2c335260ba713d94f8a4c41
Aditya Patwardhan <aditya.patwardhan@espressif.com>
2022-04-30 11:07:39 +0530

    secure_boot_test_app: Update the test_app to use pytest framework * Added custom class IdfFpgaDut in pytest from ttfw_idf.IDFFPGADUT

--- tools/test_apps/security/secure_boot/pytest_secure_boot.py
+++ tools/test_apps/security/secure_boot/pytest_secure_boot.py
@@ -1,11 +1,13 @@
+# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Unlicense OR CC0-1.0
 from __future__ import print_function
 
 import os
 import struct
 import zlib
-from io import BytesIO
 
-import ttfw_idf
+import pytest
+from pytest_embedded import Dut
 
 # To prepare a runner for these tests,
 # 1. Connect an FPGA with C3 image
@@ -65,65 +67,67 @@ def corrupt_sig_block(sig_block, seed=0, corrupt_sig=True, corrupt_crc=False):
     return result
 
 
-def dut_start_secure_app(dut):      # type: (ttfw_idf.IDFDUT) -> None
-    dut.reset_efuses()
-    bootloader_bin = os.path.join(dut.app.binary_path, 'bootloader/bootloader.bin')
-    with open(bootloader_bin, 'rb') as f:
-        dut.write_flash_data([(0x0, f)], None, True, False)
-    dut.start_app()
+def dut_start_secure_app(dut: Dut) -> None:
+    dut.serial.erase_flash()
+    dut.serial.reset_efuses()
+    dut.burn_wafer_version()
+
+    bootloader_path = os.path.join(dut.app.binary_path, 'bootloader/bootloader.bin')
+    dut.serial.bootloader_flash(bootloader_path)
+    dut.serial.flash()
 
 
 # Test secure boot flow.
 # Correctly signed bootloader + correctly signed app should work
-@ttfw_idf.idf_custom_test(env_tag='Example_Secure_Boot', target=['esp32c3fpga', 'esp32s3fpga'], ignore=True)
-def test_examples_security_secure_boot(env, _):    # type: (ttfw_idf.TinyFW.Env, None) -> None
-    efuse_port = os.getenv('EFUSEPORT')
-    dut = env.get_dut('secure_boot', 'tools/test_apps/security/secure_boot', efuse_reset_port=efuse_port)
+@pytest.mark.esp32c3
+@pytest.mark.esp32s3
+def test_examples_security_secure_boot(dut: Dut) -> None:
     dut_start_secure_app(dut)
-    dut.expect('Secure Boot is enabled', timeout=2)
+    dut.expect('Secure Boot is enabled', timeout=10)
+    dut.serial.reset_efuses()
 
 
 # Test efuse key index and key block combination.
 # Any key index can be written to any key block and should work
-@ttfw_idf.idf_custom_test(env_tag='Example_Secure_Boot', target=['esp32c3fpga', 'esp32s3fpga'], ignore=True)
-def test_examples_security_secure_boot_key_combo(env, _):      # type: (ttfw_idf.TinyFW.Env, None) -> None
-    efuse_port = os.getenv('EFUSEPORT')
-    dut = env.get_dut('secure_boot', 'tools/test_apps/security/secure_boot', efuse_reset_port=efuse_port)
+@pytest.mark.esp32c3
+@pytest.mark.esp32s3
+def test_examples_security_secure_boot_key_combo(dut: Dut) -> None:
     dut_start_secure_app(dut)
+    dut.expect('Secure Boot is enabled', timeout=10)
     for index in range(3):
+        print(index)
         for block in range(6):
-            dut.reset_efuses()
+            dut.serial.reset_efuses()
+            dut.burn_wafer_version()
             dut.secure_boot_burn_en_bit()
             dut.secure_boot_burn_digest('test_rsa_3072_key.pem', index, block)
-            dut.reset()
-            dut.expect('Secure Boot is enabled', timeout=2)
+            dut.expect('Secure Boot is enabled', timeout=10)
 
 
 # Test secure boot key revoke.
 # If a key is revoked, bootloader signed with that key should fail verification
-@ttfw_idf.idf_custom_test(env_tag='Example_Secure_Boot', target=['esp32c3fpga', 'esp32s3fpga'], ignore=True)
-def test_examples_security_secure_boot_key_revoke(env, _):     # type: (ttfw_idf.TinyFW.Env, None) -> None
-    efuse_port = os.getenv('EFUSEPORT')
-    dut = env.get_dut('secure_boot', 'tools/test_apps/security/secure_boot', efuse_reset_port=efuse_port)
+@pytest.mark.esp32c3
+@pytest.mark.esp32s3
+def test_examples_security_secure_boot_key_revoke(dut: Dut) -> None:
     dut_start_secure_app(dut)
+    dut.expect('Secure Boot is enabled', timeout=10)
     for index in range(3):
-        dut.reset_efuses()
+        dut.serial.reset_efuses()
+        dut.burn_wafer_version()
         dut.secure_boot_burn_en_bit()
+        dut.serial.burn_efuse('SECURE_BOOT_KEY_REVOKE%d' % index, 1)
         dut.secure_boot_burn_digest('test_rsa_3072_key.pem', index, 0)
-        dut.burn_efuse('SECURE_BOOT_KEY_REVOKE%d' % index, 1)
-        dut.reset()
-        dut.expect('secure boot verification failed', timeout=2)
+        dut.expect('secure boot verification failed', timeout=5)
 
 
 # Test bootloader signature corruption.
 # Corrupt one byte at a time of bootloader signature and test that the verification fails
-@ttfw_idf.idf_custom_test(env_tag='Example_Secure_Boot', target=['esp32c3fpga', 'esp32s3fpga'], ignore=True)
-def test_examples_security_secure_boot_corrupt_bl_sig(env, _):     # type: (ttfw_idf.TinyFW.Env, None) -> None
-    efuse_port = os.getenv('EFUSEPORT')
-    dut = env.get_dut('secure_boot', 'tools/test_apps/security/secure_boot', efuse_reset_port=efuse_port)
-    dut.reset_efuses()
-    dut.secure_boot_burn_en_bit()
-    dut.secure_boot_burn_digest('test_rsa_3072_key.pem', 0, 0)
+@pytest.mark.esp32c3
+@pytest.mark.esp32s3
+def test_examples_security_secure_boot_corrupt_bl_sig(dut: Dut) -> None:
+    dut_start_secure_app(dut)
+    dut.expect('Secure Boot is enabled', timeout=10)
+
     bootloader_bin = os.path.join(dut.app.binary_path, 'bootloader/bootloader.bin')
     with open(bootloader_bin, 'rb') as f:
         signed_bl = f.read()
@@ -134,20 +138,27 @@ def test_examples_security_secure_boot_corrupt_bl_sig(env, _):     # type: (ttfw
     for seed in seeds:
         print('Case %d / %d' % (seed, max_seed))
         corrupt_bl = corrupt_signature(signed_bl, seed=seed)
-        dut.write_flash_data([(0x0, BytesIO(corrupt_bl))])
-        dut.expect('Signature Check Failed', timeout=2)
+        with open('corrupt_bl.bin', 'wb') as corrupt_file:
+            corrupt_file.write(corrupt_bl)
+        dut.serial.reset_efuses()
+        dut.burn_wafer_version()
+        dut.serial.bootloader_flash('corrupt_bl.bin')
+        dut.secure_boot_burn_en_bit()
+        dut.secure_boot_burn_digest('test_rsa_3072_key.pem', 0, 0)
+        # Though the order of flashing and burning efuse would not effect the test,
+        # if we flash bootlader before burning en bit, even with no_stub = True
+        # it still calls run_stub() and throws an error as it fails to start stub.
+        dut.expect('Signature Check Failed', timeout=10)
 
 
 # Test app signature corruption.
 # Corrupt app signature, one byte at a time, and test that the verification fails
-@ttfw_idf.idf_custom_test(env_tag='Example_Secure_Boot', target=['esp32c3fpga', 'esp32s3fpga'], ignore=True)
-def test_examples_security_secure_boot_corrupt_app_sig(env, _):    # type: (ttfw_idf.TinyFW.Env, None) -> None
-    efuse_port = os.getenv('EFUSEPORT')
-    dut = env.get_dut('secure_boot', 'tools/test_apps/security/secure_boot', efuse_reset_port=efuse_port)
+@pytest.mark.esp32c3
+@pytest.mark.esp32s3
+def test_examples_security_secure_boot_corrupt_app_sig(dut: Dut) -> None:
     dut_start_secure_app(dut)
-    dut.reset_efuses()
-    dut.secure_boot_burn_en_bit()
-    dut.secure_boot_burn_digest('test_rsa_3072_key.pem', 0, 0)
+    dut.expect('Secure Boot is enabled', timeout=10)
+
     app_bin = os.path.join(dut.app.binary_path, 'secure_boot.bin')
     with open(app_bin, 'rb') as f:
         signed_app = f.read()
@@ -158,26 +169,30 @@ def test_examples_security_secure_boot_corrupt_app_sig(env, _):    # type: (ttfw
     for seed in seeds:
         print('Case %d / %d' % (seed, max_seed))
         corrupt_app = corrupt_signature(signed_app, seed=seed)
-        dut.write_flash_data([(0x20000, BytesIO(corrupt_app))])
-        dut.expect('Signature Check Failed', timeout=2)
+        with open('corrupt_app.bin', 'wb') as corrupt_file:
+            corrupt_file.write(corrupt_app)
+        dut.serial.reset_efuses()
+        dut.burn_wafer_version()
+        dut.serial.app_flash('corrupt_app.bin')
+        dut.secure_boot_burn_en_bit()
+        dut.secure_boot_burn_digest('test_rsa_3072_key.pem', 0, 0)
+        dut.expect('Signature Check Failed', timeout=10)
         dut.expect('image valid, signature bad', timeout=2)
 
     print('Testing invalid CRC...')
     # Valid signature but invalid CRC
-    dut.reset_efuses()
+    dut.serial.reset_efuses()
+    dut.burn_wafer_version()
+
+    corrupt_app = corrupt_signature(signed_app, corrupt_sig=False, corrupt_crc=True)
+    with open('corrupt_app.bin', 'wb') as corrupt_file:
+        corrupt_file.write(corrupt_app)
+
+    dut.serial.app_flash('corrupt_app.bin')
+
     dut.secure_boot_burn_en_bit()
     dut.secure_boot_burn_digest('test_rsa_3072_key.pem', 0, 0)
 
-    corrupt_app = corrupt_signature(signed_app, corrupt_sig=False, corrupt_crc=True)
-    dut.write_flash_data([(0x20000, BytesIO(corrupt_app))])
     dut.expect('Sig block 0 invalid: Stored CRC ends', timeout=2)
     dut.expect('Secure boot signature verification failed', timeout=2)
     dut.expect('No bootable app partitions in the partition table', timeout=2)
-
-
-if __name__ == '__main__':
-    test_examples_security_secure_boot()
-    test_examples_security_secure_boot_key_combo()
-    test_examples_security_secure_boot_key_revoke()
-    test_examples_security_secure_boot_corrupt_bl_sig()
-    test_examples_security_secure_boot_corrupt_app_sig()
