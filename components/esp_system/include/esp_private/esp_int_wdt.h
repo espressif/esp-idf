bd8b03888c10d5206b03f94db4e6ac7946c5eae3
Darian Leung <darian@espressif.com>
2022-05-05 17:04:59 +0800

    esp_system: Tidy up INT WDT
    This commit tidys the INT WDT code formatting, comments, and API descriptions.

--- components/esp_system/include/esp_private/esp_int_wdt.h
+++ components/esp_system/include/esp_private/esp_int_wdt.h
@@ -4,56 +4,30 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef __ESP_INT_WDT_H
-#define __ESP_INT_WDT_H
+#pragma once
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/** @addtogroup Watchdog_APIs
-  * @{
-  */
-
-/*
-This routine enables a watchdog to catch instances of processes disabling
-interrupts for too long, or code within interrupt handlers taking too long.
-It does this by setting up a watchdog which gets fed from the FreeRTOS
-task switch interrupt. When this watchdog times out, initially it will call
-a high-level interrupt routine that will panic FreeRTOS in order to allow
-for forensic examination of the state of the both CPUs. When this interrupt
-handler is not called and the watchdog times out a second time, it will
-reset the SoC.
-
-This uses the TIMERG1 WDT.
-*/
-
-
 /**
-  * @brief  Initialize the non-CPU-specific parts of interrupt watchdog.
-  *         This is called in the init code if the interrupt watchdog
-  *         is enabled in menuconfig.
-  *
-  */
+ * @brief Initialize the non-CPU-specific parts of interrupt watchdog.
+ *
+ * This function is automatically called during application startup if the
+ * interrupt watchdog is enabled in menuconfig.
+ */
 void esp_int_wdt_init(void);
 
 /**
-  * @brief  Enable the interrupt watchdog on the current CPU. This is called
-  *         in the init code by both CPUs if the interrupt watchdog is enabled
-  *         in menuconfig.
-  *
-  */
+ * @brief Enable the interrupt watchdog on the current CPU.
+ *
+ * This function is automatically called during application startup for each CPU
+ * that has enabled the interrupt watchdog in menuconfig.
+ *
+ * @note esp_int_wdt_init() must be called first before calling this function
+ */
 void esp_int_wdt_cpu_init(void);
-
-
-
-/**
-  * @}
-  */
-
 
 #ifdef __cplusplus
 }
-#endif
-
 #endif
