6d288575dc51823eea4a267b4e327406755a91cd
morris <maoshengrong@espressif.com>
2022-05-09 18:10:08 +0800

    test: fix broken host test of idf_as_lib

--- examples/build_system/cmake/idf_as_lib/stubs/freertos/task.c
+++ examples/build_system/cmake/idf_as_lib/stubs/freertos/task.c
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
 #include <unistd.h>
 #include "freertos/task.h"
 
