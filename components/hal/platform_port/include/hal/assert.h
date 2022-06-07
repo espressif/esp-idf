6f6251f3694b1ee5f3a3cfb61e32edd268843563
songruojing <songruojing@espressif.com>
2022-06-07 12:15:14 +0800

    hal: Limit the HAL_ASSERTION_LEVEL in bootloader to be no larger than 1 (i.e. silent)

--- components/hal/platform_port/include/hal/assert.h
+++ components/hal/platform_port/include/hal/assert.h
@@ -1,16 +1,8 @@
-// Copyright 2021 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #pragma once
 
 #include "sdkconfig.h"
@@ -27,6 +19,15 @@ extern void abort(void);
 #define __ASSERT_FUNC __ASSERT_FUNCTION
 #else
 #define __ASSERT_FUNC "??"
+#endif
+#endif
+
+#if BOOTLOADER_BUILD
+// Bootloader has very limited size, while full assertion takes up quite a lot bytes as it prints file, line, and
+// function info. Therefore, we set the HAL assertion level in bootloader to be no larger than 1 (silent).
+#if CONFIG_HAL_DEFAULT_ASSERTION_LEVEL == 2
+#undef CONFIG_HAL_DEFAULT_ASSERTION_LEVEL
+#define CONFIG_HAL_DEFAULT_ASSERTION_LEVEL 1
 #endif
 #endif
 
