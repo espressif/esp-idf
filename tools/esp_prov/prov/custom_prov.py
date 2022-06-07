a67ba5e6793992c4649056918e5a4465bdff768f
Cody Cutrer <cody@cutrer.us>
2022-06-07 10:16:23 -0600

    esp_prov: switch from bluez/dbus to bleak
    to enable multiplatform ble compatibility

--- tools/esp_prov/prov/custom_prov.py
+++ tools/esp_prov/prov/custom_prov.py
@@ -19,7 +19,7 @@ def custom_data_request(security_ctx, data):
     # Encrypt the custom data
     enc_cmd = security_ctx.encrypt_data(tobytes(data))
     print_verbose(security_ctx, 'Client -> Device (CustomData cmd) ' + utils.str_to_hexstr(enc_cmd))
-    return enc_cmd
+    return enc_cmd.decode('latin-1')
 
 
 def custom_data_response(security_ctx, response_data):
--- tools/esp_prov/transport/transport_console.py
+++ tools/esp_prov/transport/transport_console.py
@@ -1,16 +1,5 @@
-# Copyright 2018 Espressif Systems (Shanghai) PTE LTD
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-FileCopyrightText: 2018-2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
 #
 
 from __future__ import print_function
@@ -24,7 +13,7 @@ from .transport import Transport
 
 class Transport_Console(Transport):
 
-    def send_data(self, path, data, session_id=0):
+    async def send_data(self, path, data, session_id=0):
         print('Client->Device msg :', path, session_id, utils.str_to_hexstr(data))
         try:
             resp = input('Enter device->client msg : ')
