30e7af2ffb9544de3c7107c5cef787aaf42ab29e
jingli <jingli@espressif.com>
2022-06-14 14:49:26 +0800

    system/sleep: simplify code for time compensation when wakeup from light sleep

--- components/esp_timer/src/esp_timer_impl_systimer.c
+++ components/esp_timer/src/esp_timer_impl_systimer.c
@@ -101,10 +101,19 @@ void IRAM_ATTR esp_timer_impl_update_apb_freq(uint32_t apb_ticks_per_us)
 #endif
 }
 
-void esp_timer_impl_advance(int64_t time_us)
+void esp_timer_impl_set(uint64_t new_us)
 {
     portENTER_CRITICAL_SAFE(&s_time_update_lock);
-    systimer_hal_counter_value_advance(&systimer_hal, SYSTIMER_LL_COUNTER_CLOCK, time_us);
+    systimer_counter_value_t new_count = { .val = new_us * SYSTIMER_LL_TICKS_PER_US };
+    systimer_ll_set_counter_value(systimer_hal.dev, SYSTIMER_LL_COUNTER_CLOCK, new_count.val);
+    systimer_ll_apply_counter_value(systimer_hal.dev, SYSTIMER_LL_COUNTER_CLOCK);
+    portEXIT_CRITICAL_SAFE(&s_time_update_lock);
+}
+
+void esp_timer_impl_advance(int64_t time_diff_us)
+{
+    portENTER_CRITICAL_SAFE(&s_time_update_lock);
+    systimer_hal_counter_value_advance(&systimer_hal, SYSTIMER_LL_COUNTER_CLOCK, time_diff_us);
     portEXIT_CRITICAL_SAFE(&s_time_update_lock);
 }
 
@@ -192,6 +201,7 @@ uint64_t esp_timer_impl_get_alarm_reg(void)
 }
 
 void esp_timer_private_update_apb_freq(uint32_t apb_ticks_per_us) __attribute__((alias("esp_timer_impl_update_apb_freq")));
-void esp_timer_private_advance(int64_t time_us) __attribute__((alias("esp_timer_impl_advance")));
+void esp_timer_private_set(uint64_t new_us) __attribute__((alias("esp_timer_impl_set")));
+void esp_timer_private_advance(int64_t time_diff_us) __attribute__((alias("esp_timer_impl_advance")));
 void esp_timer_private_lock(void) __attribute__((alias("esp_timer_impl_lock")));
 void esp_timer_private_unlock(void) __attribute__((alias("esp_timer_impl_unlock")));
