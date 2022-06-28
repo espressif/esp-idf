12180aabfaa3651e73addffe3c1dd9ecba3858ce
Fu Hanxi <fuhanxi@espressif.com>
2022-06-28 12:00:11 +0800

    build&config: lift property IDF_COMPONENT_MANAGER to root level

--- tools/cmake/project.cmake
+++ tools/cmake/project.cmake
@@ -37,6 +37,13 @@ endif()
 # value passed externally.
 __target_init()
 
+# Enable the component manager for regular projects if not explicitly disabled.
+if(NOT "$ENV{IDF_COMPONENT_MANAGER}" EQUAL "0")
+    idf_build_set_property(IDF_COMPONENT_MANAGER 1)
+endif()
+# Set component manager interface version
+idf_build_set_property(__COMPONENT_MANAGER_INTERFACE_VERSION 0)
+
 #
 # Get the project version from either a version file or the Git revision. This is passed
 # to the idf_build_process call. Dependencies are also set here for when the version file
@@ -172,8 +179,6 @@ function(__project_init components_var test_components_var)
         endif()
     endfunction()
 
-    idf_build_set_property(IDF_COMPONENT_MANAGER "$ENV{IDF_COMPONENT_MANAGER}")
-
     # Add component directories to the build, given the component filters, exclusions
     # extra directories, etc. passed from the root CMakeLists.txt.
     if(COMPONENT_DIRS)
@@ -187,39 +192,6 @@ function(__project_init components_var test_components_var)
         # Add project manifest and lock file to the list of dependencies
         set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${CMAKE_CURRENT_LIST_DIR}/idf_project.yml")
         set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${CMAKE_CURRENT_LIST_DIR}/dependencies.lock")
-
-        idf_build_get_property(idf_component_manager IDF_COMPONENT_MANAGER)
-        if(idf_component_manager)
-            if(idf_component_manager EQUAL "0")
-                message(VERBOSE "IDF Component manager was explicitly disabled by setting IDF_COMPONENT_MANAGER=0")
-            elseif(idf_component_manager EQUAL "1")
-                set(managed_components_list_file ${CMAKE_BINARY_DIR}/managed_components_list.temp.cmake)
-
-                # Call for package manager to prepare remote dependencies
-                execute_process(COMMAND ${PYTHON}
-                    "-m"
-                    "idf_component_manager.prepare_components"
-                    "--project_dir=${CMAKE_CURRENT_LIST_DIR}"
-                    "prepare_dependencies"
-                    "--managed_components_list_file=${managed_components_list_file}"
-                    RESULT_VARIABLE result
-                    ERROR_VARIABLE error)
-
-                if(NOT result EQUAL 0)
-                    message(FATAL_ERROR "${error}")
-                endif()
-
-                # Include managed components
-                include(${managed_components_list_file})
-                file(REMOVE ${managed_components_list_file})
-            else()
-                message(WARNING "IDF_COMPONENT_MANAGER environment variable is set to unknown value "
-                        "\"${idf_component_manager}\". If you want to use component manager set it to 1.")
-            endif()
-        elseif(EXISTS "${CMAKE_CURRENT_LIST_DIR}/idf_project.yml")
-            message(WARNING "\"idf_project.yml\" file is found in project directory, "
-                    "but component manager is not enabled. Please set IDF_COMPONENT_MANAGER environment variable.")
-        endif()
 
         spaces2list(EXTRA_COMPONENT_DIRS)
         foreach(component_dir ${EXTRA_COMPONENT_DIRS})
