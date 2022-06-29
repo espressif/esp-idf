a5f1820120bfc4f058c0a58d26eadec073cd60d4
Aditya Patwardhan <aditya.patwardhan@espressif.com>
2022-06-29 14:13:35 +0530

    esp_hw_support: Merge esp_hmac.h file for different targets into one

--- components/esp_hw_support/include/esp_hmac.h
+++ components/esp_hw_support/include/esp_hmac.h
@@ -1,14 +1,18 @@
 /*
- * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2015-2021 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef _ESP_HMAC_H_
-#define _ESP_HMAC_H_
+#pragma once
 
 #include <stdbool.h>
 #include "esp_err.h"
+#include "soc/soc_caps.h"
+
+#if !SOC_HMAC_SUPPORTED && !CI_HEADER_CHECK
+#error "HMAC peripheral is not supported for the selected target"
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,7 +36,7 @@ typedef enum {
  * Calculate the HMAC of a given message.
  *
  * Calculate the HMAC \c hmac of a given message \c message with length \c message_len.
- * SHA256 is used for the calculation (fixed on ESP32S2).
+ * SHA256 is used for the calculation.
  *
  * @note Uses the HMAC peripheral in "upstream" mode.
  *
@@ -40,7 +44,7 @@ typedef enum {
  *        The corresponding purpose field of the key block in the efuse must be set to the HMAC upstream purpose value.
  * @param message the message for which to calculate the HMAC
  * @param message_len message length
- *             return ESP_ERR_INVALID_STATE if unsuccessful
+ *                         return ESP_ERR_INVALID_STATE if unsuccessful
  * @param [out] hmac the hmac result; the buffer behind the provided pointer must be a writeable buffer of 32 bytes
  *
  * @return
@@ -49,9 +53,9 @@ typedef enum {
  *      * ESP_FAIL, if the hmac calculation failed
  */
 esp_err_t esp_hmac_calculate(hmac_key_id_t key_id,
-        const void *message,
-        size_t message_len,
-        uint8_t *hmac);
+                             const void *message,
+                             size_t message_len,
+                             uint8_t *hmac);
 
 /**
  * @brief Use HMAC peripheral in Downstream mode to re-enable the JTAG, if it is not permanently disabled by HW.
@@ -88,5 +92,3 @@ esp_err_t esp_hmac_jtag_disable(void);
 #ifdef __cplusplus
 }
 #endif
-
-#endif // _ESP_HMAC_H_
