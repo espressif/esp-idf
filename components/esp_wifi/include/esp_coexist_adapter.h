bc2f2a31003e9a2f50025ed3b33b8f87804fe31d
liuning <liuning@espressif.com>
2022-07-05 19:53:35 +0800

    esp_wifi: select slowclk as coex hardware timer source on esp32c2

--- components/esp_wifi/include/esp_coexist_adapter.h
+++ components/esp_wifi/include/esp_coexist_adapter.h
@@ -1,16 +1,8 @@
-// Copyright 2019 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2019-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #ifndef __ESP_COEXIST_ADAPTER_H__
 #define __ESP_COEXIST_ADAPTER_H__
@@ -51,6 +43,8 @@ typedef struct {
     void (* _timer_arm_us)(void *ptimer, uint32_t us, bool repeat);
 #endif
     int64_t (* _esp_timer_get_time)(void);
+    bool (* _env_is_chip)(void);
+    uint32_t (* _slowclk_cal_get)(void);
     int32_t _magic;
 } coex_adapter_funcs_t;
 
