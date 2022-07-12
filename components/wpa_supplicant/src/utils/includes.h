6b6bb3a321395ec8b57703c343f03378dff99552
wuzhenghui <wuzhenghui@espressif.com>
2022-07-12 22:00:58 +0800

    esp32c6: add wpa_supplicant support

--- components/wpa_supplicant/src/utils/includes.h
+++ components/wpa_supplicant/src/utils/includes.h
@@ -71,6 +71,8 @@
 #include "esp32c2/rom/ets_sys.h"
 #elif CONFIG_IDF_TARGET_ESP32H2
 #include "esp32h2/rom/ets_sys.h"
+#elif CONFIG_IDF_TARGET_ESP32C6
+#include "esp32c6/rom/ets_sys.h"
 #endif
 
 #endif /* !__ets__ */
