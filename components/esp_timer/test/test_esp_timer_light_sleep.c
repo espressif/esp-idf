4ab8f00b47c8d4be74dc622fe0dde0b2519374ec
laokaiyao <laokaiyao@espressif.com>
2022-07-13 18:41:55 +0800

    ci: setup 26mhz esp32c2 runner

--- components/esp_timer/test/test_esp_timer_light_sleep.c
+++ components/esp_timer/test/test_esp_timer_light_sleep.c
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,7 +12,6 @@
 #include "unity.h"
 #include "esp_rom_sys.h"
 #include "esp_sleep.h"
-
 
 static void timer_cb1(void *arg)
 {
