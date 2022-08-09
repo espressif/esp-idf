92ef2a4c835ac5d6043ee14678ce0c5c941d3c63
simon.chupin <simon.chupin@espressif.com>
2022-08-09 15:39:23 +0200

    Tools: Add unit tests for idf.py hints

--- tools/idf_py_actions/core_ext.py
+++ tools/idf_py_actions/core_ext.py
@@ -18,8 +18,8 @@ from click.core import Context
 from idf_py_actions.constants import GENERATORS, PREVIEW_TARGETS, SUPPORTED_TARGETS, URL_TO_DOC
 from idf_py_actions.errors import FatalError
 from idf_py_actions.global_options import global_options
-from idf_py_actions.tools import (PropertyDict, TargetChoice, ensure_build_directory, get_target, idf_version,
-                                  merge_action_lists, print_hints, realpath, run_target)
+from idf_py_actions.tools import (PropertyDict, TargetChoice, ensure_build_directory, generate_hints, get_target,
+                                  idf_version, merge_action_lists, realpath, run_target, yellow_print)
 
 
 def action_extensions(base_actions: Dict, project_path: str) -> Any:
@@ -41,7 +41,8 @@ def action_extensions(base_actions: Dict, project_path: str) -> Any:
         """
 
         def tool_error_handler(e: int, stdout: str, stderr: str) -> None:
-            print_hints(stdout, stderr)
+            for hint in generate_hints(stdout, stderr):
+                yellow_print(hint)
 
         if output_format:
             os.environ['SIZE_OUTPUT_FORMAT'] = output_format
