89cede3895b32b03e06074d6bee28a986c6aada1
chenjianxing <chenjianxing@espressif.com>
2022-08-25 14:26:14 +0800

    esp_wifi: fix espnow example add peer fail when config channel
    Closes https://github.com/espressif/esp-idf/issues/9592

--- examples/wifi/espnow/main/espnow_example_main.c
+++ examples/wifi/espnow/main/espnow_example_main.c
@@ -50,6 +50,7 @@ static void example_wifi_init(void)
     ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
     ESP_ERROR_CHECK( esp_wifi_set_mode(ESPNOW_WIFI_MODE) );
     ESP_ERROR_CHECK( esp_wifi_start());
+    ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
 
 #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
     ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
