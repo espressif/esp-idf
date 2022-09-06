d5349fcc53c7450678d740a7d5fc07f0c94cd621
cjin <jinchen@espressif.com>
2022-09-06 21:35:51 +0800

    update ble controller lib for bugfix on esp32c2

--- components/bt/include/esp32c2/include/esp_bt.h
+++ components/bt/include/esp32c2/include/esp_bt.h
@@ -145,7 +145,7 @@ esp_err_t esp_ble_tx_power_set_enhanced(esp_ble_enhanced_power_type_t power_type
  */
 esp_power_level_t esp_ble_tx_power_get_enhanced(esp_ble_enhanced_power_type_t power_type, uint16_t handle);
 
-#define CONFIG_VERSION  0x20220729
+#define CONFIG_VERSION  0x20220824
 #define CONFIG_MAGIC    0x5A5AA5A5
 
 /**
