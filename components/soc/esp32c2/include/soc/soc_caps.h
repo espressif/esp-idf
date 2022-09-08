ccfbba59940a93a4b419b32fc56be0de1fa44d0e
Armando <douyiwen@espressif.com>
2022-09-08 19:10:46 +0800

    esp_adc: support adc calibration on esp32c2

--- components/soc/esp32c2/include/soc/soc_caps.h
+++ components/soc/esp32c2/include/soc/soc_caps.h
@@ -73,6 +73,9 @@
 #define SOC_ADC_RTC_MAX_BITWIDTH                (12)
 #define SOC_RTC_SLOW_CLOCK_SUPPORT_8MD256       (1)
 
+/*!< Calibration */
+#define SOC_ADC_CALIBRATION_V1_SUPPORTED        (1) /*!< support HW offset calibration version 1*/
+
 /*-------------------------- BROWNOUT CAPS -----------------------------------*/
 #define SOC_BROWNOUT_RESET_SUPPORTED 1
 
