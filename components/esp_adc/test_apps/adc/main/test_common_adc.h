f905f6ab7cd69f0f56baa2d78747a86b17700eae
Armando <douyiwen@espressif.com>
2022-09-08 19:21:24 +0800

    esp_adc: add esp32c2 adc high/low test threshold and expected val

--- components/esp_adc/test_apps/adc/main/test_common_adc.h
+++ components/esp_adc/test_apps/adc/main/test_common_adc.h
@@ -65,11 +65,11 @@ extern "C" {
 #define ADC_TEST_HIGH_THRESH     0
 
 #elif CONFIG_IDF_TARGET_ESP32C2
-#define ADC_TEST_LOW_VAL         2147
-#define ADC_TEST_LOW_THRESH      100
+#define ADC_TEST_LOW_VAL         0
+#define ADC_TEST_LOW_THRESH      15
 
-#define ADC_TEST_HIGH_VAL        4095
-#define ADC_TEST_HIGH_THRESH     0
+#define ADC_TEST_HIGH_VAL        3400
+#define ADC_TEST_HIGH_THRESH     200
 #endif
 
 
