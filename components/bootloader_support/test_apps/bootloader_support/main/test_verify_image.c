7cef82e0df2e823c53a61e27983af3e4921e0e03
Marius Vikhammer <marius.vikhammer@espressif.com>
2022-09-14 16:18:15 +0800

    bootloader support: move unit tests to pytest

--- components/bootloader_support/test_apps/bootloader_support/main/test_verify_image.c
+++ components/bootloader_support/test_apps/bootloader_support/main/test_verify_image.c
@@ -37,8 +37,6 @@ TEST_CASE("Verify bootloader image in flash", "[bootloader_support]")
     TEST_ASSERT_EQUAL(data.image_len, bootloader_length);
 }
 
-#if !TEMPORARY_DISABLED_FOR_TARGETS(ESP32C2)
-//IDF-5145
 TEST_CASE("Verify unit test app image", "[bootloader_support]")
 {
     esp_image_metadata_t data = { 0 };
@@ -53,7 +51,6 @@ TEST_CASE("Verify unit test app image", "[bootloader_support]")
     TEST_ASSERT_NOT_EQUAL(0, data.image_len);
     TEST_ASSERT_TRUE(data.image_len <= running->size);
 }
-#endif //!TEMPORARY_DISABLED_FOR_TARGETS(ESP32C2)
 
 void check_label_search (int num_test, const char *list, const char *t_label, bool result)
 {
