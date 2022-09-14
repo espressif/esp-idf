27470afb7ce44f0c1a7e95d50064aa21be52dd34
wanlei <wanlei@espressif.com>
2022-09-14 11:20:39 +0800

    spi_master:fix error when use `spi_bus_add_device` more than 3 device
    update gpio_sig at `spics_out` array in each spi_periph.c of chips later than s2
    then `spi_bus_add_device` can correctly distribute gpio_signals for cs_signal
    
    Closes https://github.com/espressif/esp-idf/issues/8876

--- components/soc/esp32s3/include/soc/soc_caps.h
+++ components/soc/esp32s3/include/soc/soc_caps.h
@@ -208,6 +208,7 @@
 #define SOC_SPI_PERIPH_NUM                  3
 #define SOC_SPI_DMA_CHAN_NUM                3
 #define SOC_SPI_PERIPH_CS_NUM(i)            3
+#define SOC_SPI_MAX_CS_NUM                  6
 #define SOC_SPI_MAXIMUM_BUFFER_SIZE         64
 #define SOC_SPI_SUPPORT_DDRCLK              1
 #define SOC_SPI_SLAVE_SUPPORT_SEG_TRANS     1
