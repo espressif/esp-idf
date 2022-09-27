f3cadb4fabd5539be44a4340cd9f51599e187bc6
zhangwenxu <zhangwenxu@espressif.com>
2022-09-27 17:22:34 +0800

    ieee802154: add 154 lib CI initialization.

---
+++ components/ieee802154/test_apps/test_ieee802154/main/include/ieee802154_cmd.h
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#pragma once
+
+void register_ieee802154_cmd(void);
