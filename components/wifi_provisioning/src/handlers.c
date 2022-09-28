9067214f33df8f97a78e7550b5ab096563b39039
sanket.wadekar <sanket.wadekar@espressif.com>
2022-09-28 14:21:07 +0530

    Changed wifi provisioning scan method
    Signed-off-by: sanket.wadekar <sanket.wadekar@espressif.com>

--- components/wifi_provisioning/src/handlers.c
+++ components/wifi_provisioning/src/handlers.c
@@ -1,16 +1,8 @@
-// Copyright 2019 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2019-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include <stdio.h>
 #include <string.h>
@@ -118,6 +110,13 @@ static esp_err_t set_config_handler(const wifi_prov_config_set_data_t *req_data,
     /* Using strlcpy allows both max passphrase length (63 bytes) and ensures null termination
      * because size of wifi_cfg->sta.password is 64 bytes (1 extra byte for null character) */
     strlcpy((char *) wifi_cfg->sta.password, req_data->password, sizeof(wifi_cfg->sta.password));
+
+#ifdef CONFIG_WIFI_PROV_STA_ALL_CHANNEL_SCAN
+    wifi_cfg->sta.scan_method = WIFI_ALL_CHANNEL_SCAN;
+#else /* CONFIG_WIFI_PROV_STA_FAST_SCAN */
+    wifi_cfg->sta.scan_method = WIFI_FAST_SCAN;
+#endif
+
     return ESP_OK;
 }
 
