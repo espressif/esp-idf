93153bdfe110294d5fb8ad7a89d46ca816d270bb
Sudeep Mohanty <sudeep.mohanty@espressif.com>
2022-10-03 16:11:15 +0200

    ulp: add all 'ignore' tests to pytest framework
    This commit adds a way to run all 'ignore' tests for ulp using the
    pytest framework.

--- components/ulp/test_apps/ulp_riscv/pytest_ulp_riscv.py
+++ components/ulp/test_apps/ulp_riscv/pytest_ulp_riscv.py
@@ -10,5 +10,16 @@ from pytest_embedded import Dut
 @pytest.mark.generic
 def test_ulp_riscv(dut: Dut) -> None:
     dut.expect('Press ENTER to see the list of tests')
-    dut.write('![ignore]')
+    dut.write('![ulp_deep_sleep_wakeup]')
     dut.expect_unity_test_output()
+
+
+# Run all deepsleep wakeup tests one after the other instead of running them all with the `ulp_deep_sleep_wakeup` tag.
+# This makes sure that all tests are run even after one test causes a system reset.
+@pytest.mark.esp32s2
+@pytest.mark.esp32s3
+@pytest.mark.generic
+def test_ulp_deep_sleep_wakeup(dut: Dut) -> None:
+    dut.expect('Press ENTER to see the list of tests')
+    dut.write('"ULP-RISC-V is able to wakeup main CPU from deep sleep"')
+    dut.expect('rst:0x5')
