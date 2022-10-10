6bad6d31c0779f19bc7cddb3f5a3f270aa8b84b0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 11:26:27 +0000

    Adding tarfile member sanitization to extractall()

--- tools/ci/python_packages/gitlab_api.py
+++ tools/ci/python_packages/gitlab_api.py
@@ -223,7 +223,26 @@ class Gitlab(object):
     def decompress_archive(path: str, destination: str) -> str:
         with tarfile.open(path, 'r') as archive_file:
             root_name = archive_file.getnames()[0]
-            archive_file.extractall(destination)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive_file, destination)
 
         return os.path.join(os.path.realpath(destination), root_name)
 
