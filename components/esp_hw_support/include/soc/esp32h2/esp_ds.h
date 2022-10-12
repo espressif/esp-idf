f9565fd31d3a28b2cf36348d4c2846cad349ae2f
Aditya Patwardhan <aditya.patwardhan@espressif.com>
2022-10-12 18:25:39 +0530

    soc/esp_ds.h: Unify esp_ds error codes for all targets

--- components/esp_hw_support/include/soc/esp32h2/esp_ds.h
+++ components/esp_hw_support/include/soc/esp32h2/esp_ds.h
@@ -8,18 +8,12 @@
 
 #include "esp_hmac.h"
 #include "esp_err.h"
+#include "esp_ds_err.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define ESP32H2_ERR_HW_CRYPTO_DS_HMAC_FAIL           ESP_ERR_HW_CRYPTO_BASE + 0x1 /*!< HMAC peripheral problem */
-#define ESP32H2_ERR_HW_CRYPTO_DS_INVALID_KEY         ESP_ERR_HW_CRYPTO_BASE + 0x2 /*!< given HMAC key isn't correct,
-                                                                                HMAC peripheral problem */
-#define ESP32H2_ERR_HW_CRYPTO_DS_INVALID_DIGEST      ESP_ERR_HW_CRYPTO_BASE + 0x4 /*!< message digest check failed,
-                                                                                result is invalid */
-#define ESP32H2_ERR_HW_CRYPTO_DS_INVALID_PADDING     ESP_ERR_HW_CRYPTO_BASE + 0x5 /*!< padding check failed, but result
-                                                                                   is produced anyway and can be read*/
 
 #define ESP_DS_IV_BIT_LEN 128
 #define ESP_DS_IV_LEN (ESP_DS_IV_BIT_LEN / 8)
