c36e06c45c577384736ca5fc38c4c632f9cc9b9b
Darian Leung <darian@espressif.com>
2022-10-15 02:29:51 +0800

    freertos: Add test app sdkconfig
    This commit adds the sdkconfig files for the FreeRTOS test app. These
    configurations were dervied from the various legacy unit test app's
    config files that included the FreeRTOS component.
    
    This commit tries to keep a 1 to 1 config parity with the legacy test app.
    Meaning, if FreeRTOS test were run on a particular target with a particular
    config, that config will be represented in one of the test app's
    sdkconfig.ci.XXX files.
    
    However, the following configurations were removed for FreeRTOS tests:
    
    - The "freertos_flash" option was removed due to redudancy (already tested in
      freertos_options)

--- components/freertos/test_apps/freertos/pytest_freertos.py
+++ components/freertos/test_apps/freertos/pytest_freertos.py
@@ -4,9 +4,18 @@
 import pytest
 from pytest_embedded import Dut
 
+CONFIGS = [
+    pytest.param('default', marks=[pytest.mark.supported_targets]),
+    pytest.param('freertos_options', marks=[pytest.mark.supported_targets]),
+    pytest.param('psram', marks=[pytest.mark.esp32]),
+    pytest.param('release', marks=[pytest.mark.supported_targets]),
+    pytest.param('single_core', marks=[pytest.mark.esp32]),
+]
+
 
 @pytest.mark.supported_targets
 @pytest.mark.generic
+@pytest.mark.parametrize('config', CONFIGS, indirect=True)
 def test_freertos(dut: Dut) -> None:
     dut.expect_exact('Press ENTER to see the list of tests')
     dut.write('*')
