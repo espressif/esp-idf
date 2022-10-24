4c7b2cb4e3e378906332179f3db0abf98165036d
Ivan Grokhotkov <ivan@espressif.com>
2022-10-24 10:44:24 +0200

    feat(tools): add espsecure.py to the Windows export scripts
    This is not needed on Linux/macOS where the path to esptool directory
    is added to PATH.

--- export.bat
+++ export.bat
@@ -49,6 +49,7 @@ if not "%PATH_ADDITIONS%"=="" echo     %PATH_ADDITIONS:;=&echo.    %
 DOSKEY idf.py=python.exe "%IDF_PATH%\tools\idf.py" $*
 DOSKEY esptool.py=python.exe "%IDF_PATH%\components\esptool_py\esptool\esptool.py" $*
 DOSKEY espefuse.py=python.exe "%IDF_PATH%\components\esptool_py\esptool\espefuse.py" $*
+DOSKEY espsecure.py=python.exe "%IDF_PATH%\components\esptool_py\esptool\espsecure.py" $*
 DOSKEY otatool.py=python.exe "%IDF_PATH%\components\app_update\otatool.py" $*
 DOSKEY parttool.py=python.exe "%IDF_PATH%\components\partition_table\parttool.py" $*
 
