8b0d0cbf5df89d4a441ff1f8b373d195b9f68712
jiangguangming <jiangguangming@espressif.com>
2022-10-26 14:57:47 +0800

    esp_rom: remove ESP_ROM_SUPPORT_MULTIPLE_UART

--- components/esp_rom/esp32/esp_rom_caps.h
+++ components/esp_rom/esp32/esp_rom_caps.h
@@ -9,5 +9,4 @@
 #define ESP_ROM_HAS_CRC_LE              (1) // ROM CRC library supports Little Endian
 #define ESP_ROM_HAS_CRC_BE              (1) // ROM CRC library supports Big Endian
 #define ESP_ROM_HAS_JPEG_DECODE         (1) // ROM has JPEG decode library
-#define ESP_ROM_SUPPORT_MULTIPLE_UART   (1) // ROM has multiple UARTs available for logging
 #define ESP_ROM_NEEDS_SWSETUP_WORKAROUND    (1) // ROM uses 32-bit time_t. A workaround is required to prevent printf functions from crashing
--- components/esp_rom/esp32s3/esp_rom_caps.h
+++ components/esp_rom/esp32s3/esp_rom_caps.h
@@ -9,7 +9,6 @@
 #define ESP_ROM_HAS_CRC_LE                  (1) // ROM CRC library supports Little Endian
 #define ESP_ROM_HAS_CRC_BE                  (1) // ROM CRC library supports Big Endian
 #define ESP_ROM_HAS_JPEG_DECODE             (1) // ROM has JPEG decode library
-#define ESP_ROM_SUPPORT_MULTIPLE_UART       (1) // ROM has multiple UARTs available for logging
 #define ESP_ROM_UART_CLK_IS_XTAL            (1) // UART clock source is selected to XTAL in ROM
 #define ESP_ROM_HAS_RETARGETABLE_LOCKING    (1) // ROM was built with retargetable locking
 #define ESP_ROM_USB_SERIAL_DEVICE_NUM       (4) // The serial port ID (UART, USB, ...) of USB_SERIAL_JTAG in the ROM.
