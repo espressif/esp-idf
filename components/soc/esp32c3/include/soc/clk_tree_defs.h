6828c011d9fe49c652665975496ae29acf59a71c
morris <maoshengrong@espressif.com>
2022-10-27 18:26:02 +0800

    twai: define clock source name
    twai clock source is target-specific,
    this commit is to define them in the soc layer

--- components/soc/esp32c3/include/soc/clk_tree_defs.h
+++ components/soc/esp32c3/include/soc/clk_tree_defs.h
@@ -248,6 +248,21 @@ typedef enum {
     SDM_CLK_SRC_DEFAULT = SOC_MOD_CLK_APB, /*!< Select APB as the default clock choice */
 } soc_periph_sdm_clk_src_t;
 
+//////////////////////////////////////////////////TWAI/////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Array initializer for all supported clock sources of TWAI
+ */
+#define SOC_TWAI_CLKS {SOC_MOD_CLK_APB}
+
+/**
+ * @brief TWAI clock source
+ */
+typedef enum {
+    TWAI_CLK_SRC_APB = SOC_MOD_CLK_APB,     /*!< Select APB as the source clock */
+    TWAI_CLK_SRC_DEFAULT = SOC_MOD_CLK_APB, /*!< Select APB as the default clock choice */
+} soc_periph_twai_clk_src_t;
+
 #ifdef __cplusplus
 }
 #endif
