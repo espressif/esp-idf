dbe59eff86c49cdc7ef7bfb64e61f60332bef2dc
liuning <liuning@espressif.com>
2022-10-27 17:30:45 +0800

    esp_wifi: add protection for mac reset (backport 4.3)

--- components/hal/esp32s3/include/hal/clk_gate_ll.h
+++ components/hal/esp32s3/include/hal/clk_gate_ll.h
@@ -116,6 +116,8 @@ static inline uint32_t periph_ll_get_rst_en_mask(periph_module_t periph, bool en
         return SYSTEM_RMT_RST;
     case PERIPH_LEDC_MODULE:
         return SYSTEM_LEDC_RST;
+    case PERIPH_WIFI_MODULE:
+        return SYSTEM_WIFIMAC_RST;
     case PERIPH_UART0_MODULE:
         return SYSTEM_UART_RST;
     case PERIPH_UART1_MODULE:
