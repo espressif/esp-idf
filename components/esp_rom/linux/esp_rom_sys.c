44aa3c3a7fe9096eb0773aa11c37670cb60e6950
Jakob Hasse <jakob.hasse@espressif.com>
2022-11-03 10:12:14 +0100

    bugfix(esp_rom): Fixed writable string issue on Linux implementation

--- components/esp_rom/linux/esp_rom_sys.c
+++ components/esp_rom/linux/esp_rom_sys.c
@@ -1,16 +1,8 @@
-// Copyright 2021 Espressif Systems (Shanghai) CO LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,7 +22,7 @@ static void call_linux_putc(char c) {
 
 #define is_digit(c) ((c >= '0') && (c <= '9'))
 
-static int _cvt(unsigned long long val, char *buf, long radix, char *digits)
+static int _cvt(unsigned long long val, char *buf, long radix, const char *digits)
 {
 #ifdef SUPPORT_LITTLE_RADIX
     char temp[64];
@@ -65,7 +57,8 @@ static int esp_rom_vprintf(void (*putc)(char c), const char *fmt, va_list ap)
 #else
     char buf[32];
 #endif
-    char c, sign, *cp=buf;
+    char c, sign;
+    const char *cp = buf;
     int left_prec, right_prec, zero_fill, pad, pad_on_right,
         islong, islonglong;
     long long val = 0;
