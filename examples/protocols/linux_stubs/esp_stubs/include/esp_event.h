5c728e94a7bb4307fac2e60310e816d5e296ce84
Harshit Malpani <harshit.malpani@espressif.com>
2022-11-08 16:31:52 +0530

    Update esp_http_client example to build for linux target

---
+++ examples/protocols/linux_stubs/esp_stubs/include/esp_event.h
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+#include "esp_err.h"
+
+esp_err_t esp_event_loop_create_default(void);
