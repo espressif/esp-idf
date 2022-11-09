8d24aa1760966966fbd07f326f21d6289513d475
Marius Vikhammer <marius.vikhammer@espressif.com>
2022-11-09 10:48:04 +0800

    ci: fix esp_timer 26 MHz not being assigned to correct runner

--- components/esp_timer/test_apps/pytest_esp_timer_ut.py
+++ components/esp_timer/test_apps/pytest_esp_timer_ut.py
@@ -10,7 +10,6 @@ CONFIGS = [
     pytest.param('single_core', marks=[pytest.mark.esp32]),
     pytest.param('freertos_compliance', marks=[pytest.mark.esp32]),
     pytest.param('isr_dispatch_esp32', marks=[pytest.mark.esp32]),
-    pytest.param('26mhz_esp32c2', marks=[pytest.mark.esp32c2]),
 ]
 
 
@@ -28,6 +27,21 @@ def test_esp_timer(dut: Dut) -> None:
     'psram',
 ], indirect=True)
 def test_esp_timer_psram(dut: Dut) -> None:
+    dut.expect_exact('Press ENTER to see the list of tests')
+    dut.write('*')
+    dut.expect_unity_test_output(timeout=240)
+
+
+@pytest.mark.esp32c2
+@pytest.mark.xtal_26mhz
+@pytest.mark.parametrize(
+    'config, baud',
+    [
+        ('26mhz_esp32c2', '74880'),
+    ],
+    indirect=True,
+)
+def test_esp_timer_esp32c2_xtal_26mhz(dut: Dut) -> None:
     dut.expect_exact('Press ENTER to see the list of tests')
     dut.write('*')
     dut.expect_unity_test_output(timeout=240)
