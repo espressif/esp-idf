f8d4bc8911f5292256620a5100d85a657e7f6be0
Michael (XIAO Xufeng) <xiaoxufeng@espressif.com>
2022-11-09 17:19:35 +0800

    soc: upgrade version printing to vX.Y

--- examples/security/flash_encryption/main/flash_encrypt_main.c
+++ examples/security/flash_encryption/main/flash_encrypt_main.c
@@ -99,7 +99,9 @@ static void example_print_chip_info(void)
             (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
             (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
 
-    printf("silicon revision %d, ", chip_info.revision);
+    unsigned major_rev = chip_info.revision / 100;
+    unsigned minor_rev = chip_info.revision % 100;
+    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
     if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
         printf("Get flash size failed");
         return;
