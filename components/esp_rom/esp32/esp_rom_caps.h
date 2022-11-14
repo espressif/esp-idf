af425e5a8e422e9229182722c8832951a78be397
jiangguangming <jiangguangming@espressif.com>
2022-11-14 17:54:53 +0800

    move {target}/rom/miniz.h to a common miniz.h
    - Move {target}/rom/miniz.h to common miniz.h
    - Add ESP_ROM_HAS_MZ_CRC32 for ESP32/S2/S3/C3/H2
    - Alias mz_crc32 to crc32_le if chips not support ESP_ROM_HAS_MZ_CRC32

--- components/esp_rom/esp32/esp_rom_caps.h
+++ components/esp_rom/esp32/esp_rom_caps.h
@@ -8,5 +8,6 @@
 
 #define ESP_ROM_HAS_CRC_LE              (1) // ROM CRC library supports Little Endian
 #define ESP_ROM_HAS_CRC_BE              (1) // ROM CRC library supports Big Endian
+#define ESP_ROM_HAS_MZ_CRC32            (1) // ROM has mz_crc32 function
 #define ESP_ROM_HAS_JPEG_DECODE         (1) // ROM has JPEG decode library
 #define ESP_ROM_NEEDS_SWSETUP_WORKAROUND    (1) // ROM uses 32-bit time_t. A workaround is required to prevent printf functions from crashing
