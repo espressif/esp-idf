674fd8feb88297d4e51ce178d99f17bca75f11bc
Harshit Malpani <harshit.malpani@espressif.com>
2022-11-16 17:31:07 +0530

    esp_http_client: Add support for esp_events

--- examples/protocols/linux_stubs/esp_stubs/esp_stubs.c
+++ examples/protocols/linux_stubs/esp_stubs/esp_stubs.c
@@ -6,10 +6,17 @@
 #include <stdlib.h>
 #include "esp_err.h"
 #include "esp_log.h"
+#include "esp_event.h"
 
 extern void app_main(void);
 
 esp_err_t esp_event_loop_create_default(void)
+{
+    return ESP_OK;
+}
+
+esp_err_t esp_event_post(esp_event_base_t event_base, int32_t event_id,
+        const void* event_data, size_t event_data_size, TickType_t ticks_to_wait)
 {
     return ESP_OK;
 }
