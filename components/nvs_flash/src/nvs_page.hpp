28c325b2916ab25e8a4640648812c142d7517de5
Jakob Hasse <jakob.hasse@espressif.com>
2022-11-23 14:40:38 +0100

    refactor(nvs): custom allocator for all objects allocated in NVS

--- components/nvs_flash/src/nvs_page.hpp
+++ components/nvs_flash/src/nvs_page.hpp
@@ -16,13 +16,14 @@
 #include "compressed_enum_table.hpp"
 #include "intrusive_list.h"
 #include "nvs_item_hash_list.hpp"
+#include "nvs_memory_management.hpp"
 #include "partition.hpp"
 
 namespace nvs
 {
 
 
-class Page : public intrusive_list_node<Page>
+class Page : public intrusive_list_node<Page>, public ExceptionlessAllocatable
 {
 public:
     static const uint32_t PSB_INIT = 0x1;
