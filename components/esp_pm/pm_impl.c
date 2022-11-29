2f504695e4cdeab5e2a09d01d7251bfd888c55d8
Darian Leung <darian@espressif.com>
2022-11-29 18:58:54 +0800

    esp_pm: Fix string formatting type errors
    This commit fixes a string formatting error in esp_pm with CONFIG_PM_PROFILING
    is enabled.
    
    Closes https://github.com/espressif/esp-idf/issues/10347

--- components/esp_pm/pm_impl.c
+++ components/esp_pm/pm_impl.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
 #include <sys/param.h>
 
 #include "esp_attr.h"
@@ -704,7 +705,7 @@ void esp_pm_impl_dump_stats(FILE* out)
             /* don't display light sleep mode if it's not enabled */
             continue;
         }
-        fprintf(out, "%-8s  %-3dM%-7s %-10lld  %-2d%%\n",
+        fprintf(out, "%-8s  %-3"PRIu32"M%-7s %-10lld  %-2d%%\n",
                 s_mode_names[i],
                 s_cpu_freq_by_mode[i].freq_mhz,
                 "",                                     //Empty space to align columns
