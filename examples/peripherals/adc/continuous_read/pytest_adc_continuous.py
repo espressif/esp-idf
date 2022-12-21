9282446d9b58441098e3f2df6a325a583b0132d1
Chen Yudong <chenyudong@espressif.com>
2022-12-21 12:07:35 +0800

    CI: add example adc tests

--- examples/peripherals/adc/continuous_read/pytest_adc_continuous.py
+++ examples/peripherals/adc/continuous_read/pytest_adc_continuous.py
@@ -9,6 +9,7 @@ from pytest_embedded.dut import Dut
 @pytest.mark.esp32s2
 @pytest.mark.esp32s3
 @pytest.mark.esp32c3
+@pytest.mark.adc
 def test_adc_continuous(dut: Dut) -> None:
     res = dut.expect(r'TASK: ret is 0, ret_num is (\d+)')
     num = res.group(1).decode('utf8')
