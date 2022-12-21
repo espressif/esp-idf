d5b4108085109056deab34af8ab5c2f6ec7bc961
morris <maoshengrong@espressif.com>
2022-12-21 21:31:03 +0800

    gpio: combine sdm, glitch filter, fast gpio tests into one app

--- components/driver/test_apps/gpio/pytest_gpio.py
+++ components/driver/test_apps/gpio/pytest_gpio.py
@@ -17,28 +17,6 @@ def test_gpio(dut: IdfDut) -> None:
     dut.run_all_single_board_cases(group='gpio')
 
 
-@pytest.mark.esp32c2
-@pytest.mark.esp32c3
-@pytest.mark.esp32c6
-@pytest.mark.esp32s2
-@pytest.mark.esp32s3
-@pytest.mark.generic
-@pytest.mark.parametrize('config', CONFIGS, indirect=True)
-def test_gpio_filter(dut: IdfDut) -> None:
-    dut.run_all_single_board_cases(group='gpio_filter')
-
-
-@pytest.mark.esp32c2
-@pytest.mark.esp32c3
-@pytest.mark.esp32c6
-@pytest.mark.esp32s2
-@pytest.mark.esp32s3
-@pytest.mark.generic
-@pytest.mark.parametrize('config', CONFIGS, indirect=True)
-def test_dedic_gpio(dut: IdfDut) -> None:
-    dut.run_all_single_board_cases(group='dedic_gpio')
-
-
 @pytest.mark.esp32
 @pytest.mark.esp32c3
 @pytest.mark.esp32c6
@@ -46,7 +24,7 @@ def test_dedic_gpio(dut: IdfDut) -> None:
 @pytest.mark.esp32s3
 @pytest.mark.generic
 @pytest.mark.parametrize('config', CONFIGS, indirect=True)
-def test_sigma_delta(dut: IdfDut) -> None:
+def test_legacy_sigma_delta(dut: IdfDut) -> None:
     dut.run_all_single_board_cases(group='sigma_delta')
 
 
