d173dec1b7317b43f5e4da7738a2b5ecb2ab3c7c
wangjialiang <wangjialiang@espressif.com>
2023-01-05 12:11:34 +0800

    ble_mesh: example: update ble mesh examples for esp32c6

--- examples/bluetooth/esp_ble_mesh/ble_mesh_node/onoff_client/main/board.h
+++ examples/bluetooth/esp_ble_mesh/ble_mesh_node/onoff_client/main/board.h
@@ -29,6 +29,10 @@
 #define LED_R GPIO_NUM_47
 #define LED_G GPIO_NUM_47
 #define LED_B GPIO_NUM_47
+#elif defined(CONFIG_BLE_MESH_ESP32C6_DEV)
+#define LED_R GPIO_NUM_8
+#define LED_G GPIO_NUM_8
+#define LED_B GPIO_NUM_8
 #endif
 
 #define LED_ON  1
