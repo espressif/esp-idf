c07d237ffe5e5f44ba114ba0a8150c748ca46202
Chen Yudong <chenyudong@espressif.com>
2023-01-09 20:58:27 +0800

    examples: enable build esp32c6 for wifi and network examples

--- examples/protocols/https_mbedtls/pytest_https_mbedtls.py
+++ examples/protocols/https_mbedtls/pytest_https_mbedtls.py
@@ -11,9 +11,6 @@ from pytest_embedded import Dut
 
 
 @pytest.mark.esp32
-@pytest.mark.esp32c3
-@pytest.mark.esp32s2
-@pytest.mark.esp32s3
 @pytest.mark.ethernet
 def test_examples_protocol_https_mbedtls(dut: Dut) -> None:
     """
