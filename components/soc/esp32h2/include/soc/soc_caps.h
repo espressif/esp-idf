6c1d98d55631d5368b2af243c0ec1750502b29da
morris <maoshengrong@espressif.com>
2023-01-10 15:13:19 +0800

    systimer: assign counter and alarm in esp_hw_support

--- components/soc/esp32h2/include/soc/soc_caps.h
+++ components/soc/esp32h2/include/soc/soc_caps.h
@@ -329,15 +329,16 @@
 
 #define SOC_MEMSPI_SRC_FREQ_48M_SUPPORTED         1
 
-// TODO: IDF-6230 (Copy from esp32c6, need check)
 /*-------------------------- SYSTIMER CAPS ----------------------------------*/
 #define SOC_SYSTIMER_COUNTER_NUM            2  // Number of counter units
 #define SOC_SYSTIMER_ALARM_NUM              3  // Number of alarm units
 #define SOC_SYSTIMER_BIT_WIDTH_LO           32 // Bit width of systimer low part
 #define SOC_SYSTIMER_BIT_WIDTH_HI           20 // Bit width of systimer high part
-#define SOC_SYSTIMER_FIXED_DIVIDER          1  // Clock source divider is fixed: 2.5
+#define SOC_SYSTIMER_FIXED_DIVIDER          1  // Clock source divider is fixed to 2 when clock source is XTAL
+#define SOC_SYSTIMER_SUPPORT_RC_FAST        1  // Systimer can use RC_FAST clock source
 #define SOC_SYSTIMER_INT_LEVEL              1  // Systimer peripheral uses level interrupt
 #define SOC_SYSTIMER_ALARM_MISS_COMPENSATE  1  // Systimer peripheral can generate interrupt immediately if t(target) > t(current)
+#define SOC_SYSTIMER_SUPPORT_ETM            1  // Systimer comparator can generate ETM event
 
 // TODO: IDF-6242 (Copy from esp32c6, need check)
 /*--------------------------- TIMER GROUP CAPS ---------------------------------------*/
