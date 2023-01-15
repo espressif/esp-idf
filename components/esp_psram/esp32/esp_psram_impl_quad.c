fe068f9e001811077729715a2ba96632d4272bb0
yn386 <wf.yn386@gmail.com>
2023-01-15 11:58:35 +0900

    esp_psram: fix compile error for SPI PSRAM 2T mode

--- components/esp_psram/esp32/esp_psram_impl_quad.c
+++ components/esp_psram/esp32/esp_psram_impl_quad.c
@@ -19,6 +19,7 @@
 #include "esp32/rom/spi_flash.h"
 #include "esp32/rom/cache.h"
 #include "esp32/rom/efuse.h"
+#include "esp32/rom/gpio.h"
 #include "esp_rom_efuse.h"
 #include "soc/dport_reg.h"
 #include "soc/efuse_periph.h"
