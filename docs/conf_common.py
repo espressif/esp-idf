67f78a03ae9a572ade98bf65ca14a4f52c21b45b
Marius Vikhammer <marius.vikhammer@espressif.com>
2023-01-17 12:51:21 +0800

    docs: ignore xtensa isa-summary during linkcheck

--- docs/conf_common.py
+++ docs/conf_common.py
@@ -228,7 +228,7 @@ linkcheck_exclude_documents = ['index',  # several false positives due to the wa
 
 linkcheck_ignore = ['https://webhome.phy.duke.edu/~rgb/General/dieharder.php',  # Certificate error
                     'https://docs.espressif.com/projects/esptool/en/latest/esp32c2/espefuse/index.html',  # Not published
-                    'https://www.espressif.com/sites/default/files/documentation/esp32-c2_datasheet_en.pdf',  # Not published
+                    'https://www.cadence.com/content/dam/cadence-www/global/en_US/documents/tools/ip/tensilica-ip/isa-summary.pdf',  # Rejects user-agent
                     ]
 
 
