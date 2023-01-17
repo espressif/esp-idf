espressif/esp-idf

commit c6e3eb0922736b4d65e93ec30196625f4d81140c

idf.py.exe changes to handle Ctrl+C in correct way. Handling Ctrl+C in correct way for gdbstubs.

tools/idf.py
@@ -39,6 +39,9 @@ from collections import Counter, OrderedDict
 from importlib import import_module
 from pkgutil import iter_modules
 
+import signal
+
+
 # pyc files remain in the filesystem when switching between branches which might raise errors for incompatible
 # idf.py extensions. Therefore, pyc file generation is turned off:
 sys.dont_write_bytecode = True
@@ -723,7 +726,16 @@ def init_cli(verbose_output=None):
     return CLI(help=cli_help, verbose_output=verbose_output, all_actions=all_actions)
 
 
+def signal_handler(signal, frame):
+    # The Ctrl+C processed by other threads inside
+    pass
+
+
 def main():
+
+    # Processing of Ctrl+C event for all threads made by main()
+    signal.signal(signal.SIGINT, signal_handler)
+
     checks_output = check_environment()
     cli = init_cli(verbose_output=checks_output)
     # the argument `prog_name` must contain name of the file - not the absolute path to it!

tools/windows/idf_exe/idf_main.c
@@ -16,6 +16,9 @@
 #include <shlwapi.h>
 #include <strsafe.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #define LINESIZE 1024
 
@@ -30,6 +33,17 @@ static void fail(LPCSTR message, ...)
     StringCchVPrintfA(msg, sizeof(msg), message, args);
     WriteFile(GetStdHandle(STD_ERROR_HANDLE), message, lstrlen(msg), &written, NULL);
     ExitProcess(1);
+}
+
+BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
+{
+    switch (fdwCtrlType) {
+    // Handle the CTRL-C signal.
+    case CTRL_C_EVENT:
+        return TRUE;
+    default:
+        return FALSE;
+    }
 }
 
 BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)

commit 92565a259fb396e1af223d558711a6cec4e61baa

tools: allow to interrupt port detection
The main idf.py process has a handler for SIGINT, which actually just
ignores it. The get_default_serial_port() function is called within the
idf.py context to detect port for several tools(monitor,flash) and it's
not possible to terminate it because of this. Let's ignore SIGINT only
while running idf_monitor, which uses it to spawn gdb.

Fixes: c6e3eb092273 ("idf.py.exe changes to handle Ctrl+C in correct way. H..")

Signed-off-by: Frantisek Hrbata <frantisek.hrbata@espressif.com>

tools/idf.py
@@ -20,13 +20,11 @@ import json
 import locale
 import os
 import os.path
-import signal
 import subprocess
 import sys
 from collections import Counter, OrderedDict, _OrderedDictKeysView
 from importlib import import_module
 from pkgutil import iter_modules
-from types import FrameType
 from typing import Any, Callable, Dict, List, Optional, Union
 
 # pyc files remain in the filesystem when switching between branches which might raise errors for incompatible
@@ -689,15 +687,7 @@ def init_cli(verbose_output: List=None) -> Any:
     return CLI(help=cli_help, verbose_output=verbose_output, all_actions=all_actions)
 
 
-def signal_handler(_signal: int, _frame: Optional[FrameType]) -> None:
-    # The Ctrl+C processed by other threads inside
-    pass
-
-
 def main() -> None:
-    # Processing of Ctrl+C event for all threads made by main()
-    signal.signal(signal.SIGINT, signal_handler)
-
     # Check the environment only when idf.py is invoked regularly from command line.
     checks_output = None if SHELL_COMPLETE_RUN else check_environment()
 

tools/idf_py_actions/serial_ext.py
@@ -3,6 +3,7 @@
 
 import json
 import os
+import signal
 import sys
 from typing import Any, Dict, List
 
@@ -151,7 +152,14 @@ def action_extensions(base_actions: Dict, project_path: str) -> Dict:
         monitor_args += ['-m', ' '.join("'%s'" % a for a in idf_py)]
         hints = False  # Temporarily disabled because of https://github.com/espressif/esp-idf/issues/9610
 
-        RunTool('idf_monitor', monitor_args, args.project_dir, build_dir=args.build_dir, hints=hints, interactive=True)()
+        # Temporally ignore SIGINT, which is used in idf_monitor to spawn gdb.
+        old_handler = signal.getsignal(signal.SIGINT)
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+        try:
+            RunTool('idf_monitor', monitor_args, args.project_dir, build_dir=args.build_dir, hints=hints, interactive=True)()
+        finally:
+            signal.signal(signal.SIGINT, old_handler)
 
     def flash(action: str, ctx: click.core.Context, args: PropertyDict) -> None:
         """