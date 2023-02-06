146b9b047b9363b88e511d7e38be3fc6ea856668
wuzhenghui <wuzhenghui@espressif.com>
2023-02-06 21:43:05 +0800

    soc: add modem regs and force enable i2c_ana_mst clock

---
+++ components/soc/esp32h2/include/modem/reg_base.h
@@ -0,0 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+#define DR_REG_MODEM_LPCON_BASE   0x600AD000
