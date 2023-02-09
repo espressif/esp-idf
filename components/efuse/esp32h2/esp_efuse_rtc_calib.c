bbbe569e03df243d56d1bb0f71abb04a8bf8eefa
Cao Sen Miao <caosenmiao@espressif.com>
2023-02-09 14:44:44 +0800

    temperature_sensor: Add support for esp32h2

--- components/efuse/esp32h2/esp_efuse_rtc_calib.c
+++ components/efuse/esp32h2/esp_efuse_rtc_calib.c
@@ -1,5 +1,16 @@
 /*
- * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+#include <esp_bit_defs.h>
+#include "esp_efuse.h"
+#include "esp_efuse_table.h"
+
+esp_err_t esp_efuse_rtc_calib_get_tsens_val(float* tsens_cal)
+{
+    // Currently calibration is not supported on ESP32-H2, IDF-5236
+    *tsens_cal = 0;
+    return ESP_OK;
+}
