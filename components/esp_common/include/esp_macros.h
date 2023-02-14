8c18393d23891b51e75c6d9af8bd50a7e25a15b0
Li Shuai <lishuai@espressif.com>
2023-02-14 14:28:35 +0800

    regdma: add regdma link operation api support

--- components/esp_common/include/esp_macros.h
+++ components/esp_common/include/esp_macros.h
@@ -46,6 +46,35 @@ extern "C" {
 #define CHOOSE_MACRO_VA_ARG(MACRO_WITH_ARGS, MACRO_WITH_NO_ARGS, ...) CHOOSE_MACRO_VA_ARG_INN(0, ##__VA_ARGS__, MACRO_WITH_ARGS, MACRO_WITH_NO_ARGS, 0)
 #endif
 
+/* Count number of arguments of __VA_ARGS__
+ * - reference https://groups.google.com/forum/#!topic/comp.std.c/d-6Mj5Lko_s
+ * - __GET_NTH_ARG__() takes args >= N (64) but only expand to Nth one (64th)
+ * - __RSEQ_N__() is reverse sequential to N to add padding to have Nth
+ *   position is the same as the number of arguments
+ * - ##__VA_ARGS__ is used to deal with 0 paramerter (swallows comma)
+ */
+#ifndef __VA_NARG__
+# define __VA_NARG__(...)   __NARG__(_0, ##__VA_ARGS__, __RSEQ_N__())
+
+# define __NARG__(...)      __GET_NTH_ARG__(__VA_ARGS__)
+# define __GET_NTH_ARG__( \
+        _01,_02,_03,_04,_05,_06,_07,_08,_09,_10, \
+        _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
+        _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
+        _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, \
+        _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
+        _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
+        _61,_62,_63,N,...) N
+# define __RSEQ_N__() \
+        62,61,60,                      \
+        59,58,57,56,55,54,53,52,51,50, \
+        49,48,47,46,45,44,43,42,41,40, \
+        39,38,37,36,35,34,33,32,31,30, \
+        29,28,27,26,25,24,23,22,21,20, \
+        19,18,17,16,15,14,13,12,11,10, \
+        9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+#endif
+
 /* test macros */
 #define foo_args(...) 1
 #define foo_no_args() 2
