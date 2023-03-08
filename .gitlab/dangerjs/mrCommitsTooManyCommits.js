58d7db6e5d5344932857e008b3e9028b5aaac4e8
Tomas Sebestik <tomas.sebestik@espressif.com>
2023-03-08 11:00:04 +0100

    dangerjs: Improve check for git commit messages

--- .gitlab/dangerjs/mrCommitsTooManyCommits.js
+++ .gitlab/dangerjs/mrCommitsTooManyCommits.js
@@ -1,12 +1,10 @@
 /**
  * Check if MR has not an excessive numbers of commits (if squashed)
  *
- * #TODO: this simple logic will be improved in future MRs - Jira IDF-6856.
- *
  * @dangerjs INFO
  */
 module.exports = function () {
-    const tooManyCommitThreshold = 5; // above this number of commits, squash is recommended
+    const tooManyCommitThreshold = 2; // above this number of commits, squash commits is suggested
     const mrCommits = danger.gitlab.commits;
 
     if (mrCommits.length > tooManyCommitThreshold) {
