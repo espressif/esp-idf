924bac9956ad42e30de6506809c419bf4c1b752f
David Cermak <cermak@espressif.com>
2023-03-17 08:13:47 +0100

    lwip: Remove AFL based fuzzer tests
    Also removed compilation pre-checks and jobs from the CI, and test data

--- components/lwip/test_afl_host/esp_attr.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#define IRAM_ATTR
--- components/lwip/test_afl_host/no_warn_host.h
+++
@@ -1,6 +0,0 @@
-// Note: these undefs and defines are used to suppress warnings and errors when compiling esp32 idf on host gcc/clang
-#undef  __nonnull
-#define __warning__ deprecated
-#define IRAM_ATTR
-#define __ESP_ATTR_H__
-#include <features.h>
