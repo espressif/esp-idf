43c98143871565f7d5d30a08ab999ac2e2db4736
lly <lly@espressif.com>
2023-03-23 10:26:23 +0800

    ble_mesh: stack: Minor fix for provisioner provisioning timeout

--- components/bt/esp_ble_mesh/mesh_core/provisioner_prov.c
+++ components/bt/esp_ble_mesh/mesh_core/provisioner_prov.c
@@ -2725,12 +2725,8 @@ static void prov_retransmit(struct k_work *work)
 #endif
     if (k_uptime_get() - link[idx].tx.start > timeout) {
         BT_WARN("Provisioner timeout, giving up transaction");
-        /**
-         * For the case MESH/PVNR/PBADV/BV-01, provisoner should
-         * sends link close with reason.
-         */
-        close_link(idx,CLOSE_REASON_TIMEOUT);
-        reset_link(idx, CLOSE_REASON_TIMEOUT);
+        /* Provisioner should send Link Close here */
+        close_link(idx, CLOSE_REASON_TIMEOUT);
         return;
     }
 
