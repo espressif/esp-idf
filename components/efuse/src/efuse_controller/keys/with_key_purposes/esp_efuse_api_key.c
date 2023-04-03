94ae902d780ca2ace7f1c0c1e35cf7b5bf50cf4a
KonstantinKondrashov <konstantin@espressif.com>
2023-04-04 00:00:56 +0800

    efuse: Prevent burning XTS_AES and ECDSA keys into BLOCK9 (BLOCK_KEY5)
    eFuse module has a hardware bug.
    It is related to ESP32-C3, C6, S3, H2 chips:
        - BLOCK9 (BLOCK_KEY5) can not be used by XTS_AES keys.
    For H2 chips, the BLOCK9 (BLOCK_KEY5) can not be used by ECDSA keys.
    S2 does not have such a hardware bug.

--- components/efuse/src/efuse_controller/keys/with_key_purposes/esp_efuse_api_key.c
+++ components/efuse/src/efuse_controller/keys/with_key_purposes/esp_efuse_api_key.c
@@ -282,6 +282,22 @@ esp_err_t esp_efuse_write_key(esp_efuse_block_t block, esp_efuse_purpose_t purpo
         ESP_EFUSE_CHK(esp_efuse_write_field_blob(s_table[idx].key, key, key_size_bytes * 8));
         ESP_EFUSE_CHK(esp_efuse_set_key_dis_write(block));
 
+#if SOC_EFUSE_BLOCK9_KEY_PURPOSE_QUIRK
+        if (block == EFUSE_BLK9 && (
+#if SOC_FLASH_ENCRYPTION_XTS_AES_256
+            purpose == ESP_EFUSE_KEY_PURPOSE_XTS_AES_256_KEY_1 ||
+            purpose == ESP_EFUSE_KEY_PURPOSE_XTS_AES_256_KEY_2 ||
+#endif
+#if SOC_ECDSA_SUPPORTED
+            purpose == ESP_EFUSE_KEY_PURPOSE_ECDSA_KEY ||
+#endif
+            purpose == ESP_EFUSE_KEY_PURPOSE_XTS_AES_128_KEY)) {
+            ESP_LOGE(TAG, "BLOCK9 can not have the %d purpose because of HW bug (see TRM for more details)", purpose);
+            err = ESP_ERR_NOT_SUPPORTED;
+            goto err_exit;
+        }
+#endif // SOC_EFUSE_BLOCK9_KEY_PURPOSE_QUIRK
+
         if (purpose == ESP_EFUSE_KEY_PURPOSE_XTS_AES_128_KEY ||
 #ifdef SOC_FLASH_ENCRYPTION_XTS_AES_256
             purpose == ESP_EFUSE_KEY_PURPOSE_XTS_AES_256_KEY_1 ||
