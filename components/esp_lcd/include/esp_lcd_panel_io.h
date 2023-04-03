f373bea201f9a9a2ae38ab141e7fc805fb86d328
morris <maoshengrong@espressif.com>
2023-04-03 14:08:59 +0800

    i80_lcd: support skip command phase
    Closes https://github.com/espressif/esp-idf/issues/10794

--- components/esp_lcd/include/esp_lcd_panel_io.h
+++ components/esp_lcd/include/esp_lcd_panel_io.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2021-2023 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -71,7 +71,7 @@ esp_err_t esp_lcd_panel_io_rx_param(esp_lcd_panel_io_handle_t io, int lcd_cmd, v
  *       this function will wait until they are finished and the queue is empty before sending the command(s).
  *
  * @param[in] io LCD panel IO handle, which is created by other factory API like `esp_lcd_new_panel_io_spi()`
- * @param[in] lcd_cmd The specific LCD command (set to -1 if no command needed - only in SPI and I2C)
+ * @param[in] lcd_cmd The specific LCD command, set to -1 if no command needed
  * @param[in] param Buffer that holds the command specific parameters, set to NULL if no parameter is needed for the command
  * @param[in] param_size Size of `param` in memory, in bytes, set to zero if no parameter is needed for the command
  * @return
