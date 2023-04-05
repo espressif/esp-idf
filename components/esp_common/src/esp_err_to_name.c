8efe2f86e9b5fc7c7ae4c388a70d0248522068b6
Laukik Hase <laukik.hase@espressif.com>
2023-04-05 18:04:18 +0530

    nvs_flash: Added helper component `nvs_sec_provider` - For processing NVS encryption-related security configuration

--- components/esp_common/src/esp_err_to_name.c
+++ components/esp_common/src/esp_err_to_name.c
@@ -72,6 +72,9 @@
 #if __has_include("nvs.h")
 #include "nvs.h"
 #endif
+#if __has_include("nvs_sec_provider.h")
+#include "nvs_sec_provider.h"
+#endif
 #if __has_include("spi_flash_mmap.h")
 #include "spi_flash_mmap.h"
 #endif
@@ -829,6 +832,25 @@ static const esp_err_msg_t esp_err_msg_table[] = {
 #   endif
 #   ifdef      ESP_ERR_TCP_TRANSPORT_NO_MEM
     ERR_TBL_IT(ESP_ERR_TCP_TRANSPORT_NO_MEM),                   /* 57348 0xe004 Memory allocation failed */
+#   endif
+    // components/nvs_sec_provider/include/nvs_sec_provider.h
+#   ifdef      ESP_ERR_NVS_SEC_BASE
+    ERR_TBL_IT(ESP_ERR_NVS_SEC_BASE),                           /* 61440 0xf000 Starting number of error codes */
+#   endif
+#   ifdef      ESP_ERR_NVS_SEC_HMAC_KEY_NOT_FOUND
+    ERR_TBL_IT(ESP_ERR_NVS_SEC_HMAC_KEY_NOT_FOUND),             /* 61441 0xf001 HMAC Key required to generate the NVS
+                                                                                encryption keys not found */
+#   endif
+#   ifdef      ESP_ERR_NVS_SEC_HMAC_KEY_BLK_ALREADY_USED
+    ERR_TBL_IT(ESP_ERR_NVS_SEC_HMAC_KEY_BLK_ALREADY_USED),      /* 61442 0xf002 Provided eFuse block for HMAC key
+                                                                                generation is already in use */
+#   endif
+#   ifdef      ESP_ERR_NVS_SEC_HMAC_KEY_GENERATION_FAILED
+    ERR_TBL_IT(ESP_ERR_NVS_SEC_HMAC_KEY_GENERATION_FAILED),     /* 61443 0xf003 Failed to generate/write the HMAC key to eFuse */
+#   endif
+#   ifdef      ESP_ERR_NVS_SEC_HMAC_XTS_KEYS_DERIV_FAILED
+    ERR_TBL_IT(ESP_ERR_NVS_SEC_HMAC_XTS_KEYS_DERIV_FAILED),     /* 61444 0xf004 Failed to derive the NVS encryption keys
+                                                                                based on the HMAC-based scheme */
 #   endif
 };
 #endif //CONFIG_ESP_ERR_TO_NAME_LOOKUP
