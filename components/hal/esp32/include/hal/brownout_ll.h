f44e8a8a8fe7034be7d33b69a3b6ecb6355ba5b1
Cao Sen Miao <caosenmiao@espressif.com>
2023-04-13 18:54:14 +0800

    brownout: Disable the hardware BOD when BOD interrupt is enabled

--- components/hal/esp32/include/hal/brownout_ll.h
+++ components/hal/esp32/include/hal/brownout_ll.h
@@ -94,6 +94,16 @@ static inline void brownout_ll_intr_enable(bool enable)
 }
 
 /**
+ * @brief Enable brownout hardware reset
+ *
+ * @param enable
+ */
+static inline void brownout_ll_ana_reset_enable(bool enable)
+{
+    // Not supported on ESP32
+}
+
+/**
  * @brief Clear interrupt bits.
  */
 __attribute__((always_inline))
