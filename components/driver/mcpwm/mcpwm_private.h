7b93cf91aa2a6f5c2d661f90bbcc00b2b4fa6d7e
morris <maoshengrong@espressif.com>
2023-05-05 16:29:20 +0800

    mcpwm: can't apply the same delay module to multiple generators
    This is a hardware limitation, one delay module can only be used by one generator at one time.
    
    Closes https://github.com/espressif/esp-idf/issues/11327

--- components/driver/mcpwm/mcpwm_private.h
+++ components/driver/mcpwm/mcpwm_private.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -102,6 +102,8 @@ struct mcpwm_oper_t {
     mcpwm_operator_brake_mode_t brake_mode_on_soft_fault;          // brake mode on software triggered fault
     mcpwm_operator_brake_mode_t brake_mode_on_gpio_fault[SOC_MCPWM_GPIO_FAULTS_PER_GROUP]; // brake mode on GPIO triggered faults
     uint32_t deadtime_resolution_hz;     // resolution of deadtime submodule
+    mcpwm_gen_t *posedge_delay_owner;    // which generator owns the positive edge delay
+    mcpwm_gen_t *negedge_delay_owner;    // which generator owns the negative edge delay
     mcpwm_brake_event_cb_t on_brake_cbc; // callback function which would be invoked when mcpwm operator goes into trip zone
     mcpwm_brake_event_cb_t on_brake_ost; // callback function which would be invoked when mcpwm operator goes into trip zone
     void *user_data;                     // user data which would be passed to the trip zone callback
