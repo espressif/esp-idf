espressif/esp-idf

commit 2b8e40e760243218b5c286ca388ad6a60610359a

esp_wifi: WPA3-SAE support for softAP

components/wpa_supplicant/esp_supplicant/src/esp_wpa_main.c
@@ -180,6 +180,7 @@ void wpa_ap_get_peer_spp_msg(void *sm_data, bool *spp_cap, bool *spp_req)
 
 bool  wpa_deattach(void)
 {
+    esp_wpa3_free_sae_data();
     esp_wifi_sta_wpa2_ent_disable();
     wpa_sm_deinit();
     return true;
@@ -275,7 +276,7 @@ static int check_n_add_wps_sta(struct hostapd_data *hapd, struct sta_info *sta_i
 }
 #endif
 
-static bool hostap_sta_join(void **sta, u8 *bssid, u8 *wpa_ie, u8 wpa_ie_len, bool *pmf_enable)
+static bool hostap_sta_join(void **sta, u8 *bssid, u8 *wpa_ie, u8 wpa_ie_len,u8 *rsnxe, u8 rsnxe_len, bool *pmf_enable, int subtype)
 {
     struct sta_info *sta_info;
     struct hostapd_data *hapd = hostapd_get_hapd_data();
@@ -284,7 +285,7 @@ static bool hostap_sta_join(void **sta, u8 *bssid, u8 *wpa_ie, u8 wpa_ie_len, bo
         return 0;
     }
 
-    if (*sta) {
+    if (*sta && !esp_wifi_ap_is_sta_sae_reauth_node(bssid)) {
        ap_free_sta(hapd, *sta);
     }
     sta_info = ap_sta_add(hapd, bssid);
@@ -298,7 +299,7 @@ static bool hostap_sta_join(void **sta, u8 *bssid, u8 *wpa_ie, u8 wpa_ie_len, bo
         return true;
     }
 #endif
-    if (wpa_ap_join(sta_info, bssid, wpa_ie, wpa_ie_len, pmf_enable)) {
+    if (wpa_ap_join(sta_info, bssid, wpa_ie, wpa_ie_len, rsnxe, rsnxe_len, pmf_enable, subtype)) {
         *sta = sta_info;
         return true;
     }
@@ -345,6 +346,7 @@ int esp_supplicant_init(void)
     wpa_cb->wpa_config_done = wpa_config_done;
     wpa_cb->wpa_sta_set_ap_rsnxe = wpa_sm_set_ap_rsnxe;
 
+    esp_wifi_register_wpa3_ap_cb(wpa_cb);
     esp_wifi_register_wpa3_cb(wpa_cb);
 #ifdef CONFIG_OWE_STA
     esp_wifi_register_owe_cb(wpa_cb);

components/esp_wifi/Kconfig
@@ -250,6 +250,14 @@ menu "Wi-Fi"
         help
             Select this option to enable SAE-PK
 
+    config ESP_WIFI_SOFTAP_SAE_SUPPORT
+        bool "Enable WPA3 Personal(SAE) SoftAP"
+        default y
+        depends on ESP_WIFI_ENABLE_WPA3_SAE
+        depends on ESP_WIFI_SOFTAP_SUPPORT
+        help
+            Select this option to enable SAE support in softAP mode.
+
     config ESP_WIFI_ENABLE_WPA3_OWE_STA
         bool "Enable OWE STA"
         default y

components/esp_wifi/include/esp_wifi_types.h
@@ -287,6 +287,7 @@ typedef struct {
     wifi_cipher_type_t pairwise_cipher;   /**< pairwise cipher of SoftAP, group cipher will be derived using this. cipher values are valid starting from WIFI_CIPHER_TYPE_TKIP, enum values before that will be considered as invalid and default cipher suites(TKIP+CCMP) will be used. Valid cipher suites in softAP mode are WIFI_CIPHER_TYPE_TKIP, WIFI_CIPHER_TYPE_CCMP and WIFI_CIPHER_TYPE_TKIP_CCMP. */
     bool ftm_responder;         /**< Enable FTM Responder mode */
     wifi_pmf_config_t pmf_cfg;  /**< Configuration for Protected Management Frame */
+    wifi_sae_pwe_method_t sae_pwe_h2e;  /**< Configuration for SAE PWE derivation method */
 } wifi_ap_config_t;
 
 /** @brief STA configuration settings for the device */
@@ -307,8 +308,8 @@ typedef struct {
     uint32_t ft_enabled:1;                    /**< Whether FT is enabled for the connection */
     uint32_t owe_enabled:1;                   /**< Whether OWE is enabled for the connection */
     uint32_t transition_disable:1;            /**< Whether to enable transition disable feature */
-    uint32_t reserved:26;                      /**< Reserved for future feature set */
-    wifi_sae_pwe_method_t sae_pwe_h2e;        /**< Whether SAE hash to element is enabled */
+    uint32_t reserved:26;                     /**< Reserved for future feature set */
+    wifi_sae_pwe_method_t sae_pwe_h2e;        /**< Configuration for SAE PWE derivation method */
     wifi_sae_pk_mode_t sae_pk_mode;           /**< SAE-PK mode */
     uint8_t failure_retry_cnt;                /**< Number of connection retries station will do before moving to next AP. scan_method should be set as WIFI_ALL_CHANNEL_SCAN to use this config.
                                                    Note: Enabling this may cause connection time to increase incase best AP doesn't behave properly. */

components/wpa_supplicant/CMakeLists.txt
@@ -4,7 +4,10 @@ set(srcs "port/os_xtensa.c"
     "src/ap/ieee802_1x.c"
     "src/ap/wpa_auth.c"
     "src/ap/wpa_auth_ie.c"
+    "src/ap/pmksa_cache_auth.c"
     "src/ap/sta_info.c"
+    "src/ap/ieee802_11.c"
+    "src/ap/comeback_token.c"
     "src/common/sae.c"
     "src/common/dragonfly.c"
     "src/common/wpa_common.c"
@@ -233,6 +236,9 @@ if(CONFIG_ESP_WIFI_ENABLE_WPA3_SAE)
 endif()
 if(CONFIG_ESP_WIFI_ENABLE_SAE_PK)
     target_compile_definitions(${COMPONENT_LIB} PRIVATE CONFIG_SAE_PK)
+endif()
+if(CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT)
+    target_compile_definitions(${COMPONENT_LIB} PRIVATE CONFIG_SAE)
 endif()
 if(CONFIG_ESP_WIFI_WPS_STRICT)
     target_compile_definitions(${COMPONENT_LIB} PRIVATE CONFIG_WPS_STRICT)

components/wpa_supplicant/esp_supplicant/src/crypto/crypto_mbedtls-bignum.c
@@ -317,10 +317,10 @@ int crypto_bignum_addmod(const struct crypto_bignum *a,
     struct crypto_bignum *tmp = crypto_bignum_init();
     int ret = -1;
 
-    if (mbedtls_mpi_add_mpi((mbedtls_mpi *) tmp, (const mbedtls_mpi *) b, (const mbedtls_mpi *) c) < 0)
+    if (mbedtls_mpi_add_mpi((mbedtls_mpi *) tmp, (const mbedtls_mpi *) a, (const mbedtls_mpi *) b) < 0)
         goto fail;
 
-    if (mbedtls_mpi_mod_mpi( (mbedtls_mpi *) a, (const mbedtls_mpi *) tmp, (const mbedtls_mpi *) d) < 0)
+    if (mbedtls_mpi_mod_mpi( (mbedtls_mpi *) d, (const mbedtls_mpi *) tmp, (const mbedtls_mpi *) c) < 0)
         goto fail;
 
     ret = 0;

components/wpa_supplicant/esp_supplicant/src/esp_hostap.c
@@ -17,8 +17,13 @@
 #include "ap/wpa_auth_i.h"
 #include "esp_wifi_driver.h"
 #include "esp_wifi_types.h"
+#include "esp_wpa3_i.h"
 
 struct hostapd_data *global_hapd;
+
+#ifdef CONFIG_SAE
+extern SemaphoreHandle_t g_wpa3_hostap_auth_api_lock;
+#endif /* CONFIG_SAE */
 
 struct hostapd_data *hostapd_get_hapd_data(void)
 {
@@ -33,6 +38,7 @@ void *hostap_init(void)
     u16 spp_attrubute = 0;
     u8 pairwise_cipher;
     wifi_pmf_config_t pmf_cfg;
+    uint8_t authmode;
 
     hapd = (struct hostapd_data *)os_zalloc(sizeof(struct hostapd_data));
 
@@ -46,7 +52,6 @@ void *hostap_init(void)
         os_free(hapd);
         return NULL;
     }
-    hapd->conf->max_num_sta = MAX_STA_COUNT;
 
     auth_conf = (struct wpa_auth_config *)os_zalloc(sizeof(struct  wpa_auth_config));
 
@@ -56,14 +61,22 @@ void *hostap_init(void)
         hapd = NULL;
         return NULL;
     }
-    if (esp_wifi_ap_get_prof_authmode_internal() == WIFI_AUTH_WPA_PSK) {
+
+    hapd->conf->sae_pwe = esp_wifi_get_config_sae_pwe_h2e_internal(WIFI_IF_AP);
+    auth_conf->sae_pwe = hapd->conf->sae_pwe;
+
+    authmode = esp_wifi_ap_get_prof_authmode_internal();
+    if (authmode == WIFI_AUTH_WPA_PSK) {
         auth_conf->wpa = WPA_PROTO_WPA;
     }
-    if (esp_wifi_ap_get_prof_authmode_internal() == WIFI_AUTH_WPA2_PSK) {
+    if (authmode == WIFI_AUTH_WPA2_PSK) {
         auth_conf->wpa = WPA_PROTO_RSN;
     }
-    if (esp_wifi_ap_get_prof_authmode_internal() == WIFI_AUTH_WPA_WPA2_PSK) {
+    if (authmode == WIFI_AUTH_WPA_WPA2_PSK) {
         auth_conf->wpa = WPA_PROTO_RSN | WPA_PROTO_WPA;
+    }
+    if (authmode == WIFI_AUTH_WPA3_PSK || authmode == WIFI_AUTH_WPA2_WPA3_PSK) {
+        auth_conf->wpa = WPA_PROTO_RSN;
     }
 
     pairwise_cipher = esp_wifi_ap_get_prof_pairwise_cipher_internal();
@@ -73,7 +86,7 @@ void *hostap_init(void)
     esp_wifi_get_pmf_config_internal(&pmf_cfg, WIFI_IF_AP);
 
     if (pmf_cfg.required) {
-	pairwise_cipher = WIFI_CIPHER_TYPE_CCMP;
+        pairwise_cipher = WIFI_CIPHER_TYPE_CCMP;
     }
 #endif /* CONFIG_IEEE80211W */
 
@@ -98,15 +111,24 @@ void *hostap_init(void)
     auth_conf->wpa_key_mgmt = WPA_KEY_MGMT_PSK;
     auth_conf->eapol_version = EAPOL_VERSION;
 
+    hapd->conf->sae_anti_clogging_threshold = SAE_ANTI_CLOGGING_THRESHOLD;
 #ifdef CONFIG_IEEE80211W
     if (pmf_cfg.required && pmf_cfg.capable) {
-	auth_conf->ieee80211w = MGMT_FRAME_PROTECTION_REQUIRED;
-	auth_conf->wpa_key_mgmt = WPA_KEY_MGMT_PSK_SHA256;
-	wpa_printf(MSG_DEBUG, "%s :pmf required", __func__);
+        auth_conf->ieee80211w = MGMT_FRAME_PROTECTION_REQUIRED;
+        auth_conf->wpa_key_mgmt = WPA_KEY_MGMT_PSK_SHA256;
+        wpa_printf(MSG_DEBUG, "%s :pmf required", __func__);
     } else if (pmf_cfg.capable && !pmf_cfg.required) {
-	auth_conf->ieee80211w = MGMT_FRAME_PROTECTION_OPTIONAL;
-	auth_conf->wpa_key_mgmt = WPA_KEY_MGMT_PSK;
-	wpa_printf(MSG_DEBUG, "%s : pmf optional", __func__);
+        auth_conf->ieee80211w = MGMT_FRAME_PROTECTION_OPTIONAL;
+        auth_conf->wpa_key_mgmt = WPA_KEY_MGMT_PSK;
+        wpa_printf(MSG_DEBUG, "%s : pmf optional", __func__);
+    }
+
+    if (authmode == WIFI_AUTH_WPA2_WPA3_PSK) {
+        auth_conf->wpa_key_mgmt |= WPA_KEY_MGMT_SAE;
+    }
+
+    if (authmode == WIFI_AUTH_WPA3_PSK) {
+        auth_conf->wpa_key_mgmt = WPA_KEY_MGMT_SAE;
     }
 #endif /* CONFIG_IEEE80211W */
 
@@ -116,6 +138,7 @@ void *hostap_init(void)
 
     memcpy(hapd->conf->ssid.ssid, ssid->ssid, ssid->len);
     hapd->conf->ssid.ssid_len = ssid->len;
+    hapd->conf->wpa_key_mgmt = auth_conf->wpa_key_mgmt;
     hapd->conf->ssid.wpa_passphrase = (char *)os_zalloc(64);
     if (hapd->conf->ssid.wpa_passphrase == NULL) {
         os_free(auth_conf);
@@ -124,7 +147,24 @@ void *hostap_init(void)
         hapd = NULL;
         return NULL;
     }
-    memcpy(hapd->conf->ssid.wpa_passphrase, esp_wifi_ap_get_prof_password_internal(), strlen((char *)esp_wifi_ap_get_prof_password_internal()));
+
+#ifdef CONFIG_SAE
+    if (authmode == WIFI_AUTH_WPA3_PSK ||
+        authmode == WIFI_AUTH_WPA2_WPA3_PSK) {
+        if (wpa3_hostap_auth_init(hapd) != 0) {
+            os_free(hapd->conf->ssid.wpa_passphrase);
+            os_free(auth_conf);
+            os_free(hapd->conf);
+            os_free(hapd);
+            hapd = NULL;
+            return NULL;
+        }
+    }
+#endif /* CONFIG_SAE */
+
+    os_memcpy(hapd->conf->ssid.wpa_passphrase, esp_wifi_ap_get_prof_password_internal(), strlen((char *)esp_wifi_ap_get_prof_password_internal()));
+
+    hapd->conf->max_num_sta = esp_wifi_ap_get_max_sta_conn();
 
     hapd->conf->ap_max_inactivity = 5 * 60;
     hostapd_setup_wpa_psk(hapd->conf);
@@ -139,6 +179,41 @@ void *hostap_init(void)
     return (void *)hapd;
 }
 
+void hostapd_cleanup(struct hostapd_data *hapd)
+{
+    if (hapd == NULL) {
+        return;
+    }
+    if(hapd->wpa_auth) {
+        wpa_deinit(hapd->wpa_auth);
+        hapd->wpa_auth = NULL;
+    }
+
+    if (hapd->conf) {
+        hostapd_config_free_bss(hapd->conf);
+        hapd->conf = NULL;
+    }
+
+#ifdef CONFIG_SAE
+
+    struct hostapd_sae_commit_queue *q, *tmp;
+
+    if (dl_list_empty(&hapd->sae_commit_queue)) {
+        dl_list_for_each_safe(q, tmp, &hapd->sae_commit_queue,
+                struct hostapd_sae_commit_queue, list) {
+            dl_list_del(&q->list);
+            os_free(q);
+        }
+    }
+
+#endif /* CONFIG_SAE */
+
+    os_free(hapd);
+    global_hapd = NULL;
+
+}
+
+
 bool hostap_deinit(void *data)
 {
     struct hostapd_data *hapd = (struct hostapd_data *)data;
@@ -146,30 +221,95 @@ bool hostap_deinit(void *data)
     if (hapd == NULL) {
         return true;
     }
-
-    if (hapd->wpa_auth != NULL) {
-        if (hapd->wpa_auth->wpa_ie != NULL) {
-            os_free(hapd->wpa_auth->wpa_ie);
-        }
-        if (hapd->wpa_auth->group != NULL) {
-            os_free(hapd->wpa_auth->group);
-        }
-        os_free(hapd->wpa_auth);
-    }
-
-    if (hapd->conf != NULL) {
-        if (hapd->conf->ssid.wpa_psk != NULL) {
-            os_free(hapd->conf->ssid.wpa_psk);
-        }
-        if (hapd->conf->ssid.wpa_passphrase != NULL) {
-            os_free(hapd->conf->ssid.wpa_passphrase);
-        }
-        os_free(hapd->conf);
-    }
-
-    os_free(hapd);
     esp_wifi_unset_appie_internal(WIFI_APPIE_WPA);
-    global_hapd = NULL;
+
+#ifdef CONFIG_SAE
+    uint8_t authmode;
+    authmode = esp_wifi_ap_get_prof_authmode_internal();
+    if (authmode == WIFI_AUTH_WPA3_PSK ||
+        authmode == WIFI_AUTH_WPA2_WPA3_PSK) {
+        wpa3_hostap_auth_deinit();
+        /* Wait till lock is released by wpa3 task */
+        if (WPA3_HOSTAP_AUTH_API_LOCK() == pdTRUE) {
+            WPA3_HOSTAP_AUTH_API_UNLOCK();
+            os_mutex_delete(g_wpa3_hostap_auth_api_lock);
+            g_wpa3_hostap_auth_api_lock = NULL;
+        }
+    }
+#endif /* CONFIG_SAE */
+
+    hostapd_cleanup(hapd);
 
     return true;
+}
+
+int esp_wifi_build_rsnxe(struct hostapd_data *hapd, u8 *eid, size_t len)
+{
+    u8 *pos = eid;
+    u16 capab = 0;
+    size_t flen;
+
+    if (!(hapd->wpa_auth->conf.wpa & WPA_PROTO_RSN)) {
+        return 0;
+    }
+
+    if (wpa_key_mgmt_sae(hapd->wpa_auth->conf.wpa_key_mgmt) &&
+        (hapd->conf->sae_pwe == SAE_PWE_HASH_TO_ELEMENT
+         || hapd->conf->sae_pwe == SAE_PWE_BOTH)) {
+        capab |= BIT(WLAN_RSNX_CAPAB_SAE_H2E);
+    }
+
+    flen = (capab & 0xff00) ? 2 : 1;
+    if (len < 2 + flen || !capab) {
+        return 0; /* no supported extended RSN capabilities */
+    }
+    capab |= flen - 1; /* bit 0-3 = Field length (n - 1) */
+
+    *pos++ = WLAN_EID_RSNX;
+    *pos++ = flen;
+    *pos++ = capab & 0x00ff;
+    capab >>= 8;
+    if (capab) {
+        *pos++ = capab;
+    }
+
+    return pos - eid;
+}
+
+u16 esp_send_assoc_resp(struct hostapd_data *hapd, struct sta_info *sta,
+        const u8 *addr, u16 status_code, bool omit_rsnxe, int subtype)
+{
+#define ASSOC_RESP_LENGTH 20
+    u8 buf[ASSOC_RESP_LENGTH];
+    wifi_mgmt_frm_req_t *reply = NULL;
+    int send_len = 0;
+
+    int res = WLAN_STATUS_SUCCESS;
+
+    if (!omit_rsnxe) {
+        send_len = esp_wifi_build_rsnxe(hapd, buf, ASSOC_RESP_LENGTH);
+    }
+
+    esp_wifi_set_appie_internal(WIFI_APPIE_ASSOC_RESP, buf, send_len, 0);
+
+    reply = os_zalloc(sizeof(wifi_mgmt_frm_req_t) + sizeof(uint16_t));
+    if (!reply) {
+        wpa_printf(MSG_ERROR, "failed to allocate memory for assoc response");
+        res = WLAN_STATUS_UNSPECIFIED_FAILURE;
+        goto done;
+    }
+    reply->ifx = WIFI_IF_AP;
+    reply->subtype = subtype;
+    os_memcpy(reply->da, addr, ETH_ALEN);
+    reply->data_len = sizeof(uint16_t);
+
+    ((uint16_t *)reply->data)[0] = status_code;
+    if (esp_wifi_send_mgmt_frm_internal(reply) != 0) {
+        res = WLAN_STATUS_UNSPECIFIED_FAILURE;
+        wpa_printf(MSG_INFO, "esp_send_assoc_resp_failed: send failed");
+    }
+#undef ASSOC_RESP_LENGTH
+done:
+    os_free(reply);
+    return res;
 }

components/wpa_supplicant/esp_supplicant/src/esp_hostap.h
@@ -15,6 +15,9 @@ extern "C" {
 #ifdef CONFIG_ESP_WIFI_SOFTAP_SUPPORT
 void *hostap_init(void);
 bool hostap_deinit(void *data);
+u16 esp_send_assoc_resp(struct hostapd_data *data, struct sta_info *sta,
+                    const u8 *addr, u16 status_code, bool omit_rsnxe,
+                    int subtype);
 #endif
 
 #ifdef __cplusplus

components/wpa_supplicant/esp_supplicant/src/esp_wifi_driver.h
@@ -123,8 +123,8 @@ struct wpa_funcs {
     bool (*wpa_sta_in_4way_handshake)(void);
     void *(*wpa_ap_init)(void);
     bool (*wpa_ap_deinit)(void *data);
-    bool (*wpa_ap_join)(void **sm, u8 *bssid, u8 *wpa_ie, u8 wpa_ie_len, bool *pmf_enable);
-    bool (*wpa_ap_remove)(void *sm);
+    bool (*wpa_ap_join)(void **sm, u8 *bssid, u8 *wpa_ie, u8 wpa_ie_len, u8* rsnxe, u8 rsnxe_len, bool *pmf_enable, int subtype);
+    bool (*wpa_ap_remove)(void *sta_info);
     uint8_t *(*wpa_ap_get_wpa_ie)(uint8_t *len);
     bool (*wpa_ap_rx_eapol)(void *hapd_data, void *sm, u8 *data, size_t data_len);
     void (*wpa_ap_get_peer_spp_msg)(void *sm, bool *spp_cap, bool *spp_req);
@@ -134,6 +134,7 @@ struct wpa_funcs {
     int (*wpa_michael_mic_failure)(u16 is_unicast);
     uint8_t *(*wpa3_build_sae_msg)(uint8_t *bssid, uint32_t type, size_t *len);
     int (*wpa3_parse_sae_msg)(uint8_t *buf, size_t len, uint32_t type, uint16_t status);
+    int (*wpa3_hostap_handle_auth)(uint8_t *buf, size_t len, uint32_t type, uint16_t status, uint8_t *bssid);
     int (*wpa_sta_rx_mgmt)(u8 type, u8 *frame, size_t len, u8 *sender, u32 rssi, u8 channel, u64 current_tsf);
     void (*wpa_config_done)(void);
     uint8_t *(*owe_build_dhie)(uint16_t group);
@@ -190,9 +191,13 @@ typedef struct {
 } wifi_wpa_igtk_t;
 
 typedef struct {
+#ifndef ETH_ALEN
+#define ETH_ALEN 6
+#endif
     wifi_interface_t ifx;
     uint8_t subtype;
     uint32_t data_len;
+    uint8_t da[ETH_ALEN];
     uint8_t data[0];
 } wifi_mgmt_frm_req_t;
 
@@ -279,10 +284,13 @@ esp_err_t esp_wifi_remain_on_channel(uint8_t ifx, uint8_t type, uint8_t channel,
 bool esp_wifi_is_mbo_enabled_internal(uint8_t if_index);
 void esp_wifi_get_pmf_config_internal(wifi_pmf_config_t *pmf_cfg, uint8_t ifx);
 bool esp_wifi_is_ft_enabled_internal(uint8_t if_index);
-uint8_t esp_wifi_sta_get_config_sae_pwe_h2e_internal(void);
 uint8_t esp_wifi_sta_get_use_h2e_internal(void);
 uint8_t esp_wifi_sta_get_config_sae_pk_internal(void);
 void esp_wifi_sta_disable_sae_pk_internal(void);
 void esp_wifi_sta_disable_wpa2_authmode_internal(void);
+uint8_t esp_wifi_ap_get_max_sta_conn(void);
+uint8_t esp_wifi_get_config_sae_pwe_h2e_internal(uint8_t ifx);
+bool esp_wifi_ap_notify_node_sae_auth_done(uint8_t *mac);
+bool esp_wifi_ap_is_sta_sae_reauth_node(uint8_t *mac);
 
 #endif /* _ESP_WIFI_DRIVER_H_ */

commit e8d9104953da4b6d4ca12f5e17ac0f5cf48054ea

esp_wifi: Bugfix wpsreg AP not responding to assoc req
Fixed regression caused by 2b8e40e7

components/wpa_supplicant/esp_supplicant/src/esp_wpa_main.c
@@ -269,7 +269,7 @@ static void wpa_sta_disconnected_cb(uint8_t reason_code)
 #ifdef CONFIG_ESP_WIFI_SOFTAP_SUPPORT
 
 #ifdef CONFIG_WPS_REGISTRAR
-static int check_n_add_wps_sta(struct hostapd_data *hapd, struct sta_info *sta_info, u8 *ies, u8 ies_len, bool *pmf_enable)
+static int check_n_add_wps_sta(struct hostapd_data *hapd, struct sta_info *sta_info, u8 *ies, u8 ies_len, bool *pmf_enable, int subtype)
 {
     struct wpabuf *wps_ie = ieee802_11_vendor_ie_concat(ies, ies_len, WPS_DEV_OUI_WFA);
     int wps_type = esp_wifi_get_wps_type_internal();
@@ -284,7 +284,10 @@ static int check_n_add_wps_sta(struct hostapd_data *hapd, struct sta_info *sta_i
 
     if (sta_info->eapol_sm) {
         wpa_printf(MSG_DEBUG, "considering station " MACSTR " for WPS", MAC2STR(sta_info->addr));
-        return 1;
+        if (esp_send_assoc_resp(hapd, sta_info, sta_info->addr, WLAN_STATUS_SUCCESS, true, subtype) != WLAN_STATUS_SUCCESS) {
+            wpa_printf(MSG_ERROR, "failed to send assoc response " MACSTR, MAC2STR(sta_info->addr));
+            return -1;
+        }
     }
 
     return 0;
@@ -293,30 +296,40 @@ static int check_n_add_wps_sta(struct hostapd_data *hapd, struct sta_info *sta_i
 
 static bool hostap_sta_join(void **sta, u8 *bssid, u8 *wpa_ie, u8 wpa_ie_len,u8 *rsnxe, u8 rsnxe_len, bool *pmf_enable, int subtype)
 {
-    struct sta_info *sta_info;
+    struct sta_info *sta_info = NULL;
     struct hostapd_data *hapd = hostapd_get_hapd_data();
 
     if (!hapd) {
-        return 0;
+        goto fail;
     }
 
     if (*sta && !esp_wifi_ap_is_sta_sae_reauth_node(bssid)) {
        ap_free_sta(hapd, *sta);
     }
+
     sta_info = ap_sta_add(hapd, bssid);
     if (!sta_info) {
         wpa_printf(MSG_ERROR, "failed to add station " MACSTR, MAC2STR(bssid));
-	return 0;
+        goto fail;
     }
 #ifdef CONFIG_WPS_REGISTRAR
-    if (check_n_add_wps_sta(hapd, sta_info, wpa_ie, wpa_ie_len, pmf_enable)) {
-        *sta = sta_info;
-        return true;
+    if (check_n_add_wps_sta(hapd, sta_info, wpa_ie, wpa_ie_len, pmf_enable, subtype) == 0) {
+        if (sta_info->eapol_sm) {
+            *sta = sta_info;
+            return true;
+        }
+    } else {
+        goto fail;
     }
 #endif
     if (wpa_ap_join(sta_info, bssid, wpa_ie, wpa_ie_len, rsnxe, rsnxe_len, pmf_enable, subtype)) {
         *sta = sta_info;
         return true;
+    }
+
+fail:
+    if (sta_info) {
+        ap_free_sta(hapd, sta_info);
     }
 
     return false;