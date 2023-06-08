ac5d805d0eda75442dd17cfea5baaca9748f8215
Marius Vikhammer <marius.vikhammer@espressif.com>
2023-06-08 10:16:36 +0800

    versions: Update version to 4.4.5

--- components/esp_common/include/esp_idf_version.h
+++ components/esp_common/include/esp_idf_version.h
@@ -23,7 +23,7 @@ extern "C" {
 /** Minor version number (x.X.x) */
 #define ESP_IDF_VERSION_MINOR   4
 /** Patch version number (x.x.X) */
-#define ESP_IDF_VERSION_PATCH   4
+#define ESP_IDF_VERSION_PATCH   5
 
 /**
  * Macro to convert IDF version number into an integer
