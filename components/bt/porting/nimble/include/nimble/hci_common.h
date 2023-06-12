6eb1a8f51fdc345528dd7fa1236bc13fe8d99a13
Rahul Tank <rahul.tank@espressif.com>
2023-06-12 16:55:52 +0530

    fix (Nimble): Add PCL Set RSSI VSC and example code for its usage
    This adds API to send a vs hci command and an event to handle vs hci events
    Also added information for Set RSSI VSC for Power control.

--- components/bt/porting/nimble/include/nimble/hci_common.h
+++ components/bt/porting/nimble/include/nimble/hci_common.h
@@ -1136,6 +1136,11 @@ struct ble_hci_vs_rd_static_addr_rp {
     uint8_t addr[6];
 } __attribute__((packed));
 
+
+#if SOC_BLE_POWER_CONTROL_SUPPORTED && MYNEWT_VAL(BLE_HCI_VS)
+#define BLE_HCI_OCF_VS_PCL_SET_RSSI 			(MYNEWT_VAL(BLE_HCI_VS_OCF_OFFSET) + (0x0111))
+#endif
+
 /* Command Specific Definitions */
 /* --- Set controller to host flow control (OGF 0x03, OCF 0x0031) --- */
 #define BLE_HCI_CTLR_TO_HOST_FC_OFF         (0)
