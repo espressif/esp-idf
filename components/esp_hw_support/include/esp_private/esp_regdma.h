30a67ed72c42ad34125391f47ca062640c39e1b0
cjin <jinchen@espressif.com>
2023-06-13 19:18:57 +0800

    btbb: support register retention in esp32c6

--- components/esp_hw_support/include/esp_private/esp_regdma.h
+++ components/esp_hw_support/include/esp_private/esp_regdma.h
@@ -40,7 +40,7 @@ extern "C" {
 #define REGDMA_SPIMEM_LINK(_pri)            ((0x13 << 8) | _pri)
 #define REGDMA_SYSTIMER_LINK(_pri)          ((0x14 << 8) | _pri)
 #define REGDMA_BLE_MAC_LINK(_pri)           ((0x15 << 8) | _pri)
-#define REGDMA_BB_LINK(_pri)                ((0x16 << 8) | _pri)
+#define REGDMA_MODEM_BT_BB_LINK(_pri)       ((0x16 << 8) | _pri)
 #define REGDMA_MODEM_FE_LINK(_pri)          ((0xFF << 8) | _pri)
 
 typedef enum {
