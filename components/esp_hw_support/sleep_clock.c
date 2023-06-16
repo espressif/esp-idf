f3457133f60e876d62b8ac677364b84327661246
cjin <jinchen@espressif.com>
2023-06-16 10:23:52 +0800

    ble:added esp32c6 light sleep support

--- components/esp_hw_support/sleep_clock.c
+++ components/esp_hw_support/sleep_clock.c
@@ -68,21 +68,21 @@ bool IRAM_ATTR clock_domain_pd_allowed(void)
     const uint32_t modules = sleep_retention_get_modules();
     const uint32_t mask = (const uint32_t) (
             SLEEP_RETENTION_MODULE_CLOCK_SYSTEM
-#if CONFIG_MAC_BB_PD
+#if CONFIG_MAC_BB_PD || CONFIG_BT_LE_SLEEP_ENABLE
           | SLEEP_RETENTION_MODULE_CLOCK_MODEM
 #endif
           );
     return ((modules & mask) == mask);
 }
 
-#if CONFIG_PM_POWER_DOWN_PERIPHERAL_IN_LIGHT_SLEEP || CONFIG_MAC_BB_PD
+#if CONFIG_PM_POWER_DOWN_PERIPHERAL_IN_LIGHT_SLEEP || CONFIG_MAC_BB_PD || CONFIG_BT_LE_SLEEP_ENABLE
 ESP_SYSTEM_INIT_FN(sleep_clock_startup_init, BIT(0), 106)
 {
 #if CONFIG_PM_POWER_DOWN_PERIPHERAL_IN_LIGHT_SLEEP
     sleep_clock_system_retention_init();
 #endif
 
-#if CONFIG_MAC_BB_PD
+#if CONFIG_MAC_BB_PD || CONFIG_BT_LE_SLEEP_ENABLE
     sleep_clock_modem_retention_init();
 #endif
     return ESP_OK;
