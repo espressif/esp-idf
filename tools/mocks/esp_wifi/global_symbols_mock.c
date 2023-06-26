9ed56031b2ef98024e073ef7fa2edeacdf684954
Jakob Hasse <jakob.hasse@espressif.com>
2023-06-26 13:22:03 +0800

    feat(linux): added esp_wifi mocking support

---
+++ tools/mocks/esp_wifi/global_symbols_mock.c
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "esp_wifi.h"
+
+/**
+ * The following global objects are defined when WiFi is mocked.
+ */
+
+const wpa_crypto_funcs_t g_wifi_default_wpa_crypto_funcs = { };
+
+wifi_osi_funcs_t g_wifi_osi_funcs = { };
+
+uint64_t g_wifi_feature_caps = 0;
