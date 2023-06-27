434a9e4e2f5f3a57457434b827aa84e099052888
Zim Kalinowski <zim.kalinowski@espressif.com>
2023-06-27 17:13:54 +0800

    ci(bootloader): Enable QEMU tests (ESP32 and ESP32C3)

--- components/bootloader_support/test_apps/bootloader_support/pytest_bootloader_support.py
+++ components/bootloader_support/test_apps/bootloader_support/pytest_bootloader_support.py
@@ -7,5 +7,22 @@ from pytest_embedded import Dut
 
 @pytest.mark.generic
 @pytest.mark.supported_targets
+@pytest.mark.nightly_run
 def test_bootloader_support(dut: Dut) -> None:
     dut.run_all_single_board_cases()
+
+
+@pytest.mark.host_test
+@pytest.mark.qemu
+@pytest.mark.esp32
+def test_bootloader_support_qemu_esp32(dut: Dut) -> None:
+    dut.run_all_single_board_cases()
+
+
+@pytest.mark.host_test
+@pytest.mark.qemu
+@pytest.mark.esp32c3
+def test_bootloader_support_qemu_esp32c3(dut: Dut) -> None:
+    for case in dut.test_menu:
+        if 'qemu-ignore-c3' not in case.groups:
+            dut._run_normal_case(case)
