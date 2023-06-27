5c608f0f50a93fe2b40dae9c162f110a8256a9c1
Lou Tianhao <loutianhao@espressif.com>
2023-06-27 16:31:56 +0800

    TODO: IDF-7657

--- components/esp_pm/test_apps/esp_pm/pytest_esp_pm.py
+++ components/esp_pm/test_apps/esp_pm/pytest_esp_pm.py
@@ -4,13 +4,14 @@
 import pytest
 from pytest_embedded import Dut
 
+CONFIGS = [
+    pytest.param('default', marks=[pytest.mark.supported_targets, pytest.mark.temp_skip_ci(targets=['esp32h2'], reason='TODO: IDF-7657')]),
+    pytest.param('limits', marks=[pytest.mark.supported_targets, pytest.mark.temp_skip_ci(targets=['esp32h2'], reason='TODO: IDF-7657')]),
+    pytest.param('options', marks=[pytest.mark.supported_targets, pytest.mark.temp_skip_ci(targets=['esp32h2'], reason='TODO: IDF-7657')]),
+]
+
 
 @pytest.mark.generic
-@pytest.mark.supported_targets
-@pytest.mark.parametrize('config', [
-    'default',
-    'limits',
-    'options',
-], indirect=True)
+@pytest.mark.parametrize('config', CONFIGS, indirect=True)
 def test_esp_pm(dut: Dut) -> None:
     dut.run_all_single_board_cases()
