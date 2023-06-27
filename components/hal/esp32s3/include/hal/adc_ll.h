d21e332281b3f2fb67913c1245e4387b698bb2a9
Armando <douyiwen@espressif.com>
2023-06-27 12:17:33 +0800

    fix(adc): fixed esp32, esp32s2,esp32s3 adc oneshot mode clk div issue

--- components/hal/esp32s3/include/hal/adc_ll.h
+++ components/hal/esp32s3/include/hal/adc_ll.h
@@ -34,7 +34,7 @@ extern "C" {
                     Oneshot
 ---------------------------------------------------------------*/
 #define ADC_LL_DATA_INVERT_DEFAULT(PERIPH_NUM)         (0)
-#define ADC_LL_SAR_CLK_DIV_DEFAULT(PERIPH_NUM)         ((PERIPH_NUM==0)? 2 : 1)
+#define ADC_LL_SAR_CLK_DIV_DEFAULT(PERIPH_NUM)         (1)
 
 /*---------------------------------------------------------------
                     DMA
