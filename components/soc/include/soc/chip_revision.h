96b312ddde6fdcb4f864e07135562c30f3749896
Darian Leung <darian@espressif.com>
2023-06-30 13:26:14 +0100

    soc: Move revision MAX/MIN static assert to esp_hw_support
    Previously, "soc/chip_revision.h" contained a static assert to check that the
    CONFIG_ESP_REV_MIN_FULL <= CONFIG_ESP_REV_MAX_FULL. There are two issues with
    this assert:
    
    - Contained in a header file, so it is only compiled if the "chip_revision.h"
    is included somewhere
    - CONFIG_ESP_REV_MIN_FULL and CONFIG_ESP_REV_MAX_FULL are defined in
    "esp_hw_support", which is a G0 component. This creates a reverse dependency
    of G0 on G1.
    
    This commit moves the static assert "revision.c" in "esp_hw_support".

--- components/soc/include/soc/chip_revision.h
+++ components/soc/include/soc/chip_revision.h
@@ -1,12 +1,10 @@
 /*
- * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2021-2023 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
-
-#include "sdkconfig.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,8 +30,6 @@ extern "C" {
 
 #define ESP_CHIP_REV_ABOVE(rev, min_rev) ((min_rev) <= (rev))
 #define ESP_CHIP_REV_MAJOR_AND_ABOVE(rev, min_rev) (((rev) / 100 == (min_rev) / 100) && ((rev) >= (min_rev)))
-
-_Static_assert(CONFIG_ESP_REV_MIN_FULL <= CONFIG_ESP_REV_MAX_FULL);
 
 #ifdef __cplusplus
 }
