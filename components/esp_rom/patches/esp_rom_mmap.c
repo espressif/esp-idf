69563f06c9fec2ae7f27da1754c5cf67ae8d471c
Armando <douyiwen@espressif.com>
2023-07-03 16:49:38 +0800

    fix(memory): no enough memory for rodata

--- components/esp_rom/patches/esp_rom_mmap.c
+++ components/esp_rom/patches/esp_rom_mmap.c
@@ -13,7 +13,6 @@
 uint32_t Cache_Get_IROM_MMU_End(void)
 {
 #if CONFIG_IDF_TARGET_ESP32S3
-	esp_rom_printf("0x800\n");
     return 0x800;
 #elif CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32H2
     return 0x200;
