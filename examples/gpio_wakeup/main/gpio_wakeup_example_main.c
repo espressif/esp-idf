489ffbb6ae8770385e79f01b0c4e56aca937eba7
Laiwen-Li <lilaiwen990902@163.com>
2023-07-05 15:00:54 +0800

    auto light sleep, empty isr and task, configurable parameters are set to macros

--- examples/gpio_wakeup/main/gpio_wakeup_example_main.c
+++ examples/gpio_wakeup/main/gpio_wakeup_example_main.c
@@ -21,13 +21,16 @@
 
 static const char *TAG = "gpio_wake_up_main";
 
+// for test
+// #define GPIO_OUTPUT_IO_0    5
+// #define GPIO_OUTPUT_PIN_SEL  (1ULL<<GPIO_OUTPUT_IO_0)
 
 esp_err_t all_config_init(void)
 {
     //config gpio wake
-    ESP_RETURN_ON_ERROR(example_register_gpio_wakeup(), TAG, "example_register_gpio_wakeup error");
+    ESP_RETURN_ON_ERROR(example_register_gpio_wakeup_sleep(), TAG, "example_register_gpio_wakeup error");
     // auto light sleep and light sleep lock init
-    ESP_RETURN_ON_ERROR(light_sleep_config(), TAG, "light_sleep_config error");
+    ESP_RETURN_ON_ERROR(power_config(), TAG, "power_config error");
 
     return ESP_OK;
 }
@@ -37,6 +40,31 @@ void app_main(void)
     // all init: light sleep、interrupt...
     ESP_ERROR_CHECK(all_config_init());
     esp_rom_printf("all init success!\n");
+
+    //------------------ for test -----------------------------
+    // //the config structure.
+    // gpio_config_t io_conf = {};
+    // //disable interrupt
+    // io_conf.intr_type = GPIO_INTR_DISABLE;
+    // //set as output mode
+    // io_conf.mode = GPIO_MODE_OUTPUT;
+    // //bit mask of the pins that you want to set,e.g.GPIO18/19
+    // io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    // //disable pull-down mode
+    // io_conf.pull_down_en = 0;
+    // //disable pull-up mode
+    // io_conf.pull_up_en = 0;
+    // //configure GPIO with the given settings
+    // gpio_config(&io_conf);
+
+
+    // int cnt = 0;
+    // while(cnt++ < 100) {
+    //     //printf("cnt: %d\n", cnt++);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    //     gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
+    // }
+    //------------------ for test -----------------------------
 
 
 #if CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
