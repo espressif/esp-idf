909d8b6db5b060faf305891df5bdf4b4d4c93d0d
IhorNehrutsa <Ihor.Nehrutsa@gmail.com>
2023-07-05 16:11:48 +0300

    fix(ledc): fix frequency calculation
    ledc.c: Fix frequency calculation.
    Round int instead of truncate.
    
    Merges https://github.com/espressif/esp-idf/pull/11810

--- components/driver/test_apps/ledc/main/test_app_main.c
+++ components/driver/test_apps/ledc/main/test_app_main.c
@@ -10,7 +10,7 @@
 #include "esp_heap_caps.h"
 
 // Some resources are lazy allocated in LEDC driver, the threshold is left for that case
-#define TEST_MEMORY_LEAK_THRESHOLD (150)
+#define TEST_MEMORY_LEAK_THRESHOLD (230)
 
 static size_t before_free_8bit;
 static size_t before_free_32bit;
