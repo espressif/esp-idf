ee6436cee986ae8665df95bc9a45678741325e8f
Laiwen-Li <lilaiwen990902@163.com>
2023-07-05 15:00:54 +0800

    auto light sleep, empty isr and task, configurable parameters are set to macros

--- examples/gpio_wakeup/main/gpio_wake_sleep/gpio_wakeup_sleep.c
+++ examples/gpio_wakeup/main/gpio_wake_sleep/gpio_wakeup_sleep.c
@@ -14,22 +14,19 @@
 #include <unistd.h>
 #include "gpio_wakeup_example.h"
 
-/* Most development boards have "boot" button attached to GPIO0.
- * You can also change this to another pin.
- */
-#if CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32C2 || CONFIG_IDF_TARGET_ESP32H2 \
-    || CONFIG_IDF_TARGET_ESP32C6
-#define BOOT_BUTTON_NUM         9
-#else
-#define BOOT_BUTTON_NUM         0
-#endif
+//---最终应该设计成在menuconfig中可配置的
+#define QUEUE_LEN                     5
+#define WAKE_SLEEP_TASK_STACK_SIZE    2048
+#define WAKE_SLEEP_TASK_PRIORITY      10
+#define PULL_UP_EN                    false
+#define PULL_DOWN_EN                  true
 
-/* Use boot button as gpio input */
-#define GPIO_WAKEUP_NUM         BOOT_BUTTON_NUM
-/* "Boot" button is active low */
-#define GPIO_WAKEUP_LEVEL       0
 
 #define ESP_INTR_FLAG_DEFAULT 0
+
+typedef struct{
+    uint32_t gpio_num;
+}queue_items_t;
 
 static const char *TAG = "gpio_wakeup_sleep";
 QueueHandle_t gpio_evt_queue = NULL;//声明队列
@@ -38,87 +35,86 @@ QueueHandle_t gpio_evt_queue = NULL;//声明队列
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
 
-    uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);//将触发中断的引脚号放入队列
+    queue_items_t isr_arg = 
+    {
+        .gpio_num = GPIO_WAKEUP_SLEEP_NUM
+    };
+    xQueueSendFromISR(gpio_evt_queue, &isr_arg, NULL);//将触发中断的引脚号放入队列
 
-    //改为下降沿触发中断
-    gpio_intr_disable(GPIO_WAKEUP_NUM);
-    gpio_set_intr_type(GPIO_WAKEUP_NUM, GPIO_INTR_NEGEDGE);
-    gpio_intr_enable(GPIO_WAKEUP_NUM);
+    // /* Enable wake up from GPIO */
+    // // low level wake up
+    // ESP_RETURN_ON_ERROR(gpio_wakeup_enable(GPIO_WAKEUP_NUM, GPIO_WAKEUP_LEVEL == 0 ? GPIO_INTR_LOW_LEVEL : GPIO_INTR_HIGH_LEVEL),
+    //                     TAG, "Enable gpio wakeup failed");
+    // ESP_RETURN_ON_ERROR(esp_sleep_enable_gpio_wakeup(), TAG, "Configure gpio as wakeup source failed");
+
+    // //设置在light sleep时，io口保持high level
+    // ESP_RETURN_ON_ERROR(gpio_sleep_set_pull_mode(GPIO_WAKEUP_SLEEP_NUM, GPIO_PULLUP_ONLY));
+
+    // //改为下降沿触发中断
+    // gpio_intr_disable(GPIO_WAKEUP_NUM);
+    // gpio_set_intr_type(GPIO_WAKEUP_NUM, GPIO_INTR_NEGEDGE);
+    // gpio_intr_enable(GPIO_WAKEUP_NUM);
 }
 
 //另开启的一个task
 static void gpio_task_example(void* arg)
 {
-    uint32_t io_num;
+    queue_items_t queue_item;
     for(;;) {//循环，从队列中取数据
-        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        if(xQueueReceive(gpio_evt_queue, &queue_item, portMAX_DELAY)) {
+            printf("GPIO[%"PRIu32"] intr, val: %d\n", queue_item.gpio_num, gpio_get_level(queue_item.gpio_num));
 
             // handle lock
-            ESP_ERROR_CHECK(esp_pm_lock_acquire(light_sleep_handle));
-            ESP_LOGI(TAG, "gpio wake up success, and handle no_light_sleep lock success!");
+            //ESP_ERROR_CHECK(esp_pm_lock_acquire(light_sleep_handle));
+            //ESP_LOGI(TAG, "gpio wake up success, and handle no_light_sleep lock success!");
         }
     }
 }
 
 void example_wait_gpio_inactive(void)
 {
-    if(gpio_get_level(GPIO_WAKEUP_NUM) == GPIO_WAKEUP_LEVEL)
-        printf("Waiting for GPIO%d to go high...\n", GPIO_WAKEUP_NUM);
-    while (gpio_get_level(GPIO_WAKEUP_NUM) == GPIO_WAKEUP_LEVEL) {
+    //init时，先等待IO口变为low level
+    if(gpio_get_level(GPIO_WAKEUP_SLEEP_NUM) == GPIO_SLEEP_LEVEL)
+        printf("Waiting for GPIO%d to go low...\n", GPIO_WAKEUP_SLEEP_NUM);
+    while (gpio_get_level(GPIO_WAKEUP_SLEEP_NUM) == GPIO_SLEEP_LEVEL) {
         vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
 
-esp_err_t example_register_gpio_wakeup(void)
+esp_err_t example_register_gpio_wakeup_sleep(void)
 {
-    // 根据外部情况，进行不同的配置
-
     /* Initialize GPIO */
     gpio_config_t config = {
-            .pin_bit_mask = BIT64(GPIO_WAKEUP_NUM),
-            //set as input mode
-            .mode = GPIO_MODE_INPUT,
-            //enable pull-up mode  上拉模式
-            .pull_up_en = 1, //检查外部是否有上拉？ 可配置
-            .pull_down_en = false,
-            .intr_type = GPIO_INTR_LOW_LEVEL  //enable interrupt
+        .pin_bit_mask = BIT64(GPIO_WAKEUP_SLEEP_NUM),
+        //set as input mode
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = PULL_UP_EN,
+        .pull_down_en = PULL_DOWN_EN,
+        .intr_type = TO_SLEEP_INTR_TYPE //上电后，应设置high level intr
     };
-    ESP_RETURN_ON_ERROR(gpio_config(&config), TAG, "Initialize GPIO%d failed", GPIO_WAKEUP_NUM);
-
-    /* Enable wake up from GPIO */
-    // low level wake up
-    ESP_RETURN_ON_ERROR(gpio_wakeup_enable(GPIO_WAKEUP_NUM, GPIO_WAKEUP_LEVEL == 0 ? GPIO_INTR_LOW_LEVEL : GPIO_INTR_HIGH_LEVEL),
-                        TAG, "Enable gpio wakeup failed");
-    ESP_RETURN_ON_ERROR(esp_sleep_enable_gpio_wakeup(), TAG, "Configure gpio as wakeup source failed");
-
-    //gpio0 low io状态与system状态保持一致
+    ESP_RETURN_ON_ERROR(gpio_config(&config), TAG, "Initialize GPIO%d failed", GPIO_WAKEUP_SLEEP_NUM);
 
     /* Make sure the GPIO is inactive and it won't trigger wakeup immediately */
     example_wait_gpio_inactive();
-    ESP_LOGI(TAG, "gpio wakeup source is ready");
-
-    /* Initialize ESP_PM_NO_LIGHT_SLEEP lock */
-    esp_pm_lock_type_t light_sleep_lock = ESP_PM_NO_LIGHT_SLEEP;// 设置成可配置
-    ESP_RETURN_ON_ERROR(esp_pm_lock_create(light_sleep_lock, 0, "contorl_light_sleep", &light_sleep_handle), TAG, "create NO_LIGHT_SLEEP lock failed");
-    ESP_LOGI(TAG, "create no_light_sleep lock success!");
+    // system sctive时，io口应保持low level
+    ESP_RETURN_ON_ERROR(gpio_hold_en(GPIO_WAKEUP_SLEEP_NUM), TAG, "enable gpio%d hold failed", GPIO_WAKEUP_SLEEP_NUM);
+    ESP_LOGI(TAG, "gpio is ready");
 
     //create a queue to handle gpio event from isr（中断服务程序）
-
     // item len的大小需要评估，频繁切换的时候，要预留  需要测试板
-    // item size大小后面可能需要修改，放到结构体中，方便1后期修改
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));  //创建队列，用于存放isr存入的数据
-    //start gpio task  创建一个task，并开始运行
+    // item size大小后面可能需要修改，放到结构体中，方便后期修改
+    gpio_evt_queue = xQueueCreate(QUEUE_LEN, sizeof(queue_items_t));
 
+    //start gpio task  创建一个task，并开始运行
     //stack导出去 可配置，如果用户在task中malloc stack，那么2048就不够用了
     //task的优先级：有多种task时，优先级也会有影响运行效果，一般设置成10
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+    xTaskCreate(gpio_task_example, "gpio_task_example", WAKE_SLEEP_TASK_STACK_SIZE, NULL, WAKE_SLEEP_TASK_PRIORITY, NULL);
+    ESP_LOGI(TAG, "gpio task is ready");
 
     //install gpio isr service
     ESP_RETURN_ON_ERROR(gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT), TAG, "install isr failed");
-    //hook isr handler for specific gpio pin
-    ESP_RETURN_ON_ERROR(gpio_isr_handler_add(GPIO_WAKEUP_NUM, gpio_isr_handler, (void*) GPIO_WAKEUP_NUM), TAG, "add isr for specific gpio pin failed");
+    //hook isr handler for specific gpio pin 
+    ESP_RETURN_ON_ERROR(gpio_isr_handler_add(GPIO_WAKEUP_SLEEP_NUM, gpio_isr_handler, NULL), TAG, "add isr for specific gpio pin failed");
     ESP_LOGI(TAG, "gpio interrupt is ready");
 
     return ESP_OK;
--- examples/gpio_wakeup/main/power_manage/pm_configure.c
+++ examples/gpio_wakeup/main/power_manage/pm_configure.c
@@ -9,15 +9,28 @@
 
 static const char *TAG = "light_sleep";
 
-esp_err_t light_sleep_config(void)
+esp_err_t power_config(void)
 {
+    /* Initialize pm lock */
+    esp_pm_lock_type_t pm_lock = PM_LOCK_TYPE;
+    ESP_RETURN_ON_ERROR(esp_pm_lock_create(pm_lock, 0, "contorl_light_sleep", &light_sleep_handle), TAG, "create lock failed");
+    ESP_LOGI(TAG, "create pm lock success!");
+
     /* Initialize pm for auto light sleep */
     esp_pm_config_t pm_conf = {};
-    pm_conf.max_freq_mhz = CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ;
-    pm_conf.min_freq_mhz = CONFIG_XTAL_FREQ;
+    pm_conf.max_freq_mhz = PM_CONFIG_MAX_FREQ_MHZ;
+    pm_conf.min_freq_mhz = PM_CONFIG_MIN_FREQ_MHZ;
     pm_conf.light_sleep_enable = true;// enable auto light sleep
 
     ESP_RETURN_ON_ERROR(esp_pm_configure(&pm_conf), TAG, "light sleep config failed");
+
+    //系统上电后，应保持active，所以应acquire lock
+    ESP_RETURN_ON_ERROR(esp_pm_lock_acquire(light_sleep_handle), TAG, "acquire pm lock failed");
+    ESP_LOGI(TAG, "acquired pm lock, system is active");
+
+    //设置初始的软件状态
+    SET_SOFTWARE_STATE(ACTIVE_STATE);
+    ESP_LOGI(TAG, "Set software state success");
 
     return ESP_OK;
 }
