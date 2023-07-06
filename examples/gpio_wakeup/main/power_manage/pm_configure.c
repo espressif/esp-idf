452c980560b95fc1c06e370103bfd990c11ea1ac
Laiwen-Li <lilaiwen990902@163.com>
2023-07-06 20:20:39 +0800

    Only the logic is correct, but not pass the ammeter test

--- examples/gpio_wakeup/main/power_manage/pm_configure.c
+++ examples/gpio_wakeup/main/power_manage/pm_configure.c
@@ -7,14 +7,17 @@
 #include "esp_check.h"
 #include "gpio_wakeup_example.h"
 
-static const char *TAG = "light_sleep";
+static const char *TAG = "power_config";
+
+extern esp_pm_lock_handle_t light_sleep_handle;
+extern software_state_t sys_state;
 
 esp_err_t power_config(void)
 {
     /* Initialize pm lock */
     esp_pm_lock_type_t pm_lock = PM_LOCK_TYPE;
     ESP_RETURN_ON_ERROR(esp_pm_lock_create(pm_lock, 0, "contorl_light_sleep", &light_sleep_handle), TAG, "create lock failed");
-    ESP_LOGI(TAG, "create pm lock success!");
+    ESP_LOGI(TAG, "create %s lock success!", PM_LOCK_TYPE_TO_STRING);
 
     /* Initialize pm for auto light sleep */
     esp_pm_config_t pm_conf = {};
@@ -22,15 +25,15 @@ esp_err_t power_config(void)
     pm_conf.min_freq_mhz = PM_CONFIG_MIN_FREQ_MHZ;
     pm_conf.light_sleep_enable = true;// enable auto light sleep
 
-    ESP_RETURN_ON_ERROR(esp_pm_configure(&pm_conf), TAG, "light sleep config failed");
+    ESP_RETURN_ON_ERROR(esp_pm_configure(&pm_conf), TAG, "auto light sleep config failed");
 
     //系统上电后，应保持active，所以应acquire lock
-    ESP_RETURN_ON_ERROR(esp_pm_lock_acquire(light_sleep_handle), TAG, "acquire pm lock failed");
-    ESP_LOGI(TAG, "acquired pm lock, system is active");
+    ESP_RETURN_ON_ERROR(esp_pm_lock_acquire(light_sleep_handle), TAG, "acquire %s lock failed", PM_LOCK_TYPE_TO_STRING);
+    ESP_LOGI(TAG, "acquired %s lock, system is active", PM_LOCK_TYPE_TO_STRING);
 
     //设置初始的软件状态
-    SET_SOFTWARE_STATE(ACTIVE_STATE);
-    ESP_LOGI(TAG, "Set software state success");
+    MARK_SOFTWARE_STATE(ACTIVE_STATE);
+    ESP_LOGI(TAG, "Set software state success: %s", sys_state ? "active" : "sleep");
 
     return ESP_OK;
 }
