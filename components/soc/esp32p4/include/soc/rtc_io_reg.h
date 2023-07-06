1c3aedf45e95a2e308b2ea0ee882ddca9182e6db
Armando <douyiwen@espressif.com>
2023-07-06 17:51:54 +0800

    remove p4 rtc_io related files as requested

--- components/soc/esp32p4/include/soc/rtc_io_reg.h
+++
@@ -1,9 +0,0 @@
-/**
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
- *
- *  SPDX-License-Identifier: Apache-2.0
- */
-#pragma once
-
-#include "soc/lp_gpio_reg.h"
-#include "soc/lp_iomux_reg.h"
