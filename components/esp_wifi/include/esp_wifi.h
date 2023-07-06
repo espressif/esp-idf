05695f7cba49609750c7f105db5d9469f84e0057
zhangyanjiao <zhangyanjiao@espressif.com>
2023-07-06 14:35:56 +0800

    feat(wifi): Add API to get RSSI info after station connects to AP
    - Add esp_wifi_sta_get_rssi() function to retrieve RSSI info after station connects to AP

--- components/esp_wifi/include/esp_wifi.h
+++ components/esp_wifi/include/esp_wifi.h
@@ -1,17 +1,8 @@
-// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+/*
+ * SPDX-FileCopyrightText: 2015-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 /*               Notes about WiFi Programming
  *
@@ -1324,6 +1315,20 @@ esp_err_t esp_wifi_config_80211_tx_rate(wifi_interface_t ifx, wifi_phy_rate_t ra
   *    - others: failed
   */
 esp_err_t esp_wifi_disable_pmf_config(wifi_interface_t ifx);
+
+/**
+  * @brief      Get the rssi info after station connected to AP
+  *
+  * @attention  This API should be called after station connected to AP.
+  *
+  * @param      rssi store the rssi info received from last beacon.
+  *
+  * @return
+  *    - ESP_OK: succeed
+  *    - ESP_ERR_INVALID_ARG: invalid argument
+  *    - ESP_FAIL: failed
+  */
+esp_err_t esp_wifi_sta_get_rssi(int *rssi);
 
 #ifdef __cplusplus
 }
