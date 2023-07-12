38255c048ac4213b0b4e9d5c0647db3b2c9ee84b
harshal.patil <harshal.patil@espressif.com>
2023-07-12 14:07:43 +0530

    fix(mbedtls): Fixed the transmission of return values of the esp-aes APIs
    - Earlier, some intermediate return values were not stored and returned,
    thus incorrect return values used to get transmitted to the upper layer of APIs.
    
    - Also, zeroised the output buffer in case of error condition.

--- components/mbedtls/port/aes/dma/esp_aes.c
+++ components/mbedtls/port/aes/dma/esp_aes.c
@@ -189,7 +189,10 @@ static esp_err_t esp_aes_isr_initialise( void )
             return ESP_FAIL;
         }
 
-        esp_intr_alloc(ETS_AES_INTR_SOURCE, 0, esp_aes_complete_isr, NULL, NULL);
+        esp_err_t ret = esp_intr_alloc(ETS_AES_INTR_SOURCE, 0, esp_aes_complete_isr, NULL, NULL);
+        if (ret != ESP_OK) {
+            return ret;
+        }
     }
 
     /* AES is clocked proportionally to CPU clock, take power management lock */
@@ -244,6 +247,8 @@ static int esp_aes_process_dma(esp_aes_context *ctx, const unsigned char *input,
 /* Output buffers in external ram needs to be 16-byte aligned and DMA cant access input in the iCache mem range,
    reallocate them into internal memory and encrypt in chunks to avoid
    having to malloc too big of a buffer
+
+  The function esp_aes_process_dma_ext_ram zeroises the output buffer in the case of memory allocation failure.
 */
 
 static int esp_aes_process_dma_ext_ram(esp_aes_context *ctx, const unsigned char *input, unsigned char *output, size_t len, uint8_t *stream_out, bool realloc_input, bool realloc_output)
@@ -260,9 +265,9 @@ static int esp_aes_process_dma_ext_ram(esp_aes_context *ctx, const unsigned char
         input_buf = heap_caps_malloc(chunk_len, MALLOC_CAP_DMA);
 
         if (input_buf == NULL) {
+            mbedtls_platform_zeroize(output, len);
             ESP_LOGE(TAG, "Failed to allocate memory");
-            ret = -1;
-            goto cleanup;
+            return -1;
         }
     }
 
@@ -270,9 +275,9 @@ static int esp_aes_process_dma_ext_ram(esp_aes_context *ctx, const unsigned char
         output_buf = heap_caps_malloc(chunk_len, MALLOC_CAP_DMA);
 
         if (output_buf == NULL) {
+            mbedtls_platform_zeroize(output, len);
             ESP_LOGE(TAG, "Failed to allocate memory");
-            ret = -1;
-            goto cleanup;
+            return -1;
         }
     } else {
         output_buf = output;
@@ -316,7 +321,13 @@ cleanup:
     return ret;
 }
 
-/* Encrypt/decrypt the input using DMA */
+/* Encrypt/decrypt the input using DMA
+ * The function esp_aes_process_dma zeroises the output buffer in the case of following conditions:
+ * 1. If key is not written in the hardware
+ * 2. Memory allocation failures
+ * 3. If AES interrupt is enabled and ISR initialisation fails
+ * 4. Failure in any of the AES operations
+ */
 static int esp_aes_process_dma(esp_aes_context *ctx, const unsigned char *input, unsigned char *output, size_t len, uint8_t *stream_out)
 {
     lldesc_t *in_desc_head = NULL, *out_desc_head = NULL;
@@ -340,7 +351,7 @@ static int esp_aes_process_dma(esp_aes_context *ctx, const unsigned char *input,
        key write to hardware. Treat this as a fatal error and zero the output block.
     */
     if (ctx->key_in_hardware != ctx->key_bytes) {
-        bzero(output, len);
+        mbedtls_platform_zeroize(output, len);
         return MBEDTLS_ERR_AES_INVALID_INPUT_LENGTH;
     }
 
@@ -377,9 +388,9 @@ static int esp_aes_process_dma(esp_aes_context *ctx, const unsigned char *input,
         /* Allocate both in and out descriptors to save a malloc/free per function call */
         block_desc = heap_caps_calloc(lldesc_num * 2, sizeof(lldesc_t), MALLOC_CAP_DMA);
         if (block_desc == NULL) {
+            mbedtls_platform_zeroize(output, len);
             ESP_LOGE(TAG, "Failed to allocate memory");
-            ret = -1;
-            goto cleanup;
+            return -1;
         }
 
         block_in_desc = block_desc;
@@ -425,6 +436,7 @@ static int esp_aes_process_dma(esp_aes_context *ctx, const unsigned char *input,
     if (len > AES_DMA_INTR_TRIG_LEN) {
         use_intr = true;
         if (esp_aes_isr_initialise() == ESP_FAIL) {
+            ESP_LOGE(TAG, "ESP-AES ISR initialisation failed");
             ret = -1;
             goto cleanup;
         }
@@ -463,6 +475,9 @@ static int esp_aes_process_dma(esp_aes_context *ctx, const unsigned char *input,
     }
 
 cleanup:
+    if (ret != 0) {
+        mbedtls_platform_zeroize(output, len);
+    }
     free(block_desc);
     return ret;
 }
@@ -470,7 +485,13 @@ cleanup:
 
 #if SOC_AES_SUPPORT_GCM
 
-/* Encrypt/decrypt with AES-GCM the input using DMA */
+/* Encrypt/decrypt with AES-GCM the input using DMA
+ * The function esp_aes_process_dma_gcm zeroises the output buffer in the case of following conditions:
+ * 1. If key is not written in the hardware
+ * 2. Memory allocation failures
+ * 3. If AES interrupt is enabled and ISR initialisation fails
+ * 4. Failure in any of the AES operations
+ */
 int esp_aes_process_dma_gcm(esp_aes_context *ctx, const unsigned char *input, unsigned char *output, size_t len, lldesc_t *aad_desc, size_t aad_len)
 {
     lldesc_t *in_desc_head = NULL, *out_desc_head = NULL, *len_desc = NULL;
@@ -494,7 +515,7 @@ int esp_aes_process_dma_gcm(esp_aes_context *ctx, const unsigned char *input, un
        key write to hardware. Treat this as a fatal error and zero the output block.
     */
     if (ctx->key_in_hardware != ctx->key_bytes) {
-        bzero(output, len);
+        mbedtls_platform_zeroize(output, len);
         return MBEDTLS_ERR_AES_INVALID_INPUT_LENGTH;
     }
 
@@ -504,9 +525,9 @@ int esp_aes_process_dma_gcm(esp_aes_context *ctx, const unsigned char *input, un
     /* Allocate both in and out descriptors to save a malloc/free per function call, add 1 for length descriptor */
     block_desc = heap_caps_calloc( (lldesc_num * 2) + 1, sizeof(lldesc_t), MALLOC_CAP_DMA);
     if (block_desc == NULL) {
+        mbedtls_platform_zeroize(output, len);
         ESP_LOGE(TAG, "Failed to allocate memory");
-        ret = -1;
-        goto cleanup;
+        return -1;
     }
 
     block_in_desc = block_desc;
@@ -553,6 +574,7 @@ int esp_aes_process_dma_gcm(esp_aes_context *ctx, const unsigned char *input, un
     if (len > AES_DMA_INTR_TRIG_LEN) {
         use_intr = true;
         if (esp_aes_isr_initialise() == ESP_FAIL) {
+            ESP_LOGE(TAG, "ESP-AES ISR initialisation failed");
             ret = -1;
             goto cleanup;
         }
@@ -584,6 +606,9 @@ int esp_aes_process_dma_gcm(esp_aes_context *ctx, const unsigned char *input, un
     }
 
 cleanup:
+    if (ret != 0) {
+        mbedtls_platform_zeroize(output, len);
+    }
     free(block_desc);
     return ret;
 }
@@ -797,12 +822,12 @@ int esp_aes_crypt_cfb8(esp_aes_context *ctx,
         aes_hal_mode_init(ESP_AES_BLOCK_MODE_CFB8);
         aes_hal_set_iv(iv);
         r = esp_aes_process_dma(ctx, input, output, block_bytes, NULL);
-        aes_hal_read_iv(iv);
-
         if (r != 0) {
             esp_aes_release_hardware();
             return r;
         }
+
+        aes_hal_read_iv(iv);
 
         length -= block_bytes;
         input += block_bytes;
