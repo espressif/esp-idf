a67e8c1972529fe7b9b3e9e2e4c52c69f7157140
Darian Leung <darian@espressif.com>
2023-08-28 20:21:41 +0800

    refactor(freertos): Rename freertos_v8_compat.c to freertos_compatibility.c
    Rename API compatibility file to be more general, and add a proper description.

--- components/freertos/esp_additions/freertos_compatibility.c
+++ components/freertos/esp_additions/freertos_compatibility.c
@@ -4,11 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/*
+ * FreeRTOS has changed some functions in to macros (and vice-versa) over multiple
+ * releases. This is not a breaking API change for source code, but may cause issues
+ * for pre-compiled libraries that call these removed APIs.
+ *
+ * This file maintains these legacy APIs until the next ESP-IDF major release.
+ *
+ * Todo: Clean up for ESP-IDF v6.0 (IDF-8144)
+*/
+
 #include "FreeRTOS.h"
 #include "queue.h"
 #include "semphr.h"
 
-/* This API is kept for backward ABI compatibility with prebuilt libraries against FreeRTOS v8/v9 in ESP-IDF */
 BaseType_t xQueueGenericReceive( QueueHandle_t xQueue, void * const pvBuffer, TickType_t xTicksToWait, const BaseType_t xPeek )
 {
 	if ( xPeek == pdTRUE )
