5a1d9da84f01088bde89608c98dcc018eb81c6dc
Eduardo Lacerda Campos <educampos28@yahoo.com.br>
2023-09-01 18:33:08 -0400

    fix(uart): Fix uart_ll_set_baudrate div-by-zero crash due to uint32_t overflow
    Merges https://github.com/espressif/esp-idf/pull/12179

--- components/hal/esp32c2/include/hal/uart_ll.h
+++ components/hal/esp32c2/include/hal/uart_ll.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2020-2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2020-2023 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,6 +9,8 @@
 
 
 #pragma once
+
+#include <stdlib.h>
 #include "hal/uart_types.h"
 #include "soc/uart_periph.h"
 
@@ -154,13 +156,15 @@ static inline void uart_ll_set_baudrate(uart_dev_t *hw, uint32_t baud, uint32_t 
 {
 #define DIV_UP(a, b)    (((a) + (b) - 1) / (b))
     const uint32_t max_div = BIT(12) - 1;   // UART divider integer part only has 12 bits
-    int sclk_div = DIV_UP(sclk_freq, max_div * baud);
+    uint32_t sclk_div = DIV_UP(sclk_freq, (uint64_t)max_div * baud);
+
+    if (sclk_div == 0) abort();
 
     uint32_t clk_div = ((sclk_freq) << 4) / (baud * sclk_div);
     // The baud rate configuration register is divided into
     // an integer part and a fractional part.
     hw->clk_div.div_int = clk_div >> 4;
-    hw->clk_div.div_frag = clk_div &  0xf;
+    hw->clk_div.div_frag = clk_div & 0xf;
     hw->clk_conf.sclk_div_num = sclk_div - 1;
 #undef DIV_UP
 }
