1963f75fa7f60021d253cbc807e8aeaab73fcde8
liqigan <liqigan@espressif.com>
2023-09-04 16:30:47 +0800

    change(bt/bluedroid): Optimize HIDD sending interrupt report behavior
    HID device is allowed to trigger reconnection by sending an interrupt
    report. When reconnection is in progress, no more interrupt report can
    be sent until the procedure is accomplished.

--- components/bt/host/bluedroid/btc/profile/std/include/btc_hd.h
+++ components/bt/host/bluedroid/btc/profile/std/include/btc_hd.h
@@ -39,7 +39,15 @@ typedef enum {
     BTC_HD_UNPLUG_EVT,
 } BTC_HD_EVT;
 
-typedef enum { BTC_HD_DISABLED = 0, BTC_HD_ENABLED, BTC_HD_CONNECTED, BTC_HD_DISABLING } BTC_HD_STATUS;
+typedef enum {
+    BTC_HD_DISABLED = 0,
+    BTC_HD_ENABLED,
+    BTC_HD_DISABLING,
+    BTC_HD_CONNECTING,
+    BTC_HD_CONNECTED,
+    BTC_HD_DISCONNECTING,
+    BTC_HD_DISCONNECTED,
+} BTC_HD_STATUS;
 
 /* BTIF-HD control block */
 typedef struct {
