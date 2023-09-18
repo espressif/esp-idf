6a7caa7b8ea79b0b40389fdc75f25c67e897eed8
harshal.patil <harshal.patil@espressif.com>
2023-09-18 16:30:42 +0530

    feat(esp_hw_support): Added locking mechanism for the ECDSA and ECC peripheral

--- components/esp_hw_support/port/esp32p4/esp_crypto_lock.c
+++ components/esp_hw_support/port/esp32p4/esp_crypto_lock.c
@@ -12,8 +12,10 @@
 SHA: peripheral independent, but DMA is shared with AES
 AES: peripheral independent, but DMA is shared with SHA
 MPI/RSA: independent
+ECC: independent
 HMAC: needs SHA
 DS: needs HMAC (which needs SHA), AES and MPI
+ECDSA: needs ECC and MPI
 */
 
 /* Lock for DS peripheral */
@@ -27,6 +29,12 @@ static _lock_t s_crypto_mpi_lock;
 
 /* Single lock for SHA and AES, sharing a reserved GDMA channel */
 static _lock_t s_crypto_sha_aes_lock;
+
+/* Lock for ECC peripheral */
+static _lock_t s_crypto_ecc_lock;
+
+/* Lock for ECDSA peripheral */
+static _lock_t s_crypto_ecdsa_lock;
 
 void esp_crypto_hmac_lock_acquire(void)
 {
@@ -72,4 +80,28 @@ void esp_crypto_mpi_lock_acquire(void)
 void esp_crypto_mpi_lock_release(void)
 {
     _lock_release(&s_crypto_mpi_lock);
+}
+
+void esp_crypto_ecc_lock_acquire(void)
+{
+    _lock_acquire(&s_crypto_ecc_lock);
+}
+
+void esp_crypto_ecc_lock_release(void)
+{
+    _lock_release(&s_crypto_ecc_lock);
+}
+
+void esp_crypto_ecdsa_lock_acquire(void)
+{
+    _lock_acquire(&s_crypto_ecdsa_lock);
+    esp_crypto_ecc_lock_acquire();
+    esp_crypto_mpi_lock_acquire();
+}
+
+void esp_crypto_ecdsa_lock_release(void)
+{
+    esp_crypto_mpi_lock_release();
+    esp_crypto_ecc_lock_release();
+    _lock_release(&s_crypto_ecdsa_lock);
 }
