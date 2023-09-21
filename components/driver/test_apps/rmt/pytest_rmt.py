fcfb16050f219974d364e0f8d30f6120a986711f
morris <maoshengrong@espressif.com>
2023-09-21 11:41:06 +0800

    fix(intr): always allocate memory from internal ram
    Closes https://github.com/espressif/esp-idf/issues/12271

--- components/driver/test_apps/rmt/pytest_rmt.py
+++ components/driver/test_apps/rmt/pytest_rmt.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2021-2023 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 
 import pytest
@@ -7,7 +7,6 @@ from pytest_embedded import Dut
 
 @pytest.mark.esp32
 @pytest.mark.esp32s2
-@pytest.mark.esp32s3
 @pytest.mark.esp32c3
 @pytest.mark.generic
 @pytest.mark.parametrize(
@@ -19,6 +18,22 @@ from pytest_embedded import Dut
     indirect=True,
 )
 def test_rmt(dut: Dut) -> None:
+    dut.expect('Press ENTER to see the list of tests')
+    dut.write('*')
+    dut.expect_unity_test_output()
+
+
+@pytest.mark.esp32s3
+@pytest.mark.octal_psram
+@pytest.mark.parametrize(
+    'config',
+    [
+        'iram_safe',
+        'release',
+    ],
+    indirect=True,
+)
+def test_rmt_psram(dut: Dut) -> None:
     dut.expect('Press ENTER to see the list of tests')
     dut.write('*')
     dut.expect_unity_test_output()
