1fbee174af088d9e8c2b3fe7fb8e35ec6ab11c85
Ivan Grokhotkov <ivan@espressif.com>
2023-09-22 20:00:53 +0200

    fix(bt): fixed -Write-strings warning in btm_ble_5_gap.c

--- components/bt/host/bluedroid/stack/btm/btm_ble_5_gap.c
+++ components/bt/host/bluedroid/stack/btm/btm_ble_5_gap.c
@@ -35,7 +35,7 @@ typedef struct {
 tBTM_EXT_ADV_RECORD adv_record[MAX_BLE_ADV_INSTANCE] = {0};
 extern void btm_ble_inter_set(bool extble_inter);
 
-static char *btm_ble_hci_status_to_str(tHCI_STATUS status)
+static const char *btm_ble_hci_status_to_str(tHCI_STATUS status)
 {
     switch(status) {
     case HCI_SUCCESS:
