c9f7186357df1c2b6b039e0d6932251ffbae449e
luomanruo <luomanruo@espressif.com>
2023-10-10 11:57:10 +0800

    ble: update tx power level index of ESP32-H2 and ESP32-C2

--- components/bt/include/esp32h2/include/esp_bt.h
+++ components/bt/include/esp32h2/include/esp_bt.h
@@ -88,7 +88,10 @@ typedef enum {
     ESP_PWR_LVL_P9  = 11,             /*!< Corresponding to  +9dbm */
     ESP_PWR_LVL_P12 = 12,             /*!< Corresponding to  +12dbm */
     ESP_PWR_LVL_P15 = 13,             /*!< Corresponding to  +15dbm */
+    ESP_PWR_LVL_P16 = 14,             /*!< Corresponding to  +18dbm, this enum variable has been deprecated */
+    ESP_PWR_LVL_P17 = 14,             /*!< Corresponding to  +18dbm, this enum variable has been deprecated */
     ESP_PWR_LVL_P18 = 14,             /*!< Corresponding to  +18dbm */
+    ESP_PWR_LVL_P19 = 15,             /*!< Corresponding to  +20dbm, this enum variable has been deprecated */
     ESP_PWR_LVL_P20 = 15,             /*!< Corresponding to  +20dbm */
     ESP_PWR_LVL_INVALID = 0xFF,       /*!< Indicates an invalid value */
 } esp_power_level_t;
