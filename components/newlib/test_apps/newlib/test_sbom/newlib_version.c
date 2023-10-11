8705da6ff38a4968dfed92220efcedb4283cdb09
Alexey Lapshin <alexey.lapshin@espressif.com>
2023-10-11 13:25:59 +0400

    feat(newlib): Add sbom manifest file

---
+++ components/newlib/test_apps/newlib/test_sbom/newlib_version.c
@@ -0,0 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+#include <_newlib_version.h>
+__NEWLIB__ __NEWLIB_MINOR__ __NEWLIB_PATCHLEVEL__
