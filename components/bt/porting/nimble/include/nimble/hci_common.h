d31cb0d0cd3f2a5f6f9180fdce8e0c1ba5535e9c
Rahul Tank <rahul.tank@espressif.com>
2023-10-18 18:18:54 +0530

    fix(nimble): Add support for VSC to clear legacy adv memory
     Legacy adv does not have an API for clear adv memory in controller.
     Added VSC for same.

--- components/bt/porting/nimble/include/nimble/hci_common.h
+++ components/bt/porting/nimble/include/nimble/hci_common.h
@@ -1141,6 +1141,7 @@ struct ble_hci_vs_duplicate_exception_list_cp {
     uint8_t device_info[6];
 } __attribute__((packed));
 
+#define BLE_HCI_OCF_VS_LEGACY_ADV_CLEAR                 (MYNEWT_VAL(BLE_HCI_VS_OCF_OFFSET) + (0x010C))
 
 /* Command Specific Definitions */
 /* --- Set controller to host flow control (OGF 0x03, OCF 0x0031) --- */
