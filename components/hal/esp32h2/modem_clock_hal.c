1537cbf9c7089940a434d02fa4d73b62681aeaef
wuzhenghui <wuzhenghui@espressif.com>
2023-11-09 17:21:33 +0800

    fix(esp_hw_support): move fe_32m to fe/adc common clock

--- components/hal/esp32h2/modem_clock_hal.c
+++ components/hal/esp32h2/modem_clock_hal.c
@@ -22,6 +22,7 @@ typedef enum {
 void IRAM_ATTR modem_clock_hal_enable_modem_adc_common_fe_clock(modem_clock_hal_context_t *hal, bool enable)
 {
     modem_syscon_ll_enable_fe_apb_clock(hal->syscon_dev, enable);
+    modem_syscon_ll_enable_fe_32m_clock(hal->syscon_dev, enable);
 }
 
 void IRAM_ATTR modem_clock_hal_enable_modem_private_fe_clock(modem_clock_hal_context_t *hal, bool enable)
@@ -29,7 +30,6 @@ void IRAM_ATTR modem_clock_hal_enable_modem_private_fe_clock(modem_clock_hal_con
     modem_lpcon_ll_enable_fe_mem_clock(hal->lpcon_dev, enable);
     modem_syscon_ll_enable_fe_sdm_clock(hal->syscon_dev, enable);
     modem_syscon_ll_enable_fe_adc_clock(hal->syscon_dev, enable);
-    modem_syscon_ll_enable_fe_32m_clock(hal->syscon_dev, enable);
     modem_syscon_ll_enable_fe_16m_clock(hal->syscon_dev, enable);
 }
 
