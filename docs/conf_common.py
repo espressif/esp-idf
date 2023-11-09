bd1825f9b13fdfe0c40faef689f262df417394c4
Linda <zhangxiaoyan@espressif.com>
2023-11-09 16:50:39 +0800

    docs: remove WiFi related documentation for ESP32-H2

--- docs/conf_common.py
+++ docs/conf_common.py
@@ -66,7 +66,9 @@ WIFI_DOCS = ['api-guides/wifi.rst',
              'api-reference/network/esp_now.rst',
              'api-reference/network/esp_smartconfig.rst',
              'api-reference/network/esp_wifi.rst',
-             'api-reference/network/esp_dpp.rst']
+             'api-reference/network/esp_dpp.rst',
+             'api-reference/provisioning/provisioning.rst',
+             'api-reference/provisioning/wifi_provisioning.rst']
 
 NAN_DOCS = ['api-reference/network/esp_nan.rst']
 
