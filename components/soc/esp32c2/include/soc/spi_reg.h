815ae4b4cf29b29e1ea7ca1cd024f80894135918
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-11-11 12:38:44 +0100

    Correct REG_SPI_BASE(i) for C2

--- components/soc/esp32c2/include/soc/spi_reg.h
+++ components/soc/esp32c2/include/soc/spi_reg.h
@@ -11,7 +11,7 @@
 extern "C" {
 #endif
 #include "soc/soc.h"
-#define REG_SPI_BASE(i)     (DR_REG_SPI2_BASE + (i - 2) * 0x1000)
+#define REG_SPI_BASE(i)     ((i==2) ? (DR_REG_SPI2_BASE : (DR_REG_SPI0_BASE - (i * 0x1000))))
 
 #define SPI_CMD_REG(i)          (REG_SPI_BASE(i) + 0x0)
 /* SPI_USR : R/W/SC ;bitpos:[24] ;default: 1'b0 ; */
