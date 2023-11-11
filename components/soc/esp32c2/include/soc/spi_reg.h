75560ffe7f8c5c3dc276aed61f01b69eda2ba068
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-11-11 19:13:46 +0100

    Update spi_reg.h

--- components/soc/esp32c2/include/soc/spi_reg.h
+++ components/soc/esp32c2/include/soc/spi_reg.h
@@ -11,8 +11,7 @@
 extern "C" {
 #endif
 #include "soc/soc.h"
-#define REG_SPI_BASE(i)     (DR_REG_SPI1_BASE + (((i)>1) ? (((i)* 0x1000) + 0x20000) : (((~(i)) & 1)* 0x1000 )))
-
+#define REG_SPI_BASE(i)     (DR_REG_SPI2_BASE + (i - 2) * 0x1000)
 #define SPI_CMD_REG(i)          (REG_SPI_BASE(i) + 0x0)
 /* SPI_USR : R/W/SC ;bitpos:[24] ;default: 1'b0 ; */
 /*description: User define command enable.  An operation will be triggered when the bit is set.
