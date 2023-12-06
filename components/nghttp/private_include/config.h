e6330d9cdc11c541c1d412a3c8f185ac0bc8eb9c
Harshit Malpani <harshit.malpani@espressif.com>
2023-12-06 11:52:01 +0530

    feat(nghttp2): Update nghttp2 lib to v1.58.0

--- components/nghttp/private_include/config.h
+++ components/nghttp/private_include/config.h
@@ -5,25 +5,24 @@
 
 #define SIZEOF_INT_P 2
 
-//#define DEBUGBUILD
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 
+/* Define to 1 if you have the `clock_gettime' function. */
+#define HAVE_CLOCK_GETTIME 1
+
+/* Define to 1 if you have the <time.h> header file. */
+#define HAVE_TIME_H 1
+
 #if (!defined(nghttp_unlikely))
-#define nghttp_unlikely(Expression)	!!(Expression)
+#define nghttp_unlikely(Expression) !!(Expression)
 #endif
 
 #define nghttp_ASSERT(Expression) do{if (!(Expression)) printf("%d\n", __LINE__);}while(0)
 
 #define CU_ASSERT(a) nghttp_ASSERT(a)
 #define CU_ASSERT_FATAL(a) nghttp_ASSERT(a)
-
-#if 1
-#define NGHTTP2_DEBUG_INFO() printf("%s %d\n", __FILE__, __LINE__)
-#else
-#define NGHTTP2_DEBUG_INFO()
-#endif
 
 #define NGHTTP_PLATFORM_HTONS(_n)  ((uint16_t)((((_n) & 0xff) << 8) | (((_n) >> 8) & 0xff)))
 #define NGHTTP_PLATFORM_HTONL(_n)  ((uint32_t)( (((_n) & 0xff) << 24) | (((_n) & 0xff00) << 8) | (((_n) >> 8)  & 0xff00) | (((_n) >> 24) & 0xff) ))
@@ -33,4 +32,4 @@
 #define htonl(x) NGHTTP_PLATFORM_HTONL(x)
 #define ntohl(x) NGHTTP_PLATFORM_HTONL(x)
 
-#endif
+#endif // __HAVE_CONFIG_H_
