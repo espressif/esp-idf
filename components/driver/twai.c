espressif/esp-idf

commit efef84012429080f010d752a7a365c96e5228372

fix(twai): twai example crash using usb-serial-jtag pins
Calling gpio_hal_iomux_func_sel() via gpio_config() may
disconnects/disables any influence from USJ when using USJ pins

components/driver/twai.c
@@ -277,19 +277,30 @@ TWAI_ISR_ATTR static void twai_intr_handler_main(void *arg)
 }
 
 /* -------------------------- Helper functions  ----------------------------- */
-
-static void twai_configure_gpio(gpio_num_t tx, gpio_num_t rx, gpio_num_t clkout, gpio_num_t bus_status)
+static esp_err_t twai_configure_gpio(gpio_num_t tx, gpio_num_t rx, gpio_num_t clkout, gpio_num_t bus_status)
 {
-    //Set TX pin
-    gpio_set_pull_mode(tx, GPIO_FLOATING);
-    esp_rom_gpio_connect_out_signal(tx, TWAI_TX_IDX, false, false);
-    esp_rom_gpio_pad_select_gpio(tx);
-
+    esp_err_t ret = ESP_FAIL;
+    assert(tx >= 0 && rx >= 0);
+    // if TX and RX set to the same GPIO, which means we want to create a loop-back in the GPIO matrix
+    bool io_loop_back = (tx == rx);
+    gpio_config_t gpio_conf = {
+        .intr_type = GPIO_INTR_DISABLE,
+        .pull_down_en = false,
+        .pull_up_en = false,
+    };
     //Set RX pin
-    gpio_set_pull_mode(rx, GPIO_FLOATING);
+    gpio_conf.mode = GPIO_MODE_INPUT | (io_loop_back ? GPIO_MODE_OUTPUT : 0);
+    gpio_conf.pin_bit_mask = 1ULL << rx;
+    ret = gpio_config(&gpio_conf);
+    TWAI_CHECK(ESP_OK == ret, ret);
     esp_rom_gpio_connect_in_signal(rx, TWAI_RX_IDX, false);
-    esp_rom_gpio_pad_select_gpio(rx);
-    gpio_set_direction(rx, GPIO_MODE_INPUT);
+
+    //Set TX pin
+    gpio_conf.mode = GPIO_MODE_OUTPUT | (io_loop_back ? GPIO_MODE_INPUT : 0);
+    gpio_conf.pin_bit_mask = 1ULL << tx;
+    ret = gpio_config(&gpio_conf);
+    TWAI_CHECK(ESP_OK == ret, ret);
+    esp_rom_gpio_connect_out_signal(tx, TWAI_TX_IDX, false, false);
 
     //Configure output clock pin (Optional)
     if (clkout >= 0 && clkout < GPIO_NUM_MAX) {
@@ -304,6 +315,7 @@ static void twai_configure_gpio(gpio_num_t tx, gpio_num_t rx, gpio_num_t clkout,
         esp_rom_gpio_connect_out_signal(bus_status, TWAI_BUS_OFF_ON_IDX, false, false);
         esp_rom_gpio_pad_select_gpio(bus_status);
     }
+    return ESP_OK;
 }
 
 static void twai_free_driver_obj(twai_obj_t *p_obj)
@@ -448,6 +460,13 @@ esp_err_t twai_driver_install(const twai_general_config_t *g_config, const twai_
     p_twai_obj_dummy->mode = g_config->mode;
     p_twai_obj_dummy->alerts_enabled = g_config->alerts_enabled;
 
+    //Assign GPIO
+    ret = twai_configure_gpio(g_config->tx_io, g_config->rx_io, g_config->clkout_io, g_config->bus_off_io);
+    if (ESP_OK != ret) {
+        ESP_LOGE(TWAI_TAG, "init gpio for twai failed");
+        goto err;
+    }
+
     TWAI_ENTER_CRITICAL();
     //Assign the TWAI object
     if (p_twai_obj == NULL) {
@@ -468,9 +487,6 @@ esp_err_t twai_driver_install(const twai_general_config_t *g_config, const twai_
     (void)init;
     twai_hal_configure(&twai_context, t_config, f_config, DRIVER_DEFAULT_INTERRUPTS, g_config->clkout_divider);
     TWAI_EXIT_CRITICAL();
-
-    //Assign GPIO
-    twai_configure_gpio(g_config->tx_io, g_config->rx_io, g_config->clkout_io, g_config->bus_off_io);
 
 #if CONFIG_PM_ENABLE
     //Acquire PM lock

commit 62efb02ecfa0449b6667c4707abd56fca1cd3064

fix(twai): fix build error TWAI_TAG used but not defined
bug introduced from efef8401
closes https://github.com/espressif/esp-idf/issues/12760

components/driver/twai.c
@@ -27,6 +27,7 @@
 
 /* ---------------------------- Definitions --------------------------------- */
 //Internal Macros
+#define TWAI_TAG "TWAI"
 #define TWAI_CHECK(cond, ret_val) ({                                        \
             if (!(cond)) {                                                  \
                 return (ret_val);                                           \
@@ -40,11 +41,11 @@
 })
 #define TWAI_SET_FLAG(var, mask)    ((var) |= (mask))
 #define TWAI_RESET_FLAG(var, mask)  ((var) &= ~(mask))
+
 #ifdef CONFIG_TWAI_ISR_IN_IRAM
 #define TWAI_ISR_ATTR       IRAM_ATTR
 #define TWAI_MALLOC_CAPS    (MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT)
 #else
-#define TWAI_TAG "TWAI"
 #define TWAI_ISR_ATTR
 #define TWAI_MALLOC_CAPS    MALLOC_CAP_DEFAULT
 #endif  //CONFIG_TWAI_ISR_IN_IRAM
@@ -463,7 +464,6 @@ esp_err_t twai_driver_install(const twai_general_config_t *g_config, const twai_
     //Assign GPIO
     ret = twai_configure_gpio(g_config->tx_io, g_config->rx_io, g_config->clkout_io, g_config->bus_off_io);
     if (ESP_OK != ret) {
-        ESP_LOGE(TWAI_TAG, "init gpio for twai failed");
         goto err;
     }
 