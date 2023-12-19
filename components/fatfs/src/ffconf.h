1447420a9142345c4dcb263ba8005e3438f182ba
Tomáš Rohlínek <tomas.rohlinek@espressif.com>
2023-12-19 10:41:01 +0100

    feat(storage/fatfs): add Kconfig option to enable dynamic window buffers

--- components/fatfs/src/ffconf.h
+++ components/fatfs/src/ffconf.h
@@ -311,7 +311,7 @@
 /  The FF_FS_TIMEOUT defines timeout period in unit of O/S time tick.
 */
 
-#define FF_USE_DYN_BUFFER 0
+#define FF_USE_DYN_BUFFER CONFIG_FATFS_USE_DYN_BUFFERS
 /* The option FF_USE_DYN_BUFFER controls source of size used for buffers in the FS and FIL objects.
 /
 /   0: Disable dynamic buffer size and use static size buffers defined by FF_MAX_SS.
