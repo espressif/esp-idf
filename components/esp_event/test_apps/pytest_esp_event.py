8fe0f99368e2a2590b5f69bf4cc32ce380884a41
Marius Vikhammer <marius.vikhammer@espressif.com>
2023-12-20 12:01:44 +0800

    change(system): enabled pthread, ringbuf and event tests

--- components/esp_event/test_apps/pytest_esp_event.py
+++ components/esp_event/test_apps/pytest_esp_event.py
@@ -14,24 +14,10 @@ def test_esp_event(dut: Dut) -> None:
 
 
 @pytest.mark.esp32
-@pytest.mark.host_test
-@pytest.mark.qemu
-@pytest.mark.parametrize('qemu_extra_args', [
-    '-global driver=timer.esp32.timg,property=wdt_disable,value=true',
-], indirect=True)  # need to disable wdt since it is not synchronized with target cpu clock on QEMU for ESP32
-def test_esp_event_qemu_esp32(dut: Dut) -> None:
-    for case in dut.test_menu:
-        if 'qemu-ignore' not in case.groups and not case.is_ignored and case.type == 'normal':
-            dut._run_normal_case(case)
-
-
 @pytest.mark.esp32c3
 @pytest.mark.host_test
 @pytest.mark.qemu
-@pytest.mark.parametrize('qemu_extra_args', [
-    '-icount 3',
-], indirect=True)  # need to add -icount 3 to make WDT accurate on QEMU for ESP32-C3
-def test_esp_event_qemu_esp32c3(dut: Dut) -> None:
+def test_esp_event_qemu(dut: Dut) -> None:
     for case in dut.test_menu:
         if 'qemu-ignore' not in case.groups and not case.is_ignored and case.type == 'normal':
             dut._run_normal_case(case)
