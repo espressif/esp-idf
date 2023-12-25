8cae0ed7556bd694b98a7aade0739da566646432
zwx <zhangwenxu@espressif.com>
2023-12-25 16:01:12 +0800

    fix(pm): fix extra retention function to iram
    * Also remove unused function on H2

--- components/hal/esp32h2/pau_hal.c
+++ components/hal/esp32h2/pau_hal.c
@@ -40,26 +40,6 @@ void IRAM_ATTR pau_hal_stop_regdma_system_link(pau_hal_context_t *hal)
     pau_ll_clear_regdma_backup_done_intr_state(hal->dev);
 }
 
-void pau_hal_start_regdma_extra_link(pau_hal_context_t *hal, bool backup_or_restore)
-{
-    pau_ll_clear_regdma_backup_done_intr_state(hal->dev);
-    /* The link 3 of REGDMA is reserved, we use it as an extra linked list to
-     * provide backup and restore services for BLE, IEEE802.15.4 and possibly
-     * other modules */
-    pau_ll_select_regdma_entry_link(hal->dev, 3);
-    pau_ll_set_regdma_entry_link_backup_direction(hal->dev, backup_or_restore);
-    pau_ll_set_regdma_entry_link_backup_start_enable(hal->dev);
-
-    while (!(pau_ll_get_regdma_intr_raw_signal(hal->dev) & PAU_DONE_INT_RAW));
-}
-
-void pau_hal_stop_regdma_extra_link(pau_hal_context_t *hal)
-{
-    pau_ll_set_regdma_entry_link_backup_start_disable(hal->dev);
-    pau_ll_select_regdma_entry_link(hal->dev, 0); /* restore link select to default */
-    pau_ll_clear_regdma_backup_done_intr_state(hal->dev);
-}
-
 void IRAM_ATTR pau_hal_regdma_clock_configure(pau_hal_context_t *hal, bool enable)
 {
     HAL_FORCE_MODIFY_U32_REG_FIELD(PCR.regdma_conf, regdma_rst_en, !enable);
