8ac4d9ab99fe76e978803006631faa71cd2c9956
harshal.patil <harshal.patil@espressif.com>
2023-12-27 12:54:05 +0530

    fix(mbedtls/gcm): Avoid using GCM hardware when config MBEDTLS_HARDWARE_GCM is disabled

--- components/mbedtls/port/aes/dma/esp_aes.c
+++ components/mbedtls/port/aes/dma/esp_aes.c
@@ -53,10 +53,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 
-#if SOC_AES_SUPPORT_GCM
-#include "aes/esp_aes_gcm.h"
-#endif
-
 #if SOC_AES_GDMA
 #define AES_LOCK() esp_crypto_sha_aes_lock_acquire()
 #define AES_RELEASE() esp_crypto_sha_aes_lock_release()
@@ -488,7 +484,7 @@ cleanup:
 }
 
 
-#if SOC_AES_SUPPORT_GCM
+#if CONFIG_MBEDTLS_HARDWARE_GCM
 
 /* Encrypt/decrypt with AES-GCM the input using DMA
  * The function esp_aes_process_dma_gcm zeroises the output buffer in the case of following conditions:
@@ -623,7 +619,7 @@ cleanup:
     return ret;
 }
 
-#endif //SOC_AES_SUPPORT_GCM
+#endif //CONFIG_MBEDTLS_HARDWARE_GCM
 
 static int esp_aes_validate_input(esp_aes_context *ctx, const unsigned char *input,
                                   unsigned char *output )
