espressif/esp-mqtt

docs(esp32p4): update misc docs for esp32p4

docs/conf_common.py
@@ -68,7 +68,8 @@ WIFI_DOCS = ['api-guides/wifi.rst',
              'api-reference/network/esp_wifi.rst',
              'api-reference/network/esp_dpp.rst',
              'api-reference/provisioning/provisioning.rst',
-             'api-reference/provisioning/wifi_provisioning.rst']
+             'api-reference/provisioning/wifi_provisioning.rst',
+             'migration-guides/release-5.x/5.2/wifi.rst']
 
 NAN_DOCS = ['api-reference/network/esp_nan.rst']
 

espressif/esp-idf

docs/conf_common.py
@@ -66,7 +66,8 @@ WIFI_DOCS = ['api-guides/wifi.rst',
              'api-reference/network/esp_now.rst',
              'api-reference/network/esp_smartconfig.rst',
              'api-reference/network/esp_wifi.rst',
-             'api-reference/network/esp_dpp.rst']
+             'api-reference/network/esp_dpp.rst',
+             'migration-guides/release-5.x/5.2/wifi.rst']
 
 NAN_DOCS = ['api-reference/network/esp_nan.rst']
 