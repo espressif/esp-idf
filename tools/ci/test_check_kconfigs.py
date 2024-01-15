474d150477595d5e8f949172c5206272654ac9b0
Jakub Kocka <jakub.kocka@espressif.com>
2024-01-15 09:15:58 +0100

    fix(kconfcheck): Fixed false-positive indent errors and extended limits

--- tools/ci/test_check_kconfigs.py
+++ tools/ci/test_check_kconfigs.py
@@ -183,7 +183,6 @@ class TestIndent(TestIndentAndNameChecker):
         self.expt_success('    # comment')
         self.expt_success('    help')
         self.expt_success('        text')
-        self.expect_error('# comment', expect='        # comment')
         self.expt_success('        # second not realcomment"')
 
 
