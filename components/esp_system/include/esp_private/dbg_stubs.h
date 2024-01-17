f50d83413e248b636d89a3936e469b7c02d734f5
Darian Leung <darian@espressif.com>
2024-01-17 14:16:06 +0800

    refactor(tools): Tidy up core component files copyright ignore
    Some files that should have their copyrights checked are still placed on the
    copyright ignore list.
    
    - These entries have been tidied up
    - Copyrights of those files have been updated.

--- components/esp_system/include/esp_private/dbg_stubs.h
+++ components/esp_system/include/esp_private/dbg_stubs.h
@@ -1,16 +1,9 @@
-// Copyright 2017 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+/*
+ * SPDX-FileCopyrightText: 2017-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 #ifndef ESP_DBG_STUBS_H_
 #define ESP_DBG_STUBS_H_
 
@@ -73,4 +66,4 @@ esp_err_t esp_dbg_stub_entry_get(esp_dbg_stub_id_t id, uint32_t *entry);
 }
 #endif
 
-#endif //ESP_DBG_STUBS_H_
+#endif // ESP_DBG_STUBS_H_
--- components/heap/test_multi_heap_host/test_multi_heap.cpp
+++ components/heap/test_multi_heap_host/test_multi_heap.cpp
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include "catch.hpp"
 #include "multi_heap.h"
 
