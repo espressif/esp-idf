9ab0d95f65f982c902e4fbff0851c25904f58fc4
Marius Vikhammer <marius.vikhammer@espressif.com>
2024-01-17 16:46:32 +0800

    test(newlib): enable and fix newlib tests on P4

--- components/esp_rom/esp32p4/esp_rom_caps.h
+++ components/esp_rom/esp32p4/esp_rom_caps.h
@@ -19,3 +19,4 @@
 #define ESP_ROM_WDT_INIT_PATCH              (1) // ROM version does not configure the clock
 #define ESP_ROM_HAS_LP_ROM                  (1) // ROM also has a LP ROM placed in LP memory
 #define ESP_ROM_WITHOUT_REGI2C              (1) // ROM has no regi2c APIs
+#define ESP_ROM_HAS_NEWLIB_NANO_FORMAT      (1) // ROM has the newlib nano version of formatting functions
