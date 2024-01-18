9b3dc69908de8eb023f1b1789906b56713fd59f8
wuzhenghui <wuzhenghui@espressif.com>
2024-01-18 18:56:40 +0800

    refactor(esp_hw_support): move regdma structure defination to soc components

--- components/hal/include/hal/pau_types.h
+++ components/hal/include/hal/pau_types.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -7,18 +7,19 @@
 
 #include "sdkconfig.h"
 #include "soc/soc_caps.h"
+#include "soc/regdma.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #if SOC_PAU_SUPPORTED
-#define PAU_REGDMA_LINK_NUM     (SOC_PM_PAU_LINK_NUM)
+#define PAU_REGDMA_LINK_NUM     (REGDMA_LINK_ENTRY_NUM)
 
 /**
  * @brief PAU REGDMA all link address buffer
  */
-typedef void * pau_regdma_link_addr_t[PAU_REGDMA_LINK_NUM];
+typedef regdma_entry_buf_t pau_regdma_link_addr_t;
 
 #endif
 
