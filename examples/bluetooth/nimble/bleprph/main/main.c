7bda1db4f89b5ee84cf8fbbf57a90b233b7f6fab
SumeetSingh19 <sumeet.singh@espressif.com>
2024-01-18 17:07:05 +0530

    feat(nimble): authorization permission on gatt read and write

--- examples/bluetooth/nimble/bleprph/main/main.c
+++ examples/bluetooth/nimble/bleprph/main/main.c
@@ -393,6 +393,16 @@ bleprph_gap_event(struct ble_gap_event *event, void *arg)
         }
         return 0;
 
+    case BLE_GAP_EVENT_AUTHORIZE:
+        MODLOG_DFLT(INFO, "authorize event: conn_handle=%d attr_handle=%d is_read=%d",
+                    event->authorize.conn_handle,
+                    event->authorize.attr_handle,
+                    event->authorize.is_read);
+
+        /* The default behaviour for the event is to reject authorize request */
+        event->authorize.out_response = BLE_GAP_AUTHORIZE_REJECT;
+        return 0;
+
 #if MYNEWT_VAL(BLE_POWER_CONTROL)
     case BLE_GAP_EVENT_TRANSMIT_POWER:
         MODLOG_DFLT(INFO, "Transmit power event : status=%d conn_handle=%d reason=%d "
