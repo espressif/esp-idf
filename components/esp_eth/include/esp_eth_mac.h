79d34bf5d58abf06831ed49999e664bca875839c
Ondrej Kosta <ondrej.kosta@espressif.com>
2024-01-29 14:00:50 +0100

    feat(esp_eth): added SPI Ethernet module polling mode
    Closes https://github.com/espressif/esp-idf/issues/12682

--- components/esp_eth/include/esp_eth_mac.h
+++ components/esp_eth/include/esp_eth_mac.h
@@ -573,7 +573,8 @@ typedef struct
  *
  */
 typedef struct {
-    int int_gpio_num;                                   /*!< Interrupt GPIO number */
+    int int_gpio_num;                                   /*!< Interrupt GPIO number, set -1 to not use interrupt and to poll rx status periodically */
+    uint32_t poll_period_ms;                            /*!< Period in ms to poll rx status when interrupt mode is not used */
     spi_host_device_t spi_host_id;                      /*!< SPI peripheral (this field is invalid when custom SPI driver is defined) */
     spi_device_interface_config_t *spi_devcfg;          /*!< SPI device configuration (this field is invalid when custom SPI driver is defined) */
     eth_spi_custom_driver_config_t custom_spi_driver;   /*!< Custom SPI driver definitions */
@@ -586,9 +587,10 @@ typedef struct {
 #define ETH_DM9051_DEFAULT_CONFIG(spi_host, spi_devcfg_p) \
     {                                           \
         .int_gpio_num = 4,                      \
+        .poll_period_ms = 0,                    \
         .spi_host_id = spi_host,                \
         .spi_devcfg = spi_devcfg_p,             \
-        .custom_spi_driver = ETH_DEFAULT_SPI, \
+        .custom_spi_driver = ETH_DEFAULT_SPI,   \
     }
 
 /**
@@ -610,7 +612,8 @@ esp_eth_mac_t *esp_eth_mac_new_dm9051(const eth_dm9051_config_t *dm9051_config, 
  *
  */
 typedef struct {
-    int int_gpio_num;                                   /*!< Interrupt GPIO number */
+    int int_gpio_num;                                   /*!< Interrupt GPIO number, set -1 to not use interrupt and to poll rx status periodically */
+    uint32_t poll_period_ms;                            /*!< Period in ms to poll rx status when interrupt mode is not used */
     spi_host_device_t spi_host_id;                      /*!< SPI peripheral (this field is invalid when custom SPI driver is defined)*/
     spi_device_interface_config_t *spi_devcfg;          /*!< SPI device configuration (this field is invalid when custom SPI driver is defined)*/
     eth_spi_custom_driver_config_t custom_spi_driver;   /*!< Custom SPI driver definitions */
@@ -623,9 +626,10 @@ typedef struct {
 #define ETH_W5500_DEFAULT_CONFIG(spi_host, spi_devcfg_p) \
     {                                           \
         .int_gpio_num = 4,                      \
+        .poll_period_ms = 0,                    \
         .spi_host_id = spi_host,                \
         .spi_devcfg = spi_devcfg_p,             \
-        .custom_spi_driver = ETH_DEFAULT_SPI, \
+        .custom_spi_driver = ETH_DEFAULT_SPI,   \
     }
 
 /**
@@ -647,7 +651,8 @@ esp_eth_mac_t *esp_eth_mac_new_w5500(const eth_w5500_config_t *w5500_config, con
  *
  */
 typedef struct {
-    int int_gpio_num;                                   /*!< Interrupt GPIO number */
+    int int_gpio_num;                                   /*!< Interrupt GPIO number, set -1 to not use interrupt and to poll rx status periodically */
+    uint32_t poll_period_ms;                            /*!< Period in ms to poll rx status when interrupt mode is not used */
     spi_host_device_t spi_host_id;                      /*!< SPI peripheral (this field is invalid when custom SPI driver is defined) */
     spi_device_interface_config_t *spi_devcfg;          /*!< SPI device configuration (this field is invalid when custom SPI driver is defined) */
     eth_spi_custom_driver_config_t custom_spi_driver;   /*!< Custom SPI driver definitions */
@@ -660,9 +665,10 @@ typedef struct {
 #define ETH_KSZ8851SNL_DEFAULT_CONFIG(spi_host, spi_devcfg_p) \
     {                                               \
         .int_gpio_num = 4,                          \
+        .poll_period_ms = 0,                        \
         .spi_host_id = spi_host,                    \
         .spi_devcfg = spi_devcfg_p,                 \
-        .custom_spi_driver = ETH_DEFAULT_SPI,     \
+        .custom_spi_driver = ETH_DEFAULT_SPI,       \
     }
 
 /**
--- components/esp_eth/test_apps/pytest_esp_eth.py
+++ components/esp_eth/test_apps/pytest_esp_eth.py
@@ -264,6 +264,7 @@ def test_esp_eth_dp83848(dut: IdfDut) -> None:
 @pytest.mark.eth_w5500
 @pytest.mark.parametrize('config', [
     'default_w5500',
+    'poll_w5500',
 ], indirect=True)
 def test_esp_eth_w5500(dut: IdfDut) -> None:
     ethernet_test(dut)
@@ -276,6 +277,7 @@ def test_esp_eth_w5500(dut: IdfDut) -> None:
 @pytest.mark.eth_ksz8851snl
 @pytest.mark.parametrize('config', [
     'default_ksz8851snl',
+    'poll_ksz8851snl',
 ], indirect=True)
 def test_esp_eth_ksz8851snl(dut: IdfDut) -> None:
     ethernet_test(dut)
@@ -288,6 +290,7 @@ def test_esp_eth_ksz8851snl(dut: IdfDut) -> None:
 @pytest.mark.eth_dm9051
 @pytest.mark.parametrize('config', [
     'default_dm9051',
+    'poll_dm9051',
 ], indirect=True)
 def test_esp_eth_dm9051(dut: IdfDut) -> None:
     ethernet_test(dut)
