194c38479e4f5aae4490b087377323a20fc6656a
wuzhenghui <wuzhenghui@espressif.com>
2024-01-29 21:51:15 +0800

    refactor(esp_hw_support): split pd_top clock retention initialization by target

--- components/esp_hw_support/include/esp_private/sleep_clock.h
+++ components/esp_hw_support/include/esp_private/sleep_clock.h
@@ -1,14 +1,24 @@
 /*
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
+#include <stddef.h>
+#include <string.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include "sdkconfig.h"
 #include "esp_err.h"
+#include "esp_log.h"
+#include "esp_attr.h"
+#include "esp_check.h"
+#include "esp_regdma.h"
+#include "esp_private/sleep_retention.h"
+#include "esp_private/startup_internal.h"
+#include "soc/soc_caps.h"
 
 #ifdef __cplusplus
 extern "C" {
