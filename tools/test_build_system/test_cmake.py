52da5b765f3f57153fb3b715691075384aa15854
Marek Fiala <marek.fiala@espressif.com>
2024-01-30 16:53:12 +0100

    feat(tools): Run skipped build system pytests on Win

--- tools/test_build_system/test_cmake.py
+++ tools/test_build_system/test_cmake.py
@@ -1,22 +1,25 @@
-# SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
 import logging
 import os
 import re
 import shutil
-import sys
 from pathlib import Path
 
-import pytest
-from test_build_system_helpers import EnvDict, IdfPyFunc, append_to_file, file_contains, run_cmake, run_cmake_and_build
+from test_build_system_helpers import append_to_file
+from test_build_system_helpers import EnvDict
+from test_build_system_helpers import file_contains
+from test_build_system_helpers import IdfPyFunc
+from test_build_system_helpers import run_cmake
+from test_build_system_helpers import run_cmake_and_build
 
 
-@pytest.mark.skipif(sys.platform == 'win32', reason='Failing on Windows runner. TODO')
 def test_build_custom_cmake_project(test_app_copy: Path) -> None:
+    # Test is compatible with any target. Random targets in the list are selected for performance reasons
     for target in ['esp32', 'esp32s3', 'esp32c6', 'esp32h2']:
         logging.info(f'Test build ESP-IDF as a library to a custom CMake projects for {target}')
         idf_path = Path(os.environ['IDF_PATH'])
-        run_cmake_and_build(str(idf_path / 'examples' / 'build_system' / 'cmake' / 'idf_as_lib'),
+        run_cmake_and_build(str(idf_path / 'examples' / 'build_system' / 'cmake' / 'idf_as_lib'), '-G', 'Ninja',
                             '-DCMAKE_TOOLCHAIN_FILE={}'.format(idf_path / 'tools' / 'cmake' / f'toolchain-{target}.cmake'), f'-DTARGET={target}')
         assert file_contains((test_app_copy / 'build' / 'compile_commands.json'), '"command"')
         shutil.rmtree(test_app_copy / 'build')
@@ -52,20 +55,16 @@ def test_build_cmake_library_psram_strategies(idf_py: IdfPyFunc, test_app_copy: 
         (test_app_copy / 'sdkconfig').unlink()
 
 
-@pytest.mark.skipif(sys.platform == 'win32', reason='Failing on Windows runner. TODO')
-@pytest.mark.usefixtures('test_app_copy')
-@pytest.mark.usefixtures('idf_copy')
-def test_defaults_for_unspecified_idf_build_process_args(default_idf_env: EnvDict) -> None:
+def test_defaults_unspecified_build_args(idf_copy: Path) -> None:
     logging.info('Defaults set properly for unspecified idf_build_process args')
-    idf_path = Path(default_idf_env.get('IDF_PATH'))
-    idf_as_lib_path = idf_path / 'examples' / 'build_system' / 'cmake' / 'idf_as_lib'
+    idf_as_lib_path = idf_copy / 'examples' / 'build_system' / 'cmake' / 'idf_as_lib'
     append_to_file(idf_as_lib_path / 'CMakeLists.txt', '\n'.join(['idf_build_get_property(project_dir PROJECT_DIR)',
                                                                   'message("Project directory: ${project_dir}")']))
-    ret = run_cmake('..',
-                    '-DCMAKE_TOOLCHAIN_FILE={}'.format(str(idf_path / 'tools' / 'cmake' / 'toolchain-esp32.cmake')),
+    ret = run_cmake('..', '-G', 'Ninja',
+                    '-DCMAKE_TOOLCHAIN_FILE={}'.format(str(idf_copy / 'tools' / 'cmake' / 'toolchain-esp32.cmake')),
                     '-DTARGET=esp32',
                     workdir=idf_as_lib_path)
-    assert 'Project directory: {}'.format(str(idf_as_lib_path)) in ret.stderr
+    assert 'Project directory: {}'.format(str(idf_as_lib_path.as_posix())) in ret.stderr
 
 
 def test_build_example_on_host(default_idf_env: EnvDict) -> None:
