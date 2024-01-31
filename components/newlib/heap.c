a7bd00eacccc41c60f79976bef927d4e2a467391
Guillaume Souchere <guillaume.souchere@espressif.com>
2024-01-31 09:00:35 +0100

    feat(newlib): Add definition of aligned_alloc to heap.c
    Add the definition of aligned_alloc to the list of
    definitions provided by heap.c.
    
    Note that memalign (strictly equivalent) to aligned_alloc
    is already defined in heap.c.

--- components/newlib/heap.c
+++ components/newlib/heap.c
@@ -74,6 +74,11 @@ void* memalign(size_t alignment, size_t n)
     return heap_caps_aligned_alloc_default(alignment, n);
 }
 
+void* aligned_alloc(size_t alignment, size_t n)
+{
+    return heap_caps_aligned_alloc_default(alignment, n);
+}
+
 int posix_memalign(void **out_ptr, size_t alignment, size_t size)
 {
     if (size == 0) {
