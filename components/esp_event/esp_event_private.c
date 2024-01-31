417de470ecc9653500474dcbe9648fc906c4049e
Jakob Hasse <jakob.hasse@espressif.com>
2024-01-31 10:24:05 +0800

    change(esp_event): reformat files with astyle

--- components/esp_event/esp_event_private.c
+++ components/esp_event/esp_event_private.c
@@ -22,8 +22,7 @@ bool esp_event_is_handler_registered(esp_event_loop_handle_t event_loop, esp_eve
 
     SLIST_FOREACH(loop_node, &(loop->loop_nodes), next) {
         SLIST_FOREACH(handler, &(loop_node->handlers), next) {
-            if(event_base == ESP_EVENT_ANY_BASE && event_id == ESP_EVENT_ANY_ID && handler->handler_ctx->handler == event_handler)
-            {
+            if (event_base == ESP_EVENT_ANY_BASE && event_id == ESP_EVENT_ANY_ID && handler->handler_ctx->handler == event_handler) {
                 result = true;
                 goto out;
             }
@@ -32,18 +31,16 @@ bool esp_event_is_handler_registered(esp_event_loop_handle_t event_loop, esp_eve
         SLIST_FOREACH(base_node, &(loop_node->base_nodes), next) {
             if (base_node->base == event_base) {
                 SLIST_FOREACH(handler, &(base_node->handlers), next) {
-                    if(event_id == ESP_EVENT_ANY_ID && handler->handler_ctx->handler == event_handler)
-                    {
+                    if (event_id == ESP_EVENT_ANY_ID && handler->handler_ctx->handler == event_handler) {
                         result = true;
                         goto out;
                     }
                 }
 
                 SLIST_FOREACH(id_node, &(base_node->id_nodes), next) {
-                    if(id_node->id == event_id) {
+                    if (id_node->id == event_id) {
                         SLIST_FOREACH(handler, &(id_node->handlers), next) {
-                            if(handler->handler_ctx->handler == event_handler)
-                            {
+                            if (handler->handler_ctx->handler == event_handler) {
                                 result = true;
                                 goto out;
                             }
