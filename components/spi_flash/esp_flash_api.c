72ba89f191151fc74fb610553f78fccb90ccb2d3
Mahavir Jain <mahavir@espressif.com>
2024-02-14 13:45:11 +0530

    fix(spi_flash): check for safe write address in encrypted flash API

--- components/spi_flash/esp_flash_api.c
+++ components/spi_flash/esp_flash_api.c
@@ -1131,11 +1131,13 @@ esp_err_t IRAM_ATTR esp_flash_write_encrypted(esp_flash_t *chip, uint32_t addres
 #endif //CONFIG_SPI_FLASH_VERIFY_WRITE
 
     esp_err_t err = rom_spiflash_api_funcs->chip_check(&chip);
+    VERIFY_CHIP_OP(write);
     // Flash encryption only support on main flash.
     if (chip != esp_flash_default_chip) {
         return ESP_ERR_NOT_SUPPORTED;
     }
-    if (err != ESP_OK) return err;
+    CHECK_WRITE_ADDRESS(chip, address, length);
+
     if (buffer == NULL || address + length > chip->size) {
         return ESP_ERR_INVALID_ARG;
     }
