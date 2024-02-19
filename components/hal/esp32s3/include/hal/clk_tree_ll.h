4200246df35497656c022bce1c9fae0dad97216b
hongshuqing <hongshuqing@espressif.com>
2024-02-19 14:33:40 +0800

    fix: fix s3 bbpll cali fail bug

--- components/hal/esp32s3/include/hal/clk_tree_ll.h
+++ components/hal/esp32s3/include/hal/clk_tree_ll.h
@@ -308,7 +308,7 @@ static inline __attribute__((always_inline)) void clk_ll_bbpll_set_config(uint32
     uint8_t dr3;
     uint8_t dchgp;
     uint8_t dcur;
-    uint8_t dbias;
+    uint8_t dbias = 3;
 
     if (pll_freq_mhz == CLK_LL_PLL_480M_FREQ_MHZ) {
         /* Configure 480M PLL */
@@ -320,7 +320,6 @@ static inline __attribute__((always_inline)) void clk_ll_bbpll_set_config(uint32
             dr3 = 0;
             dchgp = 5;
             dcur = 3;
-            dbias = 2;
             break;
         case RTC_XTAL_FREQ_32M:
             div_ref = 1;
@@ -329,7 +328,6 @@ static inline __attribute__((always_inline)) void clk_ll_bbpll_set_config(uint32
             dr3 = 1;
             dchgp = 4;
             dcur = 0;
-            dbias = 2;
             break;
         default:
             div_ref = 0;
@@ -338,7 +336,6 @@ static inline __attribute__((always_inline)) void clk_ll_bbpll_set_config(uint32
             dr3 = 0;
             dchgp = 5;
             dcur = 3;
-            dbias = 2;
             break;
         }
         REGI2C_WRITE(I2C_BBPLL, I2C_BBPLL_MODE_HF, 0x6B);
@@ -352,7 +349,6 @@ static inline __attribute__((always_inline)) void clk_ll_bbpll_set_config(uint32
             dr3 = 0;
             dchgp = 5;
             dcur = 3;
-            dbias = 2;
             break;
         case RTC_XTAL_FREQ_32M:
             div_ref = 1;
@@ -361,7 +357,6 @@ static inline __attribute__((always_inline)) void clk_ll_bbpll_set_config(uint32
             dr3 = 0;
             dchgp = 5;
             dcur = 3;
-            dbias = 2;
             break;
         default:
             div_ref = 0;
@@ -370,7 +365,6 @@ static inline __attribute__((always_inline)) void clk_ll_bbpll_set_config(uint32
             dr3 = 0;
             dchgp = 5;
             dcur = 3;
-            dbias = 2;
             break;
         }
         REGI2C_WRITE(I2C_BBPLL, I2C_BBPLL_MODE_HF, 0x69);
