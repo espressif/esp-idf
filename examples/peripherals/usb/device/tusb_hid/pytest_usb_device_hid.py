1302fd70bad5a9e9006803370d7fb2943f012391
Peter Marcisovsky <peter.marcisovsky@espressif.com>
2024-02-23 11:52:45 +0100

    ci: Add esp32s3 runner for usb device example tests
        - CI USB device tests are run on esp32s3
        - usb_host env_marker removed - not used anymore
        - failing usb_device_ncm test fixed

--- examples/peripherals/usb/device/tusb_hid/pytest_usb_device_hid.py
+++ examples/peripherals/usb/device/tusb_hid/pytest_usb_device_hid.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 import pytest
 from pytest_embedded import Dut
@@ -7,7 +7,7 @@ from pytest_embedded import Dut
 @pytest.mark.esp32s2
 @pytest.mark.esp32s3
 @pytest.mark.esp32p4
-@pytest.mark.temp_skip_ci(targets=['esp32s3', 'esp32p4'], reason='lack of runners with usb_device tag')
+@pytest.mark.temp_skip_ci(targets=['esp32p4'], reason='lack of runners with usb_device tag')
 @pytest.mark.usb_device
 def test_usb_device_hid_example(dut: Dut) -> None:
     dut.expect_exact('USB initialization DONE')
--- examples/peripherals/usb/device/tusb_serial_device/pytest_usb_device_serial.py
+++ examples/peripherals/usb/device/tusb_serial_device/pytest_usb_device_serial.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 from time import sleep
 
@@ -11,7 +11,7 @@ from serial.tools.list_ports import comports
 @pytest.mark.esp32s2
 @pytest.mark.esp32s3
 @pytest.mark.esp32p4
-@pytest.mark.temp_skip_ci(targets=['esp32s3', 'esp32p4'], reason='lack of runners with usb_device tag')
+@pytest.mark.temp_skip_ci(targets=['esp32p4'], reason='lack of runners with usb_device tag')
 @pytest.mark.usb_device
 def test_usb_device_serial_example(dut: Dut) -> None:
     dut.expect_exact('USB initialization DONE')
