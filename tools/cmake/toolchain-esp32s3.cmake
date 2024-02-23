df5d0be7671141c4f4313cb32c5b29ca1e949f88
Alexey Lapshin <alexey.lapshin@espressif.com>
2024-02-23 20:25:08 +0400

    fix(tools): do not use builtin memcpy/memset/strcpy on xtensa chips
    Closes https://github.com/espressif/esp-idf/issues/13228

--- tools/cmake/toolchain-esp32s3.cmake
+++ tools/cmake/toolchain-esp32s3.cmake
@@ -7,9 +7,15 @@ set(CMAKE_CXX_COMPILER xtensa-esp32s3-elf-g++)
 set(CMAKE_ASM_COMPILER xtensa-esp32s3-elf-gcc)
 set(_CMAKE_TOOLCHAIN_PREFIX xtensa-esp32s3-elf-)
 
-remove_duplicated_flags("-mlongcalls ${CMAKE_C_FLAGS}" UNIQ_CMAKE_C_FLAGS)
+remove_duplicated_flags("-mlongcalls \
+                         -fno-builtin-memcpy -fno-builtin-memset -fno-builtin-bzero \
+                         -fno-builtin-stpcpy -fno-builtin-strncpy \
+                         ${CMAKE_C_FLAGS}" UNIQ_CMAKE_C_FLAGS)
 set(CMAKE_C_FLAGS "${UNIQ_CMAKE_C_FLAGS}" CACHE STRING "C Compiler Base Flags" FORCE)
-remove_duplicated_flags("-mlongcalls ${CMAKE_CXX_FLAGS}" UNIQ_CMAKE_CXX_FLAGS)
+remove_duplicated_flags("-mlongcalls \
+                         -fno-builtin-memcpy -fno-builtin-memset -fno-builtin-bzero \
+                         -fno-builtin-stpcpy -fno-builtin-strncpy \
+                         ${CMAKE_CXX_FLAGS}" UNIQ_CMAKE_CXX_FLAGS)
 set(CMAKE_CXX_FLAGS "${UNIQ_CMAKE_CXX_FLAGS}" CACHE STRING "C++ Compiler Base Flags" FORCE)
 remove_duplicated_flags("-mlongcalls ${CMAKE_ASM_FLAGS}" UNIQ_CMAKE_ASM_FLAGS)
 set(CMAKE_ASM_FLAGS "${UNIQ_CMAKE_ASM_FLAGS}" CACHE STRING "ASM Compiler Base Flags" FORCE)
