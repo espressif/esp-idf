4bd9aae42b75bb92233d7cde9869fcd17498f403
Roland Dobai <roland@espressif.com>
2024-02-27 07:28:32 +0100

    change(version): Update version to 4.4.7

--- components/esp_common/include/esp_idf_version.h
+++ components/esp_common/include/esp_idf_version.h
@@ -23,7 +23,7 @@ extern "C" {
 /** Minor version number (x.X.x) */
 #define ESP_IDF_VERSION_MINOR   4
 /** Patch version number (x.x.X) */
-#define ESP_IDF_VERSION_PATCH   6
+#define ESP_IDF_VERSION_PATCH   7
 
 /**
  * Macro to convert IDF version number into an integer
