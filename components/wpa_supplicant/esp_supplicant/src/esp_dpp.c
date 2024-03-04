15ba8fb5ab86fa007a11fa415bf6e83460e9f9fe
jgujarathi <jash.gujarathi@espressif.com>
2024-03-04 15:41:44 +0530

    fix(wpa_supplicant): Cancel offchannel listen operations before sending dpp fail
    - Ensure that offchannel listening operations are cancelled before sending dpp
      fail events

--- components/wpa_supplicant/esp_supplicant/src/esp_dpp.c
+++ components/wpa_supplicant/esp_supplicant/src/esp_dpp.c
@@ -216,8 +216,9 @@ static int esp_dpp_handle_config_obj(struct dpp_authentication *auth,
         wpa_printf(MSG_INFO, DPP_EVENT_CONNECTOR "%s",
                    conf->connector);
     }
-    s_dpp_listen_in_progress = true;
-    esp_wifi_action_tx_req(WIFI_OFFCHAN_TX_CANCEL, 0, 0, NULL);
+    if (s_dpp_listen_in_progress) {
+        esp_supp_dpp_stop_listen();
+    }
     esp_dpp_call_cb(ESP_SUPP_DPP_CFG_RECVD, wifi_cfg);
 
     return 0;
