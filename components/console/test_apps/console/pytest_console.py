0b246b8c0ba029a9f71fdcbee14c0414bb41e5ed
Jakob Hasse <jakob.hasse@espressif.com>
2024-03-13 10:53:05 +0800

    refactor(console): made help command sorting depend on Kconfig option

--- components/console/test_apps/console/pytest_console.py
+++ components/console/test_apps/console/pytest_console.py
@@ -50,9 +50,15 @@ def do_test_help_quit(dut: Dut) -> None:
 
 
 @pytest.mark.parametrize(
+    'config', [
+        pytest.param('defaults'),
+    ]
+)
+@pytest.mark.parametrize(
     'test_on', [
         pytest.param('host', marks=[pytest.mark.linux, pytest.mark.host_test]),
-        pytest.param('target', marks=[pytest.mark.supported_targets, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32c3, pytest.mark.generic]),
         pytest.param('qemu', marks=[pytest.mark.esp32, pytest.mark.host_test, pytest.mark.qemu]),
     ]
 )
@@ -63,9 +69,15 @@ def test_console(dut: Dut, test_on: str) -> None:
 
 
 @pytest.mark.parametrize(
+    'config', [
+        pytest.param('defaults'),
+    ]
+)
+@pytest.mark.parametrize(
     'test_on', [
         pytest.param('host', marks=[pytest.mark.linux, pytest.mark.host_test]),
-        pytest.param('target', marks=[pytest.mark.supported_targets, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32c3, pytest.mark.generic]),
         pytest.param('qemu', marks=[pytest.mark.esp32, pytest.mark.host_test, pytest.mark.qemu]),
     ]
 )
@@ -74,9 +86,15 @@ def test_console_repl(dut: Dut, test_on: str) -> None:
 
 
 @pytest.mark.parametrize(
+    'config', [
+        pytest.param('defaults'),
+    ]
+)
+@pytest.mark.parametrize(
     'test_on', [
         pytest.param('host', marks=[pytest.mark.linux, pytest.mark.host_test]),
-        pytest.param('target', marks=[pytest.mark.supported_targets, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32c3, pytest.mark.generic]),
         pytest.param('qemu', marks=[pytest.mark.esp32, pytest.mark.host_test, pytest.mark.qemu]),
     ]
 )
@@ -85,20 +103,81 @@ def test_console_help_sorted_registration(dut: Dut, test_on: str) -> None:
 
 
 @pytest.mark.parametrize(
+    'config', [
+        pytest.param('defaults'),
+    ]
+)
+@pytest.mark.parametrize(
     'test_on', [
         pytest.param('host', marks=[pytest.mark.linux, pytest.mark.host_test]),
-        pytest.param('target', marks=[pytest.mark.supported_targets, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32c3, pytest.mark.generic]),
         pytest.param('qemu', marks=[pytest.mark.esp32, pytest.mark.host_test, pytest.mark.qemu]),
     ]
 )
 def test_console_help_reverse_registration(dut: Dut, test_on: str) -> None:
+    dut.expect_exact('Press ENTER to see the list of tests')
+    dut.write('"esp console help command - reverse registration"')
+
+    dut.expect_exact('esp>', timeout=5)
+    dut.write('help')
+
+    dut.expect_exact('zzz', timeout=5)
+    dut.expect_exact('should appear last in help', timeout=5)
+
+    dut.expect_exact('quit', timeout=5)
+    dut.expect_exact('Quit REPL environment', timeout=5)
+
+    dut.expect(r'help\s+\[<string>\]', timeout=5)
+
+    # Note: repl seems to do the line breaks by itself, this needs to be adjusted if repl changes its line width
+    dut.expect_exact('Print the summary of all registered commands if no arguments are given,', timeout=5)
+    dut.expect_exact('otherwise print summary of given command.', timeout=5)
+    dut.expect(r'<string>\s+Name of command', timeout=5)
+
+    dut.expect_exact('aaa', timeout=5)
+    dut.expect_exact('should appear first in help', timeout=5)
+    dut.expect_exact('esp>', timeout=5)
+
+
+@pytest.mark.parametrize(
+    'config', [
+        pytest.param('sorted'),
+    ]
+)
+@pytest.mark.parametrize(
+    'test_on', [
+        pytest.param('host', marks=[pytest.mark.linux, pytest.mark.host_test]),
+    ]
+)
+def test_console_sorted_help_sorted_registration(dut: Dut, test_on: str) -> None:
+    do_test_help_generic(dut, 'sorted')
+
+
+@pytest.mark.parametrize(
+    'config', [
+        pytest.param('sorted', marks=[pytest.mark.linux, pytest.mark.host_test]),
+    ]
+)
+@pytest.mark.parametrize(
+    'test_on', [
+        pytest.param('host', marks=[pytest.mark.linux, pytest.mark.host_test]),
+    ]
+)
+def test_console_sorted_help_reverse_registration(dut: Dut, test_on: str) -> None:
     do_test_help_generic(dut, 'reverse')
 
 
 @pytest.mark.parametrize(
+    'config', [
+        pytest.param('defaults'),
+    ]
+)
+@pytest.mark.parametrize(
     'test_on', [
         pytest.param('host', marks=[pytest.mark.linux, pytest.mark.host_test]),
-        pytest.param('target', marks=[pytest.mark.supported_targets, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32, pytest.mark.generic]),
+        pytest.param('target', marks=[pytest.mark.esp32c3, pytest.mark.generic]),
         pytest.param('qemu', marks=[pytest.mark.esp32, pytest.mark.host_test, pytest.mark.qemu]),
     ]
 )
