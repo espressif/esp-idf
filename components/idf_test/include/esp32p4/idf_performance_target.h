33dec4175aae6b97e50728b2da2beda6c1ea5f3d
Sudeep Mohanty <sudeep.mohanty@espressif.com>
2024-03-22 14:32:18 +0100

    fix(freertos): Updated threshold for scheduling time test for esp32p4
    This commit increases the pass threshold for the scheduling time test on
    esp32p4.

--- components/idf_test/include/esp32p4/idf_performance_target.h
+++ components/idf_test/include/esp32p4/idf_performance_target.h
@@ -7,3 +7,6 @@
 /* Spinlock performance on esp32p4 is slower. May need to adjust these values once IDF-7898 is fixed */
 #define IDF_PERFORMANCE_MAX_FREERTOS_SPINLOCK_CYCLES_PER_OP                     380
 #define IDF_PERFORMANCE_MAX_FREERTOS_SPINLOCK_CYCLES_PER_OP_UNICORE             135
+
+/* Solicited yields (portYIELD() or taskYIELD()) take longer on esp32p4. TODO: IDF-2809 */
+#define IDF_PERFORMANCE_MAX_SCHEDULING_TIME                                     2900
