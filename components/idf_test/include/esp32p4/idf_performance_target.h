afee8302783ceb88b347ed354cb0ed9eb4cebfec
Sudeep Mohanty <sudeep.mohanty@espressif.com>
2024-03-25 14:25:35 +0100

    change(freertos): Updated spinlock performance test threshold
    This commit relaxes the passing threshold for spinlock performance tests
    for the esp32p4.

--- components/idf_test/include/esp32p4/idf_performance_target.h
+++ components/idf_test/include/esp32p4/idf_performance_target.h
@@ -1,12 +1,12 @@
 /*
- * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/* Spinlock performance on esp32p4 is slower. May need to adjust these values once IDF-7898 is fixed */
-#define IDF_PERFORMANCE_MAX_FREERTOS_SPINLOCK_CYCLES_PER_OP                     380
-#define IDF_PERFORMANCE_MAX_FREERTOS_SPINLOCK_CYCLES_PER_OP_UNICORE             135
+/* Spinlock performance on esp32p4 is slower. */
+#define IDF_PERFORMANCE_MAX_FREERTOS_SPINLOCK_CYCLES_PER_OP                     400
+#define IDF_PERFORMANCE_MAX_FREERTOS_SPINLOCK_CYCLES_PER_OP_UNICORE             150
 
 /* Solicited yields (portYIELD() or taskYIELD()) take longer on esp32p4. TODO: IDF-2809 */
 #define IDF_PERFORMANCE_MAX_SCHEDULING_TIME                                     2900
