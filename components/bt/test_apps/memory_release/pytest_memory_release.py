4d7f9c5d88b477ed8673e5f9aff402c0e0e60c00
Wang Mengyang <wangmengyang@espressif.com>
2024-03-29 12:46:04 +0800

    change(bt): Add a test for release bt .bss and .data memory to heap
    1. use nimble host, initialize and then deinitialize bt stack in the test
    2. support tests for different SoC targets and multiple configuration options

---
+++ components/bt/test_apps/memory_release/pytest_memory_release.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+from pytest_embedded import Dut
+
+
+@pytest.mark.parametrize('config', [
+    pytest.param('default', marks=[pytest.mark.esp32, pytest.mark.esp32c2, pytest.mark.generic]),
+    pytest.param('iram', marks=[pytest.mark.esp32c2, pytest.mark.generic]),
+    pytest.param('psram', marks=[pytest.mark.esp32, pytest.mark.psram]),
+], indirect=True)
+def test_bt_memory_release(dut: Dut) -> None:
+    dut.expect_exact('BLE Host Task Started', timeout=6)
+    dut.expect_exact('BLE Host Task Stopped', timeout=8)
+    dut.expect_exact('SUCCESS', timeout=10)
