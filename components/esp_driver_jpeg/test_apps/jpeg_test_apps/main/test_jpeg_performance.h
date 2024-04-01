58acf93da2bc0aaa356635e826221773294ae355
Cao Sen Miao <caosenmiao@espressif.com>
2024-04-01 19:59:44 +0800

    test(jpeg_encoder): Add the test support for jpeg encoder

--- components/esp_driver_jpeg/test_apps/jpeg_test_apps/main/test_jpeg_performance.h
+++ components/esp_driver_jpeg/test_apps/jpeg_test_apps/main/test_jpeg_performance.h
@@ -11,7 +11,11 @@
 extern "C" {
 #endif
 
+// Decoder performance lists
 #define IDF_PERFORMANCE_MIN_JPEG_DECODE_1080P_2_RGB565_PERFORMANCE                 40 // 40 fps for 1080p decoder.
+
+// Encoder performance lists
+#define IDF_PERFORMANCE_MIN_JPEG_ENCODE_RGB888_2_480P_PERFORMANCE                  160 // 160 fps for 480p encoder.
 
 #ifdef __cplusplus
 }
