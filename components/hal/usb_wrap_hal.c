d944e85a5757213b471f63f49486a57cc5d8e6d0
Darian Leung <darian@espressif.com>
2024-04-04 15:04:48 +0800

    feat(hal/usb): Update USB WRAP and USJ LL
    - Added LL cap macros to distinguish feature differences between the LLs of
      different targets:
        - '..._LL_EXT_PHY_SUPPORTED' indicates whether the USB WRAP/USJ supports
          routing to an external FSLS PHY.
    - Tidied up some RCC LL functions and their callers.
    - Added 'usb_wrap_types.h' and 'usb_serial_jtag_types.h' to provide types used
      in LLs.
    - Fixed some spelling/naming issues as part of code-spell pre-commit

--- components/hal/usb_wrap_hal.c
+++ components/hal/usb_wrap_hal.c
@@ -5,7 +5,6 @@
  */
 
 #include "soc/soc_caps.h"
-#include "soc/rtc_cntl_struct.h"
 #if SOC_USB_SERIAL_JTAG_SUPPORTED
 #include "hal/usb_serial_jtag_ll.h"
 #endif
@@ -18,8 +17,12 @@ void usb_fsls_phy_hal_init(usb_fsls_phy_hal_context_t *hal)
 #if SOC_USB_SERIAL_JTAG_SUPPORTED
     hal->jtag_dev = &USB_SERIAL_JTAG;
 #endif
+#if !USB_WRAP_LL_EXT_PHY_SUPPORTED
+    usb_wrap_ll_phy_set_defaults(hal->wrap_dev);
+#endif
 }
 
+#if USB_WRAP_LL_EXT_PHY_SUPPORTED
 void usb_fsls_phy_hal_otg_conf(usb_fsls_phy_hal_context_t *hal, usb_phy_target_t phy_target)
 {
     if (phy_target == USB_PHY_TARGET_EXT) {
@@ -29,6 +32,7 @@ void usb_fsls_phy_hal_otg_conf(usb_fsls_phy_hal_context_t *hal, usb_phy_target_t
         usb_wrap_ll_phy_enable_pad(hal->wrap_dev, true);
     }
 }
+#endif // USB_WRAP_LL_EXT_PHY_SUPPORTED
 
 #if SOC_USB_SERIAL_JTAG_SUPPORTED
 void usb_fsls_phy_hal_jtag_conf(usb_fsls_phy_hal_context_t *hal, usb_phy_target_t phy_target)
@@ -45,7 +49,13 @@ void usb_fsls_phy_hal_jtag_conf(usb_fsls_phy_hal_context_t *hal, usb_phy_target_
 void usb_fsls_phy_hal_int_load_conf_host(usb_fsls_phy_hal_context_t *hal)
 {
     // HOST - upstream: dp_pd = 1, dm_pd = 1
-    usb_wrap_ll_phy_enable_pull_override(hal->wrap_dev, false, false, true, true);
+    usb_wrap_pull_override_vals_t vals = {
+        .dp_pu = false,
+        .dm_pu = false,
+        .dp_pd = true,
+        .dm_pd = true,
+    };
+    usb_wrap_ll_phy_enable_pull_override(hal->wrap_dev, &vals);
 }
 
 void usb_fsls_phy_hal_int_load_conf_dev(usb_fsls_phy_hal_context_t *hal, usb_phy_speed_t speed)
@@ -53,10 +63,22 @@ void usb_fsls_phy_hal_int_load_conf_dev(usb_fsls_phy_hal_context_t *hal, usb_phy
     // DEVICE - downstream
     if (speed == USB_PHY_SPEED_LOW) {
         // LS: dm_pu = 1
-        usb_wrap_ll_phy_enable_pull_override(hal->wrap_dev, false, true, false, false);
+        usb_wrap_pull_override_vals_t vals = {
+            .dp_pu = false,
+            .dm_pu = true,
+            .dp_pd = false,
+            .dm_pd = false,
+        };
+        usb_wrap_ll_phy_enable_pull_override(hal->wrap_dev, &vals);
     } else {
         // FS: dp_pu = 1
-        usb_wrap_ll_phy_enable_pull_override(hal->wrap_dev, true, false, false, false);
+        usb_wrap_pull_override_vals_t vals = {
+            .dp_pu = true,
+            .dm_pu = false,
+            .dm_pd = false,
+            .dp_pd = false,
+        };
+        usb_wrap_ll_phy_enable_pull_override(hal->wrap_dev, &vals);
     }
 }
 
@@ -64,16 +86,18 @@ void usb_fsls_phy_hal_int_mimick_disconn(usb_fsls_phy_hal_context_t *hal, bool d
 {
     if (disconn) {
         /*
-        We mimick a disconnect by enabling the internal PHY's test mode, then forcing the output_enable to HIGH. This will:
+        We mimic a disconnect by enabling the internal PHY's test mode, then forcing the output_enable to HIGH. This will:
         A HIGH output_enable will cause the received VP and VM to be zero, thus mimicking a disconnection.
         */
-        usb_wrap_ll_phy_test_mode_set_signals(hal->wrap_dev,
-                                            true,     // OEN
-                                            false,    // TX D+
-                                            false,    // TX D-
-                                            false,    // RX D+
-                                            false,    // RX D-
-                                            false);   // RX RCv
+        usb_wrap_test_mode_vals_t vals = {
+            .tx_enable_n = true,
+            .tx_dp = false,
+            .tx_dm = false,
+            .rx_dp = false,
+            .rx_dm = false,
+            .rx_rcv = false,
+        };
+        usb_wrap_ll_phy_test_mode_set_signals(hal->wrap_dev, &vals);
         usb_wrap_ll_phy_enable_test_mode(hal->wrap_dev, true);
     } else {
         usb_wrap_ll_phy_enable_test_mode(hal->wrap_dev, false);
