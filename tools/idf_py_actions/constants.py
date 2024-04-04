fca47d4385c5877f234a3571123f45d965e88415
Erhan Kurubas <erhan.kurubas@espressif.com>
2024-04-04 11:06:02 +0300

    doc(jtag): update esp32p4 jtag debugging guide

--- tools/idf_py_actions/constants.py
+++ tools/idf_py_actions/constants.py
@@ -43,4 +43,5 @@ OPENOCD_TAGET_CONFIG: Dict[str, str] = {
     'esp32s3': '-f board/esp32s3-builtin.cfg',
     'esp32c6': '-f board/esp32c6-builtin.cfg',
     'esp32h2': '-f board/esp32h2-builtin.cfg',
+    'esp32p4': '-f board/esp32p4-builtin.cfg',
 }
