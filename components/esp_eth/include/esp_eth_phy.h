34b3761ea9e4067863d322a1fe3b9a2b50c7c8b0
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-04-09 21:33:49 +0200

    add esp_eth_phy_new_jl1101

--- components/esp_eth/include/esp_eth_phy.h
+++ components/esp_eth/include/esp_eth_phy.h
@@ -352,6 +352,17 @@ esp_eth_phy_t *esp_eth_phy_new_dp83848(const eth_phy_config_t *config);
 */
 esp_eth_phy_t *esp_eth_phy_new_ksz80xx(const eth_phy_config_t *config);
 
+/**
+* @brief Create a PHY instance of JL1101
+*
+* @param[in] config: configuration of PHY
+*
+* @return
+*      - instance: create PHY instance successfully
+*      - NULL: create PHY instance failed because some error occurred
+*/
+esp_eth_phy_t *esp_eth_phy_new_jl1101(const eth_phy_config_t *config);
+
 #if CONFIG_ETH_SPI_ETHERNET_DM9051
 /**
 * @brief Create a PHY instance of DM9051
