57f61d2311844842ae6a37fbbaba30dbd334a3f8
Alexey Gerenkov <alexey@espressif.com>
2024-04-11 15:31:06 +0300

    feat(tools): Update Clang version to esp-17.0.1_20240419

--- tools/cmake/toolchain-clang-esp32p4.cmake
+++ tools/cmake/toolchain-clang-esp32p4.cmake
@@ -11,21 +11,21 @@ set(CMAKE_AR llvm-ar)
 set(CMAKE_RANLIB llvm-ranlib)
 set(CMAKE_OBJDUMP riscv32-esp-elf-clang-objdump)
 
-remove_duplicated_flags("--target=riscv32-esp-elf -march=rv32imac_zicsr_zifencei -mabi=ilp32 -no-integrated-as \
+remove_duplicated_flags("--target=riscv32-esp-elf -march=rv32imafc_zicsr_zifencei -mabi=ilp32f -no-integrated-as \
                         ${CMAKE_C_FLAGS}"
                         UNIQ_CMAKE_C_FLAGS)
 set(CMAKE_C_FLAGS "${UNIQ_CMAKE_C_FLAGS}"
     CACHE STRING "C Compiler Base Flags"
     FORCE)
 
-remove_duplicated_flags("--target=riscv32-esp-elf -march=rv32imac_zicsr_zifencei -mabi=ilp32 -no-integrated-as \
+remove_duplicated_flags("--target=riscv32-esp-elf -march=rv32imafc_zicsr_zifencei -mabi=ilp32f -no-integrated-as \
                         ${CMAKE_CXX_FLAGS}"
                         UNIQ_CMAKE_CXX_FLAGS)
 set(CMAKE_CXX_FLAGS "${UNIQ_CMAKE_CXX_FLAGS}"
     CACHE STRING "C++ Compiler Base Flags"
     FORCE)
 
-remove_duplicated_flags("--target=riscv32-esp-elf -march=rv32imac_zicsr_zifencei -mabi=ilp32 -no-integrated-as \
+remove_duplicated_flags("--target=riscv32-esp-elf -march=rv32imafc_zicsr_zifencei -mabi=ilp32f -no-integrated-as \
                         ${CMAKE_ASM_FLAGS}"
                         UNIQ_CMAKE_ASM_FLAGS)
 set(CMAKE_ASM_FLAGS "${UNIQ_CMAKE_ASM_FLAGS}"
