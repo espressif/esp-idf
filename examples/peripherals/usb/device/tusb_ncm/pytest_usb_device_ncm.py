6b10a3074ae1b8003a56fa92fda85899c0f65c7e
Tomas Rezucha <tomas.rezucha@espressif.com>
2024-04-17 11:43:10 +0200

    fix(ci): Temporarily disable USB device tests on S3
    Partially reverts 1302fd70bad5a9e9006803370d7fb2943f012391

--- examples/peripherals/usb/device/tusb_ncm/pytest_usb_device_ncm.py
+++ examples/peripherals/usb/device/tusb_ncm/pytest_usb_device_ncm.py
@@ -9,13 +9,14 @@ from pytest_embedded import Dut
 
 @pytest.mark.esp32s2
 @pytest.mark.esp32s3
+@pytest.mark.temp_skip_ci(targets=['esp32s3'], reason='lack of runners with usb_device tag')
 @pytest.mark.usb_device
 def test_usb_device_ncm_example(dut: Dut) -> None:
     netif_mac = dut.expect(r'Network interface HW address: ([0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2})')
     netif_mac = netif_mac.group(1).decode('utf-8')
     dut.expect_exact('USB NCM and WiFi initialized and started')
     dut.expect_exact('Returned from app_main()')
-    time.sleep(5)   # Wait 5s for the network interface to appear
+    time.sleep(1)   # Wait 1s for the network interface to appear
     out_bytes = subprocess.check_output('ifconfig', shell=True, timeout=5)
     out_str = out_bytes.decode('utf-8')
     print('expected network interface HW address: ', netif_mac)
