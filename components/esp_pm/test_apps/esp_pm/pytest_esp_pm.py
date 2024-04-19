c97ab134ef568440e9cf98a0db7243e8e1b31ea5
wuzhenghui <wuzhenghui@espressif.com>
2024-04-19 13:13:44 +0800

    ci(esp_pm): add pd_top auto lightsleep test case for esp_pm

--- components/esp_pm/test_apps/esp_pm/pytest_esp_pm.py
+++ components/esp_pm/test_apps/esp_pm/pytest_esp_pm.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
 import pytest
 from pytest_embedded import Dut
@@ -41,4 +41,19 @@ def test_esp_attr_xip_psram_esp32s2(dut: Dut) -> None:
     indirect=True,
 )
 def test_esp_attr_xip_psram_esp32s3(dut: Dut) -> None:
+    dut.run_all_single_board_cases()
+
+
+# power down CPU and TOP domain in auto-lightsleep
+@pytest.mark.esp32c6
+@pytest.mark.esp32h2
+@pytest.mark.esp32p4
+@pytest.mark.generic
+@pytest.mark.parametrize(
+    'config',
+    [
+        'pm_pd_top_sleep'
+    ],
+)
+def test_esp_pd_top_and_cpu_sleep(dut: Dut) -> None:
     dut.run_all_single_board_cases()
