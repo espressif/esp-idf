52b9c5d666ab779a489854b2f2dc4d775bfb4805
chenjianhua <chenjianhua@espressif.com>
2024-05-13 17:01:56 +0800

    feat(bt/bluedroid): support BLE set privacy mode

--- components/bt/host/bluedroid/stack/hcic/hciblecmds.c
+++ components/bt/host/bluedroid/stack/hcic/hciblecmds.c
@@ -1909,4 +1909,28 @@ UINT8 btsnd_hcic_ble_set_default_periodic_adv_sync_trans_params(UINT8 mode, UINT
     return btu_hcif_send_cmd_sync(LOCAL_BR_EDR_CONTROLLER_ID, p);
 }
 #endif // #if (BLE_FEAT_PERIODIC_ADV_SYNC_TRANSFER == TRUE)
+
+UINT8 btsnd_hcic_ble_set_privacy_mode(UINT8 addr_type, BD_ADDR addr, UINT8 privacy_mode)
+{
+    BT_HDR *p;
+    UINT8 *pp;
+
+    if ((p = HCI_GET_CMD_BUF (HCIC_PARAM_SIZE_SET_PRIVACY_MODE)) == NULL) {
+        return (FALSE);
+    }
+
+    pp = (UINT8 *)(p + 1);
+    p->len = HCIC_PREAMBLE_SIZE + HCIC_PARAM_SIZE_SET_PRIVACY_MODE;
+    p->offset = 0;
+
+    UINT16_TO_STREAM(pp, HCI_BLE_SET_PRIVACY_MODE);
+    UINT8_TO_STREAM(pp, HCIC_PARAM_SIZE_SET_PRIVACY_MODE);
+
+    UINT8_TO_STREAM(pp, addr_type);
+    BDADDR_TO_STREAM(pp, addr);
+    UINT8_TO_STREAM(pp, privacy_mode);
+
+    btu_hcif_send_cmd(LOCAL_BR_EDR_CONTROLLER_ID, p);
+    return (TRUE);
+}
 #endif
