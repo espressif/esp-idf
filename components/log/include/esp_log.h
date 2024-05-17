b445e38bf5d98a648dba1aa542ac41e2715cfd95
Konstantin Kondrashov <konstantin@espressif.com>
2024-05-17 19:15:23 +0300

    feat(log): Move esp_log_write APIs out of esp_log.h

--- components/log/include/esp_log.h
+++ components/log/include/esp_log.h
@@ -15,47 +15,11 @@
 #include "esp_log_color.h"
 #include "esp_log_buffer.h"
 #include "esp_log_timestamp.h"
+#include "esp_log_write.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-typedef int (*vprintf_like_t)(const char *, va_list);
-
-/**
- * @brief Set function used to output log entries
- *
- * By default, log output goes to UART0. This function can be used to redirect log
- * output to some other destination, such as file or network. Returns the original
- * log handler, which may be necessary to return output to the previous destination.
- *
- * @note Please note that function callback here must be re-entrant as it can be
- * invoked in parallel from multiple thread context.
- *
- * @param func new Function used for output. Must have same signature as vprintf.
- *
- * @return func old Function used for output.
- */
-vprintf_like_t esp_log_set_vprintf(vprintf_like_t func);
-
-/**
- * @brief Write message into the log
- *
- * This function is not intended to be used directly. Instead, use one of
- * ESP_LOGE, ESP_LOGW, ESP_LOGI, ESP_LOGD, ESP_LOGV macros.
- *
- * This function or these macros should not be used from an interrupt.
- */
-void esp_log_write(esp_log_level_t level, const char* tag, const char* format, ...) __attribute__((format(printf, 3, 4)));
-
-/**
- * @brief Write message into the log, va_list variant
- * @see esp_log_write()
- *
- * This function is provided to ease integration toward other logging framework,
- * so that esp_log can be used as a log sink.
- */
-void esp_log_writev(esp_log_level_t level, const char* tag, const char* format, va_list args);
 
 /** @cond */
 
