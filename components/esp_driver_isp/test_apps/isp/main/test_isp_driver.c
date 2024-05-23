f58b63d31e9b422c2e586fe6b849fbb4a19b0058
Armando <douyiwen@espressif.com>
2024-05-23 13:36:15 +0800

    test(isp_dvp): added isp_dvp test

--- components/esp_driver_isp/test_apps/isp/main/test_isp_driver.c
+++ components/esp_driver_isp/test_apps/isp/main/test_isp_driver.c
@@ -7,7 +7,6 @@
 #include "sdkconfig.h"
 #include "unity.h"
 #include "driver/isp.h"
-#include "esp_private/isp_dvp_private.h"
 #include "soc/soc_caps.h"
 
 TEST_CASE("ISP processor exhausted allocation", "[isp]")
@@ -30,45 +29,6 @@ TEST_CASE("ISP processor exhausted allocation", "[isp]")
         TEST_ESP_OK(esp_isp_del_processor(isp_proc[i]));
     }
 }
-
-#if SOC_ISP_DVP_SUPPORTED
-TEST_CASE("ISP DVP controller exhausted allocation", "[isp]")
-{
-    esp_isp_processor_cfg_t isp_config = {
-        .clk_hz = 120 * 1000 * 1000,
-        .input_data_source = ISP_INPUT_DATA_SOURCE_DVP,
-        .input_data_color_type = ISP_COLOR_RAW8,
-        .output_data_color_type = ISP_COLOR_RGB565,
-        .has_line_start_packet = false,
-        .has_line_end_packet = false,
-        .h_res = 800,
-        .v_res = 640,
-    };
-    isp_proc_handle_t isp_proc = NULL;
-    TEST_ESP_OK(esp_isp_new_processor(&isp_config, &isp_proc));
-
-    esp_cam_ctlr_isp_dvp_cfg_t dvp_ctlr_config = {
-        .data_width = 8,
-        .data_io = {53, 54, 52, 0, 1, 45, 46, 47, -1, -1, -1, -1, -1, -1, -1, -1},
-        .pclk_io = 21,
-        .hsync_io = 5,
-        .vsync_io = 23,
-        .de_io = 22,
-        .io_flags.vsync_invert = 1,
-    };
-    esp_cam_ctlr_handle_t dvp_ctrlr[SOC_ISP_DVP_CTLR_NUMS + 1] = {};
-    for (int i = 0; i < SOC_ISP_DVP_CTLR_NUMS; i++) {
-        TEST_ESP_OK(esp_isp_new_dvp_controller(isp_proc, &dvp_ctlr_config, &dvp_ctrlr[i]));
-    }
-
-    TEST_ASSERT(esp_isp_new_dvp_controller(isp_proc, &dvp_ctlr_config, &dvp_ctrlr[SOC_ISP_DVP_CTLR_NUMS]) == ESP_ERR_NOT_FOUND);
-
-    for (int i = 0; i < SOC_ISP_DVP_CTLR_NUMS; i++) {
-        TEST_ESP_OK(esp_isp_del_dvp_controller(dvp_ctrlr[i]));
-    }
-    TEST_ESP_OK(esp_isp_del_processor(isp_proc));
-}
-#endif
 
 TEST_CASE("ISP AF controller exhausted allocation", "[isp]")
 {
