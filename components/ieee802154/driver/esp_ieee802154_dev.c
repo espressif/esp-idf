fd0ea434963bb6672fba9d26ed8cc715059e0fd5
zwx <zhangwenxu@espressif.com>
2024-06-03 19:53:08 +0800

    fix(802.15.4): fixed ieee802154 will sleep when only pm enabled

--- components/ieee802154/driver/esp_ieee802154_dev.c
+++ components/ieee802154/driver/esp_ieee802154_dev.c
@@ -27,7 +27,7 @@
 #include "esp_attr.h"
 #include "esp_phy_init.h"
 
-#if SOC_PM_MODEM_RETENTION_BY_REGDMA && CONFIG_FREERTOS_USE_TICKLESS_IDLE
+#if CONFIG_PM_ENABLE
 #include "esp_pm.h"
 #include "esp_private/esp_clk.h"
 #include "esp_private/sleep_retention.h"
@@ -37,7 +37,7 @@
 #else
 #define IEEE802154_LINK_OWNER  ENTRY(0) | ENTRY(2)
 #endif // SOC_PM_RETENTION_HAS_CLOCK_BUG
-#endif // SOC_PM_MODEM_RETENTION_BY_REGDMA && CONFIG_FREERTOS_USE_TICKLESS_IDLE
+#endif // CONFIG_PM_ENABLE
 
 static bool s_rf_closed = true;
 #define CCA_DETECTION_TIME 8
@@ -915,24 +915,27 @@ esp_err_t ieee802154_receive_at(uint32_t time)
     return ESP_OK;
 }
 
-#if SOC_PM_MODEM_RETENTION_BY_REGDMA && CONFIG_FREERTOS_USE_TICKLESS_IDLE
+#if CONFIG_PM_ENABLE
 static esp_err_t ieee802154_sleep_retention_init(void *arg)
 {
+    esp_err_t err = ESP_OK;
+#if SOC_PM_MODEM_RETENTION_BY_REGDMA && CONFIG_FREERTOS_USE_TICKLESS_IDLE
     #define N_REGS_IEEE802154() (((IEEE802154_MAC_DATE_REG - IEEE802154_REG_BASE) / 4) + 1)
     const static sleep_retention_entries_config_t ieee802154_mac_regs_retention[] = {
         [0] = { .config = REGDMA_LINK_CONTINUOUS_INIT(REGDMA_MODEM_IEEE802154_LINK(0x00), IEEE802154_REG_BASE, IEEE802154_REG_BASE, N_REGS_IEEE802154(), 0, 0), .owner = IEEE802154_LINK_OWNER },
     };
-    esp_err_t err = sleep_retention_entries_create(ieee802154_mac_regs_retention, ARRAY_SIZE(ieee802154_mac_regs_retention), REGDMA_LINK_PRI_IEEE802154, SLEEP_RETENTION_MODULE_802154_MAC);
+    err = sleep_retention_entries_create(ieee802154_mac_regs_retention, ARRAY_SIZE(ieee802154_mac_regs_retention), REGDMA_LINK_PRI_IEEE802154, SLEEP_RETENTION_MODULE_802154_MAC);
     ESP_RETURN_ON_ERROR(err, IEEE802154_TAG, "failed to allocate memory for ieee802154 mac retention");
     ESP_LOGD(IEEE802154_TAG, "ieee802154 mac sleep retention initialization");
+#endif
     return err;
 }
-#endif
+#endif // CONFIG_PM_ENABLE
 
 static esp_err_t ieee802154_sleep_init(void)
 {
     esp_err_t err = ESP_OK;
-#if SOC_PM_MODEM_RETENTION_BY_REGDMA && CONFIG_FREERTOS_USE_TICKLESS_IDLE
+#if CONFIG_PM_ENABLE
     sleep_retention_module_init_param_t init_param = {
         .cbs     = { .create = { .handle = ieee802154_sleep_retention_init, .arg = NULL } },
         .depends = BIT(SLEEP_RETENTION_MODULE_BT_BB) | BIT(SLEEP_RETENTION_MODULE_CLOCK_MODEM)
@@ -946,14 +949,14 @@ static esp_err_t ieee802154_sleep_init(void)
     sleep_modem_register_mac_bb_module_prepare_callback(sleep_modem_mac_bb_power_down_prepare,
                                                    sleep_modem_mac_bb_power_up_prepare);
 #endif // SOC_PM_RETENTION_HAS_CLOCK_BUG && CONFIG_MAC_BB_PD
-#endif // SOC_PM_MODEM_RETENTION_BY_REGDMA && CONFIG_FREERTOS_USE_TICKLESS_IDLE
+#endif // CONFIG_PM_ENABLE
     return err;
 }
 
 static esp_err_t ieee802154_sleep_deinit(void)
 {
     esp_err_t err = ESP_OK;
-#if SOC_PM_MODEM_RETENTION_BY_REGDMA && CONFIG_FREERTOS_USE_TICKLESS_IDLE
+#if CONFIG_PM_ENABLE
     err = sleep_retention_module_free(SLEEP_RETENTION_MODULE_802154_MAC);
     if (err == ESP_OK) {
         err = sleep_retention_module_deinit(SLEEP_RETENTION_MODULE_802154_MAC);
@@ -962,7 +965,7 @@ static esp_err_t ieee802154_sleep_deinit(void)
     sleep_modem_unregister_mac_bb_module_prepare_callback(sleep_modem_mac_bb_power_down_prepare,
                                                      sleep_modem_mac_bb_power_up_prepare);
 #endif // SOC_PM_RETENTION_HAS_CLOCK_BUG && CONFIG_MAC_BB_PD
-#endif // SOC_PM_MODEM_RETENTION_BY_REGDMA && CONFIG_FREERTOS_USE_TICKLESS_IDLE
+#endif // CONFIG_PM_ENABLE
     return err;
 }
 
