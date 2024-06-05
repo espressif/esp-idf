0fd8e6c7bd0bc120e0875c007b236e69e853ad12
David Cermak <cermak@espressif.com>
2024-06-05 08:04:44 +0200

    fix(esp_netif): Fix missing IPv6 autoconfig for PPP netifs
    Closes https://github.com/espressif/esp-idf/issues/13713

--- components/esp_netif/lwip/esp_netif_lwip_ppp.c
+++ components/esp_netif/lwip/esp_netif_lwip_ppp.c
@@ -161,7 +161,7 @@ static void on_ppp_notify_phase(ppp_pcb *pcb, u8_t phase, void *ctx)
 #endif // PPP_NOTIFY_PHASE
 
 /**
- * @brief PPP low level output callback used to transmit data using standard esp-netif interafce
+ * @brief PPP low level output callback used to transmit data using standard esp-netif interface
  *
  * @param pcb PPP control block
  * @param data Buffer to write to serial port
@@ -245,6 +245,10 @@ esp_err_t esp_netif_start_ppp(esp_netif_t *esp_netif)
         ppp_ctx->ppp->settings.lcp_echo_interval = LCP_ECHOINTERVAL;
         ppp_ctx->ppp->settings.lcp_echo_fails = LCP_MAXECHOFAILS;
     }
+#endif
+
+#if ESP_IPV6_AUTOCONFIG
+    ppp_ctx->ppp->netif->ip6_autoconfig_enabled = 1;
 #endif
 
     ESP_LOGD(TAG, "%s: Starting PPP connection: %p", __func__, ppp_ctx->ppp);
