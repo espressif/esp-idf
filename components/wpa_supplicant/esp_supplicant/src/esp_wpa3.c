298bca09ecaea4394f948dc95e83749fa2c2a829
Shreyas Sheth <shreyas.sheth@espressif.com>
2024-06-06 11:19:17 +0530

    fix(wpa_supplicant): Fix wpa3 AP crash because of dangling pointer

--- components/wpa_supplicant/esp_supplicant/src/esp_wpa3.c
+++ components/wpa_supplicant/esp_supplicant/src/esp_wpa3.c
@@ -561,6 +561,7 @@ int wpa3_hostap_auth_init(void *data)
                     &g_wpa3_hostap_task_hdl) != pdPASS) {
         wpa_printf(MSG_ERROR, "wpa3_hostap_auth_init: failed to create task");
         os_queue_delete(g_wpa3_hostap_evt_queue);
+        g_wpa3_hostap_evt_queue = NULL;
         return ESP_FAIL;
     }
 
