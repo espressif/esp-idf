82cba6e3fffd18e055930dde495597ad45ef2ffc
laokaiyao <laokaiyao@espressif.com>
2024-06-07 17:25:35 +0800

    feat(touch_sens): add example for the touch sensor v3

--- examples/system/light_sleep/main/light_sleep_example.h
+++ examples/system/light_sleep/main/light_sleep_example.h
@@ -1,14 +1,19 @@
 /*
- * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Unlicense OR CC0-1.0
  */
 
 #pragma once
 
+#include "sdkconfig.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+// TODO: [ESP32P4] add P4 when runner is ready
+#define EXAMPLE_TOUCH_LSLEEP_WAKEUP_SUPPORT (CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3)
 
 void example_wait_gpio_inactive(void);
 
@@ -18,7 +23,7 @@ esp_err_t example_register_timer_wakeup(void);
 
 esp_err_t example_register_uart_wakeup(void);
 
-#if CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
+#if EXAMPLE_TOUCH_LSLEEP_WAKEUP_SUPPORT
 void example_register_touch_wakeup(void);
 #endif
 
