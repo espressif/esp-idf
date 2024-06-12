eb24a577284ea9cba615d850628e36a5d6772b4e
Marius Vikhammer <marius.vikhammer@espressif.com>
2024-06-12 09:24:50 +0800

    feat(intr): basic interrupt/freertos support for C61

--- components/esp_hw_support/port/esp32c61/esp_cpu_intr.c
+++ components/esp_hw_support/port/esp32c61/esp_cpu_intr.c
@@ -7,7 +7,6 @@
 #include "esp_cpu.h"
 #include "esp_riscv_intr.h"
 
-//TODO: [ESP32C61] IDF-9262, inherit from C5
 void esp_cpu_intr_get_desc(int core_id, int intr_num, esp_cpu_intr_desc_t *intr_desc_ret)
 {
     /* On targets that uses CLIC as the interrupt controller, the first 16 lines (0..15) are reserved for software
