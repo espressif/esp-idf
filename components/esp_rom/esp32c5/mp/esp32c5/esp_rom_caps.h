171e0a21a14d6f9fb928787621ef4f49eafdb8b7
Marius Vikhammer <marius.vikhammer@espressif.com>
2024-06-13 16:19:32 +0800

    fix(rom): fixed esprv_int_set_threshold on C5

--- components/esp_rom/esp32c5/mp/esp32c5/esp_rom_caps.h
+++ components/esp_rom/esp32c5/mp/esp32c5/esp_rom_caps.h
@@ -28,3 +28,4 @@
 #define ESP_ROM_RAM_APP_NEEDS_MMU_INIT      (1) // ROM doesn't init cache MMU when it's a RAM APP, needs MMU hal to init
 #define ESP_ROM_HAS_VERSION                 (1) // ROM has version/eco information
 #define ESP_ROM_SUPPORT_DEEP_SLEEP_WAKEUP_STUB  (1) // ROM supports the HP core to jump to the RTC memory to execute stub code after waking up from deepsleep.
+#define ESP_ROM_CLIC_INT_THRESH_PATCH       (1) // ROM version of esprv_intc_int_set_threshold incorrectly assumes lowest MINTTHRESH is 0x1F, should be 0xF
