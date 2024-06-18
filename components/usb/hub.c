52aa6a1d4c312f053c69d69e9d59828ca46d483e
Roman Leonov <roman.leonov@espressif.com>
2024-06-18 12:06:16 +0200

    feat(ext_port): Added External Port driver
    Closes https://github.com/espressif/esp-idf/issues/12554

--- components/usb/hub.c
+++ components/usb/hub.c
@@ -20,7 +20,7 @@
 #include "usb/usb_helpers.h"
 
 #if ENABLE_USB_HUBS
-#include "ext_hub.h"
+#include "ext_port.h"
 #endif // ENABLE_USB_HUBS
 
 /*
@@ -320,6 +320,72 @@ static bool ext_hub_callback(bool in_isr, void *user_arg)
     HUB_DRIVER_EXIT_CRITICAL_SAFE();
     return p_hub_driver_obj->constant.proc_req_cb(USB_PROC_REQ_SOURCE_HUB, in_isr, p_hub_driver_obj->constant.proc_req_cb_arg);
 }
+
+static void ext_port_callback(void *user_arg)
+{
+    HUB_DRIVER_ENTER_CRITICAL();
+    p_hub_driver_obj->dynamic.flags.actions |= HUB_DRIVER_ACTION_EXT_PORT;
+    HUB_DRIVER_EXIT_CRITICAL();
+    p_hub_driver_obj->constant.proc_req_cb(USB_PROC_REQ_SOURCE_HUB, false, p_hub_driver_obj->constant.proc_req_cb_arg);
+}
+
+static void ext_port_event_callback(ext_port_event_data_t *event_data, void *arg)
+{
+    switch (event_data->event) {
+    case EXT_PORT_CONNECTED:
+        // First reset is done by ext_port logic
+        usb_speed_t port_speed;
+
+        if (ext_hub_port_get_speed(event_data->connected.ext_hub_hdl,
+                                   event_data->connected.parent_port_num,
+                                   &port_speed) != ESP_OK) {
+            goto new_ds_dev_err;
+        }
+
+        // TODO: IDF-10023 Move responsibility of parent-child tree building to Hub Driver instead of USBH
+        usb_device_info_t parent_dev_info;
+        ESP_ERROR_CHECK(usbh_dev_get_info(event_data->connected.parent_dev_hdl, &parent_dev_info));
+        if (parent_dev_info.speed == USB_SPEED_HIGH) {
+            if (port_speed != parent_dev_info.speed) {
+                ESP_LOGE(HUB_DRIVER_TAG, "Connected device is %s, transaction translator (TT) is not supported",
+                (char *[]) {
+                    "LS", "FS", "HS"
+                }[port_speed]);
+                goto new_ds_dev_err;
+            }
+        }
+
+#if (!CONFIG_USB_HOST_EXT_PORT_SUPPORT_LS)
+        if (port_speed == USB_SPEED_LOW) {
+            ESP_LOGE(HUB_DRIVER_TAG, "Connected %s-speed device, not supported",
+            (char *[]) {
+                "Low", "Full", "High"
+            }[port_speed]);
+            goto new_ds_dev_err;
+        }
+#endif // CONFIG_USB_HOST_EXT_PORT_SUPPORT_LS
+
+        if (new_dev_tree_node(event_data->connected.parent_dev_hdl, event_data->connected.parent_port_num, port_speed) != ESP_OK) {
+            ESP_LOGE(HUB_DRIVER_TAG, "Failed to add new downstream device");
+            goto new_ds_dev_err;
+        }
+        break;
+new_ds_dev_err:
+        ext_hub_port_disable(event_data->connected.ext_hub_hdl, event_data->connected.parent_port_num);
+        break;
+    case EXT_PORT_RESET_COMPLETED:
+        ESP_ERROR_CHECK(dev_tree_node_reset_completed(event_data->reset_completed.parent_dev_hdl, event_data->reset_completed.parent_port_num));
+        break;
+    case EXT_PORT_DISCONNECTED:
+        // The node could be freed by now, no need to verify the result here
+        dev_tree_node_dev_gone(event_data->disconnected.parent_dev_hdl, event_data->disconnected.parent_port_num);
+        break;
+    default:
+        // Should never occur
+        abort();
+        break;
+    }
+}
 #endif // ENABLE_USB_HUBS
 
 // ---------------------- Handlers -------------------------
@@ -479,10 +545,20 @@ esp_err_t hub_install(hub_config_t *hub_config, void **client_ret)
     }
 
 #if ENABLE_USB_HUBS
+    // Install External Port driver
+    ext_port_driver_config_t ext_port_config = {
+        .proc_req_cb = ext_port_callback,
+        .event_cb = ext_port_event_callback,
+    };
+    ret = ext_port_install(&ext_port_config);
+    if (ret != ESP_OK) {
+        goto err_ext_port;
+    }
+
     // Install External HUB driver
     ext_hub_config_t ext_hub_config = {
         .proc_req_cb = ext_hub_callback,
-        .port_driver = NULL,
+        .port_driver = ext_port_get_driver(),
     };
     ret = ext_hub_install(&ext_hub_config);
     if (ret != ESP_OK) {
@@ -536,6 +612,8 @@ err:
 #if ENABLE_USB_HUBS
     ext_hub_uninstall();
 err_ext_hub:
+    ext_port_uninstall();
+err_ext_port:
 #endif // ENABLE_USB_HUBS
     heap_caps_free(hub_driver_obj);
     return ret;
@@ -552,6 +630,7 @@ esp_err_t hub_uninstall(void)
 
 #if ENABLE_USB_HUBS
     ESP_ERROR_CHECK(ext_hub_uninstall());
+    ESP_ERROR_CHECK(ext_port_uninstall());
 #endif // ENABLE_USB_HUBS
 
     ESP_ERROR_CHECK(hcd_port_deinit(hub_driver_obj->constant.root_port_hdl));
@@ -739,10 +818,7 @@ esp_err_t hub_process(void)
     while (action_flags) {
 #if ENABLE_USB_HUBS
         if (action_flags & HUB_DRIVER_ACTION_EXT_PORT) {
-            ESP_LOGW(HUB_DRIVER_TAG, "ext_port_process() has not been implemented yet");
-            /*
             ESP_ERROR_CHECK(ext_port_process());
-            */
         }
         if (action_flags & HUB_DRIVER_ACTION_EXT_HUB) {
             ESP_ERROR_CHECK(ext_hub_process());
