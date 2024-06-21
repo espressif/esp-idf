92393685b2c5a54eff315265583f6b6b740821b2
Abhinav Kudnar <abhinav.kudnar@espressif.com>
2024-06-21 17:41:30 +0530

    fix(nimble): Nimble Error logs in case of memory overflow/failure

--- components/bt/porting/mem/bt_osi_mem.c
+++ components/bt/porting/mem/bt_osi_mem.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2015-2021 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2015-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -7,18 +7,26 @@
 #include "esp_attr.h"
 #include "esp_heap_caps.h"
 #include "sdkconfig.h"
+#include "esp_log.h"
+#include <assert.h>
 
 IRAM_ATTR void *bt_osi_mem_malloc(size_t size)
 {
+    void *mem = NULL;
 #ifdef CONFIG_BT_NIMBLE_MEM_ALLOC_MODE_INTERNAL
-    return heap_caps_malloc(size, MALLOC_CAP_INTERNAL|MALLOC_CAP_8BIT);
+    mem = heap_caps_malloc(size, MALLOC_CAP_INTERNAL|MALLOC_CAP_8BIT);
 #elif CONFIG_BT_NIMBLE_MEM_ALLOC_MODE_EXTERNAL
-    return heap_caps_malloc(size, MALLOC_CAP_SPIRAM|MALLOC_CAP_8BIT);
+    mem =  heap_caps_malloc(size, MALLOC_CAP_SPIRAM|MALLOC_CAP_8BIT);
 #elif CONFIG_BT_NIMBLE_MEM_ALLOC_MODE_IRAM_8BIT
-    return heap_caps_malloc_prefer(size, 2, MALLOC_CAP_INTERNAL|MALLOC_CAP_IRAM_8BIT, MALLOC_CAP_INTERNAL|MALLOC_CAP_8BIT);
+    mem =  heap_caps_malloc_prefer(size, 2, MALLOC_CAP_INTERNAL|MALLOC_CAP_IRAM_8BIT, MALLOC_CAP_INTERNAL|MALLOC_CAP_8BIT);
 #else
-    return malloc(size);
+    mem =  malloc(size);
 #endif
+    if(!mem){
+       ESP_LOGI("ESP_LOG_INFO","malloc failed (size %zu)",size);
+       assert(mem != NULL);
+    }
+    return mem;
 }
 
 IRAM_ATTR void *bt_osi_mem_calloc(size_t n, size_t size)
