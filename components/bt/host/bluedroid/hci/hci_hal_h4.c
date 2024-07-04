d7838290e55ecb658868dfd3f8370847213ef231
zwl <zhaoweiliang@espressif.com>
2024-07-04 21:11:23 +0800

    feat(bluetooth/controller): optimize bt hci layer code

--- components/bt/host/bluedroid/hci/hci_hal_h4.c
+++ components/bt/host/bluedroid/hci/hci_hal_h4.c
@@ -31,10 +31,6 @@
 #include "esp_bt.h"
 #include "stack/hcimsgs.h"
 
-#if SOC_ESP_NIMBLE_CONTROLLER
-#include "ble_hci_trans.h"
-#endif
-
 #if (C2H_FLOW_CONTROL_INCLUDED == TRUE)
 #include "l2c_int.h"
 #endif ///C2H_FLOW_CONTROL_INCLUDED == TRUE
@@ -621,42 +617,7 @@ static int host_recv_pkt_cb(uint8_t *data, uint16_t len)
 
     return 0;
 }
-#if SOC_ESP_NIMBLE_CONTROLLER
 
-int
-ble_hs_hci_rx_evt(uint8_t *hci_ev, void *arg)
-{
-    if(esp_bluedroid_get_status() == ESP_BLUEDROID_STATUS_UNINITIALIZED) {
-        ble_hci_trans_buf_free(hci_ev);
-        return 0;
-    }
-    uint16_t len = hci_ev[1] + 3;
-    uint8_t *data = (uint8_t *)malloc(len);
-    assert(data != NULL);
-    data[0] = 0x04;
-    memcpy(&data[1], hci_ev, len - 1);
-    ble_hci_trans_buf_free(hci_ev);
-    host_recv_pkt_cb(data, len);
-    free(data);
-    return 0;
-}
-
-
-int
-ble_hs_rx_data(struct os_mbuf *om, void *arg)
-{
-    uint16_t len = OS_MBUF_PKTHDR(om)->omp_len + 1;
-    uint8_t *data = (uint8_t *)malloc(len);
-    assert(data != NULL);
-    data[0] = 0x02;
-    os_mbuf_copydata(om, 0, len - 1, &data[1]);
-    host_recv_pkt_cb(data, len);
-    free(data);
-    os_mbuf_free_chain(om);
-    return 0;
-}
-
-#endif
 static const esp_vhci_host_callback_t vhci_host_cb = {
     .notify_host_send_available = host_send_pkt_available_cb,
     .notify_host_recv = host_recv_pkt_cb,
