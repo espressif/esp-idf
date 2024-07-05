29b028076a9bc32bbec551651762a915f12cd10b
Alexey Lapshin <alexey.lapshin@espressif.com>
2024-07-06 00:26:41 +0700

    feat(cxx): change test value of __cplusplus to 202302L

--- tools/test_apps/system/cxx_build_test/main/test_cxx_standard.cpp
+++ tools/test_apps/system/cxx_build_test/main/test_cxx_standard.cpp
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Unlicense OR CC0-1.0
  */
 /*
- * Current (2023-05-04) value for -std=gnu++23 and -std=gnu++2b.
+ * Current GCC-14.2.0 value for -std=gnu++23 and -std=gnu++2b.
  * If you change the C++ standard for IDF, you also need to change this.
  */
-static_assert(__cplusplus == 202100L);
+static_assert(__cplusplus == 202302L);
