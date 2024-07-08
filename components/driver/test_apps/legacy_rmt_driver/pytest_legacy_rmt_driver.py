610ef9c040bd5687150095b4671430f2bc3f36b4
morris <maoshengrong@espressif.com>
2024-07-08 15:46:49 +0800

    fix(rmt): turn on memory block when enable the module

--- components/driver/test_apps/legacy_rmt_driver/pytest_legacy_rmt_driver.py
+++ components/driver/test_apps/legacy_rmt_driver/pytest_legacy_rmt_driver.py
@@ -8,7 +8,7 @@ from pytest_embedded import Dut
 @pytest.mark.esp32s2
 @pytest.mark.esp32s3
 @pytest.mark.esp32c3
-# @pytest.mark.esp32c5  # TODO: [ESP32C5] IDF-10330
+@pytest.mark.esp32c5
 @pytest.mark.esp32c6
 @pytest.mark.esp32h2
 @pytest.mark.esp32p4
