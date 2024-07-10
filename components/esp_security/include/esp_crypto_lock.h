488b2a741d89ce3979703dab7cd9fb570c5b15a8
harshal.patil <harshal.patil@espressif.com>
2024-07-10 11:29:21 +0530

    change(esp_security): Move the crypto locking layer into the security component

--- components/esp_security/include/esp_crypto_lock.h
+++ components/esp_security/include/esp_crypto_lock.h
@@ -6,10 +6,13 @@
 
 #pragma once
 
+#include "soc/soc_caps.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifdef SOC_HMAC_SUPPORTED
 /**
  * @brief Acquire lock for HMAC cryptography peripheral
  *
@@ -23,7 +26,9 @@ void esp_crypto_hmac_lock_acquire(void);
  * Internally also releases the SHA peripheral, as the HMAC depends on the SHA peripheral
  */
 void esp_crypto_hmac_lock_release(void);
+#endif /* SOC_HMAC_SUPPORTED */
 
+#ifdef SOC_DIG_SIGN_SUPPORTED
 /**
  * @brief Acquire lock for DS cryptography peripheral
  *
@@ -37,7 +42,9 @@ void esp_crypto_ds_lock_acquire(void);
  * Internally also releases the HMAC (which locks SHA), AES and MPI  peripheral, as the DS depends on these peripherals
  */
 void esp_crypto_ds_lock_release(void);
+#endif /* SOC_DIG_SIGN_SUPPORTED */
 
+#if defined(SOC_SHA_SUPPORTED) && defined(SOC_AES_SUPPORTED)
 /**
  * @brief Acquire lock for the SHA and AES cryptography peripheral.
  *
@@ -49,8 +56,29 @@ void esp_crypto_sha_aes_lock_acquire(void);
  *
  */
 void esp_crypto_sha_aes_lock_release(void);
+#endif /* defined(SOC_SHA_SUPPORTED) && defined(SOC_AES_SUPPORTED) */
 
+#if defined(SOC_SHA_CRYPTO_DMA) && defined(SOC_AES_CRYPTO_DMA)
+/**
+ * This API should be used by all components which use the SHA, AES, HMAC and DS crypto hardware on the ESP32S2.
+ * They can not be used in parallel because they use the same DMA or are calling each other.
+ * E.g., HMAC uses SHA or DS uses HMAC and AES. See the ESP32S2 Technical Reference Manual for more details.
+ *
+ * Other unrelated components must not use it.
+ */
 
+/**
+ * Acquire lock for the AES and SHA cryptography peripherals, which both use the crypto DMA.
+ */
+void esp_crypto_dma_lock_acquire(void);
+
+/**
+ * Release lock for the AES and SHA cryptography peripherals, which both use the crypto DMA.
+ */
+void esp_crypto_dma_lock_release(void);
+#endif /* defined(SOC_SHA_CRYPTO_DMA) && defined(SOC_AES_CRYPTO_DMA) */
+
+#ifdef SOC_MPI_SUPPORTED
 /**
  * @brief Acquire lock for the mpi cryptography peripheral.
  *
@@ -62,8 +90,9 @@ void esp_crypto_mpi_lock_acquire(void);
  *
  */
 void esp_crypto_mpi_lock_release(void);
+#endif /* SOC_MPI_SUPPORTED */
 
-
+#ifdef SOC_ECC_SUPPORTED
 /**
  * @brief Acquire lock for the ECC cryptography peripheral.
  *
@@ -75,8 +104,9 @@ void esp_crypto_ecc_lock_acquire(void);
  *
  */
 void esp_crypto_ecc_lock_release(void);
+#endif /* SOC_ECC_SUPPORTED */
 
-
+#ifdef SOC_ECDSA_SUPPORTED
 /**
  * @brief Acquire lock for ECDSA cryptography peripheral
  *
@@ -90,7 +120,9 @@ void esp_crypto_ecdsa_lock_acquire(void);
  * Internally also releases the ECC and MPI peripheral, as the ECDSA depends on these peripherals
  */
 void esp_crypto_ecdsa_lock_release(void);
+#endif /* SOC_ECDSA_SUPPORTED */
 
+#ifdef SOC_KEY_MANAGER_SUPPORTED
 /**
  * @brief Acquire lock for Key Manager peripheral
  *
@@ -102,6 +134,7 @@ void esp_crypto_key_manager_lock_acquire(void);
  *
  */
 void esp_crypto_key_manager_lock_release(void);
+#endif /* SOC_KEY_MANAGER_SUPPORTED */
 
 #ifdef __cplusplus
 }
