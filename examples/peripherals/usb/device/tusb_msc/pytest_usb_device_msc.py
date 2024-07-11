61577616da4ec8b80c2e3a70527efcb9a09e9eff
Tomas Rezucha <tomas.rezucha@espressif.com>
2024-07-11 11:09:12 +0200

    fix(usb/test): Update MSC device test for new console

--- examples/peripherals/usb/device/tusb_msc/pytest_usb_device_msc.py
+++ examples/peripherals/usb/device/tusb_msc/pytest_usb_device_msc.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 import pytest
 from pytest_embedded import Dut
@@ -10,11 +10,6 @@ def test_usb_device_msc_example(dut: Dut) -> None:
     dut.expect('Mount storage')
     dut.expect('TinyUSB Driver installed')
     dut.expect('USB MSC initialization DONE')
-    dut.write(' help')
-    dut.expect('read')
-    dut.expect('write')
-    dut.expect('size')
-    dut.expect('expose')
-    dut.expect('status')
-    dut.write(' status')
+    dut.expect('esp32s2>')
+    dut.write('status')
     dut.expect('storage exposed over USB')
