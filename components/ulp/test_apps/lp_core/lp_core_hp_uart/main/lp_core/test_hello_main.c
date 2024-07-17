d70383422be0d59fbbfb6e46ddc4d598e2b876c3
Marius Vikhammer <marius.vikhammer@espressif.com>
2024-07-17 09:20:35 +0800

    feat(lp-core): add reduced footprint print functions

--- components/ulp/test_apps/lp_core/lp_core_hp_uart/main/lp_core/test_hello_main.c
+++ components/ulp/test_apps/lp_core/lp_core_hp_uart/main/lp_core/test_hello_main.c
@@ -8,9 +8,18 @@
 #include <stdbool.h>
 #include "ulp_lp_core_print.h"
 
+volatile int hex_value = 0x1234ABCD;
+volatile int dec_value = 56;
+
 int main(void)
 {
     lp_core_printf("Hello, World!\n");
+
+    lp_core_print_hex(hex_value);
+    lp_core_print_char('\n');
+
+    lp_core_print_dec_two_digits(dec_value);
+    lp_core_print_char('\n');
 
     return 0;
 }
