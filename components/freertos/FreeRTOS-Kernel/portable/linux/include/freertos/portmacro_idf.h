987df81e58085ae0ddb98244e6abde4a2e83637a
Sudeep Mohanty <sudeep.mohanty@espressif.com>
2024-07-23 16:19:24 +0200

    fix(freertos): Fixed memory leak issue in vTaskDeleteWithCaps()
    vTaskDeleteWithCaps() leaked memory when a task uses the API to delete
    itself. This commit adds a fix to avoid the memory leak.
    
    Closes https://github.com/espressif/esp-idf/issues/14222

--- components/freertos/FreeRTOS-Kernel/portable/linux/include/freertos/portmacro_idf.h
+++ components/freertos/FreeRTOS-Kernel/portable/linux/include/freertos/portmacro_idf.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -103,6 +103,17 @@ static inline BaseType_t xPortInIsrContext(void)
     //Just call the FreeRTOS port interface version
     return xPortCheckIfInISR();
 }
+
+static inline void vPortAssertIfInISR(void)
+{
+    /* Assert if the interrupt nesting count is > 0 */
+    configASSERT(xPortInIsrContext() == 0);
+}
+
+/**
+ * @brief Assert if in ISR context
+ */
+#define portASSERT_IF_IN_ISR() vPortAssertIfInISR()
 
 #if CONFIG_FREERTOS_ENABLE_STATIC_TASK_CLEAN_UP
 /* If enabled, users must provide an implementation of vPortCleanUpTCB() */
