7b5508b6ff8cd890f6aa6d90cfc07917fc00c298
Sudeep Mohanty <sudeep.mohanty@espressif.com>
2024-07-24 16:56:17 +0200

    fix(freertos): Incorrect assert in FreeRTOS port layer when not in ISR context
    This commit fixes an issue where in the FreeRTOS port layer would cause
    the portASSERT_IF_IN_ISR() assert check to fail even when the system is
    not in an interrupt context.

--- components/freertos/FreeRTOS-Kernel/portable/xtensa/include/freertos/portmacro.h
+++ components/freertos/FreeRTOS-Kernel/portable/xtensa/include/freertos/portmacro.h
@@ -166,12 +166,9 @@ typedef uint32_t TickType_t;
 BaseType_t xPortInIsrContext(void);
 
 /**
- * @brief Asserts if in ISR context
+ * @brief Assert if in ISR context
  *
  * - Asserts on xPortInIsrContext() internally
- *
- * @note [refactor-todo] Check if this API is still required
- * @note [refactor-todo] Check if this should be inlined
  */
 void vPortAssertIfInISR(void);
 
@@ -431,6 +428,9 @@ FORCE_INLINE_ATTR BaseType_t xPortGetCoreID(void);
 #define portSET_INTERRUPT_MASK_FROM_ISR()                   xPortSetInterruptMaskFromISR()
 #define portCLEAR_INTERRUPT_MASK_FROM_ISR(prev_level)       vPortClearInterruptMaskFromISR(prev_level)
 
+/**
+ * @brief Assert if in ISR context
+ */
 #define portASSERT_IF_IN_ISR() vPortAssertIfInISR()
 
 /**
