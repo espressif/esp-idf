61edc67100cec481152fe3cc6ce71299b8d7329c
Rahul Tank <rahul.tank@espressif.com>
2024-08-01 13:49:10 +0530

    feat(nimble): Add support to allow connection during scanning

--- examples/bluetooth/nimble/ble_phy/phy_cent/main/main.c
+++ examples/bluetooth/nimble/ble_phy/phy_cent/main/main.c
@@ -288,12 +288,14 @@ blecent_connect_if_interesting(void *disc)
         return;
     }
 
+#if !(MYNEWT_VAL(BLE_HOST_ALLOW_CONNECT_WITH_SCAN))
     /* Scanning must be stopped before a connection can be initiated. */
     rc = ble_gap_disc_cancel();
     if (rc != 0) {
         MODLOG_DFLT(DEBUG, "Failed to cancel scan; rc=%d\n", rc);
         return;
     }
+#endif
 
     /* Figure out address to use for connect (no privacy for now) */
     rc = ble_hs_id_infer_auto(0, &own_addr_type);
@@ -438,7 +440,7 @@ blecent_gap_event(struct ble_gap_event *event, void *arg)
         return 0;
 
     case BLE_GAP_EVENT_EXT_DISC:
-        /* An advertisment report was received during GAP discovery. */
+        /* An advertisement report was received during GAP discovery. */
         ext_print_adv_report(&event->disc);
 
         blecent_connect_if_interesting(&event->disc);
