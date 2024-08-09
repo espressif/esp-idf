d3ab90ba247bb7d484936b869d1f90fc32e9dcc7
Cristian Funes <cristianfunes79@gmail.com>
2024-08-09 11:50:01 -0300

    fix(pthread): Fix cxx pthread example build for linux platform
    Closes https://github.com/espressif/esp-idf/pull/14339

--- tools/test_apps/linux_compatible/generic_build_test/main/generic_build_test.c
+++ tools/test_apps/linux_compatible/generic_build_test/main/generic_build_test.c
@@ -8,8 +8,13 @@
 #include <stddef.h>
 #include "inttypes_ext.h"
 
+#include "esp_pthread.h"
+
 void app_main(void)
 {
     size_t size = 47;
     printf("size is: %" PRIuSIZE "\n", size); // test IDF's PRIuSIZE
+
+    esp_pthread_cfg_t pthread_config = esp_pthread_get_default_config();
+    (void)pthread_config;
 }
