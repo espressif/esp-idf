00c144aa995352ebd79a8681fa0e0aeb4bc782ad
Xiao Xufeng <xiaoxufeng@espressif.com>
2024-08-12 11:45:16 +0800

    ci(mmu): add unicore test

--- tools/test_apps/system/unicore_bootloader/pytest_unicore_bootloader.py
+++ tools/test_apps/system/unicore_bootloader/pytest_unicore_bootloader.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 import os
+import re
 
 import pytest
 from artifacts_handler import ArtifactType
@@ -12,13 +13,16 @@ from pytest_embedded import Dut
 @pytest.mark.esp32s3
 @pytest.mark.esp32p4
 @pytest.mark.generic
-@pytest.mark.parametrize('config', ['multicore'], indirect=True)
-def test_multicore_app_and_unicore_bootloader(dut: Dut, app_downloader) -> None:  # type: ignore
+@pytest.mark.parametrize('config', ['multicore', 'multicore_psram'], indirect=True)
+def test_multicore_app_and_unicore_bootloader(dut: Dut, app_downloader, config) -> None:  # type: ignore
     dut.expect('Multicore bootloader')
     dut.expect('Multicore app')
     dut.expect('App is running')
 
-    path_to_unicore_build = os.path.join(dut.app.app_path, f'build_{dut.target}_unicore')
+    assert 'multicore' in config
+    app_config = config.replace('multicore', 'unicore')
+
+    path_to_unicore_build = os.path.join(dut.app.app_path, f'build_{dut.target}_{app_config}')
     if app_downloader:
         app_downloader.download_app(
             os.path.relpath(path_to_unicore_build, IDF_PATH), ArtifactType.BUILD_DIR_WITHOUT_MAP_AND_ELF_FILES
@@ -27,20 +31,26 @@ def test_multicore_app_and_unicore_bootloader(dut: Dut, app_downloader) -> None:
     dut.serial.bootloader_flash(path_to_unicore_build)
     dut.expect('Unicore bootloader')
     dut.expect('Multicore app')
+    if 'psram' in config:
+        dut.expect(re.compile(r'Adding pool of \d+K of PSRAM memory to heap allocator'))
     dut.expect('App is running')
+    dut.expect('NVS test done\n')
 
 
 @pytest.mark.esp32
 @pytest.mark.esp32s3
 @pytest.mark.esp32p4
 @pytest.mark.generic
-@pytest.mark.parametrize('config', ['unicore'], indirect=True)
-def test_unicore_app_and_multicore_bootloader(dut: Dut, app_downloader) -> None:  # type: ignore
+@pytest.mark.parametrize('config', ['unicore', 'unicore_psram'], indirect=True)
+def test_unicore_app_and_multicore_bootloader(dut: Dut, app_downloader, config) -> None:  # type: ignore
     dut.expect('Unicore bootloader')
     dut.expect('Unicore app')
     dut.expect('App is running')
 
-    path_to_multicore_build = os.path.join(dut.app.app_path, f'build_{dut.target}_multicore')
+    assert 'unicore' in config
+    app_config = config.replace('unicore', 'multicore')
+
+    path_to_multicore_build = os.path.join(dut.app.app_path, f'build_{dut.target}_{app_config}')
     if app_downloader:
         app_downloader.download_app(
             os.path.relpath(path_to_multicore_build, IDF_PATH), ArtifactType.BUILD_DIR_WITHOUT_MAP_AND_ELF_FILES
@@ -49,4 +59,7 @@ def test_unicore_app_and_multicore_bootloader(dut: Dut, app_downloader) -> None:
     dut.serial.bootloader_flash(path_to_multicore_build)
     dut.expect('Multicore bootloader')
     dut.expect('Unicore app')
+    if 'psram' in config:
+        dut.expect(re.compile(r'Adding pool of \d+K of PSRAM memory to heap allocator'))
     dut.expect('App is running')
+    dut.expect('NVS test done\n')
