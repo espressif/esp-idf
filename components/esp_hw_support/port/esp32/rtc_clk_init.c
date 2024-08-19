7b510049fbc330b8bb1a9bf5170eec942a4360ca
Song Ruo Jing <songruojing@espressif.com>
2024-08-19 15:50:41 +0800

    fix(ci): esp32 build failure due to bootloader size too large

--- components/esp_hw_support/port/esp32/rtc_clk_init.c
+++ components/esp_hw_support/port/esp32/rtc_clk_init.c
@@ -81,7 +81,7 @@ void rtc_clk_init(rtc_clk_config_t cfg)
             /* Not set yet, estimate XTAL frequency based on RTC_FAST_CLK */
             xtal_freq = rtc_clk_xtal_freq_estimate();
             if (xtal_freq == SOC_XTAL_FREQ_AUTO) {
-                ESP_HW_LOGW(TAG, "Can't estimate XTAL frequency, assuming 26MHz");
+                ESP_HW_LOGW(TAG, "Can't estimate XTAL freq, assuming 26MHz");
                 xtal_freq = SOC_XTAL_FREQ_26M;
             }
         }
@@ -93,7 +93,7 @@ void rtc_clk_init(rtc_clk_config_t cfg)
 
         soc_xtal_freq_t est_xtal_freq = rtc_clk_xtal_freq_estimate();
         if (est_xtal_freq != configured_xtal_freq) {
-            ESP_HW_LOGW(TAG, "Possibly invalid CONFIG_XTAL_FREQ setting (%dMHz). Detected %d MHz.",
+            ESP_HW_LOGW(TAG, "Possibly invalid CONFIG_XTAL_FREQ setting (%dMHz). Detected %dMHz.",
                     configured_xtal_freq, est_xtal_freq);
         }
     }
@@ -108,7 +108,7 @@ void rtc_clk_init(rtc_clk_config_t cfg)
 
     bool res = rtc_clk_cpu_freq_mhz_to_config(cfg.cpu_freq_mhz, &new_config);
     if (!res) {
-        ESP_HW_LOGE(TAG, "invalid CPU frequency value");
+        ESP_HW_LOGE(TAG, "invalid CPU freq value");
         abort();
     }
     rtc_clk_cpu_freq_set_config(&new_config);
@@ -163,18 +163,18 @@ static soc_xtal_freq_t rtc_clk_xtal_freq_estimate(void)
         xtal_freq = SOC_XTAL_FREQ_26M;
         break;
     case 32 ... 33:
-        ESP_HW_LOGW(TAG, "Potentially bogus XTAL frequency: %"PRIu32" MHz, guessing 26 MHz", freq_mhz);
+        ESP_HW_LOGW(TAG, "Potential bogus XTAL freq: %"PRIu32"MHz, guessing 26MHz", freq_mhz);
         xtal_freq = SOC_XTAL_FREQ_26M;
         break;
     case 34 ... 35:
-        ESP_HW_LOGW(TAG, "Potentially bogus XTAL frequency: %"PRIu32" MHz, guessing 40 MHz", freq_mhz);
+        ESP_HW_LOGW(TAG, "Potential bogus XTAL freq: %"PRIu32"MHz, guessing 40MHz", freq_mhz);
         xtal_freq = SOC_XTAL_FREQ_40M;
         break;
     case 36 ... 45:
         xtal_freq = SOC_XTAL_FREQ_40M;
         break;
     default:
-        ESP_HW_LOGW(TAG, "Bogus XTAL frequency: %"PRIu32" MHz", freq_mhz);
+        ESP_HW_LOGW(TAG, "Bogus XTAL freq: %"PRIu32"MHz", freq_mhz);
         xtal_freq = SOC_XTAL_FREQ_AUTO;
         break;
     }
--- components/esp_rom/patches/esp_rom_gpio.c
+++ components/esp_rom/patches/esp_rom_gpio.c
@@ -12,7 +12,7 @@
 #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
 // On such targets, the ROM code for this function enabled output for the pad first, and then connected the signal
 // This could result in an undesired glitch at the pad
-IRAM_ATTR void esp_rom_gpio_connect_out_signal(uint32_t gpio_num, uint32_t signal_idx, bool out_inv, bool oen_inv)
+IRAM_ATTR __attribute__((optimize("-Os"))) void esp_rom_gpio_connect_out_signal(uint32_t gpio_num, uint32_t signal_idx, bool out_inv, bool oen_inv)
 {
     uint32_t value = signal_idx << GPIO_FUNC0_OUT_SEL_S;
     if (out_inv) {
