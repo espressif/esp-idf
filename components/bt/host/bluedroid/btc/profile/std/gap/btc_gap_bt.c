fa11f9c7b35fc0513e391257d58ccbc4efc0edb7
chenqingqing <chenqingqing@espressif.com>
2024-08-19 17:26:00 +0800

    fix(bt): Add a parameter in the two least significant bits of Class of Device

--- components/bt/host/bluedroid/btc/profile/std/gap/btc_gap_bt.c
+++ components/bt/host/bluedroid/btc/profile/std/gap/btc_gap_bt.c
@@ -585,8 +585,8 @@ static void btc_gap_bt_set_cod(btc_gap_bt_args_t *arg)
 {
     tBTA_UTL_COD p_cod;
     esp_bt_cod_t *cod = &(arg->set_cod.cod);
+    p_cod.reserved_2 = cod->reserved_2;
     p_cod.minor = cod->minor << 2;
-    p_cod.minor |= cod->reserved_2;
     p_cod.major = cod->major;
     p_cod.service = cod->service << 5;
     bool ret = utl_set_device_class(&p_cod, arg->set_cod.mode);
@@ -603,8 +603,8 @@ esp_err_t btc_gap_bt_get_cod(esp_bt_cod_t *cod)
         BTC_TRACE_ERROR("%s get class of device failed!",__func__);
         return ESP_BT_STATUS_FAIL;
     }
+    cod->reserved_2 = p_cod.reserved_2;
     cod->minor = p_cod.minor >> 2;
-    cod->reserved_2 = p_cod.minor & 0x03;
     cod->major = p_cod.major;
     cod->service = p_cod.service >> 5;
     return ESP_BT_STATUS_SUCCESS;
