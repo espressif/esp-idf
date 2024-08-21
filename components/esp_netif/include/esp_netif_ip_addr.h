f371940992bf0fe0e1e273b8d37edaf92fa051cf
snake-4 <18491360+snake-4@users.noreply.github.com>
2024-08-21 05:04:09 +0200

    fix(esp_netif): Fixed initializer order of ESP_IP macros for C++

--- components/esp_netif/include/esp_netif_ip_addr.h
+++ components/esp_netif/include/esp_netif_ip_addr.h
@@ -79,8 +79,8 @@ extern "C" {
 
 #define ESP_IP4TOADDR(a,b,c,d) esp_netif_htonl(ESP_IP4TOUINT32(a, b, c, d))
 
-#define ESP_IP4ADDR_INIT(a, b, c, d)  { .type = ESP_IPADDR_TYPE_V4, .u_addr = { .ip4 = { .addr = ESP_IP4TOADDR(a, b, c, d) }}}
-#define ESP_IP6ADDR_INIT(a, b, c, d)  { .type = ESP_IPADDR_TYPE_V6, .u_addr = { .ip6 = { .addr = { a, b, c, d }, .zone = 0 }}}
+#define ESP_IP4ADDR_INIT(a, b, c, d)  { .u_addr = { .ip4 = { .addr = ESP_IP4TOADDR(a, b, c, d) }}, .type = ESP_IPADDR_TYPE_V4 }
+#define ESP_IP6ADDR_INIT(a, b, c, d)  { .u_addr = { .ip6 = { .addr = { a, b, c, d }, .zone = 0 }}, .type = ESP_IPADDR_TYPE_V6 }
 
 #ifndef IP4ADDR_STRLEN_MAX
 #define IP4ADDR_STRLEN_MAX  16
