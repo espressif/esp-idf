06366353d862e38eae1aa4cbf9f1781eed62767e
zhangbowen <zhangbowen@espressif.com>
2024-09-02 11:18:40 +0800

    feat(bt/bluedroid): Added BLE eddystone sender example

--- examples/bluetooth/bluedroid/ble/ble_eddystone_sender/main/esp_eddystone_protocol.h
+++ examples/bluetooth/bluedroid/ble/ble_eddystone_sender/main/esp_eddystone_protocol.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Unlicense OR CC0-1.0
  */
@@ -38,7 +38,29 @@ EDDYSTONE_TLM_TEMPERATURE_LEN + EDDYSTONE_TLM_ADV_COUNT_LEN + EDDYSTONE_TLM_TIME
 #define EDDYSTONE_URL_TX_POWER_LEN      1
 
 
-/* Eddystone UID frame */
+/****************** Eddystone UID Frame **************
+Byte offset	    Field	       Description
+   0	      Frame Type	   Value = 0x00
+   1	     Ranging Data	   Calibrated Tx power at 0 m
+   2	       NID[0]	       10-byte Namespace
+   3	       NID[1]
+   4	       NID[2]
+   5	       NID[3]
+   6	       NID[4]
+   7	       NID[5]
+   8	       NID[6]
+   9	       NID[7]
+   10	       NID[8]
+   11	       NID[9]
+   12	       BID[0]	       6-byte Instance
+   13	       BID[1]
+   14	       BID[2]
+   15	       BID[3]
+   16	       BID[4]
+   17	       BID[5]
+   18	        RFU	           Reserved for future use, must be0x00
+   19	        RFU	           Reserved for future use, must be0x00
+*********************************************/
 typedef struct {
     int8_t    ranging_data;     /*<! calibrated Tx power at 0m */
     uint8_t   namespace_id[10];
@@ -46,20 +68,44 @@ typedef struct {
     uint8_t   reserved[2];
 } __attribute__((packed))esp_eddystone_uid_t;
 
-/* Eddystone URL frame */
+/****************** Eddystone URL Frame *************
+Frame Specification
+ Byte offset	 Field	       Description
+    0	       Frame Type	    Value = 0x10
+    1	        TX Power	 Calibrated Tx power at 0 m
+    2	       URL Scheme	   Encoded Scheme Prefix
+    3+	       Encoded URL	    Length 1-17
+*******************************************************/
 typedef struct {
     int8_t    tx_power;         /*<! calibrated Tx power at 0m */
     uint8_t   url_scheme;       /*<! encoded scheme prefix */
     uint8_t   encoded_url[0];   /*<! length 1-17 */
 } __attribute__((packed))esp_eddystone_url_t;
 
-/* Eddystone TLM frame */
+/****************** Eddystone TLM Frame ***************
+Unencrypted TLM Frame Specification
+Byte offset	       Field	     Description
+    0	          Frame Type	 Value = 0x20
+    1	           Version	     TLM version, value = 0x00
+    2	           VBATT[0]	     Battery voltage, 1 mV/bit
+    3	           VBATT[1]
+    4	           TEMP[0]	     Beacon temperature
+    5	           TEMP[1]
+    6	          ADV_CNT[0]	 Advertising PDU count
+    7	          ADV_CNT[1]
+    8	          ADV_CNT[2]
+    9	          ADV_CNT[3]
+    10	          SEC_CNT[0]	 Time since power-on or reboot
+    11	          SEC_CNT[1]
+    12	          SEC_CNT[2]
+    13	          SEC_CNT[3]
+************************************************/
 typedef struct {
     uint8_t    version;        /*<! TLM version,0x00 for now */
     uint16_t   batt;           /*<! battery voltage, 1mV/bit */
     uint16_t   temp;           /*<! beacon temperature */
     uint32_t   adv_count;      /*<! adv pdu count since power-on or reboot */
-    uint32_t   time;           /*<! time sence power-on or reboot, a 0.1 second resolution counter */
+    uint32_t   time;           /*<! time since power-on or reboot, a 0.1 second resolution counter */
 } __attribute__((packed)) esp_eddystone_tlm_t;
 
 /*  AD Structure of flags */
