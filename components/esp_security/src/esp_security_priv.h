e1cd5b909e61214df029a80692e937bb9c3cc11a
harshal.patil <harshal.patil@espressif.com>
2024-09-04 13:22:07 +0530

    fix(esp_security): Fix build failure when dpa protection at startup is disabled

--- components/esp_security/src/esp_security_priv.h
+++ components/esp_security/src/esp_security_priv.h
@@ -5,7 +5,10 @@
  */
 
 #pragma once
+#include "sdkconfig.h"
 
 /* Private interface file */
 
+#if CONFIG_ESP_CRYPTO_DPA_PROTECTION_AT_STARTUP
 void esp_crypto_dpa_protection_startup(void);
+#endif
