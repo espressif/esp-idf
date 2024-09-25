7a214c1fab47517f9b55feacf7405a0f31cd0443
Ivan Grokhotkov <ivan@espressif.com>
2024-09-25 17:35:00 +0200

    feat(newlib): add dummy implementations of pathconf, chmod, dirfd
    Closes https://github.com/espressif/esp-idf/issues/14174

--- components/newlib/sysconf.c
+++ components/newlib/sysconf.c
@@ -1,12 +1,17 @@
 /*
- * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <unistd.h>
 #include <errno.h>
+#include <limits.h>
+#include "esp_err.h"
+#include "esp_log.h"
 #include "sdkconfig.h"
+
+static const char *TAG = "sysconf";
 
 #ifdef CONFIG_FREERTOS_UNICORE
 #define CPU_NUM 1
@@ -24,4 +29,25 @@ long sysconf(int arg)
         errno = EINVAL;
         return -1;
     }
+}
+
+// pathconf
+long fpathconf(int fildes, int name)
+{
+    if (name == _PC_PATH_MAX) {
+        return PATH_MAX;
+    }
+    ESP_LOGW(TAG, "fpathconf: unsupported name %d", name);
+    errno = EINVAL;
+    return -1;
+}
+
+long pathconf(const char *path, int name)
+{
+    if (name == _PC_PATH_MAX) {
+        return PATH_MAX;
+    }
+    ESP_LOGW(TAG, "pathconf: unsupported name %d", name);
+    errno = EINVAL;
+    return -1;
 }
