fa0c7636a515aba96212cb5e6ae60af9de5fa28d
wanckl <wanlei@espressif.com>
2024-09-27 11:41:59 +0800

    test(driver_i2s): p4 and c5 enable multi dut test

--- components/esp_driver_i2s/test_apps/i2s_multi_dev/pytest_i2s_multi_dev.py
+++ components/esp_driver_i2s/test_apps/i2s_multi_dev/pytest_i2s_multi_dev.py
@@ -5,9 +5,10 @@ import pytest
 
 @pytest.mark.esp32s3
 @pytest.mark.esp32c3
-# @pytest.mark.esp32c5  # TODO: [ESP32C5] IDF- 10321
+@pytest.mark.esp32c5
 @pytest.mark.esp32c6
 @pytest.mark.esp32h2
+@pytest.mark.esp32p4
 @pytest.mark.generic_multi_device
 @pytest.mark.parametrize('count', [
     2,
