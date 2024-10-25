4814f062833a8fbb7e290f319e7410a9f6dd6be6
Tomáš Rohlínek <tomas.rohlinek@espressif.com>
2024-10-25 12:23:32 +0200

    feat(storage/vfs): refactor VFS calls to multiple files

--- components/vfs/private_include/esp_vfs_private.h
+++ components/vfs/private_include/esp_vfs_private.h
@@ -18,6 +18,14 @@ extern "C" {
 #define VFS_MALLOC_FLAGS MALLOC_CAP_DEFAULT
 #endif
 
+#ifdef CONFIG_IDF_TARGET_LINUX
+typedef uint16_t local_fd_t;
+#else
+typedef uint8_t local_fd_t;
+#endif
+
+typedef int8_t vfs_index_t;
+
 typedef struct vfs_entry_ {
     int flags;                   /*!< ESP_VFS_FLAG_CONTEXT_PTR and/or ESP_VFS_FLAG_READONLY_FS or ESP_VFS_FLAG_DEFAULT */
     const esp_vfs_fs_ops_t *vfs; /*!< contains pointers to VFS functions */
@@ -26,6 +34,22 @@ typedef struct vfs_entry_ {
     size_t path_prefix_len;      /*!< micro-optimization to avoid doing extra strlen, contains length of the string, not the size of path_prefix array */
     const char path_prefix[]; /*!< path prefix mapped to this VFS */
 } vfs_entry_t;
+
+typedef struct {
+    bool permanent :1;
+    bool has_pending_close :1;
+    bool has_pending_select :1;
+    uint8_t _reserved :5;
+    vfs_index_t vfs_index;
+    local_fd_t local_fd;
+} fd_table_t;
+
+typedef struct {
+    bool isset; // none or at least one bit is set in the following 3 fd sets
+    fd_set readfds;
+    fd_set writefds;
+    fd_set errorfds;
+} fds_triple_t;
 
 /**
  * Register a virtual filesystem.
@@ -72,6 +96,31 @@ const vfs_entry_t *get_vfs_for_path(const char *path);
  * @return Pointer to the `vfs_entry_t` corresponding to the given path, which cannot be NULL.
  */
 const vfs_entry_t *get_vfs_for_index(int index);
+
+const char* translate_path(const vfs_entry_t* vfs, const char* src_path);
+
+const vfs_entry_t* get_vfs_for_path(const char* path);
+
+const vfs_entry_t *get_vfs_for_fd(int fd);
+
+int register_fd(int vfs_index, int local_fd, bool permanent);
+
+void unregister_fd(int fd);
+
+int get_local_fd(const vfs_entry_t *vfs, int fd);
+
+const fd_table_t *get_fd_entry(int fd);
+
+size_t get_vfs_count(void);
+
+void close_pending(int nfds);
+
+static inline bool esp_vfs_safe_fd_isset(int fd, const fd_set *fds)
+{
+    return fds && FD_ISSET(fd, fds);
+}
+
+fd_table_t start_select(int fd, fd_set *errorfds);
 
 #ifdef __cplusplus
 }
