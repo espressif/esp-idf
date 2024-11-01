035f2f828092617ea216fb1a4f6357a4a2621567
laokaiyao <laokaiyao@espressif.com>
2024-11-01 11:18:59 +0800

    ci(parlio_rx): enable logic analyzer example test

---
+++ examples/peripherals/parlio/parlio_rx/logic_analyzer/pytest_logic_analyzer.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: CC0-1.0
+import pytest
+from pytest_embedded import Dut
+
+
+@pytest.mark.esp32c6
+@pytest.mark.esp32h2
+@pytest.mark.esp32p4
+@pytest.mark.generic
+@pytest.mark.parametrize(
+    'config',
+    [
+        'flash_stream',
+    ],
+    indirect=True,
+)
+def test_logic_analyzer_flash_stream(dut: Dut) -> None:
+    dut.expect(r'flash_fat: Probe data partition base addr: \w+ size: \w+')
+    dut.expect(r'flash_fat: flash FATFS mounted')
+    dut.expect(r'esp_probe: Dump data size reached the max dump size')
+    dut.expect(r'example: Probe finished! [0-9]+ \(\w+\) bytes dumped')
