48a49c815411a9f829ebe4a5f68166069a21f48c
Alexey Lapshin <alexey.lapshin@espressif.com>
2024-11-06 17:01:33 +0700

    feat(debugging): move gdbinit generation to CMake
    This feature is useful for 3rd-party software to run GDB with predefined
    options that described in project_description.json file
    
    allow to pass custom options to "idf.py gdb":
    
      --gdb-commands: command line arguments for gdb. (without changes)
      -ex: pass command to gdb.
      -x: pass gdbinit file to gdb. Alias for old --gdbinit command

---
+++ tools/cmake/gdbinit.cmake
@@ -0,0 +1,79 @@
+# __generate_gdbinit
+# Prepares gdbinit files to pass into the debugger.
+
+function(__generate_gdbinit)
+    # Define static gdbinit commands
+    if(CONFIG_IDF_TARGET_LINUX)
+        set(gdbinit_connect
+            "# Run the application and stop on app_main()\n"
+            "break app_main\n"
+            "run\n")
+    else()
+        set(gdbinit_connect
+            "# Connect to the default openocd-esp port and stop on app_main()\n"
+            "set remotetimeout 10\n"
+            "target remote :3333\n"
+            "monitor reset halt\n"
+            "maintenance flush register-cache\n"
+            "thbreak app_main\n"
+            "continue\n")
+    endif()
+
+    set(gdbinit_py_extensions
+        "# Add Python GDB extensions\n"
+        "python\n"
+        "try:\n"
+        "    import freertos_gdb\n"
+        "except ModuleNotFoundError:\n"
+        "    print('warning: python extension \"freertos_gdb\" not found.')\n"
+        "end\n")
+
+    # Define paths
+    set(gdbinit_dir ${BUILD_DIR}/gdbinit)
+    set(gdbinit_rom_in_path ${gdbinit_dir}/rom.gdbinit.in)
+    set(gdbinit_rom_path ${gdbinit_dir}/rom.gdbinit)
+    set(symbols_gdbinit_path ${gdbinit_dir}/symbols)
+    set(py_extensions_gdbinit_path ${gdbinit_dir}/py_extensions)
+    set(connect_gdbinit_path ${gdbinit_dir}/connect)
+    idf_build_get_property(PROJECT_EXECUTABLE EXECUTABLE)
+    set(application_elf ${BUILD_DIR}/${PROJECT_EXECUTABLE})
+    file(TO_CMAKE_PATH $ENV{ESP_ROM_ELF_DIR} ESP_ROM_ELF_DIR)
+
+    file(MAKE_DIRECTORY ${gdbinit_dir})
+
+    # Get ROM ELFs gdbinit part
+    if(CONFIG_IDF_TARGET_LINUX)
+        set(rom_symbols)
+    else()
+        execute_process(
+            COMMAND python "${idf_path}/components/esp_rom/gen_gdbinit.py" ${IDF_TARGET}
+            OUTPUT_VARIABLE rom_symbols
+            RESULT_VARIABLE result
+        )
+        if(NOT result EQUAL 0)
+            set(rom_symbols)
+            message(WARNING "Error while generating esp_rom gdbinit")
+        endif()
+    endif()
+
+    # Check if bootloader ELF is defined and set symbol-file accordingly
+    if(DEFINED BOOTLOADER_ELF_FILE)
+        set(add_bootloader_symbols "  add-symbol-file ${BOOTLOADER_ELF_FILE}")
+    else()
+        set(add_bootloader_symbols "  # Bootloader elf was not found")
+    endif()
+
+    # Configure and generate files
+    configure_file(${idf_path}/tools/cmake/symbols.gdbinit.in ${symbols_gdbinit_path})
+    file(WRITE ${py_extensions_gdbinit_path} ${gdbinit_py_extensions})
+    file(WRITE ${connect_gdbinit_path} ${gdbinit_connect})
+
+    # TODO IDF-11667
+    file(WRITE ${gdbinit_dir}/gdbinit "source ${symbols_gdbinit_path}\n")
+    file(APPEND ${gdbinit_dir}/gdbinit "source ${connect_gdbinit_path}\n")
+
+    # NOTE: prefix_map gbinit file generates by prefix_map.cmake file
+    idf_build_set_property(GDBINIT_FILES_SYMBOLS ${symbols_gdbinit_path})
+    idf_build_set_property(GDBINIT_FILES_PY_EXTENSIONS ${py_extensions_gdbinit_path})
+    idf_build_set_property(GDBINIT_FILES_CONNECT ${connect_gdbinit_path})
+endfunction()
--- tools/cmake/prefix_map.cmake
+++ tools/cmake/prefix_map.cmake
@@ -7,6 +7,8 @@
 # remap the substituted paths back to the real paths in the filesystem.
 function(__generate_prefix_map compile_options_var)
     set(compile_options)
+    set(gdbinit_dir ${BUILD_DIR}/gdbinit)
+    set(gdbinit_path "${gdbinit_dir}/prefix_map")
     idf_build_get_property(idf_path IDF_PATH)
     idf_build_get_property(build_components BUILD_COMPONENTS)
 
@@ -44,11 +46,15 @@ function(__generate_prefix_map compile_options_var)
         list(APPEND compile_options "-fdebug-prefix-map=${compiler_sysroot}=/TOOLCHAIN")
         string(APPEND gdbinit_file_lines "set substitute-path /TOOLCHAIN ${compiler_sysroot}\n")
 
-        # Write the final gdbinit file
-        set(gdbinit_path "${BUILD_DIR}/prefix_map_gdbinit")
-        file(WRITE "${gdbinit_path}" "${gdbinit_file_lines}")
+        file(WRITE "${BUILD_DIR}/prefix_map_gdbinit" "${gdbinit_file_lines}")  # TODO IDF-11667
         idf_build_set_property(DEBUG_PREFIX_MAP_GDBINIT "${gdbinit_path}")
+    else()
+        set(gdbinit_file_lines "# There is no prefix map defined for the project.\n")
     endif()
+    # Write prefix_map_gdbinit file even it is empty.
+    file(MAKE_DIRECTORY ${gdbinit_dir})
+    file(WRITE "${gdbinit_path}" "${gdbinit_file_lines}")
+    idf_build_set_property(GDBINIT_FILES_PREFIX_MAP "${gdbinit_path}")
 
     set(${compile_options_var} ${compile_options} PARENT_SCOPE)
 endfunction()
