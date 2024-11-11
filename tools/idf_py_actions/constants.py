64bbb53b8f42c192ca57642ae422bc93e7685939
gaoxu <gaoxu@espressif.com>
2024-11-11 12:01:39 +0800

    feat(esp32h21): introduce target esp32h21(stage 1)

--- tools/idf_py_actions/constants.py
+++ tools/idf_py_actions/constants.py
@@ -34,7 +34,7 @@ if os.name != 'nt':
 URL_TO_DOC = 'https://docs.espressif.com/projects/esp-idf'
 
 SUPPORTED_TARGETS = ['esp32', 'esp32s2', 'esp32c3', 'esp32s3', 'esp32c2', 'esp32c6', 'esp32h2', 'esp32p4']
-PREVIEW_TARGETS = ['linux', 'esp32c5', 'esp32c61']
+PREVIEW_TARGETS = ['linux', 'esp32c5', 'esp32c61', 'esp32h21']
 
 OPENOCD_TAGET_CONFIG_DEFAULT = '-f interface/ftdi/esp32_devkitj_v1.cfg -f target/{target}.cfg'
 OPENOCD_TAGET_CONFIG: Dict[str, str] = {
