966f61ef6e4bafbb67c494490dbccec328d60c32
Alexey Lapshin <alexey.lapshin@espressif.com>
2024-12-09 12:45:23 +0700

    fix(spi): fix deprecation warnings

--- components/hal/esp32s2/include/hal/spi_types.h
+++ components/hal/esp32s2/include/hal/spi_types.h
@@ -12,9 +12,9 @@
 extern "C" {
 #endif
 
-#define SPI_HOST    _Pragma ("GCC warning \"SPI_HOST is deprecated in favor of SPI1_HOST\"")  SPI1_HOST
-#define FSPI_HOST   _Pragma ("GCC warning \"FSPI_HOST is deprecated in favor of SPI2_HOST\"") SPI2_HOST
-#define HSPI_HOST   _Pragma ("GCC warning \"HSPI_HOST is deprecated in favor of SPI3_HOST\"") SPI3_HOST
+#define SPI_HOST  SPI1_HOST
+#define FSPI_HOST SPI2_HOST
+#define HSPI_HOST SPI3_HOST
 
 #ifdef __cplusplus
 }
