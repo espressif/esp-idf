632a7e0a3640a86b1b94b4a25befabdadb804c36
Xu Si Yu <xusiyu@espressif.com>
2024-12-12 20:29:34 +0800

    feat(15.4): support setting 15.4 txrx pti when coex is enabled

--- components/esp_coex/include/esp_coex_i154.h
+++ components/esp_coex/include/esp_coex_i154.h
@@ -1,12 +1,11 @@
 /*
- * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2023-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 #ifndef __COEXIST_I154_H__
 #define __COEXIST_I154_H__
 
-#ifdef CONFIG_SOC_IEEE802154_SUPPORTED
 typedef enum {
     IEEE802154_HIGH = 1,
     IEEE802154_MIDDLE,
@@ -15,11 +14,16 @@ typedef enum {
     IEEE802154_EVENT_MAX,
 } ieee802154_coex_event_t;
 
+typedef struct {
+    ieee802154_coex_event_t idle;
+    ieee802154_coex_event_t txrx;
+    ieee802154_coex_event_t txrx_at;
+} esp_ieee802154_coex_config_t;
+
 void esp_coex_ieee802154_txrx_pti_set(ieee802154_coex_event_t event);
 void esp_coex_ieee802154_ack_pti_set(ieee802154_coex_event_t event);
 void esp_coex_ieee802154_coex_break_notify(void);
 void esp_coex_ieee802154_extcoex_tx_stage(void);
 void esp_coex_ieee802154_extcoex_rx_stage(void);
-#endif
 
 #endif
