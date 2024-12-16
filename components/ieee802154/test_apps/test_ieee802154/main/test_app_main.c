9e392786775c4dbc6a7f1f107e60c4c15ce02a37
Tan Yan Quan <tanyanquan@espressif.com>
2024-12-16 15:01:56 +0800

    feat(802.15.4): remove unnecessary includes in ieee802154 test case

--- components/ieee802154/test_apps/test_ieee802154/main/test_app_main.c
+++ components/ieee802154/test_apps/test_ieee802154/main/test_app_main.c
@@ -15,8 +15,6 @@
 #include "esp_ieee802154.h"
 #include "esp_phy_init.h"
 #include "ieee802154_cmd.h"
-#include "cmd_system.h"
-#include "ieee802154_debug.h"
 
 #define PROMPT_STR "ieee802154"
 
@@ -46,10 +44,6 @@ void app_main(void)
     /* Register commands */
     esp_console_register_help_command();
     register_ieee802154_cmd();
-    register_system_common();
-#if CONFIG_IEEE802154_DEBUG
-    register_ieee802154_debug_cmd();
-#endif
 
     esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
