f870a03223f001bdad43fc4faeaca91461dbc1ca
Guillaume Souchere <guillaume.souchere@espressif.com>
2024-12-17 11:46:58 +0100

    fix(esp_event): Fix event loop profiling in handler_execute function
    handler_execute function is looking to match the handler only in the
    list of loop events but does not look in the base event handler list
    nor the id event handler list. So unless the event handler is
    registered to be triggered for all event bases and all event ids of
    an event loop, its profiling fields (invoked and time) are not updated
    when it is called.
    
    This commit updates the search for the matching handler to also look
    in base event list and ID event list.
    
    Closes https://github.com/espressif/esp-idf/issues/15041

--- components/esp_event/test_apps/pytest_esp_event.py
+++ components/esp_event/test_apps/pytest_esp_event.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 import pytest
 from pytest_embedded import Dut
@@ -29,3 +29,15 @@ def test_esp_event_posix_simulator(dut: Dut) -> None:
     dut.expect_exact('Press ENTER to see the list of tests.')
     dut.write('*')
     dut.expect(r'\d{2} Tests 0 Failures 0 Ignored', timeout=120)
+
+
+@pytest.mark.esp32
+@pytest.mark.generic
+def test_esp_event_profiling(dut: Dut) -> None:
+    dut.expect_exact('Press ENTER to see the list of tests.')
+    dut.write('"profiling reports valid values"')
+    # look for all references of handlers invoked at least 1 time
+    # with an execution time superior to 0 us
+    matches = dut.expect(r'HANDLER .+ inv:[1-9][0-9]{0,} time:[1-9][0-9]{0,} us', timeout=2)
+    matches_arr = matches.group().split(b'\r\n')
+    assert (len(matches_arr) == 3)
