47ef2aa788822137a3335d005a236aa099dd6763
Song Ruo Jing <songruojing@espressif.com>
2024-12-20 21:14:34 +0800

    fix(gpio): fix 8/16-bit gpio, rtc/lp_io register access

--- components/hal/esp32h2/include/hal/gpio_ll.h
+++ components/hal/esp32h2/include/hal/gpio_ll.h
@@ -66,7 +66,7 @@ static inline void gpio_ll_get_io_config(gpio_dev_t *hw, uint32_t gpio_num,
     *od = hw->pin[gpio_num].pad_driver;
     *drv = (iomux_reg_val & FUN_DRV_M) >> FUN_DRV_S;
     *fun_sel = (iomux_reg_val & MCU_SEL_M) >> MCU_SEL_S;
-    *sig_out = hw->func_out_sel_cfg[gpio_num].out_sel;
+    *sig_out = HAL_FORCE_READ_U32_REG_FIELD(hw->func_out_sel_cfg[gpio_num], out_sel);
     *slp_sel = (iomux_reg_val & SLP_SEL_M) >> SLP_SEL_S;
 }
 
