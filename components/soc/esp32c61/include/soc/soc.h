1efea28c69719c97be2efd37ddc4e814d0c96eb7
gaoxu <gaoxu@espressif.com>
2024-12-24 19:13:32 +0800

    feat(interrupt): rename interrupt martix reg base

--- components/soc/esp32c61/include/soc/soc.h
+++ components/soc/esp32c61/include/soc/soc.h
@@ -24,7 +24,7 @@
 #define REG_SPI_MEM_BASE(i)                     (DR_REG_MSPI0_BASE + (i) * 0x1000)        // SPIMEM0 and SPIMEM1
 #define REG_SPI_BASE(i)                         (((i)==2) ? (DR_REG_SPI2_BASE) : (0))    // only one GPSPI on C61
 #define REG_I2C_BASE(i)                         (DR_REG_I2C_EXT_BASE)                    // only one I2C on C61
-#define DR_REG_INTERRUPT_CORE0_BASE             DR_REG_INTERRUPT_MATRIX_BASE
+#define DR_REG_INTERRUPT_CORE0_BASE             DR_REG_INTMTX_BASE
 
 //Registers Operation {{
 #define ETS_UNCACHED_ADDR(addr) (addr)
