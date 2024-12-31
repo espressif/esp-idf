15f6775f5d5bd9ee73072e914118024d15b685eb
Guillaume Souchere <guillaume.souchere@espressif.com>
2024-12-31 12:11:37 +0100

    fix(esp_event): Handler unregistration by itself issue
    when esp_event_handler_unregister_with_internal cannot take
    the loop mutex (e.g., when the handler unregisters itself),
    create an event with a special base identifier and add it to
    the queue of the corresponding loop to postpone the removal
    of the handler from the list at a time when the loop mutex can be
    successfully taken.

--- components/esp_event/test_apps/pytest_esp_event.py
+++ components/esp_event/test_apps/pytest_esp_event.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2025 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 import pytest
 from pytest_embedded import Dut
@@ -41,3 +41,16 @@ def test_esp_event_profiling(dut: Dut) -> None:
     matches = dut.expect(r'HANDLER .+ inv:[1-9][0-9]{0,} time:[1-9][0-9]{0,} us', timeout=2)
     matches_arr = matches.group().split(b'\r\n')
     assert (len(matches_arr) == 3)
+    dut.expect('1 Tests 0 Failures 0 Ignored', timeout=120)
+    dut.expect_exact("Enter next test, or 'enter' to see menu")
+
+    dut.write('"esp_event_dump does not show self unregistered handler"')
+    # look for 1 handlers never invoked
+    matches = dut.expect(r'HANDLER .+ inv:0 time:0 us', timeout=2)
+    matches_arr = matches.group().split(b'\r\n')
+    assert (len(matches_arr) == 1)
+    dut.expect('1 Tests 0 Failures 0 Ignored', timeout=120)
+    dut.expect_exact("Enter next test, or 'enter' to see menu")
+
+    dut.write('"self unregistered handlers are never called again after they return"')
+    dut.expect('1 Tests 0 Failures 0 Ignored', timeout=120)
