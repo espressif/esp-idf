bb0dac0a72ee7b3bcdced3505c80275f3675f5e6
chaijie@espressif.com <chaijie@espressif.com>
2025-01-13 19:56:43 +0800

    fix(esp32): Fixed qa program may fail issue when cpu 240m (v5.3)

--- components/esp_hw_support/port/esp32/include/soc/rtc.h
+++ components/esp_hw_support/port/esp32/include/soc/rtc.h
@@ -66,16 +66,13 @@ extern "C" {
 /* Core voltage needs to be increased in two cases:
  * 1. running at 240 MHz
  * 2. running with 80MHz Flash frequency
- *
- * There is a record in efuse which indicates the proper voltage for these two cases.
  */
-#define RTC_CNTL_DBIAS_HP_VOLT         (RTC_CNTL_DBIAS_1V25 - efuse_ll_get_vol_level_hp_inv())
 #ifdef CONFIG_ESPTOOLPY_FLASHFREQ_80M
-#define DIG_DBIAS_80M_160M  RTC_CNTL_DBIAS_HP_VOLT
+#define DIG_DBIAS_80M_160M  RTC_CNTL_DBIAS_1V25
 #else
 #define DIG_DBIAS_80M_160M  RTC_CNTL_DBIAS_1V10
 #endif
-#define DIG_DBIAS_240M      RTC_CNTL_DBIAS_HP_VOLT
+#define DIG_DBIAS_240M      RTC_CNTL_DBIAS_1V25
 #define DIG_DBIAS_XTAL      RTC_CNTL_DBIAS_1V10
 #define DIG_DBIAS_2M        RTC_CNTL_DBIAS_1V00
 
