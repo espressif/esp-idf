02b0d6495ed073b1bcd10bee0f4432a17d818abe
Zhou Xiao <zhouxiao@espressif.com>
2025-02-18 10:23:21 +0800

    feat(ble): add channel assessment and enhanced connect function on ESP32-H2
    (cherry picked from commit 0ffac9258680588ff60cb4602048e6fa8638e3a5)
    
    Co-authored-by: zwl <zhaoweiliang@espressif.com>

--- components/bt/include/esp32h2/include/esp_bt.h
+++ components/bt/include/esp32h2/include/esp_bt.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2015-2024 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2015-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -110,20 +110,20 @@ typedef enum {
 } esp_ble_enhanced_power_type_t;
 
 /**
- * @brief Address type and address value.
- */
-typedef struct {
-    uint8_t type;     /*!< Type of the Bluetooth address (public, random, etc.) */
-    uint8_t val[6];   /*!< Array containing the 6-byte Bluetooth address value */
-} esp_ble_addr_t;
-
-/**
  * @brief Select buffers
 */
 typedef enum {
     ESP_BLE_LOG_BUF_HCI         = 0x02,
     ESP_BLE_LOG_BUF_CONTROLLER  = 0x05,
 } esp_ble_log_buf_t;
+
+/**
+ * @brief Address type and address value.
+ */
+typedef struct {
+    uint8_t type;     /*!< Type of the Bluetooth address (public, random, etc.) */
+    uint8_t val[6];   /*!< Array containing the 6-byte Bluetooth address value */
+} esp_ble_addr_t;
 
 /**
  * @brief  Set BLE TX power
@@ -161,7 +161,7 @@ esp_err_t esp_ble_tx_power_set_enhanced(esp_ble_enhanced_power_type_t power_type
  */
 esp_power_level_t esp_ble_tx_power_get_enhanced(esp_ble_enhanced_power_type_t power_type, uint16_t handle);
 
-#define CONFIG_VERSION  0x20241121
+#define CONFIG_VERSION  0x20250104
 #define CONFIG_MAGIC    0x5A5AA5A5
 
 /**
@@ -217,7 +217,16 @@ typedef struct {
     uint8_t csa2_select;                         /*!< Select CSA#2*/
     uint8_t enable_csr;                          /*!< Enable CSR */
     uint8_t ble_aa_check;                        /*!< True if adds a verification step for the Access Address within the CONNECT_IND PDU; false otherwise. Configurable in menuconfig */
-    uint32_t config_magic;                       /*!< Configuration magic value */
+    uint8_t ble_llcp_disc_flag;                     /*!< Flag indicating whether the Controller disconnects after Instant Passed (0x28) error occurs. Configurable in menuconfig.
+                                                        - The Controller does not disconnect after Instant Passed (0x28) by default. */
+    uint16_t scan_backoff_upperlimitmax;            /*!< The value of upperlimitmax is 2^n, The maximum value is 256 */
+    uint8_t ble_chan_ass_en;                        /*!< Enable / disable BLE channel assessment. Configurable in menuconfig.
+                                                        - 0 - Disable
+                                                        - 1 - Enable (default) */
+    uint8_t ble_data_lenth_zero_aux;                /*!< Enable / disable auxiliary packets when the extended ADV data length is zero. Configurable in menuconfig.
+                                                        - 0 - Disable (default)
+                                                        - 1 - Enable */
+    uint32_t config_magic;                          /*!< Configuration magic value */
 } esp_bt_controller_config_t;
 
 
@@ -263,10 +272,14 @@ typedef struct {
     .main_xtal_freq             = CONFIG_XTAL_FREQ,                                     \
     .cpu_freq_mhz               = CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ,                      \
     .ignore_wl_for_direct_adv   = 0,                                                    \
-    .enable_pcl                 = 0,                                                    \
+    .enable_pcl                 = DEFAULT_BT_LE_POWER_CONTROL_ENABLED,                  \
     .csa2_select                = DEFAULT_BT_LE_50_FEATURE_SUPPORT,                     \
     .enable_csr                 = 0,                                                    \
     .ble_aa_check               = DEFAULT_BT_LE_CTRL_CHECK_CONNECT_IND_ACCESS_ADDRESS,  \
+    .ble_llcp_disc_flag         = BT_LE_CTRL_LLCP_DISC_FLAG,                            \
+    .scan_backoff_upperlimitmax = BT_CTRL_SCAN_BACKOFF_UPPERLIMITMAX,                   \
+    .ble_chan_ass_en            = DEFAULT_BT_LE_CTRL_CHAN_ASS_EN,                       \
+    .ble_data_lenth_zero_aux    = DEFAULT_BT_LE_CTRL_ADV_DATA_LENGTH_ZERO_AUX,          \
     .config_magic = CONFIG_MAGIC,                                                       \
 }
 
