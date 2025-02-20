d305628c25a4b291141b29d4a4ac868699c55bc2
wuzhenghui <wuzhenghui@espressif.com>
2025-02-20 15:18:13 +0800

    feat(hal): add esp32h21 modem_clock hal layer

--- components/soc/esp32h21/include/soc/soc_caps.h
+++ components/soc/esp32h21/include/soc/soc_caps.h
@@ -63,7 +63,7 @@
 // #define SOC_WDT_SUPPORTED               1    //TODO: [ESP32H21] IDF-11528
 #define SOC_SPI_FLASH_SUPPORTED         1       //TODO: [ESP32H21] IDF-11526
 // #define SOC_RNG_SUPPORTED               1    //TODO: [ESP32H21] IDF-11503
-// #define SOC_MODEM_CLOCK_SUPPORTED       1
+#define SOC_MODEM_CLOCK_SUPPORTED       1
 // #define SOC_REG_I2C_SUPPORTED           1    //TODO: [ESP32H21] IDF-11550
 // #define SOC_PHY_SUPPORTED               1
 // #define SOC_PCNT_SUPPORTED              1    //TODO: [ESP32H21] IDF-11566
