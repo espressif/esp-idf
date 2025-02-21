45580bb73e4f235544b3d38d8ccb6f1c2348d8ce
zhanghaipeng <zhanghaipeng@espressif.com>
2025-02-21 16:55:11 +0800

    fix(ble/bluedroid): Support SPI log output options for HCI

--- components/bt/common/include/bt_user_config.h
+++ components/bt/common/include/bt_user_config.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2015-2024 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2015-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -118,6 +118,13 @@
 #define UC_BT_HCI_LOG_DEBUG_EN  TRUE
 #else
 #define UC_BT_HCI_LOG_DEBUG_EN  FALSE
+#endif
+
+//HCI LOG TO SPI
+#ifdef CONFIG_BT_BLE_LOG_SPI_OUT_HCI_ENABLED
+#define UC_BT_BLE_LOG_SPI_OUT_HCI_ENABLED  TRUE
+#else
+#define UC_BT_BLE_LOG_SPI_OUT_HCI_ENABLED  FALSE
 #endif
 
 #ifdef CONFIG_BT_HCI_LOG_DATA_BUFFER_SIZE
