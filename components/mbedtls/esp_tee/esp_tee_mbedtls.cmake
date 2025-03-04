98e16412a770da68dfb340f3b179fab705293c0e
Laukik Hase <laukik.hase@espressif.com>
2025-03-04 16:17:35 +0530

    refactor(esp_tee): Use the AES-GCM port layer for operations in the TEE

--- components/mbedtls/esp_tee/esp_tee_mbedtls.cmake
+++ components/mbedtls/esp_tee/esp_tee_mbedtls.cmake
@@ -13,20 +13,22 @@ list(APPEND include_dirs "${heap_dir}/include")
 set(srcs "${COMPONENT_DIR}/esp_tee/esp_tee_crypto_shared_gdma.c")
 
 # AES-SHA implementation
-list(APPEND include_dirs "${COMPONENT_DIR}/port/aes/include"
-                         "${COMPONENT_DIR}/port/aes/dma/include"
-                         "${COMPONENT_DIR}/port/sha/core/include")
+list(APPEND srcs "${COMPONENT_DIR}/port/aes/dma/esp_aes.c"
+                 "${COMPONENT_DIR}/port/aes/dma/esp_aes_dma_core.c")
 
 list(APPEND srcs "${COMPONENT_DIR}/port/aes/esp_aes_common.c"
-                 "${COMPONENT_DIR}/port/aes/dma/esp_aes.c"
-                 "${COMPONENT_DIR}/port/aes/dma/esp_aes_dma_core.c")
+                 "${COMPONENT_DIR}/port/aes/esp_aes_gcm.c")
 
 list(APPEND srcs "${COMPONENT_DIR}/port/sha/core/sha.c"
                  "${COMPONENT_DIR}/port/sha/esp_sha.c")
 
-idf_component_register(INCLUDE_DIRS "${include_dirs}"
-                       PRIV_REQUIRES "${priv_requires}"
-                       SRCS "${srcs}")
+list(APPEND include_dirs "${COMPONENT_DIR}/port/aes/include"
+                         "${COMPONENT_DIR}/port/aes/dma/include"
+                         "${COMPONENT_DIR}/port/sha/core/include")
+
+idf_component_register(SRCS "${srcs}"
+                       INCLUDE_DIRS "${include_dirs}"
+                       PRIV_REQUIRES "${priv_requires}")
 
 # Only build mbedtls libraries
 set(ENABLE_TESTING CACHE BOOL OFF)
@@ -50,9 +52,6 @@ endforeach()
 
 target_link_libraries(${COMPONENT_LIB} INTERFACE ${mbedtls_targets})
 
-if(CONFIG_MBEDTLS_HARDWARE_SHA)
-    target_sources(mbedcrypto PRIVATE  "${COMPONENT_DIR}/port/sha/core/esp_sha1.c"
-                                       "${COMPONENT_DIR}/port/sha/core/esp_sha256.c"
-                                       "${COMPONENT_DIR}/port/sha/core/esp_sha512.c"
-    )
-endif()
+target_sources(mbedcrypto PRIVATE "${COMPONENT_DIR}/port/sha/core/esp_sha1.c"
+                                  "${COMPONENT_DIR}/port/sha/core/esp_sha256.c"
+                                  "${COMPONENT_DIR}/port/sha/core/esp_sha512.c")
