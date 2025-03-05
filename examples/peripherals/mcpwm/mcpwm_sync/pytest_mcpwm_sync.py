603ebbbc99f10fc4c14e4fbaeb836748d5c23cf6
morris <maoshengrong@espressif.com>
2025-03-05 16:48:44 +0800

    test(mcpwm): reenable sync example test

--- examples/peripherals/mcpwm/mcpwm_sync/pytest_mcpwm_sync.py
+++ examples/peripherals/mcpwm/mcpwm_sync/pytest_mcpwm_sync.py
@@ -1,5 +1,7 @@
 # SPDX-FileCopyrightText: 2021-2025 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
+import itertools
+
 import pytest
 from pytest_embedded import Dut
 from pytest_embedded_idf.utils import idf_parametrize
@@ -8,7 +10,11 @@ from pytest_embedded_idf.utils import idf_parametrize
 @pytest.mark.generic
 @idf_parametrize(
     'config,target',
-    [('gpio', 'esp32'), ('gpio', 'esp32s3'), ('tez', 'esp32'), ('tez', 'esp32s3'), ('soft', 'esp32s3')],
+    itertools.chain(
+        itertools.product(['gpio'], ['esp32', 'esp32s3', 'esp32c5', 'esp32c6', 'esp32h2', 'esp32p4']),
+        itertools.product(['tez'], ['esp32', 'esp32s3', 'esp32c5', 'esp32c6', 'esp32h2', 'esp32p4']),
+        itertools.product(['soft'], ['esp32s3', 'esp32c5', 'esp32c6', 'esp32h2', 'esp32p4']),
+    ),
     indirect=['config', 'target'],
 )
 def test_mcpwm_sync_example(dut: Dut) -> None:
