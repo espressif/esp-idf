b710ee9debd79226b87f038b0c9558de93a14338
Song Ruo Jing <songruojing@espressif.com>
2025-03-06 20:33:43 +0800

    refactor(uart): increase test app memory leak tolerance

--- components/driver/test_apps/uart/main/test_app_main.c
+++ components/driver/test_apps/uart/main/test_app_main.c
@@ -10,7 +10,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#define TEST_MEMORY_LEAK_THRESHOLD (200)
+#define TEST_MEMORY_LEAK_THRESHOLD (250)
 
 static size_t before_free_8bit;
 static size_t before_free_32bit;
