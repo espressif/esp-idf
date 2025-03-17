ed8efcf6e979281ddd829eb03f8038df9e4df8ee
luaijun <luaijun@espressif.com>
2025-03-17 17:29:03 +0800

    ci(c2/c3): add major eco version tests(v5.1)

--- components/esp_wifi/test_apps/wifi_connect/pytest_wifi_connect.py
+++ components/esp_wifi/test_apps/wifi_connect/pytest_wifi_connect.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2025 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Unlicense OR CC0-1.0
 import pytest
 from pytest_embedded_idf.unity_tester import CaseTester
@@ -17,11 +17,38 @@ def test_wifi_connect_cases(case_tester: CaseTester) -> None:  # type: ignore
 @pytest.mark.wifi_two_dut
 @pytest.mark.xtal_26mhz
 @pytest.mark.parametrize(
-    'count, config, baud',
+    'count, config, baud, target',
     [
-        (2, 'esp32c2_xtal26m', '74880'),
+        (2, 'esp32c2_xtal26m', '74880','esp32c2|esp32c2'),
     ],
     indirect=True,
 )
 def test_wifi_connect_cases_esp32c2_xtal26m(case_tester: CaseTester) -> None:
+    case_tester.run_all_cases()
+
+
+@pytest.mark.esp32c2eco4
+@pytest.mark.wifi_two_dut
+@pytest.mark.xtal_26mhz
+@pytest.mark.parametrize(
+    'count, config, baud, target',
+    [
+        (2, 'esp32c2eco4_xtal26m', '74880','esp32c2|esp32c2'),
+    ],
+    indirect=True,
+)
+def test_wifi_connect_cases_esp32c2eco4_xtal26m(case_tester: CaseTester) -> None:
+    case_tester.run_all_cases()
+
+
+@pytest.mark.wifi_two_dut
+@pytest.mark.esp32c3eco7
+@pytest.mark.parametrize(
+    'count, config, target',
+    [
+        (2, 'esp32c3eco7','esp32c3|esp32c3'),
+    ],
+    indirect=True,
+)
+def test_wifi_connect_cases_esp32c3eco7(case_tester: CaseTester) -> None:
     case_tester.run_all_cases()
