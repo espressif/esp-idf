2ced91dd73e0e70d91c8bde7f8602d130d0a56b4
rcbotics <rcbotic@gmail.com>
2025-03-18 23:19:19 -0400

    Fixed Git configuration

--- examples/build_system/cmake/idf_as_lib/run-esp32c3.sh
+++ examples/build_system/cmake/idf_as_lib/run-esp32c3.sh
@@ -1,1 +1,4 @@
-run-esp32.sh
+#!/usr/bin/env bash
+cd build
+python $IDF_PATH/components/esptool_py/esptool/esptool.py -p $1 write_flash @flash_project_args
+python -m esp_idf_monitor -p $1 idf_as_lib.elf
