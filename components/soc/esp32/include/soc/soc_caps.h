2ee0b6c4dd2d0302ce490e96680a294b6aa8c21c
Jan Beran <jan.beran@espressif.com>
2025-05-07 13:37:05 +0200

    change(gen_soc_caps_kconfig): check if config options have the same type
    Also introduce ignore pragma to prevent some #defines from .h files to
    be translated to Kconfig files.

--- components/soc/esp32/include/soc/soc_caps.h
+++ components/soc/esp32/include/soc/soc_caps.h
@@ -1,6 +1,6 @@
 
 /*
- * SPDX-FileCopyrightText: 2020-2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2020-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -54,9 +54,9 @@
 
 // Define warning strings here for ECO-ed features to show error when they are used without being
 // defined correctly
-#define SOC_BROWNOUT_RESET_SUPPORTED    "Not determined"
-#define SOC_TWAI_BRP_DIV_SUPPORTED      "Not determined"
-#define SOC_DPORT_WORKAROUND            "Not determined"
+#define SOC_BROWNOUT_RESET_SUPPORTED    "Not determined" // [gen_soc_caps:ignore]
+#define SOC_TWAI_BRP_DIV_SUPPORTED      "Not determined" // [gen_soc_caps:ignore]
+#define SOC_DPORT_WORKAROUND            "Not determined" // [gen_soc_caps:ignore]
 #endif
 
 /*-------------------------- COMMON CAPS ---------------------------------------*/
@@ -107,7 +107,7 @@
 #define SOC_PM_SUPPORTED            1
 
 #if SOC_CAPS_ECO_VER < 200
-#define SOC_DPORT_WORKAROUND                   1
+#define SOC_DPORT_WORKAROUND                   1  // [gen_soc_caps:ignore]
 #endif // SOC_CAPS_ECO_VER < 200
 #define SOC_DPORT_WORKAROUND_DIS_INTERRUPT_LVL (5U)
 
@@ -162,7 +162,7 @@
 
 #define SOC_CPU_BREAKPOINTS_NUM             2
 #define SOC_CPU_WATCHPOINTS_NUM             2
-#define SOC_CPU_WATCHPOINT_MAX_REGION_SIZE  64 // bytes
+#define SOC_CPU_WATCHPOINT_MAX_REGION_SIZE  0x40 // bytes
 
 /*-------------------------- DAC CAPS ----------------------------------------*/
 #define SOC_DAC_CHAN_NUM              2
@@ -380,7 +380,7 @@
 
 /*--------------------------- MPI CAPS ---------------------------------------*/
 #define SOC_MPI_MEM_BLOCKS_NUM (4)
-#define SOC_MPI_OPERATIONS_NUM (1)
+#define SOC_MPI_OPERATIONS_NUM (1U)
 
 /*--------------------------- RSA CAPS ---------------------------------------*/
 #define SOC_RSA_MAX_BIT_LEN    (4096)
@@ -398,7 +398,7 @@
  * Hence, for now we are handling this special capability in bootloader "security" configuration itself.
  */
 #define SOC_SECURE_BOOT_V1                  1
-#define SOC_EFUSE_SECURE_BOOT_KEY_DIGESTS   1
+#define SOC_EFUSE_SECURE_BOOT_KEY_DIGESTS   (1U)
 
 /*-------------------------- Flash Encryption CAPS----------------------------*/
 #define SOC_FLASH_ENCRYPTED_XTS_AES_BLOCK_MAX   (32)
