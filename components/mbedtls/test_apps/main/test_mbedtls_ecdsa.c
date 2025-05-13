6c290c09f3a7224f6a76baf048f55a32260e3706
nilesh.kale <nilesh.kale@espressif.com>
2025-05-13 17:01:07 +0530

    feat: enabled ECDSA-P192 support for ESP32H2

--- components/mbedtls/test_apps/main/test_mbedtls_ecdsa.c
+++ components/mbedtls/test_apps/main/test_mbedtls_ecdsa.c
@@ -1,6 +1,6 @@
 /* mbedTLS Elliptic Curve Digital Signature performance tests
  *
- * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2021-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -22,6 +22,10 @@
 
 #include "ecdsa/ecdsa_alt.h"
 
+#if SOC_ECDSA_SUPPORTED
+#include "hal/ecdsa_ll.h"
+#endif
+
 #define TEST_ASSERT_MBEDTLS_OK(X) TEST_ASSERT_EQUAL_HEX32(0, -(X))
 
 #if CONFIG_NEWLIB_NANO_FORMAT
@@ -31,6 +35,8 @@
 #define NEWLIB_NANO_COMPAT_FORMAT             PRId64
 #define NEWLIB_NANO_COMPAT_CAST(int64_t_var)  int64_t_var
 #endif
+
+__attribute__((unused)) static const char * TAG = "mbedtls_test";
 
 /*
  * All the following values are in big endian format, as required by the mbedTLS APIs
