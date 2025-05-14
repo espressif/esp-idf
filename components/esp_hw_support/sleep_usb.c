9cf4ddf797cfe3dbb33fd9838e7ab986d869780f
wuzhenghui <wuzhenghui@espressif.com>
2025-05-14 17:57:40 +0800

    fix(esp_hw_support): Fix deepsleep leakage after initializing USB HS phy

--- components/esp_hw_support/sleep_usb.c
+++ components/esp_hw_support/sleep_usb.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2024-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,10 +9,13 @@
 #include "esp_private/sleep_usb.h"
 #include "esp_attr.h"
 
-#if SOC_USB_OTG_SUPPORTED && SOC_PM_SUPPORT_CNNT_PD
+#if SOC_USB_OTG_SUPPORTED && (SOC_PM_SUPPORT_CNNT_PD || SOC_USB_UTMI_PHY_NO_POWER_OFF_ISO)
 #include "hal/usb_utmi_ll.h"
 #include "hal/usb_dwc_ll.h"
+#endif
 
+#if SOC_USB_OTG_SUPPORTED
+#if SOC_PM_SUPPORT_CNNT_PD
 static bool s_usb_utmi_bus_clock_state, s_usb_utmi_stoppclk_state;
 
 void sleep_usb_otg_phy_backup_and_disable(void)
@@ -33,4 +36,14 @@ void sleep_usb_otg_phy_restore(void)
         _usb_utmi_ll_enable_bus_clock(false);
     }
 }
+#endif
+
+#if SOC_USB_UTMI_PHY_NO_POWER_OFF_ISO
+void sleep_usb_suppress_deepsleep_leakage(void)
+{
+    if (_usb_utmi_ll_bus_clock_is_enabled()) {
+        usb_dwc_ll_gusbcfg_en_hnp_cap(&USB_DWC_HS);
+    }
+}
+#endif
 #endif
