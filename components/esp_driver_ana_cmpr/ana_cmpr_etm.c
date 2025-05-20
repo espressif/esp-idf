7af3bddbcac5360001f02f806bfe5a41f553e04f
morris <maoshengrong@espressif.com>
2025-05-20 11:42:31 +0800

    fix(drivers): some false reports from coverity

--- components/esp_driver_ana_cmpr/ana_cmpr_etm.c
+++ components/esp_driver_ana_cmpr/ana_cmpr_etm.c
@@ -23,13 +23,11 @@ static esp_err_t ana_cmpr_del_etm_event(esp_etm_event_handle_t base_event)
 
 esp_err_t ana_cmpr_new_etm_event(ana_cmpr_handle_t cmpr, const ana_cmpr_etm_event_config_t *config, esp_etm_event_handle_t *ret_event)
 {
-    esp_err_t ret = ESP_OK;
-    ana_cmpr_etm_event_t *event = NULL;
     ana_cmpr_unit_t unit = ana_cmpr_get_unit_id(cmpr);
     ESP_RETURN_ON_FALSE(((int)unit) >= 0, ESP_ERR_INVALID_ARG, TAG, "invalid analog comparator handle");
     ESP_RETURN_ON_FALSE(config && ret_event, ESP_ERR_INVALID_ARG, TAG, "invalid argument");
-    event = heap_caps_calloc(1, sizeof(ana_cmpr_etm_event_t), ETM_MEM_ALLOC_CAPS);
-    ESP_GOTO_ON_FALSE(event, ESP_ERR_NO_MEM, err, TAG, "no mem for analog comparator event");
+    ana_cmpr_etm_event_t *event = heap_caps_calloc(1, sizeof(ana_cmpr_etm_event_t), ETM_MEM_ALLOC_CAPS);
+    ESP_RETURN_ON_FALSE(event, ESP_ERR_NO_MEM, TAG, "no mem for analog comparator event");
 
     uint32_t event_id = ANALOG_CMPR_LL_ETM_SOURCE(unit, config->event_type);
     event->base.del = ana_cmpr_del_etm_event;
@@ -38,11 +36,4 @@ esp_err_t ana_cmpr_new_etm_event(ana_cmpr_handle_t cmpr, const ana_cmpr_etm_even
     ESP_LOGD(TAG, "new event @%p, event_id=%"PRIu32", unit_id=%d", event, event_id, unit);
     *ret_event = &event->base;
     return ESP_OK;
-
-err:
-    if (event) {
-        free(event);
-        event = NULL;
-    }
-    return ret;
 }
