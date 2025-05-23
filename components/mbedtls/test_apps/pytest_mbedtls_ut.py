163db6a8a5839d8e7756fc46c73b0f3c0208f897
Ashish Sharma <ashish.sharma@espressif.com>
2025-05-23 11:43:29 +0800

    feat(mbedtls): adds support for RSA decryption with DS peripheral

--- components/mbedtls/test_apps/pytest_mbedtls_ut.py
+++ components/mbedtls/test_apps/pytest_mbedtls_ut.py
@@ -86,3 +86,16 @@ def test_mbedtls_ecdsa_sign(dut: Dut) -> None:
 @idf_parametrize('target', ['esp32c2'], indirect=['target'])
 def test_mbedtls_rom_impl_esp32c2(dut: Dut) -> None:
     dut.run_all_single_board_cases()
+
+
+@pytest.mark.generic
+@pytest.mark.parametrize(
+    'config',
+    [
+        'ds_rsa',
+    ],
+    indirect=True,
+)
+@idf_parametrize('target', ['esp32s3'], indirect=['target'])
+def test_mbedtls_ds_rsa(dut: Dut) -> None:
+    dut.run_all_single_board_cases(group='ds_rsa')
