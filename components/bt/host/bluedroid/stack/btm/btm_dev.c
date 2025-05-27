3b6faa85143d5b006d6cecdfba136eab3df3dbf8
Zhang Hai Peng <zhanghaipeng@espressif.com>
2025-05-27 15:13:51 +0800

    fix(ble/bluedroid): Fixed issue with deleting link key when classic Bluetooth is not used
    (cherry picked from commit 7c16bce827002efa68c80860e018841133cb4c08)
    
    Co-authored-by: zhanghaipeng <zhanghaipeng@espressif.com>

--- components/bt/host/bluedroid/stack/btm/btm_dev.c
+++ components/bt/host/bluedroid/stack/btm/btm_dev.c
@@ -187,8 +187,10 @@ BOOLEAN BTM_SecDeleteDevice (BD_ADDR bd_addr, tBT_TRANSPORT transport)
     }
 
     if ((p_dev_rec = btm_find_dev(bd_addr)) != NULL) {
+#if (CLASSIC_BT_INCLUDED == TRUE)
         /* Tell controller to get rid of the link key, if it has one stored */
         BTM_DeleteStoredLinkKey (p_dev_rec->bd_addr, NULL);
+#endif // (CLASSIC_BT_INCLUDED == TRUE)
 
         btm_sec_free_dev(p_dev_rec, transport);
     }
