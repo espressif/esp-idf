0650ee6900818be66f6d009a24e200d8560bd8f6
Guillaume Souchere <guillaume.souchere@espressif.com>
2025-06-03 13:56:25 +0200

    feat(usb_serial_tag_vfs): Add test for read exit conditions
    Add a test to make sure the VFS read does not return on reception
    of the \n character

--- components/esp_driver_usb_serial_jtag/test_apps/usb_serial_jtag_vfs/pytest_usb_serial_jtag_vfs.py
+++ components/esp_driver_usb_serial_jtag/test_apps/usb_serial_jtag_vfs/pytest_usb_serial_jtag_vfs.py
@@ -16,10 +16,29 @@ from pytest_embedded import Dut
     ],
     indirect=True,)
 @pytest.mark.parametrize('test_message', ['test123456789!@#%^&*'])
-def test_usj_vfs_release(dut: Dut, test_message: list) -> None:
+@idf_parametrize('target', ['esp32s3', 'esp32c3', 'esp32c6', 'esp32h2'], indirect=['target'])
+def test_usj_vfs_select(dut: Dut, test_message: list) -> None:
     dut.expect_exact('Press ENTER to see the list of tests')
     dut.write('\"test select read, write and timeout\"')
     dut.expect_exact('select timed out', timeout=2)
     dut.write(test_message)
     dut.expect_exact(test_message, timeout=2)
+    dut.expect(r'\d{1} Tests 0 Failures 0 Ignored', timeout=10)
+
+
+@pytest.mark.usb_serial_jtag
+@pytest.mark.parametrize(
+    'port, config',
+    [
+        pytest.param('/dev/ttyACM1', 'release'),
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize('test_message', ['!(@*#&(!*@&#((SDasdkjhad\nce'])
+@idf_parametrize('target', ['esp32s3', 'esp32c3', 'esp32c6', 'esp32h2'], indirect=['target'])
+def test_usj_vfs_read(dut: Dut, test_message: list) -> None:
+    dut.expect_exact('Press ENTER to see the list of tests')
+    dut.write('"read with usj driver (non-blocking)"')
+    dut.expect_exact('ready to receive', timeout=2)
+    dut.write(test_message)
     dut.expect(r'\d{1} Tests 0 Failures 0 Ignored', timeout=10)
