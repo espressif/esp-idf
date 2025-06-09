8b06985fdf6e604e89b2d4e85d99596794cad551
Guillaume Souchere <guillaume.souchere@espressif.com>
2025-06-09 09:03:40 +0200

    feat(usb_serial_jtag): Update vfs read to be POSIX compliant
    The function now returns with available data in blocking mode
    instead of waiting for the requested size to be available before
    returning.

--- components/esp_driver_usb_serial_jtag/test_apps/usb_serial_jtag_vfs/pytest_usb_serial_jtag_vfs.py
+++ components/esp_driver_usb_serial_jtag/test_apps/usb_serial_jtag_vfs/pytest_usb_serial_jtag_vfs.py
@@ -34,7 +34,7 @@ def test_usj_vfs_select(dut: Dut, test_message: list) -> None:
 )
 @pytest.mark.parametrize('test_message', ['!(@*#&(!*@&#((SDasdkjhad\nce'])
 @idf_parametrize('target', ['esp32s3', 'esp32c3', 'esp32c6', 'esp32h2'], indirect=['target'])
-def test_usj_vfs_read(dut: Dut, test_message: list) -> None:
+def test_usj_vfs_read_return(dut: Dut, test_message: list) -> None:
     dut.expect_exact('Press ENTER to see the list of tests')
     dut.write('"read does not return on new line character"')
     dut.expect_exact('ready to receive', timeout=2)
@@ -52,7 +52,7 @@ def test_usj_vfs_read(dut: Dut, test_message: list) -> None:
 )
 @pytest.mark.parametrize('test_message', ['testdata'])
 @idf_parametrize('target', ['esp32s3', 'esp32c3', 'esp32c6', 'esp32h2'], indirect=['target'])
-def test_usj_vfs_read(dut: Dut, test_message: list) -> None:
+def test_usj_vfs_read_blocking(dut: Dut, test_message: list) -> None:
     dut.expect_exact('Press ENTER to see the list of tests')
     dut.write('"blocking read returns with available data"')
     dut.expect_exact('ready to receive', timeout=2)
