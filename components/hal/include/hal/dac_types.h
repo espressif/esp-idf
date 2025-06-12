bf3a050f4df873338e896ec07662e06e3f5e8423
laokaiyao <laokaiyao@espressif.com>
2025-06-12 16:51:33 +0800

    remove(legacy_dac): remove legacy dac driver in IDF v6.0

--- components/hal/include/hal/dac_types.h
+++ components/hal/include/hal/dac_types.h
@@ -7,8 +7,6 @@ extern "C" {
 typedef enum {
     DAC_CHAN_0 = 0,     /*!< DAC channel 0 is GPIO25(ESP32) / GPIO17(ESP32S2) */
     DAC_CHAN_1 = 1,     /*!< DAC channel 1 is GPIO26(ESP32) / GPIO18(ESP32S2) */
-    DAC_CHANNEL_1 __attribute__((deprecated("please use 'DAC_CHAN_0' instead"))) = 0,    /*!< Alias of 'DAC_CHAN_0', now the channel index start from '0' */
-    DAC_CHANNEL_2 __attribute__((deprecated("please use 'DAC_CHAN_1' instead"))) = 1,    /*!< Alias of 'DAC_CHAN_1', now the channel index start from '0' */
 } dac_channel_t;
 
 /**
--- docs/zh_CN/migration-guides/release-6.x/6.0/peripherals.rst
+++ docs/zh_CN/migration-guides/release-6.x/6.0/peripherals.rst
@@ -81,3 +81,10 @@ SDMMC
 -----
 
 - :cpp:type:`sdmmc_host_t` 结构体中的 ``get_dma_info`` 成员接口已经被移除。对应的 ``sdspi_host_get_dma_info`` 和 ``sdmmc_host_get_dma_info`` 函数也已经被移除。DMA 相关的设置会由驱动内部处理好。
+
+.. only:: SOC_DAC_SUPPORTED
+
+    旧版 DAC 驱动被移除
+    ----------------------
+
+    旧版的 DAC 驱动 ``driver/dac.h`` 在 5.1 的版本中就已经被弃用（请参考 :ref:`deprecate_dac_legacy_driver`）。从 6.0 版本开始，旧版驱动被完全移除。新驱动位于 :component:`esp_driver_dac` 组件中，头文件引用路径为 ``driver/dac_oneshot.h``， ``driver/dac_continuous.h`` 和 ``driver/dac_cosine.h``。
