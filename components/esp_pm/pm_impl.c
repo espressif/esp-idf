2ec5099b42f6eb6b1fd100e67cfd0f219425c03e
wuzhenghui <wuzhenghui@espressif.com>
2025-06-12 17:47:40 +0800

    fix(esp_hw_support): config lact in critical

--- components/esp_pm/pm_impl.c
+++ components/esp_pm/pm_impl.c
@@ -647,6 +647,8 @@ static void IRAM_ATTR do_switch(pm_mode_t new_mode)
         if (switch_down) {
             on_freq_update(old_ticks_per_us, new_ticks_per_us);
         }
+        extern portMUX_TYPE s_time_update_lock;
+        portENTER_CRITICAL_SAFE(&s_time_update_lock);
         if (new_config.source == SOC_CPU_CLK_SRC_PLL) {
             rtc_clk_cpu_freq_set_config_fast(&new_config);
 #if SOC_SPI_MEM_SUPPORT_TIMING_TUNING
@@ -658,6 +660,7 @@ static void IRAM_ATTR do_switch(pm_mode_t new_mode)
 #endif
             rtc_clk_cpu_freq_set_config_fast(&new_config);
         }
+        portEXIT_CRITICAL_SAFE(&s_time_update_lock);
         if (!switch_down) {
             on_freq_update(old_ticks_per_us, new_ticks_per_us);
         }
