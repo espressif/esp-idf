dd9613bed0eb74d7e31ba8b4c64707018a385182
wuzhenghui <wuzhenghui@espressif.com>
2025-06-16 22:06:49 +0800

    feat(soc): update esp32h4 mdoem soc headers

---
+++ components/soc/esp32h4/include/modem/reg_base.h
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+#define DR_REG_MODEM_SYSCON_BASE  0x600C9C00
+#define DR_REG_MODEM_LPCON_BASE   0x600CF000
