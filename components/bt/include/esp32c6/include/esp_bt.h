dc5b3de0eac4fd74d728fb3a5cbcd8da666b9712
cjin <jinchen@espressif.com>
2025-06-16 10:38:39 +0800

    feat(ble): add sm count reserve cnt setting on ESP32-C6 and ESP32-C61

--- components/bt/include/esp32c6/include/esp_bt.h
+++ components/bt/include/esp32c6/include/esp_bt.h
@@ -156,7 +156,7 @@ esp_err_t esp_ble_tx_power_set_enhanced(esp_ble_enhanced_power_type_t power_type
  */
 esp_power_level_t esp_ble_tx_power_get_enhanced(esp_ble_enhanced_power_type_t power_type, uint16_t handle);
 
-#define CONFIG_VERSION  0x20250526
+#define CONFIG_VERSION  0x20250606
 #define CONFIG_MAGIC    0x5A5AA5A5
 
 /**
@@ -232,6 +232,8 @@ typedef struct {
                                                         - 0 - Disable
                                                         - 1 - Enable (default) */
     int8_t ch39_txpwr;                               /*!< BLE transmit power (in dBm) used for BLE advertising on channel 39. */
+    uint8_t adv_rsv_cnt;                             /*!< BLE adv state machine reserve count number */
+    uint8_t conn_rsv_cnt;                            /*!< BLE conn state machine reserve count number */
     uint32_t config_magic;                           /*!< Magic number for configuration validation */
 } esp_bt_controller_config_t;
 
@@ -294,6 +296,8 @@ typedef struct {
     .skip_unnecessary_checks_en = 0,                                                    \
     .fast_conn_data_tx_en       = DEFAULT_BT_LE_CTRL_FAST_CONN_DATA_TX_EN,              \
     .ch39_txpwr                 = BLE_LL_TX_PWR_DBM_N,                                  \
+    .adv_rsv_cnt                = BLE_LL_ADV_SM_RESERVE_CNT_N,                          \
+    .conn_rsv_cnt               = BLE_LL_CONN_SM_RESERVE_CNT_N,                         \
     .config_magic = CONFIG_MAGIC,                                                       \
 }
 #elif CONFIG_IDF_TARGET_ESP32C61
@@ -353,6 +357,8 @@ typedef struct {
     .skip_unnecessary_checks_en = 0,                                                    \
     .fast_conn_data_tx_en       = DEFAULT_BT_LE_CTRL_FAST_CONN_DATA_TX_EN,              \
     .ch39_txpwr                 = BLE_LL_TX_PWR_DBM_N,                                  \
+    .adv_rsv_cnt                = BLE_LL_ADV_SM_RESERVE_CNT_N,                          \
+    .conn_rsv_cnt               = BLE_LL_CONN_SM_RESERVE_CNT_N,                         \
     .config_magic = CONFIG_MAGIC,                                                       \
 }
 #endif
