7a59713121633072262e16b7e73fb02250b9a43c
armando <douyiwen@espressif.com>
2025-06-16 13:16:56 +0800

    fix(cache): fixed SOC_CACHE_FREEZE_SUPPORTED not defined on c3/c2 issue

--- components/soc/esp32c2/include/soc/soc_caps.h
+++ components/soc/esp32c2/include/soc/soc_caps.h
@@ -95,6 +95,7 @@
 
 /*-------------------------- CACHE CAPS --------------------------------------*/
 #define SOC_SHARED_IDCACHE_SUPPORTED            1   //Shared Cache for both instructions and data
+#define SOC_CACHE_FREEZE_SUPPORTED              1
 
 /*-------------------------- CPU CAPS ----------------------------------------*/
 #define SOC_CPU_CORES_NUM               (1U)
