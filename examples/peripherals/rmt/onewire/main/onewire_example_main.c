ee76b461958a69608338535328fdbafea716cecf
morris <maoshengrong@espressif.com>
2025-06-17 15:09:23 +0800

    fix(onewire): enable internal pull-up resistor by default
    In case there's no pull-up resistor on the external device

--- examples/peripherals/rmt/onewire/main/onewire_example_main.c
+++ examples/peripherals/rmt/onewire/main/onewire_example_main.c
@@ -21,6 +21,9 @@ void app_main(void)
     onewire_bus_handle_t bus;
     onewire_bus_config_t bus_config = {
         .bus_gpio_num = EXAMPLE_ONEWIRE_BUS_GPIO,
+        .flags = {
+            .en_pull_up = true, // enable the internal pull-up resistor in case the external device didn't have one
+        }
     };
     onewire_bus_rmt_config_t rmt_config = {
         .max_rx_bytes = 10, // 1byte ROM command + 8byte ROM number + 1byte device command
