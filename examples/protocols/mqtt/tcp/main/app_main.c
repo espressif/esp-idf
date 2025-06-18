63b91c0608d1d15b885ed361c9b661e1cb0fdf35
Euripedes Rocha <rocha.euripedes@espressif.com>
2025-06-18 14:19:46 +0200

    feat(mqtt): Add reason code to SUBSCRIBED event in examples

--- examples/protocols/mqtt/tcp/main/app_main.c
+++ examples/protocols/mqtt/tcp/main/app_main.c
@@ -67,7 +67,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         break;
 
     case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d, return code=0x%02x ", event->msg_id, (uint8_t)*event->data);
         msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
         ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         break;
