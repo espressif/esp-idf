bab8d499a094a136b2b5af2290ce64b2ed44415d
Robot <julian@scheffers.net>
2025-07-01 12:56:59 +0200

    feat: Rename ESP_RETURN_ON_ERR to ESP_ERROR_CHECK_RETURN
    ... and add an example

--- components/esp_common/include/esp_err.h
+++ components/esp_common/include/esp_err.h
@@ -95,8 +95,19 @@ void _esp_error_check_failed_without_abort(esp_err_t rc, const char *file, int l
 /**
  * Macro that returns the error code if an error occurs, optionally running extra code.
  * The extra code can be placed after the function thay may error and it will only run if the error occurs.
+ * 
+ * @example
+ * esp_err_t my_function()
+ * {
+ *     // Let's say `read_file()` returns esp_err_t and we want to `close_file()` on error.
+ *     ESP_ERROR_CHECK_RETURN(read_file(), close_file())
+ *     // If the code page gets here, `read_file()` succeeded.
+ *     process_file();
+ *     close_file();
+ *     return ESP_OK;
+ * }
  */
-#define ESP_RETURN_ON_ERR(x, ...) do {        \
+#define ESP_ERROR_CHECK_RETURN(x, ...) do {   \
         esp_err_t err_rc_ = (x);              \
         if (unlikely(err_rc_ != ESP_OK)) {    \
             __VA_ARGS__;                      \
