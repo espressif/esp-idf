08b75523cbde96dce83849a346c21c92bb4dac8d
Chen Jichang <chenjichang@espressif.com>
2025-07-02 17:07:35 +0800

    refactor(spi_lcd): detect the bus mode automatically

--- components/esp_lcd/include/esp_lcd_io_spi.h
+++ components/esp_lcd/include/esp_lcd_io_spi.h
@@ -35,8 +35,8 @@ typedef struct {
         unsigned int dc_high_on_cmd: 1;  /*!< If enabled, DC level = 1 indicates command transfer */
         unsigned int dc_low_on_data: 1;  /*!< If enabled, DC level = 0 indicates color data transfer */
         unsigned int dc_low_on_param: 1; /*!< If enabled, DC level = 0 indicates parameter transfer */
-        unsigned int octal_mode: 1;      /*!< transmit with octal mode (8 data lines), this mode is used to simulate Intel 8080 timing */
-        unsigned int quad_mode: 1;       /*!< transmit with quad mode (4 data lines), this mode is useful when transmitting LCD parameters (Only use one line for command) */
+        unsigned int octal_mode: 1 __attribute__((deprecated("This bitfield is deprecated"))); /*!< Deprecated, driver will detect the bus mode automatically */
+        unsigned int quad_mode: 1  __attribute__((deprecated("This bitfield is deprecated"))); /*!< Deprecated, driver will detect the bus mode automatically */
         unsigned int sio_mode: 1;        /*!< Read and write through a single data line (MOSI) */
         unsigned int lsb_first: 1;       /*!< transmit LSB bit first */
         unsigned int cs_high_active: 1;  /*!< CS line is high active */
