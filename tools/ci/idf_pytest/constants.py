62b9e1620df8c8019124bef93d83f6da78fcc250
gaoxu <gaoxu@espressif.com>
2025-07-21 09:24:09 +0800

    feat(sdio): support sdio on esp32c61

--- tools/ci/idf_pytest/constants.py
+++ tools/ci/idf_pytest/constants.py
@@ -141,6 +141,7 @@ ENV_MARKERS = {
     'sdio_master_slave': 'Test sdio multi board, esp32+esp32',
     'sdio_multidev_32_c6': 'Test sdio multi board, esp32+esp32c6',
     'sdio_multidev_p4_c5': 'Test sdio multi board, esp32p4+esp32c5',
+    'sdio_multidev_32_c61': 'Test sdio multi board, esp32+esp32c61',
     'usj_device': 'Test usb_serial_jtag and usb_serial_jtag is used as serial only (not console)',
     'twai_std': 'twai runner with all twai supported targets connect to usb-can adapter',
     'lp_i2s': 'lp_i2s runner tested with hp_i2s',
