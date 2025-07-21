7bc929997ebb4dc06b6cf04a779b6a27cf7c9a2e
wanckl <wanlei@espressif.com>
2025-07-21 11:08:53 +0800

    ci(esp32c61): enable multi dut runner for c61

--- components/esp_driver_uart/test_apps/uart/pytest_uart.py
+++ components/esp_driver_uart/test_apps/uart/pytest_uart.py
@@ -77,9 +77,10 @@ def test_uart_single_dev_psram(case_tester) -> None:  # type: ignore
             dut.expect_unity_test_output()
 
 
-# ESP32 only supports uart wakeup if signal routes through IOMUX, ESP32S3 multi device runner has no psram IDF-12837,
-# ESP32C61 lack of runner IDF-10949, ESP32P4 not yet supported IDF-12839.
-@pytest.mark.temp_skip_ci(targets=['esp32', 'esp32s3', 'esp32c61', 'esp32p4'], reason='no multi-dev runner')
+# ESP32 only supports uart wakeup if signal routes through IOMUX
+# ESP32S3 multi device runner has no psram IDF-12837,
+# ESP32P4 not yet supported IDF-12839.
+@pytest.mark.temp_skip_ci(targets=['esp32', 'esp32s3', 'esp32p4'], reason='no multi-dev runner')
 @pytest.mark.generic_multi_device
 @idf_parametrize('target', ['supported_targets'], indirect=['target'])
 @pytest.mark.parametrize(
