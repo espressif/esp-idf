3d5d0c939ea67148513ce08154fb3b8e0172bab8
laokaiyao <laokaiyao@espressif.com>
2025-07-28 17:50:39 +0800

    fix(i2s_pdm): fixed pdm multi slot clock issue
    Fixed the sample rate slow down issue when using more than 4 slots in  pdm rx mode.

--- components/driver/i2s/i2s_private.h
+++ components/driver/i2s/i2s_private.h
@@ -52,6 +52,7 @@ extern "C" {
 #endif
 
 #define I2S_NULL_POINTER_CHECK(tag, p)          ESP_RETURN_ON_FALSE((p), ESP_ERR_INVALID_ARG, tag, "input parameter '"#p"' is NULL")
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 /**
  * @brief i2s channel state for checking if the operation in under right driver state
