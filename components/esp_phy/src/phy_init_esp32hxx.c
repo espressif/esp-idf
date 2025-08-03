f1dbd59c0876e36a0376630713052de9f37ef143
Wang Mengyang <wangmengyang@espressif.com>
2025-08-03 19:18:58 +0800

    fix(esp_phy): Add protection of consecutive disable operations by single modem source

--- components/esp_phy/src/phy_init_esp32hxx.c
+++ components/esp_phy/src/phy_init_esp32hxx.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2015-2024 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2015-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -130,8 +130,9 @@ void esp_phy_disable(esp_phy_modem_t modem)
 #if CONFIG_ESP_PHY_RECORD_USED_TIME
     phy_record_time(false, modem);
 #endif
+    esp_phy_modem_t saved_modem = phy_get_modem_flag();
     phy_clr_modem_flag(modem);
-    if (phy_get_modem_flag() == 0) {
+    if (saved_modem == modem) {
 
         phy_track_pll_deinit();
         phy_close_rf();
