9a4abbdd3a39e31544ea7e7302a867b60514b526
wuzhenghui <wuzhenghui@espressif.com>
2025-08-06 10:51:30 +0800

    fix(hal): fix esp32c5 mac link bad trigger

--- components/hal/esp32h21/pau_hal.c
+++ components/hal/esp32h21/pau_hal.c
@@ -19,23 +19,6 @@ void pau_hal_set_regdma_entry_link_addr(pau_hal_context_t *hal, pau_regdma_link_
     pau_ll_set_regdma_link_addr((uint32_t)(*link_addr)[0]);
 }
 
-void IRAM_ATTR pau_hal_start_regdma_modem_link(pau_hal_context_t *hal, bool backup_or_restore)
-{
-    pau_ll_clear_regdma_backup_done_intr_state(hal->dev);
-    pau_ll_set_regdma_select_wifimac_link(hal->dev);
-    pau_ll_set_regdma_wifimac_link_backup_direction(hal->dev, backup_or_restore);
-    pau_ll_set_regdma_wifimac_link_backup_start_enable(hal->dev);
-
-    while (!(pau_ll_get_regdma_intr_raw_signal(hal->dev) & PAU_DONE_INT_RAW));
-}
-
-void IRAM_ATTR pau_hal_stop_regdma_modem_link(pau_hal_context_t *hal)
-{
-    pau_ll_set_regdma_wifimac_link_backup_start_disable(hal->dev);
-    pau_ll_set_regdma_deselect_wifimac_link(hal->dev);
-    pau_ll_clear_regdma_backup_done_intr_state(hal->dev);
-}
-
 void IRAM_ATTR pau_hal_start_regdma_extra_link(pau_hal_context_t *hal, bool backup_or_restore)
 {
     pau_ll_clear_regdma_backup_done_intr_state(hal->dev);
