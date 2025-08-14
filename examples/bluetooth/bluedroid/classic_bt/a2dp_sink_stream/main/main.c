52e364f34617b654f20c82f887330f8a5694b7a8
yangfeng <yangfeng@espressif.com>
2025-08-14 14:55:39 +0800

    refactor(examples/classic_bt): Refactor A2DP sink and AVRCP examples
    - Add avrcp_ct_metadata example
    - Add avrcp_absolute_volume example
    - Add the example components about common code for a2dp sink and avrcp

---
+++ examples/bluetooth/bluedroid/classic_bt/a2dp_sink_stream/main/main.c
@@ -0,0 +1,123 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "esp_log.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
+#include "esp_a2dp_api.h"
+
+#include "bt_app_core_utils.h"
+#include "bredr_app_common_utils.h"
+#include "a2dp_sink_common_utils.h"
+#include "a2dp_utils_tags.h"
+#include "a2dp_sink_int_codec_utils.h"
+
+/* device name */
+static const char local_device_name[] = CONFIG_EXAMPLE_LOCAL_DEVICE_NAME;
+
+/* event for stack up */
+enum {
+    BT_APP_EVT_STACK_UP = 0,
+};
+
+/********************************
+ * STATIC FUNCTION DECLARATIONS
+ *******************************/
+
+/* GAP callback function */
+static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param);
+
+/* callback function for A2DP sink */
+static void bt_app_a2d_cb(esp_a2d_cb_event_t event, esp_a2d_cb_param_t *param);
+
+/* callback function for A2DP sink audio data stream */
+static void bt_app_a2d_data_cb(const uint8_t *data, uint32_t len);
+
+/* handler for bluetooth stack enabled events */
+static void bt_av_hdl_stack_evt(uint16_t event, void *p_param);
+
+/*******************************
+ * STATIC FUNCTION DEFINITIONS
+ ******************************/
+
+static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+    bredr_app_gap_evt_def_hdl(event, param);
+}
+
+static void bt_app_a2d_cb(esp_a2d_cb_event_t event, esp_a2d_cb_param_t *param)
+{
+    switch (event) {
+    case ESP_A2D_PROF_STATE_EVT:
+    case ESP_A2D_SNK_PSC_CFG_EVT:
+    case ESP_A2D_SNK_SET_DELAY_VALUE_EVT:
+    case ESP_A2D_SNK_GET_DELAY_VALUE_EVT: {
+        bt_app_work_dispatch(bt_a2d_evt_def_hdl, event, param, sizeof(esp_a2d_cb_param_t), NULL);
+        break;
+    }
+    case ESP_A2D_CONNECTION_STATE_EVT:
+    case ESP_A2D_AUDIO_STATE_EVT:
+    case ESP_A2D_AUDIO_CFG_EVT: {
+        bt_app_work_dispatch(bt_a2d_evt_int_codec_hdl, event, param, sizeof(esp_a2d_cb_param_t), NULL);
+        break;
+    }
+    default:
+        ESP_LOGE(BT_AV_TAG, "Invalid A2DP event: %d", event);
+        break;
+    }
+}
+
+static void bt_app_a2d_data_cb(const uint8_t *data, uint32_t len)
+{
+    bt_a2d_data_hdl(data, len);
+}
+
+static void bt_av_hdl_stack_evt(uint16_t event, void *p_param)
+{
+    ESP_LOGD(BT_AV_TAG, "%s event: %d", __func__, event);
+
+    switch (event) {
+    /* when do the stack up, this event comes */
+    case BT_APP_EVT_STACK_UP: {
+        esp_bt_gap_set_device_name(local_device_name);
+        esp_bt_gap_register_callback(bt_app_gap_cb);
+
+        esp_a2d_register_callback(&bt_app_a2d_cb);
+        assert(esp_a2d_sink_init() == ESP_OK);
+
+        esp_a2d_sink_register_data_callback(bt_app_a2d_data_cb);
+
+        /* Get the default value of the delay value */
+        esp_a2d_sink_get_delay_value();
+        /* Get local device name */
+        esp_bt_gap_get_device_name();
+
+        /* set discoverable and connectable mode, wait to be connected */
+        esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+        break;
+    }
+    /* others */
+    default:
+        ESP_LOGE(BT_AV_TAG, "%s unhandled event: %d", __func__, event);
+        break;
+    }
+}
+
+/*******************************
+ * MAIN ENTRY POINT
+ ******************************/
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(bredr_app_common_init());
+
+    bt_app_task_start_up();
+    /* bluetooth device name, connection mode and profile set up */
+    bt_app_work_dispatch(bt_av_hdl_stack_evt, BT_APP_EVT_STACK_UP, NULL, 0, NULL);
+}
