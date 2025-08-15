eba6b814ba6e7259a88e5cedc9cabe268ee646cb
Peter Dragun <peter.dragun@espressif.com>
2025-08-15 22:43:34 +0200

    feat: Add support for esptool v5 and keep v4 for now

--- tools/test_apps/system/gdb_loadable_elf/test_gdb_loadable_elf_util/loadable_app_serial.py
+++ tools/test_apps/system/gdb_loadable_elf/test_gdb_loadable_elf_util/loadable_app_serial.py
@@ -1,24 +1,25 @@
-# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2025 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Unlicense OR CC0-1.0
 import logging
-from typing import Any, Optional
+from collections import namedtuple
+from typing import Any
 
 import esptool
 from pytest_embedded_idf.app import IdfApp
-from pytest_embedded_serial_esp.serial import EspSerial, EsptoolArgs
+from pytest_embedded_serial_esp.serial import EspSerial
 
 
 class LoadableAppSerial(EspSerial):
     def __init__(
         self,
         app: IdfApp,
-        target: Optional[str] = None,
+        target: str | None = None,
         **kwargs: Any,
     ) -> None:
         self.app = app
 
         if not hasattr(self.app, 'target'):
-            raise ValueError(f'Idf app not parsable. Please check if it\'s valid: {self.app.binary_path}')
+            raise ValueError(f"Idf app not parsable. Please check if it's valid: {self.app.binary_path}")
 
         if target and self.app.target and self.app.target != target:
             raise ValueError(f'Targets do not match. App target: {self.app.target}, Cmd target: {target}.')
@@ -37,17 +38,10 @@ class LoadableAppSerial(EspSerial):
             logging.error('No image file detected. Skipping load ram...')
             return
 
-        f_bin_file = open(self.app.bin_file, 'rb')
-
-        default_kwargs = {
-            'filename': f_bin_file,
-            'chip': self.esp.CHIP_NAME.lower().replace('-', ''),
-        }
-
-        load_ram_args = EsptoolArgs(**default_kwargs)
-
-        try:
+        with open(self.app.bin_file, 'rb') as f_bin_file:
             self.esp.change_baud(460800)
-            esptool.load_ram(self.esp, load_ram_args)
-        finally:
-            f_bin_file.close()
+            try:
+                # esptool v5.0+
+                esptool.load_ram(self.esp, input=f_bin_file)
+            except TypeError:
+                esptool.load_ram(self.esp, namedtuple('args', 'filename')(f_bin_file))
