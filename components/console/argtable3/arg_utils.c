9b11b69a4686520c9bbd2aaec173b3c6c3a55b20
Konstantin Kondrashov <konstantin@espressif.com>
2025-08-20 12:54:25 +0300

    fix(console): Fixes Coverity false positive in va_list initialization

--- components/console/argtable3/arg_utils.c
+++ components/console/argtable3/arg_utils.c
@@ -50,14 +50,14 @@ static void panic(const char* fmt, ...);
 static arg_panicfn* s_panic = panic;
 
 void dbg_printf(const char* fmt, ...) {
-    va_list args;
+    va_list args = {0};
     va_start(args, fmt);
     vfprintf(stderr, fmt, args);
     va_end(args);
 }
 
 static void panic(const char* fmt, ...) {
-    va_list args;
+    va_list args = {0};
     char* s;
 
     va_start(args, fmt);
