b2bca25a984f18b539b75bbb7a7348b51f50560a
wanckl <wanlei@espressif.com>
2025-08-27 15:03:34 +0800

    fix(driver_twai): added ctu_official listen only workaround
    Closes https://github.com/espressif/esp-idf/issues/17461

--- examples/peripherals/twai/twai_network/pytest_twai_network.py
+++ examples/peripherals/twai/twai_network/pytest_twai_network.py
@@ -54,9 +54,6 @@ def generate_target_combinations(target_list: list, count: int = 2) -> list:
     ],
     indirect=True,
 )
-@pytest.mark.temp_skip_ci(
-    targets=['esp32c5,esp32'], reason="C5 twai don't support listen only in network test, see errata issue 5"
-)
 def test_twai_network_multi(dut: Tuple[IdfDut, IdfDut], socket_can: Bus) -> None:  # type: ignore
     """
     Test TWAI network communication between two nodes:
