9c257c4f0ba80990cf39ef0c33d3bb8e336482f2
Mahavir Jain <mahavir@espressif.com>
2025-09-02 12:06:55 +0530

    test: add test case for secure update with FE case

--- tools/test_apps/security/signed_app_no_secure_boot/pytest_signed_app_no_secure_boot.py
+++ tools/test_apps/security/signed_app_no_secure_boot/pytest_signed_app_no_secure_boot.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2023-2025 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Unlicense OR CC0-1.0
+
 import pytest
 from pytest_embedded import Dut
 from pytest_embedded_idf.utils import idf_parametrize
@@ -9,6 +10,14 @@ from pytest_embedded_idf.utils import idf_parametrize
 @idf_parametrize('target', ['esp32c2', 'esp32c3'], indirect=['target'])
 def test_examples_security_on_update_no_secure_boot(dut: Dut) -> None:
     dut.expect(
-        "This app is not signed, but check signature on update is enabled in config. It won't be possible to verify any update.",
+        "This app is not signed, but check signature on update is enabled in config. It won't be possible to verify any update.",  # noqa : E501
         timeout=10,
     )
+
+
+@pytest.mark.host_test
+@pytest.mark.qemu
+@pytest.mark.parametrize('config', ['secure_update_with_fe'], indirect=True)
+@idf_parametrize('target', ['esp32c3'], indirect=['target'])
+def test_examples_secure_update_with_fe_qemu(dut: Dut) -> None:
+    dut.expect('Example for secured signed with no secure boot', timeout=60)
