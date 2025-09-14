c51c432f0810c9bd5adc99b5643cd3112ec28917
Rahul Tank <rahul.tank@espressif.com>
2025-09-14 19:25:57 +0530

    fix(nimble): Added CS service support

--- examples/bluetooth/nimble/ble_chan_sound_initiator/main/ble_chan_initiator.h
+++ examples/bluetooth/nimble/ble_chan_sound_initiator/main/ble_chan_initiator.h
@@ -1,33 +1,29 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+* SPDX-License-Identifier: Unlicense OR CC0-1.0
+*/
 
-#ifndef H_BLEPRPH_
-#define H_BLEPRPH_
+#ifndef H_BLE_CHAN_INITIATOR_
+#define H_BLE_CHAN_INITIATOR_
 
-#include <stdbool.h>
-#include "nimble/ble.h"
 #include "modlog/modlog.h"
-#include "esp_peripheral.h"
+#include "esp_central.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+struct ble_hs_adv_fields;
+struct ble_gap_conn_desc;
+struct ble_hs_cfg;
+union ble_store_value;
+union ble_store_key;
+
+
+#define BLE_HCI_LE_CS_SUBEVENT_DONE_STATUS_COMPLETE   0x0
+#define BLE_HCI_LE_CS_SUBEVENT_DONE_STATUS_PARTIAL   0x1
+#define BLE_HCI_LE_CS_SUBEVENT_DONE_STATUS_ABORTED   0xF
+
+#define LOCAL_PROCEDURE_MEM  1024  // Replace with an appropriate constant value
 struct ble_hs_cfg;
 struct ble_gatt_register_ctxt;
 
@@ -52,10 +48,6 @@ struct ble_gatt_register_ctxt;
 
 /** @brief UUID of the Ranging Data Overwritten Characteristic. **/
 #define BLE_UUID_RAS_RD_OVERWRITTEN_VAL (0x2C19)
-
-
-extern uint16_t hrs_hrm_handle;
-
 
 void gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *arg);
 int gatt_svr_init(void);
--- examples/bluetooth/nimble/bleprph/main/bleprph.h
+++ examples/bluetooth/nimble/bleprph/main/bleprph.h
@@ -39,27 +39,6 @@ struct ble_gatt_register_ctxt;
 #define GATT_SVR_CHR_UNR_ALERT_STAT_UUID      0x2A45
 #define GATT_SVR_CHR_ALERT_NOT_CTRL_PT        0x2A44
 
-#define BLE_UUID_RANGING_SERVICE_VAL (0x185B)
-
-/** @brief UUID of the RAS Features Characteristic. **/
-#define BLE_UUID_RAS_FEATURES_VAL (0x2C14)
-
-/** @brief UUID of the Real-time Ranging Data Characteristic. **/
-#define BLE_UUID_RAS_REALTIME_RD_VAL (0x2C15)
-
-/** @brief UUID of the On-demand Ranging Data Characteristic. **/
-#define BLE_UUID_RAS_ONDEMAND_RD_VAL (0x2C16)
-
-/** @brief UUID of the RAS Control Point Characteristic. **/
-#define BLE_UUID_RAS_CP_VAL (0x2C17)
-
-/** @brief UUID of the Ranging Data Ready Characteristic. **/
-#define BLE_UUID_RAS_RD_READY_VAL (0x2C18)
-
-/** @brief UUID of the Ranging Data Overwritten Characteristic. **/
-#define BLE_UUID_RAS_RD_OVERWRITTEN_VAL (0x2C19)
-
-
 void gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *arg);
 int gatt_svr_init(void);
 
