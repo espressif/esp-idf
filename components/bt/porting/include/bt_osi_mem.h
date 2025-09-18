149f6c56b3b77a380086c66c7d6f3022f7bafab6
zwl <zhaoweiliang@espressif.com>
2025-09-18 21:23:16 +0800

    feat(ble): add get used mem api

--- components/bt/porting/include/bt_osi_mem.h
+++ components/bt/porting/include/bt_osi_mem.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2015-2021 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2015-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -20,6 +20,12 @@ void *bt_osi_mem_calloc_internal(size_t n, size_t size);
 
 void bt_osi_mem_free(void *ptr);
 
+void bt_osi_mem_free_internal(void *ptr);
 #if CONFIG_BT_LE_MEM_CHECK_ENABLED
 void bt_osi_mem_count_limit_set(uint16_t count_limit);
 #endif // CONFIG_BT_LE_MEM_CHECK_ENABLED
+
+#if CONFIG_BT_LE_USED_MEM_STATISTICS_ENABLED
+size_t bt_osi_mem_internal_used_size_get(void);
+size_t bt_osi_mem_used_size_get(void);
+#endif // CONFIG_BT_LE_USED_MEM_STATISTICS_ENABLED
