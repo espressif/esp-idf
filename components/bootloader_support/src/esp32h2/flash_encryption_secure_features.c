b0442de93705af4c5a76d99871ae0f6c190d955c
harshal.patil <harshal.patil@espressif.com>
2025-09-19 12:51:50 +0530

    fix(bootloader_support): Allow pre-programmed XTS-AES psuedo round level efuses
    - The API esp_flash_encryption_set_release_mode() by defualt programs
    the XTS-AES pseudo round level efuse to level low but did not considered
    any existing value that would have been programmed in the efuse bit.

--- components/bootloader_support/src/esp32h2/flash_encryption_secure_features.c
+++ components/bootloader_support/src/esp32h2/flash_encryption_secure_features.c
@@ -36,7 +36,7 @@ esp_err_t esp_flash_encryption_enable_secure_features(void)
 
     esp_efuse_write_field_bit(ESP_EFUSE_DIS_DIRECT_BOOT);
 
-#if defined(CONFIG_SECURE_FLASH_PSEUDO_ROUND_FUNC)
+#if CONFIG_SECURE_FLASH_PSEUDO_ROUND_FUNC
     if (spi_flash_encrypt_ll_is_pseudo_rounds_function_supported()) {
         ESP_LOGI(TAG, "Enable XTS-AES pseudo rounds function...");
         uint8_t xts_pseudo_level = CONFIG_SECURE_FLASH_PSEUDO_ROUND_FUNC_STRENGTH;
