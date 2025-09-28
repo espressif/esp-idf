CodeLinaro-mirror/le_platform_external_wpa_supplicant_8

RSNE/RSNXE overriding for AP
Allow hostapd to be configured to advertised two separate sets of
RSNE/RSNXE parameters so that RSNE/RSNXE can use a reduced set of
capabilities (e.g., WPA2-Personal only) for supporting deployed STAs
that have issues with transition modes while the new override elements
can use a newer security option (e.g., WPA3-Personal only) for STAs that
support the new mechanism.

Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>

src/ap/ap_config.c
@@ -483,10 +483,11 @@ int hostapd_setup_sae_pt(struct hostapd_bss_config *conf)
 
 	if ((conf->sae_pwe == SAE_PWE_HUNT_AND_PECK &&
 	     !hostapd_sae_pw_id_in_use(conf) &&
-	     !wpa_key_mgmt_sae_ext_key(conf->wpa_key_mgmt) &&
+	     !wpa_key_mgmt_sae_ext_key(conf->wpa_key_mgmt |
+				       conf->rsn_override_key_mgmt) &&
 	     !hostapd_sae_pk_in_use(conf)) ||
 	    conf->sae_pwe == SAE_PWE_FORCE_HUNT_AND_PECK ||
-	    !wpa_key_mgmt_sae(conf->wpa_key_mgmt))
+	    !wpa_key_mgmt_sae(conf->wpa_key_mgmt | conf->rsn_override_key_mgmt))
 		return 0; /* PT not needed */
 
 	sae_deinit_pt(ssid->pt);

espressif/esp-idf

components/wpa_supplicant/src/ap/ap_config.c
@@ -150,7 +150,8 @@ int hostapd_setup_sae_pt(struct hostapd_bss_config *conf)
 #ifdef CONFIG_SAE_H2E
     struct hostapd_ssid *ssid = &conf->ssid;
     if (conf->sae_pwe == SAE_PWE_HUNT_AND_PECK ||
-        !wpa_key_mgmt_sae(conf->wpa_key_mgmt))
+        !wpa_key_mgmt_sae(conf->wpa_key_mgmt |
+                          conf->rsn_override_key_mgmt))
         return 0; /* PT not needed */
 
     sae_deinit_pt(ssid->pt);