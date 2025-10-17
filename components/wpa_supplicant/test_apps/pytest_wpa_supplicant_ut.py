77fa2c542063bff434d1e62464e2d792a2633846
Sajia <sajia.ali@espressif.com>
2025-10-17 11:00:25 +0530

    fix(unit_test): Fix esp_wifi and wpa_supplicant unit tests

--- components/wpa_supplicant/test_apps/pytest_wpa_supplicant_ut.py
+++ components/wpa_supplicant/test_apps/pytest_wpa_supplicant_ut.py
@@ -19,6 +19,12 @@ def test_wpa_supplicant_ut(dut: Dut) -> None:
 
 
 @pytest.mark.esp32
+@pytest.mark.esp32c3
+@pytest.mark.esp32s2
+@pytest.mark.esp32s3
+@pytest.mark.esp32c5
+@pytest.mark.esp32c6
+@pytest.mark.esp32c61
 @pytest.mark.wifi_two_dut
 @pytest.mark.parametrize(
     'count',
