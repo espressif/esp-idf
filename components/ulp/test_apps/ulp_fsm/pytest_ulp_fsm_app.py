30b2a15cee7c1059c2e02b826bbd4b6664f8c678
Marius Vikhammer <marius.vikhammer@espressif.com>
2025-10-24 10:15:35 +0800

    ci(ulp): reset board between ulp-fsm tests

--- components/ulp/test_apps/ulp_fsm/pytest_ulp_fsm_app.py
+++ components/ulp/test_apps/ulp_fsm/pytest_ulp_fsm_app.py
@@ -10,4 +10,4 @@ from pytest_embedded import Dut
 @pytest.mark.esp32s3
 @pytest.mark.generic
 def test_ulp_fsm(dut: Dut) -> None:
-    dut.run_all_single_board_cases()
+    dut.run_all_single_board_cases(reset=True)
