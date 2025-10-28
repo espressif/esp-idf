cb48f80f52a07a0077837ab16bc003248e1630fe
Chen Chen <chenchen@espressif.com>
2025-10-28 18:15:00 +0800

    feat(mcpwm): support mcpwm on esp32h4

--- components/esp_hal_mcpwm/esp32h4/mcpwm_periph.c
+++ components/esp_hal_mcpwm/esp32h4/mcpwm_periph.c
@@ -1,18 +1,18 @@
 /*
- * SPDX-FileCopyrightText: 2023-2025 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "soc/soc.h"
+#include "soc/interrupts.h"
 #include "hal/mcpwm_periph.h"
 #include "soc/mcpwm_reg.h"
 #include "soc/gpio_sig_map.h"
 
-const soc_mcpwm_signal_desc_t soc_mcpwm_signals[1] = {
+const soc_mcpwm_signal_desc_t soc_mcpwm_signals[2] = {
     {
         .module_name = "MCPWM0",
-        .irq_id = ETS_MCPWM0_INTR_SOURCE,
+        .irq_id = ETS_PWM0_INTR_SOURCE,
         .operators = {
             [0] = {
                 .generators = {
@@ -78,13 +78,81 @@ const soc_mcpwm_signal_desc_t soc_mcpwm_signals[1] = {
                 .sync_sig = PWM0_SYNC2_IN_IDX
             }
         }
+    },
+    {
+        .module_name = "MCPWM1",
+        .irq_id = ETS_PWM1_INTR_SOURCE,
+        .operators = {
+            [0] = {
+                .generators = {
+                    [0] = {
+                        .pwm_sig = PWM1_OUT0A_IDX
+                    },
+                    [1] = {
+                        .pwm_sig = PWM1_OUT0B_IDX
+                    }
+                }
+            },
+            [1] = {
+                .generators = {
+                    [0] = {
+                        .pwm_sig = PWM1_OUT1A_IDX
+                    },
+                    [1] = {
+                        .pwm_sig = PWM1_OUT1B_IDX
+                    }
+                }
+            },
+            [2] = {
+                .generators = {
+                    [0] = {
+                        .pwm_sig = PWM1_OUT2A_IDX
+                    },
+                    [1] = {
+                        .pwm_sig = PWM1_OUT2B_IDX
+                    }
+                }
+            }
+        },
+        .gpio_faults = {
+            [0] = {
+                .fault_sig = PWM1_F0_IN_IDX
+            },
+            [1] = {
+                .fault_sig = PWM1_F1_IN_IDX
+            },
+            [2] = {
+                .fault_sig = PWM1_F2_IN_IDX
+            }
+        },
+        .captures = {
+            [0] = {
+                .cap_sig = PWM1_CAP0_IN_IDX
+            },
+            [1] = {
+                .cap_sig = PWM1_CAP1_IN_IDX
+            },
+            [2] = {
+                .cap_sig = PWM1_CAP2_IN_IDX
+            }
+        },
+        .gpio_synchros = {
+            [0] = {
+                .sync_sig = PWM1_SYNC0_IN_IDX
+            },
+            [1] = {
+                .sync_sig = PWM1_SYNC1_IN_IDX
+            },
+            [2] = {
+                .sync_sig = PWM1_SYNC2_IN_IDX
+            }
+        }
     }
-
 };
 
 /**
  * MCPWM Registers to be saved during sleep retention
- * - Timer Configuration registers, e.g.: MCPWM_TIMER_SYNCI_CFG_REG, MCPWM_TIMER0_CFG0_REG, MCPWM_TIMER0_CFG1_REG, MCPWM_TIMER0_CFG1_REG
+ * - Timer Configuration registers, e.g.: MCPWM_TIMER_SYNCI_CFG_REG, MCPWM_TIMER0_CFG0_REG, MCPWM_TIMER0_CFG1_REG, MCPWM_TIMER0_CFG1_REG，MCPWM_TIMER0_SYNC_REG
  * - Operator Configuration registers, e.g.: MCPWM_OPERATOR_TIMERSEL_REG
  *   |- Generator Configuration registers, e.g.: MCPWM_GEN0_STMP_CFG_REG, MCPWM_GEN0_TSTMP_A_REG, MCPWM_GEN0_TSTMP_B_REG, MCPWM_GEN0_CFG0_REG, MCPWM_GEN0_FORCE_REG, MCPWM_GEN0_A_REG, MCPWM_GEN0_B_REG
  *   |- Dead Time Configuration registers, e.g.: MCPWM_DT0_CFG_REG, MCPWM_DT0_FED_CFG_REG, MCPWM_DT0_RED_CFG_REG
@@ -95,37 +163,40 @@ const soc_mcpwm_signal_desc_t soc_mcpwm_signals[1] = {
  * - ETM Configurations, e.g.: MCPWM_EVT_EN_REG, MCPWM_EVT_EN2_REG, MCPWM_TASK_EN_REG, MCPWM_OP0_TSTMP_E1_REG, MCPWM_OP0_TSTMP_E2_REG, MCPWM_OP1_TSTMP_E1_REG, MCPWM_OP1_TSTMP_E2_REG, MCPWM_OP2_TSTMP_E1_REG, MCPWM_OP2_TSTMP_E2_REG
  * - Misc Configurations, e.g.: MCPWM_UPDATE_CFG_REG
 */
+
 #define MCPWM_RETENTION_REGS_CNT 67
-#define MCPWM_RETENTION_REGS_BASE (DR_REG_MCPWM_BASE + 0x4)
+#define MCPWM_RETENTION_REGS_BASE(i) DR_REG_MCPWM_BASE(i)
 static const uint32_t mcpwm_regs_map[4] = {0xf7fff777, 0x3f7ffdff, 0xff8c, 0x0};
-static const regdma_entries_config_t mcpwm_regs_retention[] = {
-    // backup stage: save configuration registers
-    // restore stage: restore the configuration registers
+static const regdma_entries_config_t mcpwm0_regdma_entries[] = {
     [0] = {
         .config = REGDMA_LINK_ADDR_MAP_INIT(REGDMA_MCPWM_LINK(0x00),
-                                            MCPWM_RETENTION_REGS_BASE, MCPWM_RETENTION_REGS_BASE,
+                                            MCPWM_RETENTION_REGS_BASE(0), MCPWM_RETENTION_REGS_BASE(0),
                                             MCPWM_RETENTION_REGS_CNT, 0, 0,
                                             mcpwm_regs_map[0], mcpwm_regs_map[1],
                                             mcpwm_regs_map[2], mcpwm_regs_map[3]),
-        .owner = ENTRY(0) | ENTRY(2)
+        .owner = ENTRY(0) | ENTRY(2),
     },
-    // restore stage: trigger a forced update of all active registers
-    [1] = {
-        .config = REGDMA_LINK_WRITE_INIT(REGDMA_MCPWM_LINK(0x01),
-                                         MCPWM_UPDATE_CFG_REG(0), MCPWM_GLOBAL_FORCE_UP, MCPWM_GLOBAL_FORCE_UP_M, 1, 0),
-        .owner = ENTRY(0) | ENTRY(2)
-    },
-    [2] = {
-        .config = REGDMA_LINK_WRITE_INIT(REGDMA_MCPWM_LINK(0x02),
-                                         MCPWM_UPDATE_CFG_REG(0), 0, MCPWM_GLOBAL_FORCE_UP_M, 1, 0),
-        .owner = ENTRY(0) | ENTRY(2)
+};
+static const regdma_entries_config_t mcpwm1_regdma_entries[] = {
+    [0] = {
+        .config = REGDMA_LINK_ADDR_MAP_INIT(REGDMA_MCPWM_LINK(0x00),
+                                            MCPWM_RETENTION_REGS_BASE(1), MCPWM_RETENTION_REGS_BASE(1),
+                                            MCPWM_RETENTION_REGS_CNT, 0, 0,
+                                            mcpwm_regs_map[0], mcpwm_regs_map[1],
+                                            mcpwm_regs_map[2], mcpwm_regs_map[3]),
+        .owner = ENTRY(0) | ENTRY(2),
     },
 };
 
-const mcpwm_reg_retention_info_t mcpwm_reg_retention_info[1] = {
+const mcpwm_reg_retention_info_t mcpwm_reg_retention_info[2] = {
     [0] = {
-        .regdma_entry_array = mcpwm_regs_retention,
-        .array_size = ARRAY_SIZE(mcpwm_regs_retention),
+        .regdma_entry_array = mcpwm0_regdma_entries,
+        .array_size = ARRAY_SIZE(mcpwm0_regdma_entries),
         .retention_module = SLEEP_RETENTION_MODULE_MCPWM0
+    },
+    [1] = {
+        .regdma_entry_array = mcpwm0_regdma_entries,
+        .array_size = ARRAY_SIZE(mcpwm1_regdma_entries),
+        .retention_module = SLEEP_RETENTION_MODULE_MCPWM1
     },
 };
