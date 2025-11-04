dbdefc1306d35475e24e403650e289848630d135
Rasmus Buurman <rasmus@rbuurman.de>
2025-11-05 00:18:03 +0100

    fix(examples): Fix typos in NimBLE example comments

--- examples/bluetooth/ble_get_started/nimble/NimBLE_Beacon/main/src/gap.c
+++ examples/bluetooth/ble_get_started/nimble/NimBLE_Beacon/main/src/gap.c
@@ -51,7 +51,7 @@ static void start_advertising(void) {
     adv_fields.le_role = BLE_GAP_LE_ROLE_PERIPHERAL;
     adv_fields.le_role_is_present = 1;
 
-    /* Set advertiement fields */
+    /* Set advertisement fields */
     rc = ble_gap_adv_set_fields(&adv_fields);
     if (rc != 0) {
         ESP_LOGE(TAG, "failed to set advertising data, error code: %d", rc);
@@ -74,7 +74,7 @@ static void start_advertising(void) {
         return;
     }
 
-    /* Set non-connetable and general discoverable mode to be a beacon */
+    /* Set non-connectable and general discoverable mode to be a beacon */
     adv_params.conn_mode = BLE_GAP_CONN_MODE_NON;
     adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN;
 
