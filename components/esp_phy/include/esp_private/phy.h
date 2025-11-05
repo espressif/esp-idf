5c8dd4724fe51926885f1f4671964fd8b69928e1
sibeibei <sibeibei@espressif.com>
2025-11-05 19:57:06 +0800

    feat(phy): update PHY libraries and improve PLL temperature tracking
    1. Allow customization of temperature deltas for PHY track PLL.
    
    2. Fix abnormal PHY state which may trigger task watchdog timeout.

--- components/esp_phy/include/esp_private/phy.h
+++ components/esp_phy/include/esp_private/phy.h
@@ -272,6 +272,16 @@ void esp_phy_sleep_data_deinit(void);
  */
 void phy_wait_freq_hw_hop_done(void);
 
+#if CONFIG_ESP_PHY_PLL_TRACK_TEMP_DEBUG
+/**
+ * @brief Set the temperature delta for PHY track pll
+ *
+ * @param     debug_flag  Debug flag for PHY temperature tracking
+ * @param     track_temp   Temperature delta for PHY temperature tracking
+ */
+void phy_track_temp_debug(uint8_t debug_flag, uint8_t track_temp);
+#endif
+
 #ifdef __cplusplus
 }
 #endif
