e7054752ed64693b833727dee217cdf6ab0095c0
Konstantin Kondrashov <konstantin@espressif.com>
2025-11-06 11:09:55 +0200

    feat(ulp): Removes ambiguity between ticks and cycles in ULP APIs
    Closes https://github.com/espressif/esp-idf/issues/17820

--- components/ulp/lp_core/lp_core/include/ulp_lp_core_spi.h
+++ components/ulp/lp_core/lp_core/include/ulp_lp_core_spi.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2024-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -39,26 +39,26 @@ typedef struct {
  * from the device.
  *
  * @param trans_desc        LP SPI transaction configuration descriptor
- * @param ticks_to_wait     Operation timeout in CPU cycles. Set to -1 to wait forever.
+ * @param cycles_to_wait    Operation timeout in CPU cycles. Set to -1 to wait forever.
  *
  * @return esp_err_t        ESP_OK when successful
  *                          ESP_ERR_INVALID_ARG if the configuration is invalid
  *                          ESP_ERR_TIMEOUT when the operation times out
  */
-esp_err_t lp_core_lp_spi_master_transfer(lp_spi_transaction_t *trans_desc, int32_t ticks_to_wait);
+esp_err_t lp_core_lp_spi_master_transfer(lp_spi_transaction_t *trans_desc, int32_t cycles_to_wait);
 
 /**
  * @brief Initiate an LP SPI transaction in slave mode to receive data from an SPI master and optionally transmit data
  * back to the master.
  *
  * @param trans_desc        LP SPI transaction configuration descriptor
- * @param ticks_to_wait     Operation timeout in CPU cycles. Set to -1 to wait forever.
+ * @param cycles_to_wait    Operation timeout in CPU cycles. Set to -1 to wait forever.
  *
  * @return esp_err_t        ESP_OK when successful
  *                          ESP_ERR_INVALID_ARG if the configuration is invalid
  *                          ESP_ERR_TIMEOUT when the operation times out
  */
-esp_err_t lp_core_lp_spi_slave_transfer(lp_spi_transaction_t *trans_desc, int32_t ticks_to_wait);
+esp_err_t lp_core_lp_spi_slave_transfer(lp_spi_transaction_t *trans_desc, int32_t cycles_to_wait);
 
 #ifdef __cplusplus
 }
