dfe4a6e5f78e56702089e2ff317be59f49ee6de8
armando <douyiwen@espressif.com>
2025-11-07 14:18:09 +0800

    feat(hal):graudate the camera hal driver into a new component

--- components/esp_hal_cam/include/hal/cam_periph.h
+++ components/esp_hal_cam/include/hal/cam_periph.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2024-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,6 +8,9 @@
 
 #include "soc/soc_caps.h"
 #include "soc/periph_defs.h"
+#if SOC_HAS(LCDCAM_CAM)
+#include "hal/cam_ll.h"
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,13 +21,13 @@ typedef struct {
     struct {
         const shared_periph_module_t module;
         const int irq_id;
-        const int data_sigs[SOC_LCDCAM_CAM_DATA_WIDTH_MAX];
+        const int data_sigs[CAM_LL_DATA_WIDTH_MAX];
         const int hsync_sig;
         const int vsync_sig;
         const int pclk_sig;
         const int de_sig;
         const int clk_sig;
-    } buses[SOC_LCDCAM_CAM_PERIPH_NUM];
+    } buses[CAM_LL_PERIPH_NUM];
 } cam_signal_conn_t;
 
 extern const cam_signal_conn_t cam_periph_signals;
