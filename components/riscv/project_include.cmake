0c1d917f782b03030ada09c8ad9b618acc7a329b
Alexey Lapshin <alexey.lapshin@espressif.com>
2025-11-10 18:25:39 +0700

    feat(build): propagate compiler flags from files to toolchain.cmake
    This change improves build consistency across external projects integrated
    through CMake by ensuring that compiler flags defined in configuration files
    are passed correctly to the toolchain. It covers the majority of use cases,
    as external projects are typically also CMake-based. For projects that use
    a custom build system, users will still need to specify the required flags
    manually.

--- components/riscv/project_include.cmake
+++
@@ -1,5 +0,0 @@
-# avoid esp-clang "unused-command-line-argument" warning
-if(NOT (CMAKE_C_COMPILER_ID MATCHES "Clang") )
-    set(compile_options "-nostartfiles")
-    idf_build_set_property(COMPILE_OPTIONS "${compile_options}" APPEND)
-endif()
