1c08b38b27302037d4672829273a6556f2e70653
Alexey Lapshin <alexey.lapshin@espressif.com>
2025-11-10 18:25:39 +0700

    feat(build): propagate compiler flags from files to toolchain.cmake
    This change improves build consistency across external projects integrated
    through CMake by ensuring that compiler flags defined in configuration files
    are passed correctly to the toolchain. It covers the majority of use cases,
    as external projects are typically also CMake-based. For projects that use
    a custom build system, users will still need to specify the required flags
    manually.

--- tools/cmake/toolchain-esp32s3.cmake
+++ tools/cmake/toolchain-esp32s3.cmake
@@ -1,23 +1,2 @@
-include($ENV{IDF_PATH}/tools/cmake/utilities.cmake)
-
-set(CMAKE_SYSTEM_NAME Generic)
-
-set(CMAKE_C_COMPILER xtensa-esp32s3-elf-gcc)
-set(CMAKE_CXX_COMPILER xtensa-esp32s3-elf-g++)
-set(CMAKE_ASM_COMPILER xtensa-esp32s3-elf-gcc)
 set(_CMAKE_TOOLCHAIN_PREFIX xtensa-esp32s3-elf-)
-
-remove_duplicated_flags("-mlongcalls \
-                         -fno-builtin-memcpy -fno-builtin-memset -fno-builtin-bzero \
-                         -fno-builtin-stpcpy -fno-builtin-strncpy \
-                         ${CMAKE_C_FLAGS}" UNIQ_CMAKE_C_FLAGS)
-set(CMAKE_C_FLAGS "${UNIQ_CMAKE_C_FLAGS}" CACHE STRING "C Compiler Base Flags" FORCE)
-remove_duplicated_flags("-mlongcalls \
-                         -fno-builtin-memcpy -fno-builtin-memset -fno-builtin-bzero \
-                         -fno-builtin-stpcpy -fno-builtin-strncpy \
-                         ${CMAKE_CXX_FLAGS}" UNIQ_CMAKE_CXX_FLAGS)
-set(CMAKE_CXX_FLAGS "${UNIQ_CMAKE_CXX_FLAGS}" CACHE STRING "C++ Compiler Base Flags" FORCE)
-remove_duplicated_flags("-mlongcalls ${CMAKE_ASM_FLAGS}" UNIQ_CMAKE_ASM_FLAGS)
-set(CMAKE_ASM_FLAGS "${UNIQ_CMAKE_ASM_FLAGS}" CACHE STRING "ASM Compiler Base Flags" FORCE)
-remove_duplicated_flags("-nostartfiles ${CMAKE_EXE_LINKER_FLAGS}" UNIQ_CMAKE_SAFE_EXE_LINKER_FLAGS)
-set(CMAKE_EXE_LINKER_FLAGS "${UNIQ_CMAKE_SAFE_EXE_LINKER_FLAGS}" CACHE STRING "Linker Base Flags" FORCE)
+include($ENV{IDF_PATH}/tools/cmake/toolchain.cmake)
