222d81976aaa4496f242fbfef485d513814e3b03
Song Ruo Jing <songruojing@espressif.com>
2025-11-11 14:32:35 +0800

    fix(gpio): oe control by register only take effect when func sel is GPIO

--- components/hal/esp32c5/include/hal/gpio_ll.h
+++ components/hal/esp32c5/include/hal/gpio_ll.h
@@ -489,7 +489,7 @@ static inline void gpio_ll_iomux_in(gpio_dev_t *hw, uint32_t gpio, uint32_t sign
 }
 
 /**
-  * @brief Configure the source of output enable signal for the GPIO pin.
+  * @brief Configure the source of output enable signal for the pad (only takes effect if func sel is selected to be GPIO).
   *
   * @param hw Peripheral GPIO hardware instance address.
   * @param gpio_num GPIO number of the pad.
