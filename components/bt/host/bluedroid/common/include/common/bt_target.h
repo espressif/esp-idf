f6900e0375aed5c98f72026ba900de7233004cce
zhiweijian <zhiweijian@espressif.com>
2025-11-19 10:23:39 +0800

    fix(ble/bluedroid): Delete batch_scan related code

--- components/bt/host/bluedroid/common/include/common/bt_target.h
+++ components/bt/host/bluedroid/common/include/common/bt_target.h
@@ -1582,11 +1582,6 @@
 #endif
 #endif
 
-
-#ifndef BLE_BATCH_SCAN_INCLUDED
-#define BLE_BATCH_SCAN_INCLUDED  TRUE
-#endif
-
 /******************************************************************************
 **
 ** ATT/GATT Protocol/Profile Settings
