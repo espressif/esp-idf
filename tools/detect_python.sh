0865ae64b2b3bc3ff527d7994ffb492698cd41cd
garywill <garywill@disroot.org>
2025-11-20 12:03:00 +0800

    Make detect_python.sh POSIX compatitable

--- tools/detect_python.sh
+++ tools/detect_python.sh
@@ -10,16 +10,20 @@
 OLDEST_PYTHON_SUPPORTED_MAJOR=3
 OLDEST_PYTHON_SUPPORTED_MINOR=10
 
+PYTHON_CANDIDATES="python3
+python
+python3.10
+python3.11
+python3.12
+python3.13"
 
-if [[ -z "$ESP_PYTHON" ]]; then
-    PYTHON_CANDIDATES=(python3 python python3.10 python3.11 python3.12 python3.13)
-else
+if [ -n "$ESP_PYTHON" ]; then
     echo "Reading ESP_PYTHON from environment: \"$ESP_PYTHON\""
-    PYTHON_CANDIDATES=("$ESP_PYTHON")
+    PYTHON_CANDIDATES="$ESP_PYTHON"
 fi
 ESP_PYTHON=  # Unset it. Will assign after candidate check
 
-for p_cmd in "${PYTHON_CANDIDATES[@]}"; do
+while IFS= read -r p_cmd; do
     "$p_cmd" --version >/dev/null 2>&1 || continue
     echo "Checking \"$p_cmd\" ..."
 
@@ -28,11 +32,14 @@ for p_cmd in "${PYTHON_CANDIDATES[@]}"; do
 
     export ESP_PYTHON="$p_cmd"
     break
-done
+done << EOF
+$PYTHON_CANDIDATES
+EOF
+
 unset PYTHON_CANDIDATES
 
-[[ -n "$ESP_PYTHON" ]] &&  "$ESP_PYTHON" --version 2>/dev/null || {
+if [ -z "$ESP_PYTHON" ]; then
     echo "Python ${OLDEST_PYTHON_SUPPORTED_MAJOR}.${OLDEST_PYTHON_SUPPORTED_MINOR}+ is not installed! Please see the documentation for how to install it." >&2
     exit 1
-}
+fi
 echo "\"$ESP_PYTHON\" has been detected"
