31369940101c7001debf40199e8eb5dd6c0e1752
wuzhenghui <wuzhenghui@espressif.com>
2025-11-25 10:37:06 +0800

    feat(esp_hw_support): re-enable P4 sleep wakeup tests for rev3.0

--- components/esp_pm/test_apps/esp_pm/main/test_pm.c
+++ components/esp_pm/test_apps/esp_pm/main/test_pm.c
@@ -69,7 +69,11 @@ static const int test_freqs[] = {32, CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ, 64, 48, 32
 static const int test_freqs[] = {CONFIG_XTAL_FREQ, CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ, 80, CONFIG_XTAL_FREQ, 80,
                                  CONFIG_XTAL_FREQ / 2, CONFIG_XTAL_FREQ}; // C2 xtal has 40/26MHz option
 #elif CONFIG_IDF_TARGET_ESP32P4
+#if CONFIG_ESP32P4_SELECTS_REV_LESS_V3
 static const int test_freqs[] = {40, CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ, 90, 40, 90, 10, 90, 20, 40, 360, 90, 180, 90, 40};
+#else
+static const int test_freqs[] = {40, CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ, 100, 40, 100, 10, 100, 20, 40, 400, 100, 200, 100, 40};
+#endif
 #else
 static const int test_freqs[] = {240, 40, 160, 240, 80, 40, 240, 40, 80, 10, 80, 20, 40};
 #endif
