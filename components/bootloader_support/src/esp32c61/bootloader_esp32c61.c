01ec965252427077da4e381e65dabc46fcef1fe7
wuzhenghui <wuzhenghui@espressif.com>
2025-11-25 21:40:51 +0800

    fix(esp_system): fix mspi write stuck after cpu/digital reset on c5/c61

--- components/bootloader_support/src/esp32c61/bootloader_esp32c61.c
+++ components/bootloader_support/src/esp32c61/bootloader_esp32c61.c
@@ -44,6 +44,7 @@
 #include "hal/lpwdt_ll.h"
 #include "hal/regi2c_ctrl_ll.h"
 #include "hal/brownout_ll.h"
+#include "hal/axi_icm_ll.h"
 
 ESP_LOG_ATTR_TAG(TAG, "boot.esp32c61");
 
@@ -86,6 +87,9 @@ static void bootloader_super_wdt_auto_feed(void)
 
 static inline void bootloader_hardware_init(void)
 {
+    // Clear bit reset_event_bypass to ensure that the system bus is also reset during a core reset (WDT),
+    // preventing bus freezing caused by an incorrect MSPI core reset in ROM.
+    axi_icm_ll_reset_with_core_reset(true);
     _regi2c_ctrl_ll_master_enable_clock(true); // keep ana i2c mst clock always enabled in bootloader
     regi2c_ctrl_ll_master_configure_clock();
 }
