8c67e8bc00686a9b92a2e1d22241242ec442faa8
armando <douyiwen@espressif.com>
2025-12-09 16:14:54 +0800

    ci(isp): reenable tests

--- components/esp_driver_isp/test_apps/isp/pytest_isp.py
+++ components/esp_driver_isp/test_apps/isp/pytest_isp.py
@@ -1,7 +1,13 @@
-# SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2024-2025 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
-from pytest_embedded_idf import IdfDut
+import pytest
+from pytest_embedded import Dut
+from pytest_embedded_idf.utils import idf_parametrize
+from pytest_embedded_idf.utils import soc_filtered_targets
 
 
-def test_isp(dut: IdfDut) -> None:
+@pytest.mark.camera
+@pytest.mark.ov5647
+@idf_parametrize('target', soc_filtered_targets('SOC_ISP_SUPPORTED == 1'), indirect=['target'])
+def test_isp(dut: Dut) -> None:
     dut.run_all_single_board_cases()
