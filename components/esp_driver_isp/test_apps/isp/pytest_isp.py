502a10b5dcd2e9e3d7f661aac54191ad769ffd8a
armando <douyiwen@espressif.com>
2025-12-15 11:54:38 +0800

    ci: enable isp default tests

--- components/esp_driver_isp/test_apps/isp/pytest_isp.py
+++ components/esp_driver_isp/test_apps/isp/pytest_isp.py
@@ -8,6 +8,13 @@ from pytest_embedded_idf.utils import soc_filtered_targets
 
 @pytest.mark.camera
 @pytest.mark.ov5647
+@pytest.mark.parametrize(
+    'config',
+    [
+        ('default'),
+    ],
+    indirect=True,
+)
 @idf_parametrize('target', soc_filtered_targets('SOC_ISP_SUPPORTED == 1'), indirect=['target'])
 def test_isp(dut: Dut) -> None:
     dut.run_all_single_board_cases()
