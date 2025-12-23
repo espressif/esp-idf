731018494904fab3210157f28c1cd40bbf71dae7
Ashish Sharma <ashish.sharma@espressif.com>
2025-12-23 10:47:08 +0800

    feat: migrates mbedtls to PSA APIs

--- components/mbedtls/port/esp_hardware.c
+++ components/mbedtls/port/esp_hardware.c
@@ -3,22 +3,13 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#include <mbedtls/build_info.h>
 
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "esp_random.h"
-#include "mbedtls/esp_mbedtls_random.h"
-#if defined(MBEDTLS_PLATFORM_GET_ENTROPY_ALT)
-#include "psa/crypto.h"
-#endif
-
 #include <entropy_poll.h>
-
-#ifndef MBEDTLS_ENTROPY_HARDWARE_ALT
-#error "MBEDTLS_ENTROPY_HARDWARE_ALT should always be set in ESP-IDF"
-#endif
+#include "psa/crypto.h"
 
 int mbedtls_hardware_poll( void *data,
                            unsigned char *output, size_t len, size_t *olen )
@@ -28,17 +19,7 @@ int mbedtls_hardware_poll( void *data,
     return 0;
 }
 
-int mbedtls_esp_random(void *ctx, unsigned char *buf, size_t len)
-{
-    (void) ctx; // unused
-    esp_fill_random(buf, len);
-    return 0;
-}
-
-#if defined(MBEDTLS_PLATFORM_GET_ENTROPY_ALT)
-int mbedtls_platform_get_entropy(unsigned char *output, size_t output_size,
-    size_t *output_len, size_t *entropy_content);
-
+#if defined(MBEDTLS_PSA_DRIVER_GET_ENTROPY)
 psa_status_t mbedtls_psa_external_get_random(
     mbedtls_psa_external_random_context_t *context,
     uint8_t *output, size_t output_size, size_t *output_length)
@@ -50,17 +31,4 @@ psa_status_t mbedtls_psa_external_get_random(
     *output_length = output_size;
     return PSA_SUCCESS;
 }
-
-int mbedtls_platform_get_entropy(unsigned char *output, size_t output_size,
-    size_t *output_len, size_t *entropy_content)
-{
-    if (output == NULL || output_size == 0 || output_len == NULL || entropy_content == NULL) {
-        return -1;
-    }
-
-    esp_fill_random(output, output_size);
-    *output_len = output_size;
-    *entropy_content = 8 * output_size;
-    return 0;
-}
-#endif // MBEDTLS_PLATFORM_GET_ENTROPY_ALT
+#endif // MBEDTLS_PSA_DRIVER_GET_ENTROPY
