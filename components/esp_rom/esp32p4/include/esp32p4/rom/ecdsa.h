88fc8952e55c3162b4470b331da5e399c1e0e1ea
harshal.patil <harshal.patil@espressif.com>
2025-12-26 11:49:15 +0530

    test(ota): Add tests for verifying app build's SBv2 ECDSA signature verify APIs

--- components/esp_rom/esp32p4/include/esp32p4/rom/ecdsa.h
+++ components/esp_rom/esp32p4/include/esp32p4/rom/ecdsa.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2023-2025 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -12,11 +12,13 @@
 extern "C" {
 #endif
 
-#define ETS_DIGEST_LEN 32 /* SHA-256, bytes */
+#define ETS_DIGEST_SHA256_LEN 32 /* SHA-256, bytes */
+#define ETS_DIGEST_SHA384_LEN 48 /* SHA-384, bytes */
 
 typedef enum {
     ECDSA_CURVE_P192 = 1,
-    ECDSA_CURVE_P256 = 2
+    ECDSA_CURVE_P256 = 2,
+    ECDSA_CURVE_P384 = 3
 } ECDSA_CURVE;
 
 int ets_ecdsa_verify(const uint8_t *key, const uint8_t *sig, ECDSA_CURVE curve_id, const uint8_t *digest, uint8_t *verified_digest);
