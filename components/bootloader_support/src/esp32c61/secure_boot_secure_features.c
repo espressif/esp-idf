f2068bb34ffb93f2fb540c9df6d5b45a9bd6be28
nilesh.kale <nilesh.kale@espressif.com>
2026-01-06 14:43:15 +0530

    fix: remove reference to closed JIRA tracker IDF-10694

--- components/bootloader_support/src/esp32c61/secure_boot_secure_features.c
+++ components/bootloader_support/src/esp32c61/secure_boot_secure_features.c
@@ -40,8 +40,6 @@ esp_err_t esp_secure_boot_enable_secure_features(void)
     ESP_LOGI(TAG, "Disable hardware & software JTAG...");
     esp_efuse_write_field_bit(ESP_EFUSE_DIS_PAD_JTAG);
     esp_efuse_write_field_bit(ESP_EFUSE_DIS_USB_JTAG);
-    // TODO in IDF-10694
-    // esp_efuse_write_field_cnt(ESP_EFUSE_SOFT_DIS_JTAG, ESP_EFUSE_SOFT_DIS_JTAG[0]->bit_count);
 #else
     ESP_LOGW(TAG, "Not disabling JTAG - SECURITY COMPROMISED");
 #endif
