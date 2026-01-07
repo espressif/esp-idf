cbf4640668e2a553fb56313d72c7ad5ce33fe4a0
Zhi Wei Jian <zhiweijian@espressif.com>
2026-01-07 17:24:34 +0800

    fix(ble/bluedroid): Fixed bt buuld error
    fix(ble/bluedroid): disable power management if bt is disabled
    
    
    (cherry picked from commit 6f27074c1ee4486cedf8adeb1a57e67b9cb6a525)
    
    Co-authored-by: zhiweijian <zhiweijian@espressif.com>

--- components/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h
+++ components/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h
@@ -823,9 +823,10 @@ typedef enum {
     ESP_BLE_DUPLICATE_SCAN_EXCEPTIONAL_ALL_LIST                   = 0xFFFF,                 /*!< duplicate scan exceptional all list */
 } esp_duplicate_scan_exceptional_list_type_t;
 
+#endif //#if (BLE_42_FEATURE_SUPPORT == TRUE)
+
 typedef uint8_t esp_duplicate_info_t[ESP_BD_ADDR_LEN];
 
-#endif //#if (BLE_42_FEATURE_SUPPORT == TRUE)
 
 #if (BLE_50_FEATURE_SUPPORT == TRUE)
 #define ESP_BLE_GAP_SET_EXT_ADV_PROP_NONCONN_NONSCANNABLE_UNDIRECTED      (0 << 0) /*!< Non-Connectable and Non-Scannable Undirected advertising */
@@ -1894,7 +1895,6 @@ typedef union {
         esp_bt_status_t status;                     /*!< Indicate the add or remove whitelist operation success status */
         esp_ble_wl_operation_t wl_operation;        /*!< The value is ESP_BLE_WHITELIST_ADD if add address to whitelist operation success, ESP_BLE_WHITELIST_REMOVE if remove address from the whitelist operation success */
     } update_whitelist_cmpl;                        /*!< Event parameter of ESP_GAP_BLE_UPDATE_WHITELIST_COMPLETE_EVT */
-#if (BLE_42_FEATURE_SUPPORT == TRUE)
     /**
      * @brief ESP_GAP_BLE_UPDATE_DUPLICATE_EXCEPTIONAL_LIST_COMPLETE_EVT
      */
@@ -1904,7 +1904,6 @@ typedef union {
         uint16_t         length;                     /*!< The length of device_info */
         esp_duplicate_info_t device_info;           /*!< device information, when subcode is ESP_BLE_DUPLICATE_EXCEPTIONAL_LIST_CLEAN, the value is invalid */
     } update_duplicate_exceptional_list_cmpl;       /*!< Event parameter of ESP_GAP_BLE_UPDATE_DUPLICATE_EXCEPTIONAL_LIST_COMPLETE_EVT */
-#endif // #if (BLE_42_FEATURE_SUPPORT == TRUE)
     /**
      * @brief ESP_GAP_BLE_SET_CHANNELS_EVT
       */
--- components/bt/host/bluedroid/bta/dm/include/bta_dm_int.h
+++ components/bt/host/bluedroid/bta/dm/include/bta_dm_int.h
@@ -1609,12 +1609,14 @@ typedef union {
     tBTA_DM_API_CFG_COEX_STATUS cfg_coex_status;
 #endif
     tBTA_DM_API_SEND_VENDOR_HCI_CMD vendor_hci_cmd;
+#if CLASSIC_BT_INCLUDED
     tBTA_DM_API_CONFIG_EIR config_eir;
 
     tBTA_DM_API_SET_AFH_CHANNELS set_afh_channels;
     tBTA_DM_API_PAGE_TO_SET set_page_timeout;
     tBTA_DM_API_PAGE_TO_GET get_page_timeout;
     tBTA_DM_API_SET_ACL_PKT_TYPES set_acl_pkt_types;
+#endif /* CLASSIC_BT_INCLUDED */
 #if (ENC_KEY_SIZE_CTRL_MODE != ENC_KEY_SIZE_CTRL_MODE_NONE)
     tBTA_DM_API_SET_MIN_ENC_KEY_SIZE set_min_enc_key_size;
 #endif
