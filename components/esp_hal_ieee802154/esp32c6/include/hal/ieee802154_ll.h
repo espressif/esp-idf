3e1721143187b4357b8e725052b24bedf73802ec
zwx <zhangwenxu@espressif.com>
2026-01-07 15:28:05 +0800

    feat(ieee802154): separated the 802.15.4 HAL codes from the HAL component

--- components/esp_hal_ieee802154/esp32c6/include/hal/ieee802154_ll.h
+++ components/esp_hal_ieee802154/esp32c6/include/hal/ieee802154_ll.h
@@ -1,12 +1,11 @@
 /*
- * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
 
-#include <stdbool.h>
 #include "hal/ieee802154_common_ll.h"
 
 #define IEEE802154_TXPOWER_VALUE_MAX  20
