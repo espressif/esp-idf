c61a9d1ab6a84c119e9dc7350cefa2f3a7f0603c
zwx <zhangwenxu@espressif.com>
2026-01-20 15:43:44 +0800

    fix(802.15.4): fixed security frame configuration

--- components/ieee802154/driver/esp_ieee802154_sec.c
+++ components/ieee802154/driver/esp_ieee802154_sec.c
@@ -1,28 +1,31 @@
 /*
- * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2023-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "sdkconfig.h"
 #include <string.h>
 #include "esp_err.h"
 #include "hal/ieee802154_ll.h"
 #include "esp_ieee802154_frame.h"
 #include "esp_ieee802154_sec.h"
+#include "esp_ieee802154_util.h"
 
 #include "esp_ieee802154.h"
-static bool s_is_security = false;
 
 void ieee802154_transmit_security_config(uint8_t *frame, uint8_t *key, uint8_t *addr)
 {
+#if !CONFIG_IEEE802154_TEST
+    IEEE802154_ASSERT(ieee802154_frame_is_security_enabled(frame));
+#endif
     ieee802154_ll_set_security_addr(addr);
     ieee802154_ll_set_security_key(key);
     ieee802154_ll_set_security_offset(ieee802154_frame_get_security_payload_offset(frame));
-    s_is_security = true;
+    ieee802154_ll_set_transmit_security(true);
 }
 
-void ieee802154_sec_update(void)
+void ieee802154_sec_clear(void)
 {
-    ieee802154_ll_set_transmit_security(s_is_security);
-    s_is_security = false;
+    ieee802154_ll_set_transmit_security(false);
 }
