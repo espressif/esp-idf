3ffec317dd4ac620232e91f4c749b01f3f282d23
zwx <zhangwenxu@espressif.com>
2026-01-20 15:43:44 +0800

    fix(802.15.4): fixed security frame configuration

--- components/ieee802154/driver/esp_ieee802154_dev.c
+++ components/ieee802154/driver/esp_ieee802154_dev.c
@@ -122,7 +122,6 @@ static IRAM_ATTR void event_end_process(void)
 {
     ieee802154_etm_channel_clear(IEEE802154_ETM_CHANNEL0);
     ieee802154_etm_channel_clear(IEEE802154_ETM_CHANNEL1);
-    ieee802154_ll_set_transmit_security(false);
     ieee802154_timer0_stop();
     ieee802154_timer1_stop();
 }
@@ -301,6 +300,7 @@ IEEE802154_NOINLINE static bool stop_rx(void)
 IEEE802154_NOINLINE static bool stop_tx_ack(void)
 {
     ieee802154_set_cmd(IEEE802154_CMD_STOP);
+    ieee802154_sec_clear();
 
     ieee802154_receive_done((uint8_t *)s_rx_frame[s_rx_index], &s_rx_frame_info[s_rx_index]);
 
@@ -314,6 +314,7 @@ IEEE802154_NOINLINE static bool stop_tx(void)
     ieee802154_ll_events events;
 
     ieee802154_set_cmd(IEEE802154_CMD_STOP);
+    ieee802154_sec_clear();
 
     events = ieee802154_ll_get_events();
 
@@ -468,6 +469,7 @@ static IRAM_ATTR void next_operation(void)
 
 static IRAM_ATTR void isr_handle_tx_done(void)
 {
+    ieee802154_sec_clear();
     event_end_process();
 #if !CONFIG_IEEE802154_TEST
     if (!ieee802154_is_supported_frame_type(ieee802154_frame_get_type(s_tx_frame))) {
@@ -528,7 +530,6 @@ static IRAM_ATTR void isr_handle_rx_done(void)
                 // Send the Enh-Ack frame if generator succeeds.
                 ieee802154_ll_set_tx_addr(s_enh_ack_frame);
                 s_tx_frame = s_enh_ack_frame;
-                ieee802154_sec_update();
                 ieee802154_ll_enhack_generate_done_notify();
                 ieee802154_set_state(IEEE802154_STATE_TX_ENH_ACK);
 #endif
@@ -548,6 +549,7 @@ static IRAM_ATTR void isr_handle_rx_done(void)
 
 static IRAM_ATTR void isr_handle_ack_tx_done(void)
 {
+    ieee802154_sec_clear();
     extcoex_rx_stage_start();
     ieee802154_receive_done((uint8_t *)s_rx_frame[s_rx_index], &s_rx_frame_info[s_rx_index]);
     NEEDS_NEXT_OPT(true);
@@ -602,13 +604,17 @@ static IRAM_ATTR void isr_handle_rx_phase_rx_abort(ieee802154_ll_rx_abort_reason
 
 static IRAM_ATTR void isr_handle_tx_ack_phase_rx_abort(ieee802154_ll_rx_abort_reason_t rx_abort_reason)
 {
+    ieee802154_sec_clear();
     event_end_process();
 #if CONFIG_IEEE802154_TEST
     uint32_t rx_status = ieee802154_ll_get_rx_status();
 #endif
     switch (rx_abort_reason) {
-    case IEEE802154_RX_ABORT_BY_RX_STOP:
     case IEEE802154_RX_ABORT_BY_TX_ACK_STOP:
+#if CONFIG_IEEE802154_TEST
+        ieee802154_receive_done((uint8_t *)s_rx_frame[s_rx_index], &s_rx_frame_info[s_rx_index]);
+#endif
+    case IEEE802154_RX_ABORT_BY_RX_STOP:
     case IEEE802154_RX_ABORT_BY_ED_STOP:
     case IEEE802154_RX_ABORT_BY_SFD_TIMEOUT:
     case IEEE802154_RX_ABORT_BY_CRC_ERROR:
@@ -652,11 +658,19 @@ static IRAM_ATTR void isr_handle_tx_ack_phase_rx_abort(ieee802154_ll_rx_abort_re
 
 static IRAM_ATTR void isr_handle_tx_abort(ieee802154_ll_tx_abort_reason_t tx_abort_reason)
 {
+    ieee802154_sec_clear();
     event_end_process();
     switch (tx_abort_reason) {
     case IEEE802154_TX_ABORT_BY_RX_ACK_STOP:
+#if CONFIG_IEEE802154_TEST
+        ieee802154_inner_transmit_failed(s_tx_frame, ESP_IEEE802154_TX_ERR_NO_ACK);
+        break;
+#endif
     case IEEE802154_TX_ABORT_BY_TX_STOP:
         // do nothing
+#if CONFIG_IEEE802154_TEST
+        ieee802154_inner_transmit_failed(s_tx_frame, ESP_IEEE802154_TX_ERR_ABORT);
+#endif
         break;
     case IEEE802154_TX_ABORT_BY_RX_ACK_SFD_TIMEOUT:
     case IEEE802154_TX_ABORT_BY_RX_ACK_CRC_ERROR:
@@ -734,7 +748,6 @@ IEEE802154_NOINLINE static void ieee802154_isr(void *arg)
     ieee802154_ll_events events = ieee802154_ll_get_events();
     ieee802154_ll_rx_abort_reason_t rx_abort_reason = ieee802154_ll_get_rx_abort_reason();
     ieee802154_ll_tx_abort_reason_t tx_abort_reason = ieee802154_ll_get_tx_abort_reason();
-
     IEEE802154_PROBE(events);
 
     ieee802154_ll_clear_events(events);
@@ -923,7 +936,6 @@ IEEE802154_STATIC void tx_init(const uint8_t *frame)
     s_tx_frame = (uint8_t *)frame;
     stop_current_operation();
     ieee802154_pib_update();
-    ieee802154_sec_update();
 
     ieee802154_ll_set_tx_addr(s_tx_frame);
 
@@ -967,6 +979,7 @@ esp_err_t ieee802154_transmit(const uint8_t *frame, bool cca)
         } else {
             ieee802154_inner_transmit_failed(frame, ESP_IEEE802154_TX_ERR_ABORT);
         }
+        ieee802154_sec_clear();
         IEEE802154_TX_DEFERRED_NUMS_UPDATE();
         ieee802154_exit_critical();
         return ESP_OK;
