e7033132c19a50042c201551ecaf1192b45d5836
C.S.M <caosenmiao@espressif.com>
2026-01-27 17:59:46 +0800

    fix(esp32s31): update esp32s31 rom after its freeze

--- components/esp_rom/esp32s31/esp_rom_caps.h
+++ components/esp_rom/esp32s31/esp_rom_caps.h
@@ -8,6 +8,7 @@
 
 #define ESP_ROM_HAS_CRC_LE                  (1) // ROM CRC library supports Little Endian
 #define ESP_ROM_HAS_CRC_BE                  (1) // ROM CRC library supports Big Endian
+#define ESP_ROM_HAS_JPEG_DECODE             (1) // ROM has JPEG decode library
 #define ESP_ROM_UART_CLK_IS_XTAL            (1) // UART clock source is selected to XTAL in ROM
 #define ESP_ROM_USB_SERIAL_DEVICE_NUM       (5) // The serial port ID (UART, USB, ...) of USB_SERIAL_JTAG in the ROM.
 #define ESP_ROM_USB_OTG_NUM                 (4) // The serial port ID (UART, USB, ...) of USB_OTG_CDC in the ROM.
