a5801fa24b670859370f0669ac697ae6a8c49bd3
Adam Múdry <adam.mudry@espressif.com>
2026-01-28 13:37:23 +0100

    fix(fatfs): fatfs test_apps remove esp32c3 from sdcard_sdmode test

--- components/fatfs/test_apps/sdcard/pytest_fatfs_sdcard.py
+++ components/fatfs/test_apps/sdcard/pytest_fatfs_sdcard.py
@@ -1,11 +1,10 @@
-# SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2026 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 import pytest
 from pytest_embedded import Dut
 
 
 @pytest.mark.esp32
-@pytest.mark.esp32c3
 @pytest.mark.sdcard_sdmode
 @pytest.mark.parametrize(
     'config',
