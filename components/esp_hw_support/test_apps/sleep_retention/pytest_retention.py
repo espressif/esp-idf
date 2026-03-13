08be3a6c85936a94ceddf5f9c2ed6e1547678333
wuzhenghui <wuzhenghui@espressif.com>
2026-03-13 19:04:54 +0800

    feat(esp_hw_support): add sleep_reentent UT case back

---
+++ components/esp_hw_support/test_apps/sleep_retention/pytest_retention.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2022-2026 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: CC0-1.0
+import pytest
+from pytest_embedded import Dut
+from pytest_embedded_idf.utils import idf_parametrize
+from pytest_embedded_idf.utils import soc_filtered_targets
+
+
+@pytest.mark.generic
+@pytest.mark.parametrize(
+    'config',
+    [
+        'default',
+        'xip_psram',
+    ],
+    indirect=True,
+)
+@idf_parametrize('target', soc_filtered_targets('SOC_PAU_SUPPORTED == 1'), indirect=['target'])
+def test_sleep_retention(dut: Dut) -> None:
+    dut.run_all_single_board_cases()
