08aa130027acf2e14046419d50d5b65ff28ebef3
zhangyanjiao <zhangyanjiao@espressif.com>
2026-03-26 14:35:04 +0800

    feat(wifi): Added new API to return value for vendor ie cb

--- components/esp_wifi/include/esp_private/wifi.h
+++ components/esp_wifi/include/esp_private/wifi.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2015-2025 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2015-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -845,6 +845,28 @@ void pm_beacon_offset_funcs_empty_init(void);
  * @brief This API is not context safe and enable easy fragment just for internal test only.
  */
 void esp_wifi_enable_easy_fragment(bool enable);
+
+/**
+  * @brief     Function signature for received Vendor-Specific Information Element callback with return value.
+  * @param     ctx Context argument, as passed to esp_wifi_set_vendor_ie_cb() when registering callback.
+  * @param     type Information element type, based on frame type received.
+  * @param     sa Source 802.11 address.
+  * @param     vnd_ie Pointer to the vendor specific element data received.
+  * @param     rssi Received signal strength indication.
+  */
+typedef esp_err_t (*esp_vendor_ie_cb_with_ret_t)(void *ctx, wifi_vendor_ie_type_t type, const uint8_t sa[6], const vendor_ie_data_t *vnd_ie, int rssi);
+
+/**
+  * @brief     Register Vendor-Specific Information Element monitoring callback.
+  *
+  * @param     cb   Callback function with return value
+  * @param     ctx  Context argument, passed to callback function.
+  *
+  * @return
+  *    - ESP_OK: succeed
+  *    - ESP_ERR_WIFI_NOT_INIT: WiFi is not initialized by esp_wifi_init
+  */
+esp_err_t esp_wifi_set_vendor_ie_with_ret_cb(esp_vendor_ie_cb_with_ret_t cb, void *ctx);
 
 #ifdef __cplusplus
 }
