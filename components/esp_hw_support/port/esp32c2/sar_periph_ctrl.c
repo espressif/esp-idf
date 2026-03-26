e9120b757286b29c8a669d735329c7546c9794c8
gaoxu <gaoxu@espressif.com>
2026-03-26 15:58:32 +0800

    fix(tsens): fix temp_sensor affect by adc reset

--- components/esp_hw_support/port/esp32c2/sar_periph_ctrl.c
+++ components/esp_hw_support/port/esp32c2/sar_periph_ctrl.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2022-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -19,6 +19,9 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "esp_private/sar_periph_ctrl.h"
+#include "esp_private/critical_section.h"
+#include "esp_private/adc_share_hw_ctrl.h"
+#include "esp_private/periph_ctrl.h"
 #include "hal/sar_ctrl_ll.h"
 #include "hal/adc_ll.h"
 
@@ -127,4 +130,24 @@ void sar_periph_ctrl_adc_continuous_power_acquire(void)
 void sar_periph_ctrl_adc_continuous_power_release(void)
 {
     abort();  //c2 not supported, should never reach here
+}
+
+/*------------------------------------------------------------------------------
+* ADC Reset
+*----------------------------------------------------------------------------*/
+void sar_periph_ctrl_adc_reset(void)
+{
+    ADC_BUS_CLK_ATOMIC() {
+        adc_ll_reset_register();
+    }
+}
+
+void adc_reset_lock_acquire(void)
+{
+    // Empty implementation
+}
+
+void adc_reset_lock_release(void)
+{
+    // Empty implementation
 }
