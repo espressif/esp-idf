6449eaeeab3524e97cb1dd4954f38b0b928500fb
Ashish Sharma <ashish.sharma@espressif.com>
2026-04-01 18:09:38 +0800

    feat(mbedtls): update to version 3.6.6

--- components/idf_test/include/esp32s2/idf_performance_target.h
+++ components/idf_test/include/esp32s2/idf_performance_target.h
@@ -17,11 +17,11 @@
 #define IDF_PERFORMANCE_MAX_TIME_SHA512_32KB                                    900
 
 #define IDF_PERFORMANCE_MAX_RSA_2048KEY_PUBLIC_OP                               13500
-#define IDF_PERFORMANCE_MAX_RSA_2048KEY_PRIVATE_OP                              650000
+#define IDF_PERFORMANCE_MAX_RSA_2048KEY_PRIVATE_OP                              420000
 #define IDF_PERFORMANCE_MAX_RSA_3072KEY_PUBLIC_OP                               36000
 #define IDF_PERFORMANCE_MAX_RSA_3072KEY_PRIVATE_OP                              960000
 #define IDF_PERFORMANCE_MAX_RSA_4096KEY_PUBLIC_OP                               62000
-#define IDF_PERFORMANCE_MAX_RSA_4096KEY_PRIVATE_OP                              2850000
+#define IDF_PERFORMANCE_MAX_RSA_4096KEY_PRIVATE_OP                              1850000
 
 #define IDF_PERFORMANCE_MAX_ADC_CONTINUOUS_STD_ATTEN3_NO_FILTER                 3
 #define IDF_PERFORMANCE_MAX_ADC_CONTINUOUS_STD_ATTEN3_FILTER_2                  3
