709656d8a16530867588a0757a1dcb622f40f801
Alexey Lapshin <alexey.lapshin@espressif.com>
2026-04-16 23:59:32 +0700

    fix(libc): picolibc: workaround wrong pthread_exit declaration for c++

--- components/newlib/platform_include/sys/cdefs.h
+++ components/newlib/platform_include/sys/cdefs.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2025-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,4 +9,15 @@
 #if CONFIG_LIBC_PICOLIBC_NEWLIB_COMPATIBILITY
 #include <stdint.h>
 #include <stddef.h>
+#endif
+
+#if CONFIG_LIBC_PICOLIBC // TODO IDF-15569
+#undef __noreturn
+#if __has_attribute(__noreturn__)
+#define __noreturn __attribute__((__noreturn__))
+#else
+#define __noreturn
+#endif
+#else
+#define __noreturn
 #endif
