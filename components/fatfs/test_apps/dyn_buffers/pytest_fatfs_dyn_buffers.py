4f7b1af0f945060f6f210b74750e554c130a6fe6
LiPeng <lipeng@espressif.com>
2026-04-24 18:59:28 +0800

    fix(fatfs): fix a memory leak bug when FF_USE_DYN_BUFFER was enabled

--- components/fatfs/test_apps/dyn_buffers/pytest_fatfs_dyn_buffers.py
+++ components/fatfs/test_apps/dyn_buffers/pytest_fatfs_dyn_buffers.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022-2025 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2026 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 import pytest
 from pytest_embedded import Dut
@@ -15,8 +15,4 @@ from pytest_embedded_idf.utils import idf_parametrize
 )
 @idf_parametrize('target', ['esp32'], indirect=['target'])
 def test_fatfs_flash_dyn_buffers(config: str, dut: Dut) -> None:
-    dut.expect('Mounting FATFS')
-    dut.expect('Mounted')
-    dut.expect('Unmounting FATFS')
-    dut.expect('Unmounted')
-    dut.expect('Done')
+    dut.run_all_single_board_cases(timeout=30)
