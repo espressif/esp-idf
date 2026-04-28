a9d73601a2bfba5c6b33ccc880449f42eb346898
Zhou Xiao <zhouxiao@espressif.com>
2026-04-28 13:41:36 +0800

    feat(ble): add BLE UART bridge core

---
+++ tools/ble/ble_uart_bridge/src/core/models.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+from __future__ import annotations
+
+from enum import Enum
+
+from .constants import NUS_RX_CHAR_UUID
+from .constants import NUS_SERVICE_UUID
+from .constants import NUS_TX_CHAR_UUID
+
+
+class ConnectionState(str, Enum):
+    DISCONNECTED = 'DISCONNECTED'
+    CONNECTING = 'CONNECTING'
+    CONNECTED = 'CONNECTED'
+
+
+class DeviceInfo:
+    device_id: str
+    name: str | None
+    rssi: int | None
+
+
+class BLEUARTProfile:
+    service_uuid: str = NUS_SERVICE_UUID
+    rx_char_uuid: str = NUS_RX_CHAR_UUID
+    tx_char_uuid: str = NUS_TX_CHAR_UUID
