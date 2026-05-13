85de8836d78a6152c230ebd5fa501de4a36ada76
ding huan <dinghuan@espressif.com>
2026-05-13 19:45:25 +0800

    ci: Add wifi static buffer case

--- components/esp_wifi/test_apps/wifi_connect/pytest_wifi_connect.py
+++ components/esp_wifi/test_apps/wifi_connect/pytest_wifi_connect.py
@@ -6,7 +6,14 @@ from pytest_embedded_idf.utils import idf_parametrize
 
 
 @pytest.mark.two_duts
-@pytest.mark.parametrize('count', [2], indirect=True)
+@pytest.mark.parametrize(
+    'count, config',
+    [
+        (2, 'default'),
+        (2, 'static_tx_buf'),
+    ],
+    indirect=True,
+)
 @idf_parametrize(
     'target', ['esp32', 'esp32c3', 'esp32s2', 'esp32s3', 'esp32c5', 'esp32c6', 'esp32c61'], indirect=['target']
 )
@@ -20,6 +27,7 @@ def test_wifi_connect_cases(case_tester: CaseTester) -> None:  # type: ignore
     'count, config, baud',
     [
         (2, 'esp32c2_xtal26m', '74880'),
+        (2, 'esp32c2_xtal26m_static_tx_buf', '74880'),
     ],
     indirect=True,
 )
