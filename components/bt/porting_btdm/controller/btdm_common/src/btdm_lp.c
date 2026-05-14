36b9f73e11b80b3c13551e203394b0db5c6ba099
cjin <jinchen@espressif.com>
2026-05-14 14:09:00 +0800

    fix(ble): fix btdm lp log tags compilation error

--- components/bt/porting_btdm/controller/btdm_common/src/btdm_lp.c
+++ components/bt/porting_btdm/controller/btdm_common/src/btdm_lp.c
@@ -259,19 +259,19 @@ btdm_lp_modem_state_init(void)
 
     esp_err_t err = sleep_retention_module_init(SLEEP_RETENTION_MODULE_BLE_MAC, &init_param);
     if (err != ESP_OK) {
-        ESP_LOGE(NIMBLE_PORT_LOG_TAG, "BT sleep retention init error");
+        ESP_LOGE(BTDM_LOG_TAG, "BT sleep retention init error");
         return err;
     }
 
     err = sleep_retention_module_allocate(SLEEP_RETENTION_MODULE_BLE_MAC);
     if (err != ESP_OK) {
-        ESP_LOGE(NIMBLE_PORT_LOG_TAG, "BT sleep retention allocate error");
+        ESP_LOGE(BTDM_LOG_TAG, "BT sleep retention allocate error");
         return err;
     }
 
     err = sleep_retention_module_attach(SLEEP_RETENTION_MODULE_BLE_MAC);
     if (err != ESP_OK) {
-        ESP_LOGE(NIMBLE_PORT_LOG_TAG, "BT sleep retention attach error");
+        ESP_LOGE(BTDM_LOG_TAG, "BT sleep retention attach error");
     }
     return err;
 }
@@ -281,19 +281,19 @@ btdm_lp_modem_state_deinit(void)
 {
     esp_err_t err = sleep_retention_module_detach(SLEEP_RETENTION_MODULE_BLE_MAC);
     if (err != ESP_OK) {
-        ESP_LOGE(NIMBLE_PORT_LOG_TAG, "BT sleep retention detach error");
+        ESP_LOGE(BTDM_LOG_TAG, "BT sleep retention detach error");
         assert(err == ESP_OK);
     }
 
     err = sleep_retention_module_free(SLEEP_RETENTION_MODULE_BLE_MAC);
     if (err != ESP_OK) {
-        ESP_LOGE(NIMBLE_PORT_LOG_TAG, "BT sleep retention free error");
+        ESP_LOGE(BTDM_LOG_TAG, "BT sleep retention free error");
         assert(err == ESP_OK);
     }
 
     err = sleep_retention_module_deinit(SLEEP_RETENTION_MODULE_BLE_MAC);
     if (err != ESP_OK) {
-        ESP_LOGE(NIMBLE_PORT_LOG_TAG, "BT sleep retention deinit error");
+        ESP_LOGE(BTDM_LOG_TAG, "BT sleep retention deinit error");
         assert(err == ESP_OK);
     }
 }
