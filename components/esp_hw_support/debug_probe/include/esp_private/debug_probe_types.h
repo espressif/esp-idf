e20db709aa6ab9dcc069554eb6f52432c3f7aa93
morris <maoshengrong@espressif.com>
2026-05-25 12:18:01 +0800

    feat(debug): add esp32s31 debug probe support
    - add ESP32-S31 debug probe LL, target IDs, capability macros, and GPIO signal mapping
    - allow LP probe channels to configure four signal bytes while keeping 16 GPIO outputs
    - add HP and LP probe read tests, including LP 32-bit read verification
    - keep existing vbat LP probe usage aligned with the new LP channel behavior

--- components/esp_hw_support/debug_probe/include/esp_private/debug_probe_types.h
+++ components/esp_hw_support/debug_probe/include/esp_private/debug_probe_types.h
@@ -22,12 +22,12 @@ extern "C" {
 #if SOC_DEBUG_PROBE_SUPPORTED
 #define DEBUG_PROBE_MAX_OUTPUT_WIDTH   SOC_DEBUG_PROBE_MAX_OUTPUT_WIDTH
 #define DEBUG_PROBE_GPIO_NUMS_NONE      { [0 ... (SOC_DEBUG_PROBE_MAX_OUTPUT_WIDTH - 1)] = -1 }
-typedef soc_debug_probe_target_t debug_probe_target_t;
+typedef soc_debug_probe_target_hp_t debug_probe_target_hp_t;
 typedef soc_debug_probe_target_lp_t debug_probe_target_lp_t;
 #else
 #define DEBUG_PROBE_MAX_OUTPUT_WIDTH   16
 #define DEBUG_PROBE_GPIO_NUMS_NONE     { [0 ... 15] = -1 }
-typedef int debug_probe_target_t;
+typedef int debug_probe_target_hp_t;
 typedef int debug_probe_target_lp_t;
 #endif
 
--- components/soc/esp32s31/include/soc/debug_probe_targets.h
+++ components/soc/esp32s31/include/soc/debug_probe_targets.h
@@ -14,37 +14,29 @@ extern "C" {
  * @brief Target module that we want to attach the HP probe to
  */
 typedef enum {
-    DEBUG_PROBE_TARGET_HP_MIPI_CSI_HOST = 0,  // MIPI CSI Host
+    DEBUG_PROBE_TARGET_HP_USB_OTG_HS = 0,     // USB OTG HS
     DEBUG_PROBE_TARGET_HP_AXI_GDMA = 1,       // AXI GDMA
-    DEBUG_PROBE_TARGET_HP_I3C = 2,            // I3C
-    DEBUG_PROBE_TARGET_HP_TCM = 3,            // HP TCM Memory
-    DEBUG_PROBE_TARGET_HP_USB_OTG_FS = 4,     // USB OTG FS (1.1)
-    DEBUG_PROBE_TARGET_HP_MIPI_CSI_BRG = 5,   // MIPI CSI Bridge
+    DEBUG_PROBE_TARGET_HP_TCM = 3,            // HP TCM RAM
     DEBUG_PROBE_TARGET_HP_MSPI_PSRAM = 6,     // MSPI PSRAM
-    DEBUG_PROBE_TARGET_HP_MIPI_DSI_BRG = 7,   // MIPI DSI Bridge
-    DEBUG_PROBE_TARGET_HP_DW_GDMA = 8,        // DW GDMA
-    DEBUG_PROBE_TARGET_HP_AXI_ICM = 9,        // AXI ICM matrix
-    DEBUG_PROBE_TARGET_HP_L2_MEM = 10,        // L2 Memory
+    DEBUG_PROBE_TARGET_HP_AXI_ICM = 9,        // HP ICM matrix
     DEBUG_PROBE_TARGET_HP_BIT_SCRAMBLER = 11, // Bit Scrambler
     DEBUG_PROBE_TARGET_HP_MSPI_FLASH = 12,    // MSPI Flash
-    DEBUG_PROBE_TARGET_HP_L1_CACHE = 13,      // L1 Cache
+    DEBUG_PROBE_TARGET_HP_CACHE = 13,         // Cache
     DEBUG_PROBE_TARGET_HP_CORE = 14,          // HP Core
-    DEBUG_PROBE_TARGET_HP_L2_CACHE = 15,      // L2 Cache
     DEBUG_PROBE_TARGET_HP_LP_PROBE_IN = 16,   // LP System Probe In
-    DEBUG_PROBE_TARGET_HP_USJ = 17,           // USB Serial JTAG
-    DEBUG_PROBE_TARGET_HP_EMAC = 18,          // EMAC
+    DEBUG_PROBE_TARGET_HP_EMAC = 18,          // GMAC
     DEBUG_PROBE_TARGET_HP_JPEG = 19,          // JPEG
     DEBUG_PROBE_TARGET_HP_PPA = 20,           // PPA
     DEBUG_PROBE_TARGET_HP_DMA2D = 21,         // DMA2D
     DEBUG_PROBE_TARGET_HP_LEDC0 = 22,         // LEDC0
     DEBUG_PROBE_TARGET_HP_SDMMC = 23,         // SDMMC
-    DEBUG_PROBE_TARGET_HP_ISP = 24,           // ISP
-    DEBUG_PROBE_TARGET_HP_USB_OTG_HS = 25,    // USB OTG HS (2.0)
-    DEBUG_PROBE_TARGET_HP_H264 = 26,          // H264
+    DEBUG_PROBE_TARGET_HP_LEDC1 = 24,         // LEDC1
+    DEBUG_PROBE_TARGET_HP_MCPWM2 = 25,        // MCPWM2
+    DEBUG_PROBE_TARGET_HP_MCPWM3 = 26,        // MCPWM3
     DEBUG_PROBE_TARGET_HP_MCPWM0 = 27,        // MCPWM0
     DEBUG_PROBE_TARGET_HP_MCPWM1 = 28,        // MCPWM1
     DEBUG_PROBE_TARGET_HP_REGDMA = 29,        // REGDMA
-    DEBUG_PROBE_TARGET_HP_PVT = 30,           // PVT
+    DEBUG_PROBE_TARGET_HP_ZERO_DET = 30,      // Zero detection
 } soc_debug_probe_target_hp_t;
 
 /**
@@ -52,16 +44,17 @@ typedef enum {
  */
 typedef enum {
     DEBUG_PROBE_TARGET_LP_PMU = 0,           // LP PMU
-    DEBUG_PROBE_TARGET_LP_TCM_RAM = 1,       // LP TCM RAM
-    DEBUG_PROBE_TARGET_LP_TCM_ROM = 2,       // LP TCM ROM
+    DEBUG_PROBE_TARGET_LP_TCM = 1,           // LP TCM
+    DEBUG_PROBE_TARGET_LP_TSENS = 2,         // LP TSENS
     DEBUG_PROBE_TARGET_LP_CORE = 3,          // LP Core
     DEBUG_PROBE_TARGET_LP_ADC = 4,           // LP ADC
     DEBUG_PROBE_TARGET_LP_SPI = 5,           // LP SPI
     DEBUG_PROBE_TARGET_LP_TOUCH = 6,         // LP Touch
     DEBUG_PROBE_TARGET_LP_ANA_PERI = 7,      // LP Analog Peripherals
     DEBUG_PROBE_TARGET_LP_AON_CLKRST = 8,    // LP AON Clock/Reset
-    DEBUG_PROBE_TARGET_LP_MISC = 9,          // LP System Misc
-    DEBUG_PROBE_TARGET_LP_HP_PROBE_IN = 10,  // HP System Probe In
+    DEBUG_PROBE_TARGET_LP_TRNG = 9,          // LP TRNG
+    DEBUG_PROBE_TARGET_LP_MISC = 10,         // LP System Misc
+    DEBUG_PROBE_TARGET_LP_HP_PROBE_IN = 11,  // HP System Probe In
 } soc_debug_probe_target_lp_t;
 
 #ifdef __cplusplus
