5934c5b7f10d479574adc70afdea191e82040dc7
Song Ruo Jing <songruojing@espressif.com>
2026-05-25 15:24:44 +0800

    fix(spi_flash): gpspi flash clock could reach higher frequency

--- components/soc/esp32h4/include/soc/clk_tree_defs.h
+++ components/soc/esp32h4/include/soc/clk_tree_defs.h
@@ -274,7 +274,7 @@ typedef enum {
  * @brief Type of SPI clock source.
  */
 typedef enum {
-    SPI_CLK_SRC_DEFAULT = SOC_MOD_CLK_PLL_F48M,     /*!< Select XTAL as SPI source clock */
+    SPI_CLK_SRC_DEFAULT = SOC_MOD_CLK_PLL_F48M,     /*!< Select PLL_F48M as SPI source clock */
     SPI_CLK_SRC_XTAL = SOC_MOD_CLK_XTAL,            /*!< Select XTAL as SPI source clock */
     SPI_CLK_SRC_PLL_F48M = SOC_MOD_CLK_PLL_F48M,    /*!< Select PLL_48M as SPI source clock */
     SPI_CLK_SRC_RC_FAST = SOC_MOD_CLK_RC_FAST,      /*!< Select RC_FAST as SPI source clock */
