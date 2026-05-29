c5e03fd55c4837acbc021312a0c16b9192248ce6
harshal.patil <harshal.patil@espressif.com>
2026-05-29 17:29:45 +0530

    fix(esp_rom): Patch ets_ecdsa_verify() to include signature bounds check

--- components/esp_rom/esp32h4/esp_rom_caps.h
+++ components/esp_rom/esp32h4/esp_rom_caps.h
@@ -23,5 +23,6 @@
 #define ESP_ROM_USB_OTG_NUM                 (-1) // No USB_OTG CDC in the ROM, set -1 for Kconfig usage.
 #define ESP_ROM_WDT_INIT_PATCH              (1) // ROM version does not configure the clock
 #define ESP_ROM_RAM_APP_NEEDS_MMU_INIT      (1) // ROM doesn't init cache MMU when it's a RAM APP, needs MMU hal to init
+#define ESP_ROM_ECDSA_VERIFY_PATCH (1) // ROM ets_ecdsa_verify API requires a software patch
 #define ESP_ROM_BOOTLOADER_OFFSET_FLASH     (0x2000) // Bootloader offset in flash determined by the ROM bootloader
 #define ESP_ROM_CACHE_WRITEBACK_NEEDS_SYNC_TWICE_NO_MAP (1) // ROM cache writeback related needs patch to avoid sync loss, no map parameter
