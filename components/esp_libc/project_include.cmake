72682b9f365c6ea6f710a8b4142f832fb62a8578
Alexey Lapshin <alexey.lapshin@espressif.com>
2026-06-10 13:31:09 +0700

    Revert "fix(libc): nano: add -lnosys to linking to avoid CMake configuration issues"
    This reverts commit f54b5d4747849ccc07858eebb8bc975fdc34c937.

--- components/esp_libc/project_include.cmake
+++ components/esp_libc/project_include.cmake
@@ -1,5 +1,6 @@
-function(_get_modified_specs_path out_var specs_filename modification)
-    set(modified_specs_path "${CMAKE_CURRENT_BINARY_DIR}/specs/${specs_filename}")
+# Get picolibc specs option with --gc-sections flag removed
+function(get_picolibc_specs_path out_var)
+    set(modified_specs_path "${CMAKE_CURRENT_BINARY_DIR}/specs/picolibc.specs")
     set(${out_var} "${modified_specs_path}" PARENT_SCOPE)
 
     # Return if modified specs file already exists to avoid regeneration
@@ -7,48 +8,28 @@ function(_get_modified_specs_path out_var specs_filename modification)
         return()
     endif()
 
-    # Find the original specs file in the toolchain directory
+    # Find the original picolibc.specs file in the toolchain directory
     execute_process(
-        COMMAND ${CMAKE_C_COMPILER} --print-file-name=${specs_filename}
-        OUTPUT_VARIABLE specs_path
+        COMMAND ${CMAKE_C_COMPILER} --print-file-name=picolibc.specs
+        OUTPUT_VARIABLE picolibc_specs_path
         OUTPUT_STRIP_TRAILING_WHITESPACE
         ERROR_QUIET
     )
 
-    if(specs_path AND EXISTS "${specs_path}")
-        file(READ "${specs_path}" specs_content)
+    if(picolibc_specs_path AND EXISTS "${picolibc_specs_path}")
+        # Read the original specs file
+        file(READ "${picolibc_specs_path}" specs_content)
 
-        if(modification STREQUAL "remove_gc_sections")
-            string(REGEX REPLACE "--gc-sections" "" specs_content "${specs_content}")
-        elseif(modification STREQUAL "add_nosys_to_lib")
-            set(original_lib_specs [=[
-%{!shared:%{g*:-lg_nano} %{!p:%{!pg:-lc_nano}}%{p:-lc_p}%{pg:-lc_p}}
-]=])
-            set(modified_lib_specs [=[
-%{!shared:%{g*:-lg_nano} %{!p:%{!pg:-lc_nano -lnosys -lc_nano}}%{p:-lc_p}%{pg:-lc_p}}
-]=])
-            string(REPLACE "${original_lib_specs}" "${modified_lib_specs}" specs_content "${specs_content}")
-        endif()
+        # Remove --gc-sections flag from the content
+        string(REGEX REPLACE "--gc-sections" "" specs_content "${specs_content}")
 
-        file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/specs")
+        # Write the modified specs file
         file(WRITE "${modified_specs_path}" "${specs_content}")
     else()
         # Fallback: use original specs if not found
-        message(WARNING "${specs_filename} not found in toolchain, using default")
-        set(${out_var} "${specs_filename}" PARENT_SCOPE)
+        message(WARNING "picolibc.specs not found in toolchain, using default")
+        set(${out_var} "picolibc.specs" PARENT_SCOPE)
     endif()
-endfunction()
-
-# Get picolibc specs option with --gc-sections flag removed
-function(get_picolibc_specs_path out_var)
-    _get_modified_specs_path(specs_path picolibc.specs remove_gc_sections)
-    set(${out_var} "${specs_path}" PARENT_SCOPE)
-endfunction()
-
-# Get nano specs option with nosys included between libc nano references
-function(get_newlib_nano_specs_path out_var)
-    _get_modified_specs_path(specs_path nano.specs add_nosys_to_lib)
-    set(${out_var} "${specs_path}" PARENT_SCOPE)
 endfunction()
 
 if(CONFIG_IDF_TOOLCHAIN_GCC)
@@ -77,10 +58,9 @@ if(CONFIG_IDF_TOOLCHAIN_GCC)
     endif()
 
     if(CONFIG_LIBC_NEWLIB_NANO_FORMAT)
-        get_newlib_nano_specs_path(nano_specs_path)
-        idf_toolchain_add_flags(LINK_OPTIONS "\"--specs=${nano_specs_path}\"")
+        idf_toolchain_add_flags(LINK_OPTIONS "--specs=nano.specs")
     else()
-        idf_toolchain_remove_flags(LINK_OPTIONS "--specs=.*nano.*")
+        idf_toolchain_remove_flags(LINK_OPTIONS "--specs=nano.specs")
     endif()
 
     idf_toolchain_rerun_abi_detection()
