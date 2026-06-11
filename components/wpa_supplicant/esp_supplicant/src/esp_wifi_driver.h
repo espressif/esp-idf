afd5a13e7125972d95265593cb0e841b5c33c43f
Akshat Agrawal <akshat.agrawal@espressif.com>
2026-06-11 18:56:55 +0530

    Change the NIRA verification logic

--- components/wpa_supplicant/esp_supplicant/src/esp_wifi_driver.h
+++ components/wpa_supplicant/esp_supplicant/src/esp_wifi_driver.h
@@ -340,12 +340,12 @@ void esp_wifi_ap_set_group_mgmt_cipher_internal(wifi_cipher_type_t cipher);
 uint8_t esp_wifi_op_class_supported_internal(uint8_t op_class, uint8_t min_chan, uint8_t max_chan, uint8_t inc, uint8_t bw, channel_bitmap_t *non_pref_channels);
 bool esp_wifi_is_wpa3_compatible_mode_enabled(uint8_t if_index);
 uint8_t esp_wifi_ap_get_owe_config_internal(void);
-esp_err_t esp_nan_disable_pairing(uint8_t svc_id);
+esp_err_t esp_nan_complete_pairing(uint8_t svc_id);
 esp_err_t esp_wifi_nan_load_saved_creds(uint8_t own_nik[ESP_WIFI_NAN_NIK_LEN], bool *own_nik_valid,
-                wifi_nan_peer_creds_t peer_creds[ESP_WIFI_NAN_MAX_PEER_CREDS], uint8_t *num_peer_creds);
+                                        wifi_nan_peer_creds_t peer_creds[ESP_WIFI_NAN_MAX_PEER_CREDS], uint8_t *num_peer_creds);
 esp_err_t esp_wifi_nan_save_own_nik(const uint8_t own_nik[ESP_WIFI_NAN_NIK_LEN]) ;
 esp_err_t esp_wifi_nan_save_creds_for_peer(const uint8_t peer_nik[ESP_WIFI_NAN_NIK_LEN],
-                const uint8_t npk[ESP_WIFI_NAN_NPK_LEN]);
+                                           const uint8_t npk[ESP_WIFI_NAN_NPK_LEN]);
 esp_err_t esp_wifi_nan_erase_all_creds(void);
 
 #endif /* _ESP_WIFI_DRIVER_H_ */
