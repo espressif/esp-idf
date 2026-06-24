ddd4e7c3a3442d779d6b77b9e49fc483199ef05a
sonika.rathi <sonika.rathi@espressif.com>
2026-06-24 13:54:33 +0200

    fix(fatfs): re-enable fatfs and vfs psram/ccomp CI tests

--- components/spiffs/test_apps/pytest_spiffs.py
+++ components/spiffs/test_apps/pytest_spiffs.py
@@ -18,3 +18,16 @@ from pytest_embedded_idf.utils import idf_parametrize
 @idf_parametrize('target', ['esp32', 'esp32c3'], indirect=['target'])
 def test_spiffs_generic(dut: Dut) -> None:
     dut.run_all_single_board_cases(timeout=120)
+
+
+@pytest.mark.quad_psram
+@pytest.mark.parametrize(
+    'config',
+    [
+        'psram',
+    ],
+    indirect=True,
+)
+@idf_parametrize('target', ['esp32s3'], indirect=['target'])
+def test_spiffs_psram(dut: Dut) -> None:
+    dut.run_all_single_board_cases(timeout=120)
