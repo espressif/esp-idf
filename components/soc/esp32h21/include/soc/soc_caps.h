583f5b4de32f9aa90451065119dd1dcc83654619
gaoxu <gaoxu@espressif.com>
2026-06-26 10:55:15 +0800

    feat(rng): basic rng support on ESP32-H21

--- components/soc/esp32h21/include/soc/soc_caps.h
+++ components/soc/esp32h21/include/soc/soc_caps.h
@@ -71,7 +71,7 @@
 #define SOC_RTC_WDT_SUPPORTED           1
 #define SOC_SPI_FLASH_SUPPORTED         1
 #define SOC_SPI_EXTERNAL_NOR_FLASH_SUPPORTED    1
-// #define SOC_RNG_SUPPORTED               1    //TODO: [ESP32H21] IDF-11503
+#define SOC_RNG_SUPPORTED               1
 #define SOC_MODEM_CLOCK_SUPPORTED       1
 #define SOC_REGI2C_SUPPORTED           1
 #define SOC_PHY_SUPPORTED               1
