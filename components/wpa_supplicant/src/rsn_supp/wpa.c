cfeb0b6a6b5f5eb0fd08eb6e396b1303cae76acf
Jouni Malinen <jouni.malinen@oss.qualcomm.com>
2026-07-06 15:33:11 +0300

    Require network_ctx and AKMP match for accepting PMKSA entry
    When wpa_supplicant was processing EAPOL-Key msg 1/4 with a PMKID
    indicated by the AP/Authenticator, a PMKSA for the same AA was accepted
    without enforcing matching network_ctx (i.e., same network configuration
    block) and AKMP. This could allow misbehaving APs to make wpa_supplicant
    use an unacceptable PMKSA entry that was generated for a different
    network for AKMP under certain conditions. This could result in showing
    a connection to an incorrect network when an attacker has credentials to
    one network in wpa_supplicant configuration, but not to another network.
    
    Fix this by accepting the PMKID to set the PMKSA for an association only
    if the PMKSA with the same PMKID is for the same network and was
    generated using the same AKMP.
    
    Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

--- components/wpa_supplicant/src/rsn_supp/wpa.c
+++ components/wpa_supplicant/src/rsn_supp/wpa.c
@@ -405,7 +405,7 @@ static int wpa_supplicant_get_pmk(struct wpa_sm *sm,
          * event before receiving this 1/4 message, so try to find a
          * matching PMKSA cache entry here. */
         sm->cur_pmksa = pmksa_cache_get(sm->pmksa, src_addr, sm->own_addr,
-                pmkid, NULL);
+                pmkid, sm->network_ctx, sm->key_mgmt);
         if (sm->cur_pmksa) {
             wpa_printf(MSG_DEBUG,
                     "RSN: found matching PMKID from PMKSA cache");
@@ -474,7 +474,8 @@ static int wpa_supplicant_get_pmk(struct wpa_sm *sm,
                                      sm->network_ctx, sm->key_mgmt);
             }
             if (!sm->cur_pmksa && pmkid &&
-                pmksa_cache_get(sm->pmksa, src_addr, sm->own_addr, pmkid, NULL))
+                pmksa_cache_get(sm->pmksa, src_addr, sm->own_addr, pmkid,
+                                sm->network_ctx, sm->key_mgmt))
             {
                 wpa_printf( MSG_DEBUG,
                     "RSN: the new PMK matches with the "
@@ -2687,7 +2688,7 @@ int wpa_set_bss(uint8_t *macddr, uint8_t *bssid, uint8_t pairwise_cipher, uint8_
     struct rsn_pmksa_cache_entry *pmksa = NULL;
     if (use_pmk_cache) {
         pmksa = pmksa_cache_get(sm->pmksa, (const u8 *)bssid, sm->own_addr,
-                NULL, NULL);
+                NULL, NULL, 0);
         if (pmksa && (pmksa->akmp != sm->key_mgmt)) {
             use_pmk_cache = false;
         }
