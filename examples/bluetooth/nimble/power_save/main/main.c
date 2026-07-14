872ce2b9ba8dc8fb3e5672541175cbfca2fc8fc6
cjin <jinchen@espressif.com>
2026-07-14 17:25:05 +0800

    feat(ble): updated power save readme for power optimzation

--- examples/bluetooth/nimble/power_save/main/main.c
+++ examples/bluetooth/nimble/power_save/main/main.c
@@ -638,9 +638,11 @@ app_main(void)
 
     nimble_port_freertos_init(bleprph_host_task);
 
+#if CONFIG_EXAMPLE_USE_SCLI
     /* Initialize command line interface to accept input from user */
     rc = scli_init();
     if (rc != ESP_OK) {
         ESP_LOGE(tag, "scli_init() failed");
     }
+#endif // CONFIG_EXAMPLE_USE_SCLI
 }
