a827d27b12e8bcd3fdfbd2aca912d7fb185d82f2
harshal.patil <harshal.patil@espressif.com>
2026-07-21 11:58:12 +0530

    change(security): disable Key Manager support on ESP32-C5/P4/S31
    The Key Manager hardware peripheral in its current form needs further
    design changes before it can be offered as a production feature.
    Until a revised peripheral design is available, withdraw ESP-IDF
    support for it on all Key Manager capable targets.

--- components/soc/esp32p4/include/soc/soc_caps.h
+++ components/soc/esp32p4/include/soc/soc_caps.h
@@ -77,7 +77,7 @@
 #define SOC_ECC_SUPPORTED               1
 #define SOC_ECC_EXTENDED_MODES_SUPPORTED   1
 #define SOC_ECDSA_SUPPORTED             1
-#define SOC_KEY_MANAGER_SUPPORTED       1
+#define SOC_KEY_MANAGER_SUPPORTED       0
 #define SOC_HUK_SUPPORTED               1
 #define SOC_FLASH_ENC_SUPPORTED         1
 #define SOC_SECURE_BOOT_SUPPORTED       1
