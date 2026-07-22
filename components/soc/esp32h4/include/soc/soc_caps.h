b26969aaf64c58ded3fd929cf677df731791c856
linruihao <linruihao@espressif.com>
2026-07-22 17:16:58 +0800

    docs(coex): enable coexist docs for c61 h21 h4 s31

--- components/soc/esp32h4/include/soc/soc_caps.h
+++ components/soc/esp32h4/include/soc/soc_caps.h
@@ -65,7 +65,7 @@
 #define SOC_LEDC_SUPPORTED              1
 #define SOC_I2C_SUPPORTED               1
 #define SOC_SYSTIMER_SUPPORTED          1
-// #define SOC_SUPPORT_COEXISTENCE         1    // TODO: [ESP32H4] IDF-12251 IDF-12252 IDF-12253
+#define SOC_SUPPORT_COEXISTENCE         1
 #define SOC_AES_SUPPORTED               1
 #define SOC_SHA_SUPPORTED               1
 #define SOC_HMAC_SUPPORTED              1
