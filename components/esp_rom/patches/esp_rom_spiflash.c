3631cf25370b0fd037f072c78d0e9810d7cbb162
wuzhenghui <wuzhenghui@espressif.com>
2026-07-31 17:48:43 +0800

    fix(spi_flash): integrate branch prediction control during cache operations

--- components/esp_rom/patches/esp_rom_spiflash.c
+++ components/esp_rom/patches/esp_rom_spiflash.c
@@ -25,10 +25,6 @@
 #include "esp32c61/rom/opi_flash.h"
 #endif
 
-#if SOC_BRANCH_PREDICTOR_SUPPORTED
-#include "riscv/rv_utils.h"
-#endif
-
 #define SPI_IDX   1
 
 #if CONFIG_IDF_TARGET_ESP32
@@ -815,10 +811,6 @@ void esp_rom_opiflash_cache_mode_config(esp_rom_spiflash_read_mode_t mode, const
 extern void rom_spi_flash_disable_cache(uint32_t cpuid, uint32_t *saved_state);
 void spi_flash_disable_cache(uint32_t cpuid, uint32_t *saved_state)
 {
-#if SOC_BRANCH_PREDICTOR_SUPPORTED
-    //branch predictor will start cache request as well
-    rv_utils_dis_branch_predictor();
-#endif
     rom_spi_flash_disable_cache(cpuid, saved_state);
 }
 
@@ -826,8 +818,5 @@ extern void rom_spi_flash_restore_cache(uint32_t cpuid, uint32_t saved_state);
 void spi_flash_restore_cache(uint32_t cpuid, uint32_t saved_state)
 {
     rom_spi_flash_restore_cache(cpuid, saved_state);
-#if SOC_BRANCH_PREDICTOR_SUPPORTED
-    rv_utils_en_branch_predictor();
-#endif
 }
 #endif
