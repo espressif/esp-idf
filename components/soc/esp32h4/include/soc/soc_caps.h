01582e0f121c48eb2bdcd5275315c1e6a0251083
Marius Vikhammer <marius.vikhammer@espressif.com>
2026-08-26 16:43:53 +0800

    fix(soc): use standard touch wakeup cap name on ESP32-H4
    The H4-only spelling was unread, so touch wakeup never compiled in.

--- components/soc/esp32h4/include/soc/soc_caps.h
+++ components/soc/esp32h4/include/soc/soc_caps.h
@@ -477,7 +477,7 @@
 #define SOC_PM_SUPPORT_BT_WAKEUP        (1)
 #define SOC_PM_SUPPORT_EXT1_WAKEUP      (1)
 #define SOC_PM_SUPPORT_EXT1_WAKEUP_MODE_PER_PIN   (1) /*!<Supports one bit per pin to configure the EXT1 trigger level */
-#define SOC_PM_SUPPORT_TOUCH_WAKEUP     (1)
+#define SOC_PM_SUPPORT_TOUCH_SENSOR_WAKEUP    (1)     /*!<Supports waking up from touch pad trigger */
 #define SOC_PM_SUPPORT_CPU_PD           (1)
 #define SOC_PM_SUPPORT_MODEM_PD         (1)
 #define SOC_PM_SUPPORT_XTAL32K_PD       (1)
