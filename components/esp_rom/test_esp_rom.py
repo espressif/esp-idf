b3a54ed36961fbc5ef45c782077ff84669f79997
Zhaoqi Xu <lzy00419@outlook.com>
2026-09-02 05:21:13 +0800

    fix(esp_rom): posixpath.join ESP_ROM_ELF_DIR when generating gdb symbols
    ESP_ROM_ELF_DIR from the Windows export scripts often has no trailing
    separator. Concatenating it with the ROM ELF filename produced
    .../20241011esp32c3_rev101_rom.elf. Use posixpath.join so GDB always
    gets a POSIX path with a slash.
    
    Closes https://github.com/espressif/esp-idf/issues/18418

--- components/esp_rom/test_esp_rom.py
+++ components/esp_rom/test_esp_rom.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
+import importlib.util
 import json
 import os
 
@@ -54,3 +55,36 @@ def test_roms_validate_build_date() -> None:
             build_date_str = get_string_from_elf_by_addr(rom_file, int(k['build_date_str_addr'], base=16))
             assert len(build_date_str) == 11
             assert build_date_str == k['build_date_str']
+
+
+def test_gen_gdbinit_joins_rom_elf_dir() -> None:
+    module_path = os.path.join(os.path.dirname(ROMS_JSON), 'gen_gdbinit.py')
+    spec = importlib.util.spec_from_file_location('gen_gdbinit', module_path)
+    assert spec is not None and spec.loader is not None
+    gen_gdbinit = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(gen_gdbinit)
+
+    rom_dir = '/tmp/esp-rom-elfs/20241011'
+    old = os.environ.get('ESP_ROM_ELF_DIR')
+    os.environ['ESP_ROM_ELF_DIR'] = rom_dir
+    try:
+        out = gen_gdbinit.generate_gdbinit_rom_add_symbols('esp32c3')
+    finally:
+        if old is None:
+            os.environ.pop('ESP_ROM_ELF_DIR', None)
+        else:
+            os.environ['ESP_ROM_ELF_DIR'] = old
+
+    assert f'add-symbol-file {rom_dir}/esp32c3_rev' in out
+    assert f'add-symbol-file {rom_dir}esp32c3_rev' not in out
+
+    os.environ['ESP_ROM_ELF_DIR'] = rom_dir + '/'
+    try:
+        out_slash = gen_gdbinit.generate_gdbinit_rom_add_symbols('esp32c3')
+    finally:
+        if old is None:
+            os.environ.pop('ESP_ROM_ELF_DIR', None)
+        else:
+            os.environ['ESP_ROM_ELF_DIR'] = old
+
+    assert f'add-symbol-file {rom_dir}/esp32c3_rev' in out_slash
