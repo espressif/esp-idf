9afd391d1d1d1d76461112437b03f2713a2a9132
Aditi <aditi.lonkar@espressif.com>
2026-09-10 11:27:53 +0530

    feat(wpa_supplicant): Match SPA when searching PMKSA cache
    Store the supplicant address (SPA) in PMKSA cache entries and match
    it when looking up the PMKSA cache.
    
    (partially picked from commit 18cbdbf2b1998c374b7ef0c00eec4bf708ff6056)

--- components/wpa_supplicant/src/rsn_supp/wpa.c
+++ components/wpa_supplicant/src/rsn_supp/wpa.c
@@ -404,8 +404,8 @@ static int wpa_supplicant_get_pmk(struct wpa_sm *sm,
          * not have enough time to get the association information
          * event before receiving this 1/4 message, so try to find a
          * matching PMKSA cache entry here. */
-        sm->cur_pmksa = pmksa_cache_get(sm->pmksa, src_addr, pmkid,
-                NULL);
+        sm->cur_pmksa = pmksa_cache_get(sm->pmksa, src_addr, sm->own_addr,
+                pmkid, NULL);
         if (sm->cur_pmksa) {
             wpa_printf(MSG_DEBUG,
                     "RSN: found matching PMKID from PMKSA cache");
@@ -454,7 +454,7 @@ static int wpa_supplicant_get_pmk(struct wpa_sm *sm,
                                      sm->network_ctx, sm->key_mgmt);
             }
             if (!sm->cur_pmksa && pmkid &&
-                pmksa_cache_get(sm->pmksa, src_addr, pmkid, NULL))
+                pmksa_cache_get(sm->pmksa, src_addr, sm->own_addr, pmkid, NULL))
             {
                 wpa_printf( MSG_DEBUG,
                     "RSN: the new PMK matches with the "
@@ -2366,7 +2366,8 @@ int wpa_set_bss(char *macddr, char * bssid, u8 pairwise_cipher, u8 group_cipher,
 
     struct rsn_pmksa_cache_entry *pmksa = NULL;
     if (use_pmk_cache) {
-        pmksa = pmksa_cache_get(sm->pmksa, (const u8 *)bssid, NULL, NULL);
+        pmksa = pmksa_cache_get(sm->pmksa, (const u8 *)bssid, sm->own_addr,
+                NULL, NULL);
         if (pmksa && (pmksa->akmp != sm->key_mgmt)) {
             use_pmk_cache = false;
         }
