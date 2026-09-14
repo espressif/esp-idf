6fc617c06a2214b5c5b5bd9fa2bddc5d4f469e0a
Michael.B <xiaoxufeng@espressif.com>
2026-09-14 20:20:22 +0800

    refactor(pmu): move S31 dbias vol gap getter from efuse_ll to pmu_ll
    Keep PMU calibration efuse reads in the PMU LL layer, matching other chips.

--- components/hal/esp32s31/include/hal/efuse_ll.h
+++ components/hal/esp32s31/include/hal/efuse_ll.h
@@ -99,11 +99,6 @@ __attribute__((always_inline)) static inline uint32_t efuse_ll_get_recovery_boot
     return EFUSE.rd_repeat_data5.recovery_bootloader_flash_sector;
 }
 
-__attribute__((always_inline)) static inline int32_t efuse_ll_get_dbias_vol_gap(void)
-{
-    return EFUSE.rd_mac_sys5.lp_hp_dbias_vol_gap;
-}
-
 /******************* eFuse control functions *************************/
 
 __attribute__((always_inline)) static inline bool efuse_ll_get_read_cmd(void)
