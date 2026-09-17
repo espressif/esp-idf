f4d37c005463b145718df7cbae6f24b678d36781
Marius Vikhammer <marius.vikhammer@espressif.com>
2026-09-17 17:50:22 +0800

    test(cpu): drop explicit ESP32-H21 pytest parametrize

--- components/esp_hw_support/test_apps/cpu/pytest_cpu.py
+++ components/esp_hw_support/test_apps/cpu/pytest_cpu.py
@@ -25,7 +25,6 @@ def _run_non_recoverable_case(dut: IdfDut, test_name: str, expected_reason: str)
     'config,target',
     [
         ('default', 'supported_targets'),
-        ('default', 'esp32h21'),
     ],
     indirect=['config', 'target'],
 )
@@ -38,7 +37,6 @@ def test_cpu(dut: IdfDut) -> None:
     'config,target',
     [
         ('default', 'supported_targets'),
-        ('default', 'esp32h21'),
     ],
     indirect=['config', 'target'],
 )
@@ -51,7 +49,6 @@ def test_cpu_reset(dut: IdfDut) -> None:
     'config,target',
     [
         ('default', 'supported_targets'),
-        ('default', 'esp32h21'),
     ],
     indirect=['config', 'target'],
 )
